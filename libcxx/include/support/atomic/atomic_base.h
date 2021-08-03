6fb0a9a384248bd486e200c9fc3e1f59ab0f0fe9
Wesley Maxey <wesley.maxey@gmail.com>
2021-08-02 18:23:26 -0700

    Rename __to_gcc_order to __cxx_atomic_order_to_int

--- libcxx/include/support/atomic/atomic_base.h
+++ libcxx/include/support/atomic/atomic_base.h
@@ -97,7 +97,7 @@ _LIBCUDACXX_INLINE_VISIBILITY auto __cxx_atomic_base_unwrap(_Tp* __a) _NOEXCEPT 
 template <typename _Tp>
 using __cxx_atomic_underlying_t = typename _Tp::__underlying_t;
 
-_LIBCUDACXX_INLINE_VISIBILITY inline _LIBCUDACXX_CONSTEXPR int __to_gcc_order(memory_order __order) {
+_LIBCUDACXX_INLINE_VISIBILITY inline _LIBCUDACXX_CONSTEXPR int __cxx_atomic_order_to_int(memory_order __order) {
   // Avoid switch statement to make this a constexpr.
   return __order == memory_order_relaxed ? __ATOMIC_RELAXED:
          (__order == memory_order_acquire ? __ATOMIC_ACQUIRE:
@@ -107,7 +107,7 @@ _LIBCUDACXX_INLINE_VISIBILITY inline _LIBCUDACXX_CONSTEXPR int __to_gcc_order(me
               __ATOMIC_CONSUME))));
 }
 
-_LIBCUDACXX_INLINE_VISIBILITY inline _LIBCUDACXX_CONSTEXPR int __to_gcc_failure_order(memory_order __order) {
+_LIBCUDACXX_INLINE_VISIBILITY inline _LIBCUDACXX_CONSTEXPR int __cxx_atomic_failure_order_to_int(memory_order __order) {
   // Avoid switch statement to make this a constexpr.
   return __order == memory_order_relaxed ? __ATOMIC_RELAXED:
          (__order == memory_order_acquire ? __ATOMIC_ACQUIRE:
@@ -131,12 +131,12 @@ inline void __cxx_atomic_init(_Tp* __a,  _Up __val) {
 
 inline
 void __cxx_atomic_thread_fence(memory_order __order) {
-  __atomic_thread_fence(__to_gcc_order(__order));
+  __atomic_thread_fence(__cxx_atomic_order_to_int(__order));
 }
 
 inline
 void __cxx_atomic_signal_fence(memory_order __order) {
-  __atomic_signal_fence(__to_gcc_order(__order));
+  __atomic_signal_fence(__cxx_atomic_order_to_int(__order));
 }
 
 template <typename _Tp, typename _Up>
@@ -144,7 +144,7 @@ inline void __cxx_atomic_store(_Tp* __a,  _Up __val,
                         memory_order __order) {
   typename _CUDA_VSTD::remove_cv<_Tp>::type __v_temp(__val);
   (void)__a;
-  __atomic_store(__a, &__v_temp, __to_gcc_order(__order));
+  __atomic_store(__a, &__v_temp, __cxx_atomic_order_to_int(__order));
 }
 
 template <typename _Tp>
@@ -152,7 +152,7 @@ inline auto __cxx_atomic_load(const _Tp* __a,
                        memory_order __order) -> __cxx_atomic_underlying_t<_Tp> {
   typename _CUDA_VSTD::remove_cv<_Tp>::type __ret;
   (void)__a;
-  __atomic_load(__a, &__ret, __to_gcc_order(__order));
+  __atomic_load(__a, &__ret, __cxx_atomic_order_to_int(__order));
   return __ret.__a_value;
 }
 
@@ -162,7 +162,7 @@ inline auto __cxx_atomic_exchange(_Tp* __a, _Up __value,
   typename _CUDA_VSTD::remove_cv<_Tp>::type __v_temp(__value);
   typename _CUDA_VSTD::remove_cv<_Tp>::type __ret;
   (void)__a;
-  __atomic_exchange(__a, &__v_temp, &__ret, __to_gcc_order(__order));
+  __atomic_exchange(__a, &__v_temp, &__ret, __cxx_atomic_order_to_int(__order));
   return __ret.__a_value;
 }
 
@@ -175,8 +175,8 @@ inline bool __cxx_atomic_compare_exchange_strong(
   (void)__expected;
   return __atomic_compare_exchange(__a_tmp, __expected, &__value,
                                    false,
-                                   __to_gcc_order(__success),
-                                   __to_gcc_failure_order(__failure));
+                                   __cxx_atomic_order_to_int(__success),
+                                   __cxx_atomic_failure_order_to_int(__failure));
 }
 
 template <typename _Tp, typename _Up>
@@ -188,8 +188,8 @@ inline bool __cxx_atomic_compare_exchange_weak(
   (void)__expected;
   return __atomic_compare_exchange(__a_tmp, __expected, &__value,
                                    true,
-                                   __to_gcc_order(__success),
-                                   __to_gcc_failure_order(__failure));
+                                   __cxx_atomic_order_to_int(__success),
+                                   __cxx_atomic_failure_order_to_int(__failure));
 }
 
 template <typename _Tp>
@@ -211,7 +211,7 @@ inline auto __cxx_atomic_fetch_add(_Tp* __a, _Td __delta,
   constexpr auto __skip_v = __atomic_ptr_inc<__cxx_atomic_underlying_t<_Tp>>::value;
   auto __a_tmp = __cxx_atomic_base_unwrap(__a);
   return __atomic_fetch_add(__a_tmp, __delta * __skip_v,
-                            __to_gcc_order(__order));
+                            __cxx_atomic_order_to_int(__order));
 }
 
 template <typename _Tp, typename _Td>
@@ -220,7 +220,7 @@ inline auto __cxx_atomic_fetch_sub(_Tp* __a, _Td __delta,
   constexpr auto __skip_v = __atomic_ptr_inc<__cxx_atomic_underlying_t<_Tp>>::value;
   auto __a_tmp = __cxx_atomic_base_unwrap(__a);
   return __atomic_fetch_sub(__a_tmp, __delta * __skip_v,
-                            __to_gcc_order(__order));
+                            __cxx_atomic_order_to_int(__order));
 }
 
 template <typename _Tp, typename _Td>
@@ -228,7 +228,7 @@ inline auto __cxx_atomic_fetch_and(_Tp* __a, _Td __pattern,
                             memory_order __order) -> __cxx_atomic_underlying_t<_Tp> {
   auto __a_tmp = __cxx_atomic_base_unwrap(__a);
   return __atomic_fetch_and(__a_tmp, __pattern,
-                            __to_gcc_order(__order));
+                            __cxx_atomic_order_to_int(__order));
 }
 
 template <typename _Tp, typename _Td>
@@ -236,7 +236,7 @@ inline auto __cxx_atomic_fetch_or(_Tp* __a, _Td __pattern,
                           memory_order __order) -> __cxx_atomic_underlying_t<_Tp> {
   auto __a_tmp = __cxx_atomic_base_unwrap(__a);
   return __atomic_fetch_or(__a_tmp, __pattern,
-                           __to_gcc_order(__order));
+                           __cxx_atomic_order_to_int(__order));
 }
 
 template <typename _Tp, typename _Td>
@@ -244,7 +244,7 @@ inline auto __cxx_atomic_fetch_xor(_Tp* __a, _Td __pattern,
                            memory_order __order) -> __cxx_atomic_underlying_t<_Tp> {
   auto __a_tmp = __cxx_atomic_base_unwrap(__a);
   return __atomic_fetch_xor(__a_tmp, __pattern,
-                            __to_gcc_order(__order));
+                            __cxx_atomic_order_to_int(__order));
 }
 
 inline constexpr
