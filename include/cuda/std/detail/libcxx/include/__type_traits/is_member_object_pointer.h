65d11218aa001aa55ff20f3b1a71472e3b2ee475
Michael Schellenberger Costa <miscco@nvidia.com>
2022-11-29 15:17:25 +0100

    Move `is_member_object_pointer` into its own file

---
+++ include/cuda/std/detail/libcxx/include/__type_traits/is_member_object_pointer.h
@@ -0,0 +1,55 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCUDACXX___TYPE_TRAITS_IS_MEMBER_OBJECT_POINTER_H
+#define _LIBCUDACXX___TYPE_TRAITS_IS_MEMBER_OBJECT_POINTER_H
+
+#ifndef __cuda_std__
+#include <__config>
+#include <__type_traits/integral_constant.h>
+#include <__type_traits/is_member_function_pointer.h>
+#include <__type_traits/remove_cv.h>
+#else
+#include "../__type_traits/integral_constant.h"
+#include "../__type_traits/is_member_function_pointer.h"
+#include "../__type_traits/remove_cv.h"
+#endif // __cuda_std__
+
+#if defined(_LIBCUDACXX_USE_PRAGMA_GCC_SYSTEM_HEADER)
+#pragma GCC system_header
+#endif
+
+_LIBCUDACXX_BEGIN_NAMESPACE_STD
+
+#if __has_builtin(__is_member_object_pointer)
+
+template<class _Tp>
+struct _LIBCUDACXX_TEMPLATE_VIS is_member_object_pointer
+    : _BoolConstant<__is_member_object_pointer(_Tp)> { };
+
+#if _LIBCUDACXX_STD_VER > 11 && !defined(_LIBCUDACXX_HAS_NO_VARIABLE_TEMPLATES)
+template <class _Tp>
+_LIBCUDACXX_INLINE_VAR constexpr bool is_member_object_pointer_v = __is_member_object_pointer(_Tp);
+#endif
+
+#else // __has_builtin(__is_member_object_pointer)
+
+template <class _Tp> struct _LIBCUDACXX_TEMPLATE_VIS is_member_object_pointer
+    : public _BoolConstant< __libcpp_is_member_pointer<__remove_cv_t<_Tp> >::__is_obj >  {};
+
+#if _LIBCUDACXX_STD_VER > 11 && !defined(_LIBCUDACXX_HAS_NO_VARIABLE_TEMPLATES)
+template <class _Tp>
+_LIBCUDACXX_INLINE_VAR constexpr bool is_member_object_pointer_v = is_member_object_pointer<_Tp>::value;
+#endif
+
+#endif // __has_builtin(__is_member_object_pointer)
+
+_LIBCUDACXX_END_NAMESPACE_STD
+
+#endif // _LIBCUDACXX___TYPE_TRAITS_IS_MEMBER_FUNCTION_POINTER_H
