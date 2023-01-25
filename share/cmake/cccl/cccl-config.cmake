5c84765c4e1fdb76a077577c40362437e626f5b2
Allison Vacanti <avacanti@nvidia.com>
2023-01-25 15:05:40 -0500

    Iniital monorepo setup.

---
+++ share/cmake/cccl/cccl-config.cmake
@@ -0,0 +1,59 @@
+#
+# find_package(CCCL) config file.
+#
+# Imports the Thrust, CUB, and libcudacxx components of the NVIDIA
+# CUDA/C++ Core Libraries.
+
+unset(cccl_version) # TODO
+
+set(cccl_cmake_dir "${CMAKE_CURRENT_LIST_DIR}")
+
+if (${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+  set(cccl_quiet_flag "QUIET")
+else()
+  set(cccl_quiet_flag "")
+endif()
+
+foreach(component ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS})
+  unset(req)
+  if (${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED_${component})
+    set(cccl_comp_required_flag "REQUIRED")
+  endif()
+
+  if(component STREQUAL "libcudacxx")
+    find_package(libcudacxx ${cccl_version} CONFIG
+      ${cccl_quiet_flag}
+      ${cccl_comp_required_flag}
+      NO_DEFAULT_PATH # Only check the explicit HINTS below:
+      HINTS
+        "${cccl_cmake_dir}/../../../libcudacxx/lib/cmake/" # Source layout (GitHub)
+        "${cccl_cmake_dir}/.."                             # Install layout
+      )
+  elseif(component STREQUAL "CUB")
+    find_package(CUB ${cccl_version} CONFIG
+      ${cccl_quiet_flag}
+      ${cccl_comp_required_flag}
+      NO_DEFAULT_PATH # Only check the explicit HINTS below:
+      HINTS
+        "${cccl_cmake_dir}/../../../cub/cub/cmake/" # Source layout (GitHub)
+        "${cccl_cmake_dir}/.."                      # Install layout
+    )
+  elseif(component STREQUAL "Thrust")
+    find_package(Thrust ${cccl_version} CONFIG
+      ${cccl_quiet_flag}
+      ${cccl_comp_required_flag}
+      NO_DEFAULT_PATH # Only check the explicit HINTS below:
+      HINTS
+        "${cccl_cmake_dir}/../../../thrust/thrust/cmake/" # Source layout (GitHub)
+        "${cccl_cmake_dir}/.."                            # Install layout
+    )
+  else()
+    message(FATAL_ERROR "Invalid CCCL component requested: '${component}'")
+  endif()
+endforeach()
+
+include(FindPackageHandleStandardArgs)
+if (NOT CCCL_CONFIG)
+  set(CCCL_CONFIG "${CMAKE_CURRENT_LIST_FILE}")
+endif()
+find_package_handle_standard_args(CCCL CONFIG_MODE)
