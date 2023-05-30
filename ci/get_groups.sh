f81a015ef182d767396272515662b0b8705a8d33
Jake Hemstad <jhemstad@nvidia.com>
2023-05-30 07:59:30 -0500

    Update how output is returned from get_groups

--- ci/get_groups.sh
+++ ci/get_groups.sh
@@ -3,17 +3,16 @@
 # Ensure that the script is being executed from its directory
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
-# Check if input JSON, field, and file arguments are provided
-if [ "$#" -ne 3 ]; then
-  echo "Usage: ./get_groups.sh <input_json> <field> <output_file>"
+# Check if input JSON and field arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./get_groups.sh <input_json> <field>"
   exit 1
 fi
 
 # Assign command-line arguments to variables
 input_json="$1"
 field="$2"
-output_file="$3"
 
 output=$(echo $input_json | jq -L . -c --arg field "$field" 'include "group_by_field"; .include | group_by_field($field)')
-echo $output | tee -a "$output_file"
+echo $output 
 
