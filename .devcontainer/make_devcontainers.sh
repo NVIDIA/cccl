007cdebe88fa8a5508970e2c353c9f948204e2b1
Jake Hemstad <jhemstad@nvidia.com>
2023-06-05 14:59:36 +0000

    Add name field to devcontainers.

--- .devcontainer/make_devcontainers.sh
+++ .devcontainer/make_devcontainers.sh
@@ -29,17 +29,14 @@ for combination in $combinations; do
     compiler_version=$(echo "$combination" | jq -r '.compiler_version')
     ubuntu_version=$(echo "$combination" | jq -r '.ubuntu')
 
-    directory="cuda$cuda_version-$compiler_name$compiler_version"
-
-    mkdir -p "$directory"
-
-    devcontainer_file="$directory/devcontainer.json"
-
-    image_name="$IMAGE_ROOT$compiler_name$compiler_version-cuda$cuda_version-ubuntu$ubuntu_version"
+    name="cuda$cuda_version-$compiler_name$compiler_version"
+    mkdir -p "$name"
+    devcontainer_file="$name/devcontainer.json"
+    image="$IMAGE_ROOT$compiler_name$compiler_version-cuda$cuda_version-ubuntu$ubuntu_version"
 
     # Use the base_devcontainer.json as a template, plug in the CUDA, compiler names, versions, and Ubuntu version,
     # and write the output to the new devcontainer.json file
-    jq --arg image_name "$image_name" '. + {image: $image_name}' $base_devcontainer_file > "$devcontainer_file"
+    jq --arg image "$image"  --arg name "$name" '. + {image: $image, name: $name}' $base_devcontainer_file > "$devcontainer_file"
 
     echo "Created $devcontainer_file"
 done
