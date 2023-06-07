6f38850224e7903cc3bb3b0f35fe78e111649dc1
Jake Hemstad <jhemstad@nvidia.com>
2023-06-07 15:31:14 +0000

    Add libcu++ test script and enable libcu++ test jobs.

--- ci/test_libcudacxx.sh
+++ ci/test_libcudacxx.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-echo "test libcudacxx"
+source "$(dirname "$0")/build_common.sh"
 
-# Require a compiler to be specified
-#if [ $# -ne 1 ]; then
-#    echo "Usage: $0 <CXX_COMPILER>"
-#    exit 1
-#fi
+cmake -S .. -B ../build \
+    -DCCCL_ENABLE_THRUST=OFF \
+    -DCCCL_ENABLE_LIBCUDACXX=ON \
+    -DCCCL_ENABLE_CUB=OFF \
+    -DCCCL_ENABLE_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=${HOST_COMPILER} \
+    -DCMAKE_CUDA_ARCHITECTURES=${GPU_ARCHS} \
+    -DCMAKE_CUDA_HOST_COMPILER=${HOST_COMPILER} \
+    -DLIBCUDACXX_ENABLE_LIBCUDACXX_TESTS=ON \
+    -Dlibcudacxx_ENABLE_INSTALL_RULES=ON \
+    -DCUB_ENABLE_INSTALL_RULES=ON \
+    -DTHRUST_ENABLE_INSTALL_RULES=ON \
+    -G Ninja
 
-# First argument is the CXX compiler
-#CXX_COMPILER=$1
+LIBCUDACXX_SITE_CONFIG="../build/libcudacxx/test/lit.site.cfg" lit -v --no-progress-bar -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
 
-# Clone Thrust repository 
-#git clone --recursive --depth=1 https://github.com/NVIDIA/thrust.git
-
-# Configure Thrust
-#cmake -S thrust -B thrust/build -GNinja -DTHRUST_DISABLE_ARCH_BY_DEFAULT=ON -DTHRUST_ENABLE_COMPUTE_70=ON #-DCMAKE_CXX_COMPILER=${CXX_COMPILER}
-
-
-# Build Thrust tests
-#cmake --build thrust/build
-
-
-# Run Thrust tests
-#ctest --output-on-failure
