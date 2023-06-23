25878222d43445437aa377ab8fce1d1083155098
Jake Hemstad <jhemstad@nvidia.com>
2023-06-23 15:12:11 +0000

    Make hit rate script more robust to allow spaces at start.

--- ci/sccache_hit_rate.sh
+++ ci/sccache_hit_rate.sh
@@ -19,18 +19,21 @@ cat $2 >&2
 echo "=== End of $2 ===" >&2
 
 # Extract compile requests and cache hits from the before and after files
-requests_before=$(awk '/^Compile requests[[:space:]]+[[:digit:]]+/ {print $3}' $1)
-hits_before=$(awk '/^Cache hits[[:space:]]+[[:digit:]]+/ {print $3}' $1)
-requests_after=$(awk '/^Compile requests[[:space:]]+[[:digit:]]+/ {print $3}' $2)
-hits_after=$(awk '/^Cache hits[[:space:]]+[[:digit:]]+/ {print $3}' $2)
+requests_before=$(awk '/^[[:space:]]*Compile requests[[:space:]]+[[:digit:]]+/ {print $3}' $1)
+hits_before=$(awk '/^[[:space:]]*Cache hits[[:space:]]+[[:digit:]]+/ {print $3}' $1)
+requests_after=$(awk '/^[[:space:]]*Compile requests[[:space:]]+[[:digit:]]+/ {print $3}' $2)
+hits_after=$(awk '/^[[:space:]]*Cache hits[[:space:]]+[[:digit:]]+/ {print $3}' $2)
 
 # Calculate the differences to find out how many new requests and hits
 requests_diff=$((requests_after - requests_before))
 hits_diff=$((hits_after - hits_before))
 
+echo "New Compile Requests: $requests_diff" >&2
+echo "New Hits: $hits_diff" >&2
+
 # Calculate and print the hit rate
 if [ $requests_diff -eq 0 ]; then
-    echo "No new compile requests, hit rate is not applicable" 
+    echo "No new compile requests, hit rate is not applicable"
 else
     hit_rate=$(awk -v hits=$hits_diff -v requests=$requests_diff 'BEGIN {printf "%.2f", hits/requests * 100}')
     echo "sccache hit rate: $hit_rate%" >&2
