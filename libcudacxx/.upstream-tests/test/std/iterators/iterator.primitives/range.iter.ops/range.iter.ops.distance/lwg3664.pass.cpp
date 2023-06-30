19444643fdb6dfc72c1399ab48265ab58f2d15e3
Michael Schellenberger Costa <miscco@nvidia.com>
2023-06-30 09:03:04 +0200

    Implement `ranges::distance`
    NOTE: This is currently disabled, as it relies on ranges features such as `ranges::begin` and `ranges::size`

---
+++ libcudacxx/.upstream-tests/test/std/iterators/iterator.primitives/range.iter.ops/range.iter.ops.distance/lwg3664.pass.cpp
@@ -0,0 +1,88 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// XFAIL: c++17, c++20
+
+// template<class I, sentinel_for<I> S>
+//   requires (!sized_sentinel_for<S, I>)
+//     constexpr iter_difference_t<I> ranges::distance(I first, S last);
+//
+// template<class I, sized_sentinel_for<decay_t<I>> S>
+//   constexpr iter_difference_t<I> ranges::distance(const I& first, S last);
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic ignored "-Wnon-template-friend"
+#endif
+
+#include <cuda/std/iterator>
+#include <cuda/std/cassert>
+
+#include "test_iterators.h"
+
+template<class It>
+struct EvilSentinel {
+  It p_;
+  __host__ __device__ friend constexpr bool operator==(EvilSentinel s, It p) { return s.p_ == p; }
+#if TEST_STD_VER < 20
+  __host__ __device__ friend constexpr bool operator==(It p, EvilSentinel s) { return s.p_ == p; }
+  __host__ __device__ friend constexpr bool operator!=(EvilSentinel s, It p) { return s.p_ != p; }
+  __host__ __device__ friend constexpr bool operator!=(It p, EvilSentinel s) { return s.p_ != p; }
+#endif
+  __host__ __device__ friend constexpr auto operator-(EvilSentinel s, It p) { return s.p_ - p; }
+  __host__ __device__ friend constexpr auto operator-(It p, EvilSentinel s) { return p - s.p_; }
+// Older clang confuses the all deleted overloads
+#if (!defined(TEST_CLANG_VER) || TEST_CLANG_VER >= 1000)
+  __host__ __device__ friend constexpr void operator-(EvilSentinel s, int(&)[3]) = delete;
+  __host__ __device__ friend constexpr void operator-(EvilSentinel s, const int(&)[3]) = delete;
+// Older gcc confuses the rvalue overloads with the lvalue overloads and complains about duplicated function definitions
+#if (!defined(TEST_COMPILER_GCC)|| __GNUC__ >= 10)
+  __host__ __device__ friend constexpr void operator-(EvilSentinel s, int(&&)[3]) = delete;
+  __host__ __device__ friend constexpr void operator-(EvilSentinel s, const int(&&)[3]) = delete;
+#endif
+#endif
+};
+static_assert( cuda::std::sized_sentinel_for<EvilSentinel<int*>, int*>);
+static_assert(!cuda::std::sized_sentinel_for<EvilSentinel<int*>, const int*>);
+static_assert( cuda::std::sized_sentinel_for<EvilSentinel<const int*>, int*>);
+static_assert( cuda::std::sized_sentinel_for<EvilSentinel<const int*>, const int*>);
+
+__host__ __device__ constexpr bool test() {
+  {
+    int a[] = {1, 2, 3};
+    assert(cuda::std::ranges::distance(a, a + 3) == 3);
+    assert(cuda::std::ranges::distance(a, a) == 0);
+    assert(cuda::std::ranges::distance(a + 3, a) == -3);
+  }
+  {
+    int a[] = {1, 2, 3};
+    assert(cuda::std::ranges::distance(a, EvilSentinel<int*>{a+3}) == 3);
+    assert(cuda::std::ranges::distance(a, EvilSentinel<int*>{a}) == 0);
+    assert(cuda::std::ranges::distance(a+3, EvilSentinel<int*>{a}) == -3);
+    assert(cuda::std::ranges::distance(cuda::std::move(a), EvilSentinel<int*>{a+3}) == 3);
+  }
+  {
+    const int a[] = {1, 2, 3};
+    assert(cuda::std::ranges::distance(a, EvilSentinel<const int*>{a+3}) == 3);
+    assert(cuda::std::ranges::distance(a, EvilSentinel<const int*>{a}) == 0);
+    assert(cuda::std::ranges::distance(a+3, EvilSentinel<const int*>{a}) == -3);
+    assert(cuda::std::ranges::distance(cuda::std::move(a), EvilSentinel<const int*>{a+3}) == 3);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), const int(&)[3], EvilSentinel<int*>>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), const int(&&)[3], EvilSentinel<int*>>);
+  }
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
---
+++ libcudacxx/.upstream-tests/test/std/iterators/iterator.primitives/range.iter.ops/range.iter.ops.distance/range.pass.cpp
@@ -0,0 +1,112 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// XFAIL: c++17, c++20
+
+// template<range R>
+//   constexpr range_difference_t<R> ranges::distance(R&& r);
+
+#include <cuda/std/iterator>
+#include <cuda/std/cassert>
+#include <cuda/std/ranges>
+
+#include "test_iterators.h"
+#include "test_macros.h"
+
+template<class It, class Sent>
+__host__ __device__ constexpr void test_ordinary() {
+  struct R {
+    mutable int a[3] = {1, 2, 3};
+    __host__ __device__ constexpr It begin() const { return It(a); }
+    __host__ __device__ constexpr Sent end() const { return Sent(It(a + 3)); }
+  };
+  R r;
+  assert(cuda::std::ranges::distance(r) == 3);
+  assert(cuda::std::ranges::distance(static_cast<R&&>(r)) == 3);
+  assert(cuda::std::ranges::distance(static_cast<const R&>(r)) == 3);
+  assert(cuda::std::ranges::distance(static_cast<const R&&>(r)) == 3);
+  ASSERT_SAME_TYPE(decltype(cuda::std::ranges::distance(r)), cuda::std::ranges::range_difference_t<R>);
+}
+
+__host__ __device__ constexpr bool test() {
+  {
+    using R = int[3];
+    int a[] = {1, 2, 3};
+    assert(cuda::std::ranges::distance(static_cast<R&>(a)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<R&&>(a)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const R&>(a)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const R&&>(a)) == 3);
+    ASSERT_SAME_TYPE(decltype(cuda::std::ranges::distance(a)), cuda::std::ptrdiff_t);
+    ASSERT_SAME_TYPE(decltype(cuda::std::ranges::distance(a)), cuda::std::ranges::range_difference_t<R>);
+  }
+#ifdef _LIBCUDACXX_HAS_RANGES
+  {
+    // Unsized range, non-copyable iterator type, rvalue-ref-qualified begin()
+    using It = cpp20_input_iterator<int*>;
+    using Sent = sentinel_wrapper<cpp20_input_iterator<int*>>;
+    using R = cuda::std::ranges::subrange<It, Sent, cuda::std::ranges::subrange_kind::unsized>;
+
+    int a[] = {1, 2, 3};
+    auto r = R(It(a), Sent(It(a + 3)));
+    assert(cuda::std::ranges::distance(r) == 3);
+    assert(cuda::std::ranges::distance(static_cast<R&&>(r)) == 3);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), const R&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), const R&&>);
+  }
+  {
+    // Sized range (unsized sentinel type), non-copyable iterator type, rvalue-ref-qualified begin()
+    using It = cpp20_input_iterator<int*>;
+    using Sent = sentinel_wrapper<cpp20_input_iterator<int*>>;
+    using R = cuda::std::ranges::subrange<It, Sent, cuda::std::ranges::subrange_kind::sized>;
+
+    int a[] = {1, 2, 3};
+    auto r = R(It(a), Sent(It(a + 3)), 3);
+    assert(cuda::std::ranges::distance(r) == 3);
+    assert(cuda::std::ranges::distance(static_cast<R&&>(r)) == 3);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), const R&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), const R&&>);
+  }
+#endif
+  {
+    // Sized range (sized sentinel type), non-copyable iterator type
+    test_ordinary<cpp20_input_iterator<int*>, sized_sentinel<cpp20_input_iterator<int*>>>();
+  }
+  test_ordinary<cpp17_input_iterator<int*>, sentinel_wrapper<cpp17_input_iterator<int*>>>();
+  test_ordinary<cpp20_input_iterator<int*>, sentinel_wrapper<cpp20_input_iterator<int*>>>();
+  test_ordinary<cpp17_output_iterator<int*>, sentinel_wrapper<cpp17_output_iterator<int*>>>();
+  test_ordinary<forward_iterator<int*>, sentinel_wrapper<forward_iterator<int*>>>();
+  test_ordinary<bidirectional_iterator<int*>, sentinel_wrapper<bidirectional_iterator<int*>>>();
+  test_ordinary<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>();
+  test_ordinary<contiguous_iterator<int*>, sentinel_wrapper<contiguous_iterator<int*>>>();
+  test_ordinary<int*, sentinel_wrapper<int*>>();
+
+  test_ordinary<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>();
+  test_ordinary<cpp20_input_iterator<int*>, sized_sentinel<cpp20_input_iterator<int*>>>();
+  test_ordinary<cpp17_output_iterator<int*>, sized_sentinel<cpp17_output_iterator<int*>>>();
+  test_ordinary<forward_iterator<int*>, sized_sentinel<forward_iterator<int*>>>();
+  test_ordinary<bidirectional_iterator<int*>, sized_sentinel<bidirectional_iterator<int*>>>();
+  test_ordinary<random_access_iterator<int*>, sized_sentinel<random_access_iterator<int*>>>();
+  test_ordinary<contiguous_iterator<int*>, sized_sentinel<contiguous_iterator<int*>>>();
+  test_ordinary<int*, sized_sentinel<int*>>();
+  test_ordinary<int*, int*>();
+
+  // Calling it on a non-range isn't allowed.
+  static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), int>);
+  static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), int*>);
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
