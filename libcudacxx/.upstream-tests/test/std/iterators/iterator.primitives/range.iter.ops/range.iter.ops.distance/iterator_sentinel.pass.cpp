76d7d9c039562bc2b9152f07f2b89a94bfd34de4
Michael Schellenberger Costa <miscco@nvidia.com>
2023-06-30 09:03:04 +0200

    Implement `ranges::distance`
    NOTE: This is currently disabled, as it relies on ranges features such as `ranges::begin` and `ranges::size`

---
+++ libcudacxx/.upstream-tests/test/std/iterators/iterator.primitives/range.iter.ops/range.iter.ops.distance/iterator_sentinel.pass.cpp
@@ -0,0 +1,244 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// XFAIL: c++17, c++20
+
+// template<class I, sentinel_for<I> S>
+//   requires (!sized_sentinel_for<S, I>)
+//     constexpr iter_difference_t<I> ranges::distance(I first, S last);
+//
+// template<class I, sized_sentinel_for<decay_t<I>> S>
+//   constexpr iter_difference_t<I> ranges::distance(I&& first, S last); // TODO: update when LWG3664 is resolved
+
+#include <cuda/std/iterator>
+#include <cuda/std/cassert>
+
+#include "test_iterators.h"
+#include "test_macros.h"
+
+template<class It, class Sent>
+__host__ __device__ constexpr void test_unsized() {
+  static_assert(cuda::std::sentinel_for<Sent, It> && !cuda::std::sized_sentinel_for<Sent, It>);
+  int a[3] = {1,2,3};
+  {
+    It first = It(a);
+    auto last = Sent(It(a));
+    assert(cuda::std::ranges::distance(first, last) == 0);
+    assert(cuda::std::ranges::distance(It(a), last) == 0);
+    assert(cuda::std::ranges::distance(first, Sent(It(a))) == 0);
+    assert(cuda::std::ranges::distance(It(a), Sent(It(a))) == 0);
+    ASSERT_SAME_TYPE(decltype(cuda::std::ranges::distance(It(a), Sent(It(a)))), cuda::std::iter_difference_t<It>);
+  }
+  {
+    It first = It(a);
+    auto last = Sent(It(a + 3));
+    assert(cuda::std::ranges::distance(first, last) == 3);
+
+    // Test all const/ref-qualifications of both operands.
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<const Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<const Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<const Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<const Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<const Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<const Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<Sent&&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<const Sent&>(last)) == 3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<const Sent&&>(last)) == 3);
+  }
+}
+
+template<class It, class Sent>
+__host__ __device__ constexpr void test_sized() {
+  static_assert(cuda::std::sized_sentinel_for<Sent, It>);
+  int a[] = {1,2,3};
+  {
+    It first = It(a + 3);
+    auto last = Sent(It(a));
+    assert(cuda::std::ranges::distance(first, last) == -3);
+
+    // Test all const/ref-qualifications of both operands.
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<const Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&>(first), static_cast<const Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<const Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<It&&>(first), static_cast<const Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<const Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&>(first), static_cast<const Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<Sent&&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<const Sent&>(last)) == -3);
+    assert(cuda::std::ranges::distance(static_cast<const It&&>(first), static_cast<const Sent&&>(last)) == -3);
+  }
+  {
+    It first = It(a);
+    auto last = Sent(It(a));
+    assert(cuda::std::ranges::distance(first, last) == 0);
+    assert(cuda::std::ranges::distance(It(a), last) == 0);
+    assert(cuda::std::ranges::distance(first, Sent(It(a))) == 0);
+    assert(cuda::std::ranges::distance(It(a), Sent(It(a))) == 0);
+    ASSERT_SAME_TYPE(decltype(cuda::std::ranges::distance(It(a), Sent(It(a)))), cuda::std::iter_difference_t<It>);
+  }
+  {
+    It first = It(a);
+    auto last = Sent(It(a + 3));
+    assert(cuda::std::ranges::distance(first, last) == 3);
+    assert(cuda::std::ranges::distance(It(a), last) == 3);
+    assert(cuda::std::ranges::distance(first, Sent(It(a + 3))) == 3);
+    assert(cuda::std::ranges::distance(It(a), Sent(It(a + 3))) == 3);
+  }
+}
+
+struct StrideCounter {
+  int *it_;
+  int *inc_;
+  using value_type = int;
+  using difference_type = int;
+  __host__ __device__ explicit StrideCounter();
+  __host__ __device__ constexpr explicit StrideCounter(int *it, int *inc) : it_(it), inc_(inc) {}
+  __host__ __device__ constexpr auto& operator++() { ++it_; *inc_ += 1; return *this; }
+  __host__ __device__ StrideCounter operator++(int);
+  __host__ __device__ int& operator*() const;
+  __host__ __device__ bool operator==(StrideCounter) const;
+  __host__ __device__ bool operator!=(StrideCounter) const;
+};
+static_assert(cuda::std::forward_iterator<StrideCounter>);
+static_assert(!cuda::std::sized_sentinel_for<StrideCounter, StrideCounter>);
+
+struct SizedStrideCounter {
+  int *it_;
+  int *minus_;
+  using value_type = int;
+  __host__ __device__ explicit SizedStrideCounter();
+  __host__ __device__ constexpr explicit SizedStrideCounter(int *it, int *minus) : it_(it), minus_(minus) {}
+  __host__ __device__ SizedStrideCounter& operator++();
+  __host__ __device__ SizedStrideCounter operator++(int);
+  __host__ __device__ int& operator*() const;
+  __host__ __device__ bool operator==(SizedStrideCounter) const;
+  __host__ __device__ bool operator!=(SizedStrideCounter) const;
+  __host__ __device__ constexpr int operator-(SizedStrideCounter rhs) const {
+    *minus_ += 1;
+    return static_cast<int>(it_ - rhs.it_);
+  }
+};
+static_assert(cuda::std::forward_iterator<SizedStrideCounter>);
+static_assert(cuda::std::sized_sentinel_for<SizedStrideCounter, SizedStrideCounter>);
+
+__host__ __device__ constexpr void test_stride_counting() {
+  {
+    int a[] = {1, 2, 3};
+    int inc = 0;
+    StrideCounter first(a, &inc);
+    StrideCounter last(a+3, nullptr);
+    decltype(auto) result = cuda::std::ranges::distance(first, last);
+    static_assert(cuda::std::same_as<decltype(result), int>);
+    assert(result == 3);
+    assert(inc == 3);
+  }
+  {
+    int a[] = {1, 2, 3};
+    int minus = 0;
+    SizedStrideCounter first(a, &minus);
+    SizedStrideCounter last(a+3, nullptr);
+    decltype(auto) result = cuda::std::ranges::distance(first, last);
+    static_assert(cuda::std::same_as<decltype(result), int>);
+    assert(result == 3);
+    assert(minus == 1);
+  }
+}
+
+__host__ __device__ constexpr bool test() {
+  {
+    int a[] = {1, 2, 3};
+    assert(cuda::std::ranges::distance(a, a + 3) == 3);
+    assert(cuda::std::ranges::distance(a, a) == 0);
+    assert(cuda::std::ranges::distance(a + 3, a) == -3);
+  }
+
+  test_unsized<cpp17_input_iterator<int*>, sentinel_wrapper<cpp17_input_iterator<int*>>>();
+  test_unsized<cpp17_output_iterator<int*>, sentinel_wrapper<cpp17_output_iterator<int*>>>();
+  test_unsized<forward_iterator<int*>, sentinel_wrapper<forward_iterator<int*>>>();
+  test_unsized<bidirectional_iterator<int*>, sentinel_wrapper<bidirectional_iterator<int*>>>();
+  test_unsized<random_access_iterator<int*>, sentinel_wrapper<random_access_iterator<int*>>>();
+  test_unsized<contiguous_iterator<int*>, sentinel_wrapper<contiguous_iterator<int*>>>();
+  test_unsized<int*, sentinel_wrapper<int*>>();
+  test_unsized<const int*, sentinel_wrapper<const int*>>();
+  test_unsized<forward_iterator<int*>, forward_iterator<int*>>();
+  test_unsized<bidirectional_iterator<int*>, bidirectional_iterator<int*>>();
+
+  test_sized<cpp17_input_iterator<int*>, sized_sentinel<cpp17_input_iterator<int*>>>();
+  test_sized<cpp20_input_iterator<int*>, sized_sentinel<cpp20_input_iterator<int*>>>();
+  test_sized<cpp17_output_iterator<int*>, sized_sentinel<cpp17_output_iterator<int*>>>();
+  test_sized<forward_iterator<int*>, sized_sentinel<forward_iterator<int*>>>();
+  test_sized<bidirectional_iterator<int*>, sized_sentinel<bidirectional_iterator<int*>>>();
+  test_sized<random_access_iterator<int*>, sized_sentinel<random_access_iterator<int*>>>();
+  test_sized<contiguous_iterator<int*>, sized_sentinel<contiguous_iterator<int*>>>();
+  test_sized<int*, sized_sentinel<int*>>();
+  test_sized<const int*, sized_sentinel<const int*>>();
+  test_sized<int*, int*>();
+  test_sized<int*, const int*>();
+  test_sized<random_access_iterator<int*>, random_access_iterator<int*>>();
+  test_sized<contiguous_iterator<int*>, contiguous_iterator<int*>>();
+
+  {
+    using It = cpp20_input_iterator<int*>;  // non-copyable, thus not a sentinel for itself
+    static_assert(!cuda::std::is_copy_constructible_v<It>);
+    static_assert(!cuda::std::sentinel_for<It, It>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&, It&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&, It&&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&&, It&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&&, It&&>);
+  }
+  {
+    using It = cpp20_input_iterator<int*>;  // non-copyable
+    using Sent = sentinel_wrapper<It>;  // not a sized sentinel
+    static_assert(cuda::std::sentinel_for<Sent, It> && !cuda::std::sized_sentinel_for<Sent, It>);
+    int a[] = {1,2,3};
+    Sent last = Sent(It(a + 3));
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&, Sent&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&, Sent&&>);
+    assert(cuda::std::ranges::distance(It(a), last) == 3);
+    assert(cuda::std::ranges::distance(It(a), Sent(It(a + 3))) == 3);
+  }
+  {
+    using It = cpp17_input_iterator<int*>;  // not a sentinel for itself
+    static_assert(!cuda::std::sentinel_for<It, It>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&, It&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&, It&&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&&, It&>);
+    static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), It&&, It&&>);
+  }
+
+  // Calling it on a non-iterator or non-sentinel isn't allowed.
+  static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), int, int>);
+  static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), int*, int>);
+  static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), int, int*>);
+  static_assert(!cuda::std::is_invocable_v<decltype(cuda::std::ranges::distance), int*, char*>);
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
--- libcudacxx/include/cuda/std/detail/libcxx/include/__iterator/distance.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__iterator/distance.h
@@ -15,7 +15,16 @@
 #include <__config>
 #endif // __cuda_std__
 
+#include "../__iterator/concepts.h"
+#include "../__iterator/incrementable_traits.h"
 #include "../__iterator/iterator_traits.h"
+#ifdef _LIBCUDACXX_HAS_RANGES
+#include "../__ranges/access.h"
+#include "../__ranges/concepts.h"
+#include "../__ranges/size.h"
+#endif // _LIBCUDACXX_HAS_RANGES
+#include "../__type_traits/decay.h"
+#include "../__type_traits/remove_cvref.h"
 
 #if defined(_LIBCUDACXX_USE_PRAGMA_GCC_SYSTEM_HEADER)
 #pragma GCC system_header
@@ -47,10 +56,62 @@ inline _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX14
 typename iterator_traits<_InputIter>::difference_type
 distance(_InputIter __first, _InputIter __last)
 {
-    return __distance(__first, __last, typename iterator_traits<_InputIter>::iterator_category());
+    return _CUDA_VSTD::__distance(__first, __last, typename iterator_traits<_InputIter>::iterator_category());
 }
 
-
 _LIBCUDACXX_END_NAMESPACE_STD
+#ifdef _LIBCUDACXX_HAS_RANGES
+#if _LIBCUDACXX_STD_VER > 14
+
+// [range.iter.op.distance]
+
+_LIBCUDACXX_BEGIN_NAMESPACE_RANGES
+_LIBCUDACXX_BEGIN_NAMESPACE_CPO(__distance)
+struct __fn {
+  _LIBCUDACXX_TEMPLATE(class _Ip, class _Sp)
+    (requires (sentinel_for<_Sp, _Ip> && !sized_sentinel_for<_Sp, _Ip>))
+  _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY
+  constexpr iter_difference_t<_Ip> operator()(_Ip __first, _Sp __last) const {
+    iter_difference_t<_Ip> __n = 0;
+    while (__first != __last) {
+      ++__first;
+      ++__n;
+    }
+    return __n;
+  }
+
+  _LIBCUDACXX_TEMPLATE(class _Ip, class _Sp)
+    (requires (sized_sentinel_for<_Sp, decay_t<_Ip>>))
+  _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY
+  constexpr iter_difference_t<_Ip> operator()(_Ip&& __first, _Sp __last) const {
+    if constexpr (sized_sentinel_for<_Sp, remove_cvref_t<_Ip>>) {
+      return __last - __first;
+    } else {
+      return __last - decay_t<_Ip>(__first);
+    }
+    _LIBCUDACXX_UNREACHABLE();
+  }
+
+  _LIBCUDACXX_TEMPLATE(class _Rp)
+    (requires (range<_Rp>))
+  _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY
+  constexpr range_difference_t<_Rp> operator()(_Rp&& __r) const {
+    if constexpr (sized_range<_Rp>) {
+      return static_cast<range_difference_t<_Rp>>(_CUDA_VRANGES::size(__r));
+    } else {
+      return operator()(_CUDA_VRANGES::begin(__r), _CUDA_VRANGES::end(__r));
+    }
+    _LIBCUDACXX_UNREACHABLE();
+  }
+};
+_LIBCUDACXX_END_NAMESPACE_CPO
+
+inline namespace __cpo {
+  _LIBCUDACXX_CPO_ACCESSIBILITY auto distance = __distance::__fn{};
+} // namespace __cpo
+_LIBCUDACXX_END_NAMESPACE_RANGES
+
+#endif // _LIBCUDACXX_STD_VER > 14
+#endif // _LIBCUDACXX_HAS_RANGES
 
 #endif // _LIBCUDACXX___ITERATOR_DISTANCE_H
