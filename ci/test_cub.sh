dcb741b4404747c49ead34757f3ed2bd79ea7f02
Jake Hemstad <jhemstad@nvidia.com>
2023-07-05 23:13:38 +0000

    Exclude device_radix_sort CUB tests for now.

--- ci/test_cub.sh
+++ ci/test_cub.sh
@@ -4,6 +4,6 @@ source "$(dirname "$0")/build_common.sh"
 
 ./build_cub.sh "$@"
 
-ctest --test-dir ${BUILD_DIR} --output-on-failure 
+ctest --test-dir ${BUILD_DIR} --output-on-failure -E device_radix_sort
 
 echo "CUB test complete"
