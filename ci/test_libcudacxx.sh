a2d5000accfbf409711965d2db5fc55e562ab2ca
Jake Hemstad <jhemstad@nvidia.com>
2023-07-10 12:26:31 -0500

    Enable tests (#167)
    * Add DEVCONTAINER_NAME env variable to devcontainers.
    
    * Remove enabling install rules.
    
    These were needed for the cmake install testing which is disabled.
    
    We will add a separate job for testing the install.
    
    * Factor out common build/configure logic.
    
    * Add compile commands to common cmake options.
    
    * Update sccache_stats to selectively add "notfice" prefix when running on action runner.
    
    * Remove manual sccache stats calculation from workflow.
    
    This is no longer needed because the build scripts do it automatically.
    
    * Remove devcontainer name env variable.
    
    * Revert "Remove devcontainer name env variable."
    
    This reverts commit 2cbc501650ff7ed72eed4b490637af7f548fa810.
    
    * Use devcontainer specific build directory.
    
    * Get rid of stale Windows runs-on handling.
    
    * Make test job conditional on new input.
    
    * Add simple matrix for test.
    
    * Enable Thrust test script.
    
    * Update thrust test script to use build_common.
    
    * Remove redundant build job.
    
    * Run nvidia-smi step in test job.
    
    * Update CUB test script.
    
    * Update libcu++ test script.
    
    * Enable CUB and libcu++ test jobs.
    
    * Hack to disable verbose CUB test output.
    
    * Exclude device_radix_sort CUB tests for now.
    
    * Update matrix to full matrix with tests on latest host compiler.
    
    * Run tests for all C++ dialects on latest host compiler.
    
    * Limit libcu++ tests parallel execution.
    
    * Add back set +x to build_common
    
    * Disable Thrust and CUB jobs for now.
    
    * Hardcode 8 workers for libcu++ tests.
    
    * Re-enable Thrust/CUB builds and tests.
    
    * Add build/latest.
    
    Make it so builds outside of devcontainer build to build/local.
    
    Also, always make it so the most recent build is symlinked to build/latest.

--- ci/test_libcudacxx.sh
+++ ci/test_libcudacxx.sh
@@ -2,20 +2,17 @@
 
 source "$(dirname "$0")/build_common.sh"
 
-cmake -S .. -B ../build \
+CMAKE_OPTIONS="
     -DCCCL_ENABLE_THRUST=OFF \
     -DCCCL_ENABLE_LIBCUDACXX=ON \
     -DCCCL_ENABLE_CUB=OFF \
     -DCCCL_ENABLE_TESTING=OFF \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=${HOST_COMPILER} \
-    -DCMAKE_CUDA_ARCHITECTURES=${GPU_ARCHS} \
-    -DCMAKE_CUDA_HOST_COMPILER=${HOST_COMPILER} \
     -DLIBCUDACXX_ENABLE_LIBCUDACXX_TESTS=ON \
-    -Dlibcudacxx_ENABLE_INSTALL_RULES=ON \
-    -DCUB_ENABLE_INSTALL_RULES=ON \
-    -DTHRUST_ENABLE_INSTALL_RULES=ON \
-    -G Ninja
+"
+configure "$CMAKE_OPTIONS"
 
-LIBCUDACXX_SITE_CONFIG="../build/libcudacxx/test/lit.site.cfg" lit -v --no-progress-bar -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
+readonly TEST_PARALLEL_LEVEL=8
 
+source "./sccache_stats.sh" "start"
+LIBCUDACXX_SITE_CONFIG="${BUILD_DIR}/libcudacxx/test/lit.site.cfg" lit -v -j ${TEST_PARALLEL_LEVEL} --no-progress-bar -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
+source "./sccache_stats.sh" "end"
