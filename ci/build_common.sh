963d3152f3ac31c08c8463b3d10ae1e44928fb1c
Wesley Maxey <wesley.maxey@gmail.com>
2023-07-17 16:24:11 -0700

    Enable some extra argument parsing in build_common.sh

--- ci/build_common.sh
+++ ci/build_common.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # Ensure the script is being executed in its containing directory
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
+# Script defaults
+CUDA_COMPILER=nvcc
+
 # Check if the correct number of arguments has been provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <HOST_COMPILER> <CXX_STANDARD> <GPU_ARCHS>"
+function usage {
+    echo "Usage: $0 [OPTIONS] <HOST_COMPILER> <CXX_STANDARD> <GPU_ARCHS>"
     echo "The PARALLEL_LEVEL environment variable controls the amount of build parallelism. Default is the number of cores."
     echo "Example: PARALLEL_LEVEL=8 $0 g++-8 14 \"70\" "
     echo "Example: $0 clang++-8 17 \"70;75;80-virtual\" "
+    echo "Possible options: "
+    echo "  -nvcc: path/to/nvcc"
+    echo "  -v/--verbose: enable shell echo for debugging"
     exit 1
+}
+
+# Check for extra options
+# NARGS
+while [ "$#" -gt 3 ]
+do
+  case "${1}" in
+  -h)     usage ;;
+  -help)  usage ;;
+  --help) usage ;;
+  --verbose) VERBOSE=1; shift ;;
+  -v)        VERBOSE=1; shift ;;
+  -nvcc) CUDA_COMPILER="${2}"; shift 2;;
+  *) usage ;;
+  esac
+done
+
+if [ $VERBOSE ]; then
+    set -x
 fi
+
+if [ "$#" -ne 3 ]; then
+    usage
+fi
+
+# Begin processing failures after option parsing
+set -euo pipefail
 
 # Assign command line arguments to variables
 readonly HOST_COMPILER=$(which $1)
@@ -22,7 +52,7 @@ readonly CXX_STANDARD=$2
 readonly GPU_ARCHS=$(echo $3 | tr ' ,' ';')
 
 readonly PARALLEL_LEVEL=${PARALLEL_LEVEL:=$(nproc)}
-readonly NVCC_VERSION=$(nvcc --version | grep release | awk '{print $6}' | cut -c2-)
+readonly NVCC_VERSION=$($CUDA_COMPILER --version | grep release | awk '{print $6}' | cut -c2-)
 
 if [ -z ${DEVCONTAINER_NAME+x} ]; then
     BUILD_DIR=../build/local
@@ -40,6 +70,7 @@ COMMON_CMAKE_OPTIONS="
     -DCMAKE_CXX_STANDARD=${CXX_STANDARD} \
     -DCMAKE_CUDA_STANDARD=${CXX_STANDARD} \
     -DCMAKE_CXX_COMPILER=${HOST_COMPILER} \
+    -DCMAKE_CUDA_COMPILER=${CUDA_COMPILER} \
     -DCMAKE_CUDA_HOST_COMPILER=${HOST_COMPILER} \
     -DCMAKE_CUDA_ARCHITECTURES=${GPU_ARCHS} \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
