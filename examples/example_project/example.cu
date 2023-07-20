0aed2208b185c3e9d07d0b59b8bf79f044cd9ff5
Jake Hemstad <jhemstad@nvidia.com>
2023-07-20 19:25:31 +0000

    Add more complete example file.

--- examples/example_project/example.cu
+++ examples/example_project/example.cu
@@ -1,8 +1,51 @@
 
 #include <cstdio>
+#include <thrust/device_vector.h>
+#include <cub/block/block_reduce.cuh>
+#include <cuda/atomic>
 
-int main(){
+__global__ void sumKernel(int const* data, int* result, std::size_t N)
+{
+    using BlockReduce = cub::BlockReduce<int, 256> ;
 
-    printf("Hello\n");
+    __shared__ typename BlockReduce::TempStorage temp_storage;
 
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+
+    int sum = 0;
+    if (index < N) {
+        sum += data[index];
+    }
+
+    sum = BlockReduce(temp_storage).Sum(sum);
+
+    if (threadIdx.x == 0){
+        cuda::atomic_ref<int, cuda::thread_scope_device> atomic_result(*result);
+        atomic_result.fetch_add(sum, cuda::memory_order_relaxed);
+    }
+}
+
+int main()
+{
+    std::size_t N = 1000;
+    thrust::device_vector<int> data(N, 1);
+    thrust::device_vector<int> result(1);
+
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+
+    sumKernel<<<numBlocks, blockSize>>>(thrust::raw_pointer_cast(data.data()),
+                                        thrust::raw_pointer_cast(result.data()), N);
+
+    auto err = cudaDeviceSynchronize();
+    if(err != cudaSuccess){
+        std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
+        return -1;
+    }
+
+    std::cout << "Sum: " << result[0] << std::endl;
+
+    assert(result[0] == N);
+
+    return 0;
 }
