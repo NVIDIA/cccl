58fc9c2dff22845a21aa41674776e9d49da7e7e7
Allison Vacanti <alliepiper16@gmail.com>
2023-08-07 21:37:04 -0400

    Split cccl tests from examples in naming schemes.
    Move the CCCL example targets/tests to `cccl.example.*`

--- cmake/CCCLUtilities.cmake
+++ cmake/CCCLUtilities.cmake
@@ -16,12 +16,13 @@
 
 # Add a build-and-test CTest.
 # - full_test_name_var will be set to the full name of the test.
+# - name_prefix is the prefix of the test's name (e.g. `cccl.test.cmake`)
 # - subdir is the relative path to the test project directory.
 # - test_id is used to generate a unique name for this test, allowing the
 #   subdir to be reused.
 # - Any additional args will be passed to the project configure step.
-function(cccl_add_compile_test full_test_name_var subdir test_id)
-  set(test_name cccl.test.cmake.${subdir}.${test_id})
+function(cccl_add_compile_test full_test_name_var name_prefix subdir test_id)
+  set(test_name ${name_prefix}.${subdir}.${test_id})
   set(src_dir "${CMAKE_CURRENT_SOURCE_DIR}/${subdir}")
   set(build_dir "${CMAKE_CURRENT_BINARY_DIR}/${subdir}/${test_id}")
   add_test(NAME ${test_name}
