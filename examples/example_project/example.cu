90b0e91f5abc5b4c7b98af305e4eb6a7ce40a045
Jake Hemstad <jhemstad@nvidia.com>
2023-08-08 17:59:37 -0500

    Add example cmake project (#177)
    * Add initial skeleton for example cmake project.
    
    * Add support for nvcc-specific matrix.
    
    * Remove errnoeous runs-on
    
    * Fix path to compute-matrix script.
    
    * debug
    
    * debug.
    
    * debug.
    
    * lol, use hyphen not underscore.
    
    * Get absolute path for matrix file.
    
    * Fix reference to matrix_file var.
    
    * Remove extraneous matrix_query
    
    * Use composite action instead of reusable workflow for matrix.
    
    * Fix quotes around composite action inputs.
    
    * Need to checkout repo to get local composite action.
    
    * Fix quotes.
    
    * Add shell to composite action.
    
    * Specify shell in step.
    
    * Correctly use inputs for composite action.
    
    * debug
    
    * Fix expansion.
    
    * Remove debug.
    
    * Remove debug.
    
    * Fix indention.
    
    * Add CCCL::CCCL, CCCL::libcudacxx, CCCL::CUB, and CCCL::Thrust targets.
    
    The Thrust target is configured via cache options as discussed in #203.
    
    Closes #203.
    
    * (Not working) Update to use new targets.
    
    * Make CPM point to the PR branch instead.
    
    * Escape quotes in jq query string.
    
    * Use underscore instead of hyphen to avoid needing quotes in query.
    
    * Use alternative syntax for jq query.
    
    * Missing quote.
    
    * Add post processing to set nvcc-specific matrix outputs.
    
    * Quotes around matrix output.
    
    * Single quotes.
    
    * Make CCCL::CCCL target GLOBAL.
    
    * Remove old compute-matrix reusable workflow.
    
    * Fix usage message.
    
    * Update devcontainer script to reference nvcc field.
    
    * Removed unused GITHUB_OUTPUT var.
    
    * EOF
    
    * Fix quotes.
    
    * Add more complete example file.
    
    * Add top level cmake file for building/running all the examples.
    
    * Enable examples to run via CI.
    
    * Add missing newlines.
    
    * Enable overriding CPM source for CI.
    
    * Add top level job for running examples.
    
    * Add devcontainer_version field to matrix file.
    
    * Update make_devcontainers script to read devcontainer_version.
    
    * Add job to get devcontainer_version and pass to dependent jobs.
    
    * Add and use input for devcontainer_version.
    
    * Avoid unnecessarily using compute-matrix action.
    
    * Use yaml alias/anchor for cuda versions.
    
    * [skip tests] Update devcontainer version job name.
    
    * Update to CUDA 12.2 and devcontainer 23.08.
    
    * Update devcontainer.json image versions and update to CUDA 12.2.
    
    * Add more docs.
    
    * Mark the CCCL::Thrust options as advanced.
    
    * Guard against rebuilding pre-existing targets.
    
    * Make the imported `CCCL::CCCL` target global in our CMake package.
    
    This ensures that users will be able to use it from their builds.
    
    * Include cmake/CCCLUtilities
    
    * Don't use magic number.
    
    * Update CPM to point to cccl/main.
    
    * Update README.
    
    * Add some more comments to the cmakelists.txt.
    
    * Update examples/README
    
    * Get devcontainer version from prior job.
    
    * Hard code example job to only build/run example_project.
    
    * Set error handling options in run-as-coder shell.
    
    * Add license.
    
    * Fix spacing.
    
    * Add license headers.
    
    * [skip-tests]
    
    * Remove accidental submodule?
    
    * Split cccl tests from examples in naming schemes.
    
    Move the CCCL example targets/tests to `cccl.example.*`
    
    * Remove unnecessary include.
    
    This file is included in the top-level CMakeLists.txt.
    
    * Add options to allow configuration of example CPM repo.
    
    * Add CMakePresets.txt.json with recipe for testing examples.
    
    * Update PR workflow to run examples using presets and new options.
    
    * Specify the CUDA compiler for the example config step.
    
    ---------
    
    Co-authored-by: Allison Vacanti <alliepiper16@gmail.com>
    Co-authored-by: Allison Vacanti <users.noreply.github.com>

---
+++ examples/example_project/example.cu
@@ -0,0 +1,77 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/*
+This is a simple example demonstrating the use of CCCL functionality from Thrust, CUB, and libcu++.
+
+The example computes the sum of an array of integers using a simple parallel reduction. Each thread block
+computes the sum of a subset of the array using cuB::BlockRecuce. The sum of each block is then reduced 
+to a single value using an atomic add via cuda::atomic_ref from libcu++. The result is stored in a device_vector
+from Thrust. The sum is then printed to the console.
+*/
+
+#include <cstdio>
+#include <thrust/device_vector.h>
+#include <cub/block/block_reduce.cuh>
+#include <cuda/atomic>
+
+constexpr int block_size = 256;
+
+__global__ void sumKernel(int const* data, int* result, std::size_t N)
+{
+    using BlockReduce = cub::BlockReduce<int, block_size> ;
+
+    __shared__ typename BlockReduce::TempStorage temp_storage;
+
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+
+    int sum = 0;
+    if (index < N) {
+        sum += data[index];
+    }
+
+    sum = BlockReduce(temp_storage).Sum(sum);
+
+    if (threadIdx.x == 0){
+        cuda::atomic_ref<int, cuda::thread_scope_device> atomic_result(*result);
+        atomic_result.fetch_add(sum, cuda::memory_order_relaxed);
+    }
+}
+
+int main()
+{
+    std::size_t N = 1000;
+    thrust::device_vector<int> data(N, 1);
+    thrust::device_vector<int> result(1);
+
+    int num_blocks = (N + block_size - 1) / block_size;
+
+    sumKernel<<<num_blocks, block_size>>>(thrust::raw_pointer_cast(data.data()),
+                                          thrust::raw_pointer_cast(result.data()), N);
+
+    auto err = cudaDeviceSynchronize();
+    if(err != cudaSuccess){
+        std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
+        return -1;
+    }
+
+    std::cout << "Sum: " << result[0] << std::endl;
+
+    assert(result[0] == N);
+
+    return 0;
+}
