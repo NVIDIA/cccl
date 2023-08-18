c065df870273d8450516e1de9e873ffb4818afba
Allison Vacanti <alliepiper16@gmail.com>
2023-08-18 17:34:57 +0000

    Migrate libcu++ to cmake presets.
    Configure step only, still need to invoke lit for testing.

--- ci/build_libcudacxx.sh
+++ ci/build_libcudacxx.sh
@@ -2,15 +2,11 @@
 
 source "$(dirname "$0")/build_common.sh"
 
-CMAKE_OPTIONS="
-    -DCCCL_ENABLE_THRUST=OFF \
-    -DCCCL_ENABLE_LIBCUDACXX=ON \
-    -DCCCL_ENABLE_CUB=OFF \
-    -DCCCL_ENABLE_TESTING=OFF \
-    -DLIBCUDACXX_ENABLE_LIBCUDACXX_TESTS=ON \
-"
-configure "$CMAKE_OPTIONS"
+PRESET="libcudacxx-cpp${CXX_STANDARD}"
+CMAKE_OPTIONS=""
+
+configure_preset libcudacxx "$PRESET" "$CMAKE_OPTIONS"
 
 source "./sccache_stats.sh" "start"
-LIBCUDACXX_SITE_CONFIG="${BUILD_DIR}/libcudacxx/test/lit.site.cfg" lit -v --no-progress-bar -Dexecutor="NoopExecutor()" -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
+LIBCUDACXX_SITE_CONFIG="${BUILD_DIR}/${PRESET}/libcudacxx/test/lit.site.cfg" lit -v --no-progress-bar -Dexecutor="NoopExecutor()" -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
 source "./sccache_stats.sh" "end"
--- ci/test_libcudacxx.sh
+++ ci/test_libcudacxx.sh
@@ -2,17 +2,13 @@
 
 source "$(dirname "$0")/build_common.sh"
 
-CMAKE_OPTIONS="
-    -DCCCL_ENABLE_THRUST=OFF \
-    -DCCCL_ENABLE_LIBCUDACXX=ON \
-    -DCCCL_ENABLE_CUB=OFF \
-    -DCCCL_ENABLE_TESTING=OFF \
-    -DLIBCUDACXX_ENABLE_LIBCUDACXX_TESTS=ON \
-"
-configure "$CMAKE_OPTIONS"
+PRESET="libcudacxx-cpp${CXX_STANDARD}"
+CMAKE_OPTIONS=""
+
+configure_preset libcudacxx "$PRESET" "$CMAKE_OPTIONS"
 
 readonly TEST_PARALLEL_LEVEL=8
 
 source "./sccache_stats.sh" "start"
-LIBCUDACXX_SITE_CONFIG="${BUILD_DIR}/libcudacxx/test/lit.site.cfg" lit -v -j ${TEST_PARALLEL_LEVEL} --no-progress-bar -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
+LIBCUDACXX_SITE_CONFIG="${BUILD_DIR}/${PRESET}/libcudacxx/test/lit.site.cfg" lit -v -j ${TEST_PARALLEL_LEVEL} --no-progress-bar -Dcompute_archs=${GPU_ARCHS} -Dstd="c++${CXX_STANDARD}" ../libcudacxx/.upstream-tests/test
 source "./sccache_stats.sh" "end"
