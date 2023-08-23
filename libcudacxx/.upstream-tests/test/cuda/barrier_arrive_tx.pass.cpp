01edfd2c096b25e5ddca1e7cdfaf57cdd82004c2
Allard Hendriksen <allard@allardhendriksen.nl>
2023-08-23 16:03:52 +0200

    Update libcudacxx/.upstream-tests/test/cuda/barrier_arrive_tx.pass.cpp
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- libcudacxx/.upstream-tests/test/cuda/barrier_arrive_tx.pass.cpp
+++ libcudacxx/.upstream-tests/test/cuda/barrier_arrive_tx.pass.cpp
@@ -26,8 +26,6 @@ enum class BlockSize {
 TEST_NV_DIAG_SUPPRESS(static_var_with_dynamic_init)
 
 using barrier = cuda::barrier<cuda::thread_scope_block>;
-
-
 inline __device__ void mbarrier_complete_tx(barrier &bar, int transaction_count) {
 
   NV_DISPATCH_TARGET(
