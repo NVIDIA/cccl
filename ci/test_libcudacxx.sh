53344d06e845c1e7e3cdcf69857f570e6c8c77c6
Allison Vacanti <alliepiper16@gmail.com>
2023-08-26 12:41:40 +0000

    Split the libcudacxx tests into two test presets.
    - libcudacxx-ctest-cppXX test presets will run all of the
      CTest tests, except for libcudacxx.test.lit.
    - libcudacxx-lit-cppXX test presets will run only libcudacxx.test.lit,
      extra verbosity settings, etc.
    
      The CI script runs both.

--- ci/test_libcudacxx.sh
+++ ci/test_libcudacxx.sh
@@ -7,6 +7,14 @@ CMAKE_OPTIONS=""
 
 configure_preset libcudacxx "$PRESET" "$CMAKE_OPTIONS"
 
+# The libcudacxx tests are split into two presets, one for
+# regular ctest tests and another that invokes the lit tests
+# harness with extra options for verbosity, etc:
+CTEST_PRESET="libcudacxx-ctest-cpp${CXX_STANDARD}"
+LIT_PRESET="libcudacxx-lit-cpp${CXX_STANDARD}"
+
+test_preset "libcudacxx (CTest)" ${CTEST_PRESET}
+
 source "./sccache_stats.sh" "start"
-test_preset libcudacxx ${PRESET}
+test_preset "libcudacxx (lit)" ${LIT_PRESET}
 source "./sccache_stats.sh" "end"
