f2866f6584b68c606c3c73bf468c12b16ec38f0d
Jake Hemstad <jhemstad@nvidia.com>
2023-09-19 14:51:21 -0500

    Consolidate matrix logic into single script/job (#361)
    * Consolidate matrix logic into single script/job.
    
    * Update .github/actions/compute-matrix/compute-matrix.sh

--- .github/actions/compute-matrix/compute-matrix.sh
+++ .github/actions/compute-matrix/compute-matrix.sh
@@ -2,22 +2,48 @@
 
 set -euo pipefail
 
-# Check for the correct number of arguments
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 MATRIX_FILE MATRIX_QUERY"
-    echo "MATRIX_FILE: The path to the matrix file."
-    echo "MATRIX_QUERY: The jq query used to specify the desired matrix. e.g., '.pull-request.nvcc'"
+write_output() {
+  local key="$1"
+  local value="$(echo "$2" | jq -c .)"
+  echo "$key=$value" | tee --append "${GITHUB_OUTPUT:-/dev/null}"
+}
+
+explode_std_versions() {
+  jq -r 'map(. as $o | {std: $o.std[]} + del($o.std))'
+}
+
+extract_matrix() {
+  local file="$1"
+  local type="$2"
+  local matrix=$(yq -o=json "$file" | jq -r ".$type")
+  write_output "DEVCONTAINER_VERSION" "$(yq -o json "$file" | jq -r '.devcontainer_version')"
+  local nvcc_full_matrix="$(echo "$matrix" | jq -r '.nvcc' | explode_std_versions )"
+  write_output "NVCC_FULL_MATRIX" "$nvcc_full_matrix"
+  write_output "CUDA_VERSIONS" "$(echo "$nvcc_full_matrix" | jq -r '[.[] | .cuda] | unique')"
+  write_output "HOST_COMPILERS" "$(echo "$nvcc_full_matrix" | jq -r '[.[] | .compiler.name] | unique')"
+  write_output "PER_CUDA_COMPILER_MATRIX" "$(echo "$nvcc_full_matrix" | jq -r ' group_by(.cuda + .compiler.name) | map({(.[0].cuda + "-" + .[0].compiler.name): .}) | add')"
+  write_output "NVRTC_MATRIX" "$(echo "$matrix" | jq '.nvrtc' | explode_std_versions)"
+}
+
+main() {
+  if [ "$1" == "-v" ]; then
+    set -x
+    shift
+  fi
+
+  if [ $# -ne 2 ] || [ "$2" != "pull_request" ]; then
+    echo "Usage: $0 [-v] MATRIX_FILE MATRIX_TYPE"
+    echo "  -v            : Enable verbose output"
+    echo "  MATRIX_FILE   : The path to the matrix file."
+    echo "  MATRIX_TYPE   : The desired matrix. Supported values: 'pull_request'"
     exit 1
-fi
+  fi
 
-# Get realpath before changing directory
-MATRIX_FILE=$(realpath "$1")
-MATRIX_QUERY="$2"
+  echo "Input matrix file:" >&2
+  cat "$1" >&2
+  echo "Matrix Type: $2" >&2
 
-# Ensure the script is being executed in its containing directory
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+  extract_matrix "$1" "$2"
+}
 
-echo "Input matrix file:" >&2
-cat "$MATRIX_FILE" >&2
-echo "Query: $MATRIX_QUERY" >&2
-echo $(yq -o=json "$MATRIX_FILE" | jq -c -r "$MATRIX_QUERY | map(. as \$o | {std: .std[]} + del(\$o.std))")
+main "$@"
