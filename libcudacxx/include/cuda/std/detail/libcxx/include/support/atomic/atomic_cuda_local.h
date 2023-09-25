236824adcfb1fc26b2f49905ed04c3584607d1d0
gonzalobg <65027571+gonzalobg@users.noreply.github.com>
2023-09-25 18:00:30 +0200

    Fix formatting

--- libcudacxx/include/cuda/std/detail/libcxx/include/support/atomic/atomic_cuda_local.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/support/atomic/atomic_cuda_local.h
@@ -140,7 +140,9 @@ _LIBCUDACXX_DEVICE void __cuda_fetch_local_bop_max(volatile _Type& __atom, _Type
 {
   __atom = __atom < __v ? __v : __atom;
 }
-template <class _Type> _LIBCUDACXX_DEVICE void __cuda_fetch_local_bop_min(volatile _Type& __atom, _Type const & __v) {
+template <class _Type>
+_LIBCUDACXX_DEVICE void __cuda_fetch_local_bop_min(volatile _Type& __atom, _Type const & __v)
+{
   __atom = __v < __atom ? __v : __atom;
 }
 
