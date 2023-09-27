4223917afb0fc166bd54153aa875084dc4a55a76
Michael Schellenberger Costa <miscco@nvidia.com>
2023-09-27 18:15:40 +0200

    Remove trailing commata

--- libcudacxx/.upstream-tests/test/heterogeneous/barrier_parity.std.pass.cpp
+++ libcudacxx/.upstream-tests/test/heterogeneous/barrier_parity.std.pass.cpp
@@ -79,7 +79,7 @@ using aw_aw_pw1 = performer_list<
     barrier_arrive_and_wait,
     barrier_arrive_and_wait,
     async_tester_fence,
-    clear_token,
+    clear_token
 >;
 
 using aw_aw_pw2 = performer_list<
