d634859fd3d26023d7316a3f98678f1418a1cf1b
Michael Schellenberger Costa <miscco@nvidia.com>
2023-09-27 18:15:21 +0200

    Silence more warnings

--- libcudacxx/.upstream-tests/test/std/utilities/meta/meta.trans/meta.trans.other/result_of11.pass.cpp
+++ libcudacxx/.upstream-tests/test/std/utilities/meta/meta.trans/meta.trans.other/result_of11.pass.cpp
@@ -26,6 +26,9 @@
 #include "test_macros.h"
 
 TEST_NV_DIAG_SUPPRESS(3013) // a volatile function parameter is deprecated
+#ifdef TEST_COMPILER_CLANG_CUDA
+#pragma clang diagnostic ignored "-Wdeprecated-volatile"
+#endif // TEST_COMPILER_CLANG_CUDA
 
 struct wat
 {
