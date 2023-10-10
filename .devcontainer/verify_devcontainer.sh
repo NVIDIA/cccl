f9fa71e05a20aa43029713fdf233b545f27e9e1a
Jake Hemstad <jhemstad@nvidia.com>
2023-10-10 14:52:47 +0000

    [skip-tests] Fix accidental copy/paste.

--- .devcontainer/verify_devcontainer.sh
+++ .devcontainer/verify_devcontainer.sh
@@ -39,54 +39,16 @@ check_host_compiler_version() {
 
     if [[ "$expected_compiler" != "${CCCL_HOST_COMPILER}" || "$actual_version" != "$CCCL_HOST_COMPILER_VERSION" ]]; then
         echo "::error:: CXX ($CXX) version ($actual_version) does not match the expected compiler (${CCCL_HOST_COMPILER}) and version (${CCCL_HOST_COMPILER_VERSION})."
-        exit 1          set -euo pipefail
-          if [[ -z "${CXX:-}" ]]; then
-            echo "::error:: CXX variable is not set."
-            exit 1
-          fi
-          $CXX --version
-          version_output=$($CXX --version)
-          if [[ "$CXX" == "g++" ]]; then
-              # Extracts major version for g++ output format
-              actual_version=$(echo "$version_output" | head -n 1 | cut -d ' ' -f 4 | cut -d '.' -f 1)
-              expected_compiler="gcc"
-          elif [[ "$CXX" == "clang++" ]]; then
-              # Extracts major version for clang++ output format
-              if [[ $version_output =~ clang\ version\ ([0-9]+) ]]; then
-                  actual_version=${BASH_REMATCH[1]}
-              else
-                  echo "::error:: Unable to determine clang version."
-                  exit 1
-              fi
-              expected_compiler="llvm"
-          else
-              echo "::error:: Unexpected CXX value ($CXX)."
-              exit 1
-          fi
+        exit 1
+    else
+        echo "Detected host compiler: $CXX version $actual_version"
+    fi
+}
 
-          expected_version="${CCCL_HOST_COMPILER_VERSION}"
-
-          if [[ "$expected_compiler" != "${CCCL_HOST_COMPILER}" || "$actual_version" != "$expected_version" ]]; then
-              echo "::error:: CXX ($CXX) version ($actual_version) does not match the expected compiler (${CCCL_HOST_COMPILER}) and version (${CCCL_HOST_COMPILER_VERSION})."
-              exit 1
-          fi
-
-          # Check CUDA version from nvcc
-          cuda_version_output=$(nvcc --version)
-          # Extract the MAJOR.MINOR version from the output
-          if [[ $cuda_version_output =~ release\ ([0-9]+\.[0-9]+) ]]; then
-              actual_cuda_version=${BASH_REMATCH[1]}
-          else
-              echo "::error:: Unable to determine CUDA version from nvcc."
-              exit 1
-          fi
-
-          expected_cuda_version="${CCCL_CUDA_VERSION}"
-          if [[ "$actual_cuda_version" != "$expected_cuda_version" ]]; then
-              echo "::error:: CUDA version ($actual_cuda_version) does not match the expected CUDA version ($expected_cuda_version)."
-              exit 1
-          fi
-ion=${BASH_REMATCH[1]}
+check_cuda_version() {
+    local cuda_version_output=$(nvcc --version)
+    if [[ $cuda_version_output =~ release\ ([0-9]+\.[0-9]+) ]]; then
+        local actual_cuda_version=${BASH_REMATCH[1]}
     else
         echo "::error:: Unable to determine CUDA version from nvcc."
         exit 1
