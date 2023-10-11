d522e755df514ac20fce79aeb819de787de6ab51
Michael Schellenberger Costa <miscco@nvidia.com>
2023-10-11 09:02:20 +0200

    Move `std::count_if` into its own file

---
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__algorithm/count_if.h
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCUDACXX___ALGORITHM_COUNT_IF_H
+#define _LIBCUDACXX___ALGORITHM_COUNT_IF_H
+
+#ifndef __cuda_std__
+#  include <__config>
+#endif // __cuda_std__
+
+#include "../__iterator/iterator_traits.h"
+
+#if defined(_CCCL_COMPILER_NVHPC) && defined(_CCCL_USE_IMPLICIT_SYSTEM_DEADER)
+#  pragma GCC system_header
+#else // ^^^ _CCCL_COMPILER_NVHPC ^^^ / vvv !_CCCL_COMPILER_NVHPC vvv
+_CCCL_IMPLICIT_SYSTEM_HEADER
+#endif // !_CCCL_COMPILER_NVHPC
+_LIBCUDACXX_BEGIN_NAMESPACE_STD
+
+template <class _InputIterator, class _Predicate>
+_LIBCUDACXX_NODISCARD_EXT inline _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX11
+  __iter_diff_t<_InputIterator>
+  count_if(_InputIterator __first, _InputIterator __last, _Predicate __pred)
+{
+  __iter_diff_t<_InputIterator> __r{0};
+  for (; __first != __last; ++__first)
+  {
+    if (__pred(*__first))
+    {
+      ++__r;
+    }
+  }
+  return __r;
+}
+
+_LIBCUDACXX_END_NAMESPACE_STD
+
+#endif // _LIBCUDACXX___ALGORITHM_COUNT_IF_H
