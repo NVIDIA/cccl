dc4467fdf185694053eda6c186744ab8be119d23
Danial Javady <122740063+ZelboK@users.noreply.github.com>
2023-10-23 21:36:11 -0400

    Update cub/cub/detail/triple_chevron_launch.cuh
    Co-authored-by: Georgy Evtushenko <evtushenko.georgy@gmail.com>

--- cub/cub/detail/triple_chevron_launch.cuh
+++ cub/cub/detail/triple_chevron_launch.cuh
@@ -26,13 +26,17 @@
  ******************************************************************************/
 #pragma once
 
-#include <thrust/detail/config.h>
-#include <thrust/system/cuda/detail/core/alignment.h>
-#include <thrust/system/cuda/detail/guarded_cuda_runtime_api.h>
+#include <cuda/__cccl_config> // _CCCL_ATTRIBUTE_HIDDEN
 
-#include <cuda/std/version> // _LIBCUDACXX_HIDDEN
+#if defined(_CCCL_COMPILER_NVHPC) && defined(_CCCL_USE_IMPLICIT_SYSTEM_DEADER)
+#pragma GCC system_header
+#else // ^^^ _CCCL_COMPILER_NVHPC ^^^ / vvv !_CCCL_COMPILER_NVHPC vvv
+_CCCL_IMPLICIT_SYSTEM_HEADER
+#endif // !_CCCL_COMPILER_NVHPC
 
-#include <cassert>
+#include <cub/config.cuh>
+#include <cuda/std/type_traits>
+#include <nv/target>
 
 CUB_NAMESPACE_BEGIN
 
