e351c799eaabcfa8bfd3c95031f8a07ab435d973
Allard Hendriksen <ahendriksen@nvidia.com>
2023-10-25 15:08:13 +0200

    Use numerical PTX ISA/SM target macros

--- libcudacxx/include/cuda/std/detail/libcxx/include/__cuda/ptx/ptx_isa_target_macros.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__cuda/ptx/ptx_isa_target_macros.h
@@ -21,43 +21,42 @@
  * https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#release-notes
  */
 
-#if (defined(__CUDA_MINIMUM_ARCH__) && 800 <= __CUDA_MINIMUM_ARCH__) || (!defined(__CUDA_MINIMUM_ARCH__))
-#  define _LIBCUDACXX_PTX_SM_80_AVAILABLE
-#endif
+
+// SM version
 
 #if (defined(__CUDA_MINIMUM_ARCH__) && 900 <= __CUDA_MINIMUM_ARCH__) || (!defined(__CUDA_MINIMUM_ARCH__))
-#  define _LIBCUDACXX_PTX_SM_90_AVAILABLE
+#  define __cccl_ptx_sm 900ULL
+#elif (defined(__CUDA_MINIMUM_ARCH__) && 800 <= __CUDA_MINIMUM_ARCH__) || (!defined(__CUDA_MINIMUM_ARCH__))
+#  define __cccl_ptx_sm 800ULL
+// Fallback case. Define the SM version to be zero. This ensures that the macro is always defined.
+#else
+#  define __cccl_ptx_sm 0ULL
 #endif
 
-// PTX ISA 7.8 is available from CTK 11.8, driver r520
-#if (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 11 && __CUDACC_VER_MINOR__ >= 8)) || (!defined(__CUDACC_VER_MAJOR__))
-#  define _LIBCUDACXX_PTX_ISA_78_AVAILABLE
-#endif
 
-// PTX ISA 7.8 is available from CTK 11.8, driver r520 (so also from CTK 12.0 onwards)
-#if (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 0)) || (!defined(__CUDACC_VER_MAJOR__))
-#  define _LIBCUDACXX_PTX_ISA_78_AVAILABLE
-#endif
-
-// PTX ISA 8.0 is available from CTK 12.0, driver r525
-#if (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 0)) || (!defined(__CUDACC_VER_MAJOR__))
-#  define _LIBCUDACXX_PTX_ISA_80_AVAILABLE
-#endif
-
-// PTX ISA 8.1 is available from CTK 12.1, driver r530
-#if (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 1)) || (!defined(__CUDACC_VER_MAJOR__))
-#  define _LIBCUDACXX_PTX_ISA_81_AVAILABLE
-#endif
-
-// PTX ISA 8.2 is available from CTK 12.2, driver r535
-#if (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 2)) || (!defined(__CUDACC_VER_MAJOR__))
-#  define _LIBCUDACXX_PTX_ISA_82_AVAILABLE
-#endif
+// PTX ISA version
 
 // PTX ISA 8.3 is available from CTK 12.3, driver r545
-#if (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 3)) || (!defined(__CUDACC_VER_MAJOR__))
-#  define _LIBCUDACXX_PTX_ISA_83_AVAILABLE
+#if   (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 3)) || (!defined(__CUDACC_VER_MAJOR__))
+#  define __cccl_ptx_isa 830ULL
+// PTX ISA 8.1 is available from CTK 12.1, driver r530
+#elif (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 2)) || (!defined(__CUDACC_VER_MAJOR__))
+#  define __cccl_ptx_isa 820ULL
+// PTX ISA 8.2 is available from CTK 12.2, driver r535
+#elif (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 1)) || (!defined(__CUDACC_VER_MAJOR__))
+#  define __cccl_ptx_isa 810ULL
+// PTX ISA 8.0 is available from CTK 12.0, driver r525
+#elif (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 0)) || (!defined(__CUDACC_VER_MAJOR__))
+#  define __cccl_ptx_isa 800ULL
+// PTX ISA 7.8 is available from CTK 11.8, driver r520 (so also from CTK 12.0 onwards)
+#elif (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 0)) || (!defined(__CUDACC_VER_MAJOR__))
+#  define __cccl_ptx_isa 780ULL
+// PTX ISA 7.8 is available from CTK 11.8, driver r520
+#elif (defined(__CUDACC_VER_MAJOR__) && (__CUDACC_VER_MAJOR__ >= 11 && __CUDACC_VER_MINOR__ >= 8)) || (!defined(__CUDACC_VER_MAJOR__))
+#  define __cccl_ptx_isa 780ULL
+// Fallback case. Define the ISA version to be zero. This ensures that the macro is always defined.
+#else
+#  define __cccl_ptx_isa 0ULL
 #endif
-
 
 #endif // PTX_ISA_TARGET_MACROS_H_
