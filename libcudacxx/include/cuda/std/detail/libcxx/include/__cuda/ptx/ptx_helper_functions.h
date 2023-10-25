4038861b7a5d506a36433fc9f8d2e719b49e28a3
Allard Hendriksen <ahendriksen@nvidia.com>
2023-10-25 13:17:06 +0200

    Format code
    (cherry picked from commit e356271672c645c1956a5ac209b34ddc275b9341)

--- libcudacxx/include/cuda/std/detail/libcxx/include/__cuda/ptx/ptx_helper_functions.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__cuda/ptx/ptx_helper_functions.h
@@ -15,27 +15,31 @@
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA_PTX
 
 // Private helper functions
-inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint32_t __as_smem_ptr(const void * __ptr)   {
-    return static_cast<_CUDA_VSTD::uint32_t>(__cvta_generic_to_shared(__ptr));
+inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint32_t __as_smem_ptr(const void* __ptr)
+{
+  return static_cast<_CUDA_VSTD::uint32_t>(__cvta_generic_to_shared(__ptr));
 }
-
-inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint32_t __as_remote_dsmem_ptr(const void * __ptr)   {
-    return static_cast<_CUDA_VSTD::uint32_t>(__cvta_generic_to_shared(__ptr));
+inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint32_t __as_remote_dsmem_ptr(const void* __ptr)
+{
+  return static_cast<_CUDA_VSTD::uint32_t>(__cvta_generic_to_shared(__ptr));
 }
-inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint64_t __as_gmem_ptr(const void * __ptr)   {
-    return static_cast<_CUDA_VSTD::uint64_t>(__cvta_generic_to_global(__ptr));
-}
-
-template <typename _Tp>
-inline _LIBCUDACXX_DEVICE int __as_b32(_Tp __val) {
-    static_assert(sizeof(_Tp) == 4, "");
-    return *reinterpret_cast<int*>(&__val);
+inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint64_t __as_gmem_ptr(const void* __ptr)
+{
+  return static_cast<_CUDA_VSTD::uint64_t>(__cvta_generic_to_global(__ptr));
 }
 
 template <typename _Tp>
-inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint64_t __as_b64(_Tp __val) {
-    static_assert(sizeof(_Tp) == 8, "");
-    return *reinterpret_cast<_CUDA_VSTD::uint64_t*>(&__val);
+inline _LIBCUDACXX_DEVICE int __as_b32(_Tp __val)
+{
+  static_assert(sizeof(_Tp) == 4, "");
+  return *reinterpret_cast<int*>(&__val);
+}
+
+template <typename _Tp>
+inline _LIBCUDACXX_DEVICE _CUDA_VSTD::uint64_t __as_b64(_Tp __val)
+{
+  static_assert(sizeof(_Tp) == 8, "");
+  return *reinterpret_cast<_CUDA_VSTD::uint64_t*>(&__val);
 }
 
 _LIBCUDACXX_END_NAMESPACE_CUDA_PTX
