26a9a7586fc47e1c021ba3edf4caa365546026f0
Michael Schellenberger Costa <miscco@nvidia.com>
2023-10-27 10:21:45 +0200

    format swap_ranges

--- libcudacxx/include/cuda/std/detail/libcxx/include/__algorithm/swap_ranges.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__algorithm/swap_ranges.h
@@ -11,13 +11,13 @@
 #define _LIBCUDACXX___ALGORITHM_SWAP_RANGES_H
 
 #ifndef __cuda_std__
-#include <__config>
+#  include <__config>
 #endif // __cuda_std__
 
 #include "../__utility/swap.h"
 
 #if defined(_CCCL_COMPILER_NVHPC) && defined(_CCCL_USE_IMPLICIT_SYSTEM_DEADER)
-#pragma GCC system_header
+#  pragma GCC system_header
 #else // ^^^ _CCCL_COMPILER_NVHPC ^^^ / vvv !_CCCL_COMPILER_NVHPC vvv
 _CCCL_IMPLICIT_SYSTEM_HEADER
 #endif // !_CCCL_COMPILER_NVHPC
@@ -25,12 +25,14 @@ _CCCL_IMPLICIT_SYSTEM_HEADER
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 template <class _ForwardIterator1, class _ForwardIterator2>
-inline _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX17
-_ForwardIterator2 swap_ranges(_ForwardIterator1 __first1, _ForwardIterator1 __last1, _ForwardIterator2 __first2)
+inline _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX17 _ForwardIterator2
+swap_ranges(_ForwardIterator1 __first1, _ForwardIterator1 __last1, _ForwardIterator2 __first2)
 {
-    for(; __first1 != __last1; ++__first1, (void) ++__first2)
-        swap(*__first1, *__first2);
-    return __first2;
+  for (; __first1 != __last1; ++__first1, (void) ++__first2)
+  {
+    swap(*__first1, *__first2);
+  }
+  return __first2;
 }
 
 _LIBCUDACXX_END_NAMESPACE_STD
