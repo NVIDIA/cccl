701d2a2d4d8fdfa55587cc3ed7a1997671215a43
Jake Hemstad <jhemstad@nvidia.com>
2023-10-30 20:43:52 +0000

    Emit error.

--- ci/build_common.sh
+++ ci/build_common.sh
@@ -222,7 +222,7 @@ function build_preset() {
     fi
 
     if [ $build_status -ne 0 ]; then
-        echo "Build failed with status $build_status"
+        echo "::error:: ${BUILD_NAME} failed."
         exit $build_status
     fi
 }
