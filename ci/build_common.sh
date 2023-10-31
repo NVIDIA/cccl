7f01fed41448331b02ab9aa2ea344d9faa5ed74e
Jake Hemstad <jhemstad@nvidia.com>
2023-10-31 17:28:43 +0000

    Don't print in GHA on success.

--- ci/build_common.sh
+++ ci/build_common.sh
@@ -129,8 +129,6 @@ function end_group() {
 
         if [ "$build_status" -ne 0 ]; then
             echo -e "::error::\e[${red}m ⬆️  ${name} - Failed (click above for full log)⬆️\e[0m"
-        else
-            echo -e "⬆️ \e[${green}m${name} - Succeeded ⬆️\e[0m"
         fi
     else
         if [ "$build_status" -ne 0 ]; then
