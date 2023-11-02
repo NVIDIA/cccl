d23d6e923c1688a2dff3497e2edd260a8a105489
Jake Hemstad <jhemstad@nvidia.com>
2023-11-02 18:08:03 +0000

    Dont parse version headers for CMake package version.
    (cherry picked from commit a1958f4dec670c710b0dc81a033b13518ecc2770)

--- libcudacxx/lib/cmake/libcudacxx/libcudacxx-config-version.cmake
+++ libcudacxx/lib/cmake/libcudacxx/libcudacxx-config-version.cmake
@@ -1,19 +1,10 @@
 # Parse version information from version header:
 include("${CMAKE_CURRENT_LIST_DIR}/libcudacxx-header-search.cmake")
 
-file(READ "${_libcudacxx_VERSION_INCLUDE_DIR}/cuda/std/detail/__config"
-  libcudacxx_VERSION_HEADER
-)
-
-string(REGEX MATCH
-  "#define[ \t]+_LIBCUDACXX_CUDA_API_VERSION[ \t]+([0-9]+)" unused_var
-  "${libcudacxx_VERSION_HEADER}"
-)
-
 set(libcudacxx_VERSION_FLAT ${CMAKE_MATCH_1})
-math(EXPR libcudacxx_VERSION_MAJOR "${libcudacxx_VERSION_FLAT} / 1000000")
-math(EXPR libcudacxx_VERSION_MINOR "(${libcudacxx_VERSION_FLAT} / 1000) % 1000")
-math(EXPR libcudacxx_VERSION_PATCH "${libcudacxx_VERSION_FLAT} % 1000")
+set(libcudacxx_VERSION_MAJOR 2)
+set(libcudacxx_VERSION_MINOR 2)
+set(libcudacxx_VERSION_PATCH 0)
 set(libcudacxx_VERSION_TWEAK 0)
 
 set(libcudacxx_VERSION
