698672251432fa6d1c61f1be70ed8e190d2c8331
Allison Vacanti <alliepiper16@gmail.com>
2023-11-04 19:44:55 +0000

    Add new Catch2 matchers for radix sort cases.
    Adds a matcher that treats `NaN == NaN`, and a bitwise matcher
    to allow testing for stability when sorting pos/neg zeros.

--- cub/test/catch2_test_helper.h
+++ cub/test/catch2_test_helper.h
@@ -38,8 +38,12 @@
 #include <cub/util_compiler.cuh>
 #include "test_util_vec.h"
 
+#include "c2h/utility.cuh"
+
 #include "catch2_main.cuh"
 #include "test_warning_suppression.cuh"
+
+#include <cuda/std/cmath>
 
 #ifndef VAR_IDX
 #define VAR_IDX 0
@@ -109,6 +113,80 @@ namespace detail
   auto vec_ref = detail::to_vec(ref);  \
   auto vec_out = detail::to_vec(out);  \
   REQUIRE_THAT(vec_ref, Catch::Approx(vec_out)); \
+}
+
+namespace detail
+{
+  // Returns true if values are equal, or both NaN:
+  struct equal_or_nans
+  {
+    template <typename T>
+    bool operator()(const T& a, const T& b) const
+    {
+      return (cuda::std::isnan(a) && cuda::std::isnan(b)) || a == b;
+    }
+  };
+
+  struct bitwise_equal
+  {
+    template <typename T>
+    bool operator()(const T&a, const T&b) const
+    {
+      using bits_t = typename cub::Traits<T>::UnsignedBits;
+      bits_t a_bits = c2h::bit_cast<bits_t>(a);
+      bits_t b_bits = c2h::bit_cast<bits_t>(b);
+      return a_bits == b_bits;
+    }
+  };
+
+  // Catch2 Matcher that calls `std::equal` with a default-constructable custom predicate
+  template <typename Range, typename Pred>
+  struct CustomEqualsRangeMatcher : Catch::MatcherBase<Range>
+  {
+    CustomEqualsRangeMatcher(Range const& range)
+        : range{range}
+    {}
+
+    bool match(Range const& other) const override
+    {
+      using std::begin;
+      using std::end;
+
+      return std::equal(begin(range), end(range), begin(other), Pred{});
+    }
+
+    std::string describe() const override
+    {
+      return "Equals: " + Catch::rangeToString(range);
+    }
+
+  private:
+    Range const& range;
+  };
+
+  template <typename Range>
+  auto NaNEqualsRange(const Range& range) -> CustomEqualsRangeMatcher<Range, equal_or_nans>
+  {
+    return CustomEqualsRangeMatcher<Range, equal_or_nans>(range);
+  }
+
+  template <typename Range>
+  auto BitwiseEqualsRange(const Range& range) -> CustomEqualsRangeMatcher<Range, bitwise_equal>
+  {
+    return CustomEqualsRangeMatcher<Range, bitwise_equal>(range);
+  }
+} // namespace detail
+
+#define REQUIRE_EQ_WITH_NAN_MATCHING(ref, out) { \
+  auto vec_ref = detail::to_vec(ref);  \
+  auto vec_out = detail::to_vec(out);  \
+  REQUIRE_THAT(vec_ref, detail::NaNEqualsRange(vec_out)); \
+}
+
+#define REQUIRE_BITWISE_EQ(ref, out) { \
+  auto vec_ref = detail::to_vec(ref);  \
+  auto vec_out = detail::to_vec(out);  \
+  REQUIRE_THAT(vec_ref, detail::NaNEqualsRange(vec_out)); \
 }
 
 #include <c2h/generators.cuh>
