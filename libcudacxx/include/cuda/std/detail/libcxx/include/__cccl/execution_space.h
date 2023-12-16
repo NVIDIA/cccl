97ada720e4a7e28cb7fdec8e6e2ba2cc4d934d85
David Olsen <dolsen@nvidia.com>
2023-12-15 16:29:32 -0800

    Fix 'nvc++ -stdpar'
    NVC++ stdpar mode enables CUDA support but does not define the macro
    `__CUDACC__`.  Any code in CCCL (which is used by NVC++ stdpar under the
    covers) needs to check for either `__CUDACC__` or `_NVHPC_CUDA` being
    set.
    
    https://github.com/NVIDIA/cccl/pull/1199 only checks for `__CUDACC__`
    when defining `_CCCL_HOST` and `_CCCL_DEVICE` and some other macros.
    This change completely broke `nvc++ -stdpar` because all functions
    became unannotated host functions.
    
    Fix this by changing several files in
    libcudacxx/include/cuda/std/detail/libcxx/include to check for either
    `__CUDACC__` or `_NVHPC_CUDA`.

--- libcudacxx/include/cuda/std/detail/libcxx/include/__cccl/execution_space.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__cccl/execution_space.h
@@ -11,17 +11,17 @@
 #ifndef __CCCL_EXECUTION_SPACE_H
 #define __CCCL_EXECUTION_SPACE_H
 
-#ifdef __CUDACC__
+#if defined(__CUDACC__) || defined(_NVHPC_CUDA)
 #  define _CCCL_HOST         __host__
 #  define _CCCL_DEVICE       __device__
 #  define _CCCL_HOST_DEVICE  __host__ __device__
 #  define _CCCL_FORCEINLINE __forceinline__
-#else // ^^^ __CUDACC__ ^^^ / vvv !__CUDACC__
+#else // ^^^ __CUDACC__ || _NVHPC_CUDA ^^^ / vvv !__CUDACC__ && !_NVHPC_CUDA
 #  define _CCCL_HOST
 #  define _CCCL_DEVICE
 #  define _CCCL_HOST_DEVICE
 #  define _CCCL_FORCEINLINE
-#endif // !__CUDACC__
+#endif // !__CUDACC__ && !_NVHPC_CUDA
 
 #if !defined(_CCCL_EXEC_CHECK_DISABLE)
 #  if defined(_CCCL_COMPILER_NVRTC) || defined(_CCCL_CUDA_COMPILER_NVHPC) || defined(_CCCL_CUDA_COMPILER_CLANG)
