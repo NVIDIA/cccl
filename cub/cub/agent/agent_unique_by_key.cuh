ced226550fd0b499e14bdef6f34e5bb466436943
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2024-01-03 11:19:35 -0800

    prevent loop unrolling to alleviate perf degradation for u32

--- cub/cub/agent/agent_unique_by_key.cuh
+++ cub/cub/agent/agent_unique_by_key.cuh
@@ -302,6 +302,9 @@ struct AgentUniqueByKey
 
         CTA_SYNC();
 
+        // Preventing loop unrolling helps avoid perf degradation when switching from signed to unsigned 32-bit offset
+        // types
+        #pragma unroll(1)
         for (int item = threadIdx.x;
              item < num_tile_selections;
              item += BLOCK_THREADS)
