dba5031fa42710a831662e5b1ed37f240bd4975d
Michael Schellenberger Costa <miscco@nvidia.com>
2024-01-17 14:27:51 +0100

    Try and fix including the exception header

--- libcudacxx/include/cuda/std/detail/libcxx/include/__expected/bad_expected_access.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__expected/bad_expected_access.h
@@ -13,9 +13,6 @@
 #include <__config>
 #endif // __cuda_std__
 
-#include "../__utility/move.h"
-#include "../exception"
-
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
@@ -23,6 +20,9 @@
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
 #endif // no system header
+
+#include "../__utility/move.h"
+#include "../exception"
 
 #if _CCCL_STD_VER > 2011
 
