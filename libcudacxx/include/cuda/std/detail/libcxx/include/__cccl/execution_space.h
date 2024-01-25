2418541edbb464aec912366f44e22420da2fd621
Michael Schellenberger Costa <miscco@nvidia.com>
2024-01-25 13:44:22 +0100

    Cleanup our `__cccl_config`
    We want to consolidate our configuration and keep it maintainable. Consequently put the different functionality into individual headers

--- libcudacxx/include/cuda/std/detail/libcxx/include/__cccl/execution_space.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__cccl/execution_space.h
@@ -11,10 +11,21 @@
 #ifndef __CCCL_EXECUTION_SPACE_H
 #define __CCCL_EXECUTION_SPACE_H
 
-#if defined(__CUDACC__) || defined(_NVHPC_CUDA)
-#  define _CCCL_HOST         __host__
-#  define _CCCL_DEVICE       __device__
-#  define _CCCL_HOST_DEVICE  __host__ __device__
+#include "../__cccl/compiler.h"
+#include "../__cccl/system_header.h"
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+#if defined(_CCCL_CUDA_COMPILER)
+#  define _CCCL_HOST        __host__
+#  define _CCCL_DEVICE      __device__
+#  define _CCCL_HOST_DEVICE __host__ __device__
 #  define _CCCL_FORCEINLINE __forceinline__
 #else // ^^^ __CUDACC__ || _NVHPC_CUDA ^^^ / vvv !__CUDACC__ && !_NVHPC_CUDA
 #  define _CCCL_HOST
@@ -24,9 +35,7 @@
 #endif // !__CUDACC__ && !_NVHPC_CUDA
 
 #if !defined(_CCCL_EXEC_CHECK_DISABLE)
-#  if defined(_CCCL_COMPILER_NVRTC) || defined(_CCCL_CUDA_COMPILER_NVHPC) || defined(_CCCL_CUDA_COMPILER_CLANG)
-#    define _CCCL_EXEC_CHECK_DISABLE
-#  elif defined(_CCCL_CUDA_COMPILER_NVCC) // defined(_CCCL_CUDA_COMPILER_NVCC)
+#  if defined(_CCCL_CUDA_COMPILER_NVCC)
 #    if defined(_CCCL_COMPILER_MSVC)
 #      define _CCCL_EXEC_CHECK_DISABLE __pragma("nv_exec_check_disable")
 #    else // ^^^ _CCCL_COMPILER_MSVC ^^^ / vvv !_CCCL_COMPILER_MSVC vvv
