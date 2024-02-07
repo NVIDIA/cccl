3566a8b160c37eba63fce77768786ef9e2f303e6
Jordan Rupprecht <jordanrupprecht@gmail.com>
2024-02-07 02:46:32 -0800

    Replace deprecated `_VSTD` macro with `std` (#1331)
    * Replace deprecated `_VSTD` macro with `std`

--- thrust/thrust/type_traits/is_contiguous_iterator.h
+++ thrust/thrust/type_traits/is_contiguous_iterator.h
@@ -147,7 +147,11 @@ struct is_libcxx_wrap_iter : false_type {};
 #if defined(_LIBCPP_VERSION)
 template <typename Iterator>
 struct is_libcxx_wrap_iter<
+#  if _LIBCPP_VERSION < 14000
   _VSTD::__wrap_iter<Iterator>
+#  else
+  std::__wrap_iter<Iterator>
+#  endif
 > : true_type {};
 #endif
 
