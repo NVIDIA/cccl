a1b216143c87a1ce81d3ae35c1f93f1cdcdd35a2
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2024-02-08 11:42:20 -0800

    adds some tests for large number of items

--- cub/test/catch2_test_device_merge_sort.cu
+++ cub/test/catch2_test_device_merge_sort.cu
@@ -31,6 +31,7 @@
 #include <thrust/detail/raw_pointer_cast.h>
 #include <thrust/equal.h>
 #include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/reverse_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/random.h>
@@ -64,6 +65,18 @@ using wide_key_types = c2h::type_list<std::uint32_t, double>;
 
 using value_types =
   c2h::type_list<std::uint8_t, float, c2h::custom_type_t<c2h::equal_comparable_t, c2h::less_comparable_t>>;
+
+template <typename OffsetT, typename KeyT = std::uint8_t>
+struct type_tuple
+{
+  using offset_t = OffsetT;
+  using key_t    = KeyT;
+};
+using offset_types =
+  c2h::type_list<type_tuple<std::int32_t>,
+                 type_tuple<std::int32_t, std::uint32_t>,
+                 type_tuple<std::uint32_t>,
+                 type_tuple<std::uint64_t>>;
 
 /**
  * Function object that maps the targeted sorted rank of an item to a key.
@@ -107,6 +120,64 @@ struct tuple_to_custom_op_t
     custom_val.key = static_cast<std::size_t>(thrust::get<0>(val));
     custom_val.val = static_cast<std::size_t>(thrust::get<1>(val));
     return custom_val;
+  }
+};
+
+template <typename UnsignedIntegralKeyT>
+struct index_to_key_value_op
+{
+  static constexpr std::size_t max_key_value =
+    static_cast<std::size_t>(::cuda::std::numeric_limits<UnsignedIntegralKeyT>::max());
+  static constexpr std::size_t lowest_key_value =
+    static_cast<std::size_t>(::cuda::std::numeric_limits<UnsignedIntegralKeyT>::lowest());
+  static constexpr std::size_t num_distinct_key_values = (max_key_value - lowest_key_value + std::size_t{1ULL});
+
+  __device__ __host__ UnsignedIntegralKeyT operator()(std::size_t index)
+  {
+    return static_cast<UnsignedIntegralKeyT>(index % num_distinct_key_values);
+  }
+};
+
+template <typename UnsignedIntegralKeyT>
+class index_to_expected_key_op
+{
+private:
+  static constexpr std::size_t max_key_value =
+    static_cast<std::size_t>(::cuda::std::numeric_limits<UnsignedIntegralKeyT>::max());
+  static constexpr std::size_t lowest_key_value =
+    static_cast<std::size_t>(::cuda::std::numeric_limits<UnsignedIntegralKeyT>::lowest());
+  static constexpr std::size_t num_distinct_key_values = (max_key_value - lowest_key_value + std::size_t{1ULL});
+
+  // item_count / num_distinct_key_values
+  std::size_t expected_count_per_item;
+  // num remainder items: item_count%num_distinct_key_values
+  std::size_t num_remainder_items;
+  // remainder item_count: expected_count_per_item+1
+  std::size_t remainder_item_count;
+
+public:
+  index_to_expected_key_op(std::size_t num_total_items)
+      : expected_count_per_item(num_total_items / num_distinct_key_values)
+      , num_remainder_items(num_total_items % num_distinct_key_values)
+      , remainder_item_count(expected_count_per_item + std::size_t{1ULL})
+  {}
+
+  __device__ __host__ UnsignedIntegralKeyT operator()(std::size_t index)
+  {
+    // The first (num_remainder_items * remainder_item_count) are items that appear once more often than the items that
+    // follow remainder_items_offset
+    std::size_t remainder_items_offset = num_remainder_items * remainder_item_count;
+
+    UnsignedIntegralKeyT target_item_index =
+      (index <= remainder_items_offset)
+        ?
+        // This is one of the remainder items
+        static_cast<UnsignedIntegralKeyT>(index / remainder_item_count)
+        :
+        // This is an item that appears exactly expected_count_per_item times
+        static_cast<UnsignedIntegralKeyT>(
+          num_remainder_items + ((index - remainder_items_offset) / expected_count_per_item));
+    return target_item_index;
   }
 };
 
@@ -307,4 +378,78 @@ CUB_TEST(
 
   REQUIRE(keys_expected == keys_in_out);
   REQUIRE(values_expected == values_in_out);
+}
+
+CUB_TEST("DeviceMergeSort::StableSortPairs works for large inputs", "[.][merge][sort][device][extensive]", offset_types)
+{
+  using testing_types_tuple = c2h::get<0, TestType>;
+  using key_t               = typename testing_types_tuple::key_t;
+  using offset_t            = typename c2h::get<0, TestType>::offset_t;
+
+  // Clamp 64-bit offset type problem sizes to just slightly larger than 2^32 items
+  auto num_items_ull =
+    std::min(static_cast<std::size_t>(::cuda::std::numeric_limits<offset_t>::max()) - 1,
+             ::cuda::std::numeric_limits<std::uint32_t>::max() + static_cast<std::size_t>(2000000ULL));
+  offset_t num_items = static_cast<offset_t>(num_items_ull);
+
+  thrust::device_vector<key_t> keys_in_out(num_items);
+
+  // TODO this will build on common functionality in https://github.com/NVIDIA/cccl/pull/1349
+  // SECTION("Random")
+  // {
+  //   try
+  //   {
+  //   }
+  //   catch (std::bad_alloc&)
+  //   {
+  //     std::cout << "Not enough memory, skipping large num items test" << std::endl;
+  //   }
+  // }
+
+  SECTION("Pre-sorted input")
+  {
+    try
+    {
+      // Pre-populated array with a constant value
+      auto counting_it = thrust::make_counting_iterator(std::size_t{0});
+      thrust::copy(counting_it, counting_it + num_items, keys_in_out.begin());
+
+      // Perform sort
+      stable_sort_keys(thrust::raw_pointer_cast(keys_in_out.data()), num_items, custom_less_op_t{});
+
+      // Perform comparison
+      auto expected_result_it = thrust::make_transform_iterator(
+        thrust::make_counting_iterator(std::size_t{}), index_to_expected_key_op<key_t>(num_items));
+      bool is_correct = thrust::equal(expected_result_it, expected_result_it + num_items, keys_in_out.begin());
+      REQUIRE(is_correct == true);
+    }
+    catch (std::bad_alloc&)
+    {
+      std::cout << "Not enough memory, skipping large num items test" << std::endl;
+    }
+  }
+
+  SECTION("Reverse-sorted input")
+  {
+    try
+    {
+      auto counting_it   = thrust::make_counting_iterator(std::size_t{0});
+      auto key_value_it  = thrust::make_transform_iterator(counting_it, index_to_key_value_op<key_t>{});
+      auto rev_sorted_it = thrust::make_reverse_iterator(key_value_it + num_items);
+      thrust::copy(rev_sorted_it, rev_sorted_it + num_items, keys_in_out.begin());
+
+      // Perform sort
+      stable_sort_keys(thrust::raw_pointer_cast(keys_in_out.data()), num_items, custom_less_op_t{});
+
+      // Perform comparison
+      auto expected_result_it = thrust::make_transform_iterator(
+        thrust::make_counting_iterator(std::size_t{}), index_to_expected_key_op<key_t>(num_items));
+      bool is_correct = thrust::equal(expected_result_it, expected_result_it + num_items, keys_in_out.cbegin());
+      REQUIRE(is_correct == true);
+    }
+    catch (std::bad_alloc&)
+    {
+      std::cout << "Not enough memory, skipping large num items test" << std::endl;
+    }
+  }
 }
