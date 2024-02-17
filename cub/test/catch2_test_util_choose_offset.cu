5616ee295aa50df48644dd97a576d7a907a63208
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2024-02-17 00:20:56 -0800

    adds test for choose_offset utils

---
+++ cub/test/catch2_test_util_choose_offset.cu
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the NVIDIA CORPORATION nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+#include <cub/detail/choose_offset.cuh>
+
+#include <cuda/std/type_traits>
+
+#include "catch2_test_helper.h"
+
+CUB_TEST("Tests ChooseOffsetT", "[util][type]")
+{
+  // Uses unsigned 32-bit type for signed 32-bit type
+  STATIC_REQUIRE(::cuda::std::is_same<typename cub::detail::ChooseOffsetT<std::int32_t>::Type, std::uint32_t>::value);
+
+  // Uses unsigned 32-bit type for type smaller than 32 bits
+  STATIC_REQUIRE(::cuda::std::is_same<typename cub::detail::ChooseOffsetT<std::int8_t>::Type, std::uint32_t>::value);
+
+  // Uses unsigned 64-bit type for signed 64-bit type
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::ChooseOffsetT<std::int64_t>::Type, unsigned long long>::value);
+}
+
+CUB_TEST("Tests PromoteSmallOffsetT", "[util][type]")
+{
+  // Uses input type for types of at least 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::int32_t>::Type, std::int32_t>::value);
+
+  // Uses input type for types of at least 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::uint32_t>::Type, std::uint32_t>::value);
+
+  // Uses input type for types of at least 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::uint64_t>::Type, std::uint64_t>::value);
+
+  // Uses input type for types of at least 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::int64_t>::Type, std::int64_t>::value);
+
+  // Uses 32-bit type for type smaller than 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::int8_t>::Type, std::int32_t>::value);
+
+  // Uses 32-bit type for type smaller than 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::int16_t>::Type, std::int32_t>::value);
+
+  // Uses 32-bit type for type smaller than 32 bits
+  STATIC_REQUIRE(
+    ::cuda::std::is_same<typename cub::detail::PromoteSmallOffsetT<std::uint16_t>::Type, std::int32_t>::value);
+}
