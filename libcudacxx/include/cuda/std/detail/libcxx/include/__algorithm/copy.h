2c9060d32f3a02ecb2caf2ce5e59c24495ba5eb1
Michael Schellenberger Costa <miscco@nvidia.com>
2024-02-21 09:55:42 +0100

    Move set algorithms to their own files

--- libcudacxx/include/cuda/std/detail/libcxx/include/__algorithm/copy.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__algorithm/copy.h
@@ -3,7 +3,7 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+// SPDX-FileCopyrightText: Copyright (c) 2023-24 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
@@ -22,6 +22,7 @@
 #  pragma system_header
 #endif // no system header
 
+#include "../__algorithm/iterator_operations.h"
 #include "../__algorithm/unwrap_iter.h"
 #include "../__type_traits/enable_if.h"
 #include "../__type_traits/is_constant_evaluated.h"
@@ -33,15 +34,16 @@
 
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
-template <class _InputIterator, class _OutputIterator>
-inline _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX11 _OutputIterator
-__copy(_InputIterator __first, _InputIterator __last, _OutputIterator __result)
+template <class _AlgPolicy, class _InputIterator, class _OutputIterator>
+inline _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX11
+  pair<_InputIterator, _OutputIterator>
+  __copy(_InputIterator __first, _InputIterator __last, _OutputIterator __result)
 {
   for (; __first != __last; ++__first, (void) ++__result)
   {
     *__result = *__first;
   }
-  return __result;
+  return {__last, __result};
 }
 
 template <class _Tp, class _Up>
@@ -66,11 +68,12 @@ __dispatch_memmove(_Up* __result, _Tp* __first, const size_t __n)
   }
 }
 
-template <class _Tp,
+template <class _AlgPolicy,
+          class _Tp,
           class _Up,
           __enable_if_t<_LIBCUDACXX_TRAIT(is_same, __remove_const_t<_Tp>, _Up), int> = 0,
           __enable_if_t<_LIBCUDACXX_TRAIT(is_trivially_copy_assignable, _Up), int>   = 0>
-inline _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX11 _Up*
+inline _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX11 pair<_Tp*, _Up*>
 __copy(_Tp* __first, _Tp* __last, _Up* __result)
 {
   const ptrdiff_t __n = __last - __first;
@@ -78,21 +81,22 @@ __copy(_Tp* __first, _Tp* __last, _Up* __result)
   {
     if (__dispatch_memmove(__result, __first, __n))
     {
-      return __result + __n;
+      return {__last, __result + __n};
     }
     for (ptrdiff_t __i = 0; __i < __n; ++__i)
     {
       *(__result + __i) = *(__first + __i);
     }
   }
-  return __result + __n;
+  return {__last, __result + __n};
 }
 
 template <class _InputIterator, class _OutputIterator>
 inline _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_INLINE_VISIBILITY _LIBCUDACXX_CONSTEXPR_AFTER_CXX11 _OutputIterator
 copy(_InputIterator __first, _InputIterator __last, _OutputIterator __result)
 {
-  return _CUDA_VSTD::__copy(__unwrap_iter(__first), __unwrap_iter(__last), __unwrap_iter(__result));
+  return _CUDA_VSTD::__copy<_ClassicAlgPolicy>(__unwrap_iter(__first), __unwrap_iter(__last), __unwrap_iter(__result))
+    .second;
 }
 
 _LIBCUDACXX_END_NAMESPACE_STD
---
+++ libcudacxx/test/libcudacxx/std/algorithms/alg.sorting/alg.set.operations/set.union/set_union_comp.pass.cpp
@@ -0,0 +1,100 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// <algorithm>
+
+// template<InputIterator InIter1, InputIterator InIter2, typename OutIter,
+//          CopyConstructible Compare>
+//   requires OutputIterator<OutIter, InIter1::reference>
+//         && OutputIterator<OutIter, InIter2::reference>
+//         && Predicate<Compare, InIter1::value_type, InIter2::value_type>
+//         && Predicate<Compare, InIter2::value_type, InIter1::value_type>
+//   constexpr OutIter       // constexpr after C++17
+//   set_union(InIter1 first1, InIter1 last1, InIter2 first2, InIter2 last2,
+//             OutIter result, Compare comp);
+
+#include <cuda/std/__algorithm>
+#include <cuda/std/cassert>
+
+#include "test_macros.h"
+#include "test_iterators.h"
+#include "../../sortable_helpers.h"
+
+template<class T, class Iter1, class Iter2, class OutIter>
+__host__ __device__ TEST_CONSTEXPR_CXX14 void test4()
+{
+    const T a[] = {11, 33, 31, 41};
+    const T b[] = {22, 32, 43, 42, 52};
+    {
+        T result[20] = {};
+        T expected[] = {11, 22, 33, 31, 41, 42, 52};
+        OutIter end = cuda::std::set_union(Iter1(a), Iter1(a+4), Iter2(b), Iter2(b+5), OutIter(result), typename T::Comparator());
+        assert(cuda::std::lexicographical_compare(result, base(end), expected, expected+7, T::less) == 0);
+        for (const T *it = base(end); it != result+20; ++it) {
+            assert(it->value == 0);
+        }
+    }
+    {
+        T result[20] = {};
+        T expected[] = {11, 22, 32, 31, 43, 42, 52};
+        OutIter end = cuda::std::set_union(Iter1(b), Iter1(b+5), Iter2(a), Iter2(a+4), OutIter(result), typename T::Comparator());
+        assert(cuda::std::lexicographical_compare(result, base(end), expected, expected+7, T::less) == 0);
+        for (const T *it = base(end); it != result+20; ++it) {
+            assert(it->value == 0);
+        }
+    }
+}
+
+template<class T, class Iter1, class Iter2>
+__host__ __device__ TEST_CONSTEXPR_CXX14 void test3()
+{
+    test4<T, Iter1, Iter2, cpp17_output_iterator<T*> >();
+    test4<T, Iter1, Iter2, forward_iterator<T*> >();
+    test4<T, Iter1, Iter2, bidirectional_iterator<T*> >();
+    test4<T, Iter1, Iter2, random_access_iterator<T*> >();
+    test4<T, Iter1, Iter2, T*>();
+}
+
+template<class T, class Iter1>
+__host__ __device__ TEST_CONSTEXPR_CXX14 void test2()
+{
+    test3<T, Iter1, cpp17_input_iterator<const T*> >();
+    test3<T, Iter1, forward_iterator<const T*> >();
+    test3<T, Iter1, bidirectional_iterator<const T*> >();
+    test3<T, Iter1, random_access_iterator<const T*> >();
+    test3<T, Iter1, const T*>();
+}
+
+template<class T>
+__host__ __device__ TEST_CONSTEXPR_CXX14 void test1()
+{
+    test2<T, cpp17_input_iterator<const T*> >();
+    test2<T, forward_iterator<const T*> >();
+    test2<T, bidirectional_iterator<const T*> >();
+    test2<T, random_access_iterator<const T*> >();
+    test2<T, const T*>();
+}
+
+__host__ __device__ TEST_CONSTEXPR_CXX14 bool test()
+{
+    test1<TrivialSortableWithComp>();
+    test1<NonTrivialSortableWithComp>();
+    return true;
+}
+
+int main(int, char**)
+{
+    test();
+#if TEST_STD_VER >= 2014
+  static_assert(test(), "");
+#endif
+
+    return 0;
+}
