18742c2f8f587666975c2fe67de99f409b515025
Michael Schellenberger Costa <miscco@nvidia.com>
2024-02-21 09:51:35 +0100

    Implement `ranges::ref_view`

---
+++ libcudacxx/test/libcudacxx/std/ranges/range.adaptors/range.all/range.ref.view/borrowing.compile.pass.cpp
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// UNSUPPORTED: msvc-19.16
+
+// template<class T>
+//   inline constexpr bool enable_borrowed_range<ref_view<T>> = true;
+
+#include <cuda/std/ranges>
+
+#include "test_range.h"
+
+static_assert(cuda::std::ranges::borrowed_range<cuda::std::ranges::ref_view<BorrowedRange>>);
+static_assert(cuda::std::ranges::borrowed_range<cuda::std::ranges::ref_view<BorrowedView>>);
+#if _LIBCUDACXX_HAS_RANGES
+static_assert(cuda::std::ranges::borrowed_range<cuda::std::ranges::ref_view<NonBorrowedView>>);
+#endif
+
+int main(int, char**)
+{
+  return 0;
+}
