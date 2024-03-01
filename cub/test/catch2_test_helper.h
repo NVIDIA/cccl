edda6228347fb411e199c1de886176e78c341665
Michael Schellenberger Costa <miscco@nvidia.com>
2024-03-01 14:41:06 -0800

    Add test coverage for SM90 without PTX ISA 8.0 (#1468)
    * Add test coverage for SM90 without PTX ISA 8.0
    
    * Silence internal catch2 warning about unused variable
    
    * Bring back the global variable suppression
    
    * Only supress for nvcc 11

--- cub/test/catch2_test_helper.h
+++ cub/test/catch2_test_helper.h
@@ -27,6 +27,7 @@
 
 #pragma once
 
+#include "cuda/std/detail/libcxx/include/__cccl/diagnostic.h"
 #include <metal.hpp>
 #include <type_traits>
 #include <cstdint>
@@ -34,6 +35,10 @@
 
 #include <cub/util_compiler.cuh>
 #include "test_util_vec.h"
+
+#if __CUDACC_VER_MAJOR__ == 11
+_CCCL_NV_DIAG_SUPPRESS(177) // catch2 may contain unused variableds
+#endif // nvcc-11
 
 #include <c2h/device_policy.cuh>
 #include <c2h/utility.cuh>
