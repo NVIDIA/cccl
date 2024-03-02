81bb039d1b21e522112a0c832461fa9fbb029df8
Allison Piper <alliepiper16@gmail.com>
2024-03-02 17:22:12 +0000

    Improvements to c2h::cpu_timer.
    - Add macros that can be enabled using `-DC2H_DEBUG_TIMING`.
    - Add RAII scoped_cpu_timer + macro.
    - Increase precision of output from ms -> us.

--- cub/test/c2h/cpu_timer.cuh
+++ cub/test/c2h/cpu_timer.cuh
@@ -31,6 +31,24 @@
 #include <iostream>
 #include <string>
 
+//#define C2H_DEBUG_TIMING
+
+#ifdef C2H_DEBUG_TIMING
+#  define C2H_TIME_SECTION_INIT() \
+    c2h::cpu_timer _c2h_timer_;   \
+    (void) _c2h_timer_
+#  define C2H_TIME_SECTION_RESET() _c2h_timer_.reset()
+#  define C2H_TIME_SECTION(label)  _c2h_timer_.print_elapsed_seconds_and_reset(label)
+#  define C2H_TIME_SCOPE(label)                          \
+    c2h::scoped_cpu_timer _c2h_scoped_cpu_timer_(label); \
+    (void) _c2h_scoped_cpu_timer_
+#else
+#  define C2H_TIME_SECTION_INIT()  /* no-op */ []() {}()
+#  define C2H_TIME_SECTION_RESET() /* no-op */ []() {}()
+#  define C2H_TIME_SECTION(label)  /* no-op */ []() {}()
+#  define C2H_TIME_SCOPE(label)    /* no-op */ []() {}()
+#endif
+
 namespace c2h
 {
 
@@ -40,7 +58,7 @@ class cpu_timer
 
 public:
   cpu_timer()
-  : m_start(std::chrono::high_resolution_clock::now())
+      : m_start(std::chrono::high_resolution_clock::now())
   {}
 
   void reset()
@@ -51,20 +69,43 @@ public:
   int elapsed_ms() const
   {
     auto duration = std::chrono::high_resolution_clock::now() - m_start;
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    auto ms       = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
     return static_cast<int>(ms.count());
+  }
+
+  std::uint64_t elapsed_us() const
+  {
+    auto duration = std::chrono::high_resolution_clock::now() - m_start;
+    auto us       = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+    return static_cast<std::uint64_t>(us.count());
   }
 
   void print_elapsed_seconds(const std::string& label)
   {
-    std::cout << label << ": " << (this->elapsed_ms() / 1000.f) << "s\n";
+    printf("%0.6f s: %s\n", this->elapsed_us() / 1000000.f, label.c_str());
   }
 
-    void print_elapsed_seconds_and_reset(const std::string& label)
+  void print_elapsed_seconds_and_reset(const std::string& label)
   {
     this->print_elapsed_seconds(label);
     this->reset();
   }
 };
 
-}
+class scoped_cpu_timer
+{
+  cpu_timer m_timer;
+  std::string m_label;
+
+public:
+  explicit scoped_cpu_timer(std::string label)
+      : m_label(std::move(label))
+  {}
+
+  ~scoped_cpu_timer()
+  {
+    m_timer.print_elapsed_seconds(m_label);
+  }
+};
+
+} // namespace c2h
