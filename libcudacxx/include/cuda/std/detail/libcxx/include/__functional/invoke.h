89cc6b52e725cb397ebbe0f33cd4f47e4cf34324
Revaj <jarorozco92@gmail.com>
2024-03-07 04:12:06 +0000

    Fix source from PR
    Fix typo in diagnostic message
    Use _LIBCUDACXX_CUDACC_BELOW_12_3 instead of CUDART_VERSION
    and swap branches from diagnostic

--- libcudacxx/include/cuda/std/detail/libcxx/include/__functional/invoke.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__functional/invoke.h
@@ -481,22 +481,22 @@ struct __invoke_of
 {
 #if defined(__NVCC__) && defined(__CUDACC_EXTENDED_LAMBDA__) && \
    !defined(__CUDA_ARCH__)
-  #if defined(CUDART_VERSION) && (CUDART_VERSION >= 12030) 
-    static_assert(!__nv_is_extended_device_lambda_closure_type(_Fp) ||
-                   __nv_is_extended_host_device_lambda_closure_type(_Fp) ||
-                   __nv_is_extended_device_lambda_with_preserved_return_type(_Fp),
-                   "Attempt to use an extended __device__ lambda in a context "
-                   "that requires querying its return type in host code. Use a "
-                   "named function object, a __host__ __device__ lambda, or "
-                   "cuda::proclaim_return_type or a extended __device__ lambda "
-                   "with preserved return type instead.");
-  #elif
+  #if defined(_LIBCUDACXX_CUDACC_BELOW_12_3)
     static_assert(!__nv_is_extended_device_lambda_closure_type(_Fp),
                   "Attempt to use an extended __device__ lambda in a context "
                   "that requires querying its return type in host code. Use a "
                   "named function object, a __host__ __device__ lambda, or "
                   "cuda::proclaim_return_type instead.");
-  #endif
+  #else  // ^^^ _LIBCUDACXX_CUDACC_BELOW_12_3 ^^^ / vvv !_LIBCUDACXX_CUDACC_BELOW_12_3 vvv
+    static_assert(!__nv_is_extended_device_lambda_closure_type(_Fp) ||
+                   __nv_is_extended_host_device_lambda_closure_type(_Fp) ||
+                   __nv_is_extended_device_lambda_with_preserved_return_type(_Fp),
+                   "Attempt to use an extended __device__ lambda in a context "
+                   "that requires querying its return type in host code. Use a "
+                   "named function object, a __host__ __device__ lambda, "
+                   "cuda::proclaim_return_type, or an extended __device__ lambda "
+                   "with preserved return type instead.");
+  #endif // !_LIBCUDACXX_CUDACC_BELOW_12_3
 #endif
 };
 
