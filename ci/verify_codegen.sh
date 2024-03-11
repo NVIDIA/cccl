dbf9749eab80c877cf37c0e83637c6d433f6bb8c
Wesley Maxey <71408887+wmaxey@users.noreply.github.com>
2024-03-11 13:21:13 -0700

    Make libcudacxx's codegen part of CI and add it to the project. (#1526)
    * Add a CMake config for building/installing/testing libcudacxx's codegen
    
    * Move some functions in build_common.sh to a helper
    
    * Add a CI script to verify codegen is up to date.
    
    * Add a CI step that verifies that codegen or generated header are synchronized
    
    * Fix verify_codegen.sh permissions
    
    * Install pre-reqs for build on default ubuntu runner
    
    * Fix output file in codegen.cpp
    
    * Make codegen build/test default to OFF
    
    * Whitespace fixup
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>
    
    ---------
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

---
+++ ci/verify_codegen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eo pipefail
+
+# Ensure the script is being executed in its containing directory
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+# TBD: verbose? any extra args?
+
+source ./pretty_printing.sh
+
+pushd .. > /dev/null
+GROUP_NAME="🛠️  CMake Configure Libcudacxx Codegen"
+run_command "$GROUP_NAME" cmake --preset libcudacxx-codegen
+status=$?
+popd > /dev/null
+
+pushd .. > /dev/null
+GROUP_NAME="🏗️  Build Libcudacxx Codegen"
+run_command "$GROUP_NAME" cmake --build --preset libcudacxx-codegen
+
+status=$?
+popd > /dev/null
+
+pushd .. > /dev/null
+GROUP_NAME="🚀  Test Libcudacxx Codegen"
+run_command "$GROUP_NAME" ctest --preset libcudacxx-codegen
+status=$?
+popd > /dev/null
