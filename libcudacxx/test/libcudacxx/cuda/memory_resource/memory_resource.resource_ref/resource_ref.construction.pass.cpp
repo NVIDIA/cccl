4bf5f9a2bf1e0f5525403aa905ce6f7b7ee7cdda
Michael Schellenberger Costa <miscco@nvidia.com>
2024-03-13 09:55:52 +0100

    Drop MSVC_2017 support for memory_resource

--- libcudacxx/test/libcudacxx/cuda/memory_resource/memory_resource.resource_ref/resource_ref.construction.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/memory_resource/memory_resource.resource_ref/resource_ref.construction.pass.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11
+// UNSUPPORTED: msvc-19.16
 // UNSUPPORTED: nvrtc
 
 // cuda::mr::resource_ref construction
