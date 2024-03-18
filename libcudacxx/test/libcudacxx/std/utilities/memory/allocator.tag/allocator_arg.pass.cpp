485dd8c1ea8710fd2a2c7ff8a69fb5d6c4d13c85
Michael Schellenberger Costa <miscco@nvidia.com>
2024-03-18 08:52:12 +0100

    Move `allocator_arg_t` into tis own file

---
+++ libcudacxx/test/libcudacxx/std/utilities/memory/allocator.tag/allocator_arg.pass.cpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// <memory>
+
+// struct allocator_arg_t { explicit allocator_arg_t() = default; };
+// const allocator_arg_t allocator_arg = allocator_arg_t();
+
+#include <cuda/std/__memory>
+
+#include "test_macros.h"
+
+__host__ __device__ void test(cuda::std::allocator_arg_t) {}
+
+int main(int, char**)
+{
+    test(cuda::std::allocator_arg);
+
+    return 0;
+}
