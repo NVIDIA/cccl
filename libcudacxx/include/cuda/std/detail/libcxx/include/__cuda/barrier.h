1182640ecbe0b3cbb94338c5d234787a477804a8
Michael Schellenberger Costa <miscco@nvidia.com>
2024-03-23 15:06:32 +0100

    Ensure that `cuda::aligned_size_t` is usable in a constepxr context
    Fixes [BUG]: `aligned_size_t` constructor and conversion operator are not constexpr #1563

--- libcudacxx/include/cuda/std/detail/libcxx/include/__cuda/barrier.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__cuda/barrier.h
@@ -46,14 +46,18 @@ _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 template<thread_scope _Scope>
 class pipeline;
 
-template<_CUDA_VSTD::size_t _Alignment>
-struct aligned_size_t {
-    static constexpr _CUDA_VSTD::size_t align = _Alignment;
-    _CUDA_VSTD::size_t value;
-    _LIBCUDACXX_INLINE_VISIBILITY
-    explicit aligned_size_t(size_t __s) : value(__s) { }
-    _LIBCUDACXX_INLINE_VISIBILITY
-    operator size_t() const { return value; }
+template <_CUDA_VSTD::size_t _Alignment>
+struct aligned_size_t
+{
+  static constexpr _CUDA_VSTD::size_t align = _Alignment;
+  _CUDA_VSTD::size_t value;
+  _LIBCUDACXX_INLINE_VISIBILITY explicit constexpr aligned_size_t(size_t __s)
+      : value(__s)
+  {}
+  _LIBCUDACXX_INLINE_VISIBILITY constexpr operator size_t() const
+  {
+    return value;
+  }
 };
 
 // Type only used for logging purpose
