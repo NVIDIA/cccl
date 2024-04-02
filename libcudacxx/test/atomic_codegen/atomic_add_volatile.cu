8a674f9ab4dc1c31f4252e9288549a89e2655e63
Wesley Maxey <wesley.maxey@gmail.com>
2024-04-02 16:10:40 -0700

    Add atomic codegen tests.
    * Currently only enabled if FileCheck is found.
    * Add a preset maybe to enable building this specifically?
    * TODO: Maybe generate the tests.

---
+++ libcudacxx/test/atomic_codegen/atomic_add_volatile.cu
@@ -0,0 +1,18 @@
+#include <cuda/atomic>
+
+__global__ void add_relaxed_device_volatile(int* data, int n) {
+    if (n) {
+        volatile auto ref = cuda::atomic_ref<int, cuda::thread_scope_device>{*(data)};
+        ref.fetch_add(n, cuda::std::memory_order_relaxed);
+    }
+}
+
+/*
+## SM80 checks
+; SM8X: Fatbin elf code
+; SM8X: code for sm_8{{[0-9]}}
+; SM8X-DAG:  {{^.*}}Function : {{.*}}add_relaxed_device_volatile{{.*$}}
+; SM8X:      {{^.*}}STL.64{{.*$}}
+; SM8X:      {{^.*}}ATOM.E.ADD.STRONG.GPU{{.*$}}
+; SM8X-DAG:  {{^.*}}EXIT{{.*$}}
+*/
