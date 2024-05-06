91bc0361b5ca548439f091b4553ed9ec04e99858
Allison Piper <alliepiper16@gmail.com>
2024-05-06 18:55:46 +0000

    Move deprecation markup to `result_of` definition.
    Having the deprecation markup on the declaration instead of the definition cause the deprecation diagnostics to be emitted when defining the deprecate type on gcc.
    Moving the markup fixes this.

--- libcudacxx/include/cuda/std/__type_traits/result_of.h
+++ libcudacxx/include/cuda/std/__type_traits/result_of.h
@@ -28,10 +28,11 @@ _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 #if _CCCL_STD_VER <= 2017 || defined(_LIBCUDACXX_ENABLE_CXX20_REMOVED_TYPE_TRAITS)
 template <class _Callable>
-class _LIBCUDACXX_DEPRECATED_IN_CXX17 result_of;
+class result_of;
 
 template <class _Fp, class... _Args>
-class _LIBCUDACXX_TEMPLATE_VIS result_of<_Fp(_Args...)> : public __invoke_of<_Fp, _Args...>
+class _LIBCUDACXX_DEPRECATED_IN_CXX17 _LIBCUDACXX_TEMPLATE_VIS result_of<_Fp(_Args...)>
+    : public __invoke_of<_Fp, _Args...>
 {};
 
 #  if _CCCL_STD_VER > 2011
