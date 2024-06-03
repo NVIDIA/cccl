35f3e50899d6c6047f2fdaa2d73c87c9aa4c37af
ptaylor <paul.e.taylor@me.com>
2024-06-03 10:50:44 -0700

    copy over .devcontainer/launch.sh from https://github.com/NVIDIA/cccl/pull/1667

---
+++ .devcontainer/docker-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Maybe change the UID/GID of the container's non-root user to match the host's UID/GID
+
+: "${REMOTE_USER:="coder"}";
+: "${OLD_UID:=}";
+: "${OLD_GID:=}";
+: "${NEW_UID:=}";
+: "${NEW_GID:=}";
+
+eval "$(sed -n "s/${REMOTE_USER}:[^:]*:\([^:]*\):\([^:]*\):[^:]*:\([^:]*\).*/OLD_UID=\1;OLD_GID=\2;HOME_FOLDER=\3/p" /etc/passwd)";
+eval "$(sed -n "s/\([^:]*\):[^:]*:${NEW_UID}:.*/EXISTING_USER=\1/p" /etc/passwd)";
+eval "$(sed -n "s/\([^:]*\):[^:]*:${NEW_GID}:.*/EXISTING_GROUP=\1/p" /etc/group)";
+
+if [ -z "$OLD_UID" ]; then
+    echo "Remote user not found in /etc/passwd ($REMOTE_USER).";
+    exec "$@";
+elif [ "$OLD_UID" = "$NEW_UID" ] && [ "$OLD_GID" = "$NEW_GID" ]; then
+    echo "UIDs and GIDs are the same ($NEW_UID:$NEW_GID).";
+    exec "$@";
+elif [ "$OLD_UID" != "$NEW_UID" ] && [ -n "$EXISTING_USER" ]; then
+    echo "User with UID exists ($EXISTING_USER=$NEW_UID).";
+    exec "$@";
+else
+    if [ "$OLD_GID" != "$NEW_GID" ] && [ -n "$EXISTING_GROUP" ]; then
+        echo "Group with GID exists ($EXISTING_GROUP=$NEW_GID).";
+        NEW_GID="$OLD_GID";
+    fi
+    echo "Updating UID:GID from $OLD_UID:$OLD_GID to $NEW_UID:$NEW_GID.";
+    sed -i -e "s/\(${REMOTE_USER}:[^:]*:\)[^:]*:[^:]*/\1${NEW_UID}:${NEW_GID}/" /etc/passwd;
+    if [ "$OLD_GID" != "$NEW_GID" ]; then
+        sed -i -e "s/\([^:]*:[^:]*:\)${OLD_GID}:/\1${NEW_GID}:/" /etc/group;
+    fi
+
+    # Fast parallel `chown -R`
+    find "$HOME_FOLDER/" -not -user "$REMOTE_USER" -print0 \
+  | xargs -0 -r -n1 -P"$(nproc --all)" chown "$NEW_UID:$NEW_GID"
+
+    # shellcheck disable=SC2155
+    # Create a list of the container startup environment variable names to pass to su
+    declare -a _vars="($(env | grep '=' | grep -v '/root' | grep -Pv '^\s' | cut -d= -f1 | grep -Pv '^(.*HOME.*|PS1|_)$'))";
+    # Run the container command as $REMOTE_USER, preserving the container startup environment
+    exec su -s "$SHELL" -w "$(IFS=,; echo "${_vars[*]}")" - "$REMOTE_USER" -- cccl/.devcontainer/cccl-entrypoint.sh "$@";
+fi
--- .devcontainer/launch.sh
+++ .devcontainer/launch.sh
@@ -11,17 +11,38 @@ print_help() {
     echo "the top-level devcontainer in .devcontainer/devcontainer.json will be used."
     echo ""
     echo "Options:"
-    echo "  -c, --cuda       Specify the CUDA version. E.g., 12.2"
-    echo "  -H, --host       Specify the host compiler. E.g., gcc12"
-    echo "  -d, --docker     Launch the development environment in Docker directly without using VSCode."
-    echo "  -h, --help       Display this help message and exit."
+    echo "  -c, --cuda               Specify the CUDA version. E.g., 12.2"
+    echo "  -H, --host               Specify the host compiler. E.g., gcc12"
+    echo "  -d, --docker             Launch the development environment in Docker directly without using VSCode."
+    echo "  --gpus gpu-request       GPU devices to add to the container ('all' to pass all GPUs)."
+    echo "  -e, --env list           Set additional container environment variables."
+    echo "  -v, --volume list        Bind mount a volume."
+    echo "  -h, --help               Display this help message and exit."
+}
+
+_upvar() {
+    if unset -v "$1"; then
+        if (( $# == 2 )); then
+            eval $1=\"\$2\";
+        else
+            eval $1=\(\"\${@:2}\"\);
+        fi;
+    fi
 }
 
 parse_options() {
-    local OPTIONS=c:H:dh
-    local LONG_OPTIONS=cuda:,host:,docker,help
-    local PARSED_OPTIONS=$(getopt -n "$0" -o "${OPTIONS}" --long "${LONG_OPTIONS}" -- "$@")
+    local -;
+    set -euo pipefail;
 
+    local UNPARSED="${!#}";
+    set -- "${@:1:$#-1}";
+
+    local OPTIONS=c:e:H:dhv:
+    local LONG_OPTIONS=cuda:,env:,host:,gpus:,volume:,docker,help
+    # shellcheck disable=SC2155
+    local PARSED_OPTIONS="$(getopt -n "$0" -o "${OPTIONS}" --long "${LONG_OPTIONS}" -- "$@")"
+
+    # shellcheck disable=SC2181
     if [[ $? -ne 0 ]]; then
         exit 1
     fi
@@ -34,8 +55,16 @@ parse_options() {
                 cuda_version="$2"
                 shift 2
                 ;;
+            -e|--env)
+                env_vars+=("$1" "$2")
+                shift 2
+                ;;
             -H|--host)
                 host_compiler="$2"
+                shift 2
+                ;;
+            --gpus)
+                gpu_request="$2"
                 shift 2
                 ;;
             -d|--docker)
@@ -46,8 +75,13 @@ parse_options() {
                 print_help
                 exit 0
                 ;;
+            -v|--volume)
+                volumes+=("$1" "$2")
+                shift 2
+                ;;
             --)
                 shift
+                _upvar "${UNPARSED}" "${@}"
                 break
                 ;;
             *)
@@ -59,20 +93,217 @@ parse_options() {
     done
 }
 
+# shellcheck disable=SC2155
 launch_docker() {
-    DOCKER_IMAGE=$(grep "image" "${path}/devcontainer.json" | sed 's/.*: "\(.*\)",/\1/')
-    echo "Found image: ${DOCKER_IMAGE}"
-    docker pull ${DOCKER_IMAGE}
-    docker run   \
-        -it --rm \
-        --user coder \
-        --workdir /home/coder/cccl \
-        --mount type=bind,src="$(pwd)",dst='/home/coder/cccl' \
-        ${DOCKER_IMAGE} \
-        /bin/bash
+    local -;
+    set -euo pipefail;
+
+    tabs_to_spaces() {
+        sed $'s/\t/ /g'
+    }
+
+    no_empty_lines() {
+        grep -v -e '^$' || [ "$?" == "1" ]
+    }
+
+    trim_leading_whitespace() {
+        sed 's/^[[:space:]]*//'
+    }
+
+    trim_trailing_whitespace() {
+        sed 's/[[:space:]]*$//'
+    }
+
+    remove_leading_char() {
+        cut -d"$1" -f1 --complement
+    }
+
+    remove_trailing_char() {
+        rev | cut -d"$1" -f1 --complement | rev
+    }
+
+    strip_enclosing_chars() {
+        remove_leading_char "$1" \
+      | remove_trailing_char "$2"
+    }
+
+    read_docker_image_metadata() {
+        docker inspect --type image --format '{{json .Config.Labels}}' "$DOCKER_IMAGE" \
+      | sed -r 's/.*"devcontainer.metadata":[ ]*"(.*[]\)])",?.*/\1/g' \
+      | sed -r 's/\\\"/"/g'
+    }
+
+    json_map_key_to_shell_syntax() {
+        sed -r 's/^"(.*)":[ ]*?([^,]*),?$/\1=\2/g'
+    }
+
+    translate_local_envvars_to_shell_syntax() {
+        sed -r 's/\$\{localEnv:([^\:]*):?(.*)\}/${\1:-\2}/g'
+    }
+
+    inline_local_workspace_folder() {
+        sed "s@\${localWorkspaceFolder}@$(pwd)@g"
+    }
+
+    inline_container_workspace_folder() {
+        sed "s@\${containerWorkspaceFolder}@${WORKSPACE_FOLDER:-}@g"
+    }
+
+    inline_local_workspace_folder_basename() {
+        sed "s@\${localWorkspaceFolderBasename}@$(basename "$(pwd)")@g"
+    }
+
+    transform_to_one_line() {
+        tabs_to_spaces \
+      | trim_leading_whitespace \
+      | trim_trailing_whitespace \
+      | inline_local_workspace_folder \
+      | inline_container_workspace_folder \
+      | inline_local_workspace_folder_basename \
+      | translate_local_envvars_to_shell_syntax \
+      | tr -s '\n' '\t'
+    }
+
+    json_string() {
+        transform_to_one_line \
+      | grep -Po "\"$1\":\s*\"(.*)\"" \
+      | sed -r "s/.*\"$1\":[ ]*\"([^\"]*)\",?.*/\1/g" \
+      | no_empty_lines
+    }
+
+    json_map() {
+        transform_to_one_line \
+      | grep -Po "\"$1\":\s*{(.*?)\s*?}[^\"]" \
+      | strip_enclosing_chars '{' '}' \
+      | tr -s '\t' '\n' \
+      | json_map_key_to_shell_syntax \
+      | no_empty_lines
+    }
+
+    json_array() {
+        transform_to_one_line \
+      | grep -Po "\"$1\":\s*\[(.*?)\s*?\][^\"]" \
+      | strip_enclosing_chars '[' ']' \
+      | tr -s '\t' '\n' \
+      | sed -r 's/", "/"\n"/g' \
+      | sed -r 's/^(.*"),$/\1/' \
+      | no_empty_lines
+    }
+
+    # Read image
+    local DOCKER_IMAGE="$(
+        json_string "image" < "${path}/devcontainer.json"
+    )"
+
+    # Read workspaceFolder
+    local WORKSPACE_FOLDER="$(
+        json_string "workspaceFolder" < "${path}/devcontainer.json"
+    )"
+
+    # Read remoteUser
+    local REMOTE_USER="$(
+        json_string "remoteUser" < "${path}/devcontainer.json"
+    )"
+
+    if test -z "${REMOTE_USER:-}"; then
+        REMOTE_USER="$(
+            # Read remoteUser from image metadata
+            read_docker_image_metadata \
+          | json_string "remoteUser"
+        )"
+    fi
+
+    # Read runArgs
+    local -a RUN_ARGS="($(
+        json_array "runArgs" < "${path}/devcontainer.json"
+    ))"
+
+    # Read initializeCommand
+    local -a INITIALIZE_COMMAND="($(
+        json_array "initializeCommand" < "${path}/devcontainer.json"
+    ))"
+
+    local -a ENV_VARS="($(
+        json_map "containerEnv" < "${path}/devcontainer.json" \
+      | sed -r 's/(.*)=(.*)/--env \1=\2/'
+    ))";
+
+    local -a MOUNTS="($(
+        tee < "${path}/devcontainer.json"   \
+            1>/dev/null                     \
+            >(json_array "mounts")          \
+            >(json_string "workspaceMount") \
+      | xargs -r -I% echo --mount '%'
+    ))"
+
+    # Update run args and env vars
+
+    for flag in rm init; do
+        if [[ " ${RUN_ARGS[*]} " != *" --${flag} "* ]]; then
+            RUN_ARGS+=("--${flag}")
+        fi
+    done
+
+    if [[ " ${RUN_ARGS[*]} " != *" --pull"* ]]; then
+        RUN_ARGS+=(--pull always)
+    fi
+
+    if test -n "${gpu_request:-}"; then
+        RUN_ARGS+=(--gpus "${gpu_request}")
+    else
+        # Read hostRequirements.gpu
+        local GPU_REQUEST="$(
+            json_map "hostRequirements" < "${path}/devcontainer.json" \
+          | grep 'gpu=' \
+          | sed -r 's/(.*)=(.*)/\2/' \
+          | xargs
+        )"
+        if test "${GPU_REQUEST:-false}" = true; then
+            RUN_ARGS+=(--gpus all)
+        elif test "${GPU_REQUEST:-false}" = optional && \
+             command -v nvidia-container-runtime >/dev/null 2>&1; then
+            RUN_ARGS+=(--gpus all)
+        fi
+    fi
+
+    RUN_ARGS+=(--workdir "${WORKSPACE_FOLDER:-/home/coder/cccl}")
+
+    if test -n "${REMOTE_USER:-}"; then
+        ENV_VARS+=(--env NEW_UID="$(id -u)")
+        ENV_VARS+=(--env NEW_GID="$(id -g)")
+        ENV_VARS+=(--env REMOTE_USER="$REMOTE_USER")
+        RUN_ARGS+=(-u root:root)
+        RUN_ARGS+=(--entrypoint "${WORKSPACE_FOLDER:-/home/coder/cccl}/.devcontainer/docker-entrypoint.sh")
+    fi
+
+    if test -n "${SSH_AUTH_SOCK:-}"; then
+        ENV_VARS+=(--env "SSH_AUTH_SOCK=/tmp/ssh-auth-sock")
+        MOUNTS+=(--mount "source=${SSH_AUTH_SOCK},target=/tmp/ssh-auth-sock,type=bind")
+    fi
+
+    if test -v volumes && test ${#volumes[@]} -gt 0; then
+        MOUNTS+=("${volumes[@]}")
+    fi
+
+    if test -v env_vars && test ${#env_vars[@]} -gt 0; then
+        ENV_VARS+=("${env_vars[@]}")
+    fi
+
+    if test "${#INITIALIZE_COMMAND[@]}" -gt 0; then
+        eval "${INITIALIZE_COMMAND[*]@Q}"
+    fi
+
+    exec docker run -it \
+        "${RUN_ARGS[@]}" \
+        "${ENV_VARS[@]}" \
+        "${MOUNTS[@]}" \
+        "${DOCKER_IMAGE}" \
+        "$@"
 }
 
 launch_vscode() {
+    local -;
+    set -euo pipefail;
     # Since Visual Studio Code allows only one instance per `devcontainer.json`,
     # this code prepares a unique temporary directory structure for each launch of a devcontainer.
     # By doing so, it ensures that multiple instances of the same environment can be run
@@ -85,7 +316,7 @@ launch_vscode() {
     mkdir -p "${tmpdir}"
     mkdir -p "${tmpdir}/.devcontainer"
     cp -arL "${path}/devcontainer.json" "${tmpdir}/.devcontainer"
-    sed -i 's@\\${localWorkspaceFolder}@$(pwd)@g' "${tmpdir}/.devcontainer/devcontainer.json"
+    sed -i "s@\${localWorkspaceFolder}@$(pwd)@g" "${tmpdir}/.devcontainer/devcontainer.json"
     local path="${tmpdir}"
     local hash="$(echo -n "${path}" | xxd -pu - | tr -d '[:space:]')"
     local url="vscode://vscode-remote/dev-container+${hash}/home/coder/cccl"
@@ -105,7 +336,9 @@ launch_vscode() {
 }
 
 main() {
-    parse_options "$@"
+    local -a unparsed;
+    parse_options "$@" unparsed;
+    set -- "${unparsed[@]}";
 
     # If no CTK/Host compiler are provided, just use the default environment
     if [[ -z ${cuda_version:-} ]] && [[ -z ${host_compiler:-} ]]; then
@@ -120,7 +353,7 @@ main() {
     fi
 
     if ${docker_mode:-'false'}; then
-        launch_docker
+        launch_docker "$@"
     else
         launch_vscode
     fi
