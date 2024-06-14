5d52970f9fe5b5463727107126cdaa918c884211
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2024-06-14 15:42:09 +0200

    Drop facilities deprecated in a previous CUB major version

--- cub/cub/cub.cuh
+++ cub/cub/cub.cuh
@@ -105,7 +105,6 @@
 #include <cub/iterator/counting_input_iterator.cuh>
 #include <cub/iterator/discard_output_iterator.cuh>
 #include <cub/iterator/tex_obj_input_iterator.cuh>
-#include <cub/iterator/tex_ref_input_iterator.cuh>
 #include <cub/iterator/transform_input_iterator.cuh>
 
 // Util
