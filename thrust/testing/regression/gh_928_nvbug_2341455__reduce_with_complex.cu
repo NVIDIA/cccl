175e2859745b5794b721de0047a29e9358a21fde
Allison Piper <alliepiper16@gmail.com>
2024-06-21 15:05:50 +0000

    Remove legacy benchmarks and other dvs/p4 remenants.
    Removes dead code and other assorted eldritch horrors from bygone eras.

--- thrust/testing/regression/gh_928_nvbug_2341455__reduce_with_complex.cu
+++
@@ -1,9 +0,0 @@
-#include <thrust/complex.h>
-#include <thrust/device_vector.h>
-#include <thrust/reduce.h>
-
-int main()
-{
-  thrust::device_vector<thrust::complex<double>> d(5);
-  thrust::reduce(d.begin(), d.end());
-}
