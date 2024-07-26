76b37a0fb4fedc321c2d3e249b3ff5803d4ce882
Wesley Maxey <wesley.maxey@gmail.com>
2024-07-26 12:26:17 -0700

    Make host atomics simpler by removing the ugly alignment type

--- libcudacxx/include/cuda/std/__atomic/functions/host.h
+++ libcudacxx/include/cuda/std/__atomic/functions/host.h
@@ -40,31 +40,6 @@ struct __atomic_alignment_wrapper
   _CCCL_ALIGNAS(sizeof(_Tp)) _Tp __atom;
 };
 
-template <typename _Tp>
-__atomic_alignment_wrapper<__remove_cv_t<_Tp>>& __atomic_auto_align(_Tp* __a)
-{
-  using __aligned_t = __atomic_alignment_wrapper<__remove_cv_t<_Tp>>;
-  return *reinterpret_cast<__aligned_t*>(__a);
-};
-template <typename _Tp>
-const __atomic_alignment_wrapper<__remove_cv_t<_Tp>>& __atomic_auto_align(const _Tp* __a)
-{
-  using __aligned_t = const __atomic_alignment_wrapper<__remove_cv_t<_Tp>>;
-  return *reinterpret_cast<__aligned_t*>(__a);
-};
-template <typename _Tp>
-volatile __atomic_alignment_wrapper<__remove_cv_t<_Tp>>& __atomic_auto_align(volatile _Tp* __a)
-{
-  using __aligned_t = volatile __atomic_alignment_wrapper<__remove_cv_t<_Tp>>;
-  return *reinterpret_cast<__aligned_t*>(__a);
-};
-template <typename _Tp>
-const volatile __atomic_alignment_wrapper<__remove_cv_t<_Tp>>& __atomic_auto_align(const volatile _Tp* __a)
-{
-  using __aligned_t = const volatile __atomic_alignment_wrapper<__remove_cv_t<_Tp>>;
-  return *reinterpret_cast<__aligned_t*>(__a);
-};
-
 // Guard ifdef for lock free query in case it is assigned elsewhere (MSVC/CUDA)
 inline void __atomic_thread_fence_host(memory_order __order)
 {
@@ -76,19 +51,24 @@ inline void __atomic_signal_fence_host(memory_order __order)
   __atomic_signal_fence(__atomic_order_to_int(__order));
 }
 
+template <typename _Tp>
+_Tp* __atomic_force_align_host(_Tp* __a)
+{
+  return const_cast<_Tp*>(
+    static_cast<__remove_cv_t<_Tp>*>(__builtin_assume_aligned(const_cast<__remove_cv_t<_Tp>*>(__a), sizeof(_Tp))));
+}
+
 template <typename _Tp, typename _Up>
 inline void __atomic_store_host(_Tp* __a, _Up __val, memory_order __order)
 {
-  __atomic_store(
-    &__atomic_auto_align<_Tp>(__a), &__atomic_auto_align<__remove_cv_t<_Tp>>(&__val), __atomic_order_to_int(__order));
+  __atomic_store(__atomic_force_align_host(__a), &__val, __atomic_order_to_int(__order));
 }
 
 template <typename _Tp>
 inline auto __atomic_load_host(_Tp* __a, memory_order __order) -> __remove_cv_t<_Tp>
 {
   __remove_cv_t<_Tp> __ret;
-  __atomic_load(
-    &__atomic_auto_align<_Tp>(__a), &__atomic_auto_align<__remove_cv_t<_Tp>>(&__ret), __atomic_order_to_int(__order));
+  __atomic_load(__atomic_force_align_host(__a), &__ret, __atomic_order_to_int(__order));
   return __ret;
 }
 
@@ -96,21 +76,18 @@ template <typename _Tp, typename _Up>
 inline auto __atomic_exchange_host(_Tp* __a, _Up __val, memory_order __order) -> __remove_cv_t<_Tp>
 {
   __remove_cv_t<_Tp> __ret;
-  __atomic_exchange(&__atomic_auto_align<_Tp>(__a),
-                    &__atomic_auto_align<__remove_cv_t<_Tp>>(&__val),
-                    &__atomic_auto_align<__remove_cv_t<_Tp>>(&__ret),
-                    __atomic_order_to_int(__order));
+  __atomic_exchange(__atomic_force_align_host(__a), &__val, &__ret, __atomic_order_to_int(__order));
   return __ret;
 }
 
 template <typename _Tp, typename _Up>
 inline bool __atomic_compare_exchange_strong_host(
-  _Tp* __a, _Up* __expected, _Up __value, memory_order __success, memory_order __failure)
+  _Tp* __a, _Up* __expected, _Up __desired, memory_order __success, memory_order __failure)
 {
   return __atomic_compare_exchange(
-    &__atomic_auto_align<_Tp>(__a),
-    &__atomic_auto_align<__remove_cv_t<_Tp>>(__expected),
-    &__atomic_auto_align<__remove_cv_t<_Tp>>(&__value),
+    __atomic_force_align_host(__a),
+    __expected,
+    &__desired,
     false,
     __atomic_order_to_int(__success),
     __atomic_failure_order_to_int(__failure));
@@ -118,12 +95,12 @@ inline bool __atomic_compare_exchange_strong_host(
 
 template <typename _Tp, typename _Up>
 inline bool __atomic_compare_exchange_weak_host(
-  _Tp* __a, _Up* __expected, _Up __value, memory_order __success, memory_order __failure)
+  _Tp* __a, _Up* __expected, _Up __desired, memory_order __success, memory_order __failure)
 {
   return __atomic_compare_exchange(
-    &__atomic_auto_align<_Tp>(__a),
-    &__atomic_auto_align<__remove_cv_t<_Tp>>(__expected),
-    &__atomic_auto_align<__remove_cv_t<_Tp>>(&__value),
+    __atomic_force_align_host(__a),
+    __expected,
+    &__desired,
     true,
     __atomic_order_to_int(__success),
     __atomic_failure_order_to_int(__failure));
