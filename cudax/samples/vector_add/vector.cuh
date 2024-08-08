52e6c7ededa052215b73d43db56fc8f476b55642
Eric Niebler <eniebler@nvidia.com>
2024-08-08 02:23:51 +0000

    a working example with vector, in/out, and launch

--- cudax/samples/vector_add/vector.cuh
+++ cudax/samples/vector_add/vector.cuh
@@ -30,154 +30,118 @@
 
 #include <cuda/experimental/__detail/utility.cuh>
 
-#if 1 //_CCCL_STD_VER >= 2017
+#include "param_kind.cuh"
+
+#if _CCCL_STD_VER >= 2017
 namespace cuda::experimental
 {
 using ::cuda::std::span;
 using ::thrust::device_vector;
 using ::thrust::host_vector;
 
-namespace detail
-{
-template <typename _Ty>
-struct __in_box
-{
-  const _Ty& __val;
-};
-
-template <typename _Ty>
-struct __out_box
-{
-  _Ty& __val;
-};
-} // namespace detail
-
 template <typename _Ty>
 class vector
 {
 public:
   vector() = default;
-  explicit vector(size_t n)
-      : __h_(n)
+  explicit vector(size_t __n)
+      : __h_(__n)
   {}
 
-  _Ty& operator[](size_t i) noexcept
+  _Ty& operator[](size_t __i) noexcept
   {
     __dirty_ = true;
-    return __h_[i];
+    return __h_[__i];
   }
 
-  const _Ty& operator[](size_t i) const noexcept
+  const _Ty& operator[](size_t __i) const noexcept
   {
-    return __h_[i];
+    return __h_[__i];
   }
 
 private:
-  enum class __param : unsigned
-  {
-    _in    = 1,
-    _out   = 2,
-    _inout = 3
-  };
-
-  _CCCL_NODISCARD_FRIEND _CCCL_HOST_DEVICE constexpr __param operator&(__param __a, __param __b) noexcept
-  {
-    return __param(unsigned(__a) & unsigned(__b));
-  }
-
-  void sync_host_to_device() const
+  void sync_host_to_device(stream_ref __str, detail::__param_kind __p) const
   {
     if (__dirty_)
     {
-      printf("sync_host_to_device\n");
-      __d_     = __h_;
+      if (__p == detail::__param_kind::_out)
+      {
+        // There's no need to copy the data from host to device if the data is
+        // only going to be written to. We can just allocate the device memory.
+        __d_.resize(__h_.size());
+      }
+      else
+      {
+        // TODO: use a memcpy async here
+        __d_ = __h_;
+      }
       __dirty_ = false;
     }
   }
 
-  void sync_device_to_host()
+  void sync_device_to_host(stream_ref __str, detail::__param_kind __p) const
   {
-    printf("sync_device_to_host\n");
-    __h_ = __d_;
+    if (__p != detail::__param_kind::_in)
+    {
+      // TODO: use a memcpy async here
+      __str.wait(); // wait for the kernel to finish executing
+      __h_ = __d_;
+    }
   }
 
-  template <__param _Param>
-  struct __action : detail::__immovable
+  template <detail::__param_kind _Kind>
+  class __action : private detail::__immovable
   {
-    static constexpr bool __mut = ((_Param & __param::_out) == __param::_out);
-    using __cv_vector           = ::cuda::std::__maybe_const<!__mut, vector>;
+    using __cv_vector = ::cuda::std::__maybe_const<_Kind == detail::__param_kind::_in, vector>;
 
+  public:
     explicit __action(stream_ref __str, __cv_vector& __v) noexcept
         : __str_(__str)
         , __v_(__v)
     {
-      printf("action()\n");
-      if constexpr ((_Param & __param::_in) == __param::_in)
-      {
-        __v_.sync_host_to_device();
-      }
+      __v_.sync_host_to_device(__str_, _Kind);
     }
 
     ~__action()
     {
-      printf("~action()\n");
-      if constexpr ((_Param & __param::_out) == __param::_out)
-      {
-        printf("about to synchronize the stream\n");
-        fflush(stdout);
-        __str_.wait(); // wait for the kernel to finish
-        printf("done synchronizing the stream\n");
-        fflush(stdout);
-        __v_.sync_device_to_host();
-      }
+      __v_.sync_device_to_host(__str_, _Kind);
     }
 
     using __as_kernel_arg = ::cuda::std::span<_Ty>;
 
     operator ::cuda::std::span<_Ty>()
     {
-      printf("to span\n");
       return {__v_.__d_.data().get(), __v_.__d_.size()};
     }
 
-  public:
+  private:
     stream_ref __str_;
     __cv_vector& __v_;
   };
 
-  _CCCL_NODISCARD_FRIEND __action<__param::_inout> __cudax_launch_transform(stream_ref __str, const vector& __v) noexcept
+  _CCCL_NODISCARD_FRIEND __action<detail::__param_kind::_inout>
+  __cudax_launch_transform(stream_ref __str, vector& __v) noexcept
   {
-    return __action<__param::_inout>{__str, __v};
+    return __action<detail::__param_kind::_inout>{__str, __v};
   }
 
-  _CCCL_NODISCARD_FRIEND __action<__param::_in>
-  __cudax_launch_transform(stream_ref __str, detail::__in_box<vector> __b) noexcept
+  _CCCL_NODISCARD_FRIEND __action<detail::__param_kind::_in>
+  __cudax_launch_transform(stream_ref __str, const vector& __v) noexcept
   {
-    return __action<__param::_in>{__str, __b.__val};
+    return __action<detail::__param_kind::_in>{__str, __v};
   }
 
-  _CCCL_NODISCARD_FRIEND __action<__param::_out>
-  __cudax_launch_transform(stream_ref __str, detail::__out_box<vector> __b) noexcept
+  template <detail::__param_kind _Kind>
+  _CCCL_NODISCARD_FRIEND __action<_Kind>
+  __cudax_launch_transform(stream_ref __str, detail::__box<vector, _Kind> __b) noexcept
   {
-    return __action<__param::_out>{__str, __b.__val};
+    return __action<_Kind>{__str, __b.__val};
   }
 
-  host_vector<_Ty> __h_;
+  mutable host_vector<_Ty> __h_;
   mutable device_vector<_Ty> __d_{};
   mutable bool __dirty_ = true;
 };
-
-template <class _Ty>
-detail::__in_box<_Ty> in(const _Ty& __v) noexcept
-{
-  return {__v};
-}
-
-template <class _Ty>
-detail::__out_box<_Ty> out(_Ty& __v) noexcept
-{
-  return {__v};
-}
 
 } // namespace cuda::experimental
 
