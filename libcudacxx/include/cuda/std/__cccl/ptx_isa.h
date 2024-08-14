3cc3f85fe78e9deec3b63ae70b17bfee56b21f21
Michael Schellenberger Costa <miscco@nvidia.com>
2024-08-14 11:25:43 +0200

    Fix typo

--- libcudacxx/include/cuda/std/__cccl/ptx_isa.h
+++ libcudacxx/include/cuda/std/__cccl/ptx_isa.h
@@ -36,43 +36,43 @@
 // We make sure that these get the highest known PTX ISA version.
 #if (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR > 12))
 #  define __cccl_ptx_isa 830ULL
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 3))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 3))
 #  define __cccl_ptx_isa 830ULL
 // PTX ISA 8.2 is available from CUDA 12.2, driver r535
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 2))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 2))
 #  define __cccl_ptx_isa 820ULL
 // PTX ISA 8.1 is available from CUDA 12.1, driver r530
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 1))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 1))
 #  define __cccl_ptx_isa 810ULL
 // PTX ISA 8.0 is available from CUDA 12.0, driver r525
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 0))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 12 && _CCCL_CUDACC_VER_MINOR >= 0))
 #  define __cccl_ptx_isa 800ULL
 // PTX ISA 7.8 is available from CUDA 11.8, driver r520
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 8))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 8))
 #  define __cccl_ptx_isa 780ULL
 // PTX ISA 7.7 is available from CUDA 11.7, driver r515
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 7))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 7))
 #  define __cccl_ptx_isa 770ULL
 // PTX ISA 7.6 is available from CUDA 11.6, driver r510
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 6))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 6))
 #  define __cccl_ptx_isa 760ULL
 // PTX ISA 7.5 is available from CUDA 11.5, driver r495
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 5))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 5))
 #  define __cccl_ptx_isa 750ULL
 // PTX ISA 7.4 is available from CUDA 11.4, driver r470
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 4))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 4))
 #  define __cccl_ptx_isa 740ULL
 // PTX ISA 7.3 is available from CUDA 11.3, driver r465
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 3))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 3))
 #  define __cccl_ptx_isa 730ULL
 // PTX ISA 7.2 is available from CUDA 11.2, driver r460
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 2))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 2))
 #  define __cccl_ptx_isa 720ULL
 // PTX ISA 7.1 is available from CUDA 11.1, driver r455
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 1))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 1))
 #  define __cccl_ptx_isa 710ULL
 // PTX ISA 7.0 is available from CUDA 11.0, driver r445
-#elif (_CCCL_CUDACC && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 0))
+#elif (defined(_CCCL_CUDACC) && (_CCCL_CUDACC_VER_MAJOR >= 11 && _CCCL_CUDACC_VER_MINOR >= 0))
 #  define __cccl_ptx_isa 700ULL
 // Fallback case. Define the ISA version to be zero. This ensures that the macro is always defined.
 #else
