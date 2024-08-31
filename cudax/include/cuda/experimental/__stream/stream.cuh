f1e99ed42293e8fe6a98710c4f22601a2f41309c
pciolkosz <pciolkosz@nvidia.com>
2024-08-30 17:24:39 -0700

    Branch out experimental version of stream_ref

--- cudax/include/cuda/experimental/__stream/stream.cuh
+++ cudax/include/cuda/experimental/__stream/stream.cuh
@@ -23,10 +23,9 @@
 #endif // no system header
 
 #include <cuda/std/__cuda/api_wrapper.h>
-#include <cuda/stream_ref>
 
 #include <cuda/experimental/__device/device_ref.cuh>
-#include <cuda/experimental/__event/timed_event.cuh>
+#include <cuda/experimental/__stream/stream_ref.cuh>
 #include <cuda/experimental/__utility/ensure_current_device.cuh>
 
 namespace cuda::experimental
@@ -110,69 +109,7 @@ struct stream : stream_ref
 
   stream& operator=(const stream&) = delete;
 
-  // Ideally records and waits below would be in stream_ref, but we can't have it depend on cudax yet
-
-  //! @brief Create a new event and record it into this stream
-  //!
-  //! @return A new event that was recorded into this stream
-  //!
-  //! @throws cuda_error if event creation or record failed
-  _CCCL_NODISCARD event record_event(event::flags __flags = event::flags::none) const
-  {
-    return event(*this, __flags);
-  }
-
-  //! @brief Create a new timed event and record it into this stream
-  //!
-  //! @return A new timed event that was recorded into this stream
-  //!
-  //! @throws cuda_error if event creation or record failed
-  _CCCL_NODISCARD timed_event record_timed_event(event::flags __flags = event::flags::none) const
-  {
-    return timed_event(*this, __flags);
-  }
-
   using stream_ref::wait;
-
-  //! @brief Make all future work submitted into this stream depend on completion of the specified event
-  //!
-  //! @param __ev Event that this stream should wait for
-  //!
-  //! @throws cuda_error if inserting the dependency fails
-  void wait(event_ref __ev) const
-  {
-    assert(__ev.get() != nullptr);
-    // Need to use driver API, cudaStreamWaitEvent would push dev 0 if stack was empty
-    detail::driver::streamWaitEvent(get(), __ev.get());
-  }
-
-  //! @brief Make all future work submitted into this stream depend on completion of all work from the specified
-  //! stream
-  //!
-  //! @param __other Stream that this stream should wait for
-  //!
-  //! @throws cuda_error if inserting the dependency fails
-  void wait(stream_ref __other) const
-  {
-    // TODO consider an optimization to not create an event every time and instead have one persistent event or one
-    // per stream
-    assert(__stream != detail::invalid_stream);
-    event __tmp(__other);
-    wait(__tmp);
-  }
-
-  //! @brief Get device under which this stream was created.
-  //!
-  //! @throws cuda_error if device check fails
-  device_ref device() const
-  {
-    // Because the stream can come from_native_handle, we can't just loop over devices comparing contexts,
-    // lower to CUDART for this instead
-    __ensure_current_device __dev_setter(*this);
-    int result;
-    _CCCL_TRY_CUDA_API(cudaGetDevice, "Could not get device from a stream", &result);
-    return result;
-  }
 
   //! @brief Construct an `stream` object from a native `cudaStream_t` handle.
   //!
