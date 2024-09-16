a117b452312c6da8385727c52694c79431d4e274
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2024-09-16 11:43:05 -0700

    fix feature macro

--- cub/cub/device/dispatch/dispatch_select_if.cuh
+++ cub/cub/device/dispatch/dispatch_select_if.cuh
@@ -187,11 +187,12 @@ struct agent_select_if_wrapper_t
 /******************************************************************************
  * Kernel entry points
  *****************************************************************************/
-// TODO (elstehle) gird-private constants were introduced in CTK 11.7. The macro is temporarily placed here, do we want to make this a CCCL macro?
-#if _CCCL_CUDACC_BELOW_11_7
-#define _CUB_GRID_CONSTANT 
+// TODO (elstehle) gird-private constants were introduced in CTK 11.7. The macro is temporarily placed here, do we want
+// to make this a CCCL macro?
+#if defined(_CCCL_CUDACC_BELOW_11_7)
+#  define _CUB_GRID_CONSTANT
 #else
-#define _CUB_GRID_CONSTANT __grid_constant__
+#  define _CUB_GRID_CONSTANT __grid_constant__
 #endif
 
 /**
