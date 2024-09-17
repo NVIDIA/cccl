036ab7fae7177b90d0526651c901027fa1cfde81
Michael Schellenberger Costa <miscco@nvidia.com>
2024-09-17 09:18:52 +0200

    Rename function to `change_stream` and adopt review comments

--- cudax/include/cuda/experimental/__container/uninitialized_async_buffer.cuh
+++ cudax/include/cuda/experimental/__container/uninitialized_async_buffer.cuh
@@ -220,15 +220,12 @@ public:
   }
 
   //! @brief Replaces the stored stream
-  //! @param __stream the new stream
+  //! @param __new_stream the new stream
   //! @note Always synchronizes with the old stream
-  constexpr void set_stream(::cuda::stream_ref __stream)
+  constexpr void change_stream(::cuda::stream_ref __new_stream)
   {
-    if (__stream != __stream_)
-    {
-      __stream_.wait();
-      __stream_ = __stream;
-    }
+    __stream_.wait(__new_stream);
+    __stream_ = __new_stream;
   }
 
   //! @brief Swaps the contents with those of another \c uninitialized_async_buffer
