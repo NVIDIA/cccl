7878b66e4840c4dd7cbf28a4de0e77a11028ef75
Michael Schellenberger Costa <miscco@nvidia.com>
2024-09-20 13:15:15 +0200

    Move `__is_local_smem_barrier` to its own file

--- libcudacxx/include/cuda/std/__cuda/barrier.h
+++ libcudacxx/include/cuda/std/__cuda/barrier.h
@@ -34,6 +34,7 @@
 #include <cuda/__barrier/barrier_thread_scope_block.h>
 #include <cuda/__barrier/barrier_thread_scope_thread.h>
 #include <cuda/__barrier/completion_mechanism.h>
+#include <cuda/__barrier/is_local_smem_barrier.h>
 #include <cuda/__fwd/pipeline.h>
 #include <cuda/__memcpy_async/memcpy_async_tx.h>
 #include <cuda/std/__atomic/api/owned.h>
@@ -57,16 +58,6 @@ template <typename _Ty>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool __unused(_Ty&)
 {
   return true;
-}
-
-// __is_local_smem_barrier returns true if barrier is (1) block-scoped and (2) located in shared memory.
-template <thread_scope _Sco,
-          typename _CompF,
-          bool _Is_mbarrier = (_Sco == thread_scope_block)
-                           && _CUDA_VSTD::is_same<_CompF, _CUDA_VSTD::__empty_completion>::value>
-_LIBCUDACXX_HIDE_FROM_ABI bool __is_local_smem_barrier(barrier<_Sco, _CompF>& __barrier)
-{
-  NV_IF_ELSE_TARGET(NV_IS_DEVICE, (return _Is_mbarrier && __isShared(&__barrier);), (return false;));
 }
 
 // __try_get_barrier_handle returns barrier handle of block-scoped barriers and a nullptr otherwise.
