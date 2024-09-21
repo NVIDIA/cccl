28a9e801ccbd9fe1e0e409cc05d0a6b4d0b3d453
gonidelis <ggonidelis@nvidia.com>
2024-09-20 17:13:25 -0700

    Separate benchmarks into different files

--- thrust/benchmarks/bench/count_if/basic.cu
+++ thrust/benchmarks/bench/count_if/basic.cu
@@ -25,12 +25,10 @@
  *
  ******************************************************************************/
 
-#include <cub/device/device_find_if.cuh>
-
 #include <thrust/count.h>
-#include <thrust/find.h>
+#include <thrust/fill.h>
 
-#include <nvbench_helper.cuh>
+#include "nvbench_helper.cuh"
 
 template <typename T>
 struct equals
@@ -47,46 +45,27 @@ struct equals
 };
 
 template <typename T>
-void find_if(nvbench::state& state, nvbench::type_list<T>)
+void count_if(nvbench::state& state, nvbench::type_list<T>)
 {
   T val = 1;
   // set up input
-  const auto elements       = state.get_int64("Elements");
+  const auto elements       = static_cast<std::size_t>(state.get_int64("Elements"));
   const auto common_prefix  = state.get_float64("MismatchAt");
   const auto mismatch_point = elements * common_prefix;
 
   thrust::device_vector<T> dinput(elements, 0);
   thrust::fill(dinput.begin() + mismatch_point, dinput.end(), val);
-  thrust::device_vector<T> d_result(1);
   ///
 
-  void* d_temp_storage = nullptr;
-  size_t temp_storage_bytes{};
+  caching_allocator_t alloc;
+  thrust::count_if(policy(alloc), dinput.begin(), dinput.end(), equals<T>{val});
 
-  cub::DeviceFind::FindIf(
-    d_temp_storage,
-    temp_storage_bytes,
-    thrust::raw_pointer_cast(dinput.data()),
-    thrust::raw_pointer_cast(d_result.data()),
-    equals{val},
-    dinput.size(),
-    0);
-
-  thrust::device_vector<uint8_t> temp_storage(temp_storage_bytes);
-  d_temp_storage = thrust::raw_pointer_cast(temp_storage.data());
-
-  state.exec(nvbench::exec_tag::no_batch, [&](nvbench::launch& launch) {
-    cub::DeviceFind::FindIf(
-      d_temp_storage,
-      temp_storage_bytes,
-      thrust::raw_pointer_cast(dinput.data()),
-      thrust::raw_pointer_cast(d_result.data()),
-      equals{val},
-      dinput.size(),
-      launch.get_stream());
+  state.exec(nvbench::exec_tag::no_batch | nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+    thrust::count_if(policy(alloc, launch), dinput.begin(), dinput.end(), equals<T>{val});
   });
 }
 
-NVBENCH_BENCH_TYPES(find_if, NVBENCH_TYPE_AXES(fundamental_types))
+NVBENCH_BENCH_TYPES(count_if, NVBENCH_TYPE_AXES(fundamental_types))
+  .set_name("thrust::count_if")
   .add_int64_power_of_two_axis("Elements", nvbench::range(16, 28, 4))
   .add_float64_axis("MismatchAt", std::vector{1.0, 0.5, 0.0});
