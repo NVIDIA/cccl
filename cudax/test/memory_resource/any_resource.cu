0e28ed7cc2f3e182ecbe61d1b52f3b0877523442
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-09-24 23:12:42 +0000

    Revert changes in all .cuh, .cu, .h, .cpp files.
    Rationale: These files actually have a C++-specific license. It's left for later to change the license and the notices together.

--- cudax/test/memory_resource/any_resource.cu
+++ cudax/test/memory_resource/any_resource.cu
@@ -1,6 +1,6 @@
 //===----------------------------------------------------------------------===//
 //
-// Part of CUDA Experimental in CUDA Core Compute Libraries,
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
 // under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
