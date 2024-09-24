6fd1e5c6cb6da7468f57ecc1701e5ae80ee3f170
pciolkosz <pciolkosz@nvidia.com>
2024-09-23 23:35:09 -0700

    [CUDAX] Add placeholder green context type and logical device that can hold both a green ctx and a device (#2446)

--- cudax/include/cuda/experimental/__utility/ensure_current_device.cuh
+++ cudax/include/cuda/experimental/__utility/ensure_current_device.cuh
@@ -24,12 +24,16 @@
 #include <cuda/stream_ref>
 
 #include <cuda/experimental/__device/all_devices.cuh>
+#include <cuda/experimental/__device/logical_device.cuh>
 #include <cuda/experimental/__utility/driver_api.cuh>
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS // Do not document
 
 namespace cuda::experimental
 {
+//! TODO we might want to change the comments to indicate it operates on contexts for certains differences
+//! with green context, but it depends on how exactly green context internals end up being
+
 //! @brief RAII helper which on construction sets the current device to the specified one or one a
 //! stream was created under. It sets the state back on destruction.
 //!
@@ -41,10 +45,31 @@ struct [[maybe_unused]] __ensure_current_device
   //! @param new_device The device to switch to
   //!
   //! @throws cuda_error if the device switch fails
-  explicit __ensure_current_device(device_ref new_device)
+  explicit __ensure_current_device(device_ref __new_device)
   {
-    auto ctx = devices[new_device.get()].primary_context();
-    detail::driver::ctxPush(ctx);
+    auto __ctx = devices[__new_device.get()].primary_context();
+    detail::driver::ctxPush(__ctx);
+  }
+
+  //! @brief Construct a new `__ensure_current_device` object and switch to the specified
+  //!        device.
+  //!
+  //! Note: if this logical device contains a green_context the device under which the green
+  //! context was created will be set to current
+  //!
+  //! @param new_device The device to switch to
+  //!
+  //! @throws cuda_error if the device switch fails
+  explicit __ensure_current_device(logical_device __new_device)
+  {
+    detail::driver::ctxPush(__new_device.context());
+  }
+
+  // Doesn't really fit into the type description, we might consider changing it once
+  // green ctx design is more finalized
+  explicit __ensure_current_device(CUcontext __ctx)
+  {
+    detail::driver::ctxPush(__ctx);
   }
 
   //! @brief Construct a new `__ensure_current_device` object and switch to the device
@@ -53,10 +78,10 @@ struct [[maybe_unused]] __ensure_current_device
   //! @param stream Stream indicating the device to switch to
   //!
   //! @throws cuda_error if the device switch fails
-  explicit __ensure_current_device(stream_ref stream)
+  explicit __ensure_current_device(stream_ref __stream)
   {
-    auto ctx = detail::driver::streamGetCtx(stream.get());
-    detail::driver::ctxPush(ctx);
+    auto __ctx = detail::driver::streamGetCtx(__stream.get());
+    detail::driver::ctxPush(__ctx);
   }
 
   __ensure_current_device(__ensure_current_device&&)                 = delete;
