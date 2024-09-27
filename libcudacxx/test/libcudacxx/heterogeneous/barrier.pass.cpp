90bdfd42318ea44524d29ab8b2a183150104ccf7
Michael Schellenberger Costa <miscco@nvidia.com>
2024-09-27 12:41:13 +0000

    Disable 2 more test

--- libcudacxx/test/libcudacxx/heterogeneous/barrier.pass.cpp
+++ libcudacxx/test/libcudacxx/heterogeneous/barrier.pass.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: nvrtc, pre-sm-70
-// XFAIL: clang && (!nvcc)
+// UNSUPPORTED: clang && (!nvcc)
 
 // uncomment for a really verbose output detailing what test steps are being launched
 #define DEBUG_TESTERS
