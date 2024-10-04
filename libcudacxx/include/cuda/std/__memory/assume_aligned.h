e63b515d8c0ee619e54d41c768402864ffb7e781
Michael Schellenberger Costa <miscco@nvidia.com>
2024-10-04 08:53:36 +0000

    Make it a bit cleaner

--- libcudacxx/include/cuda/std/__memory/assume_aligned.h
+++ libcudacxx/include/cuda/std/__memory/assume_aligned.h
@@ -33,16 +33,17 @@ template <size_t _Align, class _Tp>
 _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI _CCCL_CONSTEXPR_CXX14 _Tp* assume_aligned(_Tp* __ptr) noexcept
 {
   static_assert(_CUDA_VSTD::has_single_bit(_Align), "std::assume_aligned requires the alignment to be a power of 2!");
-  if (!_CUDA_VSTD::__libcpp_is_constant_evaluated())
+#if defined(_CCCL_BUILTIN_IS_CONSTANT_EVALUATED)
+  if (!_CCCL_BUILTIN_IS_CONSTANT_EVALUATED())
   {
-// old gcc cannot handle the cast to uintptr_t and MSVC checks within the builtin
-#if (!defined(_CCCL_COMPILER_GCC) || _CCCL_GCC_VERSION >= 90000) && !defined(_CCCL_COMPILER_MSVC)
+#  if !defined(_CCCL_COMPILER_MSVC) // MSVC checks within the builtin
     _CCCL_ASSERT(reinterpret_cast<uintptr_t>(__ptr) % _Align == 0, "Alignment assumption is violated");
-#endif // gcc >= 9
-#if defined(_CCCL_BUILTIN_ASSUME_ALIGNED) && defined(_CCCL_BUILTIN_IS_CONSTANT_EVALUATED)
+#  endif // !_CCCL_COMPILER_MSVC
+#  if defined(_CCCL_BUILTIN_ASSUME_ALIGNED)
     return static_cast<_Tp*>(__builtin_assume_aligned(__ptr, _Align));
-#endif // _CCCL_BUILTIN_ASSUME_ALIGNED && _CCCL_BUILTIN_IS_CONSTANT_EVALUATED
+#  endif // _CCCL_BUILTIN_ASSUME_ALIGNED
   }
+#endif // _CCCL_BUILTIN_IS_CONSTANT_EVALUATED
   return __ptr;
 }
 
