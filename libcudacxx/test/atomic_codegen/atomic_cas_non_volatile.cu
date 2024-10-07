0479ebdab23e8ddb93a7ffbb68db6344fa82a74e
Wesley Maxey <wesley.maxey@gmail.com>
2024-10-07 21:17:27 +0000

    Atomic codegen tests: clang-cuda reorders some arguments, include them in the DAG to fix CI failures

--- libcudacxx/test/atomic_codegen/atomic_cas_non_volatile.cu
+++ libcudacxx/test/atomic_codegen/atomic_cas_non_volatile.cu
@@ -14,8 +14,8 @@ __global__ void cas_device_relaxed_non_volatile(int* data, int* out, int n)
 ; SM8X-DAG:  ld.param.u64 %rd[[#ATOM:]], {{.*}}[[FUNCTION]]_param_0{{.*}}
 ; SM8X-DAG:  ld.param.u64 %rd[[#EXPECTED:]], {{.*}}[[FUNCTION]]_param_1{{.*}}
 ; SM8X-DAG:  ld.param.u32 %r[[#INPUT:]], {{.*}}[[FUNCTION]]_param_2{{.*}}
-; SM8X-NEXT: cvta.to.global.u64 %rd[[#GOUT:]], %rd[[#EXPECTED]];
-; SM8X-NEXT: ld.global.u32 %r[[#LOCALEXP:]], [%rd[[#INPUT]]];
+; SM8X-DAG:  cvta.to.global.u64 %rd[[#GOUT:]], %rd[[#EXPECTED]];
+; SM8X-DAG:  ld.global.u32 %r[[#LOCALEXP:]], [%rd[[#INPUT]]];
 ; SM8X-NEXT: {{/*[[:space:]] *}}atom.cas.relaxed.gpu.b32 %r[[#DEST:]],[%rd[[#ATOM]]],%r[[#LOCALEXP]],%r[[#INPUT]];{{[[:space:]]/*}}
 ; SM8X-NEXT: st.global.u32 [%rd[[#GOUT]]], %r[[#DEST]];
 ; SM8X-NEXT: ret;
--- libcudacxx/test/atomic_codegen/atomic_sub_non_volatile.cu
+++ libcudacxx/test/atomic_codegen/atomic_sub_non_volatile.cu
@@ -13,7 +13,7 @@ __global__ void sub_relaxed_device_non_volatile(int* data, int* out, int n)
 ; SM8X-DAG:  ld.param.u64 %rd[[#ATOM:]], {{.*}}[[FUNCTION]]_param_0{{.*}}
 ; SM8X-DAG:  ld.param.u64 %rd[[#RESULT:]], {{.*}}[[FUNCTION]]_param_1{{.*}}
 ; SM8X-DAG:  ld.param.u32 %r[[#INPUT:]], {{.*}}[[FUNCTION]]_param_2{{.*}}
-; SM8X-NEXT: cvta.to.global.u64 %rd[[#GOUT:]], %rd[[#RESULT]];
+; SM8X-DAG:  cvta.to.global.u64 %rd[[#GOUT:]], %rd[[#RESULT]];
 ; SM8X-NEXT: neg.s32 %r[[#NEG:]], %r[[#INPUT]];
 ; SM8X-NEXT: {{/*[[:space:]] *}}atom.add.relaxed.gpu.s32 %r[[#DEST:]],[%rd[[#ATOM]]],%r[[#NEG]];{{[[:space:]]/*}}
 ; SM8X-NEXT: st.global.u32 [%rd[[#GOUT]]], %r[[#DEST]];
