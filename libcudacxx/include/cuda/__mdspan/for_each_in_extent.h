e99529051ae6d4ca4e7f5b285198126343a784fa
fbusato <fbusato@nvidia.com>
2024-10-09 23:57:24 +0000

    working version

--- libcudacxx/include/cuda/__mdspan/for_each_in_extent.h
+++ libcudacxx/include/cuda/__mdspan/for_each_in_extent.h
@@ -8,11 +8,15 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _CUDA___CMATH_FAST_DIV_MOD
-#define _CUDA___CMATH_FAST_DIV_MOD
+#ifndef _CUDA__MDSPAN_FOR_EACH_IN_EXTENT_H
+#define _CUDA__MDSPAN_FOR_EACH_IN_EXTENT_H
 
 #include <cuda/std/detail/__config>
 
+#include <cassert>
+#include <iostream>
+
+#include "cuda/std/__bit/has_single_bit.h"
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
@@ -23,10 +27,14 @@
 
 #include <cuda/__cmath/ceil_div.h>
 #include <cuda/std/__bit/integral.h>
+#include <cuda/std/__mdspan/extents.h>
 #include <cuda/std/cstdint>
-#include <cuda/std/mdspan>
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
+
+/***********************************************************************************************************************
+ * Extract higher bits after multiplication
+ **********************************************************************************************************************/
 
 _CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE constexpr unsigned
 __multiply_extract_higher_bits(unsigned __dividend, unsigned __multiplier) noexcept
@@ -34,10 +42,14 @@ __multiply_extract_higher_bits(unsigned __dividend, unsigned __multiplier) noexc
   // clang-format off
   NV_IF_ELSE_TARGET(NV_IS_DEVICE,
     (return __umulhi(__dividend, __multiplier);),
-    (return static_cast<unsigned>((static_cast<_CUDA_VSTD::uint64_t>(__dividend) * __multiplier) >> 32u);)
+    (return static_cast<unsigned>((static_cast<::cuda::std::uint64_t>(__dividend) * __multiplier) >> 32u);)
   )
   // clang-format on
 }
+
+/***********************************************************************************************************************
+ * Fast Modulo/Division based on Precomputation
+ **********************************************************************************************************************/
 
 struct fast_div_mod
 {
@@ -52,13 +64,14 @@ struct fast_div_mod
   _CCCL_NODISCARD _CCCL_HOST_DEVICE explicit fast_div_mod(unsigned divisor) noexcept
       : __divisor{divisor}
   {
+    assert(divisor > 0);
     if (divisor == 1)
     {
       return;
     }
-    auto num_bits = _CUDA_VSTD::bit_width(divisor);
-    __multiplier  = static_cast<unsigned>(::cuda::ceil_div(int64_t{1} << (num_bits + 31), divisor));
-    __shift_right = num_bits - 1;
+    auto num_bits = ::cuda::std::bit_width(divisor) + !::cuda::std::has_single_bit(divisor);
+    __multiplier  = static_cast<unsigned>(::cuda::ceil_div(int64_t{1} << (num_bits + 30), divisor));
+    __shift_right = num_bits - 2;
   }
 
   fast_div_mod(const fast_div_mod&) noexcept = default;
@@ -67,29 +80,50 @@ struct fast_div_mod
 
   _CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE result operator()(unsigned dividend) const noexcept
   {
-    auto quotient =
-      (__divisor != 1) ? __multiply_extract_higher_bits(dividend, __multiplier) >> __shift_right : dividend;
+    if (__divisor == 1)
+    {
+      return result{dividend, 0};
+    }
+    auto quotient  = __multiply_extract_higher_bits(dividend, __multiplier) >> __shift_right;
     auto remainder = dividend - (quotient * __divisor);
+    assert(remainder >= 0 && remainder < __divisor);
     return result{quotient, remainder};
   }
 
+  _CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE friend unsigned
+  operator/(unsigned dividend, fast_div_mod __div) noexcept
+  {
+    return __div(dividend).quotient;
+  }
+
+  _CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE friend unsigned
+  operator%(unsigned dividend, fast_div_mod __div) noexcept
+  {
+    return __div(dividend).remainder;
+  }
+
+  unsigned __divisor = 1;
+
 private:
-  unsigned __divisor     = 1;
   unsigned __multiplier  = 0;
   unsigned __shift_right = 0;
 };
 
 /***********************************************************************************************************************
- * sub_size
+ * Utilities
  **********************************************************************************************************************/
 
-template <_CUDA_VSTD::size_t _Rank, typename _IndexType, _CUDA_VSTD::size_t... _Extents, _CUDA_VSTD::size_t... _Indices>
-_CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE constexpr _CUDA_VSTD::size_t
-sub_size(const std::extents<_IndexType, _Extents...>& __ext, _CUDA_VSTD::index_sequence<_Indices...> = {})
+template <::cuda::std::size_t _Rank, typename _IndexType, ::cuda::std::size_t... _Extents, ::cuda::std::size_t... _Indices>
+_CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE constexpr ::cuda::std::size_t
+sub_size(const ::cuda::std::extents<_IndexType, _Extents...>& __ext, ::cuda::std::index_sequence<_Indices...> = {})
 {
-  if constexpr (sizeof...(_Extents) == 0)
+  if constexpr (_Rank >= __ext.rank())
   {
-    return sub_size<_Rank>(__ext, _CUDA_VSTD::make_index_sequence<sizeof...(_Extents) - _Rank>{});
+    return _IndexType{1};
+  }
+  else if constexpr (sizeof...(_Indices) == 0)
+  {
+    return sub_size<_Rank>(__ext, ::cuda::std::make_index_sequence<sizeof...(_Extents) - _Rank>{});
   }
   else
   {
@@ -97,42 +131,59 @@ sub_size(const std::extents<_IndexType, _Extents...>& __ext, _CUDA_VSTD::index_s
   }
 }
 
+template <typename _IndexType, ::cuda::std::size_t... _E, ::cuda::std::size_t... _Ranks>
+_CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE auto sub_sizes_fast_div_mod(
+  const ::cuda::std::extents<_IndexType, _E...>& __ext, ::cuda::std::index_sequence<_Ranks...> = {})
+{
+  return ::cuda::std::array{fast_div_mod{sub_size<_Ranks + 1>(__ext)}...};
+}
+
+template <typename _IndexType, ::cuda::std::size_t... _E, ::cuda::std::size_t... _Ranks>
+_CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE auto
+extends_fast_div_mod(const ::cuda::std::extents<_IndexType, _E...>& __ext, ::cuda::std::index_sequence<_Ranks...> = {})
+{
+  return ::cuda::std::array{fast_div_mod{__ext.extent(_Ranks)}...};
+}
+
 /***********************************************************************************************************************
  * for_each_in_extent
  **********************************************************************************************************************/
 
-template <typename _IndexType,
-          std::size_t... _Extents,
-          typename Func,
-          std::size_t FirstExtInd,
-          std::size_t... RestExtInds,
-          typename... Is>
-void __for_each_in_extent_impl(
-  _IndexType thread_id,
-  fast_div_mod (&fast_div_mod_array)[sizeof...(_Extents)],
-  const std::extents<_IndexType, _Extents...>& ext,
-  Func&& func,
-  std::index_sequence<FirstExtInd, RestExtInds...>,
-  Is... is)
+template <int Rank, typename T>
+_CCCL_NODISCARD _CCCL_HOST_DEVICE _CCCL_FORCEINLINE//
+T coordinate_at(T index, fast_div_mod sub_size, fast_div_mod extent)
 {
-  if constexpr (sizeof...(RestExtInds) > 0)
-  {
-    constexpr auto __rank                    = sizeof...(RestExtInds);
-    auto [thread_group_id, thread_group_rem] = fast_div_mod_array[__rank](thread_id);
-    for_each_in_extent(ext, std::forward<Func>(func), std::index_sequence<RestExtInds...>{}, is..., thread_group_rem);
-  }
-  else
-  {
-    std::forward<Func>(func)(is..., thread_id);
-  }
+  printf("T: %d, [q: %d, r: %d, d: %d] div: %d, coord: %d\n",
+         threadIdx.x,
+         sub_size(index).quotient,
+         sub_size(index).remainder,
+         sub_size.__divisor,
+         index / sub_size,
+         (index / sub_size) % extent);
+  return (index / sub_size) % extent;
 }
 
-template <class _IndexType, std::size_t... _Extents, typename Func>
-void for_each_in_extent(stdex::extents<_IndexType, _Extents...> ext, Func&& func)
+template <typename ExtentType, typename Func, typename FastDivModArrayType, ::cuda::std::size_t... Ranks>
+__global__ void __for_each_in_extent_impl(
+  __grid_constant__ const ExtentType ext,
+  __grid_constant__ const Func func,
+  __grid_constant__ const FastDivModArrayType sub_sizes_div_array,
+  __grid_constant__ const FastDivModArrayType extends_div_array,
+  ::cuda::std::index_sequence<Ranks...> = {})
 {
-  for_each_in_extent(ext, std::forward<Func>(func), std::make_index_sequence<sizeof...(_Extents)>{});
+  auto id = threadIdx.x + blockIdx.x * blockDim.x;
+  func(coordinate_at<Ranks>(id, sub_sizes_div_array[Ranks], extends_div_array[Ranks])...);
+}
+
+template <class _IndexType, ::cuda::std::size_t... _Extents, typename Func>
+void for_each_in_extent(const ::cuda::std::extents<_IndexType, _Extents...>& ext, const Func& func)
+{
+  constexpr auto seq                     = ::cuda::std::make_index_sequence<sizeof...(_Extents)>{};
+  ::cuda::std::array sub_sizes_div_array = sub_sizes_fast_div_mod(ext, seq);
+  ::cuda::std::array extends_div_array   = extends_fast_div_mod(ext, seq);
+  __for_each_in_extent_impl<<<1, sub_size<0>(ext)>>>(ext, func, sub_sizes_div_array, extends_div_array, seq);
 }
 
 _LIBCUDACXX_END_NAMESPACE_CUDA
 
-#endif // _CUDA___CMATH_FAST_DIV_MOD
+#endif // _CUDA__MDSPAN_FOR_EACH_IN_EXTENT_H
