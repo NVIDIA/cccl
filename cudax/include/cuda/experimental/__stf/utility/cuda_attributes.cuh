750db8051fccc1a9d6526c2c05d6ba714f4465ac
Allison Piper <alliepiper16@gmail.com>
2024-10-09 18:04:27 +0000

    Limit `no_device_stack` pragma to NVHPC.

--- cudax/include/cuda/experimental/__stf/utility/cuda_attributes.cuh
+++ cudax/include/cuda/experimental/__stf/utility/cuda_attributes.cuh
@@ -14,10 +14,17 @@
 #  define CUDASTF_HOST_DEVICE __host__ __device__
 #  define CUDASTF_DEVICE      __device__
 #  define CUDASTF_HOST        __host__
+#  define CUDASTF_NO_DEVICE_STACK
+#elif defined(__CUDACC__) && defined(_NVHPC_CUDA)
+#  define CUDASTF_HOST_DEVICE
+#  define CUDASTF_DEVICE
+#  define CUDASTF_HOST
+#  define CUDASTF_NO_DEVICE_STACK _Pragma("diag_suppress no_device_stack")
 #else
 #  define CUDASTF_HOST_DEVICE
 #  define CUDASTF_DEVICE
 #  define CUDASTF_HOST
+#  define CUDASTF_NO_DEVICE_STACK
 #endif
 
 #if __CUDA_ARCH__ >= 700
