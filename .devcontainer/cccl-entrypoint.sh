92af28ecc3877a6c162fcbb31567d518606064ae
Cedric Augonnet <caugonnet@nvidia.com>
2024-10-11 13:22:57 +0200

    Revert change commited by mistake

--- .devcontainer/cccl-entrypoint.sh
+++ .devcontainer/cccl-entrypoint.sh
@@ -8,10 +8,7 @@ devcontainer-utils-post-create-command;
 devcontainer-utils-init-git;
 devcontainer-utils-post-attach-command;
 
-# cd /home/coder/cccl/
-echo "CLANG .."
-clang-format -i cudax/include/cuda/experimental/__stf/graph/graph_ctx.cuh
-clang-format --version
+cd /home/coder/cccl/
 
 if test $# -gt 0; then
     exec "$@";
