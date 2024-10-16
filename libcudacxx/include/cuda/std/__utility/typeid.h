01440a8079267f74ef606a2773e7d016bf5081d0
Eric Niebler <eniebler@nvidia.com>
2024-10-16 01:16:55 +0000

    use a more robust strategy for parsing the `__PRETTY_FUNCTION__` string

--- libcudacxx/include/cuda/std/__utility/typeid.h
+++ libcudacxx/include/cuda/std/__utility/typeid.h
@@ -26,6 +26,7 @@
 #ifndef _LIBCUDACXX_HAS_NO_SPACESHIP_OPERATOR
 #  include <cuda/std/compare>
 #endif
+#include <cuda/std/__utility/integer_sequence.h>
 #include <cuda/std/cstddef>
 
 #ifndef _CCCL_NO_TYPEID
@@ -41,18 +42,16 @@ _LIBCUDACXX_END_NAMESPACE_STD
 
 #else // ^^^ !_CCCL_NO_TYPEID ^^^ / vvv _CCCL_NO_TYPEID
 
-// We use an unversioned namespace here so that the versioning namespace doesn't
-// appear in the __PRETTY_FUNCTION__ strings used below.
-_LIBCUDACXX_BEGIN_NAMESPACE_STD_NOVERSION
+_LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 // TODO: replace this with `cuda::std::string_view` when available.
 struct __string_view
 {
-  template <size_t N>
+  template <size_t _Np>
   _LIBCUDACXX_HIDE_FROM_ABI constexpr explicit __string_view(
-    char const (&str)[N], size_t prefix = 0, size_t suffix = 0) noexcept
-      : __str_{str + prefix}
-      , __len_{N - 1 - prefix - suffix}
+    char const (&str)[_Np], size_t __prefix = 0, size_t __suffix = 0) noexcept
+      : __str_{str + __prefix}
+      , __len_{_Np - 1 - __prefix - __suffix}
   {}
 
   _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr size_t size() const noexcept
@@ -80,6 +79,12 @@ struct __string_view
     return __str_[__n];
   }
 
+  _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view
+  substr(ptrdiff_t __start, ptrdiff_t __stop) const noexcept
+  {
+    return __string_view(__str_ + __start, __stop - __start);
+  }
+
   // C++11 constexpr string comparison
 #  if _CCCL_STD_VER < 2014 || __cpp_constexpr < 201304L
 
@@ -96,13 +101,81 @@ private:
            : int(__len1) - int(__len2);
   }
 
+  _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI static constexpr ptrdiff_t __try_find(
+    size_t __i,
+    const char* __needle,
+    size_t __needle_size,
+    char const* __haystack_begin,
+    char const* __haystack_end,
+    char const* __it) noexcept
+  {
+    return __i == __needle_size //
+           ? __it - __haystack_begin
+           : __needle[__i] != __it[__i]
+               ? -1
+               : __try_find(__i + 1, __needle, __needle_size, __haystack_begin, __haystack_end, __it);
+  }
+
+  _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI static constexpr ptrdiff_t __find(
+    ptrdiff_t __found,
+    const char* __needle,
+    size_t __needle_size,
+    char const* __haystack_begin,
+    char const* __haystack_end,
+    char const* __it) noexcept
+  {
+    return __found != -1 //
+           ? __found
+           : __it == __haystack_end
+               ? -1
+               : __find(__try_find(0, __needle, __needle_size, __haystack_begin, __haystack_end, __it),
+                        __needle,
+                        __needle_size,
+                        __haystack_begin,
+                        __haystack_end,
+                        __it + 1);
+  }
+
 public:
   _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr int compare(__string_view const& __other) const noexcept
   {
     return __compare(__str_, __len_, __other.__str_, __other.__len_, __min_(__len_, __other.__len_));
   }
 
+  template <size_t _Np>
+  _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr ptrdiff_t find(const char (&__other)[_Np]) const noexcept
+  {
+    return ((_Np - 1) > __len_) ? -1 : __find(-1, __other, _Np - 1, __str_, __str_ + __len_ - (_Np - 1), __str_);
+  }
+
 #  else // ^^^ C++11 ^^^ / vvv C++14 and beyond vvv
+
+  template <size_t _Np>
+  _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr ptrdiff_t find(const char (&__other)[_Np]) const noexcept
+  {
+    if ((_Np - 1) > __len_)
+    {
+      return -1;
+    }
+
+    char const *__it = begin(), *__end = end() - (_Np - 1);
+    for (; __it != __end; ++__it)
+    {
+      size_t __i = 0;
+      for (; __i < (_Np - 1); ++__i)
+      {
+        if (__it[__i] != __other[__i])
+        {
+          break;
+        }
+      }
+      if (__i == (_Np - 1))
+      {
+        return __it - begin();
+      }
+    }
+    return -1;
+  }
 
   _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr int compare(__string_view const& __other) const noexcept
   {
@@ -178,7 +251,7 @@ private:
     return __x < __y ? __x : __y;
   }
 
-  _LIBCUDACXX_HIDE_FROM_ABI __string_view(char const* __str, size_t __len) noexcept
+  _LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view(char const* __str, size_t __len) noexcept
       : __str_(__str)
       , __len_(__len)
   {}
@@ -187,92 +260,93 @@ private:
   size_t __len_;
 };
 
-// For the known compilers, we can extract the type name from the compiler's
-// built-in macros. This is a best-effort approach, and may not work for all
-// compilers or configurations.
+// We find a type T's name as follows:
+// 1. Use __PRETTY_FUNCTION__ in a function template parameterized by
+//    __pretty_name_begin<T>::__pretty_name_end.
+// 2. Find the substrings "__pretty_name_begin<" and ">::__pretty_name_end".
+//    Everything between them is the name of type T.
 #  if defined(_CCCL_COMPILER_MSVC)
 #    define _CCCL_PRETTY_FUNCTION __FUNCSIG__
 #  else
 #    define _CCCL_PRETTY_FUNCTION __PRETTY_FUNCTION__
 #  endif
 
-// The name of this function (__PRETTY_NAMEOF) must have the same number of
-// characters as __pretty_nameof, the function used below for extracting the
-// type name from the pretty function name. Otherwise, the trim counts computed
-// below will be incorrect.
+// Earlier versions of gcc (before gcc-9) do not treat __PRETTY_FUNCTION__ as a
+// constexpr value after a reference to it has been returned from the function.
+// Instead, arrange things so that the pretty name gets stored in a class static
+// data member, where it can be referenced from other constexpr contexts.
+
+#  if defined(_CCCL_COMPILER_GCC) && _CCCL_GCC_VERSION < 90000
+
+template <size_t _Np>
+struct __static_string
+{
+  char __str_[_Np];
+};
+
+template <size_t _Np, size_t... _Is>
+constexpr auto __pretty_nameof_5(char const (&a)[_Np], index_sequence<_Is...>)
+{
+  return __static_string<_Np>{{a[_Is]...}};
+}
+
 template <class _Tp>
-_LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view __PRETTY_NAMEOF()
+constexpr auto __pretty_nameof_4()
 {
-  return __string_view(_CCCL_PRETTY_FUNCTION);
+  return __pretty_nameof_5(_CCCL_PRETTY_FUNCTION, make_index_sequence<sizeof(_CCCL_PRETTY_FUNCTION)>());
 }
 
-// clang-format off
-// These are the known pretty names for __PRETTY_NAMEOF<int>() for various compilers.
-constexpr size_t __pretty_index_impl()
+template <class _Tp>
+using __static_name_for = decltype(__pretty_nameof_4<_Tp>());
+
+template <class _Tp>
+struct __pretty_name_begin
 {
-  return //
-    __PRETTY_NAMEOF<int>() == __string_view{"constexpr cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = int]"} ? 0
-    : __PRETTY_NAMEOF<int>() == __string_view{"cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = int]"}         ? 1
-    : __PRETTY_NAMEOF<int>() == __string_view{"constexpr cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [_Tp = int]"}    ? 2
-    : __PRETTY_NAMEOF<int>() == __string_view{"cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [_Tp = int]"}              ? 3
-    : __PRETTY_NAMEOF<int>() == __string_view{"constexpr __string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = int]"}          ? 4
-    : __PRETTY_NAMEOF<int>() == __string_view{"__string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = int]"}                    ? 5
-    : __PRETTY_NAMEOF<int>() == __string_view{"constexpr __string_view cuda::std::__PRETTY_NAMEOF() [_Tp = int]"}               ? 6
-    : __PRETTY_NAMEOF<int>() == __string_view{"__string_view cuda::std::__PRETTY_NAMEOF() [_Tp = int]"}                         ? 7
-    : __PRETTY_NAMEOF<int>() == __string_view{"cuda::std::__string_view cuda::std::__PRETTY_NAMEOF<int>()"}                     ? 8
-    : __PRETTY_NAMEOF<int>() == __string_view{"struct cuda::std::__string_view __cdecl cuda::std::__PRETTY_NAMEOF<int>(void)"}  ? 9
-    : ~size_t(0);
-}
-// clang-format on
-
-// Find the index of the pretty name for __PRETTY_NAMEOF<int>() in the known list.
-constexpr size_t __pretty_index = __pretty_index_impl();
-
-// Assert that we found a match.
-static_assert(__pretty_index != ~size_t(0), "Unrecognized __PRETTY_FUNCTION__ string format.");
-
-struct __pretty_trim_counts
-{
-  size_t __front;
-  size_t __back;
+  static constexpr __static_name_for<_Tp> value = __pretty_nameof_4<_Tp>();
+  struct __pretty_name_end;
 };
 
-// clang-format off
-// Break the pretty names into front and back parts to trim to get to the type name.
-constexpr __pretty_trim_counts __pretty_trim_impl()
-{
-  using __pretty_trim_counts_array = __pretty_trim_counts[];
-  return __pretty_trim_counts_array{
-    __pretty_trim_counts{sizeof("constexpr cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("constexpr cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [_Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("cuda::std::__string_view cuda::std::__PRETTY_NAMEOF() [_Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("constexpr __string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("__string_view cuda::std::__PRETTY_NAMEOF() [with _Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("constexpr __string_view cuda::std::__PRETTY_NAMEOF() [_Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("__string_view cuda::std::__PRETTY_NAMEOF() [_Tp = ") - 1, sizeof("]") - 1},
-    __pretty_trim_counts{sizeof("cuda::std::__string_view cuda::std::__PRETTY_NAMEOF<") - 1, sizeof(">()") - 1},
-    __pretty_trim_counts{sizeof("struct cuda::std::__string_view __cdecl cuda::std::__PRETTY_NAMEOF<") - 1, sizeof(">(void)") - 1}
-  }[__pretty_index];
-};
-// clang-format on
+#    if !defined(_CCCL_NO_INLINE_VARIABLES)
+template <class _Tp>
+constexpr __static_name_for<_Tp> __pretty_name_begin<_Tp>::value;
+#    endif // !_CCCL_NO_INLINE_VARIABLES
 
-constexpr __pretty_trim_counts __pretty_trim = __pretty_trim_impl();
+#  else // ^^^ _CCCL_GCC_VERSION < 90000 ^^^ / vvv _CCCL_GCC_VERSION >= 90000 vvv
+
+template <class _Tp>
+struct __pretty_name_begin
+{
+  struct __pretty_name_end;
+};
+
+#  endif // _CCCL_GCC_VERSION < 90000
 
 // Get the type name from the pretty name by trimming the front and back.
-template <class _Tp>
-_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view __pretty_nameof()
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view __pretty_nameof_3(__string_view __sv) noexcept
 {
-  return __string_view(_CCCL_PRETTY_FUNCTION, __pretty_trim.__front, __pretty_trim.__back);
+  return __sv.substr(__sv.find("__pretty_name_begin<") + sizeof("__pretty_name_begin<") - 1,
+                     __sv.find(">::__pretty_name_end"));
+}
+
+template <class _Tp>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view __pretty_nameof_2() noexcept
+{
+#  if defined(_CCCL_COMPILER_GCC) && _CCCL_GCC_VERSION < 90000
+  return __pretty_nameof_3(__string_view(__pretty_name_begin<_Tp>::value.__str_));
+#  else
+  return __pretty_nameof_3(__string_view(_CCCL_PRETTY_FUNCTION));
+#  endif
+}
+
+template <class _Tp>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __string_view __pretty_nameof() noexcept
+{
+  return __pretty_nameof_2<typename __pretty_name_begin<_Tp>::__pretty_name_end>();
 }
 
 // A quick smoke test to ensure that the pretty name extraction is working.
 static_assert(__pretty_nameof<int>() == __string_view("int"), "__pretty_nameof<int>() == __string_view(\"int\")");
 static_assert(__pretty_nameof<float>() < __pretty_nameof<int>(), "__pretty_nameof<float>() < __pretty_nameof<int>()");
-
-_LIBCUDACXX_END_NAMESPACE_STD_NOVERSION
-
-_LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 /// @brief A minimal implementation of `std::type_info` for platforms that do
 /// not support RTTI.
