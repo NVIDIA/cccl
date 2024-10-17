9667105387662a1db7c6ffeb0238ef6da4c5996c
Cedric Augonnet <caugonnet@nvidia.com>
2024-10-17 13:40:52 +0200

    do not use ssize_t

--- cudax/test/stf/parallel_for/parallel_for_box.cu
+++ cudax/test/stf/parallel_for/parallel_for_box.cu
@@ -33,7 +33,7 @@ int main()
 
   // Create a subset of a limited size
   box subset_shape(3, 4);
-  ctx.parallel_for(subset_shape, lA.rw())->*[=] CUDASTF_DEVICE(ssize_t i, ssize_t j, auto sA) {
+  ctx.parallel_for(subset_shape, lA.rw())->*[=] CUDASTF_DEVICE(size_t i, size_t j, auto sA) {
     sA(i, j) = 13.0;
   };
 
@@ -57,13 +57,13 @@ int main()
   std::vector<double> B(M * N);
   auto lB = ctx.logical_data(make_slice(&B[0], std::tuple<size_t, size_t>{M, N}, M));
 
-  ctx.parallel_for(lB.shape(), lB.write())->*[=] CUDASTF_DEVICE(ssize_t i, ssize_t j, auto sB) {
+  ctx.parallel_for(lB.shape(), lB.write())->*[=] CUDASTF_DEVICE(size_t i, size_t j, auto sB) {
     sB(i, j) = 42.0;
   };
 
   // Create a subset of a limited size
   box subset_shape_2({2, 5}, {5, 8});
-  ctx.parallel_for(subset_shape_2, lB.rw())->*[=] CUDASTF_DEVICE(ssize_t i, ssize_t j, auto sB) {
+  ctx.parallel_for(subset_shape_2, lB.rw())->*[=] CUDASTF_DEVICE(size_t i, size_t j, auto sB) {
     sB(i, j) = 13.0;
   };
 
