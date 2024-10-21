b0974868d865186dfe85eb0b36d37b2af20ee8f7
Wesley Maxey <wesley.maxey@gmail.com>
2024-10-21 21:09:39 +0000

    Test aligned_size_t for shape

--- libcudacxx/test/libcudacxx/cuda/annotated_ptr/apply_access_property.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/annotated_ptr/apply_access_property.pass.cpp
@@ -14,16 +14,32 @@
 // UNSUPPORTED: c++98, c++03
 
 #include "utils.h"
-#define ARR_SZ 128
+
+constexpr size_t array_size = 128;
 
 template <typename T, typename P>
 __device__ __host__ __noinline__ void test(P ap, bool shared = false)
 {
-  T* arr = alloc<T, ARR_SZ>(shared);
+  T* arr = alloc<T, array_size>(shared);
 
-  cuda::apply_access_property(arr, ARR_SZ, ap);
+  cuda::apply_access_property(arr, array_size * sizeof(T), ap);
 
-  for (int i = 0; i < ARR_SZ; ++i)
+  for (size_t i = 0; i < array_size; ++i)
+  {
+    assert(arr[i] == i);
+  }
+
+  dealloc<T>(arr, shared);
+}
+
+template <typename T, typename P>
+__device__ __host__ __noinline__ void test_aligned(P ap, bool shared = false)
+{
+  T* arr = alloc<T, array_size>(shared);
+
+  cuda::apply_access_property(arr, cuda::aligned_size_t<sizeof(T)>(array_size * sizeof(T)), ap);
+
+  for (size_t i = 0; i < array_size; ++i)
   {
     assert(arr[i] == i);
   }
@@ -35,11 +51,8 @@ __device__ __host__ __noinline__ void test_all()
 {
   test<int>(cuda::access_property::normal{});
   test<int>(cuda::access_property::persisting{});
-}
-
-__global__ void test_kernel()
-{
-  test_all();
+  test_aligned<int>(cuda::access_property::normal{});
+  test_aligned<int>(cuda::access_property::persisting{});
 }
 
 int main(int argc, char** argv)
