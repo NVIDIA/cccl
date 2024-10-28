35eb5f15a0180e420a6cedcc82c13ff57fb340a4
Michael Schellenberger Costa <miscco@nvidia.com>
2024-10-28 11:49:50 +0100

    Do not test with broken compiler

--- libcudacxx/test/libcudacxx/std/language.support/support.srcloc/general.pass.cpp
+++ libcudacxx/test/libcudacxx/std/language.support/support.srcloc/general.pass.cpp
@@ -3,8 +3,11 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: nvcc-11.1, nvcc-11.2
 
 #include <cuda/std/cassert>
 #include <cuda/std/cstdint>
