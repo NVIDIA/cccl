a2d6bb4002d31d45a53e0e95c90c683fffd9a237
Allison Piper <alliepiper16@gmail.com>
2024-10-30 15:22:56 +0000

    Use extended devcontainers for cudax testing.
    [skip-vdc][skip-rapids][skip-docs]

--- ci/build_cudax.sh
+++ ci/build_cudax.sh
@@ -10,6 +10,12 @@ PRESET="cudax-cpp$CXX_STANDARD"
 
 CMAKE_OPTIONS=""
 
+# Enable extra mathlibs if we're in an extended CUDA image:
+if $CCCL_CUDA_EXTENDED; then
+  echo "Image with extended CUDA libs detected, enabling STF MathLibs."
+  CMAKE_OPTIONS="$CMAKE_OPTIONS -Dcudax_ENABLE_CUDASTF_MATHLIBS=ON"
+fi
+
 configure_and_build_preset "CUDA Experimental" "$PRESET" "$CMAKE_OPTIONS"
 
 print_time_summary
