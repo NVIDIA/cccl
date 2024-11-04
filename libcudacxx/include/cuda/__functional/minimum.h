9c2539f3b7a25a3092037d4891674e80956204da
Jacob Faibussowitsch <jacob.fai@gmail.com>
2024-11-04 11:59:07 -0500

    review comments

--- libcudacxx/include/cuda/__functional/minimum.h
+++ libcudacxx/include/cuda/__functional/minimum.h
@@ -21,18 +21,15 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/std/__functional/binary_function.h>
 #include <cuda/std/__type_traits/common_type.h>
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 
 template <class _Tp = void>
-struct _CCCL_TYPE_VISIBILITY_DEFAULT minimum : __binary_function<_Tp, _Tp, _Tp>
+struct _CCCL_TYPE_VISIBILITY_DEFAULT minimum
 {
-  using __result_type = _Tp; // used by valarray
-
   _CCCL_EXEC_CHECK_DISABLE
-  constexpr _LIBCUDACXX_HIDE_FROM_ABI _Tp operator()(const _Tp& __x, const _Tp& __y) const
+  _LIBCUDACXX_HIDE_FROM_ABI constexpr _Tp operator()(const _Tp& __x, const _Tp& __y) const
     noexcept(noexcept((__x < __y) ? __x : __y))
   {
     return (__x < __y) ? __x : __y;
@@ -45,8 +42,8 @@ struct _CCCL_TYPE_VISIBILITY_DEFAULT minimum<void>
 {
   _CCCL_EXEC_CHECK_DISABLE
   template <class _T1, class _T2>
-  constexpr _LIBCUDACXX_HIDE_FROM_ABI typename _CUDA_VSTD::common_type<_T1, _T2>::type
-  operator()(const _T1& __t, const _T2& __u) const noexcept(noexcept((__t < __u) ? __t : __u))
+  _LIBCUDACXX_HIDE_FROM_ABI constexpr __common_type_t<_T1, _T2> operator()(const _T1& __t, const _T2& __u) const
+    noexcept(noexcept((__t < __u) ? __t : __u))
   {
     return (__t < __u) ? __t : __u;
   }
