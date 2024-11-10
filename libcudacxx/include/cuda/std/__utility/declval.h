c4a4d9a1e588ab77d7a35faea44529a432bf22e3
Eric Niebler <eniebler@nvidia.com>
2024-11-10 05:00:48 +0000

    give `declval` an implementation that compiles 3x faster

--- libcudacxx/include/cuda/std/__utility/declval.h
+++ libcudacxx/include/cuda/std/__utility/declval.h
@@ -20,7 +20,45 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/std/__type_traits/void_t.h>
+
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
+
+// When variable templates are available, a faster implementation of declval
+// is available. It compiles approximately 3x faster than the fallback.
+#if !defined(_CCCL_NO_VARIABLE_TEMPLATES)
+
+// NVCC wants the primary variable template to be well-formed. It cannot
+// handle the form below which has _Tp&& in the primary template because
+// it thinks declval<void>() is ill-formed.
+#  if defined(_CCCL_CUDA_COMPILER_NVCC)
+
+template <class _Tp, class = void>
+extern void (*declval)() noexcept;
+
+template <class _Tp>
+  extern _Tp && (*declval<_Tp, void_t<_Tp&&>>) () noexcept;
+
+#  else // ^^^ defined(_CCCL_CUDA_COMPILER_NVCC) ^^^ / vvv !defined(_CCCL_CUDA_COMPILER_NVCC) vvv
+
+template <class _Tp>
+  extern _Tp && (*declval)() noexcept;
+
+template <>
+constexpr void (*declval<void>)() noexcept = nullptr; // NOLINT (clang-tidymisc-definitions-in-headers)
+
+template <>
+constexpr void (*declval<void const>)() noexcept = nullptr; // NOLINT (clang-tidymisc-definitions-in-headers)
+
+template <>
+constexpr void (*declval<void volatile>)() noexcept = nullptr; // NOLINT (clang-tidymisc-definitions-in-headers)
+
+template <>
+constexpr void (*declval<void const volatile>)() noexcept = nullptr; // NOLINT (clang-tidymisc-definitions-in-headers)
+
+#  endif // _CCCL_CUDA_COMPILER_NVCC
+
+#else // ^^^ !_CCCL_NO_VARIABLE_TEMPLATES ^^^ / vvv _CCCL_NO_VARIABLE_TEMPLATES vvv
 
 // Suppress deprecation notice for volatile-qualified return type resulting
 // from volatile-qualified types _Tp.
@@ -33,6 +71,8 @@ _CCCL_SUPPRESS_DEPRECATED_POP
 
 template <class _Tp>
 _LIBCUDACXX_HIDE_FROM_ABI decltype(_CUDA_VSTD::__declval<_Tp>(0)) declval() noexcept;
+
+#endif // _CCCL_NO_VARIABLE_TEMPLATES
 
 _LIBCUDACXX_END_NAMESPACE_STD
 
