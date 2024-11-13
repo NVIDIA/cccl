76c88ba5a97681471348d3dc259918f3b3d154eb
David Bayer <bayer.david99@gmail.com>
2024-11-13 21:32:31 +0100

    fix sfinae

--- libcudacxx/include/cuda/std/__utility/cmp.h
+++ libcudacxx/include/cuda/std/__utility/cmp.h
@@ -21,6 +21,7 @@
 #endif // no system header
 
 #include <cuda/std/__type_traits/disjunction.h>
+#include <cuda/std/__type_traits/enable_if.h>
 #include <cuda/std/__type_traits/is_integral.h>
 #include <cuda/std/__type_traits/is_same.h>
 #include <cuda/std/__type_traits/is_signed.h>
@@ -64,29 +65,34 @@ struct __cmp_equal_impl
     return __t == __u;
   }
 
-  template <class _Tp, class _Up, enable_if_t<is_signed<_Tp>::value, int> = 0>
+  template <
+    class _Tp,
+    class _Up,
+    enable_if_t<(is_signed<_Tp>::value && !is_signed<_Up>::value) || (!is_signed<_Tp>::value && is_signed<_Up>::value),
+                int> = 0>
   static constexpr bool __do_cmp(_Tp __t, _Up __u) noexcept
   {
     return __t < 0 ? false : make_unsigned_t<_Tp>(__t) == __u;
   }
 
-  template <class _Tp, class _Up>
+  template <class _Tp, class _Up, enable_if_t<!is_signed<_Tp>::value && !is_signed<_Up>::value, int> = 0>
   static constexpr bool __do_cmp(_Tp __t, _Up __u) noexcept
   {
     return __u < 0 ? false : __t == make_unsigned_t<_Up>(__u);
   }
 };
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_equal(_Tp __t, _Up __u) noexcept
 {
-  static_assert(__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value,
-                "comparison operands must be integral types");
-
   return __cmp_equal_impl::__do_cmp(__t, __u);
 }
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_not_equal(_Tp __t, _Up __u) noexcept
 {
   return !_CUDA_VSTD::cmp_equal(__t, __u);
@@ -94,53 +100,64 @@ _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_not_equal(_Tp __t, _Up __u) noexcep
 
 struct __cmp_less_impl
 {
-  template <class _Tp, class _Up, enable_if_t<is_signed<_Tp>::value == is_signed<_Up>::value, int> = 0>
+  template <class _Tp, class _Up, enable_if_t<is_signed<_Tp>::value && is_signed<_Up>::value, int> = 0>
   static constexpr bool __do_cmp(_Tp __t, _Up __u) noexcept
   {
     return __t < __u;
   }
 
-  template <class _Tp, class _Up, enable_if_t<is_signed<_Tp>::value, int> = 0>
+  template <
+    class _Tp,
+    class _Up,
+    enable_if_t<(is_signed<_Tp>::value && !is_signed<_Up>::value) || (!is_signed<_Tp>::value && is_signed<_Up>::value),
+                int> = 0>
   static constexpr bool __do_cmp(_Tp __t, _Up __u) noexcept
   {
     return __t < 0 ? true : make_unsigned_t<_Tp>(__t) < __u;
   }
 
-  template <class _Tp, class _Up>
+  template <class _Tp, class _Up, enable_if_t<!is_signed<_Tp>::value && !is_signed<_Up>::value, int> = 0>
   static constexpr bool __do_cmp(_Tp __t, _Up __u) noexcept
   {
     return __u < 0 ? false : __t < make_unsigned_t<_Up>(__u);
   }
 };
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_less(_Tp __t, _Up __u) noexcept
 {
-  static_assert(__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value,
-                "comparison operands must be integral types");
-
   return __cmp_less_impl::__do_cmp(__t, __u);
 }
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_greater(_Tp __t, _Up __u) noexcept
 {
   return _CUDA_VSTD::cmp_less(__u, __t);
 }
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_less_equal(_Tp __t, _Up __u) noexcept
 {
   return !_CUDA_VSTD::cmp_greater(__t, __u);
 }
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool cmp_greater_equal(_Tp __t, _Up __u) noexcept
 {
   return !_CUDA_VSTD::cmp_less(__t, __u);
 }
 
-template <class _Tp, class _Up>
+template <class _Tp,
+          class _Up,
+          enable_if_t<__is_safe_integral_cmp<_Tp>::value && __is_safe_integral_cmp<_Up>::value, int> = 0>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr bool in_range(_Up __u) noexcept
 {
   return _CUDA_VSTD::cmp_less_equal(__u, numeric_limits<_Tp>::max())
