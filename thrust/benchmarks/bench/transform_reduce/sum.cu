f4d358a81c23a0171efcfc9f353cc238c9d7f95a
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2024-11-15 22:25:06 +0100

    Remove all warm-up runs from Thrust benchmarks (#2838)
    NVBench handles warm-up runs.
    
    Fixes: #2836

--- thrust/benchmarks/bench/transform_reduce/sum.cu
+++ thrust/benchmarks/bench/transform_reduce/sum.cu
@@ -52,8 +52,6 @@ static void basic(nvbench::state& state, nvbench::type_list<T>)
   state.add_global_memory_writes<T>(1);
 
   caching_allocator_t alloc;
-  do_not_optimize(thrust::transform_reduce(policy(alloc), in.begin(), in.end(), square_t<T>{}, T{}, thrust::plus<T>{}));
-
   state.exec(nvbench::exec_tag::no_batch | nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
     do_not_optimize(
       thrust::transform_reduce(policy(alloc, launch), in.begin(), in.end(), square_t<T>{}, T{}, thrust::plus<T>{}));
