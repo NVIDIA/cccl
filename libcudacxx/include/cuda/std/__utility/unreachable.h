bed27be78cc89a574150bf5a18d3f0078dd74caa
David Bayer <bayer.david99@gmail.com>
2024-11-18 16:27:27 +0100

    fix compilation for host/device unreachable

--- libcudacxx/include/cuda/std/__utility/unreachable.h
+++ libcudacxx/include/cuda/std/__utility/unreachable.h
@@ -24,28 +24,30 @@ _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void unreachable()
 {
-  NV_IF_ELSE_TARGET(
-    NV_IS_DEVICE,
+#if defined(_CCCL_COMPILER_MSVC)
+#  define _CCCL_HOST_UNREACHABLE_IMPL() __assume(0)
+#else // ^^^ _CCCL_COMPILER_MSVC ^^^ / vvv !_CCCL_COMPILER_MSVC vvv
+#  define _CCCL_HOST_UNREACHABLE_IMPL() __builtin_unreachable()
+#endif // !_CCCL_COMPILER_MSVC
+
 #if defined(_CCCL_CUDA_COMPILER_CLANG)
-    __builtin_unreachable();
+#  define _CCCL_DEVICE_UNREACHABLE_IMPL() __builtin_unreachable()
 #elif defined(__CUDA_ARCH__)
 #  if _CCCL_CUDACC_BELOW(11, 2)
-    __trap();
+#    define _CCCL_DEVICE_UNREACHABLE_IMPL() __trap()
 #  elif _CCCL_CUDACC_BELOW(11, 3)
-    __builtin_assume(false);
+#    define _CCCL_DEVICE_UNREACHABLE_IMPL() __builtin_assume(false)
 #  else
-    __builtin_unreachable();
+#    define _CCCL_DEVICE_UNREACHABLE_IMPL() __builtin_unreachable()
 #  endif // CUDACC above 11.4
 #else // ^^^ __CUDA_ARCH__ ^^^ / vvv !__CUDA_ARCH__ vvv
-    __builtin_unreachable();
+#  define _CCCL_DEVICE_UNREACHABLE_IMPL() __builtin_unreachable()
 #endif // !__CUDA_ARCH__
-    ,
-#if defined(_CCCL_COMPILER_MSVC)
-    __assume(0);
-#else // ^^^ _CCCL_COMPILER_MSVC ^^^ / vvv !_CCCL_COMPILER_MSVC vvv
-    __builtin_unreachable();
-#endif // !_CCCL_COMPILER_MSVC
-  )
+
+  NV_IF_ELSE_TARGET(NV_IS_HOST, _CCCL_HOST_UNREACHABLE_IMPL();, _CCCL_DEVICE_UNREACHABLE_IMPL();)
+
+#undef _CCCL_HOST_UNREACHABLE_IMPL
+#undef _CCCL_DEVICE_UNREACHABLE_IMPL
 }
 
 _LIBCUDACXX_END_NAMESPACE_STD
