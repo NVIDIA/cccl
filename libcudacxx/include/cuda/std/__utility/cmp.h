01e610c3c9df00d42541e3fe1d0194a4c2cbf388
David Bayer <bayer.david99@gmail.com>
2024-11-18 08:53:18 +0100

    fix concept macros include

--- libcudacxx/include/cuda/std/__utility/cmp.h
+++ libcudacxx/include/cuda/std/__utility/cmp.h
@@ -20,7 +20,7 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/std/__concepts/__concept_macros.h>
+#include <cuda/std/__concepts/concept_macros.h>
 #include <cuda/std/__type_traits/disjunction.h>
 #include <cuda/std/__type_traits/enable_if.h>
 #include <cuda/std/__type_traits/integral_constant.h>
