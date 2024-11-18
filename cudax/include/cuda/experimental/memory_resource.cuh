959a7d08e276ac1b3fcc8f1f2bc78372359ec406
Michael Schellenberger Costa <miscco@nvidia.com>
2024-11-18 18:03:04 +0100

    Duplicate pinned and managed memory resources to cudax and prepare them to be usable in stream order

--- cudax/include/cuda/experimental/memory_resource.cuh
+++ cudax/include/cuda/experimental/memory_resource.cuh
@@ -14,6 +14,8 @@
 #include <cuda/experimental/__memory_resource/any_resource.cuh>
 #include <cuda/experimental/__memory_resource/device_memory_pool.cuh>
 #include <cuda/experimental/__memory_resource/device_memory_resource.cuh>
+#include <cuda/experimental/__memory_resource/managed_memory_resource.cuh>
+#include <cuda/experimental/__memory_resource/pinned_memory_resource.cuh>
 #include <cuda/experimental/__memory_resource/properties.cuh>
 #include <cuda/experimental/__memory_resource/shared_resource.cuh>
 
