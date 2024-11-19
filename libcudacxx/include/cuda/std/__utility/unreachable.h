f7affcf7fc533a5b1764ccdbdb767b63d2579bbb
David Bayer <bayer.david99@gmail.com>
2024-11-19 21:57:15 +0100

    fall back to the original implementation

--- libcudacxx/include/cuda/std/__utility/unreachable.h
+++ libcudacxx/include/cuda/std/__utility/unreachable.h
@@ -22,35 +22,23 @@
 
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
-_CCCL_NORETURN _CCCL_HIDE_FROM_ABI _CCCL_HOST void __unreachable_host_impl()
-{
-#if defined(_CCCL_COMPILER_MSVC)
-  __assume(0);
-#else // ^^^ _CCCL_COMPILER_MSVC ^^^ / vvv !_CCCL_COMPILER_MSVC vvv
-  __builtin_unreachable();
-#endif // !_CCCL_COMPILER_MSVC
-}
-
-#if defined(_CCCL_CUDA_COMPILER)
-_CCCL_NORETURN _CCCL_HIDE_FROM_ABI _CCCL_DEVICE void __unreachable_device_impl()
-{
-#  if defined(_CCCL_CUDA_COMPILER_NVCC)
-#    if _CCCL_CUDACC_BELOW(11, 2)
-  __trap();
-#    elif _CCCL_CUDACC_BELOW(11, 3)
-  __builtin_assume(false);
-#    else
-  __builtin_unreachable();
-#    endif
-#  else // ^^^ _CCCL_CUDA_COMPILER_NVCC ^^^ / vvv !_CCCL_CUDA_COMPILER_NVCC vvv
-  __builtin_unreachable();
-#  endif // !_CCCL_CUDA_COMPILER_NVCC
-}
-#endif // _CCCL_CUDA_COMPILER
-
 _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void unreachable()
 {
-  NV_IF_ELSE_TARGET(NV_IS_HOST, _CUDA_VSTD::__unreachable_host_impl();, _CUDA_VSTD::__unreachable_device_impl();)
+#if defined(_CCCL_CUDA_COMPILER)
+#  if defined(_CCCL_CUDA_COMPILER_NVCC) && _CCCL_CUDACC_BELOW(11, 2)
+  __trap();
+#  elif defined(_CCCL_CUDA_COMPILER_NVCC) && _CCCL_CUDACC_BELOW(11, 3)
+  __builtin_assume(false);
+#  else
+  __builtin_unreachable();
+#  endif
+#else // ^^^ _CCCL_CUDA_COMPILER ^^^ / vvv !_CCCL_CUDA_COMPILER vvv
+#  if defined(_CCCL_COMPILER_MSVC)
+  __assume(0);
+#  else // ^^^ _CCCL_COMPILER_MSVC ^^^ / vvv !_CCCL_COMPILER_MSVC vvv
+  __builtin_unreachable();
+#  endif // !_CCCL_COMPILER_MSVC
+#endif // !_CCCL_CUDA_COMPILER
 }
 
 _LIBCUDACXX_END_NAMESPACE_STD
