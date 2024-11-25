0702aa28acdf3c8c457fe8396163f4716f7106ac
David Bayer <bayer.david99@gmail.com>
2024-11-25 15:49:34 +0100

    add `_CCCL_DEPRECATED_TYPE_ALIAS`

--- libcudacxx/include/cuda/std/__cccl/attributes.h
+++ libcudacxx/include/cuda/std/__cccl/attributes.h
@@ -127,14 +127,22 @@
 #  define _CCCL_DEPRECATED_BECAUSE(_MSG)
 #endif
 
-// use the C++14 deprecated attribute if available instead of the vendor specific ones except for GCC before 13 and
-// Clang before 13 which have issues with combining it with other attributes
-#if _CCCL_STD_VER >= 2014 && _CCCL_HAS_CPP_ATTRIBUTE(deprecated) && !_CCCL_COMPILER(GCC, <, 13) \
-  && !_CCCL_COMPILER(CLANG, <, 13)
+// use the C++14 deprecated attribute instead of the vendor specific ones except for GCC before 13 and Clang before 13
+// which have issues with combining it with other attributes
+#if _CCCL_STD_VER >= 2014 && !_CCCL_COMPILER(GCC, <, 13) && !_CCCL_COMPILER(CLANG, <, 13)
 #  undef _CCCL_DEPRECATED
 #  undef _CCCL_DEPRECATED_BECAUSE
 #  define _CCCL_DEPRECATED               [[deprecated]]
 #  define _CCCL_DEPRECATED_BECAUSE(_MSG) [[deprecated(_MSG)]]
+#endif
+
+// MSVC vendor specific attribute does not support type aliasing
+#if _CCCL_STD_VER < 2014 && _CCCL_COMPILER(MSVC)
+#  define _CCCL_DEPRECATED_TYPE_ALIAS
+#  define _CCCL_DEPRECATED_TYPE_ALIAS_BECAUSE(_MSG)
+#else
+#  define _CCCL_DEPRECATED_TYPE_ALIAS               _CCCL_DEPRECATED
+#  define _CCCL_DEPRECATED_TYPE_ALIAS_BECAUSE(_MSG) _CCCL_DEPRECATED_BECAUSE(_MSG)
 #endif
 
 #endif // __CCCL_ATTRIBUTES_H
