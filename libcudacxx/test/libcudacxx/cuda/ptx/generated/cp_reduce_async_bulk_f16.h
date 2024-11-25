0c35ba7644f67f0c5b235506d2404f87178c21a2
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2024-11-25 19:41:13 +0100

    Regenerate PTX test (#2953)
    Overwrites all generated PTX tests and runs `pre-commit run --all-files`

--- libcudacxx/test/libcudacxx/cuda/ptx/generated/cp_reduce_async_bulk_f16.h
+++ libcudacxx/test/libcudacxx/cuda/ptx/generated/cp_reduce_async_bulk_f16.h
@@ -1,6 +1,22 @@
+// This file was automatically generated. Do not edit.
+
+// We use a special strategy to force the generation of the PTX. This is mainly
+// a fight against dead-code-elimination in the NVVM layer.
+//
+// The reason we need this strategy is because certain older versions of ptxas
+// segfault when a non-sensical sequence of PTX is generated. So instead, we try
+// to force the instantiation and compilation to PTX of all the overloads of the
+// PTX wrapping functions.
+//
+// We do this by writing a function pointer of each overload to the kernel
+// parameter `fn_ptr`.
+//
+// Because `fn_ptr` is possibly visible outside this translation unit, the
+// compiler must compile all the functions which are stored.
+
 __global__ void test_cp_reduce_async_bulk_f16(void** fn_ptr)
 {
-#  if __cccl_ptx_isa >= 800
+#if __cccl_ptx_isa >= 800
   NV_IF_TARGET(
     NV_PROVIDES_SM_90,
     (
@@ -9,9 +25,9 @@ __global__ void test_cp_reduce_async_bulk_f16(void** fn_ptr)
           static_cast<void (*)(
             cuda::ptx::space_global_t, cuda::ptx::space_shared_t, cuda::ptx::op_min_t, __half*, const __half*, uint32_t)>(
             cuda::ptx::cp_reduce_async_bulk));));
-#  endif // __cccl_ptx_isa >= 800
+#endif // __cccl_ptx_isa >= 800
 
-#  if __cccl_ptx_isa >= 800
+#if __cccl_ptx_isa >= 800
   NV_IF_TARGET(
     NV_PROVIDES_SM_90,
     (
@@ -20,9 +36,9 @@ __global__ void test_cp_reduce_async_bulk_f16(void** fn_ptr)
           static_cast<void (*)(
             cuda::ptx::space_global_t, cuda::ptx::space_shared_t, cuda::ptx::op_max_t, __half*, const __half*, uint32_t)>(
             cuda::ptx::cp_reduce_async_bulk));));
-#  endif // __cccl_ptx_isa >= 800
+#endif // __cccl_ptx_isa >= 800
 
-#  if __cccl_ptx_isa >= 800
+#if __cccl_ptx_isa >= 800
   NV_IF_TARGET(
     NV_PROVIDES_SM_90,
     (
@@ -31,5 +47,5 @@ __global__ void test_cp_reduce_async_bulk_f16(void** fn_ptr)
           static_cast<void (*)(
             cuda::ptx::space_global_t, cuda::ptx::space_shared_t, cuda::ptx::op_add_t, __half*, const __half*, uint32_t)>(
             cuda::ptx::cp_reduce_async_bulk));));
-#  endif // __cccl_ptx_isa >= 800
+#endif // __cccl_ptx_isa >= 800
 }
