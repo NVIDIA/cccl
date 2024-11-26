13898265d3e21860a371daf41c0791bdb6975d36
Michael Schellenberger Costa <miscco@nvidia.com>
2024-11-26 09:03:05 +0100

    Add tests

--- libcudacxx/test/libcudacxx/std/containers/views/mdspan/mdspan.extents.dims/compare.pass.cpp
+++ libcudacxx/test/libcudacxx/std/containers/views/mdspan/mdspan.extents.dims/compare.pass.cpp
@@ -3,7 +3,7 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
@@ -21,6 +21,7 @@ int main(int, char**)
     cuda::std::dextents<index_t, 3> e0{1, 2, 3};
     cuda::std::dims<3> e1{1, 2, 3};
 
+    static_assert(cuda::std::is_same<decltype(e0), decltype(e1)>::value, "");
     assert(e0 == e1);
   }
 
