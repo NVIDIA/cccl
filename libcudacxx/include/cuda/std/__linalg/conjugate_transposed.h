6f18beec90765bfa82e6381b65aa92b2174f1ad7
Federico Busato <50413820+fbusato@users.noreply.github.com>
2024-12-03 09:53:30 -0800

    Update libcudacxx/include/cuda/std/__linalg/conjugate_transposed.h
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- libcudacxx/include/cuda/std/__linalg/conjugate_transposed.h
+++ libcudacxx/include/cuda/std/__linalg/conjugate_transposed.h
@@ -65,7 +65,7 @@ namespace linalg
 {
 
 template <class _ElementType, class _Extents, class _Layout, class _Accessor>
-[[nodiscard]] _LIBCUDACXX_HIDE_FROM_ABI auto
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI auto
 conjugate_transposed(mdspan<_ElementType, _Extents, _Layout, _Accessor> __a)
 {
   return conjugated(transposed(__a));
