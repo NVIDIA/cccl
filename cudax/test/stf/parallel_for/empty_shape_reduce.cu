bf372a91348e28da8265044b5d5d21faeba1fd80
Cedric Augonnet <caugonnet@nvidia.com>
2024-12-05 11:20:35 +0100

    Add a test to ensure we properly deal with empty shapes in parallel_for : it indeed requires to initialize the variable. This is failing currently because we did not implement this initialization.

---
+++ cudax/test/stf/parallel_for/empty_shape_reduce.cu
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDASTF in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2022-2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+/**
+ * @file
+ *
+ * @brief Test reduce access mode
+ *
+ */
+
+#include <cuda/experimental/stf.cuh>
+
+using namespace cuda::experimental::stf;
+
+int main()
+{
+  context ctx;
+  auto lsum = ctx.logical_data(shape_of<scalar<int>>());
+  auto lmax = ctx.logical_data(shape_of<scalar<int>>());
+
+  ctx.parallel_for(box(0), lsum.reduce(reducer::sum<int>{}), lmax.reduce(reducer::maxval<int>{}))->*[] __device__(size_t, auto&, auto &) {
+    // This is never going to be called because this is an empty shape
+  };
+
+  auto res_sum = ctx.transfer_host(lsum);
+  auto res_max = ctx.transfer_host(lmax);
+
+  ctx.finalize();
+
+  _CCCL_ASSERT(res_sum == 0, "Invalid result");
+  _CCCL_ASSERT(res_max == ::std::numeric_limits<int>::lowest(), "Invalid result");
+}
