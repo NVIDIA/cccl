cac94383f53cb6d4f087cd292f2b59ef497d960a
Cedric Augonnet <caugonnet@nvidia.com>
2024-12-11 13:08:57 +0100

    Start to implement a reduce method over CUB

--- cudax/examples/stf/08-cub-reduce.cu
+++ cudax/examples/stf/08-cub-reduce.cu
@@ -10,39 +10,65 @@
 
 /**
  * @file
- * @brief Example of reduction implementing using CUB kernels
+ * @brief Example of reduction implementing using CUB
  */
 
-#include <thrust/device_vector.h>
+#include <cub/cub.cuh>
 
 #include <cuda/experimental/stf.cuh>
 
 using namespace cuda::experimental::stf;
 
-template <int BLOCK_THREADS, typename T>
-__global__ void reduce(slice<const T> values, slice<T> partials, size_t nelems)
+template <typename BinaryOp>
+struct OpWrapper
 {
-  using namespace cub;
-  typedef BlockReduce<T, BLOCK_THREADS> BlockReduceT;
+  OpWrapper(BinaryOp _op)
+      : op(mv(_op)) {};
 
-  auto thread_id = BLOCK_THREADS * blockIdx.x + threadIdx.x;
-
-  // Local reduction
-  T local_sum = 0;
-  for (size_t ind = thread_id; ind < nelems; ind += blockDim.x * gridDim.x)
+  template <typename T>
+  __device__ __forceinline__ T operator()(const T& a, const T& b) const
   {
-    local_sum += values(ind);
+    return op(a, b);
   }
 
-  __shared__ typename BlockReduceT::TempStorage temp_storage;
+  BinaryOp op;
+};
 
-  // Per-thread tile data
-  T result = BlockReduceT(temp_storage).Sum(local_sum);
+template <typename OutT, typename Ctx, typename T, typename BinaryOp>
+auto reduce(Ctx& ctx, logical_data<T> data, BinaryOp&& op, OutT init_val)
+{
+  auto result = ctx.logical_data(shape_of<scalar<OutT>>());
 
-  if (threadIdx.x == 0)
-  {
-    partials(blockIdx.x) = result;
-  }
+  // Determine temporary device storage requirements
+  void* d_temp_storage      = nullptr;
+  size_t temp_storage_bytes = 0;
+  cub::DeviceReduce::Reduce(
+    d_temp_storage,
+    temp_storage_bytes,
+    (OutT*) nullptr,
+    (OutT*) nullptr,
+    data.shape().size(),
+    OpWrapper<BinaryOp>(op),
+    init_val,
+    0);
+
+  auto ltemp = ctx.logical_data(shape_of<slice<char>>(temp_storage_bytes));
+
+  ctx.task(data.read(), result.write(), ltemp.write())
+      ->*[&op, init_val](cudaStream_t stream, auto d_data, auto d_result, auto d_temp) {
+            size_t tmp_size = d_temp.size();
+            cub::DeviceReduce::Reduce(
+              (void*) d_temp.data_handle(),
+              tmp_size,
+              (OutT*) d_data.data_handle(),
+              (OutT*) d_result.addr,
+              shape(d_data).size(),
+              OpWrapper<BinaryOp>(op),
+              init_val,
+              stream);
+          };
+
+  return result;
 }
 
 template <typename Ctx>
@@ -50,9 +76,7 @@ void run()
 {
   Ctx ctx;
 
-  const size_t N          = 1024 * 16;
-  const size_t BLOCK_SIZE = 128;
-  const size_t num_blocks = 32;
+  const size_t N = 1024 * 16;
 
   int *X, ref_tot;
 
@@ -65,25 +89,19 @@ void run()
     ref_tot += X[ind];
   }
 
-  auto values   = ctx.logical_data(X, {N});
-  auto partials = ctx.logical_data(shape_of<slice<int>>(num_blocks));
-  auto result   = ctx.logical_data(shape_of<slice<int>>(1));
+  auto values = ctx.logical_data(X, {N});
 
-  ctx.task(values.read(), partials.write(), result.write())->*[&](auto stream, auto values, auto partials, auto result) {
-    // reduce values into partials
-    reduce<BLOCK_SIZE, int><<<num_blocks, BLOCK_SIZE, 0, stream>>>(values, partials, N);
+  // int should be deduced from "values"...
+  auto lresult = reduce<int>(
+    ctx,
+    values,
+    [] __device__(const int& a, const int& b) {
+      return a + b;
+    },
+    0);
 
-    // reduce partials on a single block into result
-    reduce<BLOCK_SIZE, int><<<1, BLOCK_SIZE, 0, stream>>>(partials, result, num_blocks);
-  };
-
-  ctx.host_launch(result.read())->*[&](auto p) {
-    if (p(0) != ref_tot)
-    {
-      fprintf(stderr, "INCORRECT RESULT: p sum = %d, ref tot = %d\n", p(0), ref_tot);
-      abort();
-    }
-  };
+  int result = ctx.wait(lresult);
+  _CCCL_ASSERT(result == ref_tot, "Incorrect result");
 
   ctx.finalize();
 }
@@ -91,5 +109,5 @@ void run()
 int main()
 {
   run<stream_ctx>();
-  run<graph_ctx>();
+  // run<graph_ctx>();
 }
