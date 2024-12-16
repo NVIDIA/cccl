c8bbe43844878f47a1e8650c0eb514120a0a0d4f
Ashwin Srinath <shwina@users.noreply.github.com>
2024-12-16 11:13:15 -0500

    Add copyright

--- python/cuda_parallel/cuda/parallel/experimental/_bindings.py
+++ python/cuda_parallel/cuda/parallel/experimental/_bindings.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import os
 import shutil
 import importlib
