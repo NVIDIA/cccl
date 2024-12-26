1f3a0291dbe13c984961403218a0e22243b773de
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-12-25 20:43:17 -0800

    Modernize cuda_cooperative/pyproject.toml, setup.py

--- python/cuda_cooperative/setup.py
+++ python/cuda_cooperative/setup.py
@@ -3,68 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import os
+from setuptools import setup
+import shutil
 
-from setuptools import setup, find_namespace_packages
-from setuptools.command.build_py import build_py
-from wheel.bdist_wheel import bdist_wheel
-
-
-project_path = os.path.abspath(os.path.dirname(__file__))
-cccl_path = os.path.abspath(os.path.join(project_path, "..", ".."))
-cccl_headers = [["cub", "cub"], ["libcudacxx", "include"], ["thrust", "thrust"]]
-__version__ = None
-with open(os.path.join(project_path, "cuda", "cooperative", "_version.py")) as f:
-    exec(f.read())
-assert __version__ is not None
-ver = __version__
-del __version__
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+CCCL_PATH = os.path.abspath(os.path.join(PROJECT_PATH, "..", ".."))
 
 
-with open("README.md") as f:
-    long_description = f.read()
+def copy_license():
+    src = os.path.abspath(os.path.join(CCCL_PATH, "LICENSE"))
+    dst = os.path.join(PROJECT_PATH, "LICENSE")
+    shutil.copy(src, dst)
 
 
-class CustomBuildCommand(build_py):
-    def run(self):
-        build_py.run(self)
-
-
-class CustomWheelBuild(bdist_wheel):
-    def run(self):
-        super().run()
-
-
-setup(
-    name="cuda-cooperative",
-    version=ver,
-    description="Experimental Core Library for CUDA Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="NVIDIA Corporation",
-    classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "Environment :: GPU :: NVIDIA CUDA",
-    ],
-    packages=find_namespace_packages(include=["cuda.*"]),
-    python_requires=">=3.9",
-    install_requires=[
-        f"cuda-cccl @ file://{cccl_path}/python/cuda_cccl",
-        "numba>=0.60.0",
-        "pynvjitlink-cu12>=0.2.4",
-        "cuda-python",
-        "jinja2",
-    ],
-    extras_require={
-        "test": [
-            "pytest",
-            "pytest-xdist",
-        ]
-    },
-    cmdclass={
-        "build_py": CustomBuildCommand,
-        "bdist_wheel": CustomWheelBuild,
-    },
-    include_package_data=True,
-    license="Apache-2.0 with LLVM exception",
-    license_files=("../../LICENSE",),
-)
+setup()
