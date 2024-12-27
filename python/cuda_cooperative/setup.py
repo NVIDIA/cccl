4a0cca1490e7809ed172a82db920f5d54bad5a09
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-12-26 20:52:28 -0800

    Revert "Convert cuda_cooperative to use hatchling as build backend."
    This reverts commit 61637d608da06fcf6851ef6197f88b5e7dbc3bbe.

---
+++ python/cuda_cooperative/setup.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import os
+from setuptools import setup
+import shutil
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+CCCL_PATH = os.path.abspath(os.path.join(PROJECT_PATH, "..", ".."))
+
+
+def copy_license():
+    src = os.path.abspath(os.path.join(CCCL_PATH, "LICENSE"))
+    dst = os.path.join(PROJECT_PATH, "LICENSE")
+    shutil.copy(src, dst)
+
+
+setup()
