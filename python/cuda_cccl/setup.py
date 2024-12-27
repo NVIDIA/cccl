bda5d51f5acfecdf1b16c3d239165ef05016bb26
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-12-26 22:28:09 -0800

    Further modernize cuda_cccl/setup.py to use pathlib

--- python/cuda_cccl/setup.py
+++ python/cuda_cccl/setup.py
@@ -2,27 +2,33 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from pathlib import Path
 from setuptools import setup, find_namespace_packages
-import os
 import shutil
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-CCCL_PATH = os.path.abspath(os.path.join(PROJECT_PATH, "..", ".."))
+PROJECT_PATH = Path(__file__).resolve().parent
+CCCL_PATH = PROJECT_PATH.parents[1]
 
 
 def copy_cccl_headers_to_cuda_cccl_include():
-    cccl_headers = [["cub", "cub"], ["libcudacxx", "include"], ["thrust", "thrust"]]
-    inc_path = os.path.join(PROJECT_PATH, "cuda", "cccl", "include")
-    os.makedirs(inc_path, exist_ok=True)
+    cccl_headers = [
+        ("cub", "cub"),
+        ("libcudacxx", "include"),
+        ("thrust", "thrust"),
+    ]
+
+    inc_path = PROJECT_PATH / "cuda" / "cccl" / "include"
+    inc_path.mkdir(parents=True, exist_ok=True)
+
     for proj_dir, header_dir in cccl_headers:
-        src_path = os.path.abspath(os.path.join(CCCL_PATH, proj_dir, header_dir))
-        dst_path = os.path.join(inc_path, proj_dir)
-        if os.path.exists(dst_path):
+        src_path = CCCL_PATH / proj_dir / header_dir
+        dst_path = inc_path / proj_dir
+        if dst_path.exists():
             shutil.rmtree(dst_path)
         shutil.copytree(src_path, dst_path)
-    init_py_path = os.path.join(inc_path, "__init__.py")
-    with open(init_py_path, "w") as f:
-        f.write("# Intentionally empty.\n")
+
+    init_py_path = inc_path / "__init__.py"
+    init_py_path.write_text("# Intentionally empty.\n")
 
 
 copy_cccl_headers_to_cuda_cccl_include()
