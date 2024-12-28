9a63830401af0ffa2f26cde3b637a408bb1aaa3a
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-12-27 22:14:43 -0800

    Use pathlib in cuda_parallel/setup.py

--- python/cuda_parallel/setup.py
+++ python/cuda_parallel/setup.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-import os
 from pathlib import Path
 import subprocess
 
@@ -24,22 +23,20 @@ class BuildCMakeExtension(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        extdir = Path(self.get_ext_fullpath(ext.name)).resolve().parent
         cmake_args = [
             "-DCCCL_ENABLE_C=YES",
-            "-DCCCL_C_PARALLEL_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            f"-DCCCL_C_PARALLEL_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             "-DCMAKE_BUILD_TYPE=Release",
         ]
 
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
+        build_temp_path = Path(self.build_temp)
+        build_temp_path.mkdir(parents=True, exist_ok=True)
 
-        subprocess.check_call(
-            ["cmake", str(CCCL_PATH)] + cmake_args, cwd=self.build_temp
-        )
+        subprocess.check_call(["cmake", CCCL_PATH] + cmake_args, cwd=build_temp_path)
         subprocess.check_call(
             ["cmake", "--build", ".", "--target", "cccl.c.parallel"],
-            cwd=self.build_temp,
+            cwd=build_temp_path,
         )
 
 
