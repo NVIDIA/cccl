8d9aeeb4aaa81a1993f1cac9374f4db9b8fe3c08
fbusato <fbusato@nvidia.com>
2025-01-03 22:15:29 +0000

    simplify OS checks

--- libcudacxx/test/libcudacxx/std/cccl/os.compile.pass.cpp
+++ libcudacxx/test/libcudacxx/std/cccl/os.compile.pass.cpp
@@ -11,40 +11,32 @@
 
 #if _CCCL_OS(WINDOWS)
 #  include <windows.h>
-constexpr int windows = 1;
-#else
-constexpr int windows = 0;
 #endif
 
 #if _CCCL_OS(LINUX)
 #  include <unistd.h>
-constexpr int linux_ = 1;
-#else
-constexpr int linux_ = 0;
 #endif
 
 #if _CCCL_OS(ANDROID)
 #  include <android/api-level.h>
-constexpr int android = 1;
 #endif
 
 #if _CCCL_OS(QNX)
 #  include <qnx.h>
-constexpr int qnx = 1;
 #endif
 
 int main(int, char**)
 {
-  static_assert(windows + linux_ == 1, "");
+  static_assert(_CCCL_OS(WINDOWS) + _CCCL_OS(LINUX) == 1, "");
 #if _CCCL_OS(ANDROID) || _CCCL_OS(QNX)
-  static_assert(linux_ == 1, "");
-  static_assert(android + qnx == 1, "");
+  static_assert(_CCCL_OS(LINUX) == 1, "");
+  static_assert(_CCCL_OS(ANDROID) + _CCCL_OS(QNX) == 1, "");
 #endif
 #if _CCCL_OS(LINUX)
-  static_assert(windows == 0, "");
+  static_assert(_CCCL_OS(WINDOWS) == 0, "");
 #endif
 #if _CCCL_OS(WINDOWS)
-  static_assert(android + qnx + linux_ == 0, "");
+  static_assert(_CCCL_OS(ANDROID) + _CCCL_OS(QNX) + _CCCL_OS(LINUX) == 0, "");
 #endif
   return 0;
 }
