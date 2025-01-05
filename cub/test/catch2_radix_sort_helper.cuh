fc71577300f5be356817977f87074f91984699e1
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2025-01-05 02:57:27 -0800

    fixes segment offset generation

--- cub/test/catch2_radix_sort_helper.cuh
+++ cub/test/catch2_radix_sort_helper.cuh
@@ -61,7 +61,7 @@ using all_offset_types =
 template <typename OffsetT, OffsetT Step>
 struct segment_iterator
 {
-  OffsetT last = 0;
+  std::int64_t last = 0;
 
   segment_iterator(std::int64_t last1)
       : last{last1}
