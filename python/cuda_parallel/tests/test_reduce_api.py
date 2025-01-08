5ff1f1921375d0870b2e42f56d3553fa7588a342
Ashwin Srinath <shwina@users.noreply.github.com>
2025-01-08 08:30:12 -0500

    Update documentation

--- python/cuda_parallel/tests/test_reduce_api.py
+++ python/cuda_parallel/tests/test_reduce_api.py
@@ -178,3 +178,35 @@ def test_transform_iterator():
     )
     assert (d_output == expected_output).all()
     # example-end transform-iterator
+
+
+def test_reduce_struct_type():
+    from cuda.parallel.experimental.struct import gpu_struct
+
+    # example-begin reduce-struct
+    @gpu_struct
+    class Pixel:
+        r: np.int32
+        g: np.int32
+        b: np.int32
+
+    def max_g_value(x, y):
+        return x if x.g > y.g else y
+
+    d_rgb = cp.random.randint(0, 256, (10, 3), dtype=np.int32).view(Pixel.dtype)
+    d_out = cp.zeros(1, Pixel.dtype)
+
+    h_init = Pixel(0, 0, 0)
+
+    reduce_into = algorithms.reduce_into(d_rgb, d_out, max_g_value, h_init)
+    temp_storage_bytes = reduce_into(None, d_rgb, d_out, len(d_rgb), h_init)
+
+    d_temp_storage = cp.zeros(temp_storage_bytes, dtype=np.uint8)
+    _ = reduce_into(d_temp_storage, d_rgb, d_out, len(d_rgb), h_init)
+
+    h_rgb = d_rgb.get()
+    expected = h_rgb[h_rgb.view("int32")[:, 1].argmax()]
+
+    np.testing.assert_equal(expected["g"], d_out.get()["g"])
+    # example-end reduce-struct
+
