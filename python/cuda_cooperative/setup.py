79057cf0eda3623e201233c4b401f6e32b0a9b7e
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-01-07 23:58:55 -0800

    Introduce cuda_cooperative/constraints.txt

--- python/cuda_cooperative/setup.py
+++ python/cuda_cooperative/setup.py
@@ -2,20 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from pathlib import Path
-
 from setuptools import setup
-
-CCCL_PYTHON_PATH = Path(__file__).resolve().parents[1]
 
 setup(
     license_files=["../../LICENSE"],
-    install_requires=[
-        f"cuda-cccl @ file://{CCCL_PYTHON_PATH}/cuda_cccl",
-        "numpy",
-        "numba>=0.60.0",
-        "pynvjitlink-cu12>=0.2.4",
-        "cuda-python",
-        "jinja2",
-    ],
 )
