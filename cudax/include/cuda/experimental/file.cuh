e72a672495c8e85f615cf07acd2fbb5d055df3b0
David Bayer <bayer.david99@gmail.com>
2025-01-11 14:58:57 +0100

    top level file

---
+++ cudax/include/cuda/experimental/file.cuh
@@ -0,0 +1,31 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef __CUDAX_FILE__
+#define __CUDAX_FILE__
+
+#include <cuda/__cccl_config>
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+#if !_CCCL_OS(LINUX)
+#  error "<cuda/experimental/file> is only supported on Linux."
+#endif // _CCCL_OS(LINUX)
+
+#include <cuda/experimental/__file/file.cuh>
+#include <cuda/experimental/__file/file_driver.cuh>
+
+#endif // __CUDAX_FILE___
