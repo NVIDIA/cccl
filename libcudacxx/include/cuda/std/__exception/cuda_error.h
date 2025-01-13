a3bd0de482608b5b2f2a7ece3e87e5927c8055d8
Cedric Augonnet <caugonnet@nvidia.com>
2025-01-13 19:53:04 +0100

    Revert "Ensure that cuda::__throw_cuda_error works even on non CUDA compilers"
    This reverts commit 15fe5e06e5670ec477554a53dc7f2a3f4f7b8e80.

--- libcudacxx/include/cuda/std/__exception/cuda_error.h
+++ libcudacxx/include/cuda/std/__exception/cuda_error.h
@@ -40,7 +40,8 @@ _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 /**
  * @brief Exception thrown when a CUDA error is encountered.
  */
-#ifndef _CCCL_NO_EXCEPTIONS
+#if _CCCL_HAS_CUDA_COMPILER
+#  ifndef _CCCL_NO_EXCEPTIONS
 class cuda_error : public ::std::runtime_error
 {
 private:
@@ -63,15 +64,11 @@ public:
 
 _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(::cudaError_t __status, const char* __msg)
 {
-#  if _CCCL_HAS_CUDA_COMPILER
   NV_IF_ELSE_TARGET(NV_IS_HOST,
                     (throw ::cuda::cuda_error(__status, __msg);),
                     ((void) __status; (void) __msg; _CUDA_VSTD_NOVERSION::terminate();))
-#  else
-  throw ::cuda::cuda_error(__status, __msg);
-#  endif // _CCCL_CUDA_COMPILER
 }
-#else // ^^^ !_CCCL_NO_EXCEPTIONS ^^^ / vvv _CCCL_NO_EXCEPTIONS vvv
+#  else // ^^^ !_CCCL_NO_EXCEPTIONS ^^^ / vvv _CCCL_NO_EXCEPTIONS vvv
 class cuda_error
 {
 public:
@@ -82,7 +79,8 @@ _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(::cudaError_t, 
 {
   _CUDA_VSTD_NOVERSION::terminate();
 }
-#endif // _CCCL_NO_EXCEPTIONS
+#  endif // _CCCL_NO_EXCEPTIONS
+#endif // _CCCL_CUDA_COMPILER
 
 _LIBCUDACXX_END_NAMESPACE_CUDA
 
