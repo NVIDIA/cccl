8ea0fc9a70a1d175b4c2baa960b5bb104c7a5010
Ashwin Srinath <shwina@users.noreply.github.com>
2025-01-14 14:25:27 -0500

    Bump copyrights

--- python/cuda_parallel/cuda/parallel/experimental/struct.py
+++ python/cuda_parallel/cuda/parallel/experimental/struct.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from dataclasses import dataclass
 from dataclasses import fields as dataclass_fields
 from typing import Type
--- python/cuda_parallel/cuda/parallel/experimental/typing.py
+++ python/cuda_parallel/cuda/parallel/experimental/typing.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from typing import Any
 
 from typing_extensions import (
