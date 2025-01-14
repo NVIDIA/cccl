2dec37985bce18760bee599735976496a22722af
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2025-01-14 16:28:47 -0500

    cuda.parallel: Support structured types as algorithm inputs (#3218)
    * Introduce gpu_struct decorator and typing
    
    * Enable `reduce` to accept arrays of structs as inputs
    
    * Add test for reducing arrays-of-struct
    
    * Update documentation
    
    * Use a numpy array rather than ctypes object
    
    * Change zeros -> empty for output array and temp storage
    
    * Add a TODO for typing GpuStruct
    
    * Documentation udpates
    
    * Remove test_reduce_struct_type from test_reduce.py
    
    * Revert to `to_cccl_value()` accepting ndarray + GpuStruct
    
    * Bump copyrights
    
    ---------
    
    Co-authored-by: Ashwin Srinath <shwina@users.noreply.github.com>

--- python/cuda_parallel/cuda/parallel/experimental/_utils/cai.py
+++ python/cuda_parallel/cuda/parallel/experimental/_utils/cai.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
 #
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
@@ -15,7 +15,14 @@ from ..typing import DeviceArrayLike
 
 
 def get_dtype(arr: DeviceArrayLike) -> np.dtype:
-    return np.dtype(arr.__cuda_array_interface__["typestr"])
+    typestr = arr.__cuda_array_interface__["typestr"]
+
+    if typestr.startswith("|V"):
+        # it's a structured dtype, use the descr field:
+        return np.dtype(arr.__cuda_array_interface__["descr"])
+    else:
+        # a simple dtype, use the typestr field:
+        return np.dtype(typestr)
 
 
 def get_strides(arr: DeviceArrayLike) -> Optional[Tuple]:
