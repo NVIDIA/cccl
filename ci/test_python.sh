d97a68a2049d9862c79d28a30d794dc35fae3cd2
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-01-15 21:01:47 -0800

    Use for loop in test_python.sh to avoid code duplication.

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -12,34 +12,24 @@ begin_group "⚙️ Existing site-packages"
 pip freeze
 end_group "⚙️ Existing site-packages"
 
-pushd ../python/cuda_cooperative >/dev/null
+for module in cuda_parallel cuda_cooperative; do
 
-rm -rf /tmp/cuda_cooperative_venv
-python -m venv /tmp/cuda_cooperative_venv
-. /tmp/cuda_cooperative_venv/bin/activate
-echo 'cuda-cccl @ file:///home/coder/cccl/python/cuda_cccl' > /tmp/cuda-cccl_constraints.txt
-run_command "⚙️  Pip install cuda_cooperative" pip install -c /tmp/cuda-cccl_constraints.txt .[test]
-begin_group "⚙️ cuda-cooperative site-packages"
-pip freeze
-end_group "⚙️ cuda-cooperative site-packages"
-run_command "🚀  Pytest cuda_cooperative" python -m pytest -v ./tests
-deactivate
+  pushd "../python/${module}" >/dev/null
 
-popd >/dev/null
+  TEMP_VENV_DIR="/tmp/${module}_venv"
+  rm -rf "${TEMP_VENV_DIR}"
+  python -m venv "${TEMP_VENV_DIR}"
+  . "${TEMP_VENV_DIR}/bin/activate"
+  echo 'cuda-cccl @ file:///home/coder/cccl/python/cuda_cccl' > /tmp/cuda-cccl_constraints.txt
+  run_command "⚙️  Pip install ${module}" pip install -c /tmp/cuda-cccl_constraints.txt .[test]
+  begin_group "⚙️ ${module} site-packages"
+  pip freeze
+  end_group "⚙️ ${module} site-packages"
+  run_command "🚀  Pytest ${module}" python -m pytest -v ./tests
+  deactivate
 
-pushd ../python/cuda_parallel >/dev/null
+  popd >/dev/null
 
-rm -rf /tmp/cuda_parallel_venv
-python -m venv /tmp/cuda_parallel_venv
-. /tmp/cuda_parallel_venv/bin/activate
-echo 'cuda-cccl @ file:///home/coder/cccl/python/cuda_cccl' > /tmp/cuda-cccl_constraints.txt
-run_command "⚙️  Pip install cuda_parallel" pip install -c /tmp/cuda-cccl_constraints.txt .[test]
-begin_group "⚙️ cuda-parallel site-packages"
-pip freeze
-end_group "⚙️ cuda-parallel site-packages"
-run_command "🚀  Pytest cuda_parallel" python -m pytest -v ./tests
-deactivate
-
-popd >/dev/null
+done
 
 print_time_summary
