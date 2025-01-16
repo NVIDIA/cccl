12664e3597a077eec5f95a0ba1bd7edaafc3cf3c
fbusato <fbusato@nvidia.com>
2025-01-15 17:19:10 -0800

    fix missing preprocessing paths

--- libcudacxx/include/cuda/std/__cccl/128bit_types.h
+++ libcudacxx/include/cuda/std/__cccl/128bit_types.h
@@ -15,15 +15,17 @@
 #include <cuda/std/__cccl/os.h>
 
 #ifndef _CCCL_DISABLE_INT128
-#  if defined(__SIZEOF_INT128__)
-#    if _CCCL_COMPILER(NVRTC) && defined(__CUDACC_RTC_INT128__)
+#  if defined(__SIZEOF_INT128__) // defined(__SIZEOF_INT128__) vvvv
+#    if _CCCL_COMPILER(NVRTC) && defined(__CUDACC_RTC_INT128__) && !_CCCL_OS(WINDOWS)
 #      define _CCCL_HAS_INT128() 1
 #    elif (_CCCL_COMPILER(GCC) || _CCCL_COMPILER(CLANG) || _CCCL_COMPILER(NVHPC)) && !_CCCL_OS(WINDOWS)
 #      define _CCCL_HAS_INT128() 1
 #    else
 #      define _CCCL_HAS_INT128() 0
 #    endif
-#  endif // defined(__SIZEOF_INT128__)
+#  else // defined(__SIZEOF_INT128__) ^^^^ / !defined(__SIZEOF_INT128__) vvvv
+#    define _CCCL_HAS_INT128() 0
+#  endif // !defined(__SIZEOF_INT128__) ^^^^
 #else
 #  define _CCCL_HAS_INT128() 0
 #endif // #ifndef _CCCL_DISABLE_INT128
@@ -35,6 +37,8 @@
 #  else
 #    define _CCCL_HAS_FLOAT128() 0
 #  endif
+#else
+#  define _CCCL_HAS_INT128() 0
 #endif // #ifndef _CCCL_DISABLE_FLOAT128
 
 #endif // __CCCL_128BIT_TYPES_H
