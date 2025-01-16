d449bdfe3e299a752f3d144671374f5ced71c85a
Michael Schellenberger Costa <miscco@nvidia.com>
2025-01-16 14:06:21 +0100

    Add escape hatch
    Co-authored-by: Bernhard Manfred Gruber <bernhardmgruber@gmail.com>

--- libcudacxx/include/cuda/std/__internal/cpp_dialect.h
+++ libcudacxx/include/cuda/std/__internal/cpp_dialect.h
@@ -35,8 +35,10 @@
       future release. Define CCCL_IGNORE_DEPRECATED_CPP_DIALECT to suppress this message.)
 // clang-format on
 
-#if _CCCL_STD_VER < 2017
-#  error libcu++ requires C++17 or later.
-#endif // _CCCL_STD_VER < 2017
+#  ifndef CCCL_IGNORE_DEPRECATED_CPP_DIALECT
+#    if _CCCL_STD_VER < 2017
+#  error libcu++ requires at least C++ 17. Define CCCL_IGNORE_DEPRECATED_CPP_DIALECT to suppress this message.
+#    endif // _CCCL_STD_VER >= 2017
+#  endif // CCCL_IGNORE_DEPRECATED_CPP_DIALECT
 
 #endif // _LIBCUDACXX___INTERNAL_CPP_DIALECT_H
