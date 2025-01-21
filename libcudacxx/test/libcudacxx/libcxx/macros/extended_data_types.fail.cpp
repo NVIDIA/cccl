ac4ecc512513045f85da90700d9a2e6006a45939
fbusato <fbusato@nvidia.com>
2025-01-21 15:36:27 -0800

    unify extended data type tests

--- libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.fail.cpp
+++ libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.fail.cpp
@@ -6,9 +6,19 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
-#include <cuda/std/__cccl/128bit_types.h>
+#include <cuda/std/__cccl/extended_data_types.h>
 
 #include "test_macros.h"
+
+#if !_CCCL_HAS_NVFP8()
+#  include <cuda_fp8.h>
+#endif
+#if !defined(_CCCL_HAS_NVFP16)
+#  include <cuda_fp16.h>
+#endif
+#if !defined(_CCCL_HAS_NVBF16)
+#  include <cuda_bf16.h>
+#endif
 
 int main(int, char**)
 {
@@ -20,9 +30,27 @@ int main(int, char**)
 #else
   static_assert(false);
 #endif
+#if !_CCCL_HAS_NVFP8()
+  auto x1 = __nv_fp8_e4m3(1.0f);
+  unused(x1);
+#else
+  static_assert(false);
+#endif
+#if !defined(_CCCL_HAS_NVFP16)
+  auto x2 = __half(1.0f);
+  unused(x2);
+#else
+  static_assert(false);
+#endif
+#if !defined(_CCCL_HAS_NVBF16)
+  auto x3 = __nv_bfloat16(1.0f);
+  unused(x3);
+#else
+  static_assert(false);
+#endif
 #if !_CCCL_HAS_FLOAT128() && !_CCCL_COMPILER(NVRTC)
-  auto z = __float128(3.14) + __float128(3.14);
-  unused(z);
+  auto x4 = __float128(3.14) + __float128(3.14);
+  unused(x4);
 #else
   static_assert(false);
 #endif
--- libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.pass.cpp
+++ libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.pass.cpp
@@ -6,39 +6,43 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
-#include <cuda/std/__cccl/extended_floating_point.h>
+#include <cuda/std/__cccl/extended_data_types.h>
 
 #include "test_macros.h"
 
-#if !_CCCL_HAS_NVFP8()
+#if _CCCL_HAS_NVFP8()
 #  include <cuda_fp8.h>
 #endif
-#if !defined(_CCCL_HAS_NVFP16)
+#if defined(_CCCL_HAS_NVFP16)
 #  include <cuda_fp16.h>
 #endif
-#if !defined(_CCCL_HAS_NVBF16)
+#if defined(_CCCL_HAS_NVBF16)
 #  include <cuda_bf16.h>
 #endif
 
 int main(int, char**)
 {
-#if !_CCCL_HAS_NVFP8()
-  auto x = __nv_fp8_e4m3(1.0f);
-  unused(x);
-#else
-  static_assert(false);
+#if _CCCL_HAS_INT128()
+  auto x1 = __int128(123456789123) + __int128(123456789123);
+  auto y1 = __uint128_t(123456789123) + __uint128_t(123456789123);
+  unused(x1);
+  unused(y1);
 #endif
-#if !defined(_CCCL_HAS_NVFP16)
-  auto y = __half(1.0f);
-  unused(y);
-#else
-  static_assert(false);
+#if _CCCL_HAS_NVFP8()
+  auto x2 = __nv_fp8_e4m3(1.0f);
+  unused(x2);
 #endif
-#if !defined(_CCCL_HAS_NVBF16)
-  auto z = __nv_bfloat16(1.0f);
-  unused(z);
-#else
-  static_assert(false);
+#if defined(_CCCL_HAS_NVFP16)
+  auto x3 = __half(1.0f);
+  unused(x3);
+#endif
+#if defined(_CCCL_HAS_NVBF16)
+  auto x4 = __nv_bfloat16(1.0f);
+  unused(x4);
+#endif
+#if _CCCL_HAS_FLOAT128()
+  auto x5 = __float128(3.14) + __float128(3.14);
+  unused(x5);
 #endif
   return 0;
 }
