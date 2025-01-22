508abe3ca6ccd4e329a05ba93ff1dd5847dc80ea
Michael Schellenberger Costa <miscco@nvidia.com>
2025-01-22 13:43:13 +0100

    Ensure that we actually can enable FP8 due to FP16 and BF16 requirements

--- libcudacxx/include/cuda/std/__cccl/extended_floating_point.h
+++ libcudacxx/include/cuda/std/__cccl/extended_floating_point.h
@@ -25,16 +25,6 @@
 #include <cuda/std/__cccl/diagnostic.h>
 #include <cuda/std/__cccl/preprocessor.h>
 
-#if !defined(_CCCL_DISABLE_NVFP8_SUPPORT)
-#  if _CCCL_HAS_INCLUDE(<cuda_fp8.h>)
-#    define _CCCL_HAS_NVFP8() 1
-#  else
-#    define _CCCL_HAS_NVFP8() 0
-#  endif // _CCCL_HAS_INCLUDE(<cuda_fp8.h>)
-#else
-#  define _CCCL_HAS_NVFP8() 0
-#endif // !defined(_CCCL_DISABLE_NVFP8_SUPPORT)
-
 #if !defined(_CCCL_HAS_NVFP16)
 #  if _CCCL_HAS_INCLUDE(<cuda_fp16.h>) && (_CCCL_HAS_CUDA_COMPILER || defined(LIBCUDACXX_ENABLE_HOST_NVFP16)) \
                         && !defined(CCCL_DISABLE_FP16_SUPPORT)
@@ -48,5 +38,15 @@
 #    define _CCCL_HAS_NVBF16 1
 #  endif
 #endif // !_CCCL_HAS_NVBF16
+
+#if !defined(_CCCL_DISABLE_NVFP8_SUPPORT)
+#  if _CCCL_HAS_INCLUDE(<cuda_fp8.h>) && defined(_CCCL_HAS_NVFP16) && defined(_CCCL_HAS_NVBF16)
+#    define _CCCL_HAS_NVFP8() 1
+#  else
+#    define _CCCL_HAS_NVFP8() 0
+#  endif // _CCCL_HAS_INCLUDE(<cuda_fp8.h>)
+#else
+#  define _CCCL_HAS_NVFP8() 0
+#endif // !defined(_CCCL_DISABLE_NVFP8_SUPPORT)
 
 #endif // __CCCL_EXTENDED_FLOATING_POINT_H
