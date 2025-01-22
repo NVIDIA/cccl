a21ec9a8b7d22f400cfd4a39ddd81a47fb23979f
fbusato <fbusato@nvidia.com>
2025-01-21 17:29:38 -0800

    __SIZEOF_INT128__ not defined with NVRTC

--- libcudacxx/include/cuda/std/__cccl/extended_data_types.h
+++ libcudacxx/include/cuda/std/__cccl/extended_data_types.h
@@ -27,17 +27,14 @@
 #include <cuda/std/__cccl/preprocessor.h>
 
 #if !defined(_CCCL_DISABLE_INT128)
-#  if defined(__SIZEOF_INT128__) // defined(__SIZEOF_INT128__) vvvv
-#    if _CCCL_COMPILER(NVRTC) && defined(__CUDACC_RTC_INT128__) && _CCCL_OS(LINUX)
-#      define _CCCL_HAS_INT128() 1
-#    elif (_CCCL_COMPILER(GCC) || _CCCL_COMPILER(CLANG) || _CCCL_COMPILER(NVHPC)) && _CCCL_OS(LINUX)
-#      define _CCCL_HAS_INT128() 1
-#    else
-#      define _CCCL_HAS_INT128() 0
-#    endif
-#  else // defined(__SIZEOF_INT128__) ^^^^ / !defined(__SIZEOF_INT128__) vvvv
+#  if _CCCL_COMPILER(NVRTC) && defined(__CUDACC_RTC_INT128__) && _CCCL_OS(LINUX)
+#    define _CCCL_HAS_INT128() 1
+#  elif defined(__SIZEOF_INT128__) && (_CCCL_COMPILER(GCC) || _CCCL_COMPILER(CLANG) || _CCCL_COMPILER(NVHPC)) \
+    && _CCCL_OS(LINUX)
+#    define _CCCL_HAS_INT128() 1
+#  else
 #    define _CCCL_HAS_INT128() 0
-#  endif // !defined(__SIZEOF_INT128__) ^^^^
+#  endif
 #else
 #  define _CCCL_HAS_INT128() 0
 #endif // #if !defined(_CCCL_DISABLE_INT128)
