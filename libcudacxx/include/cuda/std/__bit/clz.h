f4836d28ad4bbce695867c35ca5e27b47ff08ac0
fbusato <fbusato@nvidia.com>
2025-01-22 17:06:13 -0800

    fix _BitScanReverse

--- libcudacxx/include/cuda/std/__bit/clz.h
+++ libcudacxx/include/cuda/std/__bit/clz.h
@@ -54,7 +54,7 @@ _LIBCUDACXX_HIDE_FROM_ABI int __msvc_runtime_clz(_Tp __x) noexcept
   constexpr auto __digits = numeric_limits<_Tp>::digits;
   unsigned long __where;
   auto __res = sizeof(_Tp) == sizeof(uint32_t)
-               ? _BitScanReverse32(&__where, static_cast<uint32_t>(__x))
+               ? _BitScanReverse(&__where, static_cast<uint32_t>(__x))
                : _BitScanReverse64(&__where, static_cast<uint64_t>(__x));
   return (__res) ? __digits - 1 - static_cast<int>(__where) : __digits;
 }
