6bb30c623ba52ad90ac16e710e2e3271b14bd450
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2025-01-26 11:36:05 -0800

    adds benchmarks for merge keys

--- cub/benchmarks/bench/merge/keys.cu
+++ cub/benchmarks/bench/merge/keys.cu
@@ -21,11 +21,11 @@
 // %RANGE% TUNE_ITEMS_PER_THREAD ipt 7:24:1
 // %RANGE% TUNE_THREADS_PER_BLOCK_POW2 tpb 6:10:1
 
-template <typename KeyT, typename ValueT, typename OffsetT>
-void pairs(nvbench::state& state, nvbench::type_list<KeyT, ValueT, OffsetT>)
+template <typename KeyT, typename OffsetT>
+void keys(nvbench::state& state, nvbench::type_list<KeyT, OffsetT>)
 {
   using key_t            = KeyT;
-  using value_t          = ValueT;
+  using value_t          = cub::NullType;
   using key_input_it_t   = key_t*;
   using value_input_it_t = value_t*;
   using key_it_t         = key_t*;
@@ -83,9 +83,6 @@ void pairs(nvbench::state& state, nvbench::type_list<KeyT, ValueT, OffsetT>)
   thrust::device_vector<key_t> keys_lhs(num_items_lhs);
   thrust::device_vector<key_t> keys_rhs(num_items_rhs);
   thrust::device_vector<key_t> keys_out(elements);
-  thrust::device_vector<value_t> values_lhs(num_items_lhs);
-  thrust::device_vector<value_t> values_rhs(num_items_rhs);
-  thrust::device_vector<value_t> values_out(elements);
 
   // Generate increasing input range to sample from
   thrust::device_vector<key_t> increasing_input = generate(elements);
@@ -110,19 +107,14 @@ void pairs(nvbench::state& state, nvbench::type_list<KeyT, ValueT, OffsetT>)
   // From pivot point copy all remaining items to rhs
   thrust::copy(increasing_input.cbegin() + pivot_point_val, increasing_input.cbegin() + elements, end_rhs);
 
-  key_t* d_keys_lhs     = thrust::raw_pointer_cast(keys_lhs.data());
-  key_t* d_keys_rhs     = thrust::raw_pointer_cast(keys_rhs.data());
-  key_t* d_keys_out     = thrust::raw_pointer_cast(keys_out.data());
-  value_t* d_values_lhs = thrust::raw_pointer_cast(values_lhs.data());
-  value_t* d_values_rhs = thrust::raw_pointer_cast(values_rhs.data());
-  value_t* d_values_out = thrust::raw_pointer_cast(values_out.data());
+  key_t* d_keys_lhs = thrust::raw_pointer_cast(keys_lhs.data());
+  key_t* d_keys_rhs = thrust::raw_pointer_cast(keys_rhs.data());
+  key_t* d_keys_out = thrust::raw_pointer_cast(keys_out.data());
 
   // Enable throughput calculations and add "Size" column to results.
   state.add_element_count(elements);
   state.add_global_memory_reads<KeyT>(elements);
-  state.add_global_memory_reads<ValueT>(elements);
   state.add_global_memory_writes<KeyT>(elements);
-  state.add_global_memory_writes<ValueT>(elements);
 
   // Allocate temporary storage:
   std::size_t temp_size{};
@@ -130,13 +122,13 @@ void pairs(nvbench::state& state, nvbench::type_list<KeyT, ValueT, OffsetT>)
     nullptr,
     temp_size,
     d_keys_lhs,
-    d_values_lhs,
+    nullptr,
     num_items_lhs,
     d_keys_rhs,
-    d_values_rhs,
+    nullptr,
     num_items_rhs,
     d_keys_out,
-    d_values_out,
+    nullptr,
     compare_op_t{},
     cudaStream_t{});
 
@@ -148,13 +140,13 @@ void pairs(nvbench::state& state, nvbench::type_list<KeyT, ValueT, OffsetT>)
       temp_storage,
       temp_size,
       d_keys_lhs,
-      d_values_lhs,
+      nullptr,
       num_items_lhs,
       d_keys_rhs,
-      d_values_rhs,
+      nullptr,
       num_items_rhs,
       d_keys_out,
-      d_values_out,
+      nullptr,
       compare_op_t{},
       launch.get_stream());
   });
@@ -166,20 +158,8 @@ using key_types = nvbench::type_list<TUNE_KeyT>;
 using key_types = fundamental_types;
 #endif // TUNE_KeyT
 
-#ifdef TUNE_ValueT
-using value_types = nvbench::type_list<TUNE_ValueT>;
-#else // !defined(TUNE_ValueT)
-using value_types = nvbench::type_list<int8_t, int16_t, int32_t, int64_t
-#  if NVBENCH_HELPER_HAS_I128
-// nvcc currently hangs for __int128 value type with the fallback policy of {CTA: 64, IPT: 1}. NVBug 4384075
-//  ,
-//  int128_t
-#  endif
-                                       >;
-#endif // TUNE_ValueT
-
-NVBENCH_BENCH_TYPES(pairs, NVBENCH_TYPE_AXES(key_types, value_types, offset_types))
+NVBENCH_BENCH_TYPES(keys, NVBENCH_TYPE_AXES(key_types, offset_types))
   .set_name("base")
-  .set_type_axes_names({"KeyT{ct}", "ValueT{ct}", "OffsetT{ct}"})
+  .set_type_axes_names({"KeyT{ct}", "OffsetT{ct}"})
   .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4))
   .add_string_axis("Entropy", {"1.000", "0.201"});
