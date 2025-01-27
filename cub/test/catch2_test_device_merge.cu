8455c0166b54f3318d91097bcff25e70ce1854c8
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2025-01-27 07:41:18 -0800

    re-enable vsmem tests

--- cub/test/catch2_test_device_merge.cu
+++ cub/test/catch2_test_device_merge.cu
@@ -77,57 +77,56 @@ struct fallback_test_policy_hub
 };
 
 // TODO(bgruber): This test alone increases compile time from 1m16s to 8m43s. What's going on?
-// C2H_TEST("DeviceMerge::MergeKeys large key types", "[merge][device]", c2h::type_list<large_type_vsmem,
-// large_type_fallb>)
-// {
-//   using key_t    = c2h::get<0, TestType>;
-//   using offset_t = int;
+C2H_TEST("DeviceMerge::MergeKeys large key types", "[merge][device]", c2h::type_list<large_type_vsmem, large_type_fallb>)
+{
+  using key_t    = c2h::get<0, TestType>;
+  using offset_t = int;
 
-//   constexpr auto agent_sm = sizeof(key_t) * 128 * 7;
-//   constexpr auto fallback_sm =
-//     sizeof(key_t) * cub::detail::merge::fallback_BLOCK_THREADS * cub::detail::merge::fallback_ITEMS_PER_THREAD;
-//   static_assert(agent_sm > cub::detail::max_smem_per_block,
-//                 "key_t is not big enough to exceed SM and trigger fallback policy");
-//   static_assert(
-//     ::cuda::std::is_same<key_t, large_type_fallb>::value == (fallback_sm <= cub::detail::max_smem_per_block),
-//     "SM consumption by fallback policy should fit into max_smem_per_block");
+  constexpr auto agent_sm = sizeof(key_t) * 128 * 7;
+  constexpr auto fallback_sm =
+    sizeof(key_t) * cub::detail::merge::fallback_BLOCK_THREADS * cub::detail::merge::fallback_ITEMS_PER_THREAD;
+  static_assert(agent_sm > cub::detail::max_smem_per_block,
+                "key_t is not big enough to exceed SM and trigger fallback policy");
+  static_assert(
+    ::cuda::std::is_same<key_t, large_type_fallb>::value == (fallback_sm <= cub::detail::max_smem_per_block),
+    "SM consumption by fallback policy should fit into max_smem_per_block");
 
-//   test_keys<key_t, offset_t>(
-//     3623,
-//     6346,
-//     ::cuda::std::less<key_t>{},
-//     [](const key_t* k1, offset_t s1, const key_t* k2, offset_t s2, key_t* r, ::cuda::std::less<key_t> co) {
-//       using dispatch_t = cub::detail::merge::dispatch_t<
-//         const key_t*,
-//         const cub::NullType*,
-//         const key_t*,
-//         const cub::NullType*,
-//         key_t*,
-//         cub::NullType*,
-//         offset_t,
-//         ::cuda::std::less<key_t>,
-//         fallback_test_policy_hub>; // use a fixed policy for this test so the needed shared memory is deterministic
+  test_keys<key_t, offset_t>(
+    3623,
+    6346,
+    ::cuda::std::less<key_t>{},
+    [](const key_t* k1, offset_t s1, const key_t* k2, offset_t s2, key_t* r, ::cuda::std::less<key_t> co) {
+      using dispatch_t = cub::detail::merge::dispatch_t<
+        const key_t*,
+        const cub::NullType*,
+        const key_t*,
+        const cub::NullType*,
+        key_t*,
+        cub::NullType*,
+        offset_t,
+        ::cuda::std::less<key_t>,
+        fallback_test_policy_hub>; // use a fixed policy for this test so the needed shared memory is deterministic
 
-//       std::size_t temp_storage_bytes = 0;
-//       dispatch_t::dispatch(
-//         nullptr, temp_storage_bytes, k1, nullptr, s1, k2, nullptr, s2, r, nullptr, co, cudaStream_t{0});
+      std::size_t temp_storage_bytes = 0;
+      dispatch_t::dispatch(
+        nullptr, temp_storage_bytes, k1, nullptr, s1, k2, nullptr, s2, r, nullptr, co, cudaStream_t{0});
 
-//       c2h::device_vector<char> temp_storage(temp_storage_bytes);
-//       dispatch_t::dispatch(
-//         thrust::raw_pointer_cast(temp_storage.data()),
-//         temp_storage_bytes,
-//         k1,
-//         nullptr,
-//         s1,
-//         k2,
-//         nullptr,
-//         s2,
-//         r,
-//         nullptr,
-//         co,
-//         cudaStream_t{0});
-//     });
-// }
+      c2h::device_vector<char> temp_storage(temp_storage_bytes);
+      dispatch_t::dispatch(
+        thrust::raw_pointer_cast(temp_storage.data()),
+        temp_storage_bytes,
+        k1,
+        nullptr,
+        s1,
+        k2,
+        nullptr,
+        s2,
+        r,
+        nullptr,
+        co,
+        cudaStream_t{0});
+    });
+}
 
 C2H_TEST("DeviceMerge::MergeKeys offset types", "[merge][device]")
 
