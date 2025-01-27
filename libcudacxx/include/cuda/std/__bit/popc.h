0e2c0b453a17ccf266f7e15a4260e78bb976bd25
fbusato <fbusato@nvidia.com>
2025-01-27 11:13:39 -0800

    simplify popc

--- libcudacxx/include/cuda/std/__bit/popc.h
+++ libcudacxx/include/cuda/std/__bit/popc.h
@@ -36,30 +36,32 @@ _LIBCUDACXX_BEGIN_NAMESPACE_STD
 // optimized version of runtime popc that is used in device code.
 _LIBCUDACXX_HIDE_FROM_ABI constexpr int __constexpr_popc_32bit(uint32_t __x) noexcept
 {
+#if defined(_CCCL_BUILTIN_POPC)
+  return _CCCL_BUILTIN_POPC(__x);
+#else
   __x = __x - ((__x >> 1) & 0x55555555);
   __x = (__x & 0x33333333) + ((__x >> 2) & 0x33333333);
   return ((__x + (__x >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+#endif // _CCCL_BUILTIN_POPC
 }
 
 template <typename _Tp>
 _LIBCUDACXX_HIDE_FROM_ABI constexpr int __constexpr_popc(_Tp __x) noexcept
 {
   static_assert(is_same_v<_Tp, uint32_t> || is_same_v<_Tp, uint64_t>);
-#if defined(_CCCL_BUILTIN_POPC)
-  return sizeof(_Tp) == sizeof(uint32_t)
-         ? _CCCL_BUILTIN_POPC(static_cast<uint32_t>(__x))
-         : _CCCL_BUILTIN_POPCLL(static_cast<uint64_t>(__x));
-#else
   if constexpr (is_same_v<_Tp, uint32_t>)
   {
     return __constexpr_popc_32bit(__x);
   }
   else
   {
+#if defined(_CCCL_BUILTIN_POPCLL)
+    return _CCCL_BUILTIN_POPCLL(__x);
+#else
     return __constexpr_popc_32bit(static_cast<uint32_t>(__x))
          + __constexpr_popc_32bit(static_cast<uint32_t>(__x >> 32));
+#endif // defined(_CCCL_BUILTIN_POPCLL)
   }
-#endif
 }
 
 template <typename _Tp>
@@ -77,9 +79,7 @@ _LIBCUDACXX_HIDE_FROM_ABI int __host_runtime_popc(_Tp __x) noexcept
 #  endif // !defined(_M_ARM64)
   return static_cast<int>(__ret);
 #else
-  return sizeof(_Tp) == sizeof(uint32_t)
-         ? _CCCL_BUILTIN_POPCOUNT(static_cast<uint32_t>(__x))
-         : _CCCL_BUILTIN_POPCOUNTLL(static_cast<uint64_t>(__x));
+  return __constexpr_popc(__x);
 #endif
 }
 
