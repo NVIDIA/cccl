cf3694ef42bc02ebc147ec9efb21ecc8607e0a47
Cedric Augonnet <caugonnet@nvidia.com>
2025-01-29 08:38:50 +0100

    Add a test to see if we can access stackable logical data produced in a nested context

---
+++ cudax/test/stf/local_stf/stackable_export.cu
@@ -0,0 +1,63 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDASTF in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2022-2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+/**
+ * @file
+ *
+ * @brief Experiment to see if we can read data generated in a nested context
+ *
+ */
+
+#include <cuda/experimental/stf.cuh>
+
+#include "cuda/experimental/__stf/utility/stackable_ctx.cuh"
+
+using namespace cuda::experimental::stf;
+
+int X0(int i)
+{
+  return 17 * i + 45;
+}
+
+int main()
+{
+  stackable_ctx sctx;
+
+  auto lA = sctx.logical_data(shape_of<slice<int>>(512));
+  lA.set_symbol("A");
+
+  sctx.parallel_for(lA.shape(), lA.write())->*[] __device__(size_t i, auto a) {
+    a(i) = 42 + 2 * i;
+  };
+
+  /* Start to use a graph */
+  sctx.push();
+
+  lA.push(access_mode::read);
+
+  auto lB = sctx.logical_data(lA.shape());
+  lB.set_symbol("B");
+
+  sctx.parallel_for(lB.shape(), lB.write(), lA.read())->*[] __device__(size_t i, auto b, auto a) {
+    b(i) = 17 - 3 * a(i);
+  };
+
+  sctx.pop();
+
+  /* Access B in a context below the context where it was created */
+  sctx.host_launch(lB.read())->*[](auto b) {
+    for (size_t i = 0; i < b.size(); i++)
+    {
+      EXPECT(b(i) == (17 - 3*(42 + 2*i)));
+    }
+  };
+
+  sctx.finalize();
+}
