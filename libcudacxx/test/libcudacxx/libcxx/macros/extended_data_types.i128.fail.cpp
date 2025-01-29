09b12009d906bdb69f9da60de5196991d0610f9e
Federico Busato <50413820+fbusato@users.noreply.github.com>
2025-01-29 11:39:14 -0800

    Add `__int128` and `__float128` detection macros (#3413)

--- libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.i128.fail.cpp
+++ libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.i128.fail.cpp
@@ -6,37 +6,17 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
-#include <cuda/std/__cccl/extended_floating_point.h>
+#include <cuda/std/__cccl/extended_data_types.h>
 
 #include "test_macros.h"
 
-#if !_CCCL_HAS_NVFP8()
-#  include <cuda_fp8.h>
-#endif
-#if !defined(_CCCL_HAS_NVFP16)
-#  include <cuda_fp16.h>
-#endif
-#if !defined(_CCCL_HAS_NVBF16)
-#  include <cuda_bf16.h>
-#endif
-
 int main(int, char**)
 {
-#if !_CCCL_HAS_NVFP8()
-  auto x = __nv_fp8_e4m3(1.0f);
+#if !_CCCL_HAS_INT128()
+  __int128 x    = __int128(123456789123) + __int128(123456789123);
+  __uint128_t y = __uint128_t(123456789123) + __uint128_t(123456789123);
   unused(x);
-#else
-  static_assert(false);
-#endif
-#if !defined(_CCCL_HAS_NVFP16)
-  auto y = __half(1.0f);
   unused(y);
-#else
-  static_assert(false);
-#endif
-#if !defined(_CCCL_HAS_NVBF16)
-  auto z = __nv_bfloat16(1.0f);
-  unused(z);
 #else
   static_assert(false);
 #endif
