738bd6676bc5327d9df28811f83663fd774fbf13
Federico Busato <50413820+fbusato@users.noreply.github.com>
2025-01-29 09:07:15 -0800

    Update libcudacxx/include/cuda/std/__cccl/extended_data_types.h
    Co-authored-by: Bernhard Manfred Gruber <bernhardmgruber@gmail.com>

--- libcudacxx/include/cuda/std/__cccl/extended_data_types.h
+++ libcudacxx/include/cuda/std/__cccl/extended_data_types.h
@@ -57,7 +57,7 @@
 #    define _CCCL_HAS_NVFP8() 1
 #  else
 #    define _CCCL_HAS_NVFP8() 0
-#  endif // _CCCL_CUDACC_AT_LEAST(11, 8) && _CCCL_HAS_INCLUDE(<cuda_fp8.h>)
+#  endif // _CCCL_HAS_INCLUDE(<cuda_fp8.h>) && defined(_CCCL_HAS_NVFP16) && defined(_CCCL_HAS_NVBF16)
 #else
 #  define _CCCL_HAS_NVFP8() 0
 #endif // !defined(_CCCL_DISABLE_NVFP8_SUPPORT)
