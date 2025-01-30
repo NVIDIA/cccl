5b194a6e5d32fe38a9a2c419abceb4f25134820d
Nader Al Awar <naderalawar@gmail.com>
2025-01-30 02:32:13 +0000

    Use `_CCCL_HOST_DEVICE` for RTC

--- cub/cub/device/dispatch/kernels/merge_sort.cuh
+++ cub/cub/device/dispatch/kernels/merge_sort.cuh
@@ -117,24 +117,24 @@ public:
     ::cuda::std::_If<uses_fallback_policy, fallback_block_sort_agent_t, default_block_sort_agent_t>;
   using merge_agent_t = ::cuda::std::_If<uses_fallback_policy, fallback_merge_agent_t, default_merge_agent_t>;
 
-  CUB_RUNTIME_FUNCTION static constexpr std::size_t block_sort_vsmem_per_block()
+  _CCCL_HOST_DEVICE static constexpr ::cuda::std::size_t block_sort_vsmem_per_block()
   {
     return detail::vsmem_helper_impl<block_sort_agent_t>::vsmem_per_block;
   }
 
-  CUB_RUNTIME_FUNCTION static constexpr std::size_t merge_vsmem_per_block()
+  _CCCL_HOST_DEVICE static constexpr ::cuda::std::size_t merge_vsmem_per_block()
   {
     return detail::vsmem_helper_impl<merge_agent_t>::vsmem_per_block;
   }
 
   template <typename PolicyT>
-  CUB_RUNTIME_FUNCTION static constexpr int BlockThreads(PolicyT /*policy*/)
+  _CCCL_HOST_DEVICE static constexpr int BlockThreads(PolicyT /*policy*/)
   {
     return policy_t::BLOCK_THREADS;
   }
 
   template <typename PolicyT>
-  CUB_RUNTIME_FUNCTION static constexpr int ItemsPerTile(PolicyT /*policy*/)
+  _CCCL_HOST_DEVICE static constexpr int ItemsPerTile(PolicyT /*policy*/)
   {
     return policy_t::ITEMS_PER_TILE;
   }
