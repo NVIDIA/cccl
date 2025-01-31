28501e0b69403f183e8655ee3f33fd98236222b2
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-01-31 18:23:03 +0100

    Already test FP16 when CTK headers are available

--- c2h/include/c2h/extended_types.h
+++ c2h/include/c2h/extended_types.h
@@ -30,19 +30,19 @@
 #include <cuda/__cccl_config>
 
 #ifndef TEST_HALF_T
-#  if defined(_CCCL_HAS_NVFP16) && defined(_LIBCUDACXX_HAS_NVFP16)
+#  if defined(_CCCL_HAS_NVFP16)
 #    define TEST_HALF_T() 1
-#  else // defined(_CCCL_HAS_NVFP16) && defined(_LIBCUDACXX_HAS_NVFP16)
+#  else // defined(_CCCL_HAS_NVFP16)
 #    define TEST_HALF_T() 0
-#  endif // defined(_CCCL_HAS_NVFP16) && defined(_LIBCUDACXX_HAS_NVFP16)
+#  endif // defined(_CCCL_HAS_NVFP16)
 #endif // TEST_HALF_T
 
 #ifndef TEST_BF_T
-#  if defined(_CCCL_HAS_NVBF16) && defined(_LIBCUDACXX_HAS_NVBF16)
+#  if defined(_CCCL_HAS_NVBF16)
 #    define TEST_BF_T() 1
-#  else // defined(_CCCL_HAS_NVBF16) && defined(_LIBCUDACXX_HAS_NVBF16)
+#  else // defined(_CCCL_HAS_NVBF16)
 #    define TEST_BF_T() 0
-#  endif // defined(_CCCL_HAS_NVBF16) && defined(_LIBCUDACXX_HAS_NVBF16)
+#  endif // defined(_CCCL_HAS_NVBF16)
 #endif // TEST_BF_T
 
 #if TEST_HALF_T()
