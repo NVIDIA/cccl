a3c249b459190dc46cda1572ce6eaf4b7dfbb1d1
fbusato <fbusato@nvidia.com>
2025-02-04 11:22:22 -0800

    create independent test for sass checking

--- cub/test/thread_reduce/catch2_test_thread_reduce.cu
+++ cub/test/thread_reduce/catch2_test_thread_reduce.cu
@@ -40,12 +40,8 @@
 #include <functional>
 #include <limits>
 #include <numeric>
-#include <string>
-#include <tuple>
-#include <type_traits>
 
 #include "c2h/catch2_test_helper.h"
-#include "c2h/custom_type.h"
 #include "c2h/extended_types.h"
 #include "c2h/generators.h"
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
@@ -54,22 +50,15 @@
  * Thread Reduce Wrapper Kernels
  **********************************************************************************************************************/
 
-#define CCCL_CHECK_SASS // instantiate only the kernels useful for SASS inspection
-
 template <int NUM_ITEMS, typename T, typename ReduceOperator>
 __global__ void thread_reduce_kernel(const T* __restrict__ d_in, T* __restrict__ d_out, ReduceOperator reduce_operator)
 {
   T thread_data[NUM_ITEMS];
-#if defined(CCCL_CHECK_SASS)
-  auto d_in_aligned = static_cast<T*>(__builtin_assume_aligned(d_in, (sizeof(T) < 4) ? 4 : sizeof(T)));
-  ::memcpy(thread_data, d_in_aligned, sizeof(thread_data));
-#else
-#  pragma unroll
+#pragma unroll
   for (int i = 0; i < NUM_ITEMS; ++i)
   {
     thread_data[i] = d_in[i];
   }
-#endif
   *d_out = cub::ThreadReduce(thread_data, reduce_operator);
 }
 
@@ -255,19 +244,6 @@ using narrow_precision_type_list = c2h::type_list<
 #endif // TEST_BF_T()
   >;
 
-#if defined(CCCL_CHECK_SASS)
-
-using fp_type_list = c2h::type_list<float>;
-
-using integral_type_list = c2h::type_list<::cuda::std::int8_t, ::cuda::std::int16_t, ::cuda::std::int32_t>;
-
-using cub_operator_integral_list =
-  c2h::type_list<cuda::std::plus<>, cuda::std::multiplies<>, cuda::std::bit_xor<>, cuda::minimum<>>;
-
-using cub_operator_fp_list = c2h::type_list<cuda::std::plus<>, cuda::minimum<>>;
-
-#else // !defined(CCCL_CHECK_SASS)
-
 using integral_type_list = c2h::
   type_list<::cuda::std::int8_t, ::cuda::std::int16_t, ::cuda::std::uint16_t, ::cuda::std::int32_t, ::cuda::std::int64_t>;
 
@@ -284,8 +260,6 @@ using cub_operator_integral_list =
 
 using cub_operator_fp_list =
   c2h::type_list<cuda::std::plus<>, cuda::std::multiplies<>, cuda::minimum<>, cuda::maximum<>>;
-
-#endif // defined(CCCL_CHECK_SASS)
 
 /***********************************************************************************************************************
  * Verify results and kernel launch
@@ -311,7 +285,6 @@ void run_thread_reduce_kernel(
 {
   switch (num_items)
   {
-#if !defined(CCCL_CHECK_SASS)
     case 1:
       thread_reduce_kernel<1>
         <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
@@ -376,12 +349,6 @@ void run_thread_reduce_kernel(
       thread_reduce_kernel<16>
         <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
       break;
-#else
-    case 18:
-      thread_reduce_kernel<18>
-        <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
-      break;
-#endif
     default:
       FAIL("Unsupported number of items");
   }
@@ -389,15 +356,8 @@ void run_thread_reduce_kernel(
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
 }
 
-#if !defined(CCCL_CHECK_SASS)
-constexpr int min_size  = 1;
 constexpr int max_size  = 16;
 constexpr int num_seeds = 10;
-#else
-constexpr int min_size  = 18;
-constexpr int max_size  = 18;
-constexpr int num_seeds = 1;
-#endif
 
 /***********************************************************************************************************************
  * Test cases
@@ -414,7 +374,7 @@ C2H_TEST("ThreadReduce Integral Type Tests", "[reduce][thread]", integral_type_l
   c2h::device_vector<value_t> d_out(1);
   c2h::gen(C2H_SEED(num_seeds), d_in, std::numeric_limits<value_t>::min());
   c2h::host_vector<value_t> h_in = d_in;
-  for (int num_items = min_size; num_items <= max_size; ++num_items)
+  for (int num_items = 1; num_items <= max_size; ++num_items)
   {
     auto reference_result = std::accumulate(h_in.begin(), h_in.begin() + num_items, operator_identity, std_reduce_op);
     run_thread_reduce_kernel(num_items, d_in, d_out, reduce_op);
@@ -433,7 +393,7 @@ C2H_TEST("ThreadReduce Floating-Point Type Tests", "[reduce][thread]", fp_type_l
   c2h::device_vector<value_t> d_out(1);
   c2h::gen(C2H_SEED(num_seeds), d_in, std::numeric_limits<value_t>::min());
   c2h::host_vector<value_t> h_in = d_in;
-  for (int num_items = min_size; num_items <= max_size; ++num_items)
+  for (int num_items = 1; num_items <= max_size; ++num_items)
   {
     auto reference_result = std::accumulate(h_in.begin(), h_in.begin() + num_items, operator_identity, std_reduce_op);
     run_thread_reduce_kernel(num_items, d_in, d_out, reduce_op);
@@ -456,7 +416,7 @@ C2H_TEST("ThreadReduce Narrow PrecisionType Tests",
   c2h::device_vector<value_t> d_out(1);
   c2h::gen(C2H_SEED(num_seeds), d_in, value_t{1.0f}, value_t{2.0f});
   c2h::host_vector<float> h_in_float = d_in;
-  for (int num_items = min_size; num_items <= max_size; ++num_items)
+  for (int num_items = 1; num_items <= max_size; ++num_items)
   {
     CAPTURE(c2h::type_name<value_t>(), num_items, c2h::type_name<decltype(reduce_op)>());
     auto reference_result =
@@ -467,8 +427,6 @@ C2H_TEST("ThreadReduce Narrow PrecisionType Tests",
 }
 
 #endif // TEST_HALF_T() || TEST_BF_T()
-
-#if defined(CCCL_CHECK_SASS)
 
 C2H_TEST("ThreadReduce Container Tests", "[reduce][thread]")
 {
@@ -484,21 +442,19 @@ C2H_TEST("ThreadReduce Container Tests", "[reduce][thread]")
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
   verify_results(reference_result, c2h::host_vector<int>(d_out)[0]);
 
-#  if _CCCL_STD_VER >= 2014
+#if _CCCL_STD_VER >= 2014
   thread_reduce_kernel_span<max_size>
     <<<1, 1>>>(thrust::raw_pointer_cast(d_in.data()), thrust::raw_pointer_cast(d_out.data()), cuda::std::plus<>{});
   REQUIRE(cudaSuccess == cudaPeekAtLastError());
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
   verify_results(reference_result, c2h::host_vector<int>(d_out)[0]);
-#  endif // _CCCL_STD_VER >= 2014
+#endif // _CCCL_STD_VER >= 2014
 
-#  if _CCCL_STD_VER >= 2023
+#if _CCCL_STD_VER >= 2023
   thread_reduce_kernel_mdspan<max_size>
     <<<1, 1>>>(thrust::raw_pointer_cast(d_in.data()), thrust::raw_pointer_cast(d_out.data()), cuda::std::plus<>{});
   REQUIRE(cudaSuccess == cudaPeekAtLastError());
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
   verify_results(reference_result, c2h::host_vector<int>(d_out)[0]);
-#  endif // _CCCL_STD_VER >= 2023
+#endif // _CCCL_STD_VER >= 2023
 }
-
-#endif // defined(CCCL_CHECK_SASS)
