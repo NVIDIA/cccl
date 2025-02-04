afbab1bdfece103e2ccd8bb1d380a977667a2975
fbusato <fbusato@nvidia.com>
2025-02-04 15:16:23 -0800

    remove THRUST_GCC12_OMP_MISCOMPILE

--- thrust/testing/replace.cu
+++ thrust/testing/replace.cu
@@ -83,7 +83,6 @@ void TestReplace(const size_t n)
 DECLARE_VARIABLE_UNITTEST(TestReplace);
 
 #ifndef THRUST_GCC13_TBB_MISCOMPILE
-#  ifndef THRUST_GCC12_OMP_MISCOMPILE
 template <class Vector>
 THRUST_DISABLE_BROKEN_GCC_VECTORIZER void TestReplaceCopySimple()
 {
@@ -100,7 +99,6 @@ THRUST_DISABLE_BROKEN_GCC_VECTORIZER void TestReplaceCopySimple()
   ASSERT_EQUAL(dest, result);
 }
 DECLARE_VECTOR_UNITTEST(TestReplaceCopySimple);
-#  endif
 #endif
 
 template <typename InputIterator, typename OutputIterator, typename T>
