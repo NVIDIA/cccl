584c0a586b7018e979e772e437e2910073f6d705
fbusato <fbusato@nvidia.com>
2025-02-04 12:08:48 -0800

    fix diagnostic.h

--- libcudacxx/include/cuda/std/__cccl/diagnostic.h
+++ libcudacxx/include/cuda/std/__cccl/diagnostic.h
@@ -106,8 +106,13 @@
 #    define _CCCL_NV_DIAG_SUPPRESS(_WARNING) _CCCL_PRAGMA(diagnostic push) _CCCL_PRAGMA(diag_suppress _WARNING)
 #    define _CCCL_NV_DIAG_DEFAULT(_WARNING)  _CCCL_PRAGMA(diagnostic pop)
 #  else // ^^^ __NVCC_DIAG_PRAGMA_SUPPORT__ ^^^ / vvv !__NVCC_DIAG_PRAGMA_SUPPORT__ vvv
-#    define _CCCL_NV_DIAG_SUPPRESS(_WARNING) _CCCL_PRAGMA(diag_suppress _WARNING)
-#    define _CCCL_NV_DIAG_DEFAULT(_WARNING)  _CCCL_PRAGMA(diag_default _WARNING)
+#    if _CCCL_COMPILER(GCC) // these compilers have issues with restoring the warning
+#      define _CCCL_NV_DIAG_SUPPRESS(_WARNING) _CCCL_PRAGMA(diag_suppress _WARNING)
+#      define _CCCL_NV_DIAG_DEFAULT(_WARNING)
+#    else // ^^^ _CCCL_COMPILER(GCC) ^^^ / vvv !_CCCL_COMPILER(GCC) vvv
+#      define _CCCL_NV_DIAG_SUPPRESS(_WARNING) _CCCL_PRAGMA(diag_suppress _WARNING)
+#      define _CCCL_NV_DIAG_DEFAULT(_WARNING)  _CCCL_PRAGMA(diag_default _WARNING)
+#    endif // !_CCCL_COMPILER(GCC)
 #  endif // !__NVCC_DIAG_PRAGMA_SUPPORT__
 #else // ^^^ _CCCL_HAS_CUDA_COMPILER ^^^ / vvv !_CCCL_HAS_CUDA_COMPILER vvv
 #  define _CCCL_NV_DIAG_SUPPRESS(_WARNING)
