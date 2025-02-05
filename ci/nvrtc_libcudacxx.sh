f179fb8a298716328ec71176fc9f991d1d8a615f
Michael Schellenberger Costa <miscco@nvidia.com>
2025-02-05 16:53:18 +0100

    Add NVRTC CI for windows

--- ci/nvrtc_libcudacxx.sh
+++ ci/nvrtc_libcudacxx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -euo pipefail
+
 source "$(dirname "$0")/build_common.sh"
 
 print_environment_details
-
 
 PRESET="libcudacxx-nvrtc-cpp${CXX_STANDARD}"
 CMAKE_OPTIONS=""
@@ -13,3 +14,5 @@ configure_and_build_preset "libcudacxx NVRTC" "$PRESET" "$CMAKE_OPTIONS"
 source "./sccache_stats.sh" "start"
 test_preset "libcudacxx NVRTC" "${PRESET}"
 source "./sccache_stats.sh" "end"
+
+print_time_summary
