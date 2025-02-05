50c66075633bb3e036c6e16337a1442b9c38eeea
Michael Schellenberger Costa <miscco@nvidia.com>
2025-02-05 20:30:40 +0100

    Only build

--- ci/nvrtc_libcudacxx.sh
+++ ci/nvrtc_libcudacxx.sh
@@ -11,8 +11,4 @@ CMAKE_OPTIONS=""
 
 configure_and_build_preset "libcudacxx NVRTC" "$PRESET" "$CMAKE_OPTIONS"
 
-source "./sccache_stats.sh" "start"
-test_preset "libcudacxx NVRTC" "${PRESET}"
-source "./sccache_stats.sh" "end"
-
 print_time_summary
