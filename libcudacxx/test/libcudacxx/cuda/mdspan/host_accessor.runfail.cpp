52b12a635c4b1b912c1ce2ac56bfef3fe5214757
fbusato <fbusato@nvidia.com>
2025-02-05 17:01:38 -0800

    add tests

---
+++ libcudacxx/test/libcudacxx/cuda/mdspan/host_accessor.runfail.cpp
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the libcu++ Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/__mdspan/host_device_mdspan.h>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+#if !defined(__CUDA_ARCH__)
+  int* device_ptr;
+  assert(cudaMalloc(&device_ptr, 4) == cudaSuccess);
+  using ext_t = cuda::std::extents<int, 4>;
+  cuda::host_mdspan<int, ext_t> h_md{device_ptr, ext_t{}};
+  unused(h_md[0]);
+#endif
+  return 0;
+}
---
+++ libcudacxx/test/libcudacxx/cuda/mdspan/host_device_accessor.pass.cpp
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the libcu++ Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/__mdspan/host_device_mdspan.h>
+
+#include "test_macros.h"
+
+__device__ int device_array[]   = {1, 2, 3, 4};
+__managed__ int managed_array[] = {1, 2, 3, 4};
+
+int main(int, char**)
+{
+  int array[] = {1, 2, 3, 4};
+  using ext_t = cuda::std::extents<int, 4>;
+  cuda::host_mdspan<int, ext_t> h_md{array, ext_t{}};
+  cuda::device_mdspan<int, ext_t> d_md{device_array, ext_t{}};
+  cuda::managed_mdspan<int, ext_t> m_md{managed_array, ext_t{}};
+#if !defined(__CUDA_ARCH__)
+  unused(h_md[0]);
+  unused(m_md[0]);
+#else
+  unused(d_md[0]);
+  unused(m_md[0]);
+#endif
+  return 0;
+}
