a168d70b2138c14768f806e2aefb224be5179af3
Cedric Augonnet <caugonnet@nvidia.com>
2025-02-06 10:10:01 +0100

    Revert compilation environment changes that should not be committed

--- .devcontainer/cccl-entrypoint.sh
+++ .devcontainer/cccl-entrypoint.sh
@@ -8,7 +8,7 @@ devcontainer-utils-post-create-command;
 devcontainer-utils-init-git;
 devcontainer-utils-post-attach-command;
 
-# cd /home/coder/cccl/
+cd /home/coder/cccl/
 
 if test $# -gt 0; then
     exec "$@";
