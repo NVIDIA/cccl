cb6ef8579a0e6aeba2d65f00372ee7506beac007
fbusato <fbusato@nvidia.com>
2025-02-07 12:02:42 -0800

    try with aligned structure

--- libcudacxx/test/libcudacxx/std/containers/views/mdspan/mdspan.aligned_accessor/aligned_accessor.runfail.cpp
+++ libcudacxx/test/libcudacxx/std/containers/views/mdspan/mdspan.aligned_accessor/aligned_accessor.runfail.cpp
@@ -10,15 +10,21 @@
 
 #include <test_macros.h>
 
+struct alignas(64) dummy
+{
+  int array[4] = {1, 2, 3, 4};
+};
+
 int main(int, char**)
 {
 // the alignment check is disabled when it is not possible to evaluate the alignment at compile time
 #if defined(_CCCL_BUILTIN_IS_CONSTANT_EVALUATED)
-  using E                = cuda::std::extents<size_t, 2>;
-  using L                = cuda::std::layout_right;
-  using A                = cuda::std::aligned_accessor<int, 64>;
-  alignas(4) int array[] = {1, 2, 3};
-  cuda::std::mdspan<int, E, L, A> md(static_cast<int*>(array) + 1, 2);
+  using E = cuda::std::extents<size_t, 2>;
+  using L = cuda::std::layout_right;
+  using A = cuda::std::aligned_accessor<int, 64>;
+  assert(alignof(dummy) == 64);
+  dummy d;
+  cuda::std::mdspan<int, E, L, A> md(static_cast<int*>(d.array) + 1, 2);
   unused(md(0));
   return 0;
 #else
