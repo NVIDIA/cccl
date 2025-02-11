d60794ce8d159b11a401e3ad7d6902ec8de32c93
Nader Al Awar <naderalawar@gmail.com>
2025-02-11 20:56:53 +0000

    Add test for sorting custom types

--- python/cuda_parallel/tests/test_merge_sort.py
+++ python/cuda_parallel/tests/test_merge_sort.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import cupy as cp
 import numba.cuda
 import numba.types
 import numpy as np
@@ -9,6 +10,7 @@ import pytest
 from conftest import random_array, type_to_problem_sizes
 
 import cuda.parallel.experimental.algorithms as algorithms
+from cuda.parallel.experimental.struct import gpu_struct
 
 
 def merge_sort_device(
@@ -29,8 +31,8 @@ def merge_sort_device(
     )
 
 
-def compare_op(a, b):
-    return np.uint8(a < b)
+def compare_op(lhs, rhs):
+    return np.uint8(lhs < rhs)
 
 
 @pytest.mark.parametrize(
@@ -56,7 +58,8 @@ def test_device_merge_sort_keys(dtype):
 
         d_in_keys = numba.cuda.to_device(h_in_keys)
 
-        merge_sort_device(d_in_keys, None, d_in_keys, None, compare_op, num_items)
+        merge_sort_device(d_in_keys, None, d_in_keys,
+                          None, compare_op, num_items)
 
         h_out_keys = d_in_keys.copy_to_host()
         h_in_keys.sort()
@@ -129,7 +132,8 @@ def test_device_merge_sort_keys_copy(dtype):
         d_in_keys = numba.cuda.to_device(h_in_keys)
         d_out_keys = numba.cuda.to_device(h_out_keys)
 
-        merge_sort_device(d_in_keys, None, d_out_keys, None, compare_op, num_items)
+        merge_sort_device(d_in_keys, None, d_out_keys,
+                          None, compare_op, num_items)
 
         h_out_keys = d_out_keys.copy_to_host()
         h_in_keys.sort()
@@ -179,3 +183,52 @@ def test_device_merge_sort_pairs_copy(dtype):
 
         np.testing.assert_array_equal(h_out_keys, h_in_keys)
         np.testing.assert_array_equal(h_out_items, h_in_items)
+
+
+def test_device_merge_sort_pairs_struct_type():
+    @gpu_struct
+    class key_pair:
+        a: np.int8
+        b: np.uint64
+
+    @gpu_struct
+    class item_pair:
+        a: np.int32
+        b: np.float32
+
+    def struct_compare_op(lhs, rhs):
+        return np.uint8(lhs.b < rhs.b if lhs.a == rhs.a else lhs.a < rhs.a)
+
+    num_items = 10000
+
+    a_keys = np.random.randint(0, 100, num_items, dtype=np.int8)
+    b_keys = np.random.randint(0, 100, num_items, dtype=np.uint64)
+
+    a_items = np.random.randint(0, 100, num_items, dtype=np.int32)
+    b_items = np.random.rand(num_items).astype(np.float32)
+
+    h_in_keys = np.empty(num_items, dtype=key_pair.dtype)
+    h_in_items = np.empty(num_items, dtype=item_pair.dtype)
+
+    h_in_keys["a"] = a_keys
+    h_in_keys["b"] = b_keys
+
+    h_in_items["a"] = a_items
+    h_in_items["b"] = b_items
+
+    d_in_keys = numba.cuda.to_device(h_in_keys)
+    d_in_items = numba.cuda.to_device(h_in_items)
+
+    merge_sort_device(
+        d_in_keys, d_in_items, d_in_keys, d_in_items, struct_compare_op, num_items
+    )
+
+    h_out_keys = d_in_keys.copy_to_host()
+    h_out_items = d_in_items.copy_to_host()
+
+    argsort = np.argsort(h_in_keys, stable=True)
+    h_in_keys = np.array(h_in_keys)[argsort]
+    h_in_items = np.array(h_in_items)[argsort]
+
+    np.testing.assert_array_equal(h_out_keys, h_in_keys)
+    np.testing.assert_array_equal(h_out_items, h_in_items)
