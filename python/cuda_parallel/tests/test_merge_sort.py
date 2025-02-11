ccce2855dc894ce114b1594f6145293a0eb39b97
Nader Al Awar <naderalawar@gmail.com>
2025-02-11 19:20:09 +0000

    Add test to sort pairs

--- python/cuda_parallel/tests/test_merge_sort.py
+++ python/cuda_parallel/tests/test_merge_sort.py
@@ -61,3 +61,42 @@ def test_device_merge_sort_keys(dtype):
         h_in_keys.sort()
 
         np.testing.assert_array_equal(h_out_keys, h_in_keys)
+
+
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        np.uint8,
+        np.uint16,
+        np.uint32,
+        np.uint64,
+        np.int8,
+        np.int16,
+        np.int32,
+        np.int64,
+        np.float32,
+        np.float64,
+    ],
+)
+def test_device_merge_sort_pairs(dtype):
+    for num_items_pow2 in type_to_problem_sizes(dtype):
+        num_items = 2**num_items_pow2
+
+        h_in_keys = random_array(num_items, dtype)
+        h_in_items = random_array(num_items, np.float32)
+        d_in_keys = numba.cuda.to_device(h_in_keys)
+        d_in_items = numba.cuda.to_device(h_in_items)
+
+        merge_sort_device(
+            d_in_keys, d_in_items, d_in_keys, d_in_items, compare_op, num_items
+        )
+
+        h_out_keys = d_in_keys.copy_to_host()
+        h_out_items = d_in_items.copy_to_host()
+
+        argsort = np.argsort(h_in_keys, stable=True)
+        h_in_keys = np.array(h_in_keys)[argsort]
+        h_in_items = np.array(h_in_items)[argsort]
+
+        np.testing.assert_array_equal(h_out_keys, h_in_keys)
+        np.testing.assert_array_equal(h_out_items, h_in_items)
