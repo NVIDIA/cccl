a7fa209c2ec65209e54d0ae5589933fa730e9f50
fbusato <fbusato@nvidia.com>
2025-02-14 09:41:16 -0800

    remove additional testing code

--- libcudacxx/test/libcudacxx/std/utilities/memory/ptr.align/assume_aligned.runfail.cpp
+++ libcudacxx/test/libcudacxx/std/utilities/memory/ptr.align/assume_aligned.runfail.cpp
@@ -14,26 +14,12 @@
 
 int main(int, char**)
 {
-  auto ptr1 = reinterpret_cast<int*>(uintptr_t{0x4});
-  assert(reinterpret_cast<uintptr_t>(ptr1) % 64 == 0);
-
-  printf("--> %p\n", ptr1);
-  printf("-->  %d\n", (int) (reinterpret_cast<uintptr_t>(ptr1) % 64));
-  printf("-->  %d\n", (int) (reinterpret_cast<uintptr_t>(ptr1) % 64 == 0));
 // the alignment check is disabled when it is not possible to evaluate the alignment at compile time
 #if defined(_CCCL_BUILTIN_IS_CONSTANT_EVALUATED)
   auto ptr                  = cuda::std::bit_cast<int*>(uintptr_t{0x4});
   volatile auto aligned_ptr = cuda::std::assume_aligned<64>(ptr);
   assert(ptr + 1 == aligned_ptr);
   unused(aligned_ptr);
-
-#  if defined(_CCCL_ASSERT)
-  _CCCL_ASSERT(cuda::std::bit_cast<uintptr_t>(ptr) % 64 == 0, "Alignment assumption is violated");
-  _CCCL_ASSERT(reinterpret_cast<uintptr_t>(ptr) % 64 == 0, "Alignment assumption is violated");
-  //_CCCL_ASSERT(uintptr_t{0x4} % 64 == 0, "Alignment assumption is violated");
-#  else
-  static_assert(false);
-#  endif
   return 0;
 #else
   return 1;
