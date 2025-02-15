17c5ba74e415c894a1f9eda7fb6a46d7b10f42e3
Allison Piper <alliepiper16@gmail.com>
2025-02-15 17:31:24 +0000

    Skip jobs that never ran when preparing time summaries.
    There will not be an entry in `job_times` if e.g. `job` is a test job that depends on a failed build job.

--- .github/actions/workflow-results/prepare-execution-summary.py
+++ .github/actions/workflow-results/prepare-execution-summary.py
@@ -69,7 +69,7 @@ def update_summary_entry(entry, job, job_times=None):
     else:
         entry["failed"] += 1
 
-    if job_times:
+    if job_times and job["id"] in job_times:
         time_info = job_times[job["id"]]
         job_time = time_info["job_seconds"]
         command_time = time_info["command_seconds"]
