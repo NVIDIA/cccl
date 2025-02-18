240078d6eeeb1753909478ac27ff73556801bf32
Nader Al Awar <naderalawar@gmail.com>
2025-02-18 17:45:36 +0000

    Add initial c.parallel implementation of building unique_by_key

---
+++ c/parallel/src/unique_by_key.cu
@@ -0,0 +1,368 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cub/detail/choose_offset.cuh>
+#include <cub/detail/launcher/cuda_driver.cuh>
+#include <cub/device/device_merge_sort.cuh>
+
+#include <format>
+
+#include "kernels/iterators.h"
+#include "kernels/operators.h"
+#include "util/types.h"
+#include <cccl/c/unique_by_key.h>
+#include <nvrtc/command_list.h>
+
+struct op_wrapper;
+struct device_unique_by_key_policy;
+using OffsetT = int64_t;
+static_assert(std::is_same_v<cub::detail::choose_signed_offset_t<OffsetT>, OffsetT>, "OffsetT must be int64");
+
+struct input_keys_iterator_state_t;
+struct input_values_iterator_state_t;
+struct output_keys_iterator_t;
+struct output_values_iterator_t;
+struct output_num_selected_iterator_t;
+
+struct num_selected_storage_t;
+
+namespace unique_by_key
+{
+struct unique_by_key_runtime_tuning_policy
+{
+  int block_size;
+  int items_per_thread;
+  cub::BlockLoadAlgorithm load_algorithm;
+  cub::CacheLoadModifier load_modifier;
+
+  unique_by_key_runtime_tuning_policy UniqueByKey() const
+  {
+    return *this;
+  }
+};
+
+unique_by_key_runtime_tuning_policy get_policy(int /*cc*/)
+{
+  // TODO: fix later
+  return {384, 10, cub::BLOCK_LOAD_DIRECT, cub::LOAD_DEFAULT};
+}
+
+enum class unique_by_key_iterator_t
+{
+  input_keys    = 0,
+  input_values  = 1,
+  output_keys   = 2,
+  output_values = 3,
+  num_selected  = 4
+};
+
+template <typename StorageT = storage_t>
+std::string get_iterator_name(cccl_iterator_t iterator, unique_by_key_iterator_t which_iterator)
+{
+  if (iterator.type == cccl_iterator_kind_t::pointer)
+  {
+    if (iterator.state == nullptr)
+    {
+      return "cub::NullType*";
+    }
+    else
+    {
+      return cccl_type_enum_to_name<StorageT>(iterator.value_type.type, true);
+    }
+  }
+  else
+  {
+    std::string iterator_t;
+    switch (which_iterator)
+    {
+      case unique_by_key_iterator_t::input_keys: {
+        check(nvrtcGetTypeName<input_keys_iterator_state_t>(&iterator_t));
+        break;
+      }
+      case unique_by_key_iterator_t::input_values: {
+        check(nvrtcGetTypeName<input_values_iterator_state_t>(&iterator_t));
+        break;
+      }
+      case unique_by_key_iterator_t::output_keys: {
+        check(nvrtcGetTypeName<output_keys_iterator_t>(&iterator_t));
+        break;
+      }
+      case unique_by_key_iterator_t::output_values: {
+        check(nvrtcGetTypeName<output_values_iterator_t>(&iterator_t));
+        break;
+      }
+      case unique_by_key_iterator_t::num_selected: {
+        check(nvrtcGetTypeName<output_num_selected_iterator_t>(&iterator_t));
+        break;
+      }
+    }
+
+    return iterator_t;
+  }
+}
+
+std::string get_compact_init_kernel_name(cccl_iterator_t output_num_selected_it)
+{
+  std::string offset_t;
+  check(nvrtcGetTypeName<OffsetT>(&offset_t));
+
+  const std::string input_keys_iterator_t =
+    get_iterator_name(output_num_selected_it, unique_by_key_iterator_t::num_selected);
+
+  return std::format(
+    "cub::detail::scan::DeviceCompactInitKernel<cub::ScanTileState<{0}>, {1}>;", offset_t, input_keys_iterator_t);
+}
+
+std::string get_sweep_kernel_name(
+  cccl_iterator_t input_keys_it,
+  cccl_iterator_t input_values_it,
+  cccl_iterator_t output_keys_it,
+  cccl_iterator_t output_values_it,
+  cccl_iterator_t output_num_selected_it)
+{
+  std::string chained_policy_t;
+  check(nvrtcGetTypeName<device_unique_by_key_policy>(&chained_policy_t));
+
+  const std::string input_keys_iterator_t = get_iterator_name(input_keys_it, unique_by_key_iterator_t::input_keys);
+  const std::string input_values_iterator_t =
+    get_iterator_name<items_storage_t>(input_values_it, unique_by_key_iterator_t::input_values);
+  const std::string output_keys_iterator_t = get_iterator_name(output_keys_it, unique_by_key_iterator_t::output_keys);
+  const std::string output_values_iterator_t =
+    get_iterator_name<items_storage_t>(output_values_it, unique_by_key_iterator_t::output_values);
+  const std::string output_num_selected_iterator_t =
+    get_iterator_name<num_selected_storage_t>(output_num_selected_it, unique_by_key_iterator_t::output_values);
+
+  std::string offset_t;
+  check(nvrtcGetTypeName<OffsetT>(&offset_t));
+
+  auto tile_state_t = std::format("cub::ScanTileState<{0}>", offset_t);
+
+  std::string equality_op_t;
+  check(nvrtcGetTypeName<op_wrapper>(&equality_op_t));
+
+  return std::format(
+    "cub::detail::unique_by_key::DeviceUniqueByKeySweepKernel<{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}>;",
+    chained_policy_t,
+    input_keys_iterator_t,
+    input_values_iterator_t,
+    output_keys_iterator_t,
+    output_values_iterator_t,
+    output_num_selected_iterator_t,
+    tile_state_t,
+    equality_op_t,
+    offset_t);
+}
+
+} // namespace unique_by_key
+
+extern "C" CCCL_C_API CUresult cccl_device_unique_by_key_build(
+  cccl_device_unique_by_key_build_result_t* build,
+  cccl_iterator_t input_keys_it,
+  cccl_iterator_t input_values_it,
+  cccl_iterator_t output_keys_it,
+  cccl_iterator_t output_values_it,
+  cccl_iterator_t output_num_selected_it,
+  cccl_op_t op,
+  int cc_major,
+  int cc_minor,
+  const char* cub_path,
+  const char* thrust_path,
+  const char* libcudacxx_path,
+  const char* ctk_path) noexcept
+{
+  CUresult error = CUDA_SUCCESS;
+
+  try
+  {
+    const char* name = "test";
+
+    const int cc      = cc_major * 10 + cc_minor;
+    const auto policy = unique_by_key::get_policy(cc);
+
+    const auto input_keys_it_value_t          = cccl_type_enum_to_string(input_keys_it.value_type.type);
+    const auto input_values_it_value_t        = cccl_type_enum_to_string(input_values_it.value_type.type);
+    const auto output_keys_it_value_t         = cccl_type_enum_to_string(output_keys_it.value_type.type);
+    const auto output_values_it_value_t       = cccl_type_enum_to_string(output_values_it.value_type.type);
+    const auto output_num_selected_it_value_t = cccl_type_enum_to_string(output_num_selected_it.value_type.type);
+    const auto offset_t                       = cccl_type_enum_to_string(cccl_type_enum::INT64);
+
+    const std::string input_keys_iterator_src = make_kernel_input_iterator(
+      offset_t,
+      get_iterator_name(input_keys_it, unique_by_key::unique_by_key_iterator_t::input_keys),
+      input_keys_it_value_t,
+      input_keys_it);
+    const std::string input_values_iterator_src = make_kernel_input_iterator(
+      offset_t,
+      get_iterator_name(input_values_it, unique_by_key::unique_by_key_iterator_t::input_values),
+      input_values_it_value_t,
+      input_values_it);
+    const std::string output_keys_iterator_src = make_kernel_output_iterator(
+      offset_t,
+      get_iterator_name(output_keys_it, unique_by_key::unique_by_key_iterator_t::output_keys),
+      output_keys_it_value_t,
+      output_keys_it);
+    const std::string output_values_iterator_src = make_kernel_output_iterator(
+      offset_t,
+      get_iterator_name(output_values_it, unique_by_key::unique_by_key_iterator_t::output_values),
+      output_values_it_value_t,
+      output_values_it);
+    const std::string output_num_selected_iterator_src = make_kernel_output_iterator(
+      offset_t,
+      get_iterator_name(output_num_selected_it, unique_by_key::unique_by_key_iterator_t::num_selected),
+      output_num_selected_it_value_t,
+      output_num_selected_it);
+
+    const std::string op_src = make_kernel_user_comparison_operator(input_keys_it_value_t, op);
+
+    const std::string src = std::format(
+      "#include <cub/device/dispatch/kernels/unique_by_key.cuh>\n"
+      "struct __align__({1}) storage_t {{\n"
+      "  char data[{0}];\n"
+      "}};\n"
+      "struct __align__({3}) items_storage_t {{\n"
+      "  char data[{2}];\n"
+      "}};\n"
+      "struct __align__({5}) num_out_storage_t {{\n"
+      "  char data[{4}];\n"
+      "}};\n"
+      "{8}\n"
+      "{9}\n"
+      "{10}\n"
+      "{11}\n"
+      "{12}\n"
+      "struct agent_policy_t {{\n"
+      "  static constexpr int ITEMS_PER_THREAD = {7};\n"
+      "  static constexpr int BLOCK_THREADS = {6};\n"
+      "  static constexpr cub::BlockLoadAlgorithm LOAD_ALGORITHM = cub::BLOCK_LOAD_DIRECT;\n"
+      "  static constexpr cub::CacheLoadModifier LOAD_MODIFIER = cub::LOAD_DEFAULT;\n"
+      "  static constexpr cub::BlockScanAlgorithm SCAN_ALGORITHM = cub::BLOCK_SCAN_WARP_SCANS;\n"
+      "}};\n"
+      "struct device_unique_by_key_policy {{\n"
+      "  struct ActivePolicy {{\n"
+      "    using UniqueByKeyPolicy = agent_policy_t;\n"
+      "  }};\n"
+      "}};\n"
+      "{13}\n",
+      input_keys_it.value_type.size, // 0
+      input_keys_it.value_type.alignment, // 1
+      input_values_it.value_type.size, // 2
+      input_values_it.value_type.alignment, // 3
+      output_values_it.value_type.size, // 4
+      output_values_it.value_type.alignment, // 5
+      policy.block_size, // 6
+      policy.items_per_thread, // 7
+      input_keys_iterator_src, // 8
+      input_values_iterator_src, // 9
+      output_keys_iterator_src, // 10
+      output_values_iterator_src, // 11
+      output_num_selected_iterator_src, // 12
+      op_src); // 13
+
+#if false // CCCL_DEBUGGING_SWITCH
+      fflush(stderr);
+      printf("\nCODE4NVRTC BEGIN\n%sCODE4NVRTC END\n", src.c_str());
+      fflush(stdout);
+#endif
+
+    std::string compact_init_kernel_name = unique_by_key::get_compact_init_kernel_name(output_num_selected_it);
+    std::string sweep_kernel_name        = unique_by_key::get_sweep_kernel_name(
+      input_keys_it, input_values_it, output_keys_it, output_values_it, output_num_selected_it);
+    std::string compact_init_kernel_lowered_name;
+    std::string sweep_kernel_lowered_name;
+
+    const std::string arch = std::format("-arch=sm_{0}{1}", cc_major, cc_minor);
+
+    constexpr size_t num_args  = 7;
+    const char* args[num_args] = {arch.c_str(), cub_path, thrust_path, libcudacxx_path, ctk_path, "-rdc=true", "-dlto"};
+
+    constexpr size_t num_lto_args   = 2;
+    const char* lopts[num_lto_args] = {"-lto", arch.c_str()};
+
+    // Collect all LTO-IRs to be linked.
+    nvrtc_ltoir_list ltoir_list;
+    auto ltoir_list_append = [&ltoir_list](nvrtc_ltoir lto) {
+      if (lto.ltsz)
+      {
+        ltoir_list.push_back(std::move(lto));
+      }
+    };
+    ltoir_list_append({op.ltoir, op.ltoir_size});
+    if (cccl_iterator_kind_t::iterator == input_keys_it.type)
+    {
+      ltoir_list_append({input_keys_it.advance.ltoir, input_keys_it.advance.ltoir_size});
+      ltoir_list_append({input_keys_it.dereference.ltoir, input_keys_it.dereference.ltoir_size});
+    }
+    if (cccl_iterator_kind_t::iterator == input_values_it.type)
+    {
+      ltoir_list_append({input_values_it.advance.ltoir, input_values_it.advance.ltoir_size});
+      ltoir_list_append({input_values_it.dereference.ltoir, input_values_it.dereference.ltoir_size});
+    }
+    if (cccl_iterator_kind_t::iterator == output_keys_it.type)
+    {
+      ltoir_list_append({output_keys_it.advance.ltoir, output_keys_it.advance.ltoir_size});
+      ltoir_list_append({output_keys_it.dereference.ltoir, output_keys_it.dereference.ltoir_size});
+    }
+    if (cccl_iterator_kind_t::iterator == output_values_it.type)
+    {
+      ltoir_list_append({output_values_it.advance.ltoir, output_values_it.advance.ltoir_size});
+      ltoir_list_append({output_values_it.dereference.ltoir, output_values_it.dereference.ltoir_size});
+    }
+    if (cccl_iterator_kind_t::iterator == output_num_selected_it.type)
+    {
+      ltoir_list_append({output_values_it.advance.ltoir, output_values_it.advance.ltoir_size});
+      ltoir_list_append({output_values_it.dereference.ltoir, output_values_it.dereference.ltoir_size});
+    }
+
+    nvrtc_link_result result =
+      make_nvrtc_command_list()
+        .add_program(nvrtc_translation_unit{src.c_str(), name})
+        .add_expression({compact_init_kernel_name})
+        .add_expression({sweep_kernel_name})
+        .compile_program({args, num_args})
+        .get_name({compact_init_kernel_name, compact_init_kernel_lowered_name})
+        .get_name({sweep_kernel_name, sweep_kernel_lowered_name})
+        .cleanup_program()
+        .add_link_list(ltoir_list)
+        .finalize_program(num_lto_args, lopts);
+
+    cuLibraryLoadData(&build->library, result.data.get(), nullptr, nullptr, 0, nullptr, nullptr, 0);
+    check(cuLibraryGetKernel(&build->compact_init_kernel, build->library, compact_init_kernel_lowered_name.c_str()));
+    check(cuLibraryGetKernel(&build->sweep_kernel, build->library, sweep_kernel_lowered_name.c_str()));
+
+    build->cc         = cc;
+    build->cubin      = (void*) result.data.release();
+    build->cubin_size = result.size;
+  }
+  catch (const std::exception& exc)
+  {
+    fflush(stderr);
+    printf("\nEXCEPTION in cccl_device_unique_by_key_build(): %s\n", exc.what());
+    fflush(stdout);
+    error = CUDA_ERROR_UNKNOWN;
+  }
+
+  return error;
+}
+
+extern "C" CCCL_C_API CUresult cccl_device_unique_by_key(
+  cccl_device_unique_by_key_build_result_t build,
+  void* d_temp_storage,
+  size_t* temp_storage_bytes,
+  cccl_iterator_t d_keys_in,
+  cccl_iterator_t d_values_in,
+  cccl_iterator_t d_keys_out,
+  cccl_iterator_t d_values_out,
+  cccl_iterator_t d_num_selected_out,
+  cccl_op_t op,
+  unsigned long long num_items,
+  CUstream stream) noexcept;
+
+extern "C" CCCL_C_API CUresult
+cccl_device_unique_by_key_cleanup(cccl_device_unique_by_key_build_result_t* bld_ptr) noexcept;
