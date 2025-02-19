c9c3996ca11b8807d60f4c2ea96a99e10f19201c
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-02-19 13:23:52 -0600

    Update copyright year to 2025

--- c/parallel/src/scan.cu
+++ c/parallel/src/scan.cu
@@ -1,3 +1,13 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
 #include <cub/agent/single_pass_scan_operators.cuh>
 #include <cub/detail/choose_offset.cuh>
 #include <cub/detail/launcher/cuda_driver.cuh>
