69dc225f4ee99bd6bece7b992e4ad0be54fc31d6
fbusato <fbusato@nvidia.com>
2025-02-21 10:42:40 -0800

    NIT

--- libcudacxx/include/cuda/__ptx/instructions/shfl_sync.h
+++ libcudacxx/include/cuda/__ptx/instructions/shfl_sync.h
@@ -21,20 +21,16 @@
 #  pragma system_header
 #endif // no system header
 
-#if _CCCL_STD_VER >= 2017
+#include <cuda/__ptx/instructions/get_sreg.h>
+#include <cuda/__ptx/ptx_dot_variants.h>
+#include <cuda/std/__bit/bit_cast.h>
+#include <cuda/std/cstdint>
 
-#  include <cuda/__ptx/instructions/get_sreg.h>
-#  include <cuda/__ptx/ptx_dot_variants.h>
-#  include <cuda/std/__bit/bit_cast.h>
-#  include <cuda/std/__type_traits/is_integral.h>
-#  include <cuda/std/__type_traits/is_signed.h>
-#  include <cuda/std/cstdint>
-
-#  include <nv/target> // __CUDA_MINIMUM_ARCH__ and friends
+#include <nv/target> // __CUDA_MINIMUM_ARCH__ and friends
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA_PTX
 
-#  if __cccl_ptx_isa >= 600
+#if __cccl_ptx_isa >= 600
 
 enum class __dot_shfl_mode
 {
@@ -46,26 +42,26 @@ enum class __dot_shfl_mode
 
 [[maybe_unused]]
 _CCCL_DEVICE static inline _CUDA_VSTD::uint32_t __shfl_sync_dst_lane(
-  __dot_shfl_mode __shuffle_mode,
+  __dot_shfl_mode __shfl_mode,
   _CUDA_VSTD::uint32_t __lane_idx_offset,
   _CUDA_VSTD::uint32_t __clamp_segmask,
   _CUDA_VSTD::uint32_t __lane_mask)
 {
-  auto __lane              = get_sreg_laneid();
+  auto __lane              = ::cuda::ptx::get_sreg_laneid();
   auto __clamp             = __clamp_segmask & 0b11111;
   auto __segmask           = __clamp_segmask >> 8;
   auto __max_lane          = (__lane & __segmask) | (__clamp & ~__segmask);
   _CUDA_VSTD::uint32_t __j = 0;
-  if (__shuffle_mode == __dot_shfl_mode::__idx)
+  if (__shfl_mode == __dot_shfl_mode::__idx)
   {
-    auto __min_lane = (__lane & __clamp);
+    auto __min_lane = __lane & __clamp;
     __j             = __min_lane | (__lane_idx_offset & ~__segmask);
   }
-  else if (__shuffle_mode == __dot_shfl_mode::__up)
+  else if (__shfl_mode == __dot_shfl_mode::__up)
   {
     __j = __lane - __lane_idx_offset;
   }
-  else if (__shuffle_mode == __dot_shfl_mode::__down)
+  else if (__shfl_mode == __dot_shfl_mode::__down)
   {
     __j = __lane + __lane_idx_offset;
   }
@@ -73,7 +69,7 @@ _CCCL_DEVICE static inline _CUDA_VSTD::uint32_t __shfl_sync_dst_lane(
   {
     __j = __lane ^ __lane_idx_offset;
   }
-  auto __dst = (__shuffle_mode == __dot_shfl_mode::__up)
+  auto __dst = __shfl_mode == __dot_shfl_mode::__up
                ? (__j >= __max_lane ? __j : __lane) //
                : (__j <= __max_lane ? __j : __lane);
   return (1u << __dst);
@@ -88,12 +84,16 @@ _CCCL_DEVICE static inline void __shfl_sync_checks(
   _CUDA_VSTD::uint32_t __lane_mask)
 {
   static_assert(sizeof(_Tp) == 4, "shfl.sync only accepts 4-byte data types");
-  _CCCL_ASSERT(__lane_idx_offset < 32, "the lane index or offset must be less than the warp size");
+  if (__shfl_mode != __dot_shfl_mode::__idx)
+  {
+    _CCCL_ASSERT(__lane_idx_offset < 32, "the lane index or offset must be less than the warp size");
+  }
   _CCCL_ASSERT((__clamp_segmask | 0b1111100011111) == 0b1111100011111,
                "clamp value + segmentation mask must use the bit positions [0:4] and [8:12]");
   _CCCL_ASSERT((__lane_mask & __activemask()) == __lane_mask, "lane mask must be a subset of the active mask");
-  _CCCL_ASSERT(__shfl_sync_dst_lane(__shfl_mode, __lane_idx_offset, __clamp_segmask, __lane_mask) & __lane_mask,
-               "the destination lane must be a member of the lane mask");
+  _CCCL_ASSERT(
+    ::cuda::ptx::__shfl_sync_dst_lane(__shfl_mode, __lane_idx_offset, __clamp_segmask, __lane_mask) & __lane_mask,
+    "the destination lane must be a member of the lane mask");
 }
 
 template <typename _Tp>
@@ -265,9 +265,8 @@ _CCCL_NODISCARD _CCCL_DEVICE static inline _Tp shfl_sync_bfly(
   return _CUDA_VSTD::bit_cast<_CUDA_VSTD::uint32_t>(__ret);
 }
 
-#  endif // __cccl_ptx_isa >= 600
+#endif // __cccl_ptx_isa >= 600
 
 _LIBCUDACXX_END_NAMESPACE_CUDA_PTX
 
-#endif // _CCCL_STD_VER >= 2017
 #endif // _CUDA_PTX_SHFL_SYNC_H
--- libcudacxx/test/libcudacxx/cuda/ptx/manual/shfl_test.h
+++ libcudacxx/test/libcudacxx/cuda/ptx/manual/shfl_test.h
@@ -19,30 +19,30 @@ __host__ __device__ void test_shfl_full_mask()
   auto data                   = threadIdx.x;
   bool pred1, pred2, pred3, pred4;
   auto res1 = cuda::ptx::shfl_sync_idx(data, pred1, 2 /*idx*/, 0b11111 /*clamp*/, FullMask);
-  _CCCL_ASSERT(res1 == 2 && pred1, "shfl_mode_idx failed");
+  assert(res1 == 2 && pred1, "shfl_mode_idx failed");
 
   auto res2 = cuda::ptx::shfl_sync_up(data, pred2, 2 /*offset*/, 0 /*clamp*/, FullMask);
   if (threadIdx.x <= 1)
   {
-    _CCCL_ASSERT(res2 == threadIdx.x && !pred2, "shfl_mode_up failed");
+    assert(res2 == threadIdx.x && !pred2, "shfl_mode_up failed");
   }
   else
   {
-    _CCCL_ASSERT(res2 == threadIdx.x - 2 && pred2, "shfl_mode_up failed");
+    assert(res2 == threadIdx.x - 2 && pred2, "shfl_mode_up failed");
   }
 
   auto res3 = cuda::ptx::shfl_sync_down(data, pred3, 2 /*offset*/, 0b11111 /*clamp*/, FullMask);
   if (threadIdx.x >= 30)
   {
-    _CCCL_ASSERT(res3 == threadIdx.x && !pred3, "shfl_mode_down failed");
+    assert(res3 == threadIdx.x && !pred3, "shfl_mode_down failed");
   }
   else
   {
-    _CCCL_ASSERT(res3 == threadIdx.x + 2 && pred3, "shfl_mode_down failed");
+    assert(res3 == threadIdx.x + 2 && pred3, "shfl_mode_down failed");
   }
 
   auto res4 = cuda::ptx::shfl_sync_bfly(data, pred4, 2 /*offset*/, 0b11111 /*clamp*/, FullMask);
-  _CCCL_ASSERT(res4 == threadIdx.x ^ 2 && pred4, "shfl_mode_bfly failed");
+  assert(res4 == threadIdx.x ^ 2 && pred4, "shfl_mode_bfly failed");
 #endif // __cccl_ptx_isa >= 600
 }
 
@@ -52,30 +52,30 @@ __host__ __device__ void test_shfl_full_mask_no_pred()
   constexpr unsigned FullMask = 0xFFFFFFFF;
   auto data                   = threadIdx.x;
   auto res1                   = cuda::ptx::shfl_sync_idx(data, 2 /*idx*/, 0b11111 /*clamp*/, FullMask);
-  _CCCL_ASSERT(res1 == 2, "shfl_mode_idx failed");
+  assert(res1 == 2, "shfl_mode_idx failed");
 
   auto res2 = cuda::ptx::shfl_sync_up(data, 2 /*offset*/, 0 /*clamp*/, FullMask);
   if (threadIdx.x <= 1)
   {
-    _CCCL_ASSERT(res2 == threadIdx.x, "shfl_mode_up failed");
+    assert(res2 == threadIdx.x, "shfl_mode_up failed");
   }
   else
   {
-    _CCCL_ASSERT(res2 == threadIdx.x - 2, "shfl_mode_up failed");
+    assert(res2 == threadIdx.x - 2, "shfl_mode_up failed");
   }
 
   auto res3 = cuda::ptx::shfl_sync_down(data, 2 /*offset*/, 0b11111 /*clamp*/, FullMask);
   if (threadIdx.x >= 30)
   {
-    _CCCL_ASSERT(res3 == threadIdx.x, "shfl_mode_down failed");
+    assert(res3 == threadIdx.x, "shfl_mode_down failed");
   }
   else
   {
-    _CCCL_ASSERT(res3 == threadIdx.x + 2, "shfl_mode_down failed");
+    assert(res3 == threadIdx.x + 2, "shfl_mode_down failed");
   }
 
   auto res4 = cuda::ptx::shfl_sync_bfly(data, 2 /*offset*/, 0b11111 /*clamp*/, FullMask);
-  _CCCL_ASSERT(res4 == threadIdx.x ^ 2, "shfl_mode_bfly failed");
+  assert(res4 == threadIdx.x ^ 2, "shfl_mode_bfly failed");
 #endif // __cccl_ptx_isa >= 600
 }
 
@@ -88,7 +88,7 @@ __host__ __device__ void test_shfl_partial_mask()
   if (threadIdx.x <= 3)
   {
     auto res1 = cuda::ptx::shfl_sync_idx(data, pred1, 2 /*idx*/, 0b11111 /*clamp*/, PartialMask);
-    _CCCL_ASSERT(res1 == 2 && pred1, "shfl_mode_idx failed");
+    assert(res1 == 2 && pred1, "shfl_mode_idx failed");
   }
 #endif // __cccl_ptx_isa >= 600
 }
@@ -105,35 +105,34 @@ __host__ __device__ void test_shfl_partial_warp()
   auto res1 = cuda::ptx::shfl_sync_idx(data, pred1, 2 /*idx*/, clamp_segmark, FullMask);
   if (threadIdx.x < 16)
   {
-    _CCCL_ASSERT(res1 == 2 && pred1, "shfl_mode_idx failed");
+    assert(res1 == 2 && pred1, "shfl_mode_idx failed");
   }
   else
   {
-    _CCCL_ASSERT(res1 == 16 + 2 && pred1, "shfl_mode_idx failed");
+    assert(res1 == 16 + 2 && pred1, "shfl_mode_idx failed");
   }
 
   auto res2 = cuda::ptx::shfl_sync_up(data, pred2, 2 /*offset*/, (max_lane_mask << 8), FullMask);
-  printf("%d:  res2 = %d, pred2 = %d\n", threadIdx.x, res2, pred2);
   if (threadIdx.x <= 1 || threadIdx.x == 16 || threadIdx.x == 17)
   {
-    _CCCL_ASSERT(res2 == threadIdx.x && !pred2, "shfl_mode_up failed");
+    assert(res2 == threadIdx.x && !pred2, "shfl_mode_up failed");
   }
   else
   {
-    _CCCL_ASSERT(res2 == threadIdx.x - 2 && pred2, "shfl_mode_up failed");
+    assert(res2 == threadIdx.x - 2 && pred2, "shfl_mode_up failed");
   }
 
   auto res3 = cuda::ptx::shfl_sync_down(data, pred3, 2 /*offset*/, clamp_segmark, FullMask);
   if (threadIdx.x == 14 || threadIdx.x == 15 || threadIdx.x >= 30)
   {
-    _CCCL_ASSERT(res3 == threadIdx.x && !pred3, "shfl_mode_down failed");
+    assert(res3 == threadIdx.x && !pred3, "shfl_mode_down failed");
   }
   else
   {
-    _CCCL_ASSERT(res3 == threadIdx.x + 2 && pred3, "shfl_mode_down failed");
+    assert(res3 == threadIdx.x + 2 && pred3, "shfl_mode_down failed");
   }
 
   auto res4 = cuda::ptx::shfl_sync_bfly(data, pred4, 2 /*offset*/, clamp_segmark, FullMask);
-  _CCCL_ASSERT(res4 == threadIdx.x ^ 2 && pred4, "shfl_mode_bfly failed");
+  assert(res4 == threadIdx.x ^ 2 && pred4, "shfl_mode_bfly failed");
 #endif // __cccl_ptx_isa >= 600
 }
