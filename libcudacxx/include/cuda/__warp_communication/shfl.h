bc6802398a14dde34bbe6e7ff0190fdf1404790a
fbusato <fbusato@nvidia.com>
2025-02-21 15:50:31 -0800

    warp_shuffle implementation

--- libcudacxx/include/cuda/__warp_communication/shfl.h
+++ libcudacxx/include/cuda/__warp_communication/shfl.h
@@ -13,10 +13,6 @@
 
 #include <cuda/std/detail/__config>
 
-#include <cstdint>
-
-#include "cuda/__cmath/ceil_div.h"
-
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
@@ -25,34 +21,50 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/__cmath/ceil_div.h>
+#include <cuda/__ptx/instructions/get_sreg.h>
 #include <cuda/__ptx/instructions/shfl_sync.h>
 #include <cuda/std/__bit/has_single_bit.h>
 #include <cuda/std/__concepts/concept_macros.h>
+#include <cuda/std/__type_traits/integral_constant.h>
 #include <cuda/std/cstdint>
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 
 template <typename _Tp>
-_CCCL_NODISCARD _CCCL_HIDE_FROM_ABI _CCCL_DEVICE _Tp
-shfl(_Tp __data, int __src_lane, uint32_t __mask = 0xFFFFFFFF, int __width = /*warp size=*/32)
+struct ShuffleResult
 {
-  _CCCL_ASSERT(_CUDA_VSTD::has_single_bit(static_cast<uint32_t>(__width)) && __width <= 32,
-               "__width must be a power of 2 and less or equal to the warp size");
-  if (__width == 1)
+  _Tp data;
+  bool pred;
+};
+
+template <int _Width = 32, typename _Tp>
+_CCCL_NODISCARD _CCCL_HIDE_FROM_ABI _CCCL_DEVICE ShuffleResult<_Tp> warp_shuffle(
+  _Tp __data, int __src_lane, uint32_t __lane_mask = 0xFFFFFFFF, _CUDA_VSTD::integral_constant<int, _Width> = {})
+{
+  constexpr int __warp_size = 32;
+  static_assert(_CUDA_VSTD::has_single_bit(static_cast<uint32_t>(_Width)) && _Width >= 1 && _Width <= __warp_size,
+                "_Width must be a power of 2 and less or equal to the warp size");
+  if constexpr (_Width == 1)
   {
     return __data;
   }
-  constexpr auto __ratio = ::cuda::ceil_div(sizeof(_Tp), sizeof(uint32_t));
-  uint32_t __array[__ratio];
-  ::memcpy(__array, &__data, sizeof(_Tp));
-#pragma unroll
-  for (int i = 0; i < __ratio; ++i)
+  else
   {
-    __array[i] = ::cuda::ptx::shfl(__array[i], __src_lane, __mask, __width);
+    constexpr auto __ratio = ::cuda::ceil_div(sizeof(_Tp), sizeof(uint32_t));
+    auto __clamp_segmask   = ::cuda::ptx::get_sreg_laneid() | ((__warp_size - _Width) << 8);
+    bool __pred;
+    uint32_t __array[__ratio];
+    ::memcpy(__array, &__data, sizeof(_Tp));
+#pragma unroll
+    for (int i = 0; i < __ratio; ++i)
+    {
+      __array[i] = ::cuda::ptx::shfl_sync_idx(__array[i], &__pred, __src_lane, __clamp_segmask, __lane_mask);
+    }
+    _Tp __result;
+    ::memcpy(&__result, __array, sizeof(_Tp));
+    return ShuffleResult{__result, __pred};
   }
-  _Tp result;
-  ::memcpy(&result, __array, sizeof(_Tp));
-  return result;
 }
 
 _LIBCUDACXX_END_NAMESPACE_CUDA
