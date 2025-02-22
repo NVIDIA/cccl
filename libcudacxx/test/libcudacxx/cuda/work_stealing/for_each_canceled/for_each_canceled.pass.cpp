761d0885a4f1e3b78a881210fd71bcade566f20e
Gonzalo Brito Gadeschi <gonzalob@nvidia.com>
2025-02-22 04:05:28 -0800

    Add cluster tests

--- libcudacxx/test/libcudacxx/cuda/work_stealing/for_each_canceled/for_each_canceled.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/work_stealing/for_each_canceled/for_each_canceled.pass.cpp
@@ -19,27 +19,33 @@ namespace cg = cooperative_groups;
 
 #if !defined(TEST_HAS_NO_INT128_T)
 
-__device__ void vec_add_impl1(int* a, int* b, int* c, int n, dim3 block_idx)
+__device__ int vec_add_impl1(int* a, int* b, int* c, int n, dim3 block_idx)
 {
   int idx = threadIdx.x + block_idx.x * blockDim.x;
   if (idx < n)
   {
-    c[idx] += a[idx] + b[idx];
+    int s = a[idx] + b[idx];
+    c[idx] += s;
+    return s;
   }
+  return 0;
 }
 
-__device__ void vec_add_impl2(int* a, int* b, int* c, int n, dim3 block_idx)
+__device__ int vec_add_impl2(int* a, int* b, int* c, int n, dim3 block_idx)
 {
   int x_idx = threadIdx.x + (block_idx.x * blockDim.x);
   int y_idx = threadIdx.y + (block_idx.y * blockDim.y);
   int idx   = x_idx + y_idx * (blockDim.x * gridDim.x);
   if (idx < n)
   {
-    c[idx] += a[idx] + b[idx];
+    int s = a[idx] + b[idx];
+    c[idx] += s;
+    return s;
   }
+  return 0;
 }
 
-__device__ void vec_add_impl3(int* a, int* b, int* c, int n, dim3 block_idx)
+__device__ int vec_add_impl3(int* a, int* b, int* c, int n, dim3 block_idx)
 {
   int x_idx = threadIdx.x + (block_idx.x * blockDim.x);
   int y_idx = threadIdx.y + (block_idx.y * blockDim.y);
@@ -47,16 +53,22 @@ __device__ void vec_add_impl3(int* a, int* b, int* c, int n, dim3 block_idx)
   int idx   = x_idx + y_idx * (blockDim.x * gridDim.x) + z_idx * (blockDim.x * gridDim.x * blockDim.y * gridDim.y);
   if (idx < n)
   {
-    c[idx] += a[idx] + b[idx];
+    int s = a[idx] + b[idx];
+    c[idx] += s;
+    return s;
   }
+  return 0;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// for_each_cancel_block tests:
 
 __global__ void vec_add_det1(int* s, int* a, int* b, int* c, int n, int leader_tidx = 0)
 {
   int thread_sum = 0;
   bool leader = threadIdx.x == leader_tidx;
-  ::cuda::__for_each_canceled_block<1>(leader, [=](dim3 block_idx) {
-    vec_add_impl1(a, b, c, n, block_idx);
+  ::cuda::__for_each_canceled_block<1>(leader, [&](dim3 block_idx) {
+    thread_sum += vec_add_impl1(a, b, c, n, block_idx);
   });
   auto block = cg::this_thread_block();
   auto tile = cg::tiled_partition<32>(block);  
@@ -67,8 +79,8 @@ __global__ void vec_add_det2(int* s, int* a, int* b, int* c, int n, int leader_t
 {
   int thread_sum = 0;
   bool leader = threadIdx.x == leader_tidx && threadIdx.y == leader_tidx;
-  ::cuda::__for_each_canceled_block<2>(threadIdx.x == leader_tidx && threadIdx.y == leader_tidx, [=](dim3 block_idx) {
-    vec_add_impl2(a, b, c, n, block_idx);
+  ::cuda::__for_each_canceled_block<2>(leader, [&](dim3 block_idx) {
+    thread_sum += vec_add_impl2(a, b, c, n, block_idx);
   });
   auto block = cg::this_thread_block();
   auto tile = cg::tiled_partition<32>(block);  
@@ -79,10 +91,10 @@ __global__ void vec_add_det3(int* s, int* a, int* b, int* c, int n, int leader_t
 {
   int thread_sum = 0;
   bool leader = threadIdx.x == leader_tidx && threadIdx.y == leader_tidx && threadIdx.z == leader_tidx;
-  ::cuda::__for_each_canceled_block<3>(
-    threadIdx.x == leader_tidx && threadIdx.y == leader_tidx && threadIdx.z == leader_tidx, [=](dim3 block_idx) {
-      vec_add_impl3(a, b, c, n, block_idx);
-  });
+  ::cuda::__for_each_canceled_block<3>(leader
+    , [&](dim3 block_idx) {
+      thread_sum += vec_add_impl3(a, b, c, n, block_idx);
+    });
   auto block = cg::this_thread_block();
   auto tile = cg::tiled_partition<32>(block);  
   cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});  
@@ -91,49 +103,94 @@ __global__ void vec_add_det3(int* s, int* a, int* b, int* c, int n, int leader_t
 __global__ void vec_add1(int* s, int* a, int* b, int* c, int n)
 {
   int thread_sum = 0;
-  cuda::for_each_canceled_block<1>([=](dim3 block_idx) {
-    vec_add_impl1(a, b, c, n, block_idx);
+  cuda::for_each_canceled_block<1>([&](dim3 block_idx) {
+    thread_sum += vec_add_impl1(a, b, c, n, block_idx);
   });
   auto block = cg::this_thread_block();
   auto tile = cg::tiled_partition<32>(block);  
   cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});  
 }
 
-__global__ void vec_add2(int* a, int* b, int* c, int n)
-{
-  int thread_sum = 0;  
-  cuda::for_each_canceled_block<2>([=](dim3 block_idx) {
-    vec_add_impl2(a, b, c, n, block_idx);
-  });
-  auto block = cg::this_thread_block();
-  auto tile = cg::tiled_partition<32>(block);  
-  cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});  
-}
-
-__global__ void vec_add3(int* a, int* b, int* c, int n)
+__global__ void vec_add2(int* s, int* a, int* b, int* c, int n)
 {
   int thread_sum = 0;
-  cuda::for_each_canceled_block<3>([=](dim3 block_idx) {
-    vec_add_impl3(a, b, c, n, block_idx);
+  cuda::for_each_canceled_block<2>([&](dim3 block_idx) {
+    thread_sum += vec_add_impl2(a, b, c, n, block_idx);
+  });
+  auto block = cg::this_thread_block();
+  auto tile = cg::tiled_partition<32>(block);  
+  cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});  
+}
+
+__global__ void vec_add3(int* s, int* a, int* b, int* c, int n)
+{
+  int thread_sum = 0;
+  cuda::for_each_canceled_block<3>([&](dim3 block_idx) {
+    thread_sum += vec_add_impl3(a, b, c, n, block_idx);
   });
   cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});  
 }
 
-constexpr int CLUSTER_DIM = 2;
+////////////////////////////////////////////////////////////////////////////////
+// for_each_cancel_cluster tests:
 
-__global__ void  __cluster_dims__(CLUSTER_DIM, 1, 1) cluster_vec_add_det1(int* s, int* a, int* b, int* c, int n, int leader_bidx , int leader_tidx) {
+constexpr int CLUSTER_DIM1 = 8;
+constexpr int CLUSTER_DIM2 = 2;
+constexpr int CLUSTER_DIM3 = 2;
+
+__global__ void  __cluster_dims__(CLUSTER_DIM1, 1, 1) cluster_vec_add_det1(int* s, int* a, int* b, int* c, int n, int leader_bidx , int leader_tidx) {
   dim3 local_block_idx = cooperative_groups::cluster_group::block_index();
-  int thread_sum = 0;  
-  ::cuda::__for_each_canceled_cluster<1>(local_block_idx.x == leader_bidx, threadIdx.x == leader_tidx, [&](dim3 block_idx) {
+  int thread_sum = 0;
+  bool leader_block = local_block_idx.x == leader_bidx;
+  bool leader_threads = threadIdx.x == leader_tidx;
+  ::cuda::__for_each_canceled_cluster<1>(leader_block, leader_threads, [&](dim3 block_idx) {
     thread_sum += vec_add_impl1(a, b, c, n, block_idx);
   });
   cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});
 }
 
-__global__ void  __cluster_dims__(CLUSTER_DIM, 1, 1) cluster_vec_add1(int* s, int* a, int* b, int* c, int n) {
+__global__ void  __cluster_dims__(CLUSTER_DIM2, CLUSTER_DIM2, 1) cluster_vec_add_det2(int* s, int* a, int* b, int* c, int n, int leader_bidx , int leader_tidx) {
+  dim3 local_block_idx = cooperative_groups::cluster_group::block_index();
+  int thread_sum = 0;
+  bool leader_block = local_block_idx.x == leader_bidx && local_block_idx.y == leader_bidx;
+  bool leader_threads = threadIdx.x == leader_tidx && threadIdx.y == leader_tidx;
+  ::cuda::__for_each_canceled_cluster<2>(leader_block, leader_threads, [&](dim3 block_idx) {
+    thread_sum += vec_add_impl2(a, b, c, n, block_idx);
+  });
+  cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});
+}
+
+__global__ void  __cluster_dims__(CLUSTER_DIM3, CLUSTER_DIM3, CLUSTER_DIM3) cluster_vec_add_det3(int* s, int* a, int* b, int* c, int n, int leader_bidx , int leader_tidx) {
+  dim3 local_block_idx = cooperative_groups::cluster_group::block_index();
+  int thread_sum = 0;
+  bool leader_block = local_block_idx.x == leader_bidx && local_block_idx.y == leader_bidx && local_block_idx.z == leader_bidx;
+  bool leader_threads = threadIdx.x == leader_tidx && threadIdx.y == leader_tidx && threadIdx.z == leader_tidx;
+  ::cuda::__for_each_canceled_cluster<3>(leader_block, leader_threads, [&](dim3 block_idx) {
+    thread_sum += vec_add_impl3(a, b, c, n, block_idx);
+  });
+  cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});
+}
+
+__global__ void  __cluster_dims__(CLUSTER_DIM1, 1, 1) cluster_vec_add1(int* s, int* a, int* b, int* c, int n) {
   int thread_sum = 0;  
-  ::cuda::__for_each_canceled_cluster<1>([&](dim3 block_idx) {
+  ::cuda::for_each_canceled_cluster<1>([&](dim3 block_idx) {
     thread_sum += vec_add_impl1(a, b, c, n, block_idx);
+  });
+  cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});
+}
+
+__global__ void  __cluster_dims__(CLUSTER_DIM2, CLUSTER_DIM2, 1) cluster_vec_add2(int* s, int* a, int* b, int* c, int n) {
+  int thread_sum = 0;  
+  ::cuda::for_each_canceled_cluster<2>([&](dim3 block_idx) {
+    thread_sum += vec_add_impl2(a, b, c, n, block_idx);
+  });
+  cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});
+}
+
+__global__ void  __cluster_dims__(CLUSTER_DIM3, CLUSTER_DIM3, CLUSTER_DIM3) cluster_vec_add3(int* s, int* a, int* b, int* c, int n) {
+  int thread_sum = 0;  
+  ::cuda::for_each_canceled_cluster<3>([&](dim3 block_idx) {
+    thread_sum += vec_add_impl3(a, b, c, n, block_idx);
   });
   cg::reduce_update_async(cg::tiled_partition<32>(cg::this_thread_block()), cuda::atomic_ref{*s}, thread_sum, cg::plus<int>{});
 }
@@ -159,11 +216,14 @@ void test(int N, F&& f)
     f(s, a, b, c, N, tidx);
     assert(cudaGetLastError() == cudaSuccess);
     assert(cudaDeviceSynchronize() == cudaSuccess);
-
+    
+    int sref = 0;
     for (int i = 0; i < N; ++i)
     {
-      assert(c[i] == (1 + i));
+      sref += c[i];
+      assert(c[i] != (1 + i));
     }
+    assert(*s == sref);
 
     cudaFree(s);
     cudaFree(a);
@@ -175,30 +235,34 @@ void test(int N, F&& f)
 void test()
 {
   const int N = 1000000;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // for_each_canceled_block:
+  
   {
-    auto fn = [](int* a, int* b, int* c, int n, int tidx) {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
       int tpb = 256;
       int bpg = (n + tpb - 1) / tpb;
-      vec_add_det1<<<bpg, tpb>>>(a, b, c, n, tidx);
+      vec_add_det1<<<bpg, tpb>>>(s, a, b, c, n, tidx);
     };
     test(N, fn);
   }
 
   {
-    auto fn = [](int* a, int* b, int* c, int n, int tidx) {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
       dim3 tpb(16, 16, 1);
       int ntpb = tpb.x * tpb.y; // 256
       int bpg  = (n + ntpb - 1) / ntpb;
       int bpgx = (int) cuda::std::sqrt(bpg) + 1;
       int bpgy = bpgx;
       assert((bpgx * bpgy) >= bpg);
-      vec_add_det2<<<dim3(bpgx, bpgy, 1), tpb>>>(a, b, c, n, tidx);
+      vec_add_det2<<<dim3(bpgx, bpgy, 1), tpb>>>(s, a, b, c, n, tidx);
     };
     test(N, fn);
   }
 
   {
-    auto fn = [](int* a, int* b, int* c, int n, int tidx) {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
       dim3 tpb(8, 8, 8);
       int ntpb = tpb.x * tpb.y * tpb.z; // 512
       int bpg  = (n + ntpb - 1) / ntpb;
@@ -206,35 +270,35 @@ void test()
       int bpgy = bpgx;
       int bpgz = bpgx;
       assert((bpgx * bpgy * bpgz) >= bpg);
-      vec_add_det3<<<dim3(bpgx, bpgy, bpgz), tpb>>>(a, b, c, n, tidx);
+      vec_add_det3<<<dim3(bpgx, bpgy, bpgz), tpb>>>(s, a, b, c, n, tidx);
     };
     test(N, fn);
   }
 
   {
-    auto fn = [](int* a, int* b, int* c, int n, int tidx) {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
       int tpb = 256;
       int bpg = (n + tpb - 1) / tpb;
-      vec_add1<<<bpg, tpb>>>(a, b, c, n);
+      vec_add1<<<bpg, tpb>>>(s, a, b, c, n);
     };
     test(N, fn);
   }
 
   {
-    auto fn = [](int* a, int* b, int* c, int n, int tidx) {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
       dim3 tpb(16, 16, 1);
       int ntpb = tpb.x * tpb.y; // 256
       int bpg  = (n + ntpb - 1) / ntpb;
       int bpgx = (int) cuda::std::sqrt(bpg) + 1;
       int bpgy = bpgx;
       assert((bpgx * bpgy) >= bpg);
-      vec_add2<<<dim3(bpgx, bpgy, 1), tpb>>>(a, b, c, n);
+      vec_add2<<<dim3(bpgx, bpgy, 1), tpb>>>(s, a, b, c, n);
     };
     test(N, fn);
   }
 
   {
-    auto fn = [](int* a, int* b, int* c, int n, int tidx) {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
       dim3 tpb(8, 8, 8);
       int ntpb = tpb.x * tpb.y * tpb.z; // 512
       int bpg  = (n + ntpb - 1) / ntpb;
@@ -242,7 +306,88 @@ void test()
       int bpgy = bpgx;
       int bpgz = bpgx;
       assert((bpgx * bpgy * bpgz) >= bpg);
-      vec_add3<<<dim3(bpgx, bpgy, bpgz), tpb>>>(a, b, c, n);
+      vec_add3<<<dim3(bpgx, bpgy, bpgz), tpb>>>(s, a, b, c, n);
+    };
+    test(N, fn);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // for_each_canceled_cluster:
+
+  {
+    auto fn = [](int* s, int* a, int* b, int* c, size_t n, int tidx) {
+      int tpb = 256;
+      int bpg = (n + tpb - 1) / tpb + 1;
+      while (bpg % CLUSTER_DIM1 != 0) bpg++;
+      cluster_vec_add_det1<<<bpg, tpb>>>(s, a, b, c, n, 0, tidx);
+    };
+    test(N, fn);
+  }
+
+  {
+    auto fn = [](int* s, int* a, int* b, int* c, size_t n, int tidx) {
+      dim3 tpb(16, 16, 1);
+      int ntpb = tpb.x * tpb.y; // 256
+      int bpg  = (n + ntpb - 1) / ntpb;
+      int bpgx = (int) cuda::std::sqrt(bpg) + 1;
+      while (bpgx % CLUSTER_DIM2 != 0) bpgx++;
+      int bpgy = bpgx;
+      assert((bpgx * bpgy) >= bpg);
+      cluster_vec_add_det2<<<dim3(bpgx, bpgy, 1), tpb>>>(s, a, b, c, n, 0, tidx);
+    };
+    test(N, fn);
+  }
+
+  {
+    auto fn = [](int* s, int* a, int* b, int* c, size_t n, int tidx) {
+      dim3 tpb(8, 8, 8);
+      int ntpb = tpb.x * tpb.y * tpb.z; // 512
+      int bpg  = (n + ntpb - 1) / ntpb;
+      int bpgx = (int) cuda::std::pow(bpg, 1. / 3.) + 1;
+      while (bpgx % CLUSTER_DIM3 != 0) bpgx++;
+      int bpgy = bpgx;
+      int bpgz = bpgx;
+      assert((bpgx * bpgy * bpgz) >= bpg);
+      cluster_vec_add_det3<<<dim3(bpgx, bpgy, bpgz), tpb>>>(s, a, b, c, n, 0, tidx);
+    };
+    test(N, fn);
+  }
+
+  {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
+      int tpb = 256;
+      int bpg = (n + tpb - 1) / tpb;
+      while (bpg % CLUSTER_DIM1 != 0) bpg++;
+      cluster_vec_add1<<<bpg, tpb>>>(s, a, b, c, n);
+    };
+    test(N, fn);
+  }
+
+  {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
+      dim3 tpb(16, 16, 1);
+      int ntpb = tpb.x * tpb.y; // 256
+      int bpg  = (n + ntpb - 1) / ntpb;
+      int bpgx = (int) cuda::std::sqrt(bpg) + 1;
+      while (bpgx % CLUSTER_DIM2 != 0) bpgx++;
+      int bpgy = bpgx;
+      assert((bpgx * bpgy) >= bpg);
+      cluster_vec_add2<<<dim3(bpgx, bpgy, 1), tpb>>>(s, a, b, c, n);
+    };
+    test(N, fn);
+  }
+
+  {
+    auto fn = [](int* s, int* a, int* b, int* c, int n, int tidx) {
+      dim3 tpb(8, 8, 8);
+      int ntpb = tpb.x * tpb.y * tpb.z; // 512
+      int bpg  = (n + ntpb - 1) / ntpb;
+      int bpgx = (int) cuda::std::pow(bpg, 1. / 3.) + 1;
+      while (bpgx % CLUSTER_DIM3 != 0) bpgx++;      
+      int bpgy = bpgx;
+      int bpgz = bpgx;
+      assert((bpgx * bpgy * bpgz) >= bpg);
+      cluster_vec_add3<<<dim3(bpgx, bpgy, bpgz), tpb>>>(s, a, b, c, n);
     };
     test(N, fn);
   }
