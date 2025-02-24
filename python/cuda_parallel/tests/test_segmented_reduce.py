b96e9e208c4690367dc12af752d83486428aa7db
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-02-24 16:57:47 -0600

    Add a test for segmented_reduce on gpu_struct

--- python/cuda_parallel/tests/test_segmented_reduce.py
+++ python/cuda_parallel/tests/test_segmented_reduce.py
@@ -6,9 +6,12 @@ import cupy as cp
 import numpy as np
 
 import cuda.parallel.experimental.algorithms as algorithms
+from cuda.parallel.experimental.struct import gpu_struct
 
 
 def test_segmented_reduce(input_array):
+    "Test for all supported input types"
+
     def binary_op(a, b):
         return a + b
 
@@ -47,3 +50,51 @@ def test_segmented_reduce(input_array):
         d_expected[i] = cp.sum(d_in[start_offsets[i] : end_offsets[i]])
 
     assert cp.all(d_out == d_expected)
+
+
+def test_segmented_reduce_struct_type():
+    import cupy as cp
+    import numpy as np
+
+    from cuda.parallel.experimental import algorithms
+
+    @gpu_struct
+    class Pixel:
+        r: np.int32
+        g: np.int32
+        b: np.int32
+
+    def max_g_value(x, y):
+        return x if x.g > y.g else y
+
+    def ceil_up(n, m):
+        return ((n + m - 1) // m) * m
+
+    segment_size = 64
+    n_pixels = ceil_up(4000, 64)
+    offsets = cp.arange(n_pixels + segment_size - 1, step=segment_size, dtype=np.int64)
+    start_offsets = offsets[:-1]
+    end_offsets = offsets[1:]
+    n_segments = start_offsets.size
+
+    d_rgb = cp.random.randint(0, 256, (n_pixels, 3), dtype=np.int32).view(Pixel.dtype)
+    d_out = cp.empty(n_segments, Pixel.dtype)
+
+    h_init = Pixel(0, 0, 0)
+
+    alg = algorithms.segmented_reduce(
+        d_rgb, d_out, start_offsets, end_offsets, max_g_value, h_init
+    )
+    temp_storage_bytes = alg(
+        None, d_rgb, d_out, n_segments, start_offsets, end_offsets, h_init
+    )
+
+    d_temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
+    _ = alg(
+        d_temp_storage, d_rgb, d_out, n_segments, start_offsets, end_offsets, h_init
+    )
+
+    h_rgb = np.reshape(d_rgb.get(), (n_segments, -1))
+    expected = h_rgb[np.arange(h_rgb.shape[0]), h_rgb["g"].argmax(axis=-1)]
+
+    np.testing.assert_equal(expected["g"], d_out.get()["g"])
