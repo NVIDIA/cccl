6937a177d59f99e27d000883975d75bf722a0cb6
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-02-24 08:10:46 -0600

    Improve readability of test_segmented_reduce_api example

--- python/cuda_parallel/tests/test_segmented_reduce_api.py
+++ python/cuda_parallel/tests/test_segmented_reduce_api.py
@@ -18,37 +18,28 @@ def test_device_segmented_reduce():
     h_init = np.asarray(max_val, dtype=dtype)
 
     offsets = cp.array([0, 7, 11, 16], dtype=np.int64)
+    first_segment = (8, 6, 7, 5, 3, 0, 9)
+    second_segment = (-4, 3, 0, 1)
+    third_segment = (3, 1, 11, 25, 8)
     d_input = cp.array(
-        [
-            8,
-            6,
-            7,
-            5,
-            3,
-            0,
-            9,  # first segment
-            -4,
-            3,
-            0,
-            1,  # second segment
-            3,
-            1,
-            11,
-            25,
-            8,  # third segment
-        ],
+        [*first_segment, *second_segment, *third_segment],
         dtype=dtype,
     )
-    d_output = cp.empty(offsets.size - 1, dtype=dtype)
+
+    start_o = offsets[:-1]
+    end_o = offsets[1:]
+
+    n_segments = start_o.size
+    d_output = cp.empty(n_segments, dtype=dtype)
 
     # Instantiate reduction for the given operator and initial value
     segmented_reduce = algorithms.segmented_reduce(
-        d_output, d_output, offsets, offsets, min_op, h_init
+        d_output, d_output, start_o, end_o, min_op, h_init
     )
 
     # Determine temporary device storage requirements
     temp_storage_size = segmented_reduce(
-        None, d_input, d_output, d_output.size, offsets[:-1], offsets[1:], h_init
+        None, d_input, d_output, n_segments, start_o, end_o, h_init
     )
 
     # Allocate temporary storage
@@ -56,13 +47,7 @@ def test_device_segmented_reduce():
 
     # Run reduction
     segmented_reduce(
-        d_temp_storage,
-        d_input,
-        d_output,
-        d_output.size,
-        offsets[:-1],
-        offsets[1:],
-        h_init,
+        d_temp_storage, d_input, d_output, n_segments, start_o, end_o, h_init
     )
 
     # Check the result is correct
