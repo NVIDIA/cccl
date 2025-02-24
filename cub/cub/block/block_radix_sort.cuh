e1a98c3b56015af3c7bdd9da59c5852b7b14afce
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-02-24 19:50:27 +0100

    No SASS diff for cub.test.device_radix_sort_pairs.lid_0 on SM86

--- cub/cub/block/block_radix_sort.cuh
+++ cub/cub/block/block_radix_sort.cuh
@@ -431,7 +431,7 @@ private:
     // Radix sorting passes
     while (true)
     {
-      int pass_bits = CUB_MIN(RADIX_BITS, end_bit - begin_bit);
+      int pass_bits = _CUDA_VSTD::min(RADIX_BITS, end_bit - begin_bit);
       auto digit_extractor =
         traits::template digit_extractor<fundamental_digit_extractor_t>(begin_bit, pass_bits, decomposer);
 
@@ -510,7 +510,7 @@ public:
     // Radix sorting passes
     while (true)
     {
-      int pass_bits = CUB_MIN(RADIX_BITS, end_bit - begin_bit);
+      int pass_bits = _CUDA_VSTD::min(RADIX_BITS, end_bit - begin_bit);
       auto digit_extractor =
         traits::template digit_extractor<fundamental_digit_extractor_t>(begin_bit, pass_bits, decomposer);
 
