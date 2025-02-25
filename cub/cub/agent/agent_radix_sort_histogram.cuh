054d7d933fc619166d9cb9ac3827d1bb4b0d0190
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-02-25 17:50:49 +0100

    Fix format

--- cub/cub/agent/agent_radix_sort_histogram.cuh
+++ cub/cub/agent/agent_radix_sort_histogram.cuh
@@ -39,7 +39,6 @@
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
-
 #  pragma clang system_header
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
