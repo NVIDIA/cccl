7cb85dfa8fc1adf71a42f776aeb5c7ff57df4e00
fbusato <fbusato@nvidia.com>
2025-02-25 12:26:29 -0800

    add additional tests

--- libcudacxx/test/libcudacxx/std/containers/views/mdspan/mdspan.aligned_accessor/aligned_accessor.pass.cpp
+++ libcudacxx/test/libcudacxx/std/containers/views/mdspan/mdspan.aligned_accessor/aligned_accessor.pass.cpp
@@ -33,19 +33,29 @@ __host__ __device__ constexpr bool test()
   using E = cuda::std::extents<size_t, 2>;
   using L = cuda::std::layout_right;
   using A = cuda::std::aligned_accessor<T, sizeof(T)>;
+  A aligned_non_const{};
+  cuda::std::aligned_accessor<const T, sizeof(T)> aligned_const{};
+  // basic tests
   cuda::std::array<T, 2> d{42, 43};
   cuda::std::mdspan<T, E, L, A> md(d.data(), 2);
   assert(md(0) == 42);
   assert(md(1) == 43);
+  // offset
+  assert(aligned_non_const.offset(d.data(), 0) == d.data());
+  assert(aligned_non_const.offset(d.data(), 2) == d.data() + 2);
 
-  A aligned_non_const{};
+  // different alignment
+  cuda::std::aligned_accessor<T, sizeof(T) * 2> aligned_x2{};
+  cuda::std::aligned_accessor<T, sizeof(T)> aligned_x1{aligned_x2};
+  unused(aligned_x1);
+
+  // aligned accessor <-> default accessor
   cuda::std::default_accessor<int> acc1{aligned_non_const};
   cuda::std::default_accessor<int> acc2 = aligned_non_const;
   unused(acc1);
   unused(acc2);
   take_default_accessor(aligned_non_const);
 
-  cuda::std::aligned_accessor<const T, sizeof(T)> aligned_const{};
   cuda::std::default_accessor<const T> acc3{aligned_const};
   cuda::std::default_accessor<const T> acc4 = aligned_const;
   unused(acc3);
