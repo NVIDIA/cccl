f3bf56fd8e8d0382315838e4bde53e5abae9d39d
fbusato <fbusato@nvidia.com>
2025-02-26 16:08:18 -0800

    try to remove macros for device code

--- libcudacxx/include/cuda/__bit/bitfield.h
+++ libcudacxx/include/cuda/__bit/bitfield.h
@@ -29,23 +29,19 @@
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 
-#if defined(__CUDA_ARCH__) || _CCCL_COMPILER(NVHPC)
-
-_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI uint32_t __bfi(uint32_t __value, int __start, int __width) noexcept
+_CCCL_NODISCARD _CCCL_HIDE_FROM_ABI _CCCL_DEVICE uint32_t __bfi(uint32_t __value, int __start, int __width) noexcept
 {
   uint32_t __ret;
   asm("bfi.b32 %0, %1, %2, %3, %4;" : "=r"(__ret) : "r"(0xFFFFFFFF), "r"(__value), "r"(__start), "r"(__width));
   return __ret;
 }
 
-_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI uint32_t __bfe(uint32_t __value, int __start, int __width) noexcept
+_CCCL_NODISCARD _CCCL_HIDE_FROM_ABI _CCCL_DEVICE uint32_t __bfe(uint32_t __value, int __start, int __width) noexcept
 {
   uint32_t __ret;
   asm("bfe.u32 %0, %1, %2, %3;" : "=r"(__ret) : "r"(__value), "r"(__start), "r"(__width));
   return __ret;
 }
-
-#endif // defined(__CUDA_ARCH__) || _CCCL_COMPILER(NVHPC)
 
 template <typename _Tp>
 _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr _Tp
