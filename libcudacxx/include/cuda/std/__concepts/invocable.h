a3f0602e66ffc1325fee98cc33e2ca5b33b93596
Michael Schellenberger Costa <miscco@nvidia.com>
2025-02-27 10:29:27 +0100

    Implement `ranges::drop_while_view`

--- libcudacxx/include/cuda/std/__concepts/invocable.h
+++ libcudacxx/include/cuda/std/__concepts/invocable.h
@@ -33,8 +33,8 @@ _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 template <class _Fn, class... _Args>
 concept invocable = requires(_Fn&& __fn, _Args&&... __args) {
-  _CUDA_VSTD::__invoke(_CUDA_VSTD::forward<_Fn>(__fn), _CUDA_VSTD::forward<_Args>(__args)...); // not required to be
-                                                                                               // equality preserving
+  // not required to be equality preserving
+  _CUDA_VSTD::invoke(_CUDA_VSTD::forward<_Fn>(__fn), _CUDA_VSTD::forward<_Args>(__args)...);
 };
 
 // [concept.regular.invocable]
