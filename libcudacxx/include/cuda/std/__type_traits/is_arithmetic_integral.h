6e0aab85eb4a38640d64f2a8306accb94d6dc85d
Michael Schellenberger Costa <miscco@nvidia.com>
2025-02-28 08:33:16 +0100

    Update libcudacxx/include/cuda/std/__type_traits/is_arithmetic_integral.h

--- libcudacxx/include/cuda/std/__type_traits/is_arithmetic_integral.h
+++ libcudacxx/include/cuda/std/__type_traits/is_arithmetic_integral.h
@@ -21,6 +21,8 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/std/__type_traits/remove_cv.h>
+
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 // __is_arithmetic_integral is a trait that tests whether a type is an integral type intended for arithmetic.
