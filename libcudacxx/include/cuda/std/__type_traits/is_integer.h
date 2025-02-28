229aadbd40701d6439f4365db89c6e8d92d5ddaf
David Bayer <bayer.david99@gmail.com>
2025-02-28 15:38:20 +0100

    fix compilation

--- libcudacxx/include/cuda/std/__type_traits/is_integer.h
+++ libcudacxx/include/cuda/std/__type_traits/is_integer.h
@@ -31,7 +31,7 @@ _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 template <class _Tp>
 _CCCL_INLINE_VAR constexpr bool __cccl_is_integer_v =
-  __cccl_is_signed_integer_v<remove_cv_t<_Tp>> || __cccl_is_unsigned_integer_v<remove_cv_t<_Tp>>;
+  __cccl_is_signed_integer_v<_Tp> || __cccl_is_unsigned_integer_v<_Tp>;
 
 _LIBCUDACXX_END_NAMESPACE_STD
 
