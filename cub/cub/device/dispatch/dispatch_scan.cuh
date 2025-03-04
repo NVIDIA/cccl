9beebb2abbd0c60bb717b53e7bdf78001727f94e
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-03-04 13:08:06 +0100

    Assert offset type in `DispatchScan[ByKey]` to be unsigned (#3992)

--- cub/cub/device/dispatch/dispatch_scan.cuh
+++ cub/cub/device/dispatch/dispatch_scan.cuh
@@ -165,6 +165,9 @@ template <
   typename KernelLauncherFactory = detail::TripleChevronFactory>
 struct DispatchScan
 {
+  static_assert(::cuda::std::is_unsigned_v<OffsetT> && sizeof(OffsetT) >= 4,
+                "DispatchScan only supports unsigned offset types of at least 4-bytes");
+
   //---------------------------------------------------------------------
   // Constants and Types
   //---------------------------------------------------------------------
