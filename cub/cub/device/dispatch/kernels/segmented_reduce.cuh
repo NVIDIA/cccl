140f9eb9d0d815d793aebeff4541f50302b74db9
Srinivas Yadav Singanaboina <ssinganaboin@nvidia.com>
2025-03-07 20:25:50 +0000

    use ::cuda::std::int64_t for global_segment_id

--- cub/cub/device/dispatch/kernels/segmented_reduce.cuh
+++ cub/cub/device/dispatch/kernels/segmented_reduce.cuh
@@ -266,7 +266,7 @@ __launch_bounds__(int(ChainedPolicyT::ActivePolicy::ReducePolicy::BLOCK_THREADS)
     const int lane_id           = tid % small_threads_per_warp;
     const int global_segment_id = bid * segments_per_small_block + sid_within_block;
 
-    const OffsetT segment_begin = global_segment_id * segment_size;
+    const ::cuda::std::int64_t segment_begin = global_segment_id * segment_size;
 
     // If empty segment, write out the initial value
     if (segment_size == 0)
@@ -300,7 +300,7 @@ __launch_bounds__(int(ChainedPolicyT::ActivePolicy::ReducePolicy::BLOCK_THREADS)
     const int lane_id           = tid % medium_threads_per_warp;
     const int global_segment_id = bid * segments_per_medium_block + sid_within_block;
 
-    const OffsetT segment_begin = global_segment_id * segment_size;
+    const ::cuda::std::int64_t segment_begin = global_segment_id * segment_size;
 
     if (global_segment_id < num_segments)
     {
@@ -320,7 +320,7 @@ __launch_bounds__(int(ChainedPolicyT::ActivePolicy::ReducePolicy::BLOCK_THREADS)
   }
   else
   {
-    const OffsetT segment_begin = bid * segment_size;
+    const ::cuda::std::int64_t segment_begin = bid * segment_size;
 
     // Consume input tiles
     AccumT block_aggregate = AgentReduceT(temp_storage.large_storage, d_in + segment_begin, reduction_op)
