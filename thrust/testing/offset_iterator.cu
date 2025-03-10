979e11378c301fce66bca4fd8736e52435a6c5d9
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-03-10 15:49:10 +0100

    Add thrust::offset_iterator

---
+++ thrust/testing/offset_iterator.cu
@@ -0,0 +1,126 @@
+#include <thrust/distance.h>
+#include <thrust/iterator/offset_iterator.h>
+
+#include <cuda/std/iterator>
+
+#include <unittest/unittest.h>
+
+// ensure that we properly support thrust::counting_iterator from cuda::std
+void TestOffsetIteratorTraits()
+{
+  using base_it    = thrust::host_vector<int>::iterator;
+  using it         = thrust::offset_iterator<base_it>;
+  using traits     = cuda::std::iterator_traits<it>;
+  using vec_traits = cuda::std::iterator_traits<base_it>;
+
+  static_assert(cuda::std::is_same_v<traits::difference_type, vec_traits::difference_type>);
+  static_assert(cuda::std::is_same_v<traits::value_type, vec_traits::value_type>);
+  static_assert(cuda::std::is_same_v<traits::pointer, vec_traits::pointer>);
+  static_assert(cuda::std::is_same_v<traits::reference, vec_traits::reference>);
+  static_assert(cuda::std::is_same_v<traits::iterator_category, vec_traits::iterator_category>);
+
+  static_assert(cuda::std::is_same_v<thrust::iterator_traversal_t<it>, thrust::random_access_traversal_tag>);
+
+  static_assert(cuda::std::__is_cpp17_random_access_iterator<it>::value);
+
+  static_assert(cuda::std::output_iterator<it, int>);
+  static_assert(cuda::std::input_iterator<it>);
+  static_assert(cuda::std::forward_iterator<it>);
+  static_assert(cuda::std::bidirectional_iterator<it>);
+  static_assert(cuda::std::random_access_iterator<it>);
+  static_assert(!cuda::std::contiguous_iterator<it>);
+}
+DECLARE_UNITTEST(TestOffsetIteratorTraits);
+
+template <typename Vector>
+void TestOffsetConstructor()
+{
+  thrust::offset_iterator<int*> iter0;
+  ASSERT_EQUAL(iter0.base(), nullptr);
+  ASSERT_EQUAL(iter0.offset(), 0);
+
+  Vector v{42, 43};
+  thrust::offset_iterator iter1(v.begin());
+  ASSERT_EQUAL_QUIET(iter1.base(), v.begin());
+  ASSERT_EQUAL(iter1.offset(), 0);
+  ASSERT_EQUAL(*iter1, 42);
+
+  thrust::offset_iterator iter2(v.begin(), 1);
+  ASSERT_EQUAL_QUIET(iter2.base(), v.begin());
+  ASSERT_EQUAL(iter2.offset(), 1);
+  ASSERT_EQUAL(*iter2, 43);
+}
+DECLARE_VECTOR_UNITTEST(TestOffsetConstructor);
+
+template <typename Vector>
+void TestOffsetIteratorCopyConstructorAndAssignment()
+{
+  Vector v{42, 43};
+  thrust::offset_iterator iter0(v.begin());
+  thrust::offset_iterator iter1(iter0);
+
+  ASSERT_EQUAL(iter0 == iter1, true);
+  ASSERT_EQUAL(*iter0 == *iter1, true);
+
+  thrust::offset_iterator iter2(v.begin() + 1);
+  ASSERT_EQUAL(iter0 != iter2, true);
+  ASSERT_EQUAL(*iter0 != *iter2, true);
+
+  iter2 = iter0;
+  ASSERT_EQUAL(iter0 == iter2, true);
+  ASSERT_EQUAL(*iter0 == *iter2, true);
+}
+DECLARE_VECTOR_UNITTEST(TestOffsetIteratorCopyConstructorAndAssignment);
+
+template <typename Vector>
+void TestOffsetIteratorIncrement()
+{
+  Vector v{-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
+  thrust::offset_iterator iter(v.begin() + 2); // start at 0
+
+  ASSERT_EQUAL(*iter, 0);
+  iter++;
+  ASSERT_EQUAL(*iter, 1);
+  iter++;
+  iter++;
+  ASSERT_EQUAL(*iter, 3);
+  iter += 5;
+  ASSERT_EQUAL(*iter, 8);
+  iter -= 10;
+  ASSERT_EQUAL(*iter, -2);
+}
+DECLARE_VECTOR_UNITTEST(TestOffsetIteratorIncrement);
+
+template <typename Vector>
+void TestOffsetIteratorComparisonAndDistance()
+{
+  Vector v(101);
+  thrust::offset_iterator iter1(v.begin());
+  thrust::offset_iterator iter2(v.begin());
+
+  ASSERT_EQUAL(iter1 == iter2, true);
+  ASSERT_EQUAL(iter1 - iter2, 0);
+  ASSERT_EQUAL(thrust::distance(iter1, iter2), 0);
+
+  iter1++;
+  ASSERT_EQUAL(iter1 == iter2, false);
+  ASSERT_EQUAL(iter1 - iter2, 1);
+  ASSERT_EQUAL(thrust::distance(iter1, iter2), -1);
+
+  iter2++;
+  ASSERT_EQUAL(iter1 == iter2, true);
+  ASSERT_EQUAL(iter1 - iter2, 0);
+  ASSERT_EQUAL(thrust::distance(iter1, iter2), 0);
+
+  iter1 += 100;
+  iter2 += 100;
+  ASSERT_EQUAL(iter1 == iter2, true);
+  ASSERT_EQUAL(iter1 - iter2, 0);
+  ASSERT_EQUAL(thrust::distance(iter1, iter2), 0);
+
+  iter1 -= 5;
+  ASSERT_EQUAL(iter1 == iter2, false);
+  ASSERT_EQUAL(iter1 - iter2, -5);
+  ASSERT_EQUAL(thrust::distance(iter1, iter2), 5);
+}
+DECLARE_VECTOR_UNITTEST(TestOffsetIteratorComparisonAndDistance);
---
+++ thrust/thrust/iterator/offset_iterator.h
@@ -0,0 +1,128 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#pragma once
+
+#include <thrust/detail/config.h>
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/iterator_adaptor.h>
+#include <thrust/iterator/iterator_facade.h>
+
+#include <cuda/std/cstdint>
+
+THRUST_NAMESPACE_BEGIN
+
+//! \addtogroup iterators
+//! \{
+
+//! \addtogroup fancyiterator Fancy Iterators
+//! \ingroup iterators
+//! \{
+
+//! \p offset_iterator wraps another iterator and an integral offset, apply the offset to the iterator when
+//! dereferencing, comparing, or computing the distance between two offset_iterators. This is useful, when the
+//! underlying iterator cannot be incremented, decremented, or advanced (e.g., because those operations are only
+//! supported in device code).
+//!
+//! The following code snippet demonstrates how to create a \p offset_iterator:
+//!
+//! \code
+//! #include <thrust/iterator/offset_iterator.h>
+//! #include <thrust/fill.h>
+//! #include <thrust/device_vector.h>
+//!
+//! int main()
+//! {
+//!   thrust::device_vector<int> data{1, 2, 3, 4};
+//!   auto b = offset_iterator{data.begin(), 1};
+//!   auto e = offset_iterator{data.end(), -1};
+//!   thrust::fill(b, e, 42);
+//!   // data is now [1, 42, 42, 4]
+//!   ++b; // does not call ++ on the underlying iterator
+//!   assert(b == e - 1);
+//!
+//!   return 0;
+//! }
+//! \endcode
+template <typename Iterator>
+class offset_iterator : public iterator_adaptor<offset_iterator<Iterator>, Iterator>
+{
+  //! \cond
+  friend class iterator_core_access;
+  using super_t = iterator_adaptor<offset_iterator<Iterator>, Iterator>;
+
+public:
+  using reference       = typename super_t::reference;
+  using difference_type = typename super_t::difference_type;
+  //! \endcond
+
+  _CCCL_HOST_DEVICE offset_iterator(Iterator it = {}, difference_type offset = {})
+      : super_t(::cuda::std::move(it))
+      , m_offset(offset)
+  {}
+
+  _CCCL_HOST_DEVICE const difference_type& offset() const
+  {
+    return m_offset;
+  }
+
+  _CCCL_HOST_DEVICE difference_type& offset()
+  {
+    return m_offset;
+  }
+
+  //! \cond
+
+private:
+  _CCCL_HOST_DEVICE reference dereference() const
+  {
+    return *(this->base() + m_offset);
+  }
+
+  _CCCL_EXEC_CHECK_DISABLE
+  _CCCL_HOST_DEVICE bool equal(const offset_iterator& other) const
+  {
+    return (this->base() + m_offset) == (other.base() + other.m_offset);
+  }
+
+  _CCCL_HOST_DEVICE void advance(difference_type n)
+  {
+    m_offset += n;
+  }
+
+  _CCCL_HOST_DEVICE void increment()
+  {
+    ++m_offset;
+  }
+
+  _CCCL_HOST_DEVICE void decrement()
+  {
+    --m_offset;
+  }
+
+  _CCCL_EXEC_CHECK_DISABLE
+  _CCCL_HOST_DEVICE difference_type distance_to(const offset_iterator& other) const
+  {
+    return (other.base() + other.m_offset) - (this->base() + m_offset);
+  }
+
+  difference_type m_offset;
+  //! \endcond
+};
+
+template <typename Iterator>
+_CCCL_HOST_DEVICE offset_iterator(Iterator) -> offset_iterator<Iterator>;
+
+//! \} // end fancyiterators
+//! \} // end iterators
+
+THRUST_NAMESPACE_END
