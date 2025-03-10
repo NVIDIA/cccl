41e0d869a7f24ac6bbe19e4e31925ab7ec87b806
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-03-10 10:35:52 -0500

    Add cuda.parallel.experimental.iterators._strided with NdArrayIterator
    The NdArrayIterator is an input iterator which traverses the array elements
    of a strided nd-array in the same order of corresponding flat array, but without
    making copies.
    
    This iterator enables two `test_segmented_reduce_api.py` examples:
    "segmented-reduce-columnwise-maximum" and "segmented-reduce-multiaxis-sum".

--- python/cuda_parallel/tests/test_segmented_reduce_api.py
+++ python/cuda_parallel/tests/test_segmented_reduce_api.py
@@ -105,3 +105,133 @@ def test_device_segmented_reduce_for_rowwise_sum():
     expected = cp.asarray(np.sum(mat, axis=-1))
     assert cp.all(d_output == expected)
     # example-end segmented-reduce-columnwise-total
+
+
+def test_segmented_reduce_for_columnwise_max():
+    # example-begin segmented-reduce-columnwise-maximum
+    import cupy as cp
+    import numpy as np
+
+    import cuda.parallel.experimental.algorithms as algorithms
+    import cuda.parallel.experimental.iterators as iterators
+    from cuda.parallel.experimental.iterators._strided import make_ndarray_iterator
+
+    def binary_op(a, b):
+        return max(a, b)
+
+    n_rows, n_cols = 123456, 78
+    rng = np.random.default_rng()
+    mat = rng.integers(low=-31, high=32, dtype=np.int32, size=(n_rows, n_cols))
+
+    def make_scaler(step):
+        def scale(col_id):
+            return col_id * step
+
+        return scale
+
+    zero = np.int32(0)
+    row_offset = make_scaler(np.int32(n_rows))
+    start_offsets = iterators.TransformIterator(
+        iterators.CountingIterator(zero), row_offset
+    )
+
+    end_offsets = start_offsets + 1
+
+    d_input = cp.asarray(mat)
+    # identity of max operator is the smallest value held by a type
+    h_init = np.full(tuple(), fill_value=np.iinfo(np.int32).min, dtype=np.int32)
+    d_output = cp.empty(n_cols, dtype=d_input.dtype)
+
+    # iterator input array permutted so that columns are traversed first
+    input_it = make_ndarray_iterator(d_input, (1, 0))
+
+    alg = algorithms.segmented_reduce(
+        input_it, d_output, start_offsets, end_offsets, binary_op, h_init
+    )
+
+    # query size of temporary storage and allocate
+    temp_nbytes = alg(
+        None, input_it, d_output, n_cols, start_offsets, end_offsets, h_init
+    )
+    temp_storage = cp.empty(temp_nbytes, dtype=cp.uint8)
+    # launch computation
+    alg(temp_storage, input_it, d_output, n_cols, start_offsets, end_offsets, h_init)
+
+    # Verify correctness
+    expected = cp.asarray(np.max(mat, axis=0))
+    assert cp.all(d_output == expected)
+    # example-end segmented-reduce-columnwise-maximum
+
+
+def test_segmented_reduce_for_multiaxis_sum():
+    # example-begin segmented-reduce-multiaxis-sum
+    import math
+
+    import cupy as cp
+    import numpy as np
+
+    import cuda.parallel.experimental.algorithms as algorithms
+    import cuda.parallel.experimental.iterators as iterators
+    from cuda.parallel.experimental.iterators._strided import make_ndarray_iterator
+
+    def binary_op(a, b):
+        return a + b
+
+    n0, n1, n2, n3 = 123, 18, 231, 17
+    rng = np.random.default_rng()
+    arr = rng.integers(low=-31, high=32, dtype=np.int32, size=(n0, n1, n2, n3))
+
+    def make_scaler(step):
+        def scale(id):
+            return id * step
+
+        return scale
+
+    reduce_axis = (0, 2)
+    iterate_axis = (1, 3)
+
+    reduce_nelems = math.prod([arr.shape[i] for i in reduce_axis])
+    iter_nelems = math.prod([arr.shape[i] for i in iterate_axis])
+
+    zero = np.int32(0)
+    scaler_fn = make_scaler(np.int32(reduce_nelems))
+    start_offsets = iterators.TransformIterator(
+        iterators.CountingIterator(zero), scaler_fn
+    )
+
+    end_offsets = start_offsets + 1
+
+    d_input = cp.asarray(arr)
+    # identity of max operator is the smallest value held by a type
+    h_init = np.full(tuple(), fill_value=0, dtype=np.int32)
+    d_output = cp.empty(iter_nelems, dtype=d_input.dtype)
+
+    # iterator input array permutted so that columns are traversed first
+    input_it = make_ndarray_iterator(d_input, iterate_axis + reduce_axis)
+
+    alg = algorithms.segmented_reduce(
+        input_it, d_output, start_offsets, end_offsets, binary_op, h_init
+    )
+
+    # query size of temporary storage and allocate
+    temp_nbytes = alg(
+        None, input_it, d_output, iter_nelems, start_offsets, end_offsets, h_init
+    )
+    temp_storage = cp.empty(temp_nbytes, dtype=cp.uint8)
+    # launch computation
+    alg(
+        temp_storage,
+        input_it,
+        d_output,
+        iter_nelems,
+        start_offsets,
+        end_offsets,
+        h_init,
+    )
+
+    # Verify correctness
+    actual = cp.reshape(d_output, tuple(arr.shape[i] for i in iterate_axis))
+    expected = cp.asarray(np.sum(arr, axis=reduce_axis))
+
+    assert cp.all(actual == expected)
+    # example-end segmented-reduce-multiaxis-sum
