f2d571bd9e473edb2b2154f1440d36a449deba8c
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-03-10 10:18:32 +0100

    Fix

--- thrust/thrust/detail/type_traits/minimum_type.h
+++ thrust/thrust/detail/type_traits/minimum_type.h
@@ -26,23 +26,24 @@
 #  pragma system_header
 #endif // no system header
 
-#include <thrust/detail/type_traits.h>
+#include <cuda/std/type_traits>
 
 THRUST_NAMESPACE_BEGIN
 
 namespace detail
 {
+struct no_minimum_type_marker
+{};
 
-//
-// Returns the minimum type or is empty
-// if T1 and T2 are unrelated.
-//
+// Returns the minimum type, or `no_minimum_type_marker`if T1 and T2 are unrelated.
 template <typename T1,
           typename T2,
           bool GreaterEqual = ::cuda::std::is_convertible_v<T1, T2>,
           bool LessEqual    = ::cuda::std::is_convertible_v<T2, T1>>
 struct smaller_type
-{};
+{
+  using type = T1;
+};
 
 // T1 >= T2
 template <typename T1, typename T2>
@@ -51,29 +52,11 @@ struct smaller_type<T1, T2, true, false>
   using type = T2;
 };
 
-// T1 <= T2
+// unordered
 template <typename T1, typename T2>
-struct smaller_type<T1, T2, false, true>
+struct smaller_type<T1, T2, false, false>
 {
-  using type = T1;
-};
-
-template <typename T1, typename T2>
-struct smaller_type<T1, T2, true, true>
-{
-  using type = T1;
-};
-
-template <typename T>
-struct smaller_type<T, T, true, true>
-{
-  using type = T;
-};
-
-template <typename T>
-struct smaller_type<T, T, false, false>
-{
-  using type = T;
+  using type = no_minimum_type_marker;
 };
 
 template <typename Head, typename... Tail>
@@ -86,10 +69,21 @@ struct minimum_type_impl<T>
   using type = T;
 };
 
-// Alias to the minimum type of the given types. All types are convertible to the minimum type. If no such type exists,
-// a SFINAE-friendly compile-time error is generated.
+// Has no nested ::type to produce a SFINAE-friendly error, in case the minimum_type is `no_minimum_type_marker`
+template <typename SFINAE, typename... Ts>
+struct minimum_type_check_marker
+{};
+
 template <typename... Ts>
-using minimum_type = typename minimum_type_impl<Ts...>::type;
+struct minimum_type_check_marker<
+  ::cuda::std::enable_if_t<!::cuda::std::is_same_v<typename minimum_type_impl<Ts...>::type, no_minimum_type_marker>>,
+  Ts...> : minimum_type_impl<Ts...>
+{};
+
+// Alias to the minimum type of the given pack. The minimum type is the one to which all other types are convertible.
+// If no such type exists, a SFINAE-friendly compile-time error is generated.
+template <typename... Ts>
+using minimum_type = typename minimum_type_check_marker<void, Ts...>::type;
 } // namespace detail
 
 THRUST_NAMESPACE_END
