3615c1697676602908837cd72ba5ef6af3517311
Michael Schellenberger Costa <miscco@nvidia.com>
2025-03-10 18:48:35 +0100

    Drop `TEST_EXEC_CHECK_DISABLE`

--- libcudacxx/test/support/test_macros.h
+++ libcudacxx/test/support/test_macros.h
@@ -399,8 +399,6 @@ inline void DoNotOptimize(Tp const& value)
 #  define TEST_ACCESSIBLE
 #endif
 
-#define TEST_EXEC_CHECK_DISABLE _CCCL_EXEC_CHECK_DISABLE
-
 #define STATIC_MEMBER_VAR(name, type)     \
   __host__ __device__ static type& name() \
   {                                       \
