2eaae0a02ab5aa436c71e9c3b0c5c8f2e04781e7
Michael Schellenberger Costa <miscco@nvidia.com>
2025-03-10 18:47:03 +0100

    Drop `TEST_CONSTEXPR_GLOBAL`

--- libcudacxx/test/support/test_macros.h
+++ libcudacxx/test/support/test_macros.h
@@ -425,8 +425,6 @@ __host__ __device__ constexpr bool unused(T&&...)
 #  define TEST_NV_DIAG_SUPPRESS(WARNING) _CCCL_PRAGMA(diag_suppress WARNING)
 #endif
 
-#define TEST_CONSTEXPR_GLOBAL constexpr
-
 // Some convenience macros for checking nvcc versions
 #if _CCCL_CUDACC_BELOW(12, 3)
 #  define TEST_COMPILER_CUDACC_BELOW_12_3
