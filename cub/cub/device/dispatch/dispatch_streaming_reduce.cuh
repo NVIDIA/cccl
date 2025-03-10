afc1b0f94b5e49430bc18cc8c1a8aaaa3f3cac67
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-03-10 17:31:08 +0100

    Offset iterators instead of wrapping them
    Fixes: #3767

--- cub/cub/device/dispatch/dispatch_streaming_reduce.cuh
+++ cub/cub/device/dispatch/dispatch_streaming_reduce.cuh
@@ -113,39 +113,6 @@ struct local_to_global_op
   }
 };
 
-/**
- * Offsets a given input iterator by a fixed offset, such that when an item at index `i` is accessed, the item
- * `it[*offset_it + i]` is accessed.
- */
-template <typename Iterator, typename OffsetItT>
-class offset_iterator : public THRUST_NS_QUALIFIER::iterator_adaptor<offset_iterator<Iterator, OffsetItT>, Iterator>
-{
-public:
-  using super_t = THRUST_NS_QUALIFIER::iterator_adaptor<offset_iterator, Iterator>;
-
-  _CCCL_HOST_DEVICE _CCCL_FORCEINLINE offset_iterator(const Iterator& it, OffsetItT offset_it)
-      : super_t(it)
-      , offset_it(offset_it)
-  {}
-
-  // befriend thrust::iterator_core_access to allow it access to the private interface below
-  friend class THRUST_NS_QUALIFIER::iterator_core_access;
-
-private:
-  OffsetItT offset_it;
-
-  _CCCL_HOST_DEVICE _CCCL_FORCEINLINE typename super_t::reference dereference() const
-  {
-    return *(this->base() + (*offset_it));
-  }
-};
-
-template <typename Iterator, typename OffsetItT>
-_CCCL_HOST_DEVICE offset_iterator<Iterator, OffsetItT> make_offset_iterator(Iterator it, OffsetItT offset_it)
-{
-  return offset_iterator<Iterator, OffsetItT>{it, offset_it};
-}
-
 /******************************************************************************
  * Single-problem streaming reduction dispatch
  *****************************************************************************/
@@ -230,8 +197,7 @@ struct dispatch_streaming_arg_reduce_t
 
     // Wrapped input iterator to produce index-value tuples, i.e., <PerPartitionOffsetT, InputT>-tuples
     // We make sure to offset the user-provided input iterator by the current partition's offset
-    using arg_index_input_iterator_t =
-      ArgIndexInputIterator<offset_iterator<InputIteratorT, constant_offset_it_t>, PerPartitionOffsetT, InitT>;
+    using arg_index_input_iterator_t = ArgIndexInputIterator<InputIteratorT, PerPartitionOffsetT, InitT>;
 
     // The type used for the aggregate that the user wants to find the extremum for
     using output_aggregate_t = InitT;
@@ -270,7 +236,7 @@ struct dispatch_streaming_arg_reduce_t
     // The current partition's input iterator is an ArgIndex iterator that generates indices relative to the beginning
     // of the current partition, i.e., [0, partition_size) along with an OffsetIterator that offsets the user-provided
     // input iterator by the current partition's offset
-    arg_index_input_iterator_t d_indexed_offset_in(make_offset_iterator(d_in, constant_offset_it_t{GlobalOffsetT{0}}));
+    arg_index_input_iterator_t d_indexed_offset_in(d_in);
 
     // Transforms the per-partition result to a global result by adding the current partition's offset to the arg result
     // of a partition
@@ -342,8 +308,7 @@ struct dispatch_streaming_arg_reduce_t
       const GlobalOffsetT current_num_items =
         (remaining_items < max_partition_size) ? remaining_items : max_partition_size;
 
-      d_indexed_offset_in = arg_index_input_iterator_t(
-        detail::reduce::make_offset_iterator(d_in, constant_offset_it_t{current_partition_offset}));
+      d_indexed_offset_in = arg_index_input_iterator_t(d_in + current_partition_offset);
 
       error = dispatch_reduce_t::Dispatch(
         d_temp_storage,
