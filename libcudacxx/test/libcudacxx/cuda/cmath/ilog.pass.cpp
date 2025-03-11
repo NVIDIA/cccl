d959859c3f3abeeef87f466711381f092e6dd9c6
fbusato <fbusato@nvidia.com>
2025-03-11 16:57:59 -0700

    ilog2 tests

---
+++ libcudacxx/test/libcudacxx/cuda/cmath/ilog.pass.cpp
@@ -0,0 +1,82 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the libcu++ Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/cmath>
+#include <cuda/std/cassert>
+#include <cuda/std/limits>
+#include <cuda/std/type_traits>
+
+#include "test_macros.h"
+
+template <class T>
+__host__ __device__ constexpr void test()
+{
+  int i = 0;
+  for (T value = 1; value <= cuda::std::numeric_limits<T>::max() / 2; value *= 2)
+  {
+    if (i >= 1)
+    {
+      assert(cuda::ilog2(static_cast<T>(value - 1)) == i - 1);
+      assert(cuda::ilog2(static_cast<T>(value + 1)) == i);
+    }
+    assert(cuda::ilog2(value) == i);
+    i++;
+  }
+}
+
+__host__ __device__ constexpr bool test()
+{
+  // Builtin integer types:
+  test<char>();
+  test<signed char>();
+  test<unsigned char>();
+
+  test<short>();
+  test<unsigned short>();
+
+  test<int>();
+  test<unsigned int>();
+
+  test<long>();
+  test<unsigned long>();
+
+  test<long long>();
+  test<unsigned long long>();
+
+#if !defined(TEST_COMPILER_NVRTC)
+  // cstdint types:
+  test<std::size_t>();
+  test<std::ptrdiff_t>();
+  test<std::intptr_t>();
+  test<std::uintptr_t>();
+
+  test<std::int8_t>();
+  test<std::int16_t>();
+  test<std::int32_t>();
+  test<std::int64_t>();
+
+  test<std::uint8_t>();
+  test<std::uint16_t>();
+  test<std::uint32_t>();
+  test<std::uint64_t>();
+#endif // !TEST_COMPILER_NVRTC
+
+#if _CCCL_HAS_INT128()
+  test<__int128_t>();
+  test<__uint128_t>();
+#endif // _CCCL_HAS_INT128()
+  return true;
+}
+
+int main(int, char**)
+{
+  test();
+  //  static_assert(test());
+  return 0;
+}
