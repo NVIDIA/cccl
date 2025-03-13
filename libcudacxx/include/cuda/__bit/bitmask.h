80609d15335b576432e4939ed093e19868aa0a8e
fbusato <fbusato@nvidia.com>
2025-03-12 17:02:18 -0700

    one more [[maybe_unused]]

--- libcudacxx/include/cuda/__bit/bitmask.h
+++ libcudacxx/include/cuda/__bit/bitmask.h
@@ -65,7 +65,7 @@ template <typename _Tp>
 _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr _Tp bitmask(int __start, int __width) noexcept
 {
   static_assert(_CUDA_VSTD::__cccl_is_unsigned_integer_v<_Tp>, "bitmask() requires unsigned integer types");
-  constexpr auto __digits = _CUDA_VSTD::numeric_limits<_Tp>::digits;
+  [[maybe_unused]] constexpr auto __digits = _CUDA_VSTD::numeric_limits<_Tp>::digits;
   _CCCL_ASSERT(__width >= 0 && __width <= __digits, "width out of range");
   _CCCL_ASSERT(__start >= 0 && __start <= __digits, "start position out of range");
   _CCCL_ASSERT(__start + __width <= __digits, "start position + width out of range");
