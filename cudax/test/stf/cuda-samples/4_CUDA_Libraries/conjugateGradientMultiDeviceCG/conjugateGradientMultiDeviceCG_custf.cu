fb2ac2663b499145f72eaef0c5a604902fea6399
Michael Schellenberger Costa <miscco@nvidia.com>
2025-03-13 11:15:32 +0100

    Ups

--- cudax/test/stf/cuda-samples/4_CUDA_Libraries/conjugateGradientMultiDeviceCG/conjugateGradientMultiDeviceCG_custf.cu
+++ cudax/test/stf/cuda-samples/4_CUDA_Libraries/conjugateGradientMultiDeviceCG/conjugateGradientMultiDeviceCG_custf.cu
@@ -316,9 +316,9 @@ gpuScaleVectorAndSaxpy(slice<float> x, slice<float> y, float a, float scale, int
 template <typename thread_hierarchy_t>
 __device__ void multiGpuConjugateGradient(
   thread_hierarchy_t t,
-  slice<int> I,
-  slice<int> J,
-  slice<float> val,
+  slice<const int> I,
+  slice<const int> J,
+  slice<const float> val,
   slice<float> x,
   slice<float> Ax,
   slice<float> p,
