d7ad60c5f058bf9c8f44d4832383acc5dbd25c0e
Michael Schellenberger Costa <miscco@nvidia.com>
2025-03-14 11:04:15 +0100

    Cleanup the definition of `_CCCL_HAS_EXCEPTIONS()`

--- libcudacxx/test/libcudacxx/std/utilities/expected/expected.void/ctor/ctor.copy.pass.cpp
+++ libcudacxx/test/libcudacxx/std/utilities/expected/expected.void/ctor/ctor.copy.pass.cpp
@@ -94,7 +94,7 @@ void test_exceptions()
   struct Throwing
   {
     Throwing() = default;
-    __host__ __device__ Throwing(const Throwing&)
+    Throwing(const Throwing&)
     {
       throw Except{};
     }
