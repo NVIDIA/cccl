65cdd507c1cd638b9f876864e1d6a521524fcb56
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2025-03-16 06:00:48 -0700

    Restore comment

--- cub/cub/device/dispatch/kernels/transform.cuh
+++ cub/cub/device/dispatch/kernels/transform.cuh
@@ -386,6 +386,7 @@ template <typename It>
 _CCCL_HOST_DEVICE auto make_iterator_kernel_arg(It it) -> kernel_arg<It>
 {
   kernel_arg<It> arg;
+  // since we switch the active member of the union, we must use placement new or construct_at. This also uses the copy
   // constructor of It, which works in more cases than assignment (e.g. thrust::transform_iterator with
   // non-copy-assignable functor, e.g. in merge sort tests)
   ::cuda::std::__construct_at(&arg.iterator, it);
