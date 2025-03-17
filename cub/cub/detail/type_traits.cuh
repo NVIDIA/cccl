776f2edfd74e876a217e8421d6c2f8d9419e8685
fbusato <fbusato@nvidia.com>
2025-03-17 11:58:49 -0700

    add is_fixed_size_random_access_range_v

--- cub/cub/detail/type_traits.cuh
+++ cub/cub/detail/type_traits.cuh
@@ -108,6 +108,9 @@ struct is_fixed_size_random_access_range<
 template <typename T>
 using is_fixed_size_random_access_range_t = typename is_fixed_size_random_access_range<T>::type;
 
+template <typename T>
+constexpr bool is_fixed_size_random_access_range_v = is_fixed_size_random_access_range_t<T>::value;
+
 /***********************************************************************************************************************
  * static_size: a type trait that returns the number of elements in an Array-like type
  **********************************************************************************************************************/
