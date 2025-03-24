5e66d567ead6e6bdfcf19e88ae0c2ad46fd24e1d
Cedric Augonnet <caugonnet@nvidia.com>
2025-03-24 14:40:56 +0100

    WIP save changes in the loop dispatch for stackable ctx

--- cudax/test/stf/loop_dispatch/stackable_loop_dispatch.cu
+++ cudax/test/stf/loop_dispatch/stackable_loop_dispatch.cu
@@ -21,22 +21,30 @@ int main()
   // Loop count
   int n = 1024;
 
-  auto lB = ctx.logical_data<int>(size_t(1024 * 1024));
+  auto lB = ctx.logical_data<int>(size_t(1024 * 1024)).set_symbol("B");
   ctx.parallel_for(lB.shape(), lB.write())->*[] __device__(size_t i, auto b) {
     b(i) = 42;
   };
 
-  loop_dispatch(ctx, exec_place::all_devices(), 0, n, [&](size_t) {
-    auto lA = ctx.logical_data<int>(size_t(1024 * 1024));
+  lB.set_read_only(true);
 
-    ctx.parallel_for(ctx.current_exec_place(), lA.shape(), lA.write())->*[] __device__(size_t i, auto a) {
-      a(i) = (int) (10.0 * cos((double) i));
-    };
+  for (size_t iter = 0; iter < 4; iter++)
+  {
+    loop_dispatch(ctx, exec_place::all_devices(), place_partition_scope::green_context, 0, n, [&](size_t iter) {
+      auto lA = ctx.logical_data<int>(size_t(1024 * 1024)).set_symbol(::std::string("A") + ::std::to_string(iter));
 
-    ctx.parallel_for(ctx.current_exec_place(), lA.shape(), lA.rw(), lB.read())->*[] __device__(size_t i, auto a, auto b) {
-      a(i) += b(i);
-    };
-  });
+      ctx.parallel_for(ctx.current_exec_place(), lA.shape(), lA.write()).set_symbol("pfor1" + ::std::to_string(iter))
+          ->*[] __device__(size_t i, auto a) {
+                a(i) = (int) (10.0 * cos((double) i));
+              };
+
+      ctx.parallel_for(ctx.current_exec_place(), lA.shape(), lA.rw(), lB.read())
+          .set_symbol("pfor2" + ::std::to_string(iter))
+          ->*[] __device__(size_t i, auto a, auto b) {
+                a(i) += b(i);
+              };
+    });
+  }
 
   ctx.finalize();
 }
