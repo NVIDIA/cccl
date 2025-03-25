1ee74f9297922987cc80a2795c65e0e0e0a973c3
David Bayer <bayer.david99@gmail.com>
2025-03-25 22:20:22 +0100

    Implement `__fp_neg`

--- libcudacxx/include/cuda/std/__floating_point/arithmetic.h
+++ libcudacxx/include/cuda/std/__floating_point/arithmetic.h
@@ -8,10 +8,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _LIBCUDACXX___FLOATING_POINT_FP_H
-#define _LIBCUDACXX___FLOATING_POINT_FP_H
+#ifndef _LIBCUDACXX___FLOATING_POINT_ARITHMETIC_H
+#define _LIBCUDACXX___FLOATING_POINT_ARITHMETIC_H
 
-#include <cuda/__cccl_config>
+#include <cuda/std/detail/__config>
 
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
@@ -21,17 +21,26 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/std/__floating_point/arithmetic.h>
-#include <cuda/std/__floating_point/cast.h>
-#include <cuda/std/__floating_point/cccl_fp.h>
-#include <cuda/std/__floating_point/common_type.h>
-#include <cuda/std/__floating_point/conversion_rank_order.h>
-#include <cuda/std/__floating_point/format.h>
 #include <cuda/std/__floating_point/mask.h>
 #include <cuda/std/__floating_point/native_type.h>
-#include <cuda/std/__floating_point/nvfp_types.h>
-#include <cuda/std/__floating_point/properties.h>
 #include <cuda/std/__floating_point/storage.h>
-#include <cuda/std/__floating_point/traits.h>
 
-#endif // _LIBCUDACXX___FLOATING_POINT_FP_H
+_LIBCUDACXX_BEGIN_NAMESPACE_STD
+
+template <class _Tp>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr _Tp __fp_neg(const _Tp& __v) noexcept
+{
+  if constexpr (__fp_is_native_type_v<_Tp>)
+  {
+    return -__v;
+  }
+  else
+  {
+    const auto __res_storage = _CUDA_VSTD::__fp_get_storage(__v) ^ __fp_sign_mask_of_v<_Tp>;
+    return _CUDA_VSTD::__fp_from_storage<_Tp>(static_cast<__fp_storage_of_t<_Tp>>(__res_storage));
+  }
+}
+
+_LIBCUDACXX_END_NAMESPACE_STD
+
+#endif // _LIBCUDACXX___FLOATING_POINT_ARITHMETIC_H
