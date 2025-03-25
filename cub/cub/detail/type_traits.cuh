b281fc161950073dccf97473ce898fbd5e976cb5
Federico Busato <50413820+fbusato@users.noreply.github.com>
2025-03-24 18:57:17 -0700

    Add new `WarpReduce` overloadings (#3884)
    Co-authored-by: Bernhard Manfred Gruber <bernhardmgruber@gmail.com>

--- cub/cub/detail/type_traits.cuh
+++ cub/cub/detail/type_traits.cuh
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -107,6 +107,9 @@ struct is_fixed_size_random_access_range<
 
 template <typename T>
 using is_fixed_size_random_access_range_t = typename is_fixed_size_random_access_range<T>::type;
+
+template <typename T>
+constexpr bool is_fixed_size_random_access_range_v = is_fixed_size_random_access_range_t<T>::value;
 
 /***********************************************************************************************************************
  * static_size: a type trait that returns the number of elements in an Array-like type
