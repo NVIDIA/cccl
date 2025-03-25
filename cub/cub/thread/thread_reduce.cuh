4ebfad1b939185dac4927b3898eccb4908fa8faa
fbusato <fbusato@nvidia.com>
2025-03-25 12:47:26 -0700

    fix second conflict typo

--- cub/cub/thread/thread_reduce.cuh
+++ cub/cub/thread/thread_reduce.cuh
@@ -290,7 +290,7 @@ _CCCL_NODISCARD _CCCL_DEVICE _CCCL_FORCEINLINE constexpr bool enable_simd_reduct
         (return enable_sm80_simd_reduction_v<T, ReductionOp, length> ||
                 enable_sm70_simd_reduction_v<T, ReductionOp, length>;),
       NV_PROVIDES_SM_70,
-        (return enable_sm70_simd_reduction<T, ReductionOp, length>();),
+        (return enable_sm70_simd_reduction_v<T, ReductionOp, length>;),
       NV_IS_DEVICE,
         (return false;)
     );
