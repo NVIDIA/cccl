28099838a94f37fdfa134e266a1a8718b91579b9
Michael Schellenberger Costa <miscco@nvidia.com>
2025-03-31 15:58:35 +0200

    Fix invalid condition

--- libcudacxx/include/cuda/std/__cccl/exceptions.h
+++ libcudacxx/include/cuda/std/__cccl/exceptions.h
@@ -27,7 +27,7 @@
 #elif _CCCL_COMPILER(NVRTC) // NVRTC has no exceptions
 #  define _CCCL_HAS_EXCEPTIONS() 0
 #elif _CCCL_COMPILER(MSVC) // MSVC needs special checks for `_HAS_EXCEPTIONS` and `_CPPUNWIND`
-#  define _CCCL_HAS_EXCEPTIONS() (_HAS_EXCEPTIONS == 0) && (_CPPUNWIND == 0)
+#  define _CCCL_HAS_EXCEPTIONS() (_HAS_EXCEPTIONS != 0) && (_CPPUNWIND != 0)
 #else // other compilers use `__EXCEPTIONS`
 #  define _CCCL_HAS_EXCEPTIONS() __EXCEPTIONS
 #endif // has exceptions
