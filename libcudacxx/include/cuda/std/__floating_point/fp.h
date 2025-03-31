31f33e131dfaf0ed0096955b70177c9be152657d
David Bayer <48736217+davebayer@users.noreply.github.com>
2025-03-31 13:54:31 +0200

    Implement fp overflow handlers (#4261)
    * Implement fp overflow handlers
    
    * I hate nvfp types
    
    * use `[[nodiscard]]`
    
    ---------
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- libcudacxx/include/cuda/std/__floating_point/fp.h
+++ libcudacxx/include/cuda/std/__floating_point/fp.h
@@ -31,6 +31,7 @@
 #include <cuda/std/__floating_point/mask.h>
 #include <cuda/std/__floating_point/native_type.h>
 #include <cuda/std/__floating_point/nvfp_types.h>
+#include <cuda/std/__floating_point/overflow_handler.h>
 #include <cuda/std/__floating_point/properties.h>
 #include <cuda/std/__floating_point/storage.h>
 #include <cuda/std/__floating_point/traits.h>
