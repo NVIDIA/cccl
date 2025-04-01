a4c90228767c29d50f037b91a1f23fe238f2be6d
David Bayer <bayer.david99@gmail.com>
2025-04-01 08:50:58 +0200

    Drop `_LIBCUDACXX_DEPRECATED_IN_[11,14,17]`

--- libcudacxx/include/cuda/std/__functional/binary_function.h
+++ libcudacxx/include/cuda/std/__functional/binary_function.h
@@ -25,7 +25,7 @@ _LIBCUDACXX_BEGIN_NAMESPACE_STD
 #if defined(_LIBCUDACXX_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION)
 
 template <class _Arg1, class _Arg2, class _Result>
-struct _CCCL_TYPE_VISIBILITY_DEFAULT _LIBCUDACXX_DEPRECATED_IN_CXX11 binary_function
+struct _CCCL_TYPE_VISIBILITY_DEFAULT _LIBCUDACXX_DEPRECATED binary_function
 {
   using first_argument_type  = _Arg1;
   using second_argument_type = _Arg2;
@@ -38,9 +38,9 @@ template <class _Arg1, class _Arg2, class _Result>
 struct __binary_function_keep_layout_base
 {
 #if _CCCL_STD_VER <= 2017 || defined(_LIBCUDACXX_ENABLE_CXX20_REMOVED_BINDER_TYPEDEFS)
-  using first_argument_type _LIBCUDACXX_DEPRECATED_IN_CXX17  = _Arg1;
-  using second_argument_type _LIBCUDACXX_DEPRECATED_IN_CXX17 = _Arg2;
-  using result_type _LIBCUDACXX_DEPRECATED_IN_CXX17          = _Result;
+  using first_argument_type _LIBCUDACXX_DEPRECATED  = _Arg1;
+  using second_argument_type _LIBCUDACXX_DEPRECATED = _Arg2;
+  using result_type _LIBCUDACXX_DEPRECATED          = _Result;
 #endif // _LIBCUDACXX_ENABLE_CXX20_REMOVED_BINDER_TYPEDEFS
 };
 
