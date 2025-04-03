e3ae3a2a3f3b6a75626a3c24d35b362c3166ec2e
Ashwin Srinath <shwina@users.noreply.github.com>
2025-04-03 10:40:58 -0400

    Linter

--- python/cuda_parallel/cuda/parallel/experimental/algorithms/__init__.py
+++ python/cuda_parallel/cuda/parallel/experimental/algorithms/__init__.py
@@ -8,8 +8,8 @@ from .reduce import reduce_into as reduce_into
 from .scan import exclusive_scan as exclusive_scan
 from .scan import inclusive_scan as inclusive_scan
 from .segmented_reduce import segmented_reduce
-from .unique_by_key import unique_by_key as unique_by_key
 from .transform import binary_transform, unary_transform
+from .unique_by_key import unique_by_key as unique_by_key
 
 __all__ = [
     "merge_sort",
