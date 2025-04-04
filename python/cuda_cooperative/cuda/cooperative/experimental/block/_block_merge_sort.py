712d9e3b6fcf8e8c5302490274e4f214db770974
Trent Nelson <trent@trent.me>
2025-04-04 15:54:24 -0700

    Rename DependentOperator to DependentPythonOperator.
    This is in anticipation for a new DependentCxxOperator class to be
    introduced in a subsequent commit.

--- python/cuda_cooperative/cuda/cooperative/experimental/block/_block_merge_sort.py
+++ python/cuda_cooperative/cuda/cooperative/experimental/block/_block_merge_sort.py
@@ -16,7 +16,7 @@ from cuda.cooperative.experimental._types import (
     Constant,
     Dependency,
     DependentArray,
-    DependentOperator,
+    DependentPythonOperator,
     Invocable,
     Pointer,
     TemplateParameter,
@@ -94,7 +94,7 @@ def merge_sort_keys(
             [
                 Pointer(numba.uint8),
                 DependentArray(Dependency("KeyT"), Dependency("ITEMS_PER_THREAD")),
-                DependentOperator(
+                DependentPythonOperator(
                     Constant(numba.int8),
                     [Dependency("KeyT"), Dependency("KeyT")],
                     Dependency("Op"),
