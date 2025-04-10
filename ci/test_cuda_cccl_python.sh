58e09c81a79d072e40402f6fd0caa143cf59727e
Allison Piper <alliepiper16@gmail.com>
2025-04-10 15:22:16 +0000

    Refactor python scripts to use a shared implementation.

--- ci/test_cuda_cccl_python.sh
+++ ci/test_cuda_cccl_python.sh
@@ -8,27 +8,8 @@ print_environment_details
 
 fail_if_no_gpu
 
-begin_group "⚙️ Existing site-packages"
-pip freeze
-end_group "⚙️ Existing site-packages"
+source "test_python_common.sh"
 
-module="cuda_cccl"
+list_environment
 
-pushd "../python/${module}" >/dev/null
-
-TEMP_VENV_DIR="/tmp/${module}_venv"
-rm -rf "${TEMP_VENV_DIR}"
-python -m venv "${TEMP_VENV_DIR}"
-. "${TEMP_VENV_DIR}/bin/activate"
-echo 'cuda-cccl @ file:///home/coder/cccl/python/cuda_cccl' > /tmp/cuda-cccl_constraints.txt
-run_command "⚙️  Pip install ${module}" pip install -c /tmp/cuda-cccl_constraints.txt .[test]
-begin_group "⚙️ ${module} site-packages"
-pip freeze
-end_group "⚙️ ${module} site-packages"
-run_command "🚀  Pytest ${module}" pytest -n ${PARALLEL_LEVEL} -v ./tests
-deactivate
-
-popd >/dev/null
-
-
-print_time_summary
+run_tests "cuda_cccl"
---
+++ ci/test_python_common.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function list_environment {
+  begin_group "⚙️ Existing site-packages"
+  pip freeze
+  end_group "⚙️ Existing site-packages"
+}
+
+function run_tests {
+  module=$1
+
+  pushd "../python/${module}" >/dev/null
+
+  TEMP_VENV_DIR="/tmp/${module}_venv"
+  rm -rf "${TEMP_VENV_DIR}"
+  python -m venv "${TEMP_VENV_DIR}"
+  . "${TEMP_VENV_DIR}/bin/activate"
+  echo 'cuda-cccl @ file:///home/coder/cccl/python/cuda_cccl' > /tmp/cuda-cccl_constraints.txt
+  run_command "⚙️  Pip install ${module}" pip install -c /tmp/cuda-cccl_constraints.txt .[test]
+  begin_group "⚙️ ${module} site-packages"
+  pip freeze
+  end_group "⚙️ ${module} site-packages"
+  run_command "🚀  Pytest ${module}" pytest -n ${PARALLEL_LEVEL} -v ./tests
+  deactivate
+
+  popd >/dev/null
+
+  print_time_summary
+}
