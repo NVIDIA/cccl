9f67b090b81ba60e68422a4065986aee8f9a0588
Cédric Augonnet <158148890+caugonnet@users.noreply.github.com>
2025-04-15 15:24:40 +0200

    Update cudax/examples/stf/binary_fhe_stackable.cu
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- cudax/examples/stf/binary_fhe_stackable.cu
+++ cudax/examples/stf/binary_fhe_stackable.cu
@@ -13,8 +13,8 @@
  * @brief A toy example to illustrate how we can compose logical operations over encrypted data
  */
 
-#include "cuda/experimental/__stf/utility/stackable_ctx.cuh"
-#include "cuda/experimental/stf.cuh"
+#include <cuda/experimental/__stf/utility/stackable_ctx.cuh>
+#include <cuda/experimental/stf.cuh>
 
 using namespace cuda::experimental::stf;
 
