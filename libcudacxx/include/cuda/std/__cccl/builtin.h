e3e2730ea8a2ba47b473b17fe1a3b2e040a82690
David Bayer <bayer.david99@gmail.com>
2025-04-16 15:02:42 +0200

    Refactor `cuda::std::countl_*`

--- libcudacxx/include/cuda/std/__cccl/builtin.h
+++ libcudacxx/include/cuda/std/__cccl/builtin.h
@@ -217,6 +217,15 @@
 #  define _CCCL_BUILTIN_CLZLL(...) __builtin_clzll(__VA_ARGS__)
 #endif // _CCCL_CHECK_BUILTIN(builtin_clz)
 
+#if _CCCL_CHECK_BUILTIN(builtin_clzg)
+#  define _CCCL_BUILTIN_CLZG(...) __builtin_clzg(__VA_ARGS__)
+#endif // _CCCL_CHECK_BUILTIN(builtin_clzg)
+
+// NVCC cannot handle __builtin_clzg
+#if _CCCL_CUDA_COMPILER(NVCC)
+#  undef _CCCL_BUILTIN_CLZG
+#endif // _CCCL_CUDA_COMPILER(NVCC)
+
 #if _CCCL_CHECK_BUILTIN(builtin_ctz) || _CCCL_COMPILER(GCC, <, 10) || _CCCL_COMPILER(CLANG) || _CCCL_COMPILER(NVHPC)
 #  define _CCCL_BUILTIN_CTZ(...)   __builtin_ctz(__VA_ARGS__)
 #  define _CCCL_BUILTIN_CTZLL(...) __builtin_ctzll(__VA_ARGS__)
