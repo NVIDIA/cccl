e2c01239aff492dad27a11299ad18f36f96e862a
fbusato <fbusato@nvidia.com>
2025-04-17 16:04:56 -0700

    use constexpr function

--- libcudacxx/include/cuda/std/__type_traits/num_bits.h
+++ libcudacxx/include/cuda/std/__type_traits/num_bits.h
@@ -21,7 +21,7 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/std/__complex/is_complex.h>
+#include <cuda/std/__floating_point/nvfp_types.h>
 #include <cuda/std/__type_traits/always_false.h>
 #include <cuda/std/__type_traits/enable_if.h>
 #include <cuda/std/__type_traits/has_unique_object_representation.h>
@@ -30,66 +30,68 @@
 #include <cuda/std/__type_traits/is_same.h>
 #include <cuda/std/__type_traits/remove_cv.h>
 #include <cuda/std/climits>
+#include <cuda/std/complex>
 
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
 template <typename _Tp>
-struct _PreventInstantiation
+[[nodiscard]] _LIBCUDACXX_HIDE_FROM_ABI constexpr int __num_bits_impl() noexcept
 {
-  static_assert(__always_false_v<_Tp>);
-};
-
-template <typename _Tp, typename = remove_cv_t<_Tp>>
-inline constexpr int __num_bits_v = _PreventInstantiation<_Tp>{};
-
-template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp,
-                                  enable_if_t<(has_unique_object_representations_v<_Tp> || is_floating_point_v<_Tp>
-                                               || __is_complex_v<_Tp> || is_same_v<_Tp, bool>)
-                                                && !__is_sub_byte_floating_point<_Tp>,
-                                              remove_cv_t<_Tp>>> = sizeof(_Tp) * CHAR_BIT;
-
+  if constexpr (has_unique_object_representations_v<_Tp> || is_floating_point_v<_Tp> || __is_complex_v<_Tp>
+                || is_same_v<_Tp, bool>)
+  {
+    return sizeof(_Tp) * CHAR_BIT;
+  }
 #if _CCCL_HAS_NVFP16()
-
-template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp, __half2> = sizeof(__half2) * CHAR_BIT;
-
+  else if constexpr (is_same_v<_Tp, __half2>)
+  {
+    return sizeof(_Tp) * CHAR_BIT;
+  }
 #endif // _CCCL_HAS_NVFP16
-
 #if _CCCL_HAS_NVBF16()
-
-template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp, __nv_bfloat162> = sizeof(__nv_bfloat162) * CHAR_BIT;
-
+  else if constexpr (is_same_v<_Tp, __nv_bfloat162>)
+  {
+    return sizeof(_Tp) * CHAR_BIT;
+  }
 #endif // _CCCL_HAS_NVBF16
-
-#if _CCCL_HAS_NVFP6_E3M2()
-
-template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp, __nv_fp6_e3m2> = 6;
-
-#endif // _CCCL_HAS_NVFP6_E3M2()
-
-#if _CCCL_HAS_NVFP6_E2M3()
-
-template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp, __nv_fp6_e2m3> = 6;
-
-#endif // _CCCL_HAS_NVFP6_E3M2()
-
-#if _CCCL_HAS_NVFP4_E2M1()
-
-template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp, __nv_fp4_e2m1> = 4;
-
+#if _CCCL_HAS_NVFP8_E8M0()
+  else if constexpr (is_same_v<_Tp, __nv_fp8_e8m0>)
+  {
+    return 8;
+  }
 #endif // _CCCL_HAS_NVFP8_E8M0()
-
+#if _CCCL_HAS_NVFP6_E3M2()
+  else if constexpr (is_same_v<_Tp, __nv_fp6_e3m2>)
+  {
+    return 6;
+  }
+#endif // _CCCL_HAS_NVFP6_E3M2()
+#if _CCCL_HAS_NVFP6_E2M3()
+  else if constexpr (is_same_v<_Tp, __nv_fp6_e2m3>)
+  {
+    return 6;
+  }
+#endif // _CCCL_HAS_NVFP6_E2M3()
+#if _CCCL_HAS_NVFP4_E2M1()
+  else if constexpr (is_same_v<_Tp, __nv_fp4_e2m1>)
+  {
+    return 4;
+  }
+#endif // _CCCL_HAS_NVFP4_E2M1()
 #if _CCCL_HAS_FLOAT128()
+  else if constexpr (is_same_v<_Tp, __float128>)
+  {
+    return sizeof(_Tp) * CHAR_BIT;
+  }
+#endif // _CCCL_HAS_FLOAT128()
+  else
+  {
+    static_assert(__always_false_v<_Tp>, "unsupported type");
+  }
+}
 
 template <typename _Tp>
-inline constexpr int __num_bits_v<_Tp, __float128> = sizeof(__float128) * CHAR_BIT;
-
-#endif // _CCCL_HAS_FLOAT128()
+inline constexpr int __num_bits_v = __num_bits_impl<_CUDA_VSTD::remove_cv_t<_Tp>>();
 
 _LIBCUDACXX_END_NAMESPACE_STD
 
