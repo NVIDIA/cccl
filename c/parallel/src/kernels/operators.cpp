c5610f75cc8ae2f2fd6030dac897a525a189cf35
Ashwin Srinath <shwina@users.noreply.github.com>
2025-04-23 18:04:12 -0400

    Change signature of old-style operators to use void* args

--- c/parallel/src/kernels/operators.cpp
+++ c/parallel/src/kernels/operators.cpp
@@ -34,10 +34,12 @@ constexpr std::string_view binary_op_template = R"XXX(
 )XXX";
 
 constexpr std::string_view stateless_binary_op_template = R"XXX(
-extern "C" __device__ {0} OP_NAME(LHS_T lhs, RHS_T rhs);
+extern "C" __device__ void OP_NAME(void* lhs, void* rhs, void* out);
 struct op_wrapper {{
   __device__ {0} operator()(LHS_T lhs, RHS_T rhs) const {{
-    return OP_NAME(lhs, rhs);
+    {0} ret;
+    OP_NAME(&lhs, &rhs, &ret);
+    return ret;
   }}
 }};
 )XXX";
@@ -105,10 +107,12 @@ std::string make_kernel_user_unary_operator(std::string_view input_t, std::strin
 )XXX";
 
   constexpr std::string_view stateless_op = R"XXX(
-extern "C" __device__ OUTPUT_T OP_NAME(INPUT_T val);
+extern "C" __device__  void OP_NAME(void* val, void* result);
 struct op_wrapper {
   __device__ OUTPUT_T operator()(INPUT_T val) const {
-    return OP_NAME(val);
+    OUTPUT_T out;
+    OP_NAME(&val, &out);
+    return out;
   }
 };
 )XXX";
