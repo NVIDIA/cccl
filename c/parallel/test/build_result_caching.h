acbc69399549a07c0e89d03e1879fd71f335bfbe
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-05-01 08:54:21 -0500

    Add classes to enable build caching in c.parallel test suite
    1. `result_wrapper_t` is owning reference to CCCL build result based
    on shared pointer. It must be copy/move assignable/constructable
    to enable use as value type in std::unordered_map.
    
    The class is templated on CCCL build result struct type, and
    callable to clean-up resources allocated for build result.
    
    2. `build_cache_t` in simple cache based on std::unordered_map.
    
    The class is templated on key type (std::string) and value type,
    an instance of result_wrapper_t.
    
    3. `fixture` is a singleton to lazily create build cache at
      its first use. Fixture wraps the cache in std::optional
      to match use pattern in algorithms.
    
    fixture allows for Tag template parameter to be able to create
    multiple instances of singleton classes. We usually want cache
    for a test family where algorithm compile the same problem and
    execute it for different problem sizes. Caching then allows to
    reuse the build artifact.
    
    4. KeyBuilder is a struct with static methods to facilitate
       creation of test-variant identifier to use as cache key.
    
    Cache keys are crafted per test.

---
+++ c/parallel/test/build_result_caching.h
@@ -0,0 +1,150 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <typeinfo>
+#include <unordered_map>
+
+template <typename ResultT, typename CleanupCallable>
+class result_wrapper_t
+{
+  std::shared_ptr<ResultT> m_owner;
+
+public:
+  result_wrapper_t()
+      : m_owner{}
+  {}
+  result_wrapper_t(ResultT v)
+      : m_owner{std::make_shared<ResultT>(v)}
+  {}
+
+  result_wrapper_t(const result_wrapper_t&) = default;
+  result_wrapper_t(result_wrapper_t&&)      = default;
+
+  result_wrapper_t& operator=(const result_wrapper_t&) = default;
+  result_wrapper_t& operator=(result_wrapper_t&&)      = default;
+
+  ~result_wrapper_t() noexcept
+  {
+    if (!m_owner)
+    {
+      return;
+    }
+    try
+    {
+      if (m_owner.use_count() <= 1)
+      {
+        // release resources
+        CleanupCallable{}(m_owner.get());
+      }
+    }
+    catch (const std::exception& e)
+    {
+      std::cerr << "~result_wrapper_t ignores exception: " << e.what() << std::endl;
+    }
+  }
+
+  ResultT& get()
+  {
+    return *m_owner.get();
+  }
+};
+
+template <typename KeyT, typename ValueT>
+class build_cache_t
+{
+  std::unordered_map<KeyT, ValueT> m_map;
+
+public:
+  build_cache_t()
+      : m_map{} {};
+
+  bool contains(const KeyT& key) const
+  {
+    // unorder_map::contains is C++20 feature
+    return m_map.contains(key);
+  }
+
+  void insert(const KeyT& key, ValueT&& new_value)
+  {
+    m_map[key] = std::move(new_value);
+  }
+
+  ValueT& get(const KeyT& key)
+  {
+    return m_map[key];
+  }
+};
+
+template <typename T, typename Tag>
+class fixture
+{
+public:
+  using OptionalT  = typename std::optional<T>;
+  using FixtureTag = Tag;
+
+private:
+  OptionalT v;
+
+  fixture()
+      : v{T{}}
+  {}
+
+public:
+  ~fixture() = default;
+
+  OptionalT& get_value()
+  {
+    return v;
+  }
+
+  static auto& get_or_create()
+  {
+    static fixture singleton{};
+    return singleton;
+  }
+};
+
+struct KeyBuilder
+{
+  static std::string bool_as_key(bool v)
+  {
+    return (v) ? std::string("T") : std::string("F");
+  }
+
+  template <typename T>
+  static std::string type_as_key()
+  {
+    return typeid(T).name();
+  }
+
+  template <std::size_t N>
+  static std::string join(const std::string (&collection)[N])
+  {
+    constexpr std::string_view delimiter = "-";
+    std::stringstream ss;
+
+    for (std::size_t i = 0; i < N; ++i)
+    {
+      ss << collection[i];
+      if (i + 1 < N)
+      {
+        ss << delimiter;
+      }
+    }
+
+    return ss.str();
+  }
+};
