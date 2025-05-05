954f8887ef48cb74e0aa309112d5bd52cc4eecc5
Allison Piper <alliepiper16@gmail.com>
2025-05-05 14:12:43 -0400

    Add _CCCL_LOG_CUDA_API, improve cuda_error reporting (#4588)
    * Add more information to `cuda_error` messages.
    
    - Location (file:line)
    - Failing CUDA API
    - CUDA error string
    
    * Add new _CCCL_LOG_CUDA_API macro.
    
    This behaves similarly to the CubDebug macro, logging the error to stderr and returning the error code to allow the caller to decide how to handle it.
    
    * Use new API macro in c2h.
    
    Example:
    
    ```
    /home/coder/cccl/c2h/catch2_runner_helper.inl:58 cudaSetDevice: invalid device ordinal(101): Failed to set requested device
    Failed to set device ID: 2
    ```

--- libcudacxx/include/cuda/std/__exception/cuda_error.h
+++ libcudacxx/include/cuda/std/__exception/cuda_error.h
@@ -22,7 +22,12 @@
 #  pragma system_header
 #endif // no system header
 
+#if _CCCL_HAS_CUDA_COMPILER()
+#  include <cuda_runtime_api.h>
+#endif // _CCCL_HAS_CUDA_COMPILER()
+
 #include <cuda/std/__exception/terminate.h>
+#include <cuda/std/source_location>
 
 #if !_CCCL_COMPILER(NVRTC)
 #  include <cstdio>
@@ -33,43 +38,81 @@
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 
+#if _CCCL_HAS_EXCEPTIONS()
+
+namespace __detail
+{
+
+struct __msg_storage
+{
+  char __buffer[512]{0};
+};
+
+static char* __format_cuda_error(
+  __msg_storage& __msg_buffer,
+  const int __status,
+  const char* __msg,
+  const char* __api                 = nullptr,
+  _CUDA_VSTD::source_location __loc = _CUDA_VSTD::source_location::current()) noexcept
+{
+  ::snprintf(
+    __msg_buffer.__buffer,
+    512,
+    "%s:%d %s%s%s(%d): %s",
+    __loc.file_name(),
+    __loc.line(),
+    __api ? __api : "",
+    __api ? " " : "",
+#  if _CCCL_HAS_CUDA_COMPILER()
+    ::cudaGetErrorString(::cudaError_t(__status)),
+#  else
+    "cudaError",
+#  endif
+    __status,
+    __msg);
+  return __msg_buffer.__buffer;
+}
+} // namespace __detail
+
 /**
  * @brief Exception thrown when a CUDA error is encountered.
  */
-#if _CCCL_HAS_EXCEPTIONS()
 class cuda_error : public ::std::runtime_error
 {
-private:
-  struct __msg_storage
-  {
-    char __buffer[256];
-  };
-
-  static char* __format_cuda_error(const int __status, const char* __msg, char* __msg_buffer) noexcept
-  {
-    ::snprintf(__msg_buffer, 256, "cudaError %d: %s", __status, __msg);
-    return __msg_buffer;
-  }
-
 public:
-  cuda_error(const int __status, const char* __msg, __msg_storage __msg_buffer = {0}) noexcept
-      : ::std::runtime_error(__format_cuda_error(__status, __msg, __msg_buffer.__buffer))
+  cuda_error(const int __status,
+             const char* __msg,
+             const char* __api                    = nullptr,
+             _CUDA_VSTD::source_location __loc    = _CUDA_VSTD::source_location::current(),
+             __detail::__msg_storage __msg_buffer = {}) noexcept
+      : ::std::runtime_error(__detail::__format_cuda_error(__msg_buffer, __status, __msg, __api, __loc))
   {}
 };
 
-[[noreturn]] _LIBCUDACXX_HIDE_FROM_ABI void
-__throw_cuda_error([[maybe_unused]] const int __status, [[maybe_unused]] const char* __msg)
+[[noreturn]] _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(
+  [[maybe_unused]] const int __status,
+  [[maybe_unused]] const char* __msg,
+  [[maybe_unused]] const char* __api                 = nullptr,
+  [[maybe_unused]] _CUDA_VSTD::source_location __loc = _CUDA_VSTD::source_location::current())
 {
-  NV_IF_ELSE_TARGET(NV_IS_HOST, (throw ::cuda::cuda_error(__status, __msg);), (_CUDA_VSTD_NOVERSION::terminate();))
+  NV_IF_ELSE_TARGET(NV_IS_HOST,
+                    (throw ::cuda::cuda_error(__status, __msg, __api, __loc);), //
+                    (_CUDA_VSTD_NOVERSION::terminate();))
 }
 #else // ^^^ _CCCL_HAS_EXCEPTIONS() ^^^ / vvv !_CCCL_HAS_EXCEPTIONS() vvv
 class cuda_error
 {
 public:
-  _LIBCUDACXX_HIDE_FROM_ABI cuda_error(const int, const char*) noexcept {}
+  _LIBCUDACXX_HIDE_FROM_ABI cuda_error(
+    const int,
+    const char*,
+    const char*                 = nullptr,
+    _CUDA_VSTD::source_location = _CUDA_VSTD::source_location::current()) noexcept
+  {}
 };
 
-[[noreturn]] _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(const int, const char*)
+[[noreturn]] _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(
+  const int, const char*, const char* = nullptr, _CUDA_VSTD::source_location = _CUDA_VSTD::source_location::current())
 {
   _CUDA_VSTD_NOVERSION::terminate();
 }
