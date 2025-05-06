1a0f27d6566c7c3cf52cff4dbe7367c626ba2544
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-06 19:45:22 +0000

    We're already in the right directory

--- ci/build_cuda_cccl_python.sh
+++ ci/build_cuda_cccl_python.sh
@@ -19,18 +19,11 @@ docker run --rm \
     # dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ && \
     # echo -e "#!/bin/bash\nsource /opt/rh/gcc-toolset-13/enable" > /etc/profile.d/enable_devtools.sh && \
     # source /etc/profile.d/enable_devtools.sh && \
-    pwd && \
-    ls -la / && \
-    ls -la /home/ && \
-    ls -la /home/coder/workspace && \
-    ls -la /home/coder/workspace/cccl && \
-    ls -la /home/coder/workspace/cccl/cccl && \
-    ls -la /home/coder/workspace/cccl/cccl/python && \
-    ls -la /home/coder/workspace/cccl/cccl/python/cuda_cccl && \
     python -m pip wheel .'
 
 # print wheel name:
-wheel_name=$(ls python/cuda_cccl/*.whl)
+ls -la
+wheel_name=$(ls *.whl)
 echo "Wheel name: ${wheel_name}"
 
 # copy the wheel to the artifacts directory
