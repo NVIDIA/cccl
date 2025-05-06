9ed1d6b284e69e0e1805705e843d80857af947bf
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-06 18:38:05 +0000

    Try again

--- ci/build_cuda_cccl_python.sh
+++ ci/build_cuda_cccl_python.sh
@@ -12,8 +12,8 @@ which docker
 # given the py_version build the wheel and output the artifact
 # to the artifacts directory
 docker run --rm \
-  --workdir /home/coder/cccl/cccl \
-  --mount type=bind,source=${HOST_WORKSPACE},target=/home/coder/cccl \
+  --workdir /home/coder/workspace/cccl/cccl/python/cuda_cccl \
+  --mount type=bind,source=${HOST_WORKSPACE},target=/home/coder/workspace \
   rapidsai/ci-wheel:cuda12.8.0-rockylinux8-py${py_version} \
   bash -c '\
     # dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ && \
@@ -22,8 +22,11 @@ docker run --rm \
     pwd && \
     ls -la / && \
     ls -la /home/ && \
-    ls -la /home/coder/cccl && \
-    ls -la /home/coder/cccl/cccl && \
+    ls -la /home/coder/workspace && \
+    ls -la /home/coder/workspace/cccl && \
+    ls -la /home/coder/workspace/cccl/cccl && \
+    ls -la /home/coder/workspace/cccl/cccl/python && \
+    ls -la /home/coder/workspace/cccl/cccl/python/cuda_cccl && \
     python -m pip wheel .'
 
 # print wheel name:
