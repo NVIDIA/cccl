92e632f327cbb1fb8f6209656957a958a3d34f75
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-07 13:46:14 +0000

    Try running tests from the same container as the build

--- ci/test_cuda_parallel_python.sh
+++ ci/test_cuda_parallel_python.sh
@@ -2,16 +2,23 @@
 
 set -euo pipefail
 
-source "$(dirname "$0")/build_common.sh"
+# Get the Python version from the command line arguments -py-version=3.10
+py_version=${2#*=}
+echo "Python version: ${py_version}"
+echo "Docker socket: " $(ls /var/run/docker.sock)
 
-print_environment_details
-
-fail_if_no_gpu
-
-source "test_python_common.sh"
-
-list_environment
-
-# Install the wheel from the artifact location
-WHEEL_PATH="$(ls /wheelhouse/cuda_parallel-*.whl)"
-run_tests_from_wheel "cuda_parallel" "$WHEEL_PATH"
+# Run tests in the same container as the build
+docker run --rm \
+  --workdir /home/coder/workspace/cccl/python/cuda_parallel \
+  --mount type=bind,source=${HOST_WORKSPACE},target=/home/coder/workspace \
+  --gpus all \
+  rapidsai/ci-wheel:cuda12.8.0-rockylinux8-py${py_version} \
+  bash -c '\
+    source "$(dirname "$0")/build_common.sh" && \
+    print_environment_details && \
+    fail_if_no_gpu && \
+    source "test_python_common.sh" && \
+    list_environment && \
+    # Install the wheel from the artifact location \
+    WHEEL_PATH="$(ls /wheelhouse/cuda_parallel-*.whl)" && \
+    run_tests_from_wheel "cuda_parallel" "$WHEEL_PATH"'
