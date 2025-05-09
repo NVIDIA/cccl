fda8f86d87da118f75d09010cbc6bc8063376630
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-09 15:45:07 +0000

    Update scripts not to source common

--- ci/test_cuda_cccl_python.sh
+++ ci/test_cuda_cccl_python.sh
@@ -6,14 +6,6 @@ set -euo pipefail
 py_version=${2#*=}
 echo "Python version: ${py_version}"
 
-# Source common scripts and check environment
-source "$(dirname "$0")/build_common.sh"
-print_environment_details
-fail_if_no_gpu
-
-source "$(dirname "$0")/test_python_common.sh"
-list_environment
-
 # Install the wheel and run tests
 CUDA_CCCL_WHEEL_PATH="$(ls /wheelhouse/cuda_cccl-*.whl)"
 python -m pip install "${CUDA_CCCL_WHEEL_PATH}[test]"
