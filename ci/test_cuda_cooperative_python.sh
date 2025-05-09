68664d5669a35f067b8a339898811587cfd3467a
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-09 17:06:20 +0000

    Install cuda_cccl before running tests

--- ci/test_cuda_cooperative_python.sh
+++ ci/test_cuda_cooperative_python.sh
@@ -7,6 +7,8 @@ py_version=${2#*=}
 echo "Python version: ${py_version}"
 
 # Install the wheel and run tests
+CUDA_CCCL_WHEEL_PATH="$(ls /wheelhouse/cuda_cccl-*.whl)"
+python -m pip install "${CUDA_CCCL_WHEEL_PATH}"
 CUDA_COOPERATIVE_WHEEL_PATH="$(ls /wheelhouse/cuda_cooperative-*.whl)"
 python -m pip install "${CUDA_COOPERATIVE_WHEEL_PATH}[test]"
 cd "$(dirname "$0")/../python/cuda_cooperative/tests/"
