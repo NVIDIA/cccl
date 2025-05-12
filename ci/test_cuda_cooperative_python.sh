d266468e4c1fd0c4db79f6bfcd0d875e7f773456
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-12 20:46:47 +0000

    Split cuda_cooperative test job into build + test jobs

--- ci/test_cuda_cooperative_python.sh
+++ ci/test_cuda_cooperative_python.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 set -euo pipefail
+source "$(dirname "$0")/pyenv_helper.sh"
 
-source "$(dirname "$0")/build_common.sh"
+# Get the Python version from the command line arguments -py-version=3.10
+py_version=${2#*=}
+echo "Python version: ${py_version}"
 
-print_environment_details
+# Setup Python environment
+setup_python_env "${py_version}"
 
-fail_if_no_gpu
+# Install cuda_cccl
+CUDA_CCCL_WHEEL_PATH="$(ls /home/coder/cccl/wheelhouse/cuda_cccl-*.whl)"
+python -m pip install "${CUDA_CCCL_WHEEL_PATH}"
 
-source "test_python_common.sh"
+# Install cuda_cooperative
+CUDA_COOPERATIVE_WHEEL_PATH="$(ls /home/coder/cccl/wheelhouse/cuda_cooperative-*.whl)"
+python -m pip install "${CUDA_COOPERATIVE_WHEEL_PATH}[test]"
 
-list_environment
-
-run_tests "cuda_cooperative"
+# Run tests
+cd "/home/coder/cccl/python/cuda_cooperative/tests/"
+python -m pytest -n auto -v
