3cd663246d13fd042577701c148d38263724d52e
Ashwin Srinath <shwina@users.noreply.github.com>
2025-05-12 17:32:21 +0000

    Try 3.10 as minimum version

--- ci/test_cuda_parallel_python.sh
+++ ci/test_cuda_parallel_python.sh
@@ -16,7 +16,7 @@ docker run --rm \
   --mount type=bind,source=${HOST_WORKSPACE},target=/workspace/ \
   -e NVIDIA_DISABLE_REQUIRE=true \
   --gpus device=${NVIDIA_VISIBLE_DEVICES} \
-  rapidsai/citestwheel:cuda12.8.0-rockylinux8-py${py_version} \
+  rapidsai/citestwheel:cuda12.8.0-rockylinux8-py3.10 \
   bash -c '\
     source /workspace/cccl/ci/build_common.sh && \
     print_environment_details && \
