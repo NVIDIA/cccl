08f1f36c3e91cfee0b9309087f72edbd481627a4
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2025-05-15 14:29:35 -0400

    Build and test python wheels in CI (#4679)
    * Enable docker-outside-of-docker in devcontainer
    
    * Add a helper to install and setup pyenv
    
    * Split cuda_cccl test job into build + test job
    
    * Split cuda_cooperative test job into build + test jobs
    
    * Split cuda_parallel test job into build + test jobs
    
    * Extend workflow to include wheel upload/download
    
    * Update matrix.yaml
    
    * Tack on py_version to the job name
    
    * Ensure abi compatibility in produced wheels for cuda_parallel
    
    * Matrix override
    
    * Minor changes following review
    
    * Try simplifying override
    
    * Remove override
    
    ---------
    
    Co-authored-by: Ashwin Srinath <shwina@users.noreply.github.com>

---
+++ ci/pyenv_helper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+setup_python_env() {
+    local py_version=$1
+
+    # check if pyenv is installed
+    if ! command -v pyenv &> /dev/null; then
+        rm -f /pyenv
+        curl -fsSL https://pyenv.run | bash
+    fi
+
+    # Always set up pyenv environment
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init - bash)"
+
+    # Using pyenv, install the Python version
+    PYENV_DEBUG=1 pyenv install -v "${py_version}"
+    pyenv local "${py_version}"
+}
