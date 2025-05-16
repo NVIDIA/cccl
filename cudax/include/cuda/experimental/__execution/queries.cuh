d7a3655823db020e570f690808e913ba49ab24b8
Eric Niebler <eniebler@nvidia.com>
2025-05-16 15:20:14 -0700

    disable execution space warnings for all of ustdex's generic facilities (#4727)

--- cudax/include/cuda/experimental/__execution/queries.cuh
+++ cudax/include/cuda/experimental/__execution/queries.cuh
@@ -46,6 +46,7 @@ using _CUDA_STD_EXEC::__queryable_with;
 // get_allocator
 _CCCL_GLOBAL_CONSTANT struct get_allocator_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   template <class _Env>
   _CCCL_API auto operator()(const _Env& __env) const noexcept
   {
@@ -65,6 +66,7 @@ _CCCL_GLOBAL_CONSTANT struct get_allocator_t
 // get_stop_token
 _CCCL_GLOBAL_CONSTANT struct get_stop_token_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   template <class _Env>
   _CCCL_API auto operator()(const _Env& __env) const noexcept
   {
@@ -88,6 +90,7 @@ using stop_token_of_t _CCCL_NODEBUG_ALIAS = __decay_t<__call_result_t<get_stop_t
 template <class _Tag>
 struct get_completion_scheduler_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   _CCCL_TEMPLATE(class _Env)
   _CCCL_REQUIRES(__queryable_with<_Env, get_completion_scheduler_t>)
   _CCCL_API auto operator()(const _Env& __env) const noexcept
@@ -109,6 +112,7 @@ using __completion_scheduler_of_t _CCCL_NODEBUG_ALIAS =
 // get_scheduler
 _CCCL_GLOBAL_CONSTANT struct get_scheduler_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   _CCCL_TEMPLATE(class _Env)
   _CCCL_REQUIRES(__queryable_with<_Env, get_scheduler_t>)
   _CCCL_API auto operator()(const _Env& __env) const noexcept
@@ -126,6 +130,7 @@ using __scheduler_of_t _CCCL_NODEBUG_ALIAS = __decay_t<__call_result_t<get_sched
 // get_delegation_scheduler
 _CCCL_GLOBAL_CONSTANT struct get_delegation_scheduler_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   _CCCL_TEMPLATE(class _Env)
   _CCCL_REQUIRES(__queryable_with<_Env, get_delegation_scheduler_t>)
   _CCCL_API auto operator()(const _Env& __env) const noexcept
@@ -147,6 +152,7 @@ enum class forward_progress_guarantee
 
 _CCCL_GLOBAL_CONSTANT struct get_forward_progress_guarantee_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   template <class _Sch>
   _CCCL_API auto operator()(const _Sch& __sch) const noexcept
   {
@@ -166,6 +172,7 @@ _CCCL_GLOBAL_CONSTANT struct get_forward_progress_guarantee_t
 // get_domain
 _CCCL_GLOBAL_CONSTANT struct get_domain_t
 {
+  _CCCL_EXEC_CHECK_DISABLE
   template <class _Env>
   _CCCL_API constexpr auto operator()(const _Env& __env) const noexcept
   {
