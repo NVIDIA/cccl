3e7a419e5e09e7bbe1569144f1e9e968f7eba4e8
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-18 11:21:19 -0700

    Minimal changes (proof of concept) to use `cuda.bindings.path_finder` from `cuda.parallel`

--- python/cuda_parallel/cuda/parallel/experimental/__init__.py
+++ python/cuda_parallel/cuda/parallel/experimental/__init__.py
@@ -1,3 +1,12 @@
 # Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import logging
+
+from cuda.bindings.path_finder import (  # type: ignore[import-not-found]
+    _load_nvidia_dynamic_library,
+)
+
+for libname in ("cudart", "nvrtc", "nvJitLink"):
+    logging.info(str(_load_nvidia_dynamic_library(libname)))
