067530718d3f5e3f6d63b6fcafad629b1c75a147
fbusato <fbusato@nvidia.com>
2025-05-19 10:04:14 -0700

    fix license

--- cub/cub/detail/array_utils.cuh
+++ cub/cub/detail/array_utils.cuh
@@ -1,5 +1,6 @@
 /***********************************************************************************************************************
- * Copyright (c) 2011-2025, NVIDIA CORPORATION.  All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
