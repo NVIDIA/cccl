c317e4e1d039a765a10b769a8b2c69b9248d330a
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-19 15:30:16 -0700

    Manual ci/build_cuda_parallel_wheel.sh fixes

--- ci/build_cuda_parallel_wheel.sh
+++ ci/build_cuda_parallel_wheel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+dnf -y install unzip
+
 dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++
 echo -e "#!/bin/bash\nsource /opt/rh/gcc-toolset-13/enable" >/etc/profile.d/enable_devtools.sh
 source /etc/profile.d/enable_devtools.sh
@@ -13,14 +15,22 @@ echo "Done setting up python env"
 
 python -m pip wheel --no-deps /workspace/python/cuda_cccl
 python -m pip wheel --no-deps .
-python -m pip install patchelf auditwheel
+RAWCPWHL=cuda_parallel-*.whl
+
+TMPDIR=$(mktemp -d)
+unzip "$RAWCPWHL" -d "$TMPDIR"
 EXCLUDES=$(
-    python -m auditwheel show cuda_parallel-*.whl |
+    find "$TMPDIR" -name '*.so' -exec ldd {} + |
         grep -E 'libcuda\.so|libnvrtc\.so|libnvJitLink\.so' |
         awk '{print $1}' |
         sort -u |
         xargs -n1 echo --exclude
 )
-python -m auditwheel repair $EXCLUDES cuda_parallel-*.whl
+echo "Excluding the following libraries in auditwheel repair:"
+echo "$EXCLUDES"
+rm -rf "$TMPDIR"
+
+python -m pip install patchelf auditwheel
+python -m auditwheel repair $EXCLUDES "$RAWCPWHL"
 mv cuda_cccl-*.whl /workspace/cccl/wheelhouse/
 mv wheelhouse/cuda_parallel-*.whl /workspace/cccl/wheelhouse/
