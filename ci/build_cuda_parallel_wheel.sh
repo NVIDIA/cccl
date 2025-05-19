daea174b08018e62057cdf32a0af42a3ba8fa985
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-19 14:25:00 -0700

    Compute EXCLUDES, to avoid hard-wiring version numbers that will surely change. Also run `shfmt -i 4`.

--- ci/build_cuda_parallel_wheel.sh
+++ ci/build_cuda_parallel_wheel.sh
@@ -6,15 +6,19 @@ setup_python_env "${py_version}"
 echo "Done setting up python env"
 python -m pip wheel --no-deps /workspace/python/cuda_cccl
 dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++
-echo -e "#!/bin/bash\nsource /opt/rh/gcc-toolset-13/enable" > /etc/profile.d/enable_devtools.sh
+echo -e "#!/bin/bash\nsource /opt/rh/gcc-toolset-13/enable" >/etc/profile.d/enable_devtools.sh
 source /etc/profile.d/enable_devtools.sh
 which python
 python --version
 python -m pip wheel --no-deps .
 python -m pip install auditwheel
-python -m auditwheel repair cuda_parallel-*.whl \
-    --exclude libcuda.so.1 \
-    --exclude nvrtc.so.12 \
-    --exclude nvJitLink.so.12
+EXCLUDES=$(
+    python -m auditwheel show cuda_parallel-*.whl |
+        grep -E 'libcuda\.so|libnvrtc\.so|libnvJitLink\.so' |
+        awk '{print $1}' |
+        sort -u |
+        xargs -n1 echo --exclude
+)
+python -m auditwheel repair $EXCLUDES cuda_parallel-*.whl
 mv cuda_cccl-*.whl /workspace/cccl/wheelhouse/
 mv wheelhouse/cuda_parallel-*.whl /workspace/cccl/wheelhouse/
