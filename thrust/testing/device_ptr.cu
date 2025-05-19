ed1c5b4a1e5382fc48d5dc4b795707ef3469022f
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-05-19 17:16:11 +0200

    Update thrust/testing/device_ptr.cu
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- thrust/testing/device_ptr.cu
+++ thrust/testing/device_ptr.cu
@@ -7,9 +7,9 @@
 
 #include <unittest/unittest.h>
 
-#if _CCCL_STD_VER >= 2020 && defined(__cpp_lib_concepts)
+#ifdef __cpp_lib_concepts
 static_assert(std::indirectly_writable<thrust::device_ptr<uint8_t>, uint8_t>);
-#endif // _CCCL_STD_VER >= 2020 && defined(__cpp_lib_concepts)
+#endif // __cpp_lib_concepts
 static_assert(cuda::std::indirectly_writable<thrust::device_ptr<uint8_t>, uint8_t>);
 
 void TestDevicePointerManipulation()
