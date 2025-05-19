31c7e50b25aca78e82cb87ca9bda0f49eb67d9dd
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-19 14:53:02 -0700

    Add back `patchelf` to `pip install`, to get the binary. Shuffle commands in build_cuda_parallel_wheel.sh to make the flow more clear. `pip install --upgrade pip` in ci/pyenv_helper.sh to avoid "new release of pip is available" notices.

--- ci/build_cuda_parallel_wheel.sh
+++ ci/build_cuda_parallel_wheel.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-source /workspace/ci/pyenv_helper.sh
-setup_python_env "${py_version}"
-echo "Done setting up python env"
-python -m pip wheel --no-deps /workspace/python/cuda_cccl
 dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++
 echo -e "#!/bin/bash\nsource /opt/rh/gcc-toolset-13/enable" >/etc/profile.d/enable_devtools.sh
 source /etc/profile.d/enable_devtools.sh
+
+source /workspace/ci/pyenv_helper.sh
+setup_python_env "${py_version}"
 which python
 python --version
+echo "Done setting up python env"
+
+python -m pip wheel --no-deps /workspace/python/cuda_cccl
 python -m pip wheel --no-deps .
-python -m pip install auditwheel
+python -m pip install patchelf auditwheel
 EXCLUDES=$(
     python -m auditwheel show cuda_parallel-*.whl |
         grep -E 'libcuda\.so|libnvrtc\.so|libnvJitLink\.so' |
