f2af3d4c0f540b84e2c88137281ef938048a5d1e
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-05-20 11:43:50 +0200

    Add thrust::transform_n
    Fixes: #4489

--- thrust/thrust/transform.h
+++ thrust/thrust/transform.h
@@ -725,6 +725,161 @@ ForwardIterator transform_if(
   BinaryFunction binary_op,
   Predicate pred);
 
+//! Like \ref transform, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename DerivedPolicy, typename InputIterator, typename Size, typename OutputIterator, typename UnaryFunction>
+_CCCL_HOST_DEVICE OutputIterator transform_n(
+  const detail::execution_policy_base<DerivedPolicy>& exec,
+  InputIterator first,
+  Size count,
+  OutputIterator result,
+  UnaryFunction op)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_n");
+  return thrust::transform(exec, first, first + count, result, op);
+}
+
+//! Like \ref transform, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename InputIterator, typename Size, typename OutputIterator, typename UnaryFunction>
+OutputIterator transform_n(InputIterator first, Size count, OutputIterator result, UnaryFunction op)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_n");
+  return thrust::transform(first, first + count, result, op);
+}
+
+//! Like \ref transform, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename DerivedPolicy,
+          typename InputIterator1,
+          typename Size,
+          typename InputIterator2,
+          typename OutputIterator,
+          typename BinaryFunction>
+_CCCL_HOST_DEVICE OutputIterator transform_n(
+  const detail::execution_policy_base<DerivedPolicy>& exec,
+  InputIterator1 first1,
+  Size count,
+  InputIterator2 first2,
+  OutputIterator result,
+  BinaryFunction op)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_n");
+  return thrust::transform(exec, first1, first1 + count, first2, result, op);
+}
+
+//! Like \ref transform, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename InputIterator1, typename Size, typename InputIterator2, typename OutputIterator, typename BinaryFunction>
+OutputIterator
+transform_n(InputIterator1 first1, Size count, InputIterator2 first2, OutputIterator result, BinaryFunction op)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_n");
+  return thrust::transform(first1, first1 + count, first2, result, op);
+}
+
+//! Like \ref transform_if, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename DerivedPolicy,
+          typename InputIterator,
+          typename Size,
+          typename ForwardIterator,
+          typename UnaryFunction,
+          typename Predicate>
+_CCCL_HOST_DEVICE ForwardIterator transform_if_n(
+  const detail::execution_policy_base<DerivedPolicy>& exec,
+  InputIterator first,
+  Size count,
+  ForwardIterator result,
+  UnaryFunction op,
+  Predicate pred)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_if_n");
+  return thrust::transform(exec, first, first + count, result, op, pred);
+}
+
+//! Like \ref transform_if, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename InputIterator, typename Size, typename ForwardIterator, typename UnaryFunction, typename Predicate>
+ForwardIterator transform_if_n(InputIterator first, Size count, ForwardIterator result, UnaryFunction op, Predicate pred)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_if_n");
+  return thrust::transform_if(first, first + count, result, op, pred);
+}
+
+//! Like \ref transform_if, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename DerivedPolicy,
+          typename InputIterator1,
+          typename Size,
+          typename InputIterator2,
+          typename ForwardIterator,
+          typename UnaryFunction,
+          typename Predicate>
+_CCCL_HOST_DEVICE ForwardIterator transform_if_n(
+  const detail::execution_policy_base<DerivedPolicy>& exec,
+  InputIterator1 first,
+  Size count,
+  InputIterator2 stencil,
+  ForwardIterator result,
+  UnaryFunction op,
+  Predicate pred)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_if_n");
+  return thrust::transform_if(exec, first, first + count, stencil, result, op, pred);
+}
+
+//! Like \ref transform_if, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename InputIterator1,
+          typename Size,
+          typename InputIterator2,
+          typename ForwardIterator,
+          typename UnaryFunction,
+          typename Predicate>
+ForwardIterator transform_if_n(
+  InputIterator1 first, Size count, InputIterator2 stencil, ForwardIterator result, UnaryFunction op, Predicate pred)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_if_n");
+  return thrust::transform_if(first, first + count, stencil, result, op, pred);
+}
+
+//! Like \ref transform_if, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename DerivedPolicy,
+          typename InputIterator1,
+          typename Size,
+          typename InputIterator2,
+          typename InputIterator3,
+          typename ForwardIterator,
+          typename BinaryFunction,
+          typename Predicate>
+_CCCL_HOST_DEVICE ForwardIterator transform_if_n(
+  const detail::execution_policy_base<DerivedPolicy>& exec,
+  InputIterator1 first1,
+  Size count,
+  InputIterator2 first2,
+  InputIterator3 stencil,
+  ForwardIterator result,
+  BinaryFunction binary_op,
+  Predicate pred)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_if_n");
+  return thrust::transform_if(exec, first1, first1 + count, first2, stencil, result, binary_op, pred);
+}
+
+//! Like \ref transform_if, but uses an element count instead of an iterator to the last element of the input sequence.
+template <typename InputIterator1,
+          typename Size,
+          typename InputIterator2,
+          typename InputIterator3,
+          typename ForwardIterator,
+          typename BinaryFunction,
+          typename Predicate>
+ForwardIterator transform_if_n(
+  InputIterator1 first1,
+  Size count,
+  InputIterator2 first2,
+  InputIterator3 stencil,
+  ForwardIterator result,
+  BinaryFunction binary_op,
+  Predicate pred)
+{
+  _CCCL_NVTX_RANGE_SCOPE("thrust::transform_if_n");
+  return thrust::transform_if(first1, first1 + count, first2, stencil, result, binary_op, pred);
+}
+
 /*! \} // end transformations
  */
 
