7befc6005e06dc0e129686cb6d426682c3e5a9a8
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-19 20:45:32 -0700

    Use auditwheel `--exclude` glob syntax.

--- ci/build_cuda_parallel_wheel.sh
+++ ci/build_cuda_parallel_wheel.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# Install required tools
-dnf -y install unzip
-dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++
-
 # Enable GCC toolset
+dnf -y install gcc-toolset-13-gcc gcc-toolset-13-gcc-c++
 echo -e "#!/bin/bash\nsource /opt/rh/gcc-toolset-13/enable" >/etc/profile.d/enable_devtools.sh
 source /etc/profile.d/enable_devtools.sh
 
@@ -19,25 +16,14 @@ echo "Done setting up python env"
 # Build wheels
 python -m pip wheel --no-deps /workspace/python/cuda_cccl
 python -m pip wheel --no-deps .
-RAWCPWHL=$(ls cuda_parallel-*.whl | head -n1)
-
-# Collect excluded shared libs from unpacked wheel
-TMPDIR=$(mktemp -d)
-unzip "$RAWCPWHL" -d "$TMPDIR"
-EXCLUDES=$(
-    find "$TMPDIR" -name '*.so' -exec ldd {} + |
-        grep -E 'libcuda\.so|libnvrtc\.so|libnvJitLink\.so' |
-        awk '{print $1}' |
-        sort -u |
-        xargs -n1 echo --exclude
-)
-echo "Excluding the following libraries in auditwheel repair:"
-echo "$EXCLUDES"
-rm -rf "$TMPDIR"
 
 # Repair wheel
 python -m pip install patchelf auditwheel
-python -m auditwheel repair $EXCLUDES "$RAWCPWHL"
+python -m auditwheel repair \
+    --exclude 'libcuda.so*' \
+    --exclude 'libnvrtc.so*' \
+    --exclude 'libnvJitLink.so*' \
+    cuda_parallel-*.whl
 
 # Move wheels to output directory
 mv cuda_cccl-*.whl /workspace/cccl/wheelhouse/
