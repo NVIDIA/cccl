61a0d8e50c85e6fb9890029f704f9443199a832d
David Bayer <bayer.david99@gmail.com>
2025-05-26 09:10:46 +0200

    fix codegen

--- libcudacxx/include/cuda/std/__atomic/functions/cuda_ptx_generated.h
+++ libcudacxx/include/cuda/std/__atomic/functions/cuda_ptx_generated.h
@@ -42,7 +42,7 @@
 
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
-#if _CCCL_CUDA_COMPILATION()
+#if _CCCL_HAS_CUDA_COMPILER()
 
 static inline _CCCL_DEVICE void __cuda_atomic_membar(__thread_scope_block_tag)
 { asm volatile("membar.cta;" ::: "memory"); }
@@ -3960,7 +3960,7 @@ static inline _CCCL_DEVICE _Type __atomic_fetch_sub_cuda(_Type volatile* __ptr, 
   return __atomic_fetch_add_cuda(__ptr, -__op, __memorder, _Sco{});
 }
 
-#endif // _CCCL_CUDA_COMPILATION()
+#endif // _CCCL_HAS_CUDA_COMPILER()
 
 _LIBCUDACXX_END_NAMESPACE_STD
 
