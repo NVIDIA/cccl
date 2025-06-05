7de6b730480fab7a035e3873742bcfa6921799eb
Ashwin Srinath <shwina@users.noreply.github.com>
2025-06-05 15:52:16 +0000

    Update version files

--- python/cuda_cccl/cuda/cccl/__init__.py
+++ python/cuda_cccl/cuda/cccl/__init__.py
@@ -2,6 +2,7 @@
 CUDA Core Library (CCCL) Python Package
 """
 
+from ._version import __version__
 from .headers.include_paths import get_include_paths
 
-__all__ = ["get_include_paths"]
+__all__ = ["get_include_paths", "__version__"]
