6ef61412c7d2eb958bd2c53d1328f0cd667011e2
Allison Piper <alliepiper16@gmail.com>
2025-06-06 15:29:50 +0000

    Add and use new artifact/workflow functionality for CI scripts.

--- ci/build_thrust.sh
+++ ci/build_thrust.sh
@@ -12,4 +12,9 @@ CMAKE_OPTIONS=""
 
 configure_and_build_preset "Thrust" "$PRESET" "$CMAKE_OPTIONS"
 
+# Create test artifacts:
+if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
+    run_command "📦  Packaging test artifacts" /home/coder/cccl/ci/upload_thrust_test_artifacts.sh
+fi
+
 print_time_summary
