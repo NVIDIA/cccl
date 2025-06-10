34eb3e0de7d5f82c4867b30816db1b44d9440256
Srinivas Yadav Singanaboina <ssinganaboin@nvidia.com>
2025-06-10 15:22:13 -0700

    unify host and device tests into single function

--- cudax/test/cuco/utility/hash_test.cu
+++ cudax/test/cuco/utility/hash_test.cu
@@ -9,6 +9,8 @@
 //===----------------------------------------------------------------------===//
 
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/host_vector.h>
 #include <thrust/logical.h>
 
 #include <cuda/std/cstddef>
@@ -45,64 +47,58 @@ check_hash_result(Key const& key, std::uint32_t expected, HashConstructorArgs&&.
   return (h(key) == expected);
 }
 
-template <typename OutputIter>
-__global__ void check_hash_result_kernel_32(OutputIter result)
+template <typename TestFn, typename... Args>
+__global__ void test_fn_on_device(TestFn test_fn, Args... args)
 {
-  int i = 0;
-
-  result[i++] = check_hash_result<cudax::cuco::Hash<char>>(0, 3479547966, 0);
-  result[i++] = check_hash_result<cudax::cuco::Hash<char>>(42, 3774771295, 0);
-  result[i++] = check_hash_result<cudax::cuco::Hash<char>>(0, 2099223482, 42);
-
-  result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(0, 148298089, 0);
-  result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(0, 2132181312, 42);
-  result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(42, 1161967057, 0);
-  result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(123456789, 2987034094, 0);
-
-  result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(0, 3736311059, 0);
-  result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(0, 1076387279, 42);
-  result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(42, 2332451213, 0);
-  result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(123456789, 1561711919, 0);
-
-#if defined(CUCO_HAS_INT128)
-  result[i++] = check_hash_result<cudax::cuco::Hash<__int128>>(123456789, 1846633701, 0);
-#endif
-
-  result[i++] = check_hash_result<cudax::cuco::Hash<large_key<32>>>(123456789, 3715432378, 0);
+  test_fn(args...);
 }
 
-TEST_CASE("utility cudax::cuco::xxhash_32 test", "")
+struct test_xxhash_32
 {
-  // Reference hash values were computed using https://github.com/Cyan4973/xxHash
-  SECTION("Check if host-generated hash values match the reference implementation.")
+  template <typename OutputIter>
+  _CCCL_HOST_DEVICE void operator()(OutputIter result)
   {
-    CHECK(check_hash_result<cudax::cuco::Hash<char>>(0, 3479547966, 0));
-    CHECK(check_hash_result<cudax::cuco::Hash<char>>(42, 3774771295, 0));
-    CHECK(check_hash_result<cudax::cuco::Hash<char>>(0, 2099223482, 42));
+    int i = 0;
 
-    CHECK(check_hash_result<cudax::cuco::Hash<int32_t>>(0, 148298089, 0));
-    CHECK(check_hash_result<cudax::cuco::Hash<int32_t>>(0, 2132181312, 42));
-    CHECK(check_hash_result<cudax::cuco::Hash<int32_t>>(42, 1161967057, 0));
-    CHECK(check_hash_result<cudax::cuco::Hash<int32_t>>(123456789, 2987034094, 0));
+    result[i++] = check_hash_result<cudax::cuco::Hash<char>>(0, 3479547966, 0);
+    result[i++] = check_hash_result<cudax::cuco::Hash<char>>(42, 3774771295, 0);
+    result[i++] = check_hash_result<cudax::cuco::Hash<char>>(0, 2099223482, 42);
 
-    CHECK(check_hash_result<cudax::cuco::Hash<int64_t>>(0, 3736311059, 0));
-    CHECK(check_hash_result<cudax::cuco::Hash<int64_t>>(0, 1076387279, 42));
-    CHECK(check_hash_result<cudax::cuco::Hash<int64_t>>(42, 2332451213, 0));
-    CHECK(check_hash_result<cudax::cuco::Hash<int64_t>>(123456789, 1561711919, 0));
+    result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(0, 148298089, 0);
+    result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(0, 2132181312, 42);
+    result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(42, 1161967057, 0);
+    result[i++] = check_hash_result<cudax::cuco::Hash<int32_t>>(123456789, 2987034094, 0);
+
+    result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(0, 3736311059, 0);
+    result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(0, 1076387279, 42);
+    result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(42, 2332451213, 0);
+    result[i++] = check_hash_result<cudax::cuco::Hash<int64_t>>(123456789, 1561711919, 0);
 
 #if defined(CUCO_HAS_INT128)
-    CHECK(check_hash_result<cudax::cuco::Hash<__int128>>(123456789, 1846633701, 0));
+    result[i++] = check_hash_result<cudax::cuco::Hash<__int128>>(123456789, 1846633701, 0);
 #endif
 
-    // 32*4=128-byte key to test the pipelined outermost hashing loop
-    CHECK(check_hash_result<cudax::cuco::Hash<large_key<32>>>(123456789, 3715432378, 0));
+    result[i++] = check_hash_result<cudax::cuco::Hash<large_key<32>>>(123456789, 3715432378, 0);
+  }
+};
+
+TEST_CASE("Utility Hasher _XXHash_32 test", "")
+{
+  SECTION("host-generated hash values match the reference implementation.")
+  {
+    thrust::host_vector<bool> result(20, true);
+
+    test_xxhash_32 test;
+    test(result.begin());
+
+    CHECK(thrust::all_of(thrust::host, result.begin(), result.end(), ::cuda::std::identity{}));
   }
 
-  SECTION("Check if device-generated hash values match the reference implementation.")
+  SECTION("device-generated hash values match the reference implementation.")
   {
     thrust::device_vector<bool> result(20, true);
 
-    check_hash_result_kernel_32<<<1, 1>>>(result.begin());
+    test_fn_on_device<<<1, 1>>>(test_xxhash_32{}, result.begin());
 
     CHECK(thrust::all_of(result.begin(), result.end(), ::cuda::std::identity{}));
   }
