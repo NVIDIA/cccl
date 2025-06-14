137d680aac52e7c49fa0089a99e0bed6a7211204
Trent Nelson <trent@trent.me>
2025-06-13 21:23:19 -0700

    WORKING CHECKPOINT COMMIT!
    The singular test in test_block_load_store_api_single_phase.py passes!
    
    This marks the completion of two coop rewriter nodes: one for arrays,
    and one for load/store operations.

--- python/cuda_cccl/cuda/cccl/cooperative/experimental/__init__.py
+++ python/cuda_cccl/cuda/cccl/cooperative/experimental/__init__.py
@@ -2,16 +2,16 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from cuda.cccl.cooperative.experimental import block, warp
-from cuda.cccl.cooperative.experimental._array import local, shared
-from cuda.cccl.cooperative.experimental._enums import (
+from . import block, warp
+from ._array import local, shared
+from ._enums import (
     BlockLoadAlgorithm,
     BlockStoreAlgorithm,
     WarpLoadAlgorithm,
     WarpStoreAlgorithm,
 )
-from cuda.cccl.cooperative.experimental._numba_extension import _init_extension
-from cuda.cccl.cooperative.experimental._types import StatefulFunction
+from ._numba_extension import _init_extension
+from ._types import StatefulFunction
 
 __all__ = [
     "_init_extension",
@@ -29,4 +29,4 @@ __all__ = [
 # Our extension initialization doesn't appear to be getting called
 # automatically, despite the pyproject.toml entry, so, manually
 # call it here.
-_init_extension()
+# _init_extension()
