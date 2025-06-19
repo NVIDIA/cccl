e3c9c718cf829f80e53ba6aede9c87a24872cde3
Cedric Augonnet <caugonnet@nvidia.com>
2025-06-19 12:04:52 +0200

    Add a heat equation example doing JIT with static slice

---
+++ cudax/test/stf/jit/heat.cu
@@ -0,0 +1,286 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDASTF in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2022-2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+/**
+ * @file
+ *
+ * @brief An example solving heat equation with finite differences using a
+ * CUDA kernel
+ */
+
+#include <cuda/experimental/stf.cuh>
+
+using namespace cuda::experimental::stf;
+
+::std::string run_command(const char* cmd)
+{
+  std::array<char, 1024 * 64> buffer;
+  std::string result;
+
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe)
+  {
+    return result;
+  }
+
+  while (fgets(buffer.data(), buffer.size(), pipe.get()))
+  {
+    result += buffer.data();
+  }
+
+  if (result.back() == '\n')
+  {
+    result.pop_back(); // Remove trailing newline
+  }
+
+  return result;
+}
+
+
+void dump_iter(slice<const double, 2> sUn, int iter)
+{
+  /* Create a binary file in the PPM format */
+  char name[64];
+  snprintf(name, 64, "heat_%06d.ppm", iter);
+  FILE* f = fopen(name, "wb");
+  fprintf(f, "P6\n%zu %zu\n255\n", sUn.extent(0), sUn.extent(1));
+  for (size_t j = 0; j < sUn.extent(1); j++)
+  {
+    for (size_t i = 0; i < sUn.extent(0); i++)
+    {
+      int v = (int) (255.0 * sUn(i, j) / 100.0);
+      // we assume values between 0.0 and 100.0 : max value is in red,
+      // min is in blue
+      unsigned char color[3];
+      color[0] = static_cast<char>(v); /* red */
+      color[1] = static_cast<char>(0); /* green */
+      color[2] = static_cast<char>(255 - v); /* blue */
+      fwrite(color, 1, 3, f);
+    }
+  }
+  fclose(f);
+}
+
+const char* heat_kernel_template = R"(
+#include <cuda/mdspan>
+
+// Alias identical to the one you use on the host
+template <typename T, size_t dimensions = 1>
+using slice =
+    ::cuda::std::mdspan<T,
+                        ::cuda::std::dextents<size_t, dimensions>,
+                        ::cuda::std::layout_stride>;
+
+template <typename T, size_t M, size_t N>
+class static_slice
+{
+public:
+    using extents_t = ::cuda::std::extents<size_t, M, N>;
+    using layout_t = ::cuda::std::layout_stride;
+    using mdspan_t = ::cuda::std::mdspan<T, extents_t, layout_t>;
+
+    __host__ __device__
+    static_slice(T* data, typename layout_t::template mapping<extents_t> mapping)
+        : view_{data, mapping} {}
+
+    // Conversion from dynamic mdspan if extents match
+    template <typename OtherMapping>
+    __host__ __device__
+    static_slice(const ::cuda::std::mdspan<T, ::cuda::std::dextents<size_t, 2>, OtherMapping>& dyn)
+    {
+        auto ext = dyn.extents();
+        assert(ext.extent(0) == M && ext.extent(1) == N);
+        view_ = mdspan_t(dyn.data_handle(), typename layout_t::template mapping<extents_t>(dyn.mapping()));
+    }
+
+    // Access mdspan interface
+    __host__ __device__ T& operator()(size_t i, size_t j) const { return view_(i, j); }
+    __host__ __device__ T* data() const { return view_.data_handle(); }
+    __host__ __device__ auto mapping() const { return view_.mapping(); }
+    __host__ __device__ auto extents() const { return view_.extents(); }
+
+    __host__ __device__
+    size_t extent(size_t i) const { return view_.extent(i); }
+
+private:
+    mdspan_t view_;
+};
+
+//extern "C"
+//__global__ void heat_kernel(slice<const double, 2> U, slice<double, 2> U1, double c, double dx2, double dy2)
+//{
+//  int tidx = blockIdx.x * blockDim.x + threadIdx.x;
+//  int tidy = blockIdx.y * blockDim.y + threadIdx.y;
+//  int dimx = blockDim.x * gridDim.x;
+//  int dimy = blockDim.y * gridDim.y;
+//
+//  for (size_t i = tidx + 1; i < U.extent(0)-1; i+= dimx)
+//    for (size_t j = tidy + 1; j < U.extent(1)-1; j += dimy)
+//    {
+//      U1(i, j) = U(i, j) + c * ((U(i - 1, j) - 2 * U(i, j) + U(i + 1, j)) / dx2 + (U(i, j - 1) - 2 * U(i, j) + U(i, j + 1)) / dy2);
+//    }
+//}
+
+extern "C"
+__global__ void heat_kernel(static_slice<const double, %zu, %zu> U, static_slice<double, %zu, %zu> U1, double c, double dx2, double dy2)
+{
+  int tidx = blockIdx.x * blockDim.x + threadIdx.x;
+  int tidy = blockIdx.y * blockDim.y + threadIdx.y;
+  int dimx = blockDim.x * gridDim.x;
+  int dimy = blockDim.y * gridDim.y;
+
+  for (size_t i = tidx + 1; i < U.extent(0)-1; i+= dimx)
+    for (size_t j = tidy + 1; j < U.extent(1)-1; j += dimy)
+    {
+      U1(i, j) = U(i, j) + c * ((U(i - 1, j) - 2 * U(i, j) + U(i + 1, j)) / dx2 + (U(i, j - 1) - 2 * U(i, j) + U(i, j + 1)) / dy2);
+    }
+}
+
+)";
+
+int main()
+{
+  context ctx;
+
+  // Initialize CUDA
+  cuda_safe_call(cuInit(0));
+
+  // Put the include paths in the NVRTC flags
+  ::std::vector<::std::string> nvrtc_flags{"-I../../libcudacxx/include"};
+  ::std::string s =
+    run_command(R"(echo "" | nvcc -v -x cu - -c 2>&1 | grep '#$ INCLUDES="' | grep -oP '(?<=INCLUDES=").*(?=" *$)')");
+
+  fprintf(stderr, "COMMAND %s\n", s.c_str());
+  // Split by whitespace
+  std::istringstream iss(s);
+  nvrtc_flags.insert(nvrtc_flags.end(), std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{});
+
+  // Compute the exact machine
+  const int device          = cuda_try<cudaGetDevice>();
+  const cudaDeviceProp prop = cuda_try<cudaGetDeviceProperties>(device);
+  nvrtc_flags.push_back("--gpu-architecture=compute_" + ::std::to_string(prop.major) + ::std::to_string(prop.minor));
+
+  const CUdevice cuDevice = cuda_try<cuDeviceGet>(0);
+  const CUcontext context = cuda_try<cuCtxCreate>(0, cuDevice);
+
+  // Compilation options for nvrtc
+  ::std::vector<const char*> opts;
+  opts.reserve(nvrtc_flags.size());
+  for (const auto& s : nvrtc_flags)
+  {
+    opts.push_back(s.c_str());
+  }
+
+  const size_t N = 800;
+
+  auto lU  = ctx.logical_data(shape_of<slice<double, 2>>(N, N));
+  auto lU1 = ctx.logical_data(lU.shape());
+
+  // Initialize the Un field with boundary conditions, and a disk at a lower
+  // temperature in the middle.
+  ctx.parallel_for(lU.shape(), lU.write())->*[=] _CCCL_DEVICE(size_t i, size_t j, auto U) {
+    double rad = U.extent(0) / 8.0;
+    double dx  = (double) i - U.extent(0) / 2;
+    double dy  = (double) j - U.extent(1) / 2;
+
+    U(i, j) = (dx * dx + dy * dy < rad * rad) ? 100.0 : 0.0;
+
+    /* Set up boundary conditions */
+    if (j == 0.0)
+    {
+      U(i, j) = 100.0;
+    }
+    if (j == U.extent(1) - 1)
+    {
+      U(i, j) = 0.0;
+    }
+    if (i == 0.0)
+    {
+      U(i, j) = 0.0;
+    }
+    if (i == U.extent(0) - 1)
+    {
+      U(i, j) = 0.0;
+    }
+  };
+
+  // diffusion constant
+  double a = 0.5;
+
+  double dx  = 0.1;
+  double dy  = 0.1;
+  double dx2 = dx * dx;
+  double dy2 = dy * dy;
+
+  // time step
+  double dt = dx2 * dy2 / (2.0 * a * (dx2 + dy2));
+
+  double c = a * dt;
+
+  int nsteps     = 1000;
+  int image_freq = 100;
+
+  for (int iter = 0; iter < nsteps; iter++)
+  {
+    if (image_freq > 0 && iter % image_freq == 0)
+    {
+      // Dump Un in a PPM file
+      ctx.host_launch(lU.read())->*[=](auto U) {
+        dump_iter(U, iter);
+      };
+    }
+
+    // Update Un using Un1 value with a finite difference scheme
+    ctx.task(lU.read(), lU1.write())->*[c, dx2, dy2, &opts](cudaStream_t stream, auto U, auto U1)
+    {
+        static auto kernel = [&]() {
+            std::vector<char> heat_kernel_filled(8192); // XXX big enough buffer
+            std::snprintf(heat_kernel_filled.data(), heat_kernel_filled.size(), heat_kernel_template, U.extent(0), U.extent(1), U1.extent(0), U1.extent(1));
+            nvrtcProgram prog = cuda_try<nvrtcCreateProgram>(heat_kernel_filled.data(), "heat_kernel.cu", 0, nullptr, nullptr);
+            nvrtcResult res   = nvrtcCompileProgram(prog, opts.size(), opts.data());
+            if (res != NVRTC_SUCCESS)
+            {
+              size_t logSize;
+              cuda_safe_call(nvrtcGetProgramLogSize(prog, &logSize));
+
+              std::string log(logSize, '\0');
+              cuda_safe_call(nvrtcGetProgramLog(prog, log.data()));
+
+              std::cerr << "NVRTC compilation failed:\n" << log << std::endl;
+              std::cerr << "Error code: " << res << " (" << nvrtcGetErrorString(res) << ")\n";
+
+              exit(1); // or handle as appropriate
+            }
+
+            size_t ptxSize = 0;
+            cuda_safe_call(nvrtcGetPTXSize(prog, &ptxSize));
+
+            std::string ptx(ptxSize, '\0');
+            cuda_safe_call(nvrtcGetPTX(prog, ptx.data()));
+            cuda_safe_call(nvrtcDestroyProgram(&prog));
+            std::cerr << "Try to compile : " << ptx;
+
+            // Load PTX
+            const CUmodule module   = cuda_try<cuModuleLoadData>(ptx.data());
+            const CUfunction kernel = cuda_try<cuModuleGetFunction>(module, "heat_kernel");
+
+            return kernel;
+        }();
+
+        void* args[] = {&U, &U1, const_cast<double*>(&c), const_cast<double*>(&dx2), const_cast<double*>(&dy2)};
+        // heat_kernel<<<128, 32, 0, stream>>>(U, U1, c, dx2, dy2);
+        cuda_safe_call(cuLaunchKernel(kernel, 128, 1, 1, 32, 1, 1, 0, stream, args, nullptr));
+    };
+
+    std::swap(lU, lU1);
+  }
+
+  ctx.finalize();
+}
