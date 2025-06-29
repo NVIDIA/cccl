f477e8c1dea9732efb7ac4ca7ebd8aca07e216dd
pauleonix <paul.grosse-bley@ziti.uni-heidelberg.de>
2025-06-29 12:06:35 +0200

    Apply suggestions from code review
    Co-authored-by: Elias Stehle <3958403+elstehle@users.noreply.github.com>

--- cub/test/catch2_test_device_scan_invalid.cu
+++ cub/test/catch2_test_device_scan_invalid.cu
@@ -1,30 +1,5 @@
-
-/******************************************************************************
- * Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the NVIDIA CORPORATION nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "insert_nested_NVTX_range_guard.h"
 
@@ -84,7 +59,7 @@ struct Tuple2Seg
 
 // Actual scan operator doing the core test when run on device
 template <typename OffsetT>
-struct SegMerge
+struct merge_segments_op
 {
   using seg_t = Segment<OffsetT>;
   __host__ __device__ seg_t operator()(seg_t left, seg_t right)
@@ -106,7 +81,9 @@ C2H_TEST("Device scan avoids invalid data with all device interfaces", "[scan][d
   auto scan_op = op_t{};
 
   // Generate the input sizes to test for
-  const offset_t num_items = GENERATE(1, 31, cuda::ipow(31, 2), cuda::ipow(31, 4), cuda::ipow(31, 5));
+  const offset_t num_items = GENERATE_COPY(
+    take(3, random(1, 10000000)),
+    values({1, 31, cuda::ipow(31, 2), cuda::ipow(31, 4), cuda::ipow(31, 5)}));
 
   const auto d_in_it = thrust::make_transform_iterator(
     thrust::make_zip_iterator(thrust::counting_iterator<offset_t>{1}, thrust::counting_iterator<offset_t>{2}),
