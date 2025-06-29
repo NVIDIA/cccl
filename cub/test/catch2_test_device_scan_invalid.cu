9133fc2050a1e70272804bd0e19c5ee76e3689f4
pauleonix <pgrossebley@nvidia.com>
2025-06-29 10:27:35 +0000

    Finish implementing feedback

--- cub/test/catch2_test_device_scan_invalid.cu
+++ cub/test/catch2_test_device_scan_invalid.cu
@@ -36,24 +36,25 @@ struct segment
 
 // Needed for final comparison with reference
 template <typename OffsetT>
-bool operator==(Segment<OffsetT> left, Segment<OffsetT> right)
+bool operator==(segment<OffsetT> left, segment<OffsetT> right)
 {
   return left.begin == right.begin && left.end == right.end;
 }
 template <typename OffsetT>
-std::ostream& operator<<(std::ostream& os, const Segment<OffsetT>& seg)
+std::ostream& operator<<(std::ostream& os, const segment<OffsetT>& seg)
 {
   return os << "[ " << seg.begin << ", " << seg.end << " )";
 }
 
 // Needed for data input using fancy iterators
 template <typename OffsetT>
-struct Tuple2Seg
+struct tuple_to_segment_op
 {
-  using seg_t = Segment<OffsetT>;
+  using seg_t = segment<OffsetT>;
   __host__ __device__ seg_t operator()(thrust::tuple<OffsetT, OffsetT> interval)
   {
-    return {.begin = thrust::get<0>(interval), .end = thrust::get<1>(interval)};
+    const auto [begin, end] = interval;
+    return {begin, end};
   }
 };
 
@@ -61,11 +62,11 @@ struct Tuple2Seg
 template <typename OffsetT>
 struct merge_segments_op
 {
-  using seg_t = Segment<OffsetT>;
+  using seg_t = segment<OffsetT>;
   __host__ __device__ seg_t operator()(seg_t left, seg_t right)
   {
     NV_IF_TARGET(NV_IS_DEVICE, (if (left.end != right.begin) { __trap(); }));
-    return {.begin = left.begin, .end = right.end};
+    return {left.begin, right.end};
   }
 };
 
@@ -73,27 +74,26 @@ struct merge_segments_op
 C2H_TEST("Device scan avoids invalid data with all device interfaces", "[scan][device][!mayfail]")
 {
   using offset_t = int32_t;
-  using input_t  = Segment<offset_t>;
+  using input_t  = segment<offset_t>;
   using output_t = input_t;
-  using op_t     = SegMerge<offset_t>;
+  using op_t     = merge_segments_op<offset_t>;
 
   // Scan operator
   auto scan_op = op_t{};
 
   // Generate the input sizes to test for
   const offset_t num_items = GENERATE_COPY(
-    take(3, random(1, 10000000)),
-    values({1, 31, cuda::ipow(31, 2), cuda::ipow(31, 4), cuda::ipow(31, 5)}));
+    take(3, random(1, 10'000'000)), values({1, 31, cuda::ipow(31, 2), cuda::ipow(31, 4), cuda::ipow(31, 5)}));
 
   const auto d_in_it = thrust::make_transform_iterator(
     thrust::make_zip_iterator(thrust::counting_iterator<offset_t>{1}, thrust::counting_iterator<offset_t>{2}),
-    Tuple2Seg<offset_t>{});
+    tuple_to_segment_op<offset_t>{});
 
   SECTION("inclusive scan")
   {
     // Prepare verification data
     // Need neutral init in this case
-    auto const init_value = output_t{.begin = 1, .end = 1};
+    const auto init_value = output_t{.begin = 1, .end = 1};
     c2h::host_vector<output_t> expected_result(num_items);
     compute_inclusive_scan_reference(d_in_it, d_in_it + num_items, expected_result.begin(), scan_op, init_value);
 
@@ -109,7 +109,7 @@ C2H_TEST("Device scan avoids invalid data with all device interfaces", "[scan][d
 
   SECTION("inclusive scan with init value")
   {
-    auto const init_value = output_t{.begin = 0, .end = 1};
+    const auto init_value = output_t{.begin = 0, .end = 1};
 
     // Prepare verification data
     c2h::host_vector<output_t> expected_result(num_items);
@@ -127,7 +127,7 @@ C2H_TEST("Device scan avoids invalid data with all device interfaces", "[scan][d
 
   SECTION("exclusive scan")
   {
-    auto const init_value = output_t{.begin = 0, .end = 1};
+    const auto init_value = output_t{.begin = 0, .end = 1};
 
     // Prepare verification data
     c2h::host_vector<output_t> expected_result(num_items);
@@ -145,7 +145,7 @@ C2H_TEST("Device scan avoids invalid data with all device interfaces", "[scan][d
 
   SECTION("exclusive scan with future-init value")
   {
-    auto const init_value = output_t{.begin = 0, .end = 1};
+    const auto init_value = output_t{.begin = 0, .end = 1};
 
     // Prepare verification data
     c2h::host_vector<output_t> expected_result(num_items);
