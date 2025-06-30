3504995eeff248156c1800fe0b081e4e5fbb2392
David Bayer <48736217+davebayer@users.noreply.github.com>
2025-06-30 08:56:47 +0200

    Implement `format_parse_context` and `format_error` (#4939)
    * Implement `format_parse_context` and `format_error` from `<cuda/std/format>`
    
    * fix `format_error` constructors
    
    * check if `format_error` from `cuda::std::` and `std::` are the same types
    
    * fix compilation with msvc
    
    * fix nvrtc compilation
    
    * fix review

---
+++ libcudacxx/test/libcudacxx/std/text/format/format.formatter/format.parse.ctx/end.pass.cpp
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// <cuda/std/format>
+
+// constexpr end() const noexcept;
+
+#include <cuda/std/__format_>
+#include <cuda/std/cassert>
+#include <cuda/std/string_view>
+
+#include "literal.h"
+
+template <class CharT>
+__host__ __device__ constexpr void test()
+{
+  constexpr const CharT* fmt = TEST_STRLIT(CharT, "abc");
+
+  {
+    cuda::std::basic_format_parse_context<CharT> context(fmt);
+    assert(cuda::std::to_address(context.end()) == &fmt[3]);
+    static_assert(noexcept(context.end()));
+  }
+  {
+    cuda::std::basic_string_view<CharT> view{fmt};
+    cuda::std::basic_format_parse_context<CharT> context(view);
+    assert(context.end() == view.end());
+    static_assert(noexcept(context.end()));
+  }
+}
+
+__host__ __device__ constexpr bool test()
+{
+  test<char>();
+#if _CCCL_HAS_CHAR8_T()
+  test<char8_t>();
+#endif // _CCCL_HAS_CHAR8_T()
+  test<char16_t>();
+  test<char32_t>();
+#if _CCCL_HAS_WCHAR_T()
+  test<wchar_t>();
+#endif // _CCCL_HAS_WCHAR_T()
+
+  return true;
+}
+
+int main(int, char**)
+{
+  test();
+  static_assert(test());
+
+  return 0;
+}
