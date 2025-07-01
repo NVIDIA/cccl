c1e6a4acab5eb86c4a96137dc7aa66facbd1096a
Wesley Maxey <wesley.maxey@gmail.com>
2025-07-01 17:06:16 +0000

    Shrink license and change headers in helpers.h

--- examples/nvrtc/helpers.h
+++ examples/nvrtc/helpers.h
@@ -1,26 +1,12 @@
-/*
- * SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 #pragma once
 
-#include <cuda.h>
+#include <cstdio>
 
+#include <cuda.h>
 #include <nvrtc.h>
-#include <stdio.h>
 
 static inline void check_nvrtc_result(nvrtcResult res, const char* lineinfo, const char* line)
 {
