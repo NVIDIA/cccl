94ba7a94642d3f002420cea80e823c7cf7d208fa
ptaylor <paul.e.taylor@me.com>
2025-07-01 18:46:23 -0700

    remove update-ca-certificates [test-rapids]

--- .devcontainer/cccl-entrypoint.sh
+++ .devcontainer/cccl-entrypoint.sh
@@ -8,26 +8,6 @@ devcontainer-utils-post-create-command;
 devcontainer-utils-init-git;
 devcontainer-utils-post-attach-command;
 
-if ! dpkg -s ca-certificates > /dev/null 2>&1; then
-    if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
-        echo "::group::Installing ca-certificates..."
-    fi
-    sudo apt-get update
-    # Install or upgrade ca-certificates
-    sudo apt-get install -y ca-certificates
-    if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
-        echo "::endgroup::"
-    fi
-fi
-
-if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
-    echo "::group::Updating ca-certificates..."
-fi
-sudo update-ca-certificates
-if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
-    echo "::endgroup::"
-fi
-
 cd /home/coder/cccl/
 
 if test $# -gt 0; then
