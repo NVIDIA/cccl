557ab1e8f8e6d73029d31a8d44ce62f84802ed85
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-07-10 13:50:37 +0200

    Support CMAKE_CUDA_ARCHITECTURES=native

--- thrust/cmake/ThrustCudaConfig.cmake
+++ thrust/cmake/ThrustCudaConfig.cmake
@@ -7,12 +7,33 @@ enable_language(CUDA)
 # Since we have to filter the arch list based on target features, we don't
 # currently support the convenience arch flags:
 if ("all" IN_LIST CMAKE_CUDA_ARCHITECTURES OR
-    "all-major" IN_LIST CMAKE_CUDA_ARCHITECTURES OR
-    "native" IN_LIST CMAKE_CUDA_ARCHITECTURES)
+    "all-major" IN_LIST CMAKE_CUDA_ARCHITECTURES)
   message(FATAL_ERROR
-    "The Thrust dev build requires an explicit list of architectures in CMAKE_CUDA_ARCHITECTURES. "
-    "The convenience flags of 'all', 'all-major', and 'native' are not supported.\n"
+    "The Thrust dev build requires an explicit list of architectures (including 'native') in CMAKE_CUDA_ARCHITECTURES. "
+    "The convenience flags of 'all' and 'all-major' are not supported.\n"
     "CMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHITECTURES}")
+endif()
+
+# Replace 'native' with the actual compute capabilities by querying nvidia-smi.
+if (CMAKE_CUDA_ARCHITECTURES STREQUAL "native")
+  execute_process(
+    COMMAND "nvidia-smi" "--query-gpu=compute_cap" "--format=csv,noheader"
+    OUTPUT_VARIABLE native_compute_caps
+    RESULT_VARIABLE exit_code
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if (exit_code)
+    message(FATAL_ERROR
+      "Failed to query native compute capabilities using nvidia-smi. "
+      "Please remove 'native' from CMAKE_CUDA_ARCHITECTURES."
+    )
+  endif()
+  # nvidia-smi returns for example "12.0\n8.6"
+  string(REPLACE "." "" native_compute_caps "${native_compute_caps}")
+  string(REPLACE "\n" "-real;" native_compute_caps "${native_compute_caps}")
+  string(APPEND native_compute_caps "-real")
+  message(STATUS "Expanded 'native' in CMAKE_CUDA_ARCHITECTURES to '${native_compute_caps}'")
+  set(CMAKE_CUDA_ARCHITECTURES ${native_compute_caps})
 endif()
 
 # Create a new arch list that only contains arches that support CDP:
