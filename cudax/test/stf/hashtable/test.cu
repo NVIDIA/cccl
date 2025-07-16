26aeebe4c5e6cea25857337de6da3eaf061da866
Cedric Augonnet <caugonnet@nvidia.com>
2025-07-17 01:20:13 +0200

    fix compilation error

--- cudax/test/stf/hashtable/test.cu
+++ cudax/test/stf/hashtable/test.cu
@@ -26,12 +26,12 @@ __global__ void gpu_hashtable_insert_kernel(hashtable h, const reserved::KeyValu
 void gpu_hashtable_insert(hashtable d_h, const reserved::KeyValue* device_kvs, unsigned int num_kvs, cudaStream_t stream)
 {
   // Have CUDA calculate the thread block size
-  const auto occ_res = reserved::compute_occupancy(gpu_hashtable_insert_kernel);
+  const auto occ_res  = reserved::compute_occupancy(gpu_hashtable_insert_kernel);
+  int threadblocksize = occ_res.block_size;
+  int gridsize        = ((uint32_t) num_kvs + threadblocksize - 1) / threadblocksize;
 
   // Insert all the keys into the hash table
-  int gridsize = ((uint32_t) num_kvs + threadblocksize - 1) / threadblocksize;
-  gpu_hashtable_insert_kernel<<<occ_res.min_grid_size, occ_res.block_size, 0, stream>>>(
-    d_h, device_kvs, (uint32_t) num_kvs);
+  gpu_hashtable_insert_kernel<<<gridsize, threadblocksize, 0, stream>>>(d_h, device_kvs, (uint32_t) num_kvs);
 }
 
 int main()
