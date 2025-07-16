2ba828560e894ac00a2b52d5d2947487541412bd
David Bayer <bayer.david99@gmail.com>
2025-07-16 14:20:39 +0200

    increase the constexpr steps limit for 64 and 128 bit ints

--- libcudacxx/test/libcudacxx/std/text/charconv/charconv.from.chars/integral/int128.pass.cpp
+++ libcudacxx/test/libcudacxx/std/text/charconv/charconv.from.chars/integral/int128.pass.cpp
@@ -8,7 +8,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-// CONSTEXPR_STEPS: 15000000
+// CONSTEXPR_STEPS: 25000000
 
 #include <cuda/std/__charconv_>
 #include <cuda/std/array>
