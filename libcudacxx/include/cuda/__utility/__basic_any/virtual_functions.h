d32e197c884e44d0cd083dc0a10d7dd3a7b8c7a6
Eric Niebler <eniebler@nvidia.com>
2025-07-17 19:47:12 -0700

    fix license headers

--- libcudacxx/include/cuda/__utility/__basic_any/virtual_functions.h
+++ libcudacxx/include/cuda/__utility/__basic_any/virtual_functions.h
@@ -1,7 +1,6 @@
 //===----------------------------------------------------------------------===//
 //
-// Part of CUDA Experimental in CUDA C++ Core Libraries,
-// under the Apache License v2.0 with LLVM Exceptions.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
