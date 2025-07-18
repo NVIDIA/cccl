6d3ffc900fbc108192346a16706ff4ba6a950a87
Michael Schellenberger Costa <miscco@nvidia.com>
2025-07-18 14:07:57 +0200

    Port `thrust::transform_input_output_iterator` to `cuda` (#5113)
    * Port `thrust::transform_input_output_iterator` to `cuda`
    
    * Address review comments
    
    * Fix typos
    
    * Improve increment / decrement tests
    
    * Fix review comments
    
    * Proxy iterators cannot hold input_iterators
    
    * Address review comments

--- libcudacxx/test/libcudacxx/cuda/iterators/transform_input_output_iterator/iterator_traits.compile.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/iterators/transform_input_output_iterator/iterator_traits.compile.pass.cpp
@@ -15,15 +15,16 @@
 
 __host__ __device__ void test()
 {
-  using IterTraits = cuda::std::iterator_traits<cuda::transform_output_iterator<int*, PlusOne>>;
+  using IterTraits = cuda::std::iterator_traits<cuda::transform_input_output_iterator<int*, PlusOne, TimesTwo>>;
 
   static_assert(cuda::std::same_as<IterTraits::iterator_category, cuda::std::output_iterator_tag>);
   static_assert(cuda::std::same_as<IterTraits::difference_type, cuda::std::ptrdiff_t>);
-  static_assert(cuda::std::same_as<IterTraits::value_type, void>);
+  static_assert(cuda::std::same_as<IterTraits::value_type, int>);
   static_assert(cuda::std::same_as<IterTraits::pointer, void>);
-  static_assert(cuda::std::same_as<IterTraits::reference, void>);
-  static_assert(cuda::std::input_or_output_iterator<cuda::transform_output_iterator<int*, PlusOne>>);
-  static_assert(cuda::std::output_iterator<cuda::transform_output_iterator<int*, PlusOne>, int>);
+  static_assert(
+    cuda::std::same_as<IterTraits::reference, cuda::__transform_input_output_proxy<int*, PlusOne, TimesTwo>>);
+  static_assert(cuda::std::input_or_output_iterator<cuda::transform_input_output_iterator<int*, PlusOne, TimesTwo>>);
+  static_assert(cuda::std::output_iterator<cuda::transform_input_output_iterator<int*, PlusOne, TimesTwo>, int>);
 }
 
 int main(int, char**)
