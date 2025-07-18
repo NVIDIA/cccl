c9acb99b00ce1571dbfa2b0da2c56cc5f25797fc
Cedric Augonnet <caugonnet@nvidia.com>
2025-07-18 23:38:54 +0200

    clang-format

--- c/experimental/stf/src/stf.cu
+++ c/experimental/stf/src/stf.cu
@@ -1,26 +1,26 @@
 #include <cccl/c/experimental/stf/stf.h>
-//#include <cccl/c/parallel/include/cccl/c/extern_c.h>
+// #include <cccl/c/parallel/include/cccl/c/extern_c.h>
 #include <cuda/experimental/stf.cuh>
 
 using namespace cuda::experimental::stf;
 
-extern "C"
-{
+extern "C" {
 
-struct stf_ctx_handle {
-    context *ctx;
+struct stf_ctx_handle
+{
+  context* ctx;
 };
 
-void stf_ctx_create(stf_ctx_handle *handle)
+void stf_ctx_create(stf_ctx_handle* handle)
 {
-    return new context{};
+  return new context{};
 }
 
-void stf_ctx_finalize(stf_ctx_handle *handle)
+void stf_ctx_finalize(stf_ctx_handle* handle)
 {
-    if (handle) {
-        handle->finalize();
-    }
+  if (handle)
+  {
+    handle->finalize();
+  }
 }
-
 }
