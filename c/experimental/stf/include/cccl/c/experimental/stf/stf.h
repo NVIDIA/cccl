f388814e01f81ca3e99759855963f7f175fd4c15
Cedric Augonnet <caugonnet@nvidia.com>
2025-07-18 23:21:43 +0200

    Start to implement C bindings for CUDASTF (this will later be used in conjunction with cython to generate python support)

---
+++ c/experimental/stf/include/cccl/c/experimental/stf/stf.h
@@ -0,0 +1,22 @@
+// TODO use CCCL_C_EXTERN_C_BEGIN/CCCL_C_EXTERN_C_END
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct stf_ctx_handle stf_ctx_handle;
+
+void stf_ctx_create(stf_ctx_handle *handle);
+void stf_ctx_finalize(stf_ctx_handle *handle);
+
+struct stf_task_handle {
+    void *handle;
+};
+
+struct stf_logical_data_handle {
+    void *handle;
+};
+
+#ifdef __cplusplus
+}
+#endif
