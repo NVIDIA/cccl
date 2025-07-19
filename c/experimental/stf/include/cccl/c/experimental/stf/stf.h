5fa13d37e0fc82352919fb88e76a7c0abd6e648c
Cedric Augonnet <caugonnet@nvidia.com>
2025-07-19 09:41:32 +0200

    More bindings

--- c/experimental/stf/include/cccl/c/experimental/stf/stf.h
+++ c/experimental/stf/include/cccl/c/experimental/stf/stf.h
@@ -1,7 +1,17 @@
+#include <cuda_runtime.h>
+
 // TODO use CCCL_C_EXTERN_C_BEGIN/CCCL_C_EXTERN_C_END
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef enum stf_access_mode
+{
+  STF_NONE  = 0,
+  STF_READ  = 1 << 0,
+  STF_WRITE = 1 << 1,
+  STF_RW    = STF_READ | STF_WRITE
+} stf_access_mode;
 
 typedef struct stf_ctx_handle_t* stf_ctx_handle;
 
@@ -10,10 +20,21 @@ void stf_ctx_finalize(stf_ctx_handle ctx);
 
 typedef struct stf_logical_data_handle_t* stf_logical_data_handle;
 
-void stf_logical_data(stf_ctx_handle ctx, stf_logical_data_handle *ld, void *addr, size_t sz);
-void stf_logical_data_destroy(stf_ctx_handle ctx, stf_logical_data_handle ld);
+void stf_logical_data(stf_ctx_handle ctx, stf_logical_data_handle* ld, void* addr, size_t sz);
+void stf_logical_data_set_symbol(stf_logical_data_handle ld, const char* symbol);
+void stf_logical_data_destroy(stf_logical_data_handle ld);
+
+// TODO token
 
 typedef struct stf_task_handle_t* stf_task_handle;
+
+void stf_task_create(stf_ctx_handle ctx, stf_task_handle* t);
+void stf_task_set_symbol(stf_task_handle t, const char* symbol);
+void stf_task_add_dep(stf_task_handle t, stf_logical_data_handle ld, stf_access_mode m);
+void stf_task_start(stf_task_handle t);
+void stf_task_end(stf_task_handle t);
+cudaStream_t stf_task_get_stream(stf_task_handle t);
+void stf_task_destroy(stf_task_handle t);
 
 #ifdef __cplusplus
 }
--- c/experimental/stf/src/stf.cu
+++ c/experimental/stf/src/stf.cu
@@ -11,39 +11,94 @@ struct stf_ctx_handle_t
   context ctx;
 };
 
-void stf_ctx_create(stf_ctx_handle* ctx)
-{
-  if (ctx) {
-    *ctx = new stf_ctx_handle_t{context{}};
-  }
-}
-
-void stf_ctx_finalize(stf_ctx_handle ctx)
-{
-  delete ctx;
-}
-
 struct stf_logical_data_handle_t
 {
   // XXX should we always store a logical_data<slice<char>> instead ?
   logical_data_untyped ld;
 };
 
-void stf_logical_data(stf_ctx_handle ctx, stf_logical_data_handle *ld, void *addr, size_t sz)
+struct stf_task_handle_t
 {
-   assert(ld);
-   assert(ctx);
+  context::unified_task<> t;
+};
 
-   // Create a slice<char> logical data
-   auto ld_typed = ctx->ctx.logical_data(make_slice((char *)addr, sz));
-
-   // Stored in its untyped version
-   *ld = new stf_logical_data_handle_t{ld_typed};
+void stf_ctx_create(stf_ctx_handle* ctx)
+{
+  if (ctx)
+  {
+    *ctx = new stf_ctx_handle_t{context{}};
+  }
 }
 
-void stf_logical_data_destroy(stf_ctx_handle /* ctx */, stf_logical_data_handle ld)
+void stf_ctx_finalize(stf_ctx_handle ctx)
 {
-    delete ld;
+  assert(ctx);
+  delete ctx;
 }
 
+void stf_logical_data(stf_ctx_handle ctx, stf_logical_data_handle* ld, void* addr, size_t sz)
+{
+  assert(ld);
+  assert(ctx);
+
+  // Create a slice<char> logical data
+  auto ld_typed = ctx->ctx.logical_data(make_slice((char*) addr, sz));
+
+  // Stored in its untyped version
+  *ld = new stf_logical_data_handle_t{ld_typed};
+}
+
+void stf_logical_data_set_symbol(stf_logical_data_handle ld, const char* symbol)
+{
+  assert(ld);
+  ld->ld.set_symbol(symbol);
+}
+
+void stf_logical_data_destroy(stf_logical_data_handle ld)
+{
+  assert(ld);
+  delete ld;
+}
+
+void stf_task_create(stf_ctx_handle ctx, stf_task_handle* t)
+{
+  assert(t);
+  assert(ctx);
+
+  *t = new stf_task_handle_t{ctx->ctx.task()};
+}
+
+void stf_task_set_symbol(stf_task_handle t, const char* symbol)
+{
+  assert(t);
+  assert(symbol);
+
+  t->t.set_symbol(symbol);
+}
+
+void stf_task_add_dep(stf_task_handle t, stf_logical_data_handle ld, stf_access_mode m)
+{
+  assert(t);
+  assert(ld);
+
+  t->t.add_deps(task_dep_untyped(ld->ld, access_mode(m)));
+}
+
+void stf_task_start(stf_task_handle t)
+{
+  assert(t);
+  t->t.start();
+}
+
+void stf_task_end(stf_task_handle t)
+{
+  assert(t);
+  t->t.end();
+}
+
+void stf_task_destroy(stf_task_handle t)
+{
+  assert(t);
+  delete t;
+}
 }
