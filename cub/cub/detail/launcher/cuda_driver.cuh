c444d5240d58c8ec0a85099055908068da55f52b
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-07-22 11:00:36 +0200

    Add a macro to disable PDL (#5316)

--- cub/cub/detail/launcher/cuda_driver.cuh
+++ cub/cub/detail/launcher/cuda_driver.cuh
@@ -39,7 +39,7 @@ struct CudaDriverLauncher
       return status;
     }
 
-#if _CCCL_HAS_PDL
+#if _CCCL_HAS_PDL()
     if (dependent_launch)
     {
       CUlaunchAttribute attribute[1];
@@ -61,7 +61,7 @@ struct CudaDriverLauncher
       return static_cast<cudaError_t>(cuLaunchKernelEx(&config, kernel_fn, kernel_args, 0));
     }
     else
-#endif
+#endif // _CCCL_HAS_PDL()
     {
       return static_cast<cudaError_t>(cuLaunchKernel(
         kernel_fn, grid.x, grid.y, grid.z, block.x, block.y, block.z, shared_mem, stream, kernel_args, 0));
