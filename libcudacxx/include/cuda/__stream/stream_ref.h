fd2a17481e87b055879de9edf6c76a3eac4c2df5
pciolkosz <pciolkosz@nvidia.com>
2025-07-22 04:29:45 -0700

    Update the deprecation message
    Co-authored-by: Bernhard Manfred Gruber <bernhardmgruber@gmail.com>

--- libcudacxx/include/cuda/__stream/stream_ref.h
+++ libcudacxx/include/cuda/__stream/stream_ref.h
@@ -60,7 +60,7 @@ public:
   //!
   //! For behavior of the default stream,
   //! @see //! https://docs.nvidia.com/cuda/cuda-runtime-api/stream-sync-behavior.html
-  [[deprecated("Use the constructor from cudaStream_t instead")]]
+  [[deprecated("Using the default/null stream is generally discouraged. If you need to use it, please construct a  stream_ref from cudaStream_t{nullptr}")]]
   _CCCL_HIDE_FROM_ABI stream_ref() = default;
 
   //! @brief Constructs a `stream_ref` from a `cudaStream_t` handle.
