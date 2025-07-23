57d96177dea5c368d1930009e123a0f6b1bb017c
pauleonix <pgrossebley@nvidia.com>
2025-07-23 16:11:20 +0200

    Add include guards and copyright/license

--- cub/test/thread_reduce/catch2_test_thread_reduce_helper.cuh
+++ cub/test/thread_reduce/catch2_test_thread_reduce_helper.cuh
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#pragma once
+
 #include <cuda/cmath>
 #include <cuda/functional>
 #include <cuda/std/cmath>
