189e0832acdf8a0939c8041f0e7b52b876dcf500
Cedric Augonnet <caugonnet@nvidia.com>
2025-07-23 11:30:24 +0200

    Save WIP with python

---
+++ python/cuda_cccl/cuda/cccl/experimental/stf/_stf_bindings_impl.pyx
@@ -0,0 +1,38 @@
+# distutils: language = c++
+# cython: language_level=3
+# cython: linetrace=True
+
+# Python signatures are declared in the companion Python stub file _bindings.pyi
+# Make sure to update PYI with change to Python API to ensure that Python
+# static type checker tools like mypy green-lights cuda.cccl.parallel
+
+from libc.string cimport memset, memcpy
+from libc.stdint cimport uint8_t, uint32_t, uint64_t, int64_t, uintptr_t
+from cpython.bytes cimport PyBytes_FromStringAndSize
+
+from cpython.buffer cimport (
+    Py_buffer, PyBUF_SIMPLE, PyBUF_ANY_CONTIGUOUS,
+    PyBuffer_Release, PyObject_CheckBuffer, PyObject_GetBuffer
+)
+from cpython.pycapsule cimport (
+    PyCapsule_CheckExact, PyCapsule_IsValid, PyCapsule_GetPointer
+)
+
+import ctypes
+
+cdef extern from "<cuda.h>":
+    cdef struct OpaqueCUstream_st
+    cdef struct OpaqueCUkernel_st
+    cdef struct OpaqueCUlibrary_st
+
+    ctypedef int CUresult
+    ctypedef OpaqueCUstream_st *CUstream
+    ctypedef OpaqueCUkernel_st *CUkernel
+    ctypedef OpaqueCUlibrary_st *CUlibrary
+
+cdef extern from "cccl/c/experimental/stf/stf.h":
+    ctypedef struct stf_ctx_handle_t
+    ctypedef stf_ctx_handle_t* stf_ctx_handle
+
+    void stf_ctx_create(stf_ctx_handle* ctx)
+    void stf_ctx_finalize(stf_ctx_handle ctx)
