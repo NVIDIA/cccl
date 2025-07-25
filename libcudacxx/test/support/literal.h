4c236be3c80b4b220f0ff1791d30d4cf447e902f
David Bayer <bayer.david99@gmail.com>
2025-07-25 11:23:03 +0200

    Implement `format.formatter.spec`

--- libcudacxx/test/support/literal.h
+++ libcudacxx/test/support/literal.h
@@ -11,6 +11,7 @@
 #ifndef TEST_SUPPORT_LITERAL_H
 #define TEST_SUPPORT_LITERAL_H
 
+#include <cuda/std/cstddef>
 #include <cuda/std/type_traits>
 
 template <class CharT>
@@ -59,37 +60,37 @@ template <class CharT>
 #  define TEST_CHARLIT(CharT, val) _test_charlit_impl<CharT>(val, L##val, u##val, U##val)
 #endif // _CCCL_HAS_CHAR8_T()
 
-template <class CharT>
-[[nodiscard]] __host__ __device__ constexpr const CharT* _test_strlit_impl(
-  [[maybe_unused]] const char* char_ptr,
-  [[maybe_unused]] const wchar_t* wchar_ptr,
+template <class CharT, cuda::std::size_t N>
+[[nodiscard]] __host__ __device__ constexpr auto _test_strlit_impl(
+  [[maybe_unused]] const char (&char_str)[N],
+  [[maybe_unused]] const wchar_t (&wchar_str)[N],
 #if _CCCL_HAS_CHAR8_T()
-  [[maybe_unused]] const char8_t* char8_ptr,
+  [[maybe_unused]] const char8_t (&char8_str)[N],
 #endif // _CCCL_HAS_CHAR8_T()
-  [[maybe_unused]] const char16_t* char16_ptr,
-  [[maybe_unused]] const char32_t* char32_ptr) noexcept
+  [[maybe_unused]] const char16_t (&char16_str)[N],
+  [[maybe_unused]] const char32_t (&char32_str)[N]) noexcept -> const CharT (&)[N]
 {
   if constexpr (cuda::std::is_same_v<CharT, char>)
   {
-    return char_ptr;
+    return char_str;
   }
   else if constexpr (cuda::std::is_same_v<CharT, wchar_t>)
   {
-    return wchar_ptr;
+    return wchar_str;
   }
 #if _CCCL_HAS_CHAR8_T()
   else if constexpr (cuda::std::is_same_v<CharT, char8_t>)
   {
-    return char8_ptr;
+    return char8_str;
   }
 #endif // _CCCL_HAS_CHAR8_T()
   else if constexpr (cuda::std::is_same_v<CharT, char16_t>)
   {
-    return char16_ptr;
+    return char16_str;
   }
   else if constexpr (cuda::std::is_same_v<CharT, char32_t>)
   {
-    return char32_ptr;
+    return char32_str;
   }
   else
   {
