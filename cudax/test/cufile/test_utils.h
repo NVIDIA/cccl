545d81ec5062da05c876e05624053a1c23720cc3
jvera <jvera@nvidia.com>
2025-07-31 16:46:29 +0000

    added missing license and removed unused includes

--- cudax/test/cufile/test_utils.h
+++ cudax/test/cufile/test_utils.h
@@ -1,9 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
 #pragma once
 
 #include <cstddef>
-#include <stdexcept>
 #include <cstdlib>
-#include <string>
 
 namespace cuda::experimental::cufile::test_utils {
 
