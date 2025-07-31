ead5285902e4b43061561d6aa61bb332f3a0ed33
jvera <jvera@nvidia.com>
2025-07-31 16:46:29 +0000

    added missing license and removed unused includes

--- cudax/test/cufile/driver_functions.cu
+++ cudax/test/cufile/driver_functions.cu
@@ -4,17 +4,13 @@
 // under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
 #include <cuda/experimental/__cufile/driver.hpp>
 
 #include <c2h/catch2_test_helper.h>
-
-
-#include <string>
-#include <cuda/std/string_view>
 
 namespace {
     // workaround to be able to open and close the driver several times during the test. If the logging is enabled, it crashes on the second close.
