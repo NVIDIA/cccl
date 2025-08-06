2d2057d22a00dcda4cdc756fdc44733d91034c5e
Federico Busato <50413820+fbusato@users.noreply.github.com>
2025-08-06 10:55:44 -0700

    Update libcudacxx/test/libcudacxx/cuda/functional/maximum.pass.cpp
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- libcudacxx/test/libcudacxx/cuda/functional/maximum.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/functional/maximum.pass.cpp
@@ -38,7 +38,7 @@ __host__ __device__ constexpr bool test()
   test<double>(1.0, 2.0, 2.0);
 #if _CCCL_HAS_FLOAT128()
   test<__float128>(__float128(1.0f), __float128(2.0f), __float128(2.0f));
-#endif
+#endif // _CCCL_HAS_FLOAT128()
   return true;
 }
 
