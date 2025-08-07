df834f1ce3f3c823c8bc588d67a51861bfff1cd4
David Bayer <bayer.david99@gmail.com>
2025-08-07 17:58:01 +0200

    try to fix missing return statement warning

--- libcudacxx/include/cuda/std/__format/output_utils.h
+++ libcudacxx/include/cuda/std/__format/output_utils.h
@@ -82,10 +82,10 @@ __fmt_padding_size(size_t __size, size_t __width, __fmt_spec_alignment __align)
     case __fmt_spec_alignment::__default:
     case __fmt_spec_alignment::__right:
       return {__fill, 0};
+    case __fmt_spec_alignment::__zero_padding:
     default:
-      break;
+      _CCCL_UNREACHABLE();
   }
-  _CCCL_UNREACHABLE();
 }
 
 //! Copy wrapper.
