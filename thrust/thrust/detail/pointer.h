d8e4c2a95626e33c071b3cd4597b5720083e576c
pauleonix <pgrossebley@nvidia.com>
2025-08-11 18:23:47 +0200

    Move pointer doc to detail/pointer.h

--- thrust/thrust/detail/pointer.h
+++ thrust/thrust/detail/pointer.h
@@ -96,6 +96,10 @@ struct pointer_base
 };
 } // namespace detail
 
+/*! \addtogroup memory_management Memory Management
+ *  \{
+ */
+
 // the base type for all of thrust's tagged pointers.
 // for reasonable pointer-like semantics, derived types should reimplement the following:
 // 1. no-argument constructor
@@ -104,6 +108,37 @@ struct pointer_base
 // 4. constructor from OtherPointer to void
 // 5. assignment from OtherPointer related by convertibility
 // These should just call the corresponding members of pointer.
+
+/*! \p pointer stores a pointer to an object allocated in memory. Like \p device_ptr, this
+ *  type ensures type safety when dispatching standard algorithms on ranges resident in memory.
+ *
+ *  \p pointer generalizes \p device_ptr by relaxing the backend system associated with the \p pointer.
+ *  Instead of the backend system specified by \p THRUST_DEVICE_SYSTEM, \p pointer's
+ *  system is given by its second template parameter, \p Tag. For the purpose of Thrust dispatch,
+ *  <tt>device_ptr<Element></tt> and <tt>pointer<Element,device_system_tag></tt> are considered equivalent.
+ *
+ *  The raw pointer encapsulated by a \p pointer may be obtained through its <tt>get</tt> member function
+ *  or the \p raw_pointer_cast free function.
+ *
+ *  \tparam Element specifies the type of the pointed-to object.
+ *
+ *  \tparam Tag specifies the system with which this \p pointer is associated. This may be any Thrust
+ *          backend system, or a user-defined tag.
+ *
+ *  \tparam Reference allows the client to specify the reference type returned upon derereference.
+ *          By default, this type is <tt>reference<Element,pointer></tt>.
+ *
+ *  \tparam Derived allows the client to specify the name of the derived type when \p pointer is used as
+ *          a base class. This is useful to ensure that arithmetic on values of the derived type return
+ *          values of the derived type as a result. By default, this type is <tt>pointer<Element,Tag,Reference></tt>.
+ *
+ *  \note \p pointer is not a smart pointer; it is the client's responsibility to deallocate memory
+ *        pointer to by \p pointer.
+ *
+ *  \see device_ptr
+ *  \see reference
+ *  \see raw_pointer_cast
+ */
 template <typename Element, typename Tag, typename Reference, typename Derived>
 class pointer : public detail::pointer_base<Element, Tag, Reference, Derived>::type
 {
@@ -135,8 +170,12 @@ private:
   using typename super_t::base_type;
 
 public:
+  /*! The type of the raw pointer
+   */
   using raw_pointer = typename super_t::base_type;
 
+  /*! \p pointer's default constructor initializes its encapsulated pointer to \c 0
+   */
   _CCCL_HOST_DEVICE pointer()
       : super_t(static_cast<Element*>(nullptr))
   {}
@@ -146,15 +185,24 @@ public:
       : super_t(static_cast<Element*>(nullptr))
   {}
 
-  // OtherValue shall be convertible to Value
+  /*! This constructor allows construction of a <tt>pointer<const T, ...></tt> from a <tt>T*</tt>.
+   *
+   *  \param ptr A raw pointer to copy from, presumed to point to a location in \p Tag's memory.
+   *  \tparam OtherElement \p OtherElement shall be convertible to \p Element.
+   */
   // XXX consider making the pointer implementation a template parameter which defaults to Element *
   template <typename OtherElement>
   _CCCL_HOST_DEVICE explicit pointer(OtherElement* ptr)
       : super_t(ptr)
   {}
 
-  // OtherPointer's element_type shall be convertible to Element
-  // OtherPointer's system shall be convertible to Tag
+  /*! This constructor allows initialization from another pointer-like object.
+   *
+   *  \param other The \p OtherPointer to copy.
+   *
+   *  \tparam OtherPointer The tag associated with \p OtherPointer shall be convertible to \p Tag,
+   *                       and its element type shall be convertible to \p Element.
+   */
   template <typename OtherPointer,
             typename detail::enable_if_pointer_is_convertible<OtherPointer, pointer>::type* = nullptr>
   _CCCL_HOST_DEVICE pointer(const OtherPointer& other)
@@ -178,8 +226,15 @@ public:
     return static_cast<derived_type&>(*this);
   }
 
-  // OtherPointer's element_type shall be convertible to Element
-  // OtherPointer's system shall be convertible to Tag
+  /*! Assignment operator allows assigning from another pointer-like object whose element type
+   *  is convertible to \c Element.
+   *
+   *  \param other The other pointer-like object to assign from.
+   *  \return <tt>*this</tt>
+   *
+   *  \tparam OtherPointer The tag associated with \p OtherPointer shall be convertible to \p Tag,
+   *                       and its element type shall be convertible to \p Element.
+   */
   template <typename OtherPointer>
   _CCCL_HOST_DEVICE typename detail::enable_if_pointer_is_convertible<OtherPointer, pointer, derived_type&>::type
   operator=(const OtherPointer& other)
@@ -190,6 +245,9 @@ public:
 
   // observers
 
+  /*! \p get returns this \p pointer's encapsulated raw pointer.
+   *  \return This \p pointer's raw pointer.
+   */
   _CCCL_HOST_DEVICE Element* get() const
   {
     return super_t::base();
@@ -242,5 +300,8 @@ public:
     return !(nullptr == p);
   }
 };
+
+/*! \} // memory_management
+ */
 
 THRUST_NAMESPACE_END
