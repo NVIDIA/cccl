eba01c815d1cdd9eba5ec436044a965c099232f7
David Bayer <48736217+davebayer@users.noreply.github.com>
2025-08-11 10:57:37 +0200

    Update extended_data_types.fp128.fail.cpp

--- libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.fp128.fail.cpp
+++ libcudacxx/test/libcudacxx/libcxx/macros/extended_data_types.fp128.fail.cpp
@@ -7,6 +7,10 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
+
+// ADDITIONAL_COMPILE_OPTIONS_HOST: -fext-numeric-literals
+// ADDITIONAL_COMPILE_DEFINITIONS: CCCL_GCC_HAS_EXTENDED_NUMERIC_LITERALS
+
 #include <cuda/std/__cccl/extended_data_types.h>
 
 #include "test_macros.h"
