892cdbfc328280fb5a07bb0a544715e99879dae5
Eric Niebler <eniebler@boost.org>
2025-08-12 13:07:55 -0700

    fix formatting

--- cudax/include/cuda/experimental/__execution/let_value.cuh
+++ cudax/include/cuda/experimental/__execution/let_value.cuh
@@ -302,7 +302,7 @@ struct _CCCL_TYPE_VISIBILITY_DEFAULT __let_t
 
   template <class _Fn>
   struct _CCCL_TYPE_VISIBILITY_HIDDEN __closure_base_t // hidden visibility because member __fn_ is hidden if it is an
-                                                  // extended (host/device) lambda
+                                                       // extended (host/device) lambda
   {
     template <class _Sndr>
     [[nodiscard]] _CCCL_TRIVIAL_API auto operator()(_Sndr __sndr) const
