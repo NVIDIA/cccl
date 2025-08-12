2f671ba5117e2a1981a0b81dc562273a9ddbe261
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-08-12 17:57:44 +0200

    Remove unnecessary header

--- libcudacxx/include/cuda/__nvtx/nvtx.h
+++ libcudacxx/include/cuda/__nvtx/nvtx.h
@@ -12,8 +12,6 @@
 
 #include <cuda/std/detail/__config>
 
-#include <cuda/std/__cccl/diagnostic.h>
-
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
