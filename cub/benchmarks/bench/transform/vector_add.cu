f84d0bbe65ac915bbf9743c868995cb89a4bff10
Nader Al Awar <naderalawar@gmail.com>
2025-08-13 14:42:02 +0000

    Add vector_add and grayscale benchmarks

---
+++ cub/benchmarks/bench/transform/vector_add.cu
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "common.h"
+
+template <typename T, typename OffsetT>
+static void vector_add(nvbench::state& state, nvbench::type_list<T, OffsetT>)
+{
+  const auto n               = narrow<OffsetT>(state.get_int64("Elements{io}"));
+  thrust::device_vector<T> a = generate(n);
+  thrust::device_vector<T> b = generate(n);
+
+  thrust::device_vector<T> c = [&]() {
+    if constexpr (::cuda::std::is_trivially_constructible_v<T>)
+    {
+      return thrust::device_vector<T>(n, thrust::no_init);
+    }
+    else
+    {
+      return thrust::device_vector<T>(n);
+    }
+  }();
+
+  state.add_element_count(n);
+  state.add_global_memory_reads<T>(2 * n);
+  state.add_global_memory_writes<T>(n);
+
+  bench_transform(state, ::cuda::std::tuple{a.begin(), b.begin()}, c.begin(), n, ::cuda::std::plus<T>{});
+}
+
+NVBENCH_BENCH_TYPES(vector_add, NVBENCH_TYPE_AXES(all_types, offset_types))
+  .set_name("vector_add")
+  .set_type_axes_names({"T{ct}", "OffsetT{ct}"})
+  .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4));
