980088d3e6efdb7b9f28e687a5eed137422a7e44
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-08-18 18:01:58 +0200

    License

--- thrust/benchmarks/bench/transform/if_stencil.cu
+++ thrust/benchmarks/bench/transform/if_stencil.cu
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <thrust/count.h>
 #include <thrust/device_vector.h>
