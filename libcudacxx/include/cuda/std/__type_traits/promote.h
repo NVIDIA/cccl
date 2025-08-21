31ce8e3399b19faa401a690f4e31af07b42a5728
Michael Schellenberger Costa <miscco@nvidia.com>
2025-08-21 14:32:19 +0200

    Rework our `fmax` and `fmin` implementation to be potentially constexpr (#5539)
    * Rework our `fmax` and `fmin` implementation to be potentially constexpr
    
    * Optimize for host device extended floating point implementations
    
    * Try to make old gcc happy
    
    * Try and work around clang-cuda limitations with 128 bit integers
    
    * Try and improve half handling before CTK 12.2
    
    * More fixes
    
    * Try to use builtins whenever possible :(
    
    * Cleanup casting

--- libcudacxx/include/cuda/std/__type_traits/promote.h
+++ libcudacxx/include/cuda/std/__type_traits/promote.h
@@ -34,12 +34,12 @@ template <class _Tp>
 struct __numeric_type
 {
   _CCCL_API inline static void __test(...);
-#if _LIBCUDACXX_HAS_NVFP16()
+#if _CCCL_HAS_NVFP16()
   _CCCL_API inline static float __test(__half);
-#endif // _LIBCUDACXX_HAS_NVBF16()
-#if _LIBCUDACXX_HAS_NVBF16()
+#endif // _CCCL_HAS_NVFP16()
+#if _CCCL_HAS_NVBF16()
   _CCCL_API inline static float __test(__nv_bfloat16);
-#endif // _LIBCUDACXX_HAS_NVFP16()
+#endif // _CCCL_HAS_NVFP16()
   _CCCL_API inline static float __test(float);
   _CCCL_API inline static double __test(char);
   _CCCL_API inline static double __test(int);
@@ -67,7 +67,7 @@ struct __is_mixed_extended_floating_point
   static constexpr bool value = false;
 };
 
-#if _LIBCUDACXX_HAS_NVFP16() && _LIBCUDACXX_HAS_NVBF16()
+#if _CCCL_HAS_NVFP16() && _CCCL_HAS_NVBF16()
 template <class _A1>
 struct __is_mixed_extended_floating_point<_A1, __half, __nv_bfloat16>
 {
@@ -103,7 +103,7 @@ struct __is_mixed_extended_floating_point<__nv_bfloat16, __half, _A1>
 {
   static constexpr bool value = true;
 };
-#endif // _LIBCUDACXX_HAS_NVFP16() && _LIBCUDACXX_HAS_NVBF16()
+#endif // _CCCL_HAS_NVFP16() && _CCCL_HAS_NVBF16()
 
 template <class _A1,
           class _A2 = void,
