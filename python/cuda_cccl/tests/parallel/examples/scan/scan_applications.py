72b3bf0e52a15a0b2be887a1917f854c9c425c29
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-08-25 09:26:13 -0500

    Add example of computing exponential moving average using inclusive_scan

--- python/cuda_cccl/tests/parallel/examples/scan/scan_applications.py
+++ python/cuda_cccl/tests/parallel/examples/scan/scan_applications.py
@@ -31,8 +31,7 @@ def inclusive_segmented_sum_example():
     corresponding to segmented data [[1, 1], [1, 1, 1], [1], [1, 1]],
     the expected prefix-sum values are [1, 2, 1, 2, 3, 1, 1, 2]
     """
-
-    print("[Inclusive_segmented_sum example]")
+    print("[Begin inclusive_segmented_sum example]")
     data = cp.asarray([1, 1, 1, 1, 1, 1, 1, 1], dtype=cp.int64)
     hflg = cp.asarray([0, 0, 1, 0, 0, 1, 1, 0], dtype=cp.int32)
 
@@ -63,6 +62,7 @@ def inclusive_segmented_sum_example():
         f"Inclusive segmented sum: computed result={result['value']}, "
         f"expected result={expected_prefix}"
     )
+    print("[End inclusive_segmented_sum example]")
     return result
 
 
@@ -74,7 +74,7 @@ def logcdfs_from_logpdfs_example():
     Use log-add-exp binary operation to sidestep flush-to-zero
     and numerical exceptions computing logarithms of that.
     """
-    print("[Compute logcdfs from logpdfs example]")
+    print("[Begin logCDFs from logPDFs example]")
     n = 500
     p = 0.31
     m = cp.arange(n + 1, dtype=cp.float64)
@@ -119,10 +119,95 @@ def logcdfs_from_logpdfs_example():
 
     print(f"Quartiles of Binomial({n}, {p}) are {(q25, q75)}")
     print(f"CDF at quartiles: {[math.exp(logcdf[q25]), math.exp(logcdf[q75])]}")
+    print("[End logCDFs from logPDFs example]")
+
+
+def exponential_moving_average_using_scan_example():
+    """
+    Given input vector u and smoothing parameter 0<alpha<1
+    the exponential moving average sequence ma is defined
+    by ma[t] = (1 - alpha) * u[t] + alpha * ma[t-1] with
+    initial condition ma[0] = u[0].
+
+    This recurrence equation admits a closed form solution:
+
+    ma[t] = alpha ** (t + 1) * u[0] + (1 - alpha) * (alpha ** t) *
+        sum(u[s] * alpha ** (-s) for s in range(t))
+
+    Closed form solution could be computed using inclusive_scan,
+    except naive implementation suffers from underflow/overflow
+    problem for long sequences.
+
+    This implementation solves this problem by representing number
+    using (fp_value, int_exponent) to extend representable range.
+    """
+    print("[Begin exponential moving average example]")
+    u = 3.0 + cp.sin(cp.linspace(-3.0, 3.0, num=1024, dtype=cp.double))
+    u += cp.random.normal(0.0, 0.1, size=u.size)
+    alpha = 0.05
+
+    assert 0.0 < alpha < 1.0
+
+    @parallel.gpu_struct
+    class ValueScale:
+        value: cp.float64
+        scale: cp.int64
+
+    def add_op(v1: ValueScale, v2: ValueScale) -> ValueScale:
+        if v1.scale > v2.scale:
+            s = v2.scale
+            v = v2.value + v1.value * (alpha ** (v1.scale - v2.scale))
+        else:
+            s = v1.scale
+            v = v1.value + v2.value * (alpha ** (v2.scale - v1.scale))
+        return ValueScale(v, s)
+
+    def negative_op(i: cp.int64) -> cp.int64:
+        return -i
+
+    seq_it = parallel.CountingIterator(cp.int64(0))
+    negative_exponents_it = parallel.TransformIterator(seq_it, negative_op)
+    d_inp = parallel.ZipIterator(u, negative_exponents_it)
+
+    d_cumsum = cp.empty(u.shape, dtype=ValueScale.dtype)
+    h_init = ValueScale(0.0, 0)
+
+    parallel.inclusive_scan(d_inp, d_cumsum, add_op, h_init, u.size)
+
+    it_seq = parallel.CountingIterator(cp.int64(0))
+    d_ema = cp.empty_like(u)
+
+    def combine_op(v: ValueScale, t: cp.int64) -> cp.float64:
+        return (1 - alpha) * v.value * alpha ** (t + v.scale)
+
+    parallel.binary_transform(d_cumsum, it_seq, d_ema, combine_op, u.size)
+
+    d_ema += (alpha ** cp.arange(1, u.size + 1)) * u[0]
+
+    def ema_ref(u: np.ndarray, alpha: float):
+        "Sequential reference implementation of EMA"
+        a = np.empty_like(u)
+        a[0] = u[0]
+        for t in range(1, u.size):
+            a[t] = (1 - alpha) * u[t] + alpha * a[t - 1]
+
+        return a
+
+    h_u = u.get()
+    h_ema = ema_ref(h_u, alpha)
+
+    assert np.allclose(h_ema, d_ema.get())
+
+    print(f"Start of input signal: {h_u[:4]}")
+    print(f"Smoothing parameter: {alpha}")
+    print(f"EMA: {d_ema[:4]}")
+    print(f"EMA_ref: {h_ema[:4]}")
+    print("[End exponential moving average example]")
 
 
 if __name__ == "__main__":
     print("Running scan_applications examples")
     inclusive_segmented_sum_example()
     logcdfs_from_logpdfs_example()
+    exponential_moving_average_using_scan_example()
     print("All examples completed successfully!")
