28ca3fd872b54bff6729dc266cf9ae8c6cedd80e
Ashwin Srinath <shwina@users.noreply.github.com>
2025-08-25 20:24:46 +0000

    Refactor bench_scan.py after review feedback

--- python/cuda_cccl/benchmarks/parallel/bench_scan.py
+++ python/cuda_cccl/benchmarks/parallel/bench_scan.py
@@ -1,10 +1,11 @@
 import cupy as cp
 import numpy as np
+import pytest
 
 import cuda.cccl.parallel.experimental as parallel
 
 
-def scan_pointer_exclusive(input_array, build_only):
+def scan_pointer(input_array, build_only, scan_type):
     size = len(input_array)
     res = cp.empty_like(input_array)
     h_init = np.zeros(tuple(), dtype=input_array.dtype)
@@ -12,7 +13,11 @@ def scan_pointer_exclusive(input_array, build_only):
     def my_add(a, b):
         return a + b
 
-    alg = parallel.make_exclusive_scan(input_array, res, my_add, h_init)
+    if scan_type == "exclusive":
+        alg = parallel.make_exclusive_scan(input_array, res, my_add, h_init)
+    else:  # inclusive
+        alg = parallel.make_inclusive_scan(input_array, res, my_add, h_init)
+
     if not build_only:
         temp_storage_bytes = alg(None, input_array, res, size, h_init)
         temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
@@ -21,15 +26,20 @@ def scan_pointer_exclusive(input_array, build_only):
     cp.cuda.runtime.deviceSynchronize()
 
 
-def scan_pointer_inclusive(input_array, build_only):
+def scan_pointer_well_known(input_array, build_only, scan_type):
     size = len(input_array)
     res = cp.empty_like(input_array)
     h_init = np.zeros(tuple(), dtype=input_array.dtype)
 
-    def my_add(a, b):
-        return a + b
+    if scan_type == "exclusive":
+        alg = parallel.make_exclusive_scan(
+            input_array, res, parallel.OpKind.PLUS, h_init
+        )
+    else:  # inclusive
+        alg = parallel.make_inclusive_scan(
+            input_array, res, parallel.OpKind.PLUS, h_init
+        )
 
-    alg = parallel.make_inclusive_scan(input_array, res, my_add, h_init)
     if not build_only:
         temp_storage_bytes = alg(None, input_array, res, size, h_init)
         temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
@@ -38,37 +48,7 @@ def scan_pointer_inclusive(input_array, build_only):
     cp.cuda.runtime.deviceSynchronize()
 
 
-def scan_pointer_well_known_exclusive(input_array, build_only):
-    size = len(input_array)
-    res = cp.empty_like(input_array)
-    h_init = np.zeros(tuple(), dtype=input_array.dtype)
-
-    # Use the well-known PLUS operation from OpKind
-    alg = parallel.make_exclusive_scan(input_array, res, parallel.OpKind.PLUS, h_init)
-    if not build_only:
-        temp_storage_bytes = alg(None, input_array, res, size, h_init)
-        temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
-        alg(temp_storage, input_array, res, size, h_init)
-
-    cp.cuda.runtime.deviceSynchronize()
-
-
-def scan_pointer_well_known_inclusive(input_array, build_only):
-    size = len(input_array)
-    res = cp.empty_like(input_array)
-    h_init = np.zeros(tuple(), dtype=input_array.dtype)
-
-    # Use the well-known PLUS operation from OpKind
-    alg = parallel.make_inclusive_scan(input_array, res, parallel.OpKind.PLUS, h_init)
-    if not build_only:
-        temp_storage_bytes = alg(None, input_array, res, size, h_init)
-        temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
-        alg(temp_storage, input_array, res, size, h_init)
-
-    cp.cuda.runtime.deviceSynchronize()
-
-
-def scan_struct_exclusive(input_array, build_only):
+def scan_struct(input_array, build_only, scan_type):
     size = len(input_array)
     res = cp.empty_like(input_array)
     h_init = MyStruct(0, 0)
@@ -76,7 +56,11 @@ def scan_struct_exclusive(input_array, build_only):
     def my_add(a, b):
         return MyStruct(a.x + b.x, a.y + b.y)
 
-    alg = parallel.make_exclusive_scan(input_array, res, my_add, h_init)
+    if scan_type == "exclusive":
+        alg = parallel.make_exclusive_scan(input_array, res, my_add, h_init)
+    else:  # inclusive
+        alg = parallel.make_inclusive_scan(input_array, res, my_add, h_init)
+
     if not build_only:
         temp_storage_bytes = alg(None, input_array, res, size, h_init)
         temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
@@ -85,49 +69,18 @@ def scan_struct_exclusive(input_array, build_only):
     cp.cuda.runtime.deviceSynchronize()
 
 
-def scan_struct_inclusive(input_array, build_only):
-    size = len(input_array)
-    res = cp.empty_like(input_array)
-    h_init = MyStruct(0, 0)
-
-    def my_add(a, b):
-        return MyStruct(a.x + b.x, a.y + b.y)
-
-    alg = parallel.make_inclusive_scan(input_array, res, my_add, h_init)
-    if not build_only:
-        temp_storage_bytes = alg(None, input_array, res, size, h_init)
-        temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
-        alg(temp_storage, input_array, res, size, h_init)
-
-    cp.cuda.runtime.deviceSynchronize()
-
-
-def scan_iterator_exclusive(inp, size, build_only):
-    dt = cp.int32
-    res = cp.empty(size, dtype=dt)
-    h_init = np.zeros(tuple(), dtype=dt)
+def scan_iterator(inp, size, build_only, scan_type):
+    res = cp.empty(size, dtype=np.int32)
+    h_init = np.zeros(tuple(), dtype=np.int32)
 
     def my_add(a, b):
         return a + b
 
-    alg = parallel.make_exclusive_scan(inp, res, my_add, h_init)
-    if not build_only:
-        temp_storage_bytes = alg(None, inp, res, size, h_init)
-        temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
-        alg(temp_storage, inp, res, size, h_init)
+    if scan_type == "exclusive":
+        alg = parallel.make_exclusive_scan(inp, res, my_add, h_init)
+    else:  # inclusive
+        alg = parallel.make_inclusive_scan(inp, res, my_add, h_init)
 
-    cp.cuda.runtime.deviceSynchronize()
-
-
-def scan_iterator_inclusive(inp, size, build_only):
-    dt = cp.int32
-    res = cp.empty(size, dtype=dt)
-    h_init = np.zeros(tuple(), dtype=dt)
-
-    def my_add(a, b):
-        return a + b
-
-    alg = parallel.make_inclusive_scan(inp, res, my_add, h_init)
     if not build_only:
         temp_storage_bytes = alg(None, inp, res, size, h_init)
         temp_storage = cp.empty(temp_storage_bytes, dtype=np.uint8)
@@ -142,194 +95,94 @@ class MyStruct:
     y: np.int32
 
 
-def bench_compile_scan_pointer_exclusive(compile_benchmark):
-    input_array = cp.random.randint(0, 10, 10)
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["compile_benchmark", "benchmark"])
+def bench_scan_pointer(bench_fixture, request, size, scan_type):
+    # Use small size for compile benchmarks, parameterized size for runtime benchmarks
+    actual_size = 10 if bench_fixture == "compile_benchmark" else size
+    input_array = cp.random.randint(0, 10, actual_size)
 
     def run():
-        scan_pointer_exclusive(input_array, build_only=True)
+        scan_pointer(
+            input_array,
+            build_only=(bench_fixture == "compile_benchmark"),
+            scan_type=scan_type,
+        )
 
-    compile_benchmark(parallel.make_exclusive_scan, run)
-
-
-def bench_compile_scan_pointer_inclusive(compile_benchmark):
-    input_array = cp.random.randint(0, 10, 10)
-
-    def run():
-        scan_pointer_inclusive(input_array, build_only=True)
-
-    compile_benchmark(parallel.make_inclusive_scan, run)
-
-
-def bench_compile_scan_iterator_exclusive(compile_benchmark):
-    inp = parallel.CountingIterator(np.int32(0))
-
-    def run():
-        scan_iterator_exclusive(inp, 10, build_only=True)
-
-    compile_benchmark(parallel.make_exclusive_scan, run)
+    fixture = request.getfixturevalue(bench_fixture)
+    if bench_fixture == "compile_benchmark":
+        if scan_type == "exclusive":
+            fixture(parallel.make_exclusive_scan, run)
+        else:
+            fixture(parallel.make_inclusive_scan, run)
+    else:
+        fixture(run)
 
 
-def bench_compile_scan_iterator_inclusive(compile_benchmark):
-    inp = parallel.CountingIterator(np.int32(0))
-
-    def run():
-        scan_iterator_inclusive(inp, 10, build_only=True)
-
-    compile_benchmark(parallel.make_inclusive_scan, run)
-
-
-def bench_scan_pointer_exclusive(benchmark, size):
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["benchmark"])
+def bench_scan_pointer_well_known(bench_fixture, request, size, scan_type):
     input_array = cp.random.randint(0, 10, size)
 
     def run():
-        scan_pointer_exclusive(input_array, build_only=False)
+        scan_pointer_well_known(input_array, build_only=False, scan_type=scan_type)
 
-    benchmark(run)
-
-
-def bench_scan_pointer_inclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, size)
-
-    def run():
-        scan_pointer_inclusive(input_array, build_only=False)
-
-    benchmark(run)
+    fixture = request.getfixturevalue(bench_fixture)
+    fixture(run)
 
 
-def bench_scan_pointer_well_known_exclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, size)
-
-    def run():
-        scan_pointer_well_known_exclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_pointer_well_known_inclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, size)
-
-    def run():
-        scan_pointer_well_known_inclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_iterator_exclusive(benchmark, size):
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["compile_benchmark", "benchmark"])
+def bench_scan_iterator(bench_fixture, request, size, scan_type):
     inp = parallel.CountingIterator(np.int32(0))
+    # Use small size for compile benchmarks, parameterized size for runtime benchmarks
+    actual_size = 10 if bench_fixture == "compile_benchmark" else size
 
     def run():
-        scan_iterator_exclusive(inp, size, build_only=False)
+        scan_iterator(
+            inp,
+            actual_size,
+            build_only=(bench_fixture == "compile_benchmark"),
+            scan_type=scan_type,
+        )
 
-    benchmark(run)
+    fixture = request.getfixturevalue(bench_fixture)
+    if bench_fixture == "compile_benchmark":
+        if scan_type == "exclusive":
+            fixture(parallel.make_exclusive_scan, run)
+        else:
+            fixture(parallel.make_inclusive_scan, run)
+    else:
+        fixture(run)
 
 
-def bench_scan_iterator_inclusive(benchmark, size):
-    inp = parallel.CountingIterator(np.int32(0))
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["compile_benchmark", "benchmark"])
+def bench_scan_struct(bench_fixture, request, size, scan_type):
+    # Use small size for compile benchmarks, parameterized size for runtime benchmarks
+    actual_size = 10 if bench_fixture == "compile_benchmark" else size
+    input_array = cp.random.randint(0, 10, (actual_size, 2), dtype="int32").view(
+        MyStruct
+    )
 
     def run():
-        scan_iterator_inclusive(inp, size, build_only=False)
+        scan_struct(
+            input_array,
+            build_only=(bench_fixture == "compile_benchmark"),
+            scan_type=scan_type,
+        )
 
-    benchmark(run)
-
-
-def bench_scan_struct_exclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, (size, 2), dtype="int32").view(MyStruct)
-
-    def run():
-        scan_struct_exclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_struct_inclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, (size, 2), dtype="int32").view(MyStruct)
-
-    def run():
-        scan_struct_inclusive(input_array, build_only=False)
-
-    benchmark(run)
+    fixture = request.getfixturevalue(bench_fixture)
+    if bench_fixture == "compile_benchmark":
+        if scan_type == "exclusive":
+            fixture(parallel.make_exclusive_scan, run)
+        else:
+            fixture(parallel.make_inclusive_scan, run)
+    else:
+        fixture(run)
 
 
-def bench_scan_pointer_single_phase_exclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, size)
-
-    # warm up run
-    scan_pointer_single_phase_exclusive(input_array, build_only=False)
-
-    # benchmark run
-    def run():
-        scan_pointer_single_phase_exclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_pointer_single_phase_inclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, size)
-
-    # warm up run
-    scan_pointer_single_phase_inclusive(input_array, build_only=False)
-
-    # benchmark run
-    def run():
-        scan_pointer_single_phase_inclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_iterator_single_phase_exclusive(benchmark, size):
-    inp = parallel.CountingIterator(np.int32(0))
-
-    # warm up run
-    scan_iterator_single_phase_exclusive(inp, size, build_only=False)
-
-    # benchmark run
-    def run():
-        scan_iterator_single_phase_exclusive(inp, size, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_iterator_single_phase_inclusive(benchmark, size):
-    inp = parallel.CountingIterator(np.int32(0))
-
-    # warm up run
-    scan_iterator_single_phase_inclusive(inp, size, build_only=False)
-
-    # benchmark run
-    def run():
-        scan_iterator_single_phase_inclusive(inp, size, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_struct_single_phase_exclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, (size, 2), dtype="int32").view(MyStruct)
-
-    # warm up run
-    scan_struct_single_phase_exclusive(input_array, build_only=False)
-
-    # benchmark run
-    def run():
-        scan_struct_single_phase_exclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def bench_scan_struct_single_phase_inclusive(benchmark, size):
-    input_array = cp.random.randint(0, 10, (size, 2), dtype="int32").view(MyStruct)
-
-    # warm up run
-    scan_struct_single_phase_inclusive(input_array, build_only=False)
-
-    # benchmark run
-    def run():
-        scan_struct_single_phase_inclusive(input_array, build_only=False)
-
-    benchmark(run)
-
-
-def scan_pointer_single_phase_exclusive(input_array, build_only):
-    """Single-phase API that automatically manages temporary storage for exclusive scan."""
+def scan_pointer_single_phase(input_array, build_only, scan_type):
     size = len(input_array)
     res = cp.empty_like(input_array)
     h_init = np.zeros(tuple(), dtype=input_array.dtype)
@@ -337,27 +190,15 @@ def scan_pointer_single_phase_exclusive(input_array, build_only):
     def my_add(a, b):
         return a + b
 
-    parallel.exclusive_scan(input_array, res, my_add, h_init, size)
+    if scan_type == "exclusive":
+        parallel.exclusive_scan(input_array, res, my_add, h_init, size)
+    else:  # inclusive
+        parallel.inclusive_scan(input_array, res, my_add, h_init, size)
 
     cp.cuda.runtime.deviceSynchronize()
 
 
-def scan_pointer_single_phase_inclusive(input_array, build_only):
-    """Single-phase API that automatically manages temporary storage for inclusive scan."""
-    size = len(input_array)
-    res = cp.empty_like(input_array)
-    h_init = np.zeros(tuple(), dtype=input_array.dtype)
-
-    def my_add(a, b):
-        return a + b
-
-    parallel.inclusive_scan(input_array, res, my_add, h_init, size)
-
-    cp.cuda.runtime.deviceSynchronize()
-
-
-def scan_struct_single_phase_exclusive(input_array, build_only):
-    """Single-phase API that automatically manages temporary storage for structs exclusive scan."""
+def scan_struct_single_phase(input_array, build_only, scan_type):
     size = len(input_array)
     res = cp.empty_like(input_array)
     h_init = MyStruct(0, 0)
@@ -365,48 +206,72 @@ def scan_struct_single_phase_exclusive(input_array, build_only):
     def my_add(a, b):
         return MyStruct(a.x + b.x, a.y + b.y)
 
-    parallel.exclusive_scan(input_array, res, my_add, h_init, size)
+    if scan_type == "exclusive":
+        parallel.exclusive_scan(input_array, res, my_add, h_init, size)
+    else:  # inclusive
+        parallel.inclusive_scan(input_array, res, my_add, h_init, size)
 
     cp.cuda.runtime.deviceSynchronize()
 
 
-def scan_struct_single_phase_inclusive(input_array, build_only):
-    """Single-phase API that automatically manages temporary storage for structs inclusive scan."""
-    size = len(input_array)
-    res = cp.empty_like(input_array)
-    h_init = MyStruct(0, 0)
-
-    def my_add(a, b):
-        return MyStruct(a.x + b.x, a.y + b.y)
-
-    parallel.inclusive_scan(input_array, res, my_add, h_init, size)
-
-    cp.cuda.runtime.deviceSynchronize()
-
-
-def scan_iterator_single_phase_exclusive(inp, size, build_only):
-    """Single-phase API that automatically manages temporary storage for iterators exclusive scan."""
-    dt = cp.int32
-    res = cp.empty(size, dtype=dt)
-    h_init = np.zeros(tuple(), dtype=dt)
+def scan_iterator_single_phase(inp, size, build_only, scan_type):
+    res = cp.empty(size, dtype=np.int32)
+    h_init = np.zeros(tuple(), dtype=np.int32)
 
     def my_add(a, b):
         return a + b
 
-    parallel.exclusive_scan(inp, res, my_add, h_init, size)
+    if scan_type == "exclusive":
+        parallel.exclusive_scan(inp, res, my_add, h_init, size)
+    else:  # inclusive
+        parallel.inclusive_scan(inp, res, my_add, h_init, size)
 
     cp.cuda.runtime.deviceSynchronize()
 
 
-def scan_iterator_single_phase_inclusive(inp, size, build_only):
-    """Single-phase API that automatically manages temporary storage for iterators inclusive scan."""
-    dt = cp.int32
-    res = cp.empty(size, dtype=dt)
-    h_init = np.zeros(tuple(), dtype=dt)
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["benchmark"])
+def bench_scan_pointer_single_phase(bench_fixture, request, size, scan_type):
+    input_array = cp.random.randint(0, 10, size)
 
-    def my_add(a, b):
-        return a + b
+    # warm up run
+    scan_pointer_single_phase(input_array, build_only=False, scan_type=scan_type)
 
-    parallel.inclusive_scan(inp, res, my_add, h_init, size)
+    # benchmark run
+    def run():
+        scan_pointer_single_phase(input_array, build_only=False, scan_type=scan_type)
 
-    cp.cuda.runtime.deviceSynchronize()
+    fixture = request.getfixturevalue(bench_fixture)
+    fixture(run)
+
+
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["benchmark"])
+def bench_scan_iterator_single_phase(bench_fixture, request, size, scan_type):
+    inp = parallel.CountingIterator(np.int32(0))
+
+    # warm up run
+    scan_iterator_single_phase(inp, size, build_only=False, scan_type=scan_type)
+
+    # benchmark run
+    def run():
+        scan_iterator_single_phase(inp, size, build_only=False, scan_type=scan_type)
+
+    fixture = request.getfixturevalue(bench_fixture)
+    fixture(run)
+
+
+@pytest.mark.parametrize("scan_type", ["exclusive", "inclusive"])
+@pytest.mark.parametrize("bench_fixture", ["benchmark"])
+def bench_scan_struct_single_phase(bench_fixture, request, size, scan_type):
+    input_array = cp.random.randint(0, 10, (size, 2), dtype="int32").view(MyStruct)
+
+    # warm up run
+    scan_struct_single_phase(input_array, build_only=False, scan_type=scan_type)
+
+    # benchmark run
+    def run():
+        scan_struct_single_phase(input_array, build_only=False, scan_type=scan_type)
+
+    fixture = request.getfixturevalue(bench_fixture)
+    fixture(run)
