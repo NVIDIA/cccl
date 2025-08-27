1f975898c287659bfa749a015fa9f5dbe00c9083
David Bayer <bayer.david99@gmail.com>
2025-08-27 10:18:00 +0200

    Fix test and compilation

--- libcudacxx/test/libcudacxx/cuda/memory/discard_memory.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/memory/discard_memory.pass.cpp
@@ -4,32 +4,41 @@
 // under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
 #include <cuda/memory>
-
-#include "utils.h"
-#define ARR_SZ 128
+#include <cuda/std/cassert>
+#include <cuda/std/cstddef>
+#include <cuda/std/cstdlib>
 
 template <typename T>
-__device__ __host__ __noinline__ void test(bool shared = false)
+__device__ __host__ void test_discard_memory()
 {
-  T* arr = global_alloc<T, ARR_SZ>();
+  constexpr cuda::std::size_t n      = 128;
+  constexpr cuda::std::size_t nbytes = n * sizeof(T);
 
-  cuda::discard_memory(arr, ARR_SZ);
+  auto ptr = static_cast<volatile T*>(cuda::std::malloc(nbytes));
+  assert(ptr != nullptr);
 
-  dealloc<T>(arr);
+  for (cuda::std::size_t i = 0; i < n; ++i)
+  {
+    ptr[i] = static_cast<T>(i);
+  }
+
+  cuda::discard_memory(ptr, nbytes);
+
+  cuda::std::free(const_cast<T*>(ptr));
 }
 
-__device__ __host__ __noinline__ void test_all()
+__device__ __host__ void test()
 {
-  test<int>();
+  test_discard_memory<int>();
 }
 
-int main(int argc, char** argv)
+int main(int, char**)
 {
-  test_all();
+  test();
   return 0;
 }
