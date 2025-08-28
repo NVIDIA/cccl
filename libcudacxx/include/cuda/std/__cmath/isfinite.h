041deadae433139429f3097b979f8a5371b38fb5
Jamaika1 <lukaszcz18@wp.pl>
2025-08-28 11:42:33 +0200

    Fix added ::std

--- libcudacxx/include/cuda/std/__cmath/isfinite.h
+++ libcudacxx/include/cuda/std/__cmath/isfinite.h
@@ -47,7 +47,7 @@ template <class _Tp>
   static_assert(is_floating_point_v<_Tp>, "Only standard floating-point types are supported");
   if (!::cuda::std::__cccl_default_is_constant_evaluated())
   {
-    return ::isfinite(__x);
+    return ::std::isfinite(__x);
   }
   return !::cuda::std::isnan(__x) && !::cuda::std::isinf(__x);
 }
@@ -59,7 +59,7 @@ template <class _Tp>
 #else // ^^^ _CCCL_BUILTIN_ISFINITE ^^^ / vvv !_CCCL_BUILTIN_ISFINITE vvv
   if (!::cuda::std::__cccl_default_is_constant_evaluated())
   {
-    return ::isfinite(__x);
+    return ::std::isfinite(__x);
   }
 #  if _CCCL_HAS_CONSTEXPR_BIT_CAST()
   return (::cuda::std::__fp_get_storage(__x) & __fp_exp_mask_of_v<float>) != __fp_exp_mask_of_v<float>;
@@ -76,7 +76,7 @@ template <class _Tp>
 #else // ^^^ _CCCL_BUILTIN_ISFINITE ^^^ / vvv !_CCCL_BUILTIN_ISFINITE vvv
   if (!::cuda::std::__cccl_default_is_constant_evaluated())
   {
-    return ::isfinite(__x);
+    return ::std::isfinite(__x);
   }
 #  if _CCCL_HAS_CONSTEXPR_BIT_CAST()
   return (::cuda::std::__fp_get_storage(__x) & __fp_exp_mask_of_v<double>) != __fp_exp_mask_of_v<double>;
