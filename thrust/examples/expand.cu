027fc15326c52c8847b099d6ef5553abce87efb9
Sai Charan <scharan@rostam1.rostam.cct.lsu.edu>
2025-08-29 14:54:30 -0500

    fix warnings

--- thrust/examples/expand.cu
+++ thrust/examples/expand.cu
@@ -40,7 +40,7 @@ OutputIterator expand(InputIterator1 first1, InputIterator1 last1, InputIterator
 
   // compute max-scan over the output indices, filling in the holes
   thrust::inclusive_scan(
-    output_indices.begin(), output_indices.end(), output_indices.begin(), thrust::maximum<difference_type>());
+    output_indices.begin(), output_indices.end(), output_indices.begin(), cuda::maximum<difference_type>());
 
   // gather input values according to index array (output = first2[output_indices])
   thrust::gather(output_indices.begin(), output_indices.end(), first2, output);
