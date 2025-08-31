74a9262d466f026ac421068736ee28371a3538e5
Cedric AUGONNET <caugonnet@nvidia.com>
2025-08-31 14:17:27 +0200

    clang-format

--- cudax/test/stf/graph/freeze_nested_graphs.cu
+++ cudax/test/stf/graph/freeze_nested_graphs.cu
@@ -37,7 +37,6 @@ int main()
     X[i] = X0(i);
   }
 
-
   graph_ctx ctx;
 
   auto lX = ctx.logical_data(X);
@@ -60,11 +59,11 @@ int main()
 
   auto lX_alias = sub_ctx.logical_data(frozen_X, data_place::current_device());
 
-// XXX we need an adapter to allocate data from the upper context
-//  auto lY = sub_ctx.logical_data(lX.shape());
-//  sub_ctx.parallel_for(lX.shape(), lX_alias.read(), lY.write())->*[] __device__(size_t i, auto x, auto y) {
-//    y(i) = x(i);
-//  };
+  // XXX we need an adapter to allocate data from the upper context
+  //  auto lY = sub_ctx.logical_data(lX.shape());
+  //  sub_ctx.parallel_for(lX.shape(), lX_alias.read(), lY.write())->*[] __device__(size_t i, auto x, auto y) {
+  //    y(i) = x(i);
+  //  };
 
   sub_ctx.parallel_for(lX.shape(), lX_alias.rw())->*[] __device__(size_t i, auto x) {
     x(i) = x(i) + 2;
@@ -72,19 +71,22 @@ int main()
 
   sub_ctx.finalize_as_graph();
 
+  // The child graph depends on the events to get the frozen data
   ::std::vector<cudaGraphNode_t> fX_ready_nodes = reserved::join_with_graph_nodes(ctx, fX_get_events, ctx.stage());
   cudaGraphNode_t child_graph_node;
-  cuda_safe_call(cudaGraphAddChildGraphNode(&child_graph_node, ctx.get_graph(), fX_ready_nodes.data(), fX_ready_nodes.size(), sub_ctx.get_graph()));
+  cuda_safe_call(cudaGraphAddChildGraphNode(
+    &child_graph_node, ctx.get_graph(), fX_ready_nodes.data(), fX_ready_nodes.size(), sub_ctx.get_graph()));
 
-
+  // Create an event that depends on the child graph, so that we unfreeze after the child graph has been done
   event_list child_graph_event;
-  reserved::fork_from_graph_node(ctx, child_graph_node, ctx.get_graph(), ctx.stage(), child_graph_event, "child graph done");
+  reserved::fork_from_graph_node(
+    ctx, child_graph_node, ctx.get_graph(), ctx.stage(), child_graph_event, "child graph done");
   fX.unfreeze(child_graph_event);
 
   ctx.host_launch(lX.read())->*[](auto x) {
     for (int i = 0; i < static_cast<int>(x.size()); i++)
     {
-      EXPECT(x(i) == 3*X0(i) + 2);
+      EXPECT(x(i) == 3 * X0(i) + 2);
     }
   };
 
