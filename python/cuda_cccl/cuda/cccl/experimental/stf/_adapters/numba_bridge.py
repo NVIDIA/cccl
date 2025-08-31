ccfbb6b016b60afe7d2a543f11fb19c6aa6faedd
Cedric Augonnet <caugonnet@nvidia.com>
2025-08-31 18:23:02 +0200

    fix numba adapter

--- python/cuda_cccl/cuda/cccl/experimental/stf/_adapters/numba_bridge.py
+++ python/cuda_cccl/cuda/cccl/experimental/stf/_adapters/numba_bridge.py
@@ -1,3 +1,3 @@
 def cai_to_numba(cai: dict):
-    from numba import cuda as _cuda
-    return _cuda.from_cuda_array_interface(cai)
+    from numba import cuda
+    return cuda.from_cuda_array_interface(cai, owner=None, sync=False)
