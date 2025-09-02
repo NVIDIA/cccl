aaf932b79c51d007bf572b4f2b3dbcc44e7dc3a8
Vivek Raj <rajviv79@gmail.com>
2025-09-02 19:37:09 -0400

    Implement multi_device_async_resource with per-device memory pools and dispatching

--- cudax/include/cuda/experimental/memory_resource.cuh
+++ cudax/include/cuda/experimental/memory_resource.cuh
@@ -27,10 +27,13 @@
 #include <cuda/experimental/__memory_resource/device_memory_resource.cuh>
 #include <cuda/experimental/__memory_resource/legacy_managed_memory_resource.cuh>
 #include <cuda/experimental/__memory_resource/legacy_pinned_memory_resource.cuh>
+#include <cuda/experimental/__memory_resource/multi_device_async_buffer.cuh> 
+#include <cuda/experimental/__memory_resource/multi_device_async_resource.cuh>
 #include <cuda/experimental/__memory_resource/pinned_memory_pool.cuh>
 #include <cuda/experimental/__memory_resource/pinned_memory_resource.cuh>
 #include <cuda/experimental/__memory_resource/properties.cuh>
 #include <cuda/experimental/__memory_resource/resource.cuh>
 #include <cuda/experimental/__memory_resource/shared_resource.cuh>
+
 
 #endif // __CUDAX_MEMORY_RESOURCE___
