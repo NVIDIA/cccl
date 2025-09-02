a56b9a52b922837f7411a84deaa0f8f7ea742fb8
David Bayer <48736217+davebayer@users.noreply.github.com>
2025-09-02 18:23:50 +0200

    Deprecate `<cuda/discard_memory>` (#5672)

--- libcudacxx/include/cuda/__memory/discard_memory.h
+++ libcudacxx/include/cuda/__memory/discard_memory.h
@@ -22,35 +22,38 @@
 #endif // no system header
 
 #include <cuda/__memory/address_space.h>
+#include <cuda/__memory/align_down.h>
+#include <cuda/__memory/align_up.h>
 #include <cuda/std/cstddef>
-#include <cuda/std/cstdint>
 
 #include <cuda/std/__cccl/prologue.h>
 
 _CCCL_BEGIN_NAMESPACE_CUDA
 
-_CCCL_API inline void discard_memory([[maybe_unused]] volatile void* __ptr, [[maybe_unused]] size_t __nbytes) noexcept
-{
-  // The discard PTX instruction is only available with PTX ISA 7.4 and later
+_CCCL_API inline void
+discard_memory([[maybe_unused]] volatile void* __ptr, [[maybe_unused]] ::cuda::std::size_t __nbytes) noexcept {
+// The discard PTX instruction is only available with PTX ISA 7.4 and later
 #if __cccl_ptx_isa >= 740ULL
-  // clang-format off
-  NV_IF_TARGET(
-    NV_PROVIDES_SM_80,
-    (_CCCL_ASSERT(__ptr != nullptr, "null pointer passed to discard_memory");
-    if (!::cuda::device::is_address_from(__ptr, ::cuda::device::address_space::global)) {
-      return;
-    }
-    constexpr size_t __line_size = 128;
-    auto __p                     = reinterpret_cast<char*>(const_cast<void*>(__ptr));
-    auto __end_p                 = __p + __nbytes;
-    // Trim the first block and last block if they're not 128 bytes aligned
-    const auto __misalignment  = reinterpret_cast<uintptr_t>(__p) % __line_size;
-    const auto __start_aligned = __misalignment == 0 ? __p : __p + (__line_size - __misalignment);
-    const auto __end_aligned   = __end_p - (reinterpret_cast<uintptr_t>(__end_p) % __line_size);
-    for (auto __i = __start_aligned; __i < __end_aligned; __i += __line_size) {
-      asm volatile("discard.global.L2 [%0], 128;" ::"l"(__i) :);
-    }))
-  // clang-format off
+  NV_IF_TARGET(NV_PROVIDES_SM_80, ({
+                 _CCCL_ASSERT(__ptr != nullptr, "null pointer passed to discard_memory");
+                 if (!::cuda::device::is_address_from(__ptr, ::cuda::device::address_space::global))
+                 {
+                   return;
+                 }
+
+                 constexpr ::cuda::std::size_t __line_size = 128;
+
+                 // Trim the first block and last block if they're not 128 bytes aligned
+                 const auto __p             = static_cast<char*>(const_cast<void*>(__ptr));
+                 const auto __end_p         = __p + __nbytes;
+                 const auto __start_aligned = ::cuda::align_up(__p, __line_size);
+                 const auto __end_aligned   = ::cuda::align_down(__end_p, __line_size);
+
+                 for (auto __i = __start_aligned; __i < __end_aligned; __i += __line_size)
+                 {
+                   asm volatile("discard.global.L2 [%0], 128;" ::"l"(__i) :);
+                 }
+               }))
 #endif // __cccl_ptx_isa >= 740ULL
 }
 
