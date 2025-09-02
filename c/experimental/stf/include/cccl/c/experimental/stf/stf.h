c87cdaa6414c1d7fb27e692553c79f8d51ffe4e4
Cédric Augonnet <caugonnet@nvidia.com>
2025-09-02 15:50:29 +0200

    Add missing headers

--- c/experimental/stf/include/cccl/c/experimental/stf/stf.h
+++ c/experimental/stf/include/cccl/c/experimental/stf/stf.h
@@ -1,3 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#ifndef CCCL_C_EXPERIMENTAL
+#  error "C exposure is experimental and subject to change. Define CCCL_C_EXPERIMENTAL to acknowledge this notice."
+#endif // !CCCL_C_EXPERIMENTAL
+
 #include <assert.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
--- c/experimental/stf/src/stf.cu
+++ c/experimental/stf/src/stf.cu
@@ -1,3 +1,13 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
 #include <cccl/c/experimental/stf/stf.h>
 // #include <cccl/c/parallel/include/cccl/c/extern_c.h>
 #include <cuda/experimental/stf.cuh>
