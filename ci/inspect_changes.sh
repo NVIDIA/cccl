c4c99f01474ac6e46466438257d1d5c29cfa27c9
Cédric Augonnet <caugonnet@nvidia.com>
2025-09-02 16:54:46 +0200

    Add a project to ci/inspect_changes.sh

--- ci/inspect_changes.sh
+++ ci/inspect_changes.sh
@@ -63,6 +63,7 @@ declare -A dependencies=(
   [stdpar]="cccl libcudacxx cub thrust"
   [python]="cccl libcudacxx cub cccl_c_parallel"
   [cccl_c_parallel]="cccl libcudacxx cub thrust c2h"
+  [cccl_c_stf]="cccl libcudacxx cudax c2h"
   [c2h]="cccl libcudacxx cub thrust"
   [nvbench_helper]="cccl libcudacxx cub thrust"
 )
@@ -77,6 +78,7 @@ declare -A project_names=(
   [stdpar]="stdpar"
   [python]="python"
   [cccl_c_parallel]="CCCL C Parallel Library"
+  [cccl_c_stf]="CCCL C CUDASTF Library"
   [c2h]="Catch2Helper"
   [nvbench_helper]="NVBench Helper"
 )
