8dbdbe08c365d63ee82b8a4c96e0cd4ede168ad4
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-09-09 20:40:16 +0200

    Cleanup

--- thrust/thrust/detail/execution_policy.h
+++ thrust/thrust/detail/execution_policy.h
@@ -1,19 +1,5 @@
-/*
- *  Copyright 2008-2013 NVIDIA Corporation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
+// SPDX-FileCopyrightText: Copyright (c) 2008-2013, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #pragma once
 
 #include <thrust/detail/config.h>
@@ -30,12 +16,10 @@ THRUST_NAMESPACE_BEGIN
 
 namespace detail
 {
-
 struct execution_policy_marker
 {};
 
-// execution_policy_base serves as a guard against
-// infinite recursion in thrust entry points:
+// execution_policy_base serves as a guard against infinite recursion in thrust entry points:
 //
 // template<typename DerivedPolicy>
 // void foo(const thrust::detail::execution_policy_base<DerivedPolicy> &s)
@@ -70,7 +54,6 @@ constexpr _CCCL_HOST_DEVICE const DerivedPolicy& derived_cast(const execution_po
 {
   return static_cast<const DerivedPolicy&>(x);
 }
-
 } // namespace detail
 
 //! \addtogroup execution_policies
