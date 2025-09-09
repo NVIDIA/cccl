13cf487ad8774876adcd61bd51757103887bd345
Eric Niebler <eniebler@nvidia.com>
2025-09-09 00:59:35 +0000

    work around issue with msvc's old broken preprocessor

--- libcudacxx/include/cuda/std/__concepts/concept_macros.h
+++ libcudacxx/include/cuda/std/__concepts/concept_macros.h
@@ -175,11 +175,13 @@ namespace __cccl_unqualified_cuda_std = ::cuda::std; // NOLINT(misc-unused-alias
 #define _CCCL_CONCEPT_EAT_SAME_AS_(...) _CCCL_PP_CAT(_CCCL_CONCEPT_EAT_SAME_AS_, __VA_ARGS__)
 #define _CCCL_CONCEPT_EAT_SAME_AS__Same_as(...)
 
-// Converts "_Same_as(TYPE) EXPR..." to "TYPE"
+// Converts "_Same_as(TYPE) EXPR..." to "TYPE" (The rediculous concatenation of _CCCL_PP_
+// with EXPAND(__VA_ARGS__) is the only way to get MSVC's broken preprocessor to do macro
+// expansion here.)
 #define _CCCL_CONCEPT_GET_TYPE_FROM_SAME_AS_(...) \
-  _CCCL_PP_EXPAND(                                \
-    _CCCL_PP_EXPAND2 _CCCL_PP_EVAL(_CCCL_PP_FIRST, _CCCL_PP_CAT(_CCCL_CONCEPT_GET_TYPE_FROM_SAME_AS_, __VA_ARGS__)))
-#define _CCCL_CONCEPT_GET_TYPE_FROM_SAME_AS__Same_as(...) (__VA_ARGS__),
+  _CCCL_PP_CAT(_CCCL_PP_,                         \
+               _CCCL_PP_EVAL(_CCCL_PP_FIRST, _CCCL_PP_CAT(_CCCL_CONCEPT_GET_TYPE_FROM_SAME_AS_, __VA_ARGS__)))
+#define _CCCL_CONCEPT_GET_TYPE_FROM_SAME_AS__Same_as(...) EXPAND(__VA_ARGS__),
 
 // Here are the implementations of the internal macros, first for when concepts
 // are available, and then for when they're not.
