1ed55d1b15871f2a5d8a75a232a8e6ddaf91040c
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-09-15 23:43:19 +0200

    Also test traversal

--- thrust/testing/catch2_test_iterator_traits.cu
+++ thrust/testing/catch2_test_iterator_traits.cu
@@ -99,6 +99,10 @@ struct thrust_make_zip_iterator
   }
 };
 
+template <typename Iterator>
+inline constexpr bool has_random_access_traversal =
+  cuda::std::is_same_v<thrust::iterator_traversal_t<Iterator>, thrust::random_access_traversal_tag>;
+
 using cuda::std::__type_cartesian_product;
 using cuda::std::__type_list;
 
@@ -106,7 +110,7 @@ using make_counting_t  = __type_list<cuda_make_counting_iterator, thrust_make_co
 using make_transform_t = __type_list<cuda_make_transform_iterator, thrust_make_transform_iterator>;
 using make_zip_t       = __type_list<cuda_make_zip_iterator, thrust_make_zip_iterator>;
 using make_it_t        = __type_cartesian_product<make_counting_t, make_transform_t, make_zip_t>;
-TEMPLATE_LIST_TEST_CASE("iterator system propagation - any system", "[iterators]", make_it_t)
+TEMPLATE_LIST_TEST_CASE("iterator system and traversal propagation - any system", "[iterators]", make_it_t)
 {
   using namespace cuda::std;
   __type_at_c<0, TestType> make_counting_iterator;
@@ -115,12 +119,15 @@ TEMPLATE_LIST_TEST_CASE("iterator system propagation - any system", "[iterators]
 
   auto counting_it = make_counting_iterator(0);
   STATIC_REQUIRE(is_same_v<thrust::iterator_system_t<decltype(counting_it)>, thrust::any_system_tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(counting_it)>);
 
   auto transform_it = make_transform_iterator(counting_it, thrust::square<>{});
   STATIC_REQUIRE(is_same_v<thrust::iterator_system_t<decltype(transform_it)>, thrust::any_system_tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(transform_it)>);
 
   [[maybe_unused]] auto zip_it = make_zip_iterator(counting_it, transform_it);
   STATIC_REQUIRE(is_same_v<thrust::iterator_system_t<decltype(zip_it)>, thrust::any_system_tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(zip_it)>);
 }
 
 auto expected_tag(thrust::device_vector<int>) -> thrust::device_system_tag;
@@ -129,7 +136,7 @@ auto expected_tag(std::vector<int>) -> thrust::host_system_tag;
 
 using vectors           = __type_list<thrust::device_vector<int>, thrust::host_vector<int>, std::vector<int>>;
 using make_vec_and_it_t = __type_cartesian_product<vectors, make_transform_t, make_zip_t>;
-TEMPLATE_LIST_TEST_CASE("iterator system propagation - vectors", "[iterators]", make_vec_and_it_t)
+TEMPLATE_LIST_TEST_CASE("iterator system and traversal propagation - vectors", "[iterators]", make_vec_and_it_t)
 {
   using namespace cuda::std;
   __type_at_c<0, TestType> vec{};
@@ -140,17 +147,21 @@ TEMPLATE_LIST_TEST_CASE("iterator system propagation - vectors", "[iterators]", 
 
   auto vec_it = vec.begin();
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(vec_it)>, tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(vec_it)>);
 
   using thrust::placeholders::_1;
   auto transform_it = make_transform_iterator(vec_it, _1 + 1);
   STATIC_REQUIRE(is_same_v<thrust::iterator_system_t<decltype(transform_it)>, tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(transform_it)>);
 
   [[maybe_unused]] auto zip_it = make_zip_iterator(vec_it, transform_it);
   STATIC_REQUIRE(is_same_v<thrust::iterator_system_t<decltype(zip_it)>, tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(zip_it)>);
 }
 
 using make_vec_count_zip_it_t = __type_cartesian_product<vectors, make_counting_t, make_zip_t>;
-TEMPLATE_LIST_TEST_CASE("iterator system propagation - vectors and any system", "[iterators]", make_vec_count_zip_it_t)
+TEMPLATE_LIST_TEST_CASE(
+  "iterator system and traversal propagation - vectors and any system", "[iterators]", make_vec_count_zip_it_t)
 {
   using namespace cuda::std;
   __type_at_c<0, TestType> vec{};
@@ -161,10 +172,13 @@ TEMPLATE_LIST_TEST_CASE("iterator system propagation - vectors and any system", 
 
   auto vec_it = vec.begin();
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(vec_it)>, vec_tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(vec_it)>);
 
   auto counting_it = make_counting_iterator(0);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(counting_it)>, thrust::any_system_tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(counting_it)>);
 
   [[maybe_unused]] auto zip_it = make_zip_iterator(vec_it, counting_it);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(zip_it)>, vec_tag>);
+  STATIC_REQUIRE(has_random_access_traversal<decltype(zip_it)>);
 }
