770f28b7f837807f60a4fb4d2f5939da5da83585
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-09-15 11:59:39 +0200

    Fix warnings

--- thrust/testing/catch2_test_iterator_traits.cu
+++ thrust/testing/catch2_test_iterator_traits.cu
@@ -8,38 +8,38 @@
 
 TEST_CASE("iterator system propagation - any system", "[iterators]")
 {
-  auto counting_it = thrust::make_counting_iterator(0);
+  [[maybe_unused]] auto counting_it = thrust::make_counting_iterator(0);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(counting_it)>, thrust::any_system_tag>);
 
-  auto transform_it = thrust::make_transform_iterator(counting_it, thrust::square<>{});
+  [[maybe_unused]] auto transform_it = thrust::make_transform_iterator(counting_it, thrust::square<>{});
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(transform_it)>, thrust::any_system_tag>);
 
-  auto zip_it = thrust::make_zip_iterator(counting_it, transform_it);
+  [[maybe_unused]] auto zip_it = thrust::make_zip_iterator(counting_it, transform_it);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(zip_it)>, thrust::any_system_tag>);
 }
 
 TEST_CASE("iterator system propagation - device system", "[iterators]")
 {
-  auto d_vec    = thrust::device_vector<int>{};
-  auto d_vec_it = d_vec.begin();
+  [[maybe_unused]] auto d_vec    = thrust::device_vector<int>{};
+  [[maybe_unused]] auto d_vec_it = d_vec.begin();
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(d_vec_it)>, thrust::device_system_tag>);
 
-  auto transform_it = thrust::make_transform_iterator(d_vec_it, thrust::square<>{});
+  [[maybe_unused]] auto transform_it = thrust::make_transform_iterator(d_vec_it, thrust::square<>{});
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(transform_it)>, thrust::device_system_tag>);
 
-  auto zip_it = thrust::make_zip_iterator(d_vec_it, transform_it);
+  [[maybe_unused]] auto zip_it = thrust::make_zip_iterator(d_vec_it, transform_it);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(zip_it)>, thrust::device_system_tag>);
 }
 
 TEST_CASE("iterator system propagation - any and device system", "[iterators]")
 {
-  auto d_vec    = thrust::device_vector<int>{};
-  auto d_vec_it = d_vec.begin();
+  [[maybe_unused]] auto d_vec    = thrust::device_vector<int>{};
+  [[maybe_unused]] auto d_vec_it = d_vec.begin();
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(d_vec_it)>, thrust::device_system_tag>);
 
-  auto counting_it = thrust::make_counting_iterator(0);
+  [[maybe_unused]] auto counting_it = thrust::make_counting_iterator(0);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(counting_it)>, thrust::any_system_tag>);
 
-  auto zip_it = thrust::make_zip_iterator(d_vec_it, counting_it);
+  [[maybe_unused]] auto zip_it = thrust::make_zip_iterator(d_vec_it, counting_it);
   STATIC_REQUIRE(cuda::std::is_same_v<thrust::iterator_system_t<decltype(zip_it)>, thrust::device_system_tag>);
 }
