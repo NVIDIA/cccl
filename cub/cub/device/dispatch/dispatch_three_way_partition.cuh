dfd3ec92538c9e38be0201d464f2436ae454266b
Nader Al Awar <naderalawar@gmail.com>
2025-09-17 23:36:18 +0000

    Remove OffsetSize from kernel source

--- cub/cub/device/dispatch/dispatch_three_way_partition.cuh
+++ cub/cub/device/dispatch/dispatch_three_way_partition.cuh
@@ -91,11 +91,6 @@ struct DeviceThreeWayPartitionKernelSource
       SelectSecondPartOp,
       per_partition_offset_t,
       streaming_context_t>);
-
-  CUB_RUNTIME_FUNCTION static constexpr size_t OffsetSize()
-  {
-    return sizeof(OffsetT);
-  }
 };
 } // namespace detail::three_way_partition
 
@@ -191,7 +186,7 @@ struct DispatchThreeWayPartitionIf
     constexpr ::cuda::std::size_t num_counters_per_pass  = 3;
     constexpr ::cuda::std::size_t num_streaming_counters = 2 * num_counters_per_pass;
     ::cuda::std::size_t streaming_selection_storage_bytes =
-      (num_partitions > 1) ? num_streaming_counters * kernel_source.OffsetSize() : ::cuda::std::size_t{0};
+      (num_partitions > 1) ? num_streaming_counters * sizeof(OffsetT) : ::cuda::std::size_t{0};
 
     // Specify temporary storage allocation requirements
     size_t allocation_sizes[2] = {0ULL, streaming_selection_storage_bytes};
