d785c3b15d40b0b874b29ed4e9ec59097009fc33
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-09-18 23:00:57 +0200

    Comments

--- cub/cub/agent/agent_merge.cuh
+++ cub/cub/agent/agent_merge.cuh
@@ -87,8 +87,8 @@ struct agent_t
     typename block_store_keys::TempStorage store_keys;
     typename block_store_items::TempStorage store_items;
 
-    // TODO(bgruber): We could change SerialMerge to avoid reading one item out of bounds and drop the + 1 here. But
-    // that would introduce more branches. Needs benchmarking
+    // We could change SerialMerge to avoid reading one item out of bounds and drop the + 1 here. But that would
+    // introduce more branches (about 10% slower on 2^16 problem sizes on RTX 5090 in a first attempt)
     key_type keys_shared[items_per_tile + 1];
     item_type items_shared[items_per_tile + 1];
   };
