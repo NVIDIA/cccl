e1a3ebd6478039493a3322e01eed09e71378ffac
Michael Schellenberger Costa <miscco@nvidia.com>
2025-09-19 14:25:54 +0200

    Move `chrono::year` into its own file

--- libcudacxx/test/libcudacxx/std/utilities/time/time.cal/time.cal.year/time.cal.year.nonmembers/comparisons.pass.cpp
+++ libcudacxx/test/libcudacxx/std/utilities/time/time.cal/time.cal.year/time.cal.year.nonmembers/comparisons.pass.cpp
@@ -3,6 +3,7 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
@@ -10,11 +11,7 @@
 // class year;
 
 // constexpr bool operator==(const year& x, const year& y) noexcept;
-// constexpr bool operator!=(const year& x, const year& y) noexcept;
-// constexpr bool operator< (const year& x, const year& y) noexcept;
-// constexpr bool operator> (const year& x, const year& y) noexcept;
-// constexpr bool operator<=(const year& x, const year& y) noexcept;
-// constexpr bool operator>=(const year& x, const year& y) noexcept;
+// constexpr strong_ordering operator<=>(const year& x, const year& y) noexcept;
 
 #include <cuda/std/cassert>
 #include <cuda/std/chrono>
@@ -23,20 +20,18 @@
 #include "test_comparisons.h"
 #include "test_macros.h"
 
-int main(int, char**)
+__host__ __device__ constexpr bool test()
 {
   using year = cuda::std::chrono::year;
 
-  AssertComparisonsAreNoexcept<year>();
-  AssertComparisonsReturnBool<year>();
+  // Validate valid value. The range [-32768, 32767] is guaranteed to be allowed.
+  assert(testComparisonsValues<year>(-32768, -32768));
+  assert(testComparisonsValues<year>(-32768, -32767));
+  // Largest positive
+  assert(testComparisonsValues<year>(32767, 32766));
+  assert(testComparisonsValues<year>(32767, 32767));
 
-  static_assert(testComparisonsValues<year>(0, 0), "");
-  static_assert(testComparisonsValues<year>(0, 1), "");
-
-  //  Some 'ok' values as well
-  static_assert(testComparisonsValues<year>(5, 5), "");
-  static_assert(testComparisonsValues<year>(5, 10), "");
-
+  // Validate some valid values.
   for (int i = 1; i < 10; ++i)
   {
     for (int j = 1; j < 10; ++j)
@@ -44,6 +39,22 @@ int main(int, char**)
       assert(testComparisonsValues<year>(i, j));
     }
   }
+
+  return true;
+}
+
+int main(int, char**)
+{
+  using year = cuda::std::chrono::year;
+  AssertComparisonsAreNoexcept<year>();
+
+#if _LIBCUDACXX_HAS_SPACESHIP_OPERATOR()
+  AssertOrderAreNoexcept<year>();
+  AssertOrderReturn<cuda::std::strong_ordering, year>();
+#endif // _LIBCUDACXX_HAS_SPACESHIP_OPERATOR()
+
+  test();
+  static_assert(test());
 
   return 0;
 }
