51b20c7762a5d1c41b8d5a235a4476d91d906401
fbusato <fbusato@nvidia.com>
2025-09-22 15:22:00 -0700

    disable new memcpy for gcc <= 9

--- libcudacxx/include/cuda/std/__cstring/memcpy.h
+++ libcudacxx/include/cuda/std/__cstring/memcpy.h
@@ -31,8 +31,11 @@
 
 _CCCL_BEGIN_NAMESPACE_CUDA_STD
 
-using ::size_t;
 using ::memcpy;
+using ::size_t;
+
+// old compilers stll trigger the name conflict
+#if !_CCCL_COMPILER(GCC, <=, 9)
 
 // The template parameter is used to avoid name ambiguity when external code calls 'memcpy' without namespace
 // qualification. Functions with template arguments have lower precedence than non-template functions for
@@ -45,6 +48,8 @@ _CCCL_API inline void* memcpy(void* __dest, const void* __src, size_t __count) n
   _CCCL_ASSERT(::cuda::__are_ptrs_overlapping(__src, __dest, __count), "memcpy: source and destination overlap");
   return ::memcpy(__dest, __src, __count);
 }
+
+#endif // !_CCCL_COMPILER(GCC, <=, 9)
 
 _CCCL_END_NAMESPACE_CUDA_STD
 
