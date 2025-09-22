a09426f898100f71d64c543b2c569bcbd29e015b
fbusato <fbusato@nvidia.com>
2025-09-22 10:56:58 -0700

    fix example formatting

--- cub/test/catch2_test_device_for_each_in_layout_api.cu
+++ cub/test/catch2_test_device_for_each_in_layout_api.cu
@@ -44,9 +44,10 @@ C2H_TEST("Device ForEachInLayout", "[ForEachInLayout][device]")
   thrust::host_vector<data_t>      h_output(cub::detail::size(extents));
   auto                             d_output_raw = cuda::std::span<data_t>{thrust::raw_pointer_cast(d_output.data()),
                                                                           3 * 2 * 2};
-  thrust::host_vector<data_t>      expected = {{0, 0, 0}, {1, 0, 0}, {2, 0, 0}, {0, 1, 0},
-                                               {1, 1, 0}, {2, 1, 0}, {0, 0, 1}, {1, 0, 1},
-                                               {2, 0, 1}, {0, 1, 1}, {1, 1, 1}, {2, 1, 1}};
+  thrust::host_vector<data_t>      expected = {{0, 0, 0}, {1, 0, 0}, {2, 0, 0},
+                                               {0, 1, 0}, {1, 1, 0}, {2, 1, 0},
+                                               {0, 0, 1}, {1, 0, 1}, {2, 0, 1},
+                                               {0, 1, 1}, {1, 1, 1}, {2, 1, 1}};
 
   auto status = cub::DeviceFor::ForEachInLayout(cuda::std::layout_left{}, extents,
                                                 [=] __device__ (int idx, int x, int y, int z) {
