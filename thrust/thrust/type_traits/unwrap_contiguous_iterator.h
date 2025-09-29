75a148189966c27596cd61e27e27348d157a98ea
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-09-29 21:11:02 +0200

    Refactor `thrust::[try_]unwrap_contiguous_iterator[_t]`

--- thrust/thrust/type_traits/unwrap_contiguous_iterator.h
+++ thrust/thrust/type_traits/unwrap_contiguous_iterator.h
@@ -25,72 +25,37 @@
 
 THRUST_NAMESPACE_BEGIN
 
-namespace detail
-{
-// Type traits for contiguous iterators:
-template <typename Iterator>
-struct contiguous_iterator_traits
-{
-  static_assert(thrust::is_contiguous_iterator_v<Iterator>,
-                "contiguous_iterator_traits requires a contiguous iterator.");
-
-  using raw_pointer =
-    typename thrust::detail::pointer_traits<decltype(&*::cuda::std::declval<Iterator>())>::raw_pointer;
-};
-} // namespace detail
-
-//! Converts a contiguous iterator type to its underlying raw pointer type.
-template <typename ContiguousIterator>
-using unwrap_contiguous_iterator_t = typename detail::contiguous_iterator_traits<ContiguousIterator>::raw_pointer;
-
 //! Converts a contiguous iterator to its underlying raw pointer.
 template <typename ContiguousIterator>
 _CCCL_HOST_DEVICE auto unwrap_contiguous_iterator(ContiguousIterator it)
-  -> unwrap_contiguous_iterator_t<ContiguousIterator>
 {
   static_assert(thrust::is_contiguous_iterator_v<ContiguousIterator>,
                 "unwrap_contiguous_iterator called with non-contiguous iterator.");
   return thrust::raw_pointer_cast(&*it);
 }
 
-namespace detail
-{
-// Implementation for non-contiguous iterators -- passthrough.
-template <typename Iterator, bool IsContiguous = thrust::is_contiguous_iterator_v<Iterator>>
-struct try_unwrap_contiguous_iterator_impl
-{
-  using type = Iterator;
-
-  static _CCCL_HOST_DEVICE type get(Iterator it)
-  {
-    return it;
-  }
-};
-
-// Implementation for contiguous iterators -- unwraps to raw pointer.
-template <typename Iterator>
-struct try_unwrap_contiguous_iterator_impl<Iterator, true /*is_contiguous*/>
-{
-  using type = unwrap_contiguous_iterator_t<Iterator>;
-
-  static _CCCL_HOST_DEVICE type get(Iterator it)
-  {
-    return unwrap_contiguous_iterator(it);
-  }
-};
-} // namespace detail
-
-//! Takes an iterator type and, if it is contiguous, yields the raw pointer type it represents. Otherwise returns the
-//! iterator type unmodified.
-template <typename Iterator>
-using try_unwrap_contiguous_iterator_t = typename detail::try_unwrap_contiguous_iterator_impl<Iterator>::type;
+//! Converts a contiguous iterator type to its underlying raw pointer type.
+template <typename ContiguousIterator>
+using unwrap_contiguous_iterator_t = decltype(unwrap_contiguous_iterator(::cuda::std::declval<ContiguousIterator>()));
 
 //! Takes an iterator and, if it is contiguous, unwraps it to the raw pointer it represents. Otherwise returns the
 //! iterator unmodified.
 template <typename Iterator>
-_CCCL_HOST_DEVICE auto try_unwrap_contiguous_iterator(Iterator it) -> try_unwrap_contiguous_iterator_t<Iterator>
+_CCCL_HOST_DEVICE auto try_unwrap_contiguous_iterator(Iterator it)
 {
-  return detail::try_unwrap_contiguous_iterator_impl<Iterator>::get(it);
+  if constexpr (thrust::is_contiguous_iterator_v<Iterator>)
+  {
+    return unwrap_contiguous_iterator(it);
+  }
+  else
+  {
+    return it;
+  }
 }
+
+//! Takes an iterator type and, if it is contiguous, yields the raw pointer type it represents. Otherwise returns the
+//! iterator type unmodified.
+template <typename Iterator>
+using try_unwrap_contiguous_iterator_t = decltype(try_unwrap_contiguous_iterator(::cuda::std::declval<Iterator>()));
 
 THRUST_NAMESPACE_END
