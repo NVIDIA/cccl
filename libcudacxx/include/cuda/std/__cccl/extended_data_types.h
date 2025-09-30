ba3323cc899556114c09bc6b0e56c297b1372733
fbusato <fbusato@nvidia.com>
2025-09-30 15:35:59 -0700

    fix conflicts

--- libcudacxx/include/cuda/std/__cccl/extended_data_types.h
+++ libcudacxx/include/cuda/std/__cccl/extended_data_types.h
@@ -29,15 +29,14 @@
 #include <cuda/std/__cccl/os.h>
 #include <cuda/std/__cccl/preprocessor.h>
 
-#define _CCCL_HAS_INT128()                  0
-#define _CCCL_HAS_LONG_DOUBLE()             0
-#define _CCCL_HAS_NVFP4()                   0
-#define _CCCL_HAS_NVFP6()                   0
-#define _CCCL_HAS_NVFP8()                   0
-#define _CCCL_HAS_NVFP16()                  0
-#define _CCCL_HAS_NVBF16()                  0
-#define _CCCL_HAS_FLOAT128()                0
-#define _CCCL_HAS_FLOAT128_CUDA_FUNCTIONS() 0
+#define _CCCL_HAS_INT128()      0
+#define _CCCL_HAS_LONG_DOUBLE() 0
+#define _CCCL_HAS_NVFP4()       0
+#define _CCCL_HAS_NVFP6()       0
+#define _CCCL_HAS_NVFP8()       0
+#define _CCCL_HAS_NVFP16()      0
+#define _CCCL_HAS_NVBF16()      0
+#define _CCCL_HAS_FLOAT128()    0
 
 #if !defined(CCCL_DISABLE_INT128_SUPPORT) && _CCCL_OS(LINUX) \
   && ((_CCCL_COMPILER(NVRTC) && defined(__CUDACC_RTC_INT128__)) || defined(__SIZEOF_INT128__))
@@ -56,27 +55,54 @@
                       && !defined(CCCL_DISABLE_FP16_SUPPORT)
 #  undef _CCCL_HAS_NVFP16
 #  define _CCCL_HAS_NVFP16() 1
+struct __half;
+struct __half2;
 #endif
 
 #if _CCCL_HAS_INCLUDE(<cuda_bf16.h>) && _CCCL_HAS_NVFP16() && !defined(CCCL_DISABLE_BF16_SUPPORT)
 #  undef _CCCL_HAS_NVBF16
 #  define _CCCL_HAS_NVBF16() 1
+struct __nv_bfloat16;
+struct __nv_bfloat162;
 #endif
 
 #if _CCCL_HAS_INCLUDE(<cuda_fp8.h>) && _CCCL_HAS_NVFP16() && _CCCL_HAS_NVBF16() && !defined(CCCL_DISABLE_NVFP8_SUPPORT)
 #  undef _CCCL_HAS_NVFP8
 #  define _CCCL_HAS_NVFP8() 1
+struct __nv_fp8_e5m2;
+struct __nv_fp8x2_e5m2;
+struct __nv_fp8x4_e5m2;
+
+struct __nv_fp8_e4m3;
+struct __nv_fp8x2_e4m3;
+struct __nv_fp8x4_e4m3;
+
+#  if _CCCL_CTK_AT_LEAST(12, 8)
+struct __nv_fp8_e8m0;
+struct __nv_fp8x2_e8m0;
+struct __nv_fp8x4_e8m0;
+#  endif // _CCCL_CTK_AT_LEAST(12, 8)
 #endif
 
 #if _CCCL_HAS_INCLUDE(<cuda_fp6.h>) && _CCCL_HAS_NVFP8() && !_CCCL_CUDA_COMPILER(NVHPC) \
                       && !defined(CCCL_DISABLE_NVFP6_SUPPORT)
 #  undef _CCCL_HAS_NVFP6
 #  define _CCCL_HAS_NVFP6() 1
+struct __nv_fp6_e3m2;
+struct __nv_fp6x2_e3m2;
+struct __nv_fp6x4_e3m2;
+
+struct __nv_fp6_e2m3;
+struct __nv_fp6x2_e2m3;
+struct __nv_fp6x4_e2m3;
 #endif
 
 #if _CCCL_HAS_INCLUDE(<cuda_fp4.h>) && _CCCL_HAS_NVFP6() && !defined(CCCL_DISABLE_NVFP4_SUPPORT)
 #  undef _CCCL_HAS_NVFP4
 #  define _CCCL_HAS_NVFP4() 1
+struct __nv_fp4_e2m1;
+struct __nv_fp4x2_e2m1;
+struct __nv_fp4x4_e2m1;
 #endif
 
 #define _CCCL_HAS_NVFP4_E2M1() _CCCL_HAS_NVFP4()
@@ -117,11 +143,6 @@
 #  undef _CCCL_HAS_FLOAT128
 #  define _CCCL_HAS_FLOAT128() 0
 #endif // _CCCL_HAS_FLOAT128()
-
-#if _CCCL_HAS_FLOAT128() && _CCCL_HAS_INCLUDE(<crt/device_fp128_functions.h>)
-#  undef _CCCL_HAS_FLOAT128_CUDA_FUNCTIONS
-#  define _CCCL_HAS_FLOAT128_CUDA_FUNCTIONS() 1
-#endif // _CCCL_HAS_FLOAT128() && _CCCL_HAS_INCLUDE(<crt/device_fp128_functions.h>)
 
 /***********************************************************************************************************************
  * char8_t
