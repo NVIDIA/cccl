6b31ee561478a4994c8b14c27136b631592ff513
Michael Schellenberger Costa <miscco@nvidia.com>
2025-10-01 19:02:21 +0200

    Simplify tests

--- libcudacxx/test/libcudacxx/libcxx/iterators/compressed_movable_box/assign.copy.pass.cpp
+++ libcudacxx/test/libcudacxx/libcxx/iterators/compressed_movable_box/assign.copy.pass.cpp
@@ -22,173 +22,85 @@
 template <class... Ts>
 using box = cuda::std::__compressed_movable_box<Ts...>;
 
-template <class... Ts>
+template <class T>
 __host__ __device__ TEST_CONSTEXPR_CXX20 void test(const int expected)
 {
-  const box<Ts...> input{1337};
-  box<Ts...> b{42};
-  b = input;
-  assert(b.template __get<0>() == expected);
   constexpr bool is_noexcept =
-    (cuda::std::copyable<Ts> && ...)
-      ? ((cuda::std::is_nothrow_copy_constructible_v<Ts> && cuda::std::is_nothrow_copy_assignable_v<Ts>) && ...)
-      : (cuda::std::is_nothrow_copy_constructible_v<Ts> && ...);
-  static_assert(noexcept(b = input) == is_noexcept);
+    cuda::std::copyable<T>
+      ? (cuda::std::is_nothrow_copy_constructible_v<T> && cuda::std::is_nothrow_copy_assignable_v<T>)
+      : cuda::std::is_nothrow_copy_constructible_v<T>;
+  { // single item
+    const box<T> input{1337};
+    box<T> b{42};
+    b = input;
+    assert(b.template __get<0>() == expected);
+    static_assert(cuda::std::is_nothrow_copy_assignable_v<box<T>> == is_noexcept);
+  }
+  { // two items
+    const box<T, int> input{1337};
+    box<T, int> b{42};
+    b = input;
+    assert(b.template __get<0>() == expected);
+    static_assert(cuda::std::is_nothrow_copy_assignable_v<box<T, int>> == is_noexcept);
+  }
+  { // three items
+    const box<T, int, int> input{1337};
+    box<T, int, int> b{42};
+    b = input;
+    assert(b.template __get<0>() == expected);
+    static_assert(cuda::std::is_nothrow_copy_assignable_v<box<T, int, int>> == is_noexcept);
+  }
 }
 
 __host__ __device__ TEST_CONSTEXPR_CXX20 bool test()
 {
-  { // single element
-    { // trivial empty type
-      const box<TrivialEmpty> input{};
-      box<TrivialEmpty> b{};
-      b = input;
-      assert(b.__get<0>() == 42);
-      static_assert(noexcept(b = input));
-    }
-
-    { // trivial nonempty type
-      test<int>(1337);
-    }
-
-    { // non-trivial empty type
-      test<NotTriviallyCopyAssignableEmpty<13>>(13);
-    }
-
-    { // non-trivial nonempty type
-      test<NotTriviallyCopyAssignable<42>>(1337);
-    }
-
-    { // non-trivial empty type, not noexcept
-      test<NotTriviallyCopyAssignableEmpty<MayThrow>>(MayThrow);
-    }
-
-    { // non-trivial nonempty type, not noexcept
-      test<NotTriviallyCopyAssignable<MayThrow>>(1337);
-    }
-
-    { // nonempty not copy assignable
-      test<NotCopyAssignable<42>>(1337);
-    }
-
-    { // not copy assignable,
-      test<NotCopyAssignableEmpty<13>>(13);
-    }
-
-    { // nonempty not copy assignable
-      test<NotCopyAssignable<MayThrow>>(1337);
-    }
-
-    { // empty not copy assignable,
-      test<NotCopyAssignableEmpty<MayThrow>>(MayThrow);
-    }
-
-    { // neither copy constructible nor copy assignable
-      static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignable>>);
-      static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignableEmpty>>);
-    }
+  { // trivial empty type
+    const box<TrivialEmpty> input{};
+    box<TrivialEmpty> b{};
+    b = input;
+    assert(b.__get<0>() == 42);
+    static_assert(noexcept(b = input));
   }
 
-  { // two elements
-    { // trivial empty type
-      const box<TrivialEmpty, int> input{};
-      box<TrivialEmpty, int> b{};
-      b = input;
-      assert(b.__get<0>() == 42);
-      static_assert(noexcept(b = input));
-    }
-
-    { // trivial nonempty type
-      test<int, int>(1337);
-    }
-
-    { // non-trivial empty type
-      test<NotTriviallyCopyAssignableEmpty<13>, int>(13);
-    }
-
-    { // non-trivial nonempty type
-      test<NotTriviallyCopyAssignable<42>, int>(1337);
-    }
-
-    { // non-trivial empty type, not noexcept
-      test<NotTriviallyCopyAssignableEmpty<MayThrow>, int>(MayThrow);
-    }
-
-    { // non-trivial nonempty type, not noexcept
-      test<NotTriviallyCopyAssignable<MayThrow>, int>(1337);
-    }
-
-    { // nonempty not copy assignable
-      test<NotCopyAssignable<42>, int>(1337);
-    }
-
-    { // not copy assignable,
-      test<NotCopyAssignableEmpty<13>, int>(13);
-    }
-
-    { // nonempty not copy assignable
-      test<NotCopyAssignable<MayThrow>, int>(1337);
-    }
-
-    { // empty not copy assignable,
-      test<NotCopyAssignableEmpty<MayThrow>, int>(MayThrow);
-    }
-
-    { // neither copy constructible nor copy assignable
-      static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignable, int>>);
-      static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignableEmpty, int>>);
-    }
+  { // trivial nonempty type
+    test<int>(1337);
   }
 
-  { // three elements
-    { // trivial empty type
-      const box<TrivialEmpty, int> input{};
-      box<TrivialEmpty, int> b{};
-      b = input;
-      assert(b.__get<0>() == 42);
-      static_assert(noexcept(b = input));
-    }
+  { // non-trivial empty type
+    test<NotTriviallyCopyAssignableEmpty<13>>(13);
+  }
 
-    { // trivial nonempty type
-      test<int, int, int>(1337);
-    }
+  { // non-trivial nonempty type
+    test<NotTriviallyCopyAssignable<42>>(1337);
+  }
 
-    { // non-trivial empty type
-      test<NotTriviallyCopyAssignableEmpty<13>, int, int>(13);
-    }
+  { // non-trivial empty type, not noexcept
+    test<NotTriviallyCopyAssignableEmpty<MayThrow>>(MayThrow);
+  }
 
-    { // non-trivial nonempty type
-      test<NotTriviallyCopyAssignable<42>, int, int>(1337);
-    }
+  { // non-trivial nonempty type, not noexcept
+    test<NotTriviallyCopyAssignable<MayThrow>>(1337);
+  }
 
-    { // non-trivial empty type, not noexcept
-      test<NotTriviallyCopyAssignableEmpty<MayThrow>, int, int>(MayThrow);
-    }
+  { // nonempty not copy assignable
+    test<NotCopyAssignable<42>>(1337);
+  }
 
-    { // non-trivial nonempty type, not noexcept
-      test<NotTriviallyCopyAssignable<MayThrow>, int, int>(1337);
-    }
+  { // not copy assignable,
+    test<NotCopyAssignableEmpty<13>>(13);
+  }
 
-    { // nonempty not copy assignable
-      test<NotCopyAssignable<42>, int, int>(1337);
-    }
+  { // nonempty not copy assignable
+    test<NotCopyAssignable<MayThrow>>(1337);
+  }
 
-    { // not copy assignable,
-      test<NotCopyAssignableEmpty<13>, int, int>(13);
-    }
+  { // empty not copy assignable,
+    test<NotCopyAssignableEmpty<MayThrow>>(MayThrow);
+  }
 
-    { // nonempty not copy assignable
-      test<NotCopyAssignable<MayThrow>, int, int>(1337);
-    }
-
-    { // empty not copy assignable,
-      test<NotCopyAssignableEmpty<MayThrow>, int, int>(MayThrow);
-    }
-
-    { // neither copy constructible nor copy assignable
-      static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignable, int, int>>);
-      static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignableEmpty, int, int>>);
-    }
+  { // neither copy constructible nor copy assignable
+    static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignable>>);
+    static_assert(!cuda::std::is_copy_assignable_v<box<NotCopyConstructibleOrAssignableEmpty>>);
   }
 
   return true;
