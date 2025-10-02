812d2ceb1f294ae9aab16d22ff9c709cd029a222
fbusato <fbusato@nvidia.com>
2025-10-02 09:58:01 -0700

    improve API examples

--- cub/test/catch2_test_device_for_each_in_extents_api.cu
+++ cub/test/catch2_test_device_for_each_in_extents_api.cu
@@ -13,12 +13,16 @@
 
 #  include <thrust/detail/raw_pointer_cast.h>
 #  include <thrust/device_vector.h>
+#  include <thrust/equal.h>
 #  include <thrust/fill.h>
 #  include <thrust/host_vector.h>
 
 #  include <cuda/std/array>
 #  include <cuda/std/mdspan>
 #  include <cuda/std/span>
+
+#  include <cstdlib>
+#  include <iostream>
 
 #  include <c2h/catch2_test_helper.h>
 
@@ -42,25 +46,42 @@ C2H_TEST("Device ForEachInExtents", "[ForEachInExtents][device]")
   // example-begin for-each-in-extents-example
   using                            data_t = cuda::std::array<int, 3>;
   cuda::std::extents<int, 3, 2, 2> extents{};
-  thrust::device_vector<data_t>    d_output(cub::detail::size(extents));
-  thrust::host_vector<data_t>      h_output(cub::detail::size(extents));
+  thrust::device_vector<data_t>    d_output(cub::detail::size(extents), thrust::no_init);
+  thrust::host_vector<data_t>      h_output(cub::detail::size(extents), thrust::no_init);
   auto                             d_output_raw = cuda::std::span<data_t>{thrust::raw_pointer_cast(d_output.data()),
                                                                           3 * 2 * 2};
   thrust::host_vector<data_t>      expected = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
                                                {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1},
                                                {2, 0, 0}, {2, 0, 1}, {2, 1, 0}, {2, 1, 1}};
 
-  cub::DeviceFor::ForEachInExtents(extents, [=] __device__ (int idx, int x, int y, int z) {
+  auto status = cub::DeviceFor::ForEachInExtents(extents, [=] __device__ (int idx, int x, int y, int z) {
     d_output_raw[idx] = {x, y, z};
   });
+  if (status != cudaSuccess)
+  {
+    std::cerr << "cub::DeviceFor::ForEachInExtents failed with status: " << status << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   h_output = d_output;
-  REQUIRE(h_output == expected);
+  if (!thrust::equal(h_output.begin(), h_output.end(), expected.begin()))
+  {
+    std::cerr << "error: h_output != expected" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 
   thrust::fill(d_output.begin(), d_output.end(), data_t{});
-  cub::DeviceFor::ForEachInExtents(extents, linear_store_3D{d_output_raw});
-
+  status = cub::DeviceFor::ForEachInExtents(extents, linear_store_3D{d_output_raw});
+  if (status != cudaSuccess)
+  {
+    std::cerr << "cub::DeviceFor::ForEachInExtents failed with status: " << status << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   h_output = d_output;
-  REQUIRE(h_output == expected);
+  if (!thrust::equal(h_output.begin(), h_output.end(), expected.begin()))
+  {
+    std::cerr << "error: h_output != expected" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   // example-end for-each-in-extents-example
 }
 // clang-format on
--- cub/test/catch2_test_device_for_each_in_layout_api.cu
+++ cub/test/catch2_test_device_for_each_in_layout_api.cu
@@ -12,11 +12,15 @@
 #  include <cub/device/device_for.cuh>
 
 #  include <thrust/device_vector.h>
+#  include <thrust/equal.h>
 #  include <thrust/fill.h>
 #  include <thrust/host_vector.h>
 
 #  include <cuda/std/array>
 #  include <cuda/std/span>
+
+#  include <cstdlib>
+#  include <iostream>
 
 #  include <c2h/catch2_test_helper.h>
 
@@ -40,8 +44,8 @@ C2H_TEST("Device ForEachInLayout", "[ForEachInLayout][device]")
   // example-begin for-each-in-layout-example
   using                            data_t = cuda::std::array<int, 3>;
   cuda::std::extents<int, 3, 2, 2> extents{};
-  thrust::device_vector<data_t>    d_output(cub::detail::size(extents));
-  thrust::host_vector<data_t>      h_output(cub::detail::size(extents));
+  thrust::device_vector<data_t>    d_output(cub::detail::size(extents), thrust::no_init);
+  thrust::host_vector<data_t>      h_output(cub::detail::size(extents), thrust::no_init);
   auto                             d_output_raw = cuda::std::span<data_t>{thrust::raw_pointer_cast(d_output.data()),
                                                                           3 * 2 * 2};
   thrust::host_vector<data_t>      expected = {{0, 0, 0}, {1, 0, 0}, {2, 0, 0},
@@ -53,15 +57,31 @@ C2H_TEST("Device ForEachInLayout", "[ForEachInLayout][device]")
                                                 [=] __device__ (int idx, int x, int y, int z) {
     d_output_raw[idx] = {x, y, z};
   });
-  REQUIRE(status == cudaSuccess);
+  if (status != cudaSuccess)
+  {
+    std::cerr << "cub::DeviceFor::ForEachInLayout failed with status: " << status << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   h_output = d_output;
-  REQUIRE(h_output == expected);
+  if (!thrust::equal(h_output.begin(), h_output.end(), expected.begin()))
+  {
+    std::cerr << "error: h_output != expected" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 
   thrust::fill(d_output.begin(), d_output.end(), data_t{});
   status = cub::DeviceFor::ForEachInLayout(cuda::std::layout_left{}, extents, layout_store_3D{d_output_raw});
-  REQUIRE(status == cudaSuccess);
+  if (status != cudaSuccess)
+  {
+    std::cerr << "cub::DeviceFor::ForEachInLayout failed with status: " << status << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   h_output = d_output;
-  REQUIRE(h_output == expected);
+  if (!thrust::equal(h_output.begin(), h_output.end(), expected.begin()))
+  {
+    std::cerr << "error: h_output != expected" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   // example-end for-each-in-layout-example
 }
 // clang-format on
