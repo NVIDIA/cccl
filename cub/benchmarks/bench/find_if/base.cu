d67411e3d3fec72dca5ad97e3e739a4e9535f71c
gonidelis <ggonidelis@nvidia.com>
2025-10-08 08:51:43 -0700

    Revamp cub::FindIf

--- cub/benchmarks/bench/find_if/base.cu
+++ cub/benchmarks/bench/find_if/base.cu
@@ -1,47 +1,13 @@
-/******************************************************************************
- * Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the NVIDIA CORPORATION nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <cub/device/device_find_if.cuh>
 
 #include <thrust/count.h>
+#include <thrust/detail/internal_functional.h>
 #include <thrust/find.h>
 
 #include <nvbench_helper.cuh>
-
-template <typename T>
-struct equals
-{
-  T val;
-
-  __device__ __host__ bool operator()(T i)
-  {
-    return i == val;
-  }
-};
 
 template <typename T>
 void find_if(nvbench::state& state, nvbench::type_list<T>)
@@ -52,9 +18,10 @@ void find_if(nvbench::state& state, nvbench::type_list<T>)
   const auto common_prefix  = state.get_float64("MismatchAt");
   const auto mismatch_point = elements * common_prefix;
 
-  thrust::device_vector<T> dinput(elements, 0);
+  thrust::device_vector<T> dinput(elements, thrust::no_init);
+  thrust::fill(dinput.begin(), dinput.begin() + mismatch_point, 0);
   thrust::fill(dinput.begin() + mismatch_point, dinput.end(), val);
-  thrust::device_vector<T> d_result(1);
+  thrust::device_vector<T> d_result(1, thrust::no_init);
   ///
 
   void* d_temp_storage = nullptr;
@@ -65,11 +32,11 @@ void find_if(nvbench::state& state, nvbench::type_list<T>)
     temp_storage_bytes,
     thrust::raw_pointer_cast(dinput.data()),
     thrust::raw_pointer_cast(d_result.data()),
-    equals{val},
+    thrust::detail::equal_to_value<T>(val),
     dinput.size(),
     0);
 
-  thrust::device_vector<uint8_t> temp_storage(temp_storage_bytes);
+  thrust::device_vector<uint8_t> temp_storage(temp_storage_bytes, thrust::no_init);
   d_temp_storage = thrust::raw_pointer_cast(temp_storage.data());
 
   state.exec(nvbench::exec_tag::no_batch, [&](nvbench::launch& launch) {
@@ -78,7 +45,7 @@ void find_if(nvbench::state& state, nvbench::type_list<T>)
       temp_storage_bytes,
       thrust::raw_pointer_cast(dinput.data()),
       thrust::raw_pointer_cast(d_result.data()),
-      equals{val},
+      thrust::detail::equal_to_value<T>(val),
       dinput.size(),
       launch.get_stream());
   });
--- cub/cub/device/device_find_if.cuh
+++ cub/cub/device/device_find_if.cuh
@@ -1,29 +1,5 @@
-/******************************************************************************
- * Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the NVIDIA CORPORATION nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 //! @file
 //! cub::DeviceScan provides device-wide, parallel operations for computing a prefix scan across a sequence of data
@@ -32,12 +8,6 @@
 #pragma once
 
 #include <cub/config.cuh>
-
-#include "cub/util_type.cuh"
-#include "cuda/std/__cccl/dialect.h"
-#include "cuda/std/__memory/pointer_traits.h"
-#include "cuda/std/__utility/declval.h"
-#include "device_launch_parameters.h"
 
 #if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
 #  pragma GCC system_header
@@ -48,12 +18,10 @@
 #endif // no system header
 
 #include <cub/detail/choose_offset.cuh>
-#include <cub/detail/nvtx.cuh>
 #include <cub/device/dispatch/dispatch_find.cuh>
-#include <cub/device/dispatch/dispatch_scan.cuh>
-#include <cub/device/dispatch/dispatch_scan_by_key.cuh>
 #include <cub/thread/thread_operators.cuh>
-#include <cub/util_deprecated.cuh>
+
+#include <cuda/__nvtx/nvtx.h>
 
 #include <cassert>
 
@@ -71,7 +39,7 @@ struct DeviceFind
     NumItemsT num_items,
     cudaStream_t stream = 0)
   {
-    //    CUB_DETAIL_NVTX_RANGE_SCOPE_IF(d_temp_storage, "cub::DeviceFind::FindIf");
+    _CCCL_NVTX_RANGE_SCOPE("cub::DeviceFind::FindIf");
 
     // Signed integer type for global offsets
     using OffsetT = detail::choose_offset_t<NumItemsT>;
