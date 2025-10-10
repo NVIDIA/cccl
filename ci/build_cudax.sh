1e156b017a3591b6cc87db83a3daa821075bcc63
David Bayer <bayer.david99@gmail.com>
2025-10-10 18:58:59 +0200

    fix compilation

--- ci/build_cudax.sh
+++ ci/build_cudax.sh
@@ -8,7 +8,7 @@ print_environment_details
 
 # If the cudax_ENABLE_CUFILE variable is specified, we don't modify it. Otherwise if we got an nvcc binary, check the
 # nvcc version and if it's less than 12.9, disable the cuFile support
-if [[ -z "${cudax_ENABLE_CUFILE:-}"]]
+if [[ -z "${cudax_ENABLE_CUFILE:-}" ]]; then
   cudax_ENABLE_CUFILE="false"
   if [[ -n "${NVCC_VERSION:-}" ]]; then
     if util/version_compare.sh ${NVCC_VERSION} ge 12.9; then
