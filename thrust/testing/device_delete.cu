a4cc6b0a43450c169cc77c8f5ff960660fc3c11a
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-10-13 10:18:25 +0200

    Bypass allocator in `thrust::device_delete`

--- thrust/testing/device_delete.cu
+++ thrust/testing/device_delete.cu
@@ -9,36 +9,31 @@
 
 struct Foo
 {
-  _CCCL_HOST_DEVICE Foo()
+  _CCCL_DEVICE Foo()
       : set_me_upon_destruction{nullptr}
   {}
 
-  _CCCL_HOST_DEVICE ~Foo()
+  _CCCL_DEVICE ~Foo()
   {
-    NV_IF_TARGET(NV_IS_DEVICE, (if (set_me_upon_destruction != nullptr) { *set_me_upon_destruction = true; }));
+    if (set_me_upon_destruction != nullptr)
+    {
+      *set_me_upon_destruction = true;
+    }
   }
 
   bool* set_me_upon_destruction;
 };
 
-#if !defined(__QNX__)
 void TestDeviceDeleteDestructorInvocation()
 {
-  KNOWN_FAILURE;
-  //
-  //  thrust::device_vector<bool> destructor_flag(1, false);
-  //
-  //  thrust::device_ptr<Foo> foo_ptr  = thrust::device_new<Foo>();
-  //
-  //  Foo exemplar;
-  //  exemplar.set_me_upon_destruction = thrust::raw_pointer_cast(&destructor_flag[0]);
-  //  *foo_ptr = exemplar;
-  //
-  //  ASSERT_EQUAL(false, destructor_flag[0]);
-  //
-  //  thrust::device_delete(foo_ptr);
-  //
-  //  ASSERT_EQUAL(true, destructor_flag[0]);
+  thrust::device_vector<bool> destructor_flag(1, false);
+
+  thrust::device_ptr<Foo> foo_ptr = thrust::device_new<Foo>();
+  *thrust::device_ptr<bool*>(&foo_ptr.get()->set_me_upon_destruction) =
+    thrust::raw_pointer_cast(destructor_flag.data());
+
+  ASSERT_EQUAL(false, destructor_flag[0]);
+  thrust::device_delete(foo_ptr);
+  ASSERT_EQUAL(true, destructor_flag[0]);
 }
 DECLARE_UNITTEST(TestDeviceDeleteDestructorInvocation);
-#endif
