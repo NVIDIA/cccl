f5a1f10c7efb7f00cc8e5659315ceaded30b7487
fbusato <fbusato@nvidia.com>
2025-10-15 14:24:42 -0700

    improve NaN/Inf testing

--- libcudacxx/test/libcudacxx/cuda/utility/in_range.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/utility/in_range.pass.cpp
@@ -23,10 +23,7 @@
 template <typename T>
 __host__ __device__ constexpr void test()
 {
-  if constexpr (!cuda::std::is_same_v<T, __half> && !cuda::std::is_same_v<T, __nv_bfloat16>)
-  {
-    static_assert(noexcept(cuda::in_range(cuda::std::declval<T>(), cuda::std::declval<T>(), cuda::std::declval<T>())));
-  }
+  static_assert(noexcept(cuda::in_range(cuda::std::declval<T>(), cuda::std::declval<T>(), cuda::std::declval<T>())));
   assert(cuda::in_range(T{5}, T{0}, T{10}));
   assert(!cuda::in_range(T{15}, T{0}, T{10}));
   assert(cuda::in_range(T{10}, T{0}, T{10})); // test bound
@@ -42,20 +39,36 @@ __host__ __device__ constexpr void test()
   assert(!cuda::in_range(T{5}, cuda::std::numeric_limits<T>::min(), T{cuda::std::numeric_limits<T>::min() + T{1}}));
   assert(cuda::in_range(T{5}, cuda::std::numeric_limits<T>::min(), cuda::std::numeric_limits<T>::max()));
 
-  if constexpr (cuda::is_floating_point_v<T> && cuda::std::numeric_limits<T>::has_quiet_NaN
-                && cuda::std::numeric_limits<T>::has_infinity)
+  if constexpr (cuda::is_floating_point_v<T>)
   {
-    constexpr auto nan = cuda::std::numeric_limits<T>::quiet_NaN();
-    constexpr auto inf = cuda::std::numeric_limits<T>::infinity();
-    assert(cuda::in_range(inf, -inf, inf));
-    assert(cuda::in_range(-inf, -inf, inf));
-    assert(!cuda::in_range(inf, T{0}, T{10}));
-    assert(cuda::in_range(T{1}, T{-1}, inf));
-    if (!cuda::std::__cccl_default_is_constant_evaluated())
+    if constexpr (cuda::std::numeric_limits<T>::has_infinity)
     {
-      assert(!cuda::in_range(nan, -inf, inf));
-      assert(!cuda::in_range(T{1}, nan, inf));
-      assert(!cuda::in_range(T{1}, inf, nan));
+      constexpr auto inf = cuda::std::numeric_limits<T>::infinity();
+      assert(cuda::in_range(inf, -inf, inf));
+      assert(cuda::in_range(-inf, -inf, inf));
+      assert(!cuda::in_range(inf, T{0}, T{10}));
+      assert(cuda::in_range(T{1}, T{-1}, inf));
+    }
+    if constexpr (cuda::std::numeric_limits<T>::has_quiet_NaN)
+    {
+      constexpr auto nan = cuda::std::numeric_limits<T>::quiet_NaN();
+      if (!cuda::std::__cccl_default_is_constant_evaluated())
+      {
+        assert(!cuda::in_range(nan, T{0}, T{10}));
+        assert(!cuda::in_range(T{1}, nan, T{10}));
+        assert(!cuda::in_range(T{1}, T{10}, nan));
+      }
+    }
+    if constexpr (cuda::std::numeric_limits<T>::has_quiet_NaN && cuda::std::numeric_limits<T>::has_infinity)
+    {
+      constexpr auto inf = cuda::std::numeric_limits<T>::infinity();
+      constexpr auto nan = cuda::std::numeric_limits<T>::quiet_NaN();
+      if (!cuda::std::__cccl_default_is_constant_evaluated())
+      {
+        assert(!cuda::in_range(nan, -inf, inf));
+        assert(!cuda::in_range(T{1}, nan, inf));
+        assert(!cuda::in_range(T{1}, inf, nan));
+      }
     }
   }
 }
