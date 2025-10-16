7783aece16a23fdc02cd4d4bc0455409f9dfbb69
fbusato <fbusato@nvidia.com>
2025-10-16 09:28:59 -0700

    use forward iterator

--- libcudacxx/include/cuda/__memory/ptr_ranges_overlap.h
+++ libcudacxx/include/cuda/__memory/ptr_ranges_overlap.h
@@ -22,22 +22,35 @@
 #endif // no system header
 
 #include <cuda/__memory/ptr_in_range.h>
+#include <cuda/std/__concepts/concept_macros.h>
+#include <cuda/std/__iterator/addressof.h>
+#include <cuda/std/__iterator/concepts.h>
+#include <cuda/std/__iterator/iterator_traits.h>
 
 #include <cuda/std/__cccl/prologue.h>
 
 _CCCL_BEGIN_NAMESPACE_CUDA
 
-[[nodiscard]] _CCCL_API constexpr bool ptr_ranges_overlap(
-  const void* __ptr_lhs_start,
-  const void* __ptr_lhs_end,
-  const void* __ptr_rhs_start,
-  const void* __ptr_rhs_end) noexcept
+_CCCL_TEMPLATE(typename _Tp)
+_CCCL_REQUIRES(::cuda::std::forward_iterator<_Tp>)
+[[nodiscard]] _CCCL_API constexpr bool
+ranges_overlap(_Tp __lhs_begin, _Tp __lhs_end, _Tp __rhs_begin, _Tp __rhs_end) noexcept
 {
-  return ::cuda::ptr_in_range(__ptr_lhs_start, __ptr_rhs_start, __ptr_rhs_end)
-      || ::cuda::ptr_in_range(__ptr_rhs_start, __ptr_lhs_start, __ptr_lhs_end);
-}
+  if constexpr (::cuda::std::contiguous_iterator<_Tp>)
+  {
+    const auto __ptr_lhs_begin = ::cuda::std::addressof(__lhs_begin);
+    const auto __ptr_lhs_end   = ::cuda::std::addressof(__lhs_end);
+    const auto __ptr_rhs_begin = ::cuda::std::addressof(__rhs_begin);
+    const auto __ptr_rhs_end   = ::cuda::std::addressof(__rhs_end);
+    return ::cuda::ptr_in_range(__ptr_lhs_begin, __ptr_rhs_begin, __ptr_rhs_end)
+        || ::cuda::ptr_in_range(__ptr_rhs_begin, __ptr_lhs_begin, __ptr_lhs_end);
+  }
+  else
+  {
+    return __lhs_begin < __rhs_end && __rhs_begin < __lhs_end;
+  }
 
-_CCCL_END_NAMESPACE_CUDA
+  _CCCL_END_NAMESPACE_CUDA
 
 #include <cuda/std/__cccl/epilogue.h>
 
