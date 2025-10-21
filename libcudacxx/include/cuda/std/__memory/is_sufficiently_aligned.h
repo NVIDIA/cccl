56a7be3bf175c81a8d7972cec8f7d640c0da7c29
fbusato <fbusato@nvidia.com>
2025-10-21 13:24:37 -0700

    update is_sufficiently_aligned

--- libcudacxx/include/cuda/std/__memory/is_sufficiently_aligned.h
+++ libcudacxx/include/cuda/std/__memory/is_sufficiently_aligned.h
@@ -25,6 +25,7 @@
 #include <cuda/__cmath/pow2.h>
 #include <cuda/std/cstddef> // size_t
 #include <cuda/std/cstdint> // uintptr_t
+#include <cuda/std/type_traits> // is_void_v
 
 #include <cuda/std/__cccl/prologue.h>
 
@@ -33,9 +34,13 @@ _CCCL_BEGIN_NAMESPACE_CUDA_STD
 template <size_t _ByteAlignment, class _ElementType>
 [[nodiscard]] _CCCL_API inline bool is_sufficiently_aligned(_ElementType* __ptr) noexcept
 {
+  using ::cuda::std::uintptr_t;
   static_assert(::cuda::is_power_of_two(_ByteAlignment), "alignment must be a power of two");
-  static_assert(_ByteAlignment % alignof(_ElementType) == 0,
-                "the alignment must be a multiple of the element alignment");
+  if constexpr (!::cuda::std::is_void_v<_ElementType>)
+  {
+    static_assert(_ByteAlignment % alignof(_ElementType) == 0,
+                  "the alignment must be a multiple of the element alignment");
+  }
   return (reinterpret_cast<uintptr_t>(__ptr) % _ByteAlignment) == 0;
 }
 
