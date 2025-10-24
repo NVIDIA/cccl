3545619ffffc172389b8036a2c03128b968e5124
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-10-24 01:52:40 -0700

    Constexpr

--- libcudacxx/include/cuda/std/__random/seed_seq.h
+++ libcudacxx/include/cuda/std/__random/seed_seq.h
@@ -21,9 +21,12 @@
 #  pragma system_header
 #endif // no system header
 
-#if !_CCCL_COMPILER(NVRTC)
-#  include <iostream>
-#endif // !_CCCL_COMPILER(NVRTC)
+#include <cuda/std/__algorithm/copy.h>
+#include <cuda/std/__algorithm/fill.h>
+#include <cuda/std/cstddef>
+#include <cuda/std/cstdint>
+#include <cuda/std/initializer_list>
+#include <cuda/std/span>
 
 #include <cuda/std/__cccl/prologue.h>
 
@@ -39,115 +42,110 @@ public:
   seed_seq() = default;
 
   template <class _InputIt>
-  constexpr seed_seq(_InputIt __begin, _InputIt __end)
+  _CCCL_API constexpr seed_seq(_InputIt __begin, _InputIt __end)
   {
     auto __n = ::cuda::std::distance(__begin, __end);
-    result_type* m_ptr;
-    // TODO(Rory): What is the correct way to allocate managed memory and check for errors here?
-    auto err = cudaMallocManaged(&m_ptr, __n * sizeof(result_type));
-    if (err != cudaSuccess)
+    if (__n == 0)
     {
-      throw std::runtime_error("Failed to allocate managed memory");
+      return;
     }
+    auto* m_ptr = new result_type[__n];
     for (auto __i = 0; __i < __n; ++__i, ++__begin)
     {
-      m_ptr[__i] = static_cast<result_type>(*__begin) & 0xFFFFFFFF;
+      auto val   = *__begin;
+      m_ptr[__i] = static_cast<result_type>(val) & 0xFFFFFFFF;
     }
     __v_ = ::cuda::std::span<result_type>(m_ptr, __n);
   }
 
-  template <class _T>
-  constexpr seed_seq(std::initializer_list<_T> __il)
+  template <typename _InitT,
+            typename = typename ::cuda::std::enable_if<::cuda::std::is_convertible<_InitT, result_type>::value>::type>
+  _CCCL_API constexpr seed_seq(std::initializer_list<_InitT> __il)
       : seed_seq(__il.begin(), __il.end())
   {}
 
   seed_seq(const seed_seq&) = delete;
 
-  ~seed_seq()
+  _CCCL_API constexpr ~seed_seq()
   {
     // cudamanagedfree
     if (!__v_.empty())
     {
-      cudaFree(__v_.data());
+      delete[] __v_.data();
     }
-  };
+  }
 
-  operator=(const seed_seq&) = delete;
-  constexpr ::cuda::std::size_t size() const noexcept
+  _CCCL_API auto operator=(const seed_seq&) = delete;
+  _CCCL_API constexpr ::cuda::std::size_t size() const noexcept
   {
     return __v_.size();
   }
 
   template <class _OutputIt>
-  constexpr void param(_OutputIt __dest) const
+  _CCCL_API constexpr void param(_OutputIt __dest) const
   {
     ::cuda::std::copy(__v_.begin(), __v_.end(), __dest);
   }
 
   template <class _RandomIt>
-  constexpr void generate(_RandomIt __begin, _RandomIt __end)
+  _CCCL_API constexpr void generate(_RandomIt __begin, _RandomIt __end) const
   {
     if (__begin == __end)
     {
       return;
     }
-    const auto __z = __v_.size();
-    const auto __n = ::cuda::std::distance(__begin, __end);
-    const auto __m = ::cuda::std::max(__z + 1, __n);
-    const auto __t = (__n >= 623) ? 11 : (__n >= 68) ? 7 : (__n >= 39) ? 5 : (__n >= 7) ? 3 : (__n - 1) / 2;
-    const auto __p = (__n - __t) / 2;
-    const auto __q = __p + __t;
-
     // https://en.cppreference.com/w/cpp/numeric/random/seed_seq/generate.html
+    const result_type __z = __v_.size();
+    const result_type __n = ::cuda::std::distance(__begin, __end);
+    const result_type __m = ::cuda::std::max(__z + 1, __n);
+    const result_type __t = (__n >= 623) ? 11 : (__n >= 68) ? 7 : (__n >= 39) ? 5 : (__n >= 7) ? 3 : (__n - 1) / 2;
+    const result_type __p = (__n - __t) / 2;
+    const result_type __q = __p + __t;
+
     // 1.
     ::cuda::std::fill(__begin, __end, result_type{0x8b8b8b8b});
+
     // 2.
     for (::cuda::std::size_t __k = 0; __k < __m; ++__k)
     {
+      result_type __k_mod_n   = __k % __n;
+      result_type __k_p_mod_n = (__k + __p) % __n;
+      result_type __k_q_mod_n = (__k + __q) % __n;
       // 2.1
-      const result_type __r1 =
-        1664525 * __T(__begin[__k % __n] ^ __begin[(__k + __p) % __n] ^ __begin[(__k - 1) % __n]);
+      const result_type __r1 = 1664525 * __T(__begin[__k_mod_n] ^ __begin[__k_p_mod_n] ^ __begin[(__k - 1) % __n]);
       // 2.2
-      result_type __r2 = 0;
-      if (__k == 0)
-      {
-        __r2 = __r1 + __z;
-      }
-      else if (__k <= __z)
-      {
-        __r2 = __r1 + (__k % __n) + __v_[__k - 1];
-      }
-      else
-      {
-        __r2 = __r1 + (__k % __n);
-      }
+      result_type __r2 = (__k == 0) ? __r1 + __z : (__k <= __z) ? __r1 + __k_mod_n + __v_[__k - 1] : __r1 + __k_mod_n;
       // 2.3
-      __begin[(__k + __p) % __n] = (__begin[(__k + __p) % __n] + __r1) && 0xFFFFFFFF;
-      // 2.4
-      __begin[(__k + __q) % __n] = (__begin[(__k + __q) % __n] + __r2) && 0xFFFFFFFF;
-      // 2.5
-      __begin[__k % __n] = r2 && 0xFFFFFFFF;
+      __begin[__k_p_mod_n] += __r1;
+      __begin[__k_p_mod_n] &= 0xFFFFFFFF;
+      __begin[__k_q_mod_n] += __r2;
+      __begin[__k_q_mod_n] &= 0xFFFFFFFF;
+      __begin[__k_mod_n] = __r2 & 0xFFFFFFFF;
     }
 
     // 3.
     for (::cuda::std::size_t __k = __m; __k < __m + __n; ++__k)
     {
+      result_type __k_mod_n   = __k % __n;
+      result_type __k_p_mod_n = (__k + __p) % __n;
+      result_type __k_q_mod_n = (__k + __q) % __n;
       // 3.1
-      const result_type __r3 =
-        1566083941 * __T(__begin[__k % __n] + __begin[(__k + __p) % __n] + __begin[(__k - 1) % __n]);
+      const result_type __r3 = 1566083941 * __T(__begin[__k_mod_n] + __begin[__k_p_mod_n] + __begin[(__k - 1) % __n]);
       // 3.2
-      const result_type __r4 = __r3 - (__k % __n);
+      const result_type __r4 = __r3 - __k_mod_n;
       // 3.3
-      __begin[(__k + __p) % __n] = (__begin[(__k + __p) % __n] ^ __r3) && 0xFFFFFFFF;
+      __begin[__k_p_mod_n] ^= __r3;
+      __begin[__k_p_mod_n] &= 0xFFFFFFFF;
       // 3.4
-      __begin[(__k + __q) % __n] = (__begin[(__k + __q) % __n] ^ __r4) && 0xFFFFFFFF;
+      __begin[__k_q_mod_n] ^= __r4;
+      __begin[__k_q_mod_n] &= 0xFFFFFFFF;
       // 3.5
-      __begin[__k % __n] = __r4 && 0xFFFFFFFF;
+      __begin[__k_mod_n] = __r4 & 0xFFFFFFFF;
     }
   }
 
 private:
-  static constexpr result_type __T(result_type __x) noexcept
+  _CCCL_API static constexpr result_type __T(result_type __x) noexcept
   {
     return (__x ^ (__x >> 27));
   }
