34828c19854ab744ad2092268fdb4c73b0d6bc18
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-10-27 03:49:51 -0700

    All passing

--- libcudacxx/include/cuda/std/__random/bernoulli_distribution.h
+++ libcudacxx/include/cuda/std/__random/bernoulli_distribution.h
@@ -1,12 +1,11 @@
 //===----------------------------------------------------------------------===//
 //
-// Part of libcu++, the C++ Standard Library for your entire system,
-// under the Apache License v2.0 with LLVM Exceptions.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
-//===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===////===----------------------------------------------------------------------===//
 
 #ifndef _CUDA_STD___BERNOULLI_DISTRIBUTION_H
 #define _CUDA_STD___BERNOULLI_DISTRIBUTION_H
@@ -21,6 +20,12 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/std/__random/is_valid.h>
+#include <cuda/std/__random/uniform_real_distribution.h>
+#if !_CCCL_COMPILER(NVRTC)
+#  include <ios>
+#endif // !_CCCL_COMPILER(NVRTC)
+
 #include <cuda/std/__cccl/prologue.h>
 
 _CCCL_BEGIN_NAMESPACE_CUDA_STD
@@ -29,28 +34,29 @@ class bernoulli_distribution
 {
 public:
   // types
-  using result_type = bool;
+  typedef bool result_type;
 
   class param_type
   {
     double __p_;
 
   public:
-    using distribution_type = bernoulli_distribution;
-    _CCCL_API explicit param_type(double __p = 0.5) noexcept
+    typedef bernoulli_distribution distribution_type;
+
+    _CCCL_API explicit param_type(double __p = 0.5)
         : __p_(__p)
     {}
 
-    [[nodiscard]] _CCCL_API result_type p() const noexcept
+    _CCCL_API double p() const
     {
       return __p_;
     }
 
-    [[nodiscard]] _CCCL_API friend bool operator==(const param_type& __x, const param_type& __y) noexcept
+    friend _CCCL_API bool operator==(const param_type& __x, const param_type& __y)
     {
       return __x.__p_ == __y.__p_;
     }
-    [[nodiscard]] _CCCL_API friend bool operator!=(const param_type& __x, const param_type& __y) noexcept
+    friend _CCCL_API bool operator!=(const param_type& __x, const param_type& __y)
     {
       return !(__x == __y);
     }
@@ -61,102 +67,99 @@ private:
 
 public:
   // constructors and reset functions
-  _CCCL_API bernoulli_distribution() noexcept
+  _CCCL_API bernoulli_distribution()
       : bernoulli_distribution(0.5)
   {}
-  _CCCL_API explicit bernoulli_distribution(double __p) noexcept
+  _CCCL_API explicit bernoulli_distribution(double __p)
       : __p_(param_type(__p))
   {}
-  _CCCL_API explicit bernoulli_distribution(const param_type& __p) noexcept
+  _CCCL_API explicit bernoulli_distribution(const param_type& __p)
       : __p_(__p)
   {}
-  _CCCL_API void reset() noexcept {}
+  _CCCL_API void reset() {}
 
   // generating functions
-  template <class _URng>
-  [[nodiscard]] _CCCL_API result_type operator()(_URng& __g) noexcept
+  template <class _URNG>
+  _CCCL_API result_type operator()(_URNG& __g)
   {
-    static_assert(__cccl_random_is_valid_urng<_URng>, "");
     return (*this)(__g, __p_);
   }
-  _CCCL_EXEC_CHECK_DISABLE
-  template <class _URng>
-  [[nodiscard]] _CCCL_API result_type operator()(_URng& __g, const param_type& __p) noexcept
+  template <class _URNG>
+  _CCCL_API result_type operator()(_URNG& __g, const param_type& __p)
   {
-    static_assert(__cccl_random_is_valid_urng<_URng>, "");
+    static_assert(__cccl_random_is_valid_urng<_URNG>, "");
     uniform_real_distribution<double> __gen;
     return __gen(__g) < __p.p();
   }
 
   // property functions
-  [[nodiscard]] _CCCL_API result_type p() const noexcept
+  _CCCL_API double p() const
   {
     return __p_.p();
   }
 
-  [[nodiscard]] _CCCL_API param_type param() const noexcept
+  _CCCL_API param_type param() const
   {
     return __p_;
   }
-  _CCCL_API void param(const param_type& __p) noexcept
+  _CCCL_API void param(const param_type& __p)
   {
     __p_ = __p;
   }
 
-  [[nodiscard]] _CCCL_API result_type min() const noexcept
+  _CCCL_API result_type min() const
   {
     return false;
   }
-  [[nodiscard]] _CCCL_API result_type max() const noexcept
+  _CCCL_API result_type max() const
   {
     return true;
   }
 
-  [[nodiscard]] _CCCL_API friend bool
-  operator==(const bernoulli_distribution& __x, const bernoulli_distribution& __y) noexcept
+  friend _CCCL_API bool operator==(const bernoulli_distribution& __x, const bernoulli_distribution& __y)
   {
     return __x.__p_ == __y.__p_;
   }
-  [[nodiscard]] _CCCL_API friend bool
-  operator!=(const bernoulli_distribution& __x, const bernoulli_distribution& __y) noexcept
+  friend _CCCL_API bool operator!=(const bernoulli_distribution& __x, const bernoulli_distribution& __y)
   {
     return !(__x == __y);
   }
-};
-
-#if 0 // Implement stream operators
-template <class _CharT, class _Traits, class _IT>
-_CCCL_API basic_ostream<_CharT, _Traits>&
-operator<<(basic_ostream<_CharT, _Traits>& __os, const bernoulli_distribution& __x)
-{
-  __save_flags<_CharT, _Traits> __lx(__os);
-  using _Ostream = basic_ostream<_CharT, _Traits>;
-  __os.flags(_Ostream::dec | _Ostream::left);
-  _CharT __sp = __os.widen(' ');
-  __os.fill(__sp);
-  return __os << __x.p();
-}
-
-template <class _CharT, class _Traits, class _IT>
-_CCCL_API basic_istream<_CharT, _Traits>&
-operator>>(basic_istream<_CharT, _Traits>& __is, bernoulli_distribution& __x)
-{
-  using _Eng = uniform_int_distribution<_IT>;
-  using result_type = typename _Eng::result_type;
-  using param_type = typename _Eng::param_type;
-  __save_flags<_CharT, _Traits> __lx(__is);
-  using _Istream = basic_istream<_CharT, _Traits>;
-  __is.flags(_Istream::dec | _Istream::skipws);
-  result_type __a;
-  result_type __b;
-  __is >> __a >> __b;
-  if (!__is.fail())
+#if !_CCCL_COMPILER(NVRTC)
+  template <class _CharT, class _Traits>
+  friend ::std::basic_ostream<_CharT, _Traits>&
+  operator<<(::std::basic_ostream<_CharT, _Traits>& __os, const bernoulli_distribution& __x)
   {
-    __x.param(param_type(__a, __b));
+    using ostream_type                        = ::std::basic_ostream<_CharT, _Traits>;
+    using ios_base                            = typename ostream_type::ios_base;
+    const typename ios_base::fmtflags __flags = __os.flags();
+    __os.flags(ios_base::dec | ios_base::left | ios_base::fixed | ios_base::scientific);
+    _CharT __sp = __os.widen(' ');
+    __os.fill(__sp);
+    __os.flags(__flags);
+    return __os << __x.p();
   }
-  return __is;
-}
-#endif // Implement stream operators
+
+  template <class _CharT, class _Traits>
+  friend ::std::basic_istream<_CharT, _Traits>&
+  operator>>(::std::basic_istream<_CharT, _Traits>& __is, bernoulli_distribution& __x)
+  {
+    using istream_type                        = ::std::basic_istream<_CharT, _Traits>;
+    using ios_base                            = typename istream_type::ios_base;
+    const typename ios_base::fmtflags __flags = __is.flags();
+    typedef bernoulli_distribution _Eng;
+    typedef typename _Eng::param_type param_type;
+    __is.flags(ios_base::dec | ios_base::skipws);
+    double __p;
+    __is >> __p;
+    if (!__is.fail())
+    {
+      __x.param(param_type(__p));
+    }
+    __is.flags(__flags);
+    return __is;
+  }
+#endif // !_CCCL_COMPILER(NVRTC)
+};
 
 _CCCL_END_NAMESPACE_CUDA_STD
 
---
+++ libcudacxx/test/libcudacxx/std/numerics/rand/rand.dist/rand.dist.bern/rand.dist.bern/rand.dist.bern.bernoulli/eval.pass.cpp
@@ -0,0 +1,117 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <random>
+
+// class bernoulli_distribution
+
+// template<class _URNG> result_type operator()(_URNG& g);
+
+#include <cuda/std/__memory_>
+#include <cuda/std/__random_>
+#include <cuda/std/cassert>
+#include <cuda/std/cmath>
+#include <cuda/std/cstddef>
+#include <cuda/std/numeric>
+#include <cuda/std/span>
+
+#include "test_macros.h"
+
+template <class T>
+inline __host__ __device__ T sqr(T x)
+{
+  return x * x;
+}
+
+__host__ __device__ bool test()
+{
+  {
+    typedef cuda::std::bernoulli_distribution D;
+    typedef cuda::std::minstd_rand G;
+    G g;
+    D d(.75);
+    const int N                                   = 100000;
+    cuda::std::unique_ptr<D::result_type[]> array = cuda::std::make_unique<D::result_type[]>(N);
+    cuda::std::span<D::result_type, N> u{array.get(), array.get() + N};
+    for (int i = 0; i < N; ++i)
+    {
+      u[i] = d(g);
+    }
+    double mean     = cuda::std::accumulate(u.begin(), u.end(), double(0)) / u.size();
+    double var      = 0;
+    double skew     = 0;
+    double kurtosis = 0;
+    for (std::size_t i = 0; i < u.size(); ++i)
+    {
+      double dbl = (u[i] - mean);
+      double d2  = sqr(dbl);
+      var += d2;
+      skew += dbl * d2;
+      kurtosis += d2 * d2;
+    }
+    var /= u.size();
+    double dev = cuda::std::sqrt(var);
+    skew /= u.size() * dev * var;
+    kurtosis /= u.size() * var * var;
+    kurtosis -= 3;
+    double x_mean     = d.p();
+    double x_var      = d.p() * (1 - d.p());
+    double x_skew     = (1 - 2 * d.p()) / cuda::std::sqrt(x_var);
+    double x_kurtosis = (6 * sqr(d.p()) - 6 * d.p() + 1) / x_var;
+    assert(cuda::std::abs((mean - x_mean) / x_mean) < 0.01);
+    assert(cuda::std::abs((var - x_var) / x_var) < 0.01);
+    assert(cuda::std::abs((skew - x_skew) / x_skew) < 0.02);
+    assert(cuda::std::abs((kurtosis - x_kurtosis) / x_kurtosis) < 0.05);
+  }
+  {
+    typedef cuda::std::bernoulli_distribution D;
+    typedef cuda::std::minstd_rand G;
+    G g;
+    D d(.25);
+    const int N                                   = 100000;
+    cuda::std::unique_ptr<D::result_type[]> array = cuda::std::make_unique<D::result_type[]>(N);
+    cuda::std::span<D::result_type, N> u{array.get(), array.get() + N};
+    for (int i = 0; i < N; ++i)
+    {
+      u[i] = d(g);
+    }
+    double mean     = cuda::std::accumulate(u.begin(), u.end(), double(0)) / u.size();
+    double var      = 0;
+    double skew     = 0;
+    double kurtosis = 0;
+    for (cuda::std::size_t i = 0; i < u.size(); ++i)
+    {
+      double dbl = (u[i] - mean);
+      double d2  = sqr(dbl);
+      var += d2;
+      skew += dbl * d2;
+      kurtosis += d2 * d2;
+    }
+    var /= u.size();
+    double dev = cuda::std::sqrt(var);
+    skew /= u.size() * dev * var;
+    kurtosis /= u.size() * var * var;
+    kurtosis -= 3;
+    double x_mean     = d.p();
+    double x_var      = d.p() * (1 - d.p());
+    double x_skew     = (1 - 2 * d.p()) / cuda::std::sqrt(x_var);
+    double x_kurtosis = (6 * sqr(d.p()) - 6 * d.p() + 1) / x_var;
+    assert(cuda::std::abs((mean - x_mean) / x_mean) < 0.01);
+    assert(cuda::std::abs((var - x_var) / x_var) < 0.01);
+    assert(cuda::std::abs((skew - x_skew) / x_skew) < 0.02);
+    assert(cuda::std::abs((kurtosis - x_kurtosis) / x_kurtosis) < 0.05);
+  }
+
+  return true;
+}
+
+int main(int, char**)
+{
+  test();
+  return 0;
+}
