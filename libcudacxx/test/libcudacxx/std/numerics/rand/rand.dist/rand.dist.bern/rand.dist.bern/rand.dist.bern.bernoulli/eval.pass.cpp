a02e9141d0d232e8e97211599c9a7502eed4ee4d
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-10-27 10:03:45 -0700

    Add chisq test

--- libcudacxx/test/libcudacxx/std/numerics/rand/rand.dist/rand.dist.bern/rand.dist.bern/rand.dist.bern.bernoulli/eval.pass.cpp
+++ libcudacxx/test/libcudacxx/std/numerics/rand/rand.dist/rand.dist.bern/rand.dist.bern/rand.dist.bern.bernoulli/eval.pass.cpp
@@ -20,93 +20,22 @@
 #include <cuda/std/numeric>
 #include <cuda/std/span>
 
+#include "test_discrete_distribution.h"
 #include "test_macros.h"
-
-template <class T>
-inline __host__ __device__ T sqr(T x)
-{
-  return x * x;
-}
 
 __host__ __device__ TEST_CONSTEXPR_CXX20 bool test()
 {
-  {
-    typedef cuda::std::bernoulli_distribution D;
-    typedef cuda::std::minstd_rand G;
-    G g;
-    D d(.75);
-    const int N                                   = 100000;
-    cuda::std::unique_ptr<D::result_type[]> array = cuda::std::make_unique<D::result_type[]>(N);
-    cuda::std::span<D::result_type, N> u{array.get(), array.get() + N};
-    for (int i = 0; i < N; ++i)
-    {
-      u[i] = d(g);
-    }
-    double mean     = cuda::std::accumulate(u.begin(), u.end(), double(0)) / u.size();
-    double var      = 0;
-    double skew     = 0;
-    double kurtosis = 0;
-    for (std::size_t i = 0; i < u.size(); ++i)
-    {
-      double dbl = (u[i] - mean);
-      double d2  = sqr(dbl);
-      var += d2;
-      skew += dbl * d2;
-      kurtosis += d2 * d2;
-    }
-    var /= u.size();
-    double dev = cuda::std::sqrt(var);
-    skew /= u.size() * dev * var;
-    kurtosis /= u.size() * var * var;
-    kurtosis -= 3;
-    double x_mean     = d.p();
-    double x_var      = d.p() * (1 - d.p());
-    double x_skew     = (1 - 2 * d.p()) / cuda::std::sqrt(x_var);
-    double x_kurtosis = (6 * sqr(d.p()) - 6 * d.p() + 1) / x_var;
-    assert(cuda::std::abs((mean - x_mean) / x_mean) < 0.01);
-    assert(cuda::std::abs((var - x_var) / x_var) < 0.01);
-    assert(cuda::std::abs((skew - x_skew) / x_skew) < 0.02);
-    assert(cuda::std::abs((kurtosis - x_kurtosis) / x_kurtosis) < 0.05);
-  }
-  {
-    typedef cuda::std::bernoulli_distribution D;
-    typedef cuda::std::minstd_rand G;
-    G g;
-    D d(.25);
-    const int N                                   = 100000;
-    cuda::std::unique_ptr<D::result_type[]> array = cuda::std::make_unique<D::result_type[]>(N);
-    cuda::std::span<D::result_type, N> u{array.get(), array.get() + N};
-    for (int i = 0; i < N; ++i)
-    {
-      u[i] = d(g);
-    }
-    double mean     = cuda::std::accumulate(u.begin(), u.end(), double(0)) / u.size();
-    double var      = 0;
-    double skew     = 0;
-    double kurtosis = 0;
-    for (cuda::std::size_t i = 0; i < u.size(); ++i)
-    {
-      double dbl = (u[i] - mean);
-      double d2  = sqr(dbl);
-      var += d2;
-      skew += dbl * d2;
-      kurtosis += d2 * d2;
-    }
-    var /= u.size();
-    double dev = cuda::std::sqrt(var);
-    skew /= u.size() * dev * var;
-    kurtosis /= u.size() * var * var;
-    kurtosis -= 3;
-    double x_mean     = d.p();
-    double x_var      = d.p() * (1 - d.p());
-    double x_skew     = (1 - 2 * d.p()) / cuda::std::sqrt(x_var);
-    double x_kurtosis = (6 * sqr(d.p()) - 6 * d.p() + 1) / x_var;
-    assert(cuda::std::abs((mean - x_mean) / x_mean) < 0.01);
-    assert(cuda::std::abs((var - x_var) / x_var) < 0.01);
-    assert(cuda::std::abs((skew - x_skew) / x_skew) < 0.02);
-    assert(cuda::std::abs((kurtosis - x_kurtosis) / x_kurtosis) < 0.05);
-  }
-
+  typedef cuda::std::bernoulli_distribution D;
+  typedef cuda::std::minstd_rand G;
+  const int n = 10000;
+  G g;
+  D d;
+  auto result = test_discrete_distribution<D, G, 2>(g, d, D::param_type{0.5}, {0.5, 0.5}, n);
+  assert(result);
+  result = test_discrete_distribution<D, G, 2>(g, d, D::param_type{0.2}, {0.2, 0.8}, n);
+  assert(result);
+  result = test_discrete_distribution<D, G, 2>(g, d, D::param_type{0.99}, {0.99, 0.01}, n);
+  assert(result);
   return true;
 }
 
--- libcudacxx/test/libcudacxx/std/numerics/rand/rand.dist/rand.dist.bern/rand.dist.bern/rand.dist.bern.bernoulli/test_discrete_distribution.h
+++ libcudacxx/test/libcudacxx/std/numerics/rand/rand.dist/rand.dist.bern/rand.dist.bern/rand.dist.bern.bernoulli/test_discrete_distribution.h
@@ -7,8 +7,44 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
+#include <cuda/std/array>
 
-bool test_discrete_distribution()
+// Perform a chi-squared test, comparing the observed and expected frequencies
+// of outcomes from a discrete distribution. Tests to significance level 0.01. Accepts 2-10 buckets.
+template <class DiscreteDistribution, class URNG, std::size_t N>
+__host__ __device__ TEST_CONSTEXPR_CXX20 bool test_discrete_distribution(
+  URNG& g,
+  DiscreteDistribution& dist,
+  const typename DiscreteDistribution::param_type& param,
+  const cuda::std::array<double, N>& expected_probabilities,
+  cuda::std::size_t num_samples)
 {
-  return true;
+  assert(expected_probabilities.size() >= 2 && expected_probabilities.size() <= 10);
+  using result_type = typename DiscreteDistribution::result_type;
+  cuda::std::array<cuda::std::size_t, N> observed_frequencies{};
+
+  // Generate samples and record observed frequencies.
+  for (cuda::std::size_t i = 0; i < num_samples; ++i)
+  {
+    result_type sample = dist(g, param);
+    ++observed_frequencies[static_cast<int>(sample)];
+  }
+
+  // Compute chi-squared statistic.
+  double chi_squared = 0.0;
+  for (cuda::std::size_t i = 0; i < N; ++i)
+  {
+    double expected = expected_probabilities[i] * num_samples;
+    double observed = static_cast<double>(observed_frequencies[i]);
+    double diff     = observed - expected;
+    chi_squared += (diff * diff) / expected;
+  }
+
+  // Critical value for chi-squared distribution with (num_buckets - 1) degrees of freedom at significance level 0.01
+  const cuda::std::array<double, 11> critical_values = {
+    0.0, 6.635, 9.210, 11.345, 13.277, 15.086, 16.812, 18.475, 20.090, 21.666, 23.209};
+
+  double critical_value = critical_values.at(N - 1);
+
+  return chi_squared < critical_value;
 }
