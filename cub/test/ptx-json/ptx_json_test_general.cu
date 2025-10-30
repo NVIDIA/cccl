91248d6be4470aafd5bc35253e0fa9c6ecb9a516
Michał Dominiak <griwes@griwes.info>
2025-10-30 08:21:53 -1000

    c.parallel: single-stage runtime compilation. (#6341)
    * c.parallel: single-stage runtime compilation.
    
    * Fix the compile flags of the ptx-json test cubins.
    
    * Fix a warning in the ptx-json test.
    
    * Change from .ptx to .cubin in cub test artifacts.
    
    ---------
    
    Co-authored-by: Nader Al Awar <naderalawar@gmail.com>

--- cub/test/ptx-json/ptx_json_test_general.cu
+++ cub/test/ptx-json/ptx_json_test_general.cu
@@ -2,10 +2,10 @@
 
 // CHECK: {"a":1,"b":2,"c":[1,2,"a"]}
 
-__global__ void fn()
+__device__ auto& fn()
 {
-  ptx_json::id<ptx_json::string("test-json-id")>() =
-    ptx_json::object<(ptx_json::key<"a">() = ptx_json::value<1>()),
-                     (ptx_json::key<"b">() = ptx_json::value<2>()),
-                     (ptx_json::key<"c">() = ptx_json::array<1, 2, ptx_json::string("a")>())>();
+  return ptx_json::id<ptx_json::string("test-json-id")>() =
+           ptx_json::object<(ptx_json::key<"a">() = ptx_json::value<1>()),
+                            (ptx_json::key<"b">() = ptx_json::value<2>()),
+                            (ptx_json::key<"c">() = ptx_json::array<1, 2, ptx_json::string("a")>())>();
 }
