3a16ec24a351d1166c078c64f5ada0e2a47bef30
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-10-31 03:09:18 -0700

    More tests

--- libcudacxx/test/libcudacxx/std/random/engine/lcg.pass.cpp
+++ libcudacxx/test/libcudacxx/std/random/engine/lcg.pass.cpp
@@ -12,6 +12,9 @@
 #include <cuda/std/__random_>
 
 #include "test_engine.h"
+#if !_CCCL_COMPILER(NVRTC)
+#  include <random>
+#endif // !_CCCL_COMPILER(NVRTC)
 
 __host__ __device__ void test()
 {
@@ -19,8 +22,45 @@ __host__ __device__ void test()
   test_engine<cuda::std::minstd_rand, 399268537ull>();
 }
 
+#if !_CCCL_COMPILER(NVRTC)
+
+template <typename E1, typename E2>
+void test_std_engines()
+{
+  int seeds[]    = {E1::default_seed, 12345, 67890, 0, 1, 2147483646};
+  int discards[] = {0, 232, 1000};
+  for (auto seed : seeds)
+  {
+    E1 e1(seed);
+    E2 e2(seed);
+    for (auto d : discards)
+    {
+      e1.discard(d);
+      e2.discard(d);
+      for (int i = 0; i < 100; ++i)
+      {
+        auto v1 = e1();
+        auto v2 = e2();
+        assert(v1 == v2);
+      }
+    }
+  }
+}
+
+void test_against_std()
+{
+  test_std_engines<cuda::std::minstd_rand0, std::minstd_rand0>();
+  test_std_engines<cuda::std::minstd_rand, std::minstd_rand>();
+  // 64-bit engine multiplier and increment
+  test_std_engines<cuda::std::linear_congruential_engine<cuda::std::uint64_t, 1ul << 32, 1ul << 31, 1ul << 50>,
+                   std::linear_congruential_engine<std::uint64_t, 1ul << 32, 1ul << 31, 1ul << 50>>();
+}
+
+#endif // !_CCCL_COMPILER(NVRTC
+
 int main(int, char**)
 {
   test();
+  NV_IF_TARGET(NV_IS_HOST, ({ test_against_std(); }));
   return 0;
 }
