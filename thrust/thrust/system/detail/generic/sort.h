1f655b467cfee2a1ac9ae0f7070e7735f4f77fa1
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-10-31 17:53:54 +0100

    Inline *.inl files

--- thrust/thrust/system/detail/generic/sort.h
+++ thrust/thrust/system/detail/generic/sort.h
@@ -25,7 +25,14 @@
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
 #endif // no system header
+#include <thrust/detail/internal_functional.h>
+#include <thrust/distance.h>
+#include <thrust/find.h>
+#include <thrust/functional.h>
+#include <thrust/iterator/iterator_traits.h>
+#include <thrust/iterator/zip_iterator.h>
 #include <thrust/system/detail/generic/tag.h>
+#include <thrust/tuple.h>
 
 THRUST_NAMESPACE_BEGIN
 namespace system::detail::generic
@@ -33,21 +40,33 @@ namespace system::detail::generic
 
 template <typename DerivedPolicy, typename RandomAccessIterator>
 _CCCL_HOST_DEVICE void
-sort(thrust::execution_policy<DerivedPolicy>& exec, RandomAccessIterator first, RandomAccessIterator last);
+sort(thrust::execution_policy<DerivedPolicy>& exec, RandomAccessIterator first, RandomAccessIterator last)
+{
+  using value_type = thrust::detail::it_value_t<RandomAccessIterator>;
+  thrust::sort(exec, first, last, ::cuda::std::less<value_type>());
+} // end sort()
 
 template <typename DerivedPolicy, typename RandomAccessIterator, typename StrictWeakOrdering>
 _CCCL_HOST_DEVICE void
 sort(thrust::execution_policy<DerivedPolicy>& exec,
      RandomAccessIterator first,
      RandomAccessIterator last,
-     StrictWeakOrdering comp);
+     StrictWeakOrdering comp)
+{
+  // implement with stable_sort
+  thrust::stable_sort(exec, first, last, comp);
+} // end sort()
 
 template <typename DerivedPolicy, typename RandomAccessIterator1, typename RandomAccessIterator2>
 _CCCL_HOST_DEVICE void sort_by_key(
   thrust::execution_policy<DerivedPolicy>& exec,
   RandomAccessIterator1 keys_first,
   RandomAccessIterator1 keys_last,
-  RandomAccessIterator2 values_first);
+  RandomAccessIterator2 values_first)
+{
+  using value_type = thrust::detail::it_value_t<RandomAccessIterator1>;
+  thrust::sort_by_key(exec, keys_first, keys_last, values_first, ::cuda::std::less<value_type>());
+} // end sort_by_key()
 
 template <typename DerivedPolicy,
           typename RandomAccessIterator1,
@@ -58,26 +77,35 @@ _CCCL_HOST_DEVICE void sort_by_key(
   RandomAccessIterator1 keys_first,
   RandomAccessIterator1 keys_last,
   RandomAccessIterator2 values_first,
-  StrictWeakOrdering comp);
+  StrictWeakOrdering comp)
+{
+  // implement with stable_sort_by_key
+  thrust::stable_sort_by_key(exec, keys_first, keys_last, values_first, comp);
+} // end sort_by_key()
 
 template <typename DerivedPolicy, typename RandomAccessIterator>
 _CCCL_HOST_DEVICE void
-stable_sort(thrust::execution_policy<DerivedPolicy>& exec, RandomAccessIterator first, RandomAccessIterator last);
+stable_sort(thrust::execution_policy<DerivedPolicy>& exec, RandomAccessIterator first, RandomAccessIterator last)
+{
+  using value_type = thrust::detail::it_value_t<RandomAccessIterator>;
+  thrust::stable_sort(exec, first, last, ::cuda::std::less<value_type>());
+} // end stable_sort()
 
 // XXX it is an error to call this function; it has no implementation
 template <typename DerivedPolicy, typename RandomAccessIterator, typename StrictWeakOrdering>
 _CCCL_HOST_DEVICE void stable_sort(
-  thrust::execution_policy<DerivedPolicy>& exec,
-  RandomAccessIterator first,
-  RandomAccessIterator last,
-  StrictWeakOrdering comp);
+  thrust::execution_policy<DerivedPolicy>&, RandomAccessIterator, RandomAccessIterator, StrictWeakOrdering) = delete;
 
 template <typename DerivedPolicy, typename RandomAccessIterator1, typename RandomAccessIterator2>
 _CCCL_HOST_DEVICE void stable_sort_by_key(
   thrust::execution_policy<DerivedPolicy>& exec,
   RandomAccessIterator1 keys_first,
   RandomAccessIterator1 keys_last,
-  RandomAccessIterator2 values_first);
+  RandomAccessIterator2 values_first)
+{
+  using value_type = thrust::detail::it_value_t<RandomAccessIterator1>;
+  thrust::stable_sort_by_key(exec, keys_first, keys_last, values_first, ::cuda::std::less<value_type>());
+} // end stable_sort_by_key()
 
 // XXX it is an error to call this function; it has no implementation
 template <typename DerivedPolicy,
@@ -85,29 +113,57 @@ template <typename DerivedPolicy,
           typename RandomAccessIterator2,
           typename StrictWeakOrdering>
 _CCCL_HOST_DEVICE void stable_sort_by_key(
-  thrust::execution_policy<DerivedPolicy>& exec,
-  RandomAccessIterator1 keys_first,
-  RandomAccessIterator1 keys_last,
-  RandomAccessIterator2 values_first,
-  StrictWeakOrdering comp);
+  thrust::execution_policy<DerivedPolicy>&,
+  RandomAccessIterator1,
+  RandomAccessIterator1,
+  RandomAccessIterator2,
+  StrictWeakOrdering) = delete;
 
 template <typename DerivedPolicy, typename ForwardIterator>
 _CCCL_HOST_DEVICE bool
-is_sorted(thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last);
+is_sorted(thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last)
+{
+  return thrust::is_sorted_until(exec, first, last) == last;
+} // end is_sorted()
 
 template <typename DerivedPolicy, typename ForwardIterator, typename Compare>
 _CCCL_HOST_DEVICE bool
-is_sorted(thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last, Compare comp);
+is_sorted(thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last, Compare comp)
+{
+  return thrust::is_sorted_until(exec, first, last, comp) == last;
+} // end is_sorted()
 
 template <typename DerivedPolicy, typename ForwardIterator>
 _CCCL_HOST_DEVICE ForwardIterator
-is_sorted_until(thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last);
+is_sorted_until(thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last)
+{
+  using InputType = thrust::detail::it_value_t<ForwardIterator>;
+
+  return thrust::is_sorted_until(exec, first, last, ::cuda::std::less<InputType>());
+} // end is_sorted_until()
 
 template <typename DerivedPolicy, typename ForwardIterator, typename Compare>
 _CCCL_HOST_DEVICE ForwardIterator is_sorted_until(
-  thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last, Compare comp);
+  thrust::execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last, Compare comp)
+{
+  if (::cuda::std::distance(first, last) < 2)
+  {
+    return last;
+  }
+
+  using IteratorTuple = thrust::tuple<ForwardIterator, ForwardIterator>;
+  using ZipIterator   = thrust::zip_iterator<IteratorTuple>;
+
+  ForwardIterator first_plus_one = first;
+  ::cuda::std::advance(first_plus_one, 1);
+
+  ZipIterator zipped_first = thrust::make_zip_iterator(first_plus_one, first);
+  ZipIterator zipped_last  = thrust::make_zip_iterator(last, first);
+
+  return thrust::get<0>(
+    thrust::find_if(exec, zipped_first, zipped_last, thrust::detail::tuple_binary_predicate<Compare>{comp})
+      .get_iterator_tuple());
+} // end is_sorted_until()
 
 } // namespace system::detail::generic
 THRUST_NAMESPACE_END
-
-#include <thrust/system/detail/generic/sort.inl>
