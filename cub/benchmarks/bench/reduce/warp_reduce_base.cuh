d3f1cbd385e2f6988ffae27bb26c64dd24cd3c5e
Federico Busato <50413820+fbusato@users.noreply.github.com>
2025-11-03 13:17:46 -0800

    Update cub/benchmarks/bench/reduce/warp_reduce_base.cuh
    Co-authored-by: Bernhard Manfred Gruber <bernhardmgruber@gmail.com>

--- cub/benchmarks/bench/reduce/warp_reduce_base.cuh
+++ cub/benchmarks/bench/reduce/warp_reduce_base.cuh
@@ -28,11 +28,11 @@ void warp_reduce(nvbench::state& state, nvbench::type_list<T>)
   constexpr int block_size    = 256;
   constexpr int unroll_factor = 128; // compromise between compile time and noise
   const auto& kernel          = benchmark_kernel<block_size, unroll_factor, benchmark_op_t, T>;
-  int num_SMs                 = state.get_device().value().get_number_of_sms();
-  int device                  = state.get_device().value().get_id();
-  int max_blocks_per_SM       = 0;
+  const int num_SMs                 = state.get_device().value().get_number_of_sms();
+  const int device                  = state.get_device().value().get_id();
+  const int max_blocks_per_SM       = 0;
   NVBENCH_CUDA_CALL_NOEXCEPT(cudaOccupancyMaxActiveBlocksPerMultiprocessor(&max_blocks_per_SM, kernel, block_size, 0));
-  int grid_size = max_blocks_per_SM * num_SMs;
+  const int grid_size = max_blocks_per_SM * num_SMs;
   state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch, [&](nvbench::launch&) {
     kernel<<<grid_size, block_size>>>(benchmark_op_t{});
   });
