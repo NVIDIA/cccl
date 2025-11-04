f47882796993ed6c84ecc3ceaefba108048cbf59
fbusato <fbusato@nvidia.com>
2025-11-04 12:26:10 -0800

    fix non-sense complex comparison

--- cub/benchmarks/bench/reduce/warp_reduce_min.cu
+++ cub/benchmarks/bench/reduce/warp_reduce_min.cu
@@ -3,5 +3,22 @@
 
 #include <nvbench_helper.cuh>
 
+// complex types cannot be compared with operator<
+using value_types =
+  nvbench::type_list<int8_t,
+                     int16_t,
+                     int32_t,
+#if NVBENCH_HELPER_HAS_I128
+                     int128_t,
+#endif
+#if _CCCL_HAS_NVFP16()
+                     __half,
+#endif
+#if _CCCL_HAS_NVBF16()
+                     __nv_bfloat16,
+#endif
+                     float,
+                     double>;
+
 using op_t = ::cuda::minimum<>;
 #include "warp_reduce_base.cuh"
