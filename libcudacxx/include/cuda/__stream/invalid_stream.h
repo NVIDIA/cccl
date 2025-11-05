d321143a971006726fcfa859064d63fcadd6df95
David Bayer <bayer.david99@gmail.com>
2025-11-05 13:54:29 +0100

    Implement `cuda::invalid_stream`

--- libcudacxx/include/cuda/__stream/invalid_stream.h
+++ libcudacxx/include/cuda/__stream/invalid_stream.h
@@ -4,12 +4,12 @@
 // under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _CUDA_STREAM
-#define _CUDA_STREAM
+#ifndef _CUDA___STREAM_INVALID_STREAM_H
+#define _CUDA___STREAM_INVALID_STREAM_H
 
 #include <cuda/std/detail/__config>
 
@@ -21,12 +21,27 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/__event/event.h>
-#include <cuda/__event/event_ref.h>
-#include <cuda/__event/timed_event.h>
-#include <cuda/__stream/get_stream.h>
-#include <cuda/__stream/invalid_stream.h>
-#include <cuda/__stream/stream.h>
-#include <cuda/__stream/stream_ref.h>
+#if _CCCL_HAS_CTK()
 
-#endif //_CUDA_STREAM
+#  include <cuda/std/__cccl/prologue.h>
+
+_CCCL_BEGIN_NAMESPACE_CUDA
+
+enum class invalid_stream_t : unsigned char
+{
+};
+
+_CCCL_GLOBAL_CONSTANT invalid_stream_t invalid_stream{};
+
+[[nodiscard]] _CCCL_API _CCCL_FORCEINLINE ::cudaStream_t __invalid_stream() noexcept
+{
+  return reinterpret_cast<::cudaStream_t>(~0ull);
+}
+
+_CCCL_END_NAMESPACE_CUDA
+
+#  include <cuda/std/__cccl/epilogue.h>
+
+#endif // _CCCL_HAS_CTK()
+
+#endif //_CUDA___STREAM_INVALID_STREAM_H
