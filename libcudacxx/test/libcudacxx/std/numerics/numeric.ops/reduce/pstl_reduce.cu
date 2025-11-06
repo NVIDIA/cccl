e0db2bc1bdcc826446c82e8f3506fc3642bc8dd8
Michael Schellenberger Costa <miscco@nvidia.com>
2025-11-06 15:53:33 +0100

    Implement a way to provide a stream through the execution policy

--- libcudacxx/test/libcudacxx/std/numerics/numeric.ops/reduce/pstl_reduce.cu
+++ libcudacxx/test/libcudacxx/std/numerics/numeric.ops/reduce/pstl_reduce.cu
@@ -25,6 +25,7 @@
 #include <cuda/std/__pstl/reduce.h>
 #include <cuda/std/execution>
 #include <cuda/std/functional>
+#include <cuda/stream>
 
 #include <testing.cuh>
 #include <utility.cuh>
@@ -33,28 +34,62 @@ inline constexpr int size = 100;
 
 C2H_TEST("cuda::std::reduce(Iter, Iter)", "[parallel algorithm]")
 {
-  thrust::device_vector<int> data(size);
-  thrust::sequence(data.begin(), data.end(), 1);
+  SECTION("with default stream")
+  {
+    thrust::device_vector<int> data(size);
+    thrust::sequence(data.begin(), data.end(), 1);
 
-  const auto policy  = cuda::execution::__cub_par_unseq;
-  decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end());
-  static_assert(cuda::std::is_same_v<decltype(res), int>);
+    const auto policy  = cuda::execution::__cub_par_unseq;
+    decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end());
+    static_assert(cuda::std::is_same_v<decltype(res), int>);
 
-  constexpr int expected = size * (size + 1) / 2;
-  CHECK(res == expected);
+    constexpr int expected = size * (size + 1) / 2;
+    CHECK(res == expected);
+  }
+
+  SECTION("with provided stream")
+  {
+    thrust::device_vector<int> data(size);
+    thrust::sequence(data.begin(), data.end(), 1);
+
+    ::cuda::stream stream{::cuda::device_ref{0}};
+    const auto policy  = cuda::execution::__cub_par_unseq.set_stream(stream);
+    decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end());
+    static_assert(cuda::std::is_same_v<decltype(res), int>);
+
+    constexpr int expected = size * (size + 1) / 2;
+    CHECK(res == expected);
+  }
 }
 
 C2H_TEST("cuda::std::reduce(Iter, Iter, Tp)", "[parallel algorithm]")
 {
-  thrust::device_vector<int> data(size);
-  thrust::sequence(data.begin(), data.end(), 1);
+  SECTION("with default stream")
+  {
+    thrust::device_vector<int> data(size);
+    thrust::sequence(data.begin(), data.end(), 1);
 
-  const auto policy  = cuda::execution::__cub_par_unseq;
-  decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end(), 42);
-  static_assert(cuda::std::is_same_v<decltype(res), int>);
+    const auto policy  = cuda::execution::__cub_par_unseq;
+    decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end(), 42);
+    static_assert(cuda::std::is_same_v<decltype(res), int>);
 
-  constexpr int expected = size * (size + 1) / 2 + 42;
-  CHECK(res == expected);
+    constexpr int expected = size * (size + 1) / 2 + 42;
+    CHECK(res == expected);
+  }
+
+  SECTION("with provided stream")
+  {
+    thrust::device_vector<int> data(size);
+    thrust::sequence(data.begin(), data.end(), 1);
+
+    ::cuda::stream stream{::cuda::device_ref{0}};
+    const auto policy  = cuda::execution::__cub_par_unseq.set_stream(stream);
+    decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end(), 42);
+    static_assert(cuda::std::is_same_v<decltype(res), int>);
+
+    constexpr int expected = size * (size + 1) / 2 + 42;
+    CHECK(res == expected);
+  }
 }
 
 struct plus_two
@@ -67,13 +102,30 @@ struct plus_two
 
 C2H_TEST("cuda::std::reduce(Iter, Iter, Tp, Fn)", "[parallel algorithm]")
 {
-  thrust::device_vector<int> data(size);
-  thrust::sequence(data.begin(), data.end(), 1);
+  SECTION("with default stream")
+  {
+    thrust::device_vector<int> data(size);
+    thrust::sequence(data.begin(), data.end(), 1);
 
-  const auto policy  = cuda::execution::__cub_par_unseq;
-  decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end(), 42, plus_two{});
-  static_assert(cuda::std::is_same_v<decltype(res), int>);
+    const auto policy  = cuda::execution::__cub_par_unseq;
+    decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end(), 42, plus_two{});
+    static_assert(cuda::std::is_same_v<decltype(res), int>);
 
-  constexpr int expected = size * (size + 1) / 2 + 42 + size * 2;
-  CHECK(res == expected);
+    constexpr int expected = size * (size + 1) / 2 + 42 + size * 2;
+    CHECK(res == expected);
+  }
+
+  SECTION("with provided stream")
+  {
+    thrust::device_vector<int> data(size);
+    thrust::sequence(data.begin(), data.end(), 1);
+
+    ::cuda::stream stream{::cuda::device_ref{0}};
+    const auto policy  = cuda::execution::__cub_par_unseq.set_stream(stream);
+    decltype(auto) res = cuda::std::reduce(policy, data.begin(), data.end(), 42, plus_two{});
+    static_assert(cuda::std::is_same_v<decltype(res), int>);
+
+    constexpr int expected = size * (size + 1) / 2 + 42 + size * 2;
+    CHECK(res == expected);
+  }
 }
