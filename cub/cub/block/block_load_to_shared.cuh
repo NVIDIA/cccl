1dd971886dc64a05b0f2c16699e1e9a919aca748
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-06 11:56:57 +0100

    Add BlockLoadToShared improvements
    * Require CommitToken to expire when calling Wait()
    * Add convenience function CommitAndWait (similar to cuda::barrier::arrive_and_wait())

--- cub/cub/block/block_load_to_shared.cuh
+++ cub/cub/block/block_load_to_shared.cuh
@@ -376,7 +376,7 @@ public:
 
   //! @brief Wait for previously committed copies to arrive. Prepare for next
   //! calls to @c CopyAsync() .
-  _CCCL_DEVICE _CCCL_FORCEINLINE void Wait(const CommitToken&)
+  _CCCL_DEVICE _CCCL_FORCEINLINE void Wait(CommitToken&&)
   {
 #ifdef CCCL_ENABLE_DEVICE_ASSERTIONS
     _CCCL_ASSERT(state == State::committed, "Commit() must be called before Wait()");
@@ -386,6 +386,12 @@ public:
     while (!__try_wait())
       ;
     phase_parity ^= 1u;
+  }
+
+  //! @brief Convenience overload calling `Commit()` and `Wait()`.
+  _CCCL_DEVICE _CCCL_FORCEINLINE void CommitAndWait()
+  {
+    Wait(Commit());
   }
 
   // Having these as static members does require using "template" in user code which is kludgy.
