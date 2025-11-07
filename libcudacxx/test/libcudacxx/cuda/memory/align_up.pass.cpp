0b225f5a1391d2e6ce5f8ba26d87ed3718149636
fbusato <fbusato@nvidia.com>
2025-11-06 16:04:53 -0800

    Ensure align_down and align_up return pointers in the same memory space of input, while keeping optimal code

--- libcudacxx/test/libcudacxx/cuda/memory/align_up.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/memory/align_up.pass.cpp
@@ -12,27 +12,38 @@
 #include <cuda/std/cassert>
 #include <cuda/std/cstdint>
 
-template <typename T, typename U>
+template <typename T>
+__host__ __device__ void test_impl()
+{
+  if constexpr (alignof(T) <= 2)
+  {
+    uintptr_t ptr_int = 10;
+    auto ptr          = reinterpret_cast<T>(ptr_int);
+    assert(cuda::align_up(ptr, 1) == ptr);
+    assert(cuda::align_up(ptr, 2) == ptr);
+    assert(cuda::align_up(ptr, 4) == reinterpret_cast<T>(12));
+    assert(cuda::align_up(ptr, 8) == reinterpret_cast<T>(16));
+  }
+  uintptr_t ptr_int2 = 12;
+  auto ptr2          = reinterpret_cast<T>(ptr_int2);
+  assert(cuda::align_up(ptr2, 8) == reinterpret_cast<T>(16));
+}
+
+template <typename T>
 __host__ __device__ void test()
 {
-  uintptr_t ptr_int = 10;
-  auto ptr          = reinterpret_cast<T>(ptr_int);
-  assert(cuda::align_up(ptr, 1) == ptr);
-  assert(cuda::align_up(ptr, 2) == ptr);
-  assert(cuda::align_up(ptr, 4) == reinterpret_cast<T>(12));
-  assert(cuda::align_up(ptr, 8) == reinterpret_cast<T>(16));
-  uintptr_t ptr_int2 = 12;
-  auto ptr2          = reinterpret_cast<U>(ptr_int2);
-  assert(cuda::align_up(ptr2, 8) == reinterpret_cast<U>(16));
+  test_impl<T*>();
+  test_impl<const T*>();
+  test_impl<volatile T*>();
+  test_impl<const volatile T*>();
 }
 
 __host__ __device__ bool test()
 {
-  test<char*, int*>();
-  test<const char*, const int*>();
-  test<volatile char*, volatile int*>();
-  test<const volatile char*, const volatile int*>();
-  test<void*, void*>();
+  test<char>();
+  test<short>();
+  test<int>();
+  test<void>();
   return true;
 }
 
