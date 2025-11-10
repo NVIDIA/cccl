894f05ce40643f0319f8830c13dab13936fe2224
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2025-11-10 13:56:21 -0500

    [Thrust]: Use cuda::counting_iterator and thrust::tabulate in sum rows/columns examples to follow modern best practices.

--- thrust/examples/sum_columns.cu
+++ thrust/examples/sum_columns.cu
@@ -5,9 +5,10 @@
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/random.h>
 #include <thrust/reduce.h>
-#include <thrust/transform.h>
+#include <thrust/tabulate.h>
 #include <thrust/universal_vector.h>
 
+#include <cuda/iterator>
 #include <cuda/std/mdspan>
 
 #include <iomanip>
@@ -24,12 +25,11 @@ int main()
   thrust::universal_vector<int> data(rows * cols, 42);
   cuda::std::mdspan M(thrust::raw_pointer_cast(data.data()), rows, cols);
 
-  // Create a range to the flat linear index space for the multidimensional array.
-  auto flat_idx_begin = thrust::make_counting_iterator(0);
-  auto flat_idx_end   = flat_idx_begin + M.size();
+  // Create an iterator to the flat linear index space for the multidimensional array.
+  auto flat_idx = cuda::counting_iterator(0);
 
   // Fill the array with pseudorandom inputs in parallel on the device.
-  thrust::transform(thrust::device, flat_idx_begin, flat_idx_end, M.data_handle(), [] __host__ __device__(int flat) {
+  thrust::tabulate(thrust::device, M.data_handle(), M.data_handle() + M.size(), [] __host__ __device__(int flat) {
     thrust::default_random_engine rng;
     thrust::uniform_int_distribution<int> dist(0, 3);
     rng.discard(flat); // Advance to the current element's position.
@@ -37,15 +37,14 @@ int main()
   });
 
   // Create a range to the column index of each element.
-  auto col_idx_begin = thrust::make_transform_iterator(flat_idx_begin, [=] __host__ __device__(int flat) {
+  auto col_idx_begin = thrust::make_transform_iterator(flat_idx, [=] __host__ __device__(int flat) {
     return flat / rows;
   });
   auto col_idx_end   = col_idx_begin + M.size();
 
   // Create a transposed view of the multidimensional array.
   auto M_transposed = thrust::make_permutation_iterator(
-    M.data_handle(),
-    thrust::make_transform_iterator(thrust::make_counting_iterator(0), [=] __host__ __device__(int flat) {
+    M.data_handle(), thrust::make_transform_iterator(cuda::counting_iterator(0), [=] __host__ __device__(int flat) {
       int i = flat / cols;
       int j = flat % cols;
       return i + j * rows;
@@ -57,22 +56,22 @@ int main()
     thrust::device, col_idx_begin, col_idx_end, M_transposed, thrust::make_discard_iterator(), sums.begin());
 
   // Output the result.
-  thrust::for_each_n(thrust::seq, flat_idx_begin, rows, [&](int i) {
+  thrust::for_each_n(thrust::seq, flat_idx, rows, [&](int i) {
     std::cout << "[ ";
-    thrust::for_each_n(thrust::seq, flat_idx_begin, cols, [&](int j) {
+    thrust::for_each_n(thrust::seq, flat_idx, cols, [&](int j) {
       std::cout << std::setw(2) << M(i, j) << " ";
     });
     std::cout << "]\n";
   });
 
   std::cout << "  ";
-  thrust::for_each_n(thrust::seq, flat_idx_begin, cols, [&](int) {
+  thrust::for_each_n(thrust::seq, flat_idx, cols, [&](int) {
     std::cout << " = ";
   });
   std::cout << "\n";
 
   std::cout << "  ";
-  thrust::for_each_n(thrust::seq, flat_idx_begin, cols, [&](int j) {
+  thrust::for_each_n(thrust::seq, flat_idx, cols, [&](int j) {
     std::cout << std::setw(2) << sums[j] << " ";
   });
   std::cout << "\n";
--- thrust/examples/sum_rows.cu
+++ thrust/examples/sum_rows.cu
@@ -5,9 +5,10 @@
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/random.h>
 #include <thrust/reduce.h>
-#include <thrust/transform.h>
+#include <thrust/tabulate.h>
 #include <thrust/universal_vector.h>
 
+#include <cuda/iterator>
 #include <cuda/std/mdspan>
 
 #include <iomanip>
@@ -24,12 +25,11 @@ int main()
   thrust::universal_vector<int> data(rows * cols, 42);
   cuda::std::mdspan M(thrust::raw_pointer_cast(data.data()), rows, cols);
 
-  // Create a range to the flat linear index space for the multidimensional array.
-  auto flat_idx_begin = thrust::make_counting_iterator(0);
-  auto flat_idx_end   = flat_idx_begin + M.size();
+  // Create an iterator to the flat linear index space for the multidimensional array.
+  auto flat_idx = cuda::counting_iterator(0);
 
   // Fill the array with pseudorandom inputs in parallel on the device.
-  thrust::transform(thrust::device, flat_idx_begin, flat_idx_end, M.data_handle(), [] __host__ __device__(int flat) {
+  thrust::tabulate(thrust::device, M.data_handle(), M.data_handle() + M.size(), [] __host__ __device__(int flat) {
     thrust::default_random_engine rng;
     thrust::uniform_int_distribution<int> dist(0, 3);
     rng.discard(flat); // Advance to the current element's position.
@@ -37,7 +37,7 @@ int main()
   });
 
   // Create a range to the row index of each element.
-  auto row_idx_begin = thrust::make_transform_iterator(flat_idx_begin, [=] __host__ __device__(int flat) {
+  auto row_idx_begin = thrust::make_transform_iterator(flat_idx, [=] __host__ __device__(int flat) {
     return flat / cols;
   });
   auto row_idx_end   = row_idx_begin + M.size();
@@ -48,9 +48,9 @@ int main()
     thrust::device, row_idx_begin, row_idx_end, M.data_handle(), thrust::make_discard_iterator(), sums.begin());
 
   // Output the result.
-  thrust::for_each_n(thrust::seq, flat_idx_begin, rows, [&](int i) {
+  thrust::for_each_n(thrust::seq, flat_idx, rows, [&](int i) {
     std::cout << "[ ";
-    thrust::for_each_n(thrust::seq, flat_idx_begin, cols, [&](int j) {
+    thrust::for_each_n(thrust::seq, flat_idx, cols, [&](int j) {
       std::cout << std::setw(2) << M(i, j) << " ";
     });
     std::cout << "] = " << sums[i] << "\n";
