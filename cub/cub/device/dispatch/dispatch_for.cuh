7396cec2bd5eb9d68979a75358c9ec0bbbde37f9
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-11 13:05:42 +0100

    Refactor cub::detail::for_each::dispatch_t

--- cub/cub/device/dispatch/dispatch_for.cuh
+++ cub/cub/device/dispatch/dispatch_for.cuh
@@ -45,29 +45,20 @@ struct dispatch_t
   {}
 
   template <typename ActivePolicyT>
-  CUB_RUNTIME_FUNCTION
-  _CCCL_FORCEINLINE cudaError_t Invoke(::cuda::std::false_type /* block size is not known at compile time */)
+  CUB_RUNTIME_FUNCTION _CCCL_FORCEINLINE cudaError_t invoke_dynamic_block_size()
   {
-    using max_policy_t = typename PolicyHubT::MaxPolicy;
-
     if (num_items == 0)
     {
       return cudaSuccess;
     }
 
     int block_threads = 256;
-    cudaError_t error = cudaSuccess;
-
+    auto kernel       = detail::for_each::dynamic_kernel<typename PolicyHubT::MaxPolicy, OffsetT, OpT>;
     NV_IF_TARGET(NV_IS_HOST,
                  (int _{}; //
-                  error = cudaOccupancyMaxPotentialBlockSize(
-                    &_, &block_threads, detail::for_each::dynamic_kernel<max_policy_t, OffsetT, OpT>);));
-
-    error = CubDebug(error);
-    if (cudaSuccess != error)
-    {
-      return error;
-    }
+                  if (const auto error = CubDebug(cudaOccupancyMaxPotentialBlockSize(&_, &block_threads, kernel))) {
+                    return error;
+                  }));
 
     constexpr int items_per_thread = ActivePolicyT::for_policy_t::items_per_thread;
 
@@ -83,39 +74,35 @@ struct dispatch_t
             static_cast<int>(items_per_thread));
 #endif
 
-    error = THRUST_NS_QUALIFIER::cuda_cub::detail::triple_chevron(
-              static_cast<unsigned int>(num_tiles), static_cast<unsigned int>(block_threads), 0, stream)
-              .doit(detail::for_each::dynamic_kernel<max_policy_t, OffsetT, OpT>, num_items, op);
-    error = CubDebug(error);
-    if (cudaSuccess != error)
+    if (const auto error = CubDebug(
+          THRUST_NS_QUALIFIER::cuda_cub::detail::triple_chevron(
+            static_cast<unsigned int>(num_tiles), static_cast<unsigned int>(block_threads), 0, stream)
+            .doit(kernel, num_items, op)))
     {
       return error;
     }
 
-    error = CubDebug(detail::DebugSyncStream(stream));
-    if (cudaSuccess != error)
+    if (auto error = CubDebug(detail::DebugSyncStream(stream)))
     {
-      CubDebug(error = SyncStream(stream));
+      CubDebug(error = SyncStream(stream)); // TODO(bgruber): this does not make sense to me
+      return error;
     }
 
-    return error;
+    return cudaSuccess;
   }
 
   template <typename ActivePolicyT>
-  CUB_RUNTIME_FUNCTION
-  _CCCL_FORCEINLINE cudaError_t Invoke(::cuda::std::true_type /* block size is known at compile time */)
+  CUB_RUNTIME_FUNCTION _CCCL_FORCEINLINE cudaError_t invoke_static_block_size()
   {
     if (num_items == 0)
     {
       return cudaSuccess;
     }
 
-    cudaError_t error               = cudaSuccess;
     constexpr int block_threads     = ActivePolicyT::for_policy_t::block_threads;
     constexpr int items_per_thread1 = ActivePolicyT::for_policy_t::items_per_thread;
-
-    const auto tile_size = static_cast<OffsetT>(block_threads * items_per_thread1);
-    const auto num_tiles = ::cuda::ceil_div(num_items, tile_size);
+    constexpr auto tile_size        = static_cast<OffsetT>(block_threads * items_per_thread1);
+    const auto num_tiles            = ::cuda::ceil_div(num_items, tile_size);
 
 #ifdef CUB_DEBUG_LOG
     _CubLog("Invoking detail::for_each::static_kernel<<<%d, %d, 0, %lld>>>(), "
@@ -126,45 +113,46 @@ struct dispatch_t
             static_cast<int>(items_per_thread1));
 #endif
 
-    error = THRUST_NS_QUALIFIER::cuda_cub::detail::triple_chevron(
-              static_cast<unsigned int>(num_tiles), static_cast<unsigned int>(block_threads), 0, stream)
-              .doit(detail::for_each::static_kernel<typename PolicyHubT::MaxPolicy, OffsetT, OpT>, num_items, op);
-    error = CubDebug(error);
-    if (cudaSuccess != error)
+    if (const auto error = CubDebug(
+          THRUST_NS_QUALIFIER::cuda_cub::detail::triple_chevron(
+            static_cast<unsigned int>(num_tiles), static_cast<unsigned int>(block_threads), 0, stream)
+            .doit(detail::for_each::static_kernel<typename PolicyHubT::MaxPolicy, OffsetT, OpT>, num_items, op)))
     {
       return error;
     }
 
-    error = CubDebug(detail::DebugSyncStream(stream));
-    if (cudaSuccess != error)
+    if (auto error = CubDebug(detail::DebugSyncStream(stream)))
     {
-      CubDebug(error = SyncStream(stream));
+      CubDebug(error = SyncStream(stream)); // TODO(bgruber): this does not make sense to me
+      return error;
     }
 
-    return error;
+    return cudaSuccess;
   }
 
   template <typename ActivePolicyT>
   CUB_RUNTIME_FUNCTION _CCCL_FORCEINLINE cudaError_t Invoke()
   {
-    constexpr bool static_block_size = ActivePolicyT::for_policy_t::block_threads > 0;
-    return Invoke<ActivePolicyT>(::cuda::std::integral_constant<bool, static_block_size>{});
+    if constexpr (ActivePolicyT::for_policy_t::block_threads > 0)
+    {
+      return invoke_static_block_size<ActivePolicyT>();
+    }
+    else
+    {
+      return invoke_dynamic_block_size<ActivePolicyT>();
+    }
   }
 
   CUB_RUNTIME_FUNCTION _CCCL_FORCEINLINE static cudaError_t dispatch(OffsetT num_items, OpT op, cudaStream_t stream)
   {
-    int ptx_version   = 0;
-    cudaError_t error = CubDebug(PtxVersion(ptx_version));
-    if (cudaSuccess != error)
+    int ptx_version = 0;
+    if (const auto error = CubDebug(PtxVersion(ptx_version)))
     {
       return error;
     }
 
     dispatch_t dispatch(num_items, op, stream);
-
-    error = CubDebug(PolicyHubT::MaxPolicy::Invoke(ptx_version, dispatch));
-
-    return error;
+    return CubDebug(PolicyHubT::MaxPolicy::Invoke(ptx_version, dispatch));
   }
 };
 } // namespace detail::for_each
