6857a76bbf4449466070beed3c7669a74d415928
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-12 08:18:46 +0100

    Fix

--- thrust/testing/mr_pool.cu
+++ thrust/testing/mr_pool.cu
@@ -109,7 +109,7 @@ public:
 
   tracked_pointer<void> do_allocate(std::size_t n, std::size_t alignment = THRUST_MR_DEFAULT_ALIGNMENT) override
   {
-    ASSERT_EQUAL(id_to_allocate || id_to_allocate == -1u, true);
+    REQUIRE((id_to_allocate || id_to_allocate == -1u));
 
     void* raw = upstream.do_allocate(n, alignment);
     tracked_pointer<void> ret(raw);
@@ -351,7 +351,7 @@ void TestGlobalPool()
 {
   using Pool = PoolTemplate<thrust::mr::new_delete_resource>;
 
-  ASSERT_EQUAL(thrust::mr::get_global_resource<Pool>() != nullptr, true);
+  REQUIRE(thrust::mr::get_global_resource<Pool>() != nullptr);
 }
 
 void TestUnsynchronizedGlobalPool()
