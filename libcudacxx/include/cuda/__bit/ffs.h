e855ce70a6b7f8ba81cdc17eb010d78e6712bc6c
Amin Sedaghat <amin32846@gmail.com>
2025-11-15 21:16:41 -0500

    Format barrier/ffs per clang-format

--- libcudacxx/include/cuda/__bit/ffs.h
+++ libcudacxx/include/cuda/__bit/ffs.h
@@ -149,26 +149,23 @@ template <class _Tp, ::cuda::std::enable_if_t<::cuda::std::__cccl_is_cv_unsigned
 #if _CCCL_HAS_INT128()
   if constexpr (sizeof(_Unsigned) == sizeof(__uint128_t))
   {
-    NV_IF_ELSE_TARGET(
-      NV_IS_HOST,
-      (return __detail::__ffs_host128(static_cast<__uint128_t>(__v));),
-      (return __detail::__ffs_device128(static_cast<__uint128_t>(__v));))
+    NV_IF_ELSE_TARGET(NV_IS_HOST,
+                      (return __detail::__ffs_host128(static_cast<__uint128_t>(__v));),
+                      (return __detail::__ffs_device128(static_cast<__uint128_t>(__v));))
   }
 #endif // _CCCL_HAS_INT128()
 
   if constexpr (sizeof(_Unsigned) <= sizeof(::cuda::std::uint32_t))
   {
-    NV_IF_ELSE_TARGET(
-      NV_IS_HOST,
-      (return __detail::__ffs_host32(static_cast<::cuda::std::uint32_t>(__v));),
-      (return __detail::__ffs_device32(static_cast<::cuda::std::uint32_t>(__v));))
+    NV_IF_ELSE_TARGET(NV_IS_HOST,
+                      (return __detail::__ffs_host32(static_cast<::cuda::std::uint32_t>(__v));),
+                      (return __detail::__ffs_device32(static_cast<::cuda::std::uint32_t>(__v));))
   }
   else
   {
-    NV_IF_ELSE_TARGET(
-      NV_IS_HOST,
-      (return __detail::__ffs_host64(static_cast<::cuda::std::uint64_t>(__v));),
-      (return __detail::__ffs_device64(static_cast<::cuda::std::uint64_t>(__v));))
+    NV_IF_ELSE_TARGET(NV_IS_HOST,
+                      (return __detail::__ffs_host64(static_cast<::cuda::std::uint64_t>(__v));),
+                      (return __detail::__ffs_device64(static_cast<::cuda::std::uint64_t>(__v));))
   }
 }
 
