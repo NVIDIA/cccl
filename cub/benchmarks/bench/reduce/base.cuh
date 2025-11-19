8c60dc85c94798334156ef62d8a7966169464568
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-19 21:55:48 +0100

    use correct memory resource

--- cub/benchmarks/bench/reduce/base.cuh
+++ cub/benchmarks/bench/reduce/base.cuh
@@ -5,6 +5,9 @@
 
 #include <cub/device/device_reduce.cuh>
 
+#include <cuda/__device/all_devices.h>
+#include <cuda/__memory_resource/device_memory_pool.h>
+
 #include <nvbench_helper.cuh>
 
 #if !TUNE_BASE
@@ -22,87 +25,6 @@ struct arch_policies
   }
 };
 #endif // !TUNE_BASE
-
-#if 0
-struct caching_last_alloc_mr
-{
-  void* last_ptr = nullptr;
-  ::cuda::stream_ref last_stream;
-  size_t last_size;
-  bool in_use = false;
-
-  caching_last_alloc_mr() = default;
-
-  caching_last_alloc_mr(const caching_last_alloc_mr& other)
-  {
-    if (other.last_ptr)
-    {
-      throw std::runtime_error("Copying caching_last_alloc_mr after first allocation is not implemented");
-    }
-  }
-
-  caching_last_alloc_mr& operator=(const caching_last_alloc_mr&) = delete;
-
-  ~caching_last_alloc_mr()
-  {
-    if (last_ptr)
-    {
-      _CCCL_TRY_CUDA_API(::cudaFreeAsync, "cudaFreeAsync failed", last_ptr, last_stream.get());
-    }
-  }
-
-  void* allocate(::cuda::stream_ref stream, size_t size, size_t alignment = 1)
-  {
-    if (last_ptr && (stream != last_stream || size != last_size))
-    {
-      throw std::runtime_error("Cannot allocate again with a different stream or size");
-    }
-    if (in_use)
-    {
-      throw std::runtime_error("Cannot allocate again before deallocating");
-    }
-    _CCCL_TRY_CUDA_API(::cudaMallocAsync, "cudaMallocAsync failed", &last_ptr, size, stream.get());
-    _CCCL_ASSERT(cuda::is_aligned(last_ptr, alignment), "");
-    last_stream = stream;
-    last_size   = size;
-    in_use      = true;
-    return last_ptr;
-  }
-
-  void deallocate(::cuda::stream_ref /* stream */, void* /* ptr */, size_t /* size */, size_t /* alignment */ = 1)
-  {
-    in_use = false;
-  }
-
-  void* allocate_sync(size_t /* size */, size_t /* alignment */)
-  {
-    throw std::runtime_error("Not implemented");
-  }
-
-  void deallocate_sync(void* /* ptr */, size_t /* size */, size_t /* alignment */)
-  {
-    throw std::runtime_error("Not implemented");
-  }
-
-  friend auto operator==(const caching_last_alloc_mr& a, const caching_last_alloc_mr& b) -> bool
-  {
-    return a.last_ptr == b.last_ptr;
-  }
-
-  friend auto operator!=(const caching_last_alloc_mr& a, const caching_last_alloc_mr& b) -> bool
-  {
-    return !(a == b);
-  }
-
-  // to support querying when used as environment
-  auto get_memory_resource() const -> caching_last_alloc_mr
-  {
-    return {};
-  }
-};
-
-static_assert(cuda::mr::resource<caching_last_alloc_mr>);
-#endif
 
 template <typename T, typename OffsetT>
 void reduce(nvbench::state& state, nvbench::type_list<T, OffsetT>)
@@ -125,12 +47,13 @@ void reduce(nvbench::state& state, nvbench::type_list<T, OffsetT>)
   state.add_global_memory_writes<T>(1);
 
   // FIXME(bgruber): the previous implementation did target cub::DispatchReduce, and provided T as accumulator type.
-  // This is not realistic, since a user cannot set the accumulator type the same way at the public API. For example,
-  // reducing I8 over cuda::std::plus deduces accumulator type I32 at the public API, but the benchmark forces it to I8.
-  // This skews the MemBoundScaling, leading to 20% regression for the same tuning when the public API is called (with
-  // accum_t I32) over the benchmark (forced accum_t of I8). See also: https://github.com/NVIDIA/cccl/issues/6576
+  // This is not realistic, since a user cannot override the accumulator type the same way at the public API. For
+  // example, reducing I8 over cuda::std::plus deduces accumulator type I32 at the public API, but the benchmark forces
+  // it to I8. This skews the MemBoundScaling, leading to 20% regression for the same tuning when the public API is
+  // called (with accum_t I32) over the benchmark (forced accum_t of I8). See also:
+  // https://github.com/NVIDIA/cccl/issues/6576
 #if 0
-  caching_last_alloc_mr mr;
+  auto mr = cuda::device_default_memory_pool(cuda::devices[0]);
   state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch, [&](nvbench::launch& launch) {
     auto env = ::cuda::std::execution::env{
       ::cuda::stream_ref{launch.get_stream().get_stream()},
