dfd4222d84433afa776dac4fdbfaecc39203daa8
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-20 01:01:27 +0100

    Try to fix python for old GPUs

--- cub/cub/device/dispatch/tuning/tuning_transform.cuh
+++ cub/cub/device/dispatch/tuning/tuning_transform.cuh
@@ -372,7 +372,7 @@ struct policy_hub<RequiresStableAddress,
     using prefetch_policy        = prefetch_policy_t<256>;
     using vectorized_policy      = vectorized_policy_t<
            tuning_vec<500, size_of<it_value_t<RandomAccessIteratorOut>>, sizeof(it_value_t<RandomAccessIteratorsIn>)...>>;
-    using async_policy              = void;
+    using async_policy              = async_copy_policy_t<256, 16>; // dummy policy, never used
     static constexpr auto algorithm = fallback_to_prefetch ? Algorithm::prefetch : Algorithm::vectorized;
   };
 
