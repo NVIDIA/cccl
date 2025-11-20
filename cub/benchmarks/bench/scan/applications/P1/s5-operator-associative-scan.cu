95cde0d70709ec8b63acf41ec33c375a8784c8f3
Nader Al Awar <naderalawar@gmail.com>
2025-11-19 19:16:24 -0600

    Implement segmented s5 scan

--- cub/benchmarks/bench/scan/applications/P1/s5-operator-associative-scan.cu
+++ cub/benchmarks/bench/scan/applications/P1/s5-operator-associative-scan.cu
@@ -3,8 +3,10 @@
 
 #include <cub/detail/choose_offset.cuh>
 #include <cub/device/device_scan.cuh>
+#include <cub/device/device_segmented_scan.cuh>
 
 #include <thrust/device_vector.h>
+#include <thrust/sequence.h>
 
 #if _CCCL_HAS_NVFP16()
 #  include <cuda_fp16.h>
@@ -58,22 +60,6 @@ struct policy_hub_t
 
 namespace impl
 {
-/* S5 associative scan: processes pairs of vectors with elementwise operations.
- * Used in state-space models where each timestep processes vectors of state dimension.
- * Original benchmark here https://github.com/pytorch/pytorch/pull/164859.
- *
- * Given input pairs (A_i, Bu_i) at each timestep i, computes:
- *    result.A[k]  = y.A[k] * x.A[k]
- *    result.Bu[k] = y.A[k] * x.Bu[k] + y.Bu[k]
- * for each element k in the state vector.
- *
- * The operator is associative but operates on vectors rather than scalars.
- *
- * Iterator Composition:
- *   INPUT:  counting_iterator -> IndexToPointerFunctor -> zip_iterator -> LoadRowFromPointersFunctor -> VectorPair
- *   OUTPUT: counting_iterator -> IndexToPointerFunctor -> zip_iterator -> PointersToWriteProxyFunctor -> WriteProxy
- */
-
 template <typename T>
 thrust::device_vector<T> generate_data(std::size_t n)
 {
@@ -95,6 +81,23 @@ thrust::device_vector<T> generate_data(std::size_t n)
   }
 }
 
+#if S5_NORMAL_SCAN
+/* S5 associative scan: processes pairs of vectors with elementwise operations.
+ * Used in state-space models where each timestep processes vectors of state dimension.
+ * Original benchmark here https://github.com/pytorch/pytorch/pull/164859.
+ *
+ * Given input pairs (A_i, Bu_i) at each timestep i, computes:
+ *    result.A[k]  = y.A[k] * x.A[k]
+ *    result.Bu[k] = y.A[k] * x.Bu[k] + y.Bu[k]
+ * for each element k in the state vector.
+ *
+ * The operator is associative but operates on vectors rather than scalars.
+ *
+ * Iterator Composition:
+ *   INPUT:  counting_iterator -> IndexToPointerFunctor -> zip_iterator -> LoadRowFromPointersFunctor -> VectorPair
+ *   OUTPUT: counting_iterator -> IndexToPointerFunctor -> zip_iterator -> PointersToWriteProxyFunctor -> WriteProxy
+ */
+
 template <typename T, int StateDim>
 using vector_pair_t = cuda::std::pair<cuda::std::array<T, StateDim>, cuda::std::array<T, StateDim>>;
 
@@ -112,7 +115,7 @@ struct s5_op
 
     auto& [A, Bu] = result;
 
-#pragma unroll
+#  pragma unroll
     for (int i = 0; i < StateDim; i++)
     {
       const auto y_A_i = y_A[i];
@@ -135,7 +138,7 @@ struct load_row_functor
     auto a_ptr            = thrust::get<0>(ptrs);
     auto bu_ptr           = thrust::get<1>(ptrs);
 
-#pragma unroll
+#  pragma unroll
     for (int i = 0; i < StateDim; i++)
     {
       a_arr[i]  = a_ptr[i];
@@ -173,7 +176,7 @@ struct vector_pair_write_proxy
   {
     const auto& [A, Bu] = val;
 
-#pragma unroll
+#  pragma unroll
     for (int i = 0; i < StateDim; i++)
     {
       a_row[i]  = A[i];
@@ -192,8 +195,37 @@ struct pointers_to_write_proxy_functor
     return {thrust::get<0>(ptrs), thrust::get<1>(ptrs)};
   }
 };
+
+#endif
+
+template <typename T>
+struct s5_op_segmented
+{
+  __host__ __device__ cuda::std::tuple<T, T> operator()(cuda::std::tuple<T, T> x, cuda::std::tuple<T, T> y) const
+  {
+    const auto& [x_A, x_Bu] = x;
+    const auto& [y_A, y_Bu] = y;
+
+    return {y_A * x_A, y_A * x_Bu + y_Bu};
+  }
+};
+
+// Helper struct to hold the lambda functors outside the function
+struct column_major_transform
+{
+  int nrows;
+  int ncols;
+
+  __host__ __device__ int operator()(int k) const
+  {
+    int row = k % nrows;
+    int col = k / nrows;
+    return row * ncols + col;
+  }
+};
 }; // namespace impl
 
+#if S5_NORMAL_SCAN
 template <typename T, typename OffsetT>
 static void inclusive_scan(nvbench::state& state, nvbench::type_list<T, OffsetT>)
 {
@@ -220,14 +252,14 @@ static void inclusive_scan(nvbench::state& state, nvbench::type_list<T, OffsetT>
   using output_it_t =
     cuda::transform_iterator<impl::pointers_to_write_proxy_functor<value_t, state_dim>, zipped_output_ptrs_t>;
 
-#if !TUNE_BASE
+#  if !TUNE_BASE
   using policy_t   = policy_hub_t<accum_t>;
   using dispatch_t = cub::
     DispatchScan<input_it_t, output_it_t, op_t, wrapped_init_t, offset_t, accum_t, cub::ForceInclusive::No, policy_t>;
-#else
+#  else
   using dispatch_t =
     cub::DispatchScan<input_it_t, output_it_t, op_t, wrapped_init_t, offset_t, accum_t, cub::ForceInclusive::No>;
-#endif
+#  endif
 
   const auto elements       = static_cast<std::size_t>(state.get_int64("Elements{io}"));
   const auto total_size     = elements * state_dim;
@@ -274,6 +306,124 @@ static void inclusive_scan(nvbench::state& state, nvbench::type_list<T, OffsetT>
   });
 }
 
+#endif
+
+template <typename T, typename OffsetT>
+static void segmented_scan(nvbench::state& state, nvbench::type_list<T, OffsetT>)
+{
+  using wrapped_init_t      = cub::NullType;
+  constexpr int state_dim   = 40;
+  using value_t             = T;
+  using op_t                = impl::s5_op_segmented<value_t>;
+  using transformed_input_t = cuda::transform_iterator<impl::column_major_transform, cuda::counting_iterator<int>>;
+  using permuted_input_t    = cuda::permutation_iterator<value_t*, transformed_input_t>;
+  using permuted_output_t   = cuda::permutation_iterator<value_t*, transformed_input_t>;
+
+  using input_it_t  = cuda::zip_iterator<permuted_input_t, permuted_input_t>;
+  using output_it_t = cuda::zip_iterator<permuted_output_t, permuted_output_t>;
+
+  using begin_offset_it_t = int*;
+  using end_offset_it_t   = int*;
+
+  using accum_t  = cuda::std::tuple<value_t, value_t>;
+  using offset_t = cub::detail::choose_offset_t<OffsetT>;
+
+#if !TUNE_BASE
+  using policy_t   = policy_hub_t<accum_t>;
+  using dispatch_t = cub::detail::segmented_scan::dispatch_segmented_scan<
+    input_it_t,
+    output_it_t,
+    begin_offset_it_t,
+    end_offset_it_t,
+    begin_offset_it_t,
+    op_t,
+    wrapped_init_t,
+    accum_t,
+    cub::ForceInclusive::No,
+    offset_t,
+    policy_t>;
+#else
+  using dispatch_t = cub::detail::segmented_scan::dispatch_segmented_scan<
+    input_it_t,
+    output_it_t,
+    begin_offset_it_t,
+    end_offset_it_t,
+    begin_offset_it_t,
+    op_t,
+    wrapped_init_t,
+    accum_t,
+    cub::ForceInclusive::No,
+    offset_t>;
+#endif
+
+  const auto elements       = static_cast<std::size_t>(state.get_int64("Elements{io}"));
+  const auto total_size     = elements * state_dim;
+  cudaStream_t bench_stream = state.get_cuda_stream().get_stream();
+
+  thrust::device_vector<value_t> input_A  = impl::generate_data<value_t>(total_size);
+  thrust::device_vector<value_t> input_Bu = impl::generate_data<value_t>(total_size);
+  thrust::device_vector<value_t> output_A(total_size, thrust::no_init);
+  thrust::device_vector<value_t> output_Bu(total_size, thrust::no_init);
+
+  const int nrows = elements;
+  const int ncols = state_dim;
+
+  auto col_major_iter =
+    cuda::make_transform_iterator(cuda::make_counting_iterator(0), impl::column_major_transform{nrows, ncols});
+
+  auto A_in_iter  = cuda::make_permutation_iterator(thrust::raw_pointer_cast(input_A.data()), col_major_iter);
+  auto Bu_in_iter = cuda::make_permutation_iterator(thrust::raw_pointer_cast(input_Bu.data()), col_major_iter);
+  auto input_iter = cuda::make_zip_iterator(A_in_iter, Bu_in_iter);
+
+  auto A_output_iter  = cuda::make_permutation_iterator(thrust::raw_pointer_cast(output_A.data()), col_major_iter);
+  auto Bu_output_iter = cuda::make_permutation_iterator(thrust::raw_pointer_cast(output_Bu.data()), col_major_iter);
+  auto output_iter    = cuda::make_zip_iterator(A_output_iter, Bu_output_iter);
+
+  thrust::device_vector<int> begin_offsets(state_dim, thrust::no_init);
+  thrust::device_vector<int> end_offsets(state_dim, thrust::no_init);
+
+  thrust::sequence(begin_offsets.begin(), begin_offsets.end(), 0, nrows);
+  thrust::sequence(end_offsets.begin(), end_offsets.end(), nrows, nrows);
+
+  state.add_element_count(elements);
+  state.add_global_memory_reads<value_t>(total_size, "A_in");
+  state.add_global_memory_reads<value_t>(total_size, "Bu_in");
+  state.add_global_memory_writes<value_t>(total_size, "A_out");
+  state.add_global_memory_writes<value_t>(total_size, "Bu_out");
+
+  size_t tmp_size;
+  dispatch_t::dispatch(
+    nullptr,
+    tmp_size,
+    input_iter,
+    output_iter,
+    state_dim,
+    thrust::raw_pointer_cast(begin_offsets.data()),
+    thrust::raw_pointer_cast(end_offsets.data()),
+    thrust::raw_pointer_cast(begin_offsets.data()),
+    op_t{},
+    wrapped_init_t{},
+    bench_stream);
+
+  thrust::device_vector<nvbench::uint8_t> tmp(tmp_size, thrust::no_init);
+  nvbench::uint8_t* d_tmp = thrust::raw_pointer_cast(tmp.data());
+
+  state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch, [&](nvbench::launch& launch) {
+    dispatch_t::dispatch(
+      d_tmp,
+      tmp_size,
+      input_iter,
+      output_iter,
+      state_dim,
+      thrust::raw_pointer_cast(begin_offsets.data()),
+      thrust::raw_pointer_cast(end_offsets.data()),
+      thrust::raw_pointer_cast(begin_offsets.data()),
+      op_t{},
+      wrapped_init_t{},
+      launch.get_stream());
+  });
+}
+
 #ifdef TUNE_T
 using fp_types = nvbench::type_list<TUNE_T>;
 #else
@@ -284,7 +434,14 @@ using fp_types = nvbench::type_list<float, double>;
 #  endif
 #endif
 
+#if S5_NORMAL_SCAN
 NVBENCH_BENCH_TYPES(inclusive_scan, NVBENCH_TYPE_AXES(fp_types, offset_types))
   .set_name("s5-associative-scan")
+  .set_type_axes_names({"T{ct}", "OffsetT{ct}"})
+  .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 24, 2));
+#endif
+
+NVBENCH_BENCH_TYPES(segmented_scan, NVBENCH_TYPE_AXES(fp_types, offset_types))
+  .set_name("s5-segmented-scan")
   .set_type_axes_names({"T{ct}", "OffsetT{ct}"})
   .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 24, 2));
