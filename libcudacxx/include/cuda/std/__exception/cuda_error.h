93314a7661d2f25fd4ccaa1d696a8fdfcf90af1f
fbusato <fbusato@nvidia.com>
2025-11-24 13:09:01 -0800

    fix conflict

--- libcudacxx/include/cuda/std/__exception/cuda_error.h
+++ libcudacxx/include/cuda/std/__exception/cuda_error.h
@@ -21,12 +21,11 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/std/__exception/exception_macros.h>
+#include <cuda/std/__exception/msg_storage.h>
+#include <cuda/std/source_location>
+
 #if !_CCCL_COMPILER(NVRTC)
-
-#  include <cuda/std/__exception/exception_macros.h>
-#  include <cuda/std/__exception/msg_storage.h>
-#  include <cuda/std/source_location>
-
 #  include <cstdio>
 #  include <stdexcept>
 #endif // !_CCCL_COMPILER(NVRTC)
