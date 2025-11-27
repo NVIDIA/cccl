1f38507b68fda0225f5dd74f32189abda6c52b48
Michael Schellenberger Costa <miscco@nvidia.com>
2025-11-27 06:38:37 +0100

    Some nodiscard

--- cub/cub/device/dispatch/kernels/warpspeed/makeWarpUniform.cuh
+++ cub/cub/device/dispatch/kernels/warpspeed/makeWarpUniform.cuh
@@ -10,22 +10,22 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/std/cstdint> // uint32_t
+#include <cuda/std/cstdint>
 
 // Move register to uniform register
 
 // For int32_t and uint32_t, we can use the new CREDUX instruction, which is
 // coupled and has a constant 13 cycle latency.
 // For 64-bit types, we still use __shfl_sync
-static _CCCL_DEVICE_API int makeWarpUniform(int x)
+static [[nodiscard]] _CCCL_DEVICE_API int makeWarpUniform(int x)
 {
   return __reduce_min_sync(~0, x);
 }
-static _CCCL_DEVICE_API uint32_t makeWarpUniform(uint32_t x)
+static [[nodiscard]] _CCCL_DEVICE_API uint32_t makeWarpUniform(uint32_t x)
 {
   return __reduce_min_sync(~0, x);
 }
-static _CCCL_DEVICE_API uint64_t makeWarpUniform(uint64_t x)
+static [[nodiscard]] _CCCL_DEVICE_API uint64_t makeWarpUniform(uint64_t x)
 {
   return __shfl_sync(~0, x, 0);
 }
