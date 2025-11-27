13fae9cda7816bf45e7708eb0b128c366eb67235
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-27 11:11:13 +0100

    FIx benchmark problem sizes

--- cub/benchmarks/bench/scan/exclusive/base.cuh
+++ cub/benchmarks/bench/scan/exclusive/base.cuh
@@ -113,7 +113,18 @@ static void basic(nvbench::state& state, nvbench::type_list<T, OffsetT>)
   });
 }
 
-NVBENCH_BENCH_TYPES(basic, NVBENCH_TYPE_AXES(all_types, offset_types))
+constexpr int tile_size = 63 * 128;
+using value_types       = nvbench::type_list<int>;
+
+NVBENCH_BENCH_TYPES(basic, NVBENCH_TYPE_AXES(value_types, offset_types))
   .set_name("base")
   .set_type_axes_names({"T{ct}", "OffsetT{ct}"})
-  .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4));
+  //.add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4))
+  .add_int64_axis(
+    "Elements{io}",
+    {cuda::round_up(int64_t{1} << 16, tile_size),
+     cuda::round_up(int64_t{1} << 20, tile_size),
+     cuda::round_up(int64_t{1} << 24, tile_size),
+     cuda::round_up(int64_t{1} << 28, tile_size),
+     cuda::round_up(int64_t{1} << 30, tile_size),
+     cuda::round_up(int64_t{1} << 32, tile_size)});
