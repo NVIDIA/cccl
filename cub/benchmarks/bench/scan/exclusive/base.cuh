c17f1067ba3d14a244ccd85c27923da13ff3fa31
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-11-27 07:04:46 -0800

    use offset

--- cub/benchmarks/bench/scan/exclusive/base.cuh
+++ cub/benchmarks/bench/scan/exclusive/base.cuh
@@ -101,7 +101,7 @@ try
 
   size_t tmp_size;
   dispatch_t::Dispatch(
-    nullptr, tmp_size, d_input, d_output, op_t{}, wrapped_init_t{T{}}, static_cast<int>(input.size()), 0 /* stream */);
+    nullptr, tmp_size, d_input, d_output, op_t{}, wrapped_init_t{T{}}, static_cast<offset_t>(input.size()), 0 /* stream */);
 
   thrust::device_vector<nvbench::uint8_t> tmp(tmp_size);
   state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch, [&](nvbench::launch& launch) {
