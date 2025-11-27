57adbbf633bc62dc89f964c3d2885cfa9ad1ec1c
Michael Schellenberger Costa <miscco@nvidia.com>
2025-11-27 08:02:35 +0100

    Build fixes

--- cub/cub/device/dispatch/kernels/warpspeed/makeWarpUniform.cuh
+++ cub/cub/device/dispatch/kernels/warpspeed/makeWarpUniform.cuh
@@ -19,15 +19,15 @@ CUB_NAMESPACE_BEGIN
 // For int32_t and uint32_t, we can use the new CREDUX instruction, which is
 // coupled and has a constant 13 cycle latency.
 // For 64-bit types, we still use __shfl_sync
-static [[nodiscard]] _CCCL_DEVICE_API int makeWarpUniform(int x)
+[[nodiscard]] _CCCL_DEVICE_API static int makeWarpUniform(int x)
 {
   return __reduce_min_sync(~0, x);
 }
-static [[nodiscard]] _CCCL_DEVICE_API uint32_t makeWarpUniform(uint32_t x)
+[[nodiscard]] _CCCL_DEVICE_API static uint32_t makeWarpUniform(uint32_t x)
 {
   return __reduce_min_sync(~0, x);
 }
-static [[nodiscard]] _CCCL_DEVICE_API uint64_t makeWarpUniform(uint64_t x)
+[[nodiscard]] _CCCL_DEVICE_API static uint64_t makeWarpUniform(uint64_t x)
 {
   return __shfl_sync(~0, x, 0);
 }
