88769e2c4c45b5ab73b12d43c772296e2bf73a03
Michael Schellenberger Costa <miscco@nvidia.com>
2025-11-27 06:15:06 +0100

    Inline some methods in SmemAllocator

--- cub/cub/device/dispatch/kernels/warpspeed/allocators/SmemAllocator.h
+++ cub/cub/device/dispatch/kernels/warpspeed/allocators/SmemAllocator.h
@@ -11,7 +11,12 @@ struct SmemAllocator
   uint32_t mPtrSmem32 = 0;
   int mAllocatedSize  = 0;
 
-  __host__ __device__ inline SmemAllocator();
+  __host__ __device__ inline SmemAllocator()
+  {
+    NV_IF_TARGET(NV_IS_DEVICE,
+                 (extern __shared__ char warpSpeedDynamicSmemBase[];
+                  mPtrSmem32 = __cvta_generic_to_shared(warpSpeedDynamicSmemBase);))
+  }
 
   // SmemAllocator is a non-copyable, non-movable type. It must be passed by
   // (mutable) reference to be useful.
@@ -20,43 +25,27 @@ struct SmemAllocator
   SmemAllocator& operator=(const SmemAllocator&)  = delete; // Delete copy assignment
   SmemAllocator& operator=(const SmemAllocator&&) = delete; // Delete move assignment
 
-  __host__ __device__ inline void* alloc(uint32_t size, uint32_t align = 0);
+  __host__ __device__ inline void* alloc(uint32_t size, uint32_t align = 0)
+  {
+    // Align mPtrSmem32 to requested alignment (round-up)
+    uint32_t ptrAllocation32 = (mPtrSmem32 + (align - 1)) & ~(align - 1);
 
-  __host__ __device__ inline uint32_t sizeBytes() const;
+    // Move base pointer and update allocated size
+    mAllocatedSize += size + ptrAllocation32 - mPtrSmem32;
+    mPtrSmem32 = ptrAllocation32 + size;
+
+    NV_IF_ELSE_TARGET(
+      NV_IS_DEVICE,
+      (
+        // Convert allocated smem address to generic pointer
+        void* mPtrAllocation = __cvta_shared_to_generic(ptrAllocation32);
+        // Ensure alignment calculation does not move down into rest of kernel code.
+        return optimizeSmemPtr(mPtrAllocation);),
+      (return nullptr;))
+  }
+
+  __host__ __device__ inline uint32_t sizeBytes() const
+  {
+    return mAllocatedSize;
+  }
 };
-
-__host__ __device__ inline SmemAllocator::SmemAllocator()
-{
-#ifdef __CUDA_ARCH__
-  extern __shared__ char warpSpeedDynamicSmemBase[];
-  mPtrSmem32 = __cvta_generic_to_shared(warpSpeedDynamicSmemBase);
-#else
-  mPtrSmem32 = 0;
-#endif
-}
-
-__host__ __device__ inline void* SmemAllocator::alloc(uint32_t size, uint32_t align)
-{
-  // Align mPtrSmem32 to requested alignment (round-up)
-  uint32_t ptrAllocation32 = (mPtrSmem32 + (align - 1)) & ~(align - 1);
-
-  // Move base pointer and update allocated size
-  mAllocatedSize += size + ptrAllocation32 - mPtrSmem32;
-  mPtrSmem32 = ptrAllocation32 + size;
-
-#ifdef __CUDA_ARCH__ /* device */
-  // Convert allocated smem address to generic pointer
-  void* mPtrAllocation = __cvta_shared_to_generic(ptrAllocation32);
-  // Ensure alignment calculation does not move down into rest of kernel code.
-  return optimizeSmemPtr(mPtrAllocation);
-#else /* host */
-  // Host code is not going to use the return values anyway. So might as well
-  // return a nullptr.
-  return nullptr;
-#endif
-}
-
-__host__ __device__ inline uint32_t SmemAllocator::sizeBytes() const
-{
-  return mAllocatedSize;
-}
