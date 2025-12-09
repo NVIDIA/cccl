be82b6d0ee3aaadc465e8ecc524b2cbbfb9bf346
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-12-09 09:16:55 +0100

    Use vectorized kernel for FP16 workloads

--- cub/cub/device/dispatch/tuning/tuning_transform.cuh
+++ cub/cub/device/dispatch/tuning/tuning_transform.cuh
@@ -433,6 +433,10 @@ struct policy_hub<RequiresStableAddress,
         alignment)
       > int{max_smem_per_block};
 
+    static constexpr bool is_fp16_workload =
+      (::cuda::std::is_same_v<it_value_t<RandomAccessIteratorsIn>, __half> || ...)
+      || ::cuda::std::is_same_v<it_value_t<RandomAccessIteratorOut>, __half>;
+
     // if each tile size is a multiple of the bulk copy and maximum value type alignments, the alignment is retained if
     // the base pointer is sufficiently aligned (the correct check would be if it's a multiple of all value types
     // following the current tile). we would otherwise need to realign every SMEM tile individually, which is costly and
@@ -444,7 +448,7 @@ struct policy_hub<RequiresStableAddress,
     static constexpr bool enough_threads_for_peeling = AsyncBlockSize >= alignment; // head and tail bytes
     static constexpr bool fallback_to_vectorized =
       exhaust_smem || !tile_sizes_retain_alignment || !enough_threads_for_peeling || no_input_streams
-      || !can_memcpy_all_inputs;
+      || !can_memcpy_all_inputs || (PtxVersion == 900 && is_fp16_workload /* vectorized kernel works better */);
 
   public:
     static constexpr auto algorithm =
