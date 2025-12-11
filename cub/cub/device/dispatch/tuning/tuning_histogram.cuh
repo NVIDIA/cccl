821be72b25c9b9897ae85387dbf83194329a0196
Nader Al Awar <naderalawar@gmail.com>
2025-12-10 21:32:00 -0600

    Reuse histogram tuning policies in c.parallel

--- cub/cub/device/dispatch/tuning/tuning_histogram.cuh
+++ cub/cub/device/dispatch/tuning/tuning_histogram.cuh
@@ -145,7 +145,7 @@ struct sm100_tuning<false, SampleT, 1, 1, counter_size::_4, primitive_sample::ye
 template <typename PolicyT, typename = void>
 struct HistogramPolicyWrapper : PolicyT
 {
-  CUB_RUNTIME_FUNCTION HistogramPolicyWrapper(PolicyT base)
+  _CCCL_HOST_DEVICE HistogramPolicyWrapper(PolicyT base)
       : PolicyT(base)
   {}
 };
@@ -155,23 +155,25 @@ struct HistogramPolicyWrapper<StaticPolicyT,
                               ::cuda::std::void_t<decltype(StaticPolicyT::AgentHistogramPolicyT::LOAD_MODIFIER)>>
     : StaticPolicyT
 {
-  CUB_RUNTIME_FUNCTION HistogramPolicyWrapper(StaticPolicyT base)
+  _CCCL_HOST_DEVICE HistogramPolicyWrapper(StaticPolicyT base)
       : StaticPolicyT(base)
   {}
 
-  CUB_RUNTIME_FUNCTION static constexpr int BlockThreads()
+  _CCCL_HOST_DEVICE static constexpr auto Histogram()
   {
-    return StaticPolicyT::AgentHistogramPolicyT::BLOCK_THREADS;
+    return cub::detail::MakePolicyWrapper(typename StaticPolicyT::AgentHistogramPolicyT());
   }
-
-  CUB_RUNTIME_FUNCTION static constexpr int PixelsPerThread()
+#if defined(CUB_ENABLE_POLICY_PTX_JSON)
+  _CCCL_DEVICE static constexpr auto EncodedPolicy()
   {
-    return StaticPolicyT::AgentHistogramPolicyT::PIXELS_PER_THREAD;
+    using namespace ptx_json;
+    return object<key<"HistogramPolicy">() = Histogram().EncodedPolicy()>();
   }
+#endif
 };
 
 template <typename PolicyT>
-CUB_RUNTIME_FUNCTION HistogramPolicyWrapper<PolicyT> MakeHistogramPolicyWrapper(PolicyT policy)
+_CCCL_HOST_DEVICE HistogramPolicyWrapper<PolicyT> MakeHistogramPolicyWrapper(PolicyT policy)
 {
   return HistogramPolicyWrapper<PolicyT>{policy};
 }
