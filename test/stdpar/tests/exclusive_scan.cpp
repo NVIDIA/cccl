b51f01ee38d4612584833bc056b7ab706dbdbc10
Viral <viralrbhadeshiya@gmail.com>
2025-12-11 12:18:44 -0800

    added test cases for binary ops

--- test/stdpar/tests/exclusive_scan.cpp
+++ test/stdpar/tests/exclusive_scan.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <execution>
+#include <functional>
 #include <numeric>
 #include <vector>
 
@@ -10,29 +11,13 @@ int main()
   std::vector<int> in(N);
   std::vector<int> out(N);
 
-  for (std::size_t i = 0; i < N; ++i)
-  {
-    in[i] = static_cast<int>(i + 1);
-  }
-
-  // exclusive_scan with default initial value (0)
-  std::exclusive_scan(std::execution::par, in.begin(), in.end(), out.begin(), 0);
-
-  int running_sum = 0;
-  for (std::size_t i = 0; i < N; ++i)
-  {
-    if (out[i] != running_sum)
-    {
-      return 1;
-    }
-    running_sum += in[i];
-  }
+  std::iota(in.begin(), in.end(), 1);
 
   // exclusive_scan with non-zero initial value
   const int init = 42;
   std::exclusive_scan(std::execution::par, in.begin(), in.end(), out.begin(), init);
 
-  running_sum = init;
+  int running_sum = init;
   for (std::size_t i = 0; i < N; ++i)
   {
     if (out[i] != running_sum)
@@ -40,6 +25,41 @@ int main()
       return 1;
     }
     running_sum += in[i];
+  }
+
+  // exclusive_scan with initial value & binary ops
+  const int mul_init = 1;
+  std::exclusive_scan(std::execution::par, in.begin(), in.end(), out.begin(), mul_init, std::multiplies<int>{});
+
+  int expected = mul_init;
+  for (std::size_t i = 0; i < N; ++i)
+  {
+    if (out[i] != expected)
+    {
+      return 1;
+    }
+    expected *= 2;
+  }
+
+  // exclusive_scan with initial value & custom binary ops
+  const int init2 = 7;
+  std::exclusive_scan(
+    std::execution::par,
+    in.begin(),
+    in.end(),
+    out.begin(),
+    init2,
+    [](int lhs, int rhs) {
+      return lhs + rhs + 1;
+    })
+
+    for (std::size_t i = 0; i < N; ++i)
+  {
+    const int expected = init2 + static_cast<int>(2 * i);
+    if (out[i] != expected)
+    {
+      return 1;
+    }
   }
 
   return 0;
