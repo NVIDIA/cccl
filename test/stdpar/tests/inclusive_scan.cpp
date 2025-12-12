a606d90880d983069f401ef85539d682a3b3053e
Viral <viralrbhadeshiya@gmail.com>
2025-12-12 10:55:54 -0800

    added test case for binary ops with no init

--- test/stdpar/tests/inclusive_scan.cpp
+++ test/stdpar/tests/inclusive_scan.cpp
@@ -10,10 +10,7 @@ int main()
   std::vector<int> in(N);
   std::vector<int> out(N);
 
-  for (std::size_t i = 0; i < N; ++i)
-  {
-    in[i] = 1;
-  }
+  std::iota(in.start(), in.end(), 1);
 
   // inclusive_scan with default initial value
   std::inclusive_scan(std::execution::par, in.begin(), in.end(), out.begin());
@@ -34,6 +31,20 @@ int main()
   for (std::size_t i = 0; i < N; ++i)
   {
     const int expected = init + static_cast<int>(i + 1);
+    if (out[i] != expected)
+    {
+      return 1;
+    }
+  }
+
+  //   inclusive_scan with only binary ops and no init value
+  std::inclusive_scan(std::execution::par, in.begin(), in.end(), out.begin(), [](int lhs, int rhs) {
+    return lhs + rhs + 1;
+  });
+
+  for (std::size_t i = 0; i < N; ++i)
+  {
+    const int expected = static_cast<int>(2 * i + 1);
     if (out[i] != expected)
     {
       return 1;
