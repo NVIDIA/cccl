7e6770d30df6b917ee213a9889ab0fafa426ecef
pciolkosz <pciolkosz@nvidia.com>
2025-12-19 11:22:33 -0800

    [libcu++] Add explicit alignment specification in buffer (#7005)
    * Add explicit alignment specification in buffer
    
    * Fix shared resource test
    
    * Missed alignment in deallocate

--- libcudacxx/include/cuda/__container/uninitialized_async_buffer.h
+++ libcudacxx/include/cuda/__container/uninitialized_async_buffer.h
@@ -205,7 +205,7 @@ public:
       : __mr_(::cuda::std::move(__mr))
       , __stream_(__stream)
       , __count_(__count)
-      , __buf_(__count_ == 0 ? nullptr : __mr_.allocate(__stream_, __get_allocation_size(__count_)))
+      , __buf_(__count_ == 0 ? nullptr : __mr_.allocate(__stream_, __get_allocation_size(__count_), alignof(_Tp)))
   {}
 
   _CCCL_HIDE_FROM_ABI __uninitialized_async_buffer(const __uninitialized_async_buffer&)            = delete;
@@ -247,7 +247,7 @@ public:
 
     if (__buf_)
     {
-      __mr_.deallocate(__stream_, __buf_, __get_allocation_size(__count_));
+      __mr_.deallocate(__stream_, __buf_, __get_allocation_size(__count_), alignof(_Tp));
     }
     __mr_     = ::cuda::std::move(__other.__mr_);
     __stream_ = ::cuda::std::exchange(__other.__stream_, ::cuda::stream_ref{::cudaStream_t{}});
@@ -267,7 +267,7 @@ public:
   {
     if (__buf_)
     {
-      __mr_.deallocate(__stream, __buf_, __get_allocation_size(__count_));
+      __mr_.deallocate(__stream, __buf_, __get_allocation_size(__count_), alignof(_Tp));
       __buf_   = nullptr;
       __count_ = 0;
     }
