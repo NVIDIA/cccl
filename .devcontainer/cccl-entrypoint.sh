d1dcaa5e9329b90c22861233505735cbd3e404b5
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2025-12-19 12:07:55 -0800

    Use the sccache-dist build cluster for RAPIDS CI jobs (#7014)
    * use the sccache-dist build cluster for RAPIDS CI jobs [skip-matrix] [skip-vdc] [skip-docs] [skip-matx] [skip-pytorch] [test-rapids]
    
    * run devcontainer-utils lifecycle scripts [skip-matrix] [skip-vdc] [skip-docs] [skip-matx] [skip-pytorch] [test-rapids]
    
    * define GH_TOKEN [skip-matrix] [skip-vdc] [skip-docs] [skip-matx] [skip-pytorch] [test-rapids]
    
    * cpu32 -> cpu16 [skip-matrix] [skip-vdc] [skip-docs] [skip-matx] [skip-pytorch] [test-rapids]
    
    * remove preprocessor cache key prefix [skip-matrix] [skip-vdc] [skip-docs] [skip-matx] [skip-pytorch] [test-rapids]
    
    * increase nofile ulimit [skip-matrix] [skip-vdc] [skip-docs] [skip-matx] [skip-pytorch] [test-rapids]

--- .devcontainer/cccl-entrypoint.sh
+++ .devcontainer/cccl-entrypoint.sh
@@ -4,9 +4,21 @@
 
 set -e;
 
+if ! test -n "${DISABLE_SCCACHE:+x}" && test -n "${DEVCONTAINER_UTILS_ENABLE_SCCACHE_DIST:+x}" && ! test -n "${SCCACHE_DIST_URL:+x}"; then
+    export SCCACHE_DIST_URL="https://$(dpkg --print-architecture).$(uname -s | tr '[:upper:]' '[:lower:]').sccache.rapids.nvidia.com";
+    echo "export SCCACHE_DIST_URL=$SCCACHE_DIST_URL" >> ~/.bashrc;
+fi
+
+if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
+    echo "::group::Initializing devcontainer..."
+fi
+
 devcontainer-utils-post-create-command;
 devcontainer-utils-init-git;
 devcontainer-utils-post-attach-command;
+if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
+    echo "::endgroup::"
+fi
 
 if ! dpkg -s ca-certificates > /dev/null 2>&1; then
     if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
