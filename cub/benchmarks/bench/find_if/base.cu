981858b1a7c986d271f7c283e12811b714ea29ce
gonidelis <ggonidelis@nvidia.com>
2026-01-06 08:09:16 -0800

    Enable benchmarking for 32 bit offset

--- cub/benchmarks/bench/find_if/base.cu
+++ cub/benchmarks/bench/find_if/base.cu
@@ -9,25 +9,25 @@
 
 #include <nvbench_helper.cuh>
 
-template <typename T>
-void find_if(nvbench::state& state, nvbench::type_list<T>)
+template <typename T, typename OffsetT>
+void find_if(nvbench::state& state, nvbench::type_list<T, OffsetT>)
 {
   T val = 1;
   // set up input
-  const auto elements       = state.get_int64("Elements");
+  const auto elements       = static_cast<OffsetT>(state.get_int64("Elements"));
   const auto common_prefix  = state.get_float64("MismatchAt");
-  const auto mismatch_point = elements * common_prefix;
+  const auto mismatch_point = static_cast<OffsetT>(elements * common_prefix);
 
   thrust::device_vector<T> dinput(elements, thrust::no_init);
   thrust::fill(dinput.begin(), dinput.begin() + mismatch_point, 0);
   thrust::fill(dinput.begin() + mismatch_point, dinput.end(), val);
-  thrust::device_vector<size_t> d_result(1, thrust::no_init);
+  thrust::device_vector<OffsetT> d_result(1, thrust::no_init);
 
   void* d_temp_storage = nullptr;
   size_t temp_storage_bytes{};
 
   state.add_global_memory_reads<T>(mismatch_point);
-  state.add_global_memory_writes<size_t>(1);
+  state.add_global_memory_writes<OffsetT>(1);
 
   cub::DeviceFind::FindIf(
     d_temp_storage,
@@ -53,6 +53,6 @@ void find_if(nvbench::state& state, nvbench::type_list<T>)
   });
 }
 
-NVBENCH_BENCH_TYPES(find_if, NVBENCH_TYPE_AXES(fundamental_types))
+NVBENCH_BENCH_TYPES(find_if, NVBENCH_TYPE_AXES(fundamental_types, offset_types))
   .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4))
   .add_float64_axis("MismatchAt", std::vector{1.0, 0.5, 0.0});
