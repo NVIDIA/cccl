637d7bfc6d78acdad1f71c71a5b1790d5f417d06
Srinivas Yadav Singanaboina <ssinganaboin@nvidia.com>
2026-01-07 13:30:19 -0800

    add empty classes test

---
+++ libcudacxx/test/libcudacxx/cuda/execution/empty.pass.cpp
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/std/__cccl/attributes.h>
+
+struct Empty
+{};
+
+template <class T>
+struct Wrapper
+{
+  _CCCL_NO_UNIQUE_ADDRESS T value;
+};
+
+__host__ __device__ void test()
+{
+  [[maybe_unused]] Wrapper<Empty> w{};
+}
+
+int main()
+{
+  test();
+  return 0;
+}
+
---
+++ libcudacxx/test/libcudacxx/cuda/execution/empty_tuple.pass.cpp
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/std/__utility/pod_tuple.h>
+
+struct max_segment_size_42
+{};
+
+__host__ __device__ void test_tuple_empty()
+{
+  [[maybe_unused]] ::cuda::std::__tuple<max_segment_size_42> t{};
+}
+
+int main()
+{
+  test_tuple_empty();
+  return 0;
+}
+
