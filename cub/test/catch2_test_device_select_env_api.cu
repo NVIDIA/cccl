5cf4638d5d59b24b4e404cc673cf1994fbf5c2f8
gonidelis <ggonidelis@nvidia.com>
2026-01-08 10:57:09 -0800

    Add unit testing and rebase on env_dispatch boilerplate

--- cub/test/catch2_test_device_select_env_api.cu
+++ cub/test/catch2_test_device_select_env_api.cu
@@ -17,39 +17,18 @@
 C2H_TEST("cub::DeviceSelect::If accepts determinism requirements", "[select][env]")
 {
   // example-begin select-if-env-determinism
-  auto input        = c2h::device_vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
-  auto output       = c2h::device_vector<int>(4);
-  auto num_selected = c2h::device_vector<int>(1);
+  auto input        = thrust::device_vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
+  auto output       = thrust::device_vector<int>(4);
+  auto num_selected = thrust::device_vector<int>(1);
   less_than_t<int> le{input[input.size() / 2]};
 
   auto env = cuda::execution::require(cuda::execution::determinism::run_to_run);
 
   cub::DeviceSelect::If(input.begin(), output.begin(), num_selected.begin(), input.size(), le, env);
 
-  c2h::device_vector<int> expected_output{1, 2, 3, 4};
-  c2h::device_vector<int> expected_num_selected{4};
+  thrust::device_vector<int> expected_output{1, 2, 3, 4};
+  thrust::device_vector<int> expected_num_selected{4};
   // example-end select-if-env-determinism
-
-  REQUIRE(output == expected_output);
-  REQUIRE(num_selected == expected_num_selected);
-}
-
-C2H_TEST("cub::DeviceSelect::If accepts stream", "[select][env]")
-{
-  // example-begin flagged-env-determinism
-  auto input        = c2h::device_vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
-  auto output       = c2h::device_vector<int>(4);
-  auto num_selected = c2h::device_vector<int>(4);
-  less_than_t<int> le{input[input.size() / 2]};
-
-  cudaStream_t legacy_stream = 0;
-  cuda::stream_ref stream_ref{legacy_stream};
-
-  cub::DeviceSelect::If(input.begin(), output.begin(), num_selected.begin(), input.size(), le, stream_ref);
-
-  c2h::device_vector<int> expected_output{1, 2, 3, 4};
-  c2h::device_vector<int> expected_num_selected{4, 0, 0, 0};
-  // example-end flagged-env-determinism
 
   REQUIRE(output == expected_output);
   REQUIRE(num_selected == expected_num_selected);
---
+++ cub/test/catch2_test_device_select_if_env.cu
@@ -0,0 +1,119 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Should precede any includes
+struct stream_registry_factory_t;
+#define CUB_DETAIL_DEFAULT_KERNEL_LAUNCHER_FACTORY stream_registry_factory_t
+
+#include "insert_nested_NVTX_range_guard.h"
+
+#include <cub/device/device_select.cuh>
+
+#include <thrust/device_vector.h>
+
+#include <cuda/iterator>
+
+#include "catch2_test_device_select_common.cuh"
+#include "catch2_test_env_launch_helper.h"
+
+DECLARE_LAUNCH_WRAPPER(cub::DeviceSelect::If, device_select_if);
+
+// %PARAM% TEST_LAUNCH lid 0:1:2
+
+#include <cuda/__execution/determinism.h>
+#include <cuda/__execution/require.h>
+
+#include <c2h/catch2_test_helper.h>
+
+namespace stdexec = cuda::std::execution;
+
+#if TEST_LAUNCH == 0
+
+TEST_CASE("Device select works with default environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 8;
+  auto d_in             = c2h::device_vector<value_t>{1, 2, 3, 4, 5, 6, 7, 8};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  less_than_t<value_t> select_op{5};
+
+  REQUIRE(
+    cudaSuccess == cub::DeviceSelect::If(d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items, select_op));
+
+  c2h::device_vector<value_t> expected_output{1, 2, 3, 4};
+  c2h::device_vector<int> expected_num_selected{4};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+}
+
+#endif
+
+C2H_TEST("Device select uses environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 10;
+  auto d_in             = c2h::device_vector<value_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  less_than_t<value_t> select_op{6};
+
+  size_t expected_bytes_allocated{};
+  // calculate expected_bytes_allocated
+  REQUIRE(
+    cudaSuccess
+    == cub::DeviceSelect::If(
+      nullptr, expected_bytes_allocated, d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items, select_op));
+
+  auto env = stdexec::env{cuda::execution::require(cuda::execution::determinism::run_to_run), // determinism
+                          expected_allocation_size(expected_bytes_allocated)}; // temp storage size
+
+  device_select_if(d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items, select_op, env);
+
+  c2h::device_vector<value_t> expected_output{1, 2, 3, 4, 5};
+  c2h::device_vector<int> expected_num_selected{5};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+}
+
+TEST_CASE("Device select uses custom stream", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 8;
+  auto d_in             = c2h::device_vector<value_t>{1, 2, 3, 4, 5, 6, 7, 8};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  less_than_t<value_t> select_op{5};
+
+  cudaStream_t custom_stream;
+  REQUIRE(cudaSuccess == cudaStreamCreate(&custom_stream));
+
+  auto env = stdexec::prop{cuda::get_stream_t{}, cuda::stream_ref{custom_stream}};
+
+  REQUIRE(cudaSuccess
+          == cub::DeviceSelect::If(d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items, select_op, env));
+
+  REQUIRE(cudaSuccess == cudaStreamSynchronize(custom_stream));
+
+  c2h::device_vector<value_t> expected_output{1, 2, 3, 4};
+  c2h::device_vector<int> expected_num_selected{4};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+
+  REQUIRE(cudaSuccess == cudaStreamDestroy(custom_stream));
+}
