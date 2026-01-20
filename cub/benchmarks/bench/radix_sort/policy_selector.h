d1ebeecde7e4c4b908c937bd2f2e826e33dec2be
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-01-20 11:27:04 +0100

    Update cub/benchmarks/bench/radix_sort/policy_selector.h
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- cub/benchmarks/bench/radix_sort/policy_selector.h
+++ cub/benchmarks/bench/radix_sort/policy_selector.h
@@ -90,7 +90,7 @@ constexpr std::size_t max_onesweep_temp_storage_size()
   using hist_policy = typename policy_hub_t<KeyT, ValueT, OffsetT>::policy_t::HistogramPolicy;
   using hist_agent  = cub::AgentRadixSortHistogram<hist_policy, SortOrder, KeyT, OffsetT>;
 
-  return (::cuda::std::max) (sizeof(typename agent_radix_sort_onesweep_t::TempStorage),
+  return cuda::std::max(sizeof(typename agent_radix_sort_onesweep_t::TempStorage),
                              sizeof(typename hist_agent::TempStorage));
 }
 
