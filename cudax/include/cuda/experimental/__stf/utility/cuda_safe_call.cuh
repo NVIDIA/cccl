284ebac229f0a46d3b2bd2b5f1806b3fca80f7aa
David Bayer <bayer.david99@gmail.com>
2026-01-21 11:49:03 +0100

    Remove `_CCCL_HAS_INCLUDE`

--- cudax/include/cuda/experimental/__stf/utility/cuda_safe_call.cuh
+++ cudax/include/cuda/experimental/__stf/utility/cuda_safe_call.cuh
@@ -38,13 +38,13 @@
 #include <cuda_occupancy.h>
 #include <cuda_runtime.h>
 
-#if _CCCL_HAS_INCLUDE(<cusolverDn.h>)
+#if __has_include(<cusolverDn.h>)
 #  include <cusolverDn.h>
 #endif
 
 namespace cuda::experimental::stf
 {
-#if _CCCL_HAS_INCLUDE(<cusolverDn.h>)
+#if __has_include(<cusolverDn.h>)
 // Undocumented
 inline const char* cusolverGetErrorString(const cusolverStatus_t status)
 {
@@ -113,10 +113,10 @@ public:
   {
     // All "success" statuses are zero
     static_assert(cudaSuccess == 0 && CUDA_SUCCESS == 0
-#if _CCCL_HAS_INCLUDE(<cublas_v2.h>)
+#if __has_include(<cublas_v2.h>)
                     && CUBLAS_STATUS_SUCCESS == 0
 #endif
-#if _CCCL_HAS_INCLUDE(<cusolverDn.h>)
+#if __has_include(<cusolverDn.h>)
                     && CUSOLVER_STATUS_SUCCESS == 0
 #endif
                   ,
@@ -131,7 +131,7 @@ public:
     int dev = -1;
     cudaGetDevice(&dev);
 
-#if _CCCL_HAS_INCLUDE(<cusolverDn.h>)
+#if __has_include(<cusolverDn.h>)
     if constexpr (::std::is_same_v<T, cusolverStatus_t>)
     {
       format("%s(%u) [device %d] CUSOLVER error in call %s: %s.",
@@ -142,8 +142,8 @@ public:
              cusolverGetErrorString(status));
     }
     else
-#endif // _CCCL_HAS_INCLUDE(<cusolverDn.h>)
-#if _CCCL_HAS_INCLUDE(<cublas_v2.h>)
+#endif // __has_include(<cusolverDn.h>)
+#if __has_include(<cublas_v2.h>)
       if constexpr (::std::is_same_v<T, cublasStatus_t>)
     {
       format("%s(%u) [device %d] CUBLAS error in %s: %s.",
@@ -154,7 +154,7 @@ public:
              cublasGetStatusString(status));
     }
     else
-#endif // _CCCL_HAS_INCLUDE(<cublas_v2.h>)
+#endif // __has_include(<cublas_v2.h>)
       if constexpr (::std::is_same_v<T, cudaOccError>)
       {
         format("%s(%u) [device %d] CUDA OCC error in %s: %s.",
@@ -224,7 +224,7 @@ UNITTEST("cuda_exception")
 {
   auto e = cuda_exception(CUDA_SUCCESS);
   EXPECT(e.what()[0] == 0);
-#  if _CCCL_HAS_INCLUDE(<cusolverDn.h>)
+#  if __has_include(<cusolverDn.h>)
   auto e1 = cuda_exception(CUSOLVER_STATUS_ZERO_PIVOT);
   EXPECT(strlen(e1.what()) > 0u);
 #  endif
