68ad87502422e60a93a0605cab516413a41dca3b
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-01-29 22:10:14 +0100

    Fix warnings

--- cub/cub/device/dispatch/kernels/kernel_scan_warpspeed.cuh
+++ cub/cub/device/dispatch/kernels/kernel_scan_warpspeed.cuh
@@ -714,7 +714,7 @@ _CCCL_DEVICE_API _CCCL_FORCEINLINE void device_scan_lookahead_body(
 
 template <typename AccumT>
 _CCCL_DEVICE_API _CCCL_FORCEINLINE void
-device_scan_init_lookahead_body(warpspeed::tile_state_t<AccumT>* tile_states, const size_t num_temp_states)
+device_scan_init_lookahead_body(warpspeed::tile_state_t<AccumT>* tile_states, const int num_temp_states)
 {
   const int tile_id = blockDim.x * blockIdx.x + threadIdx.x;
   if (tile_id >= num_temp_states)
