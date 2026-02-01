c2bd3cc9b1f2333d105671a798d661c103ab231f
Ashwin Srinath <shwina@users.noreply.github.com>
2026-02-01 17:44:11 -0500

    Update iterator factories

--- python/cuda_cccl/cuda/compute/iterators/__init__.py
+++ python/cuda_cccl/cuda/compute/iterators/__init__.py
@@ -1,14 +1,12 @@
-from ._factories import (
-    CacheModifiedInputIterator,
-    ConstantIterator,
-    CountingIterator,
-    DiscardIterator,
-    PermutationIterator,
-    ReverseIterator,
-    TransformIterator,
-    TransformOutputIterator,
-    ZipIterator,
-)
+# Export iterator classes directly
+from ._cache_modified import CacheModifiedInputIterator
+from ._constant import ConstantIterator
+from ._counting import CountingIterator
+from ._discard import DiscardIterator
+from ._permutation import PermutationIterator
+from ._reverse import ReverseIterator
+from ._transform import TransformIterator, TransformOutputIterator
+from ._zip import ZipIterator
 
 __all__ = [
     "CacheModifiedInputIterator",
