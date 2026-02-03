3eaba265afd457a67541ca395fd822e4d63c1b95
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-02-03 12:56:51 +0100

    Apply reviewer feedback

--- cub/cub/detail/warpspeed/make_warp_uniform.cuh
+++ cub/cub/detail/warpspeed/make_warp_uniform.cuh
@@ -20,17 +20,19 @@ namespace detail::warpspeed
 {
 // Move register to uniform register
 
-// For int32_t and uint32_t, we can use the new CREDUX instruction, which is
-// coupled and has a constant 13 cycle latency.
+// For int32_t and uint32_t, we can use the CREDUX instruction, which is coupled and has a constant latency.
 // For 64-bit types, we still use __shfl_sync
+
 [[nodiscard]] _CCCL_DEVICE_API inline int makeWarpUniform(int x)
 {
   NV_IF_ELSE_TARGET(NV_PROVIDES_SM_90, (return __reduce_min_sync(~0, x);), (return x;));
 }
+
 [[nodiscard]] _CCCL_DEVICE_API inline ::cuda::std::uint32_t makeWarpUniform(::cuda::std::uint32_t x)
 {
   NV_IF_ELSE_TARGET(NV_PROVIDES_SM_90, (return __reduce_min_sync(~0, x);), (return x;));
 }
+
 [[nodiscard]] _CCCL_DEVICE_API inline ::cuda::std::uint64_t makeWarpUniform(::cuda::std::uint64_t x)
 {
   return __shfl_sync(~0, x, 0);
