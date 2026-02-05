d172b7792c54475b5cb6e103d3dd5e6af4e7e2f8
Surya <thedevmystic@gmail.com>
2026-02-05 14:29:54 +0530

    fix: commit suggestions

--- libcudacxx/include/cuda/__functional/equal_to_value.h
+++ libcudacxx/include/cuda/__functional/equal_to_value.h
@@ -3,7 +3,7 @@
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
 
@@ -33,38 +33,25 @@ struct __equal_to_value
 {
   _T value_;
 
-  explicit constexpr __equal_to_value(const T& value) noexcept
+  explicit constexpr __equal_to_value(const _T& value) noexcept
       : value_(value)
   {}
 
-  __host__ __device__ constexpr bool operator()(const T& lhs) const noexcept
+  [[nodiscard]] _CCCL_API constexpr bool operator()(const _T& lhs) const noexcept
+  {
+    return lhs == value_;
+  }
+
+  template <typename _U>
+  [[nodiscard]] _CCCL_API constexpr bool operator()(const _U& lhs) const noexcept
   {
     return lhs == value_;
   }
 };
 
-/// @brief `__equal_to_one` is a function object that checks if a value is equal to one.
-/// It inherits from `__equal_to_value` and initializes it with the value `1`.
-/// @tparam _T The type of the value to be compared.
+/// @brief Deduction guide for `__equal_to_value<_T>`.
 template <typename _T>
-struct __equal_to_one : __equal_to_value<T>
-{
-  constexpr __equal_to_one() noexcept
-      : __equal_to_value<T>(static_cast<T>(1))
-  {}
-};
-
-/// @brief `__equal_to_default` is a function object that checks if a value is equal to
-/// the default value of its type. It inherits from `__equal_to_value` and initializes
-/// it with the default-constructed value of type `_T`.
-/// @tparam _T The type of the value to be compared.
-template <typename _T>
-struct __equal_to_default : __equal_to_value<T>
-{
-  constexpr __equal_to_default() noexcept
-      : __equal_to_value<T>(T{})
-  {}
-};
+__equal_to_value(_T) -> __equal_to_value<_T>;
 
 _CCCL_END_NAMESPACE_CUDA
 
