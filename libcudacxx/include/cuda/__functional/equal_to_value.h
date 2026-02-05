7ff81a12f9ace4192060eda1b4ef690dd25a528a
Surya <thedevmystic@gmail.com>
2026-02-05 16:32:49 +0530

    fix: commit suggestions

--- libcudacxx/include/cuda/__functional/equal_to_value.h
+++ libcudacxx/include/cuda/__functional/equal_to_value.h
@@ -1,6 +1,7 @@
 //===----------------------------------------------------------------------===//
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES.
@@ -20,38 +21,43 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/std/__type_traits/is_comparable.h>
+#include <cuda/std/__type_traits/is_nothrow_copy_constructible.h>
+
 #include <cuda/std/__cccl/prologue.h>
 
 _CCCL_BEGIN_NAMESPACE_CUDA
 
-/// @brief `__equal_to_value` is a function object that checks if a value is equal to
-/// a specified value. It takes a single template parameter `_T`, which represents the
-/// type of the value to be compared.
-/// @tparam _T The type of the value to be compared.
-template <typename _T>
+//! @brief `__equal_to_value` is a function object that checks if a value is equal to
+//! a specified value. It takes a single template parameter `_Tp`, which represents the
+//! type of the value to be compared.
+//! @tparam _Tp The type of the value to be compared.
+template <typename _Tp>
 struct __equal_to_value
 {
-  _T value_;
+  _Tp __value_;
 
-  explicit constexpr __equal_to_value(const _T& value) noexcept
-      : value_(value)
+  explicit constexpr __equal_to_value(const _Tp& __value) noexcept(::cuda::std::is_nothrow_copy_constructible_v<_Tp>)
+      : __value_(__value)
   {}
 
-  [[nodiscard]] _CCCL_API constexpr bool operator()(const _T& lhs) const noexcept
+  [[nodiscard]] _CCCL_API constexpr bool operator()(const _Tp& __lhs) const noexcept
   {
-    return lhs == value_;
+    return __lhs == __value_;
   }
 
-  template <typename _U>
-  [[nodiscard]] _CCCL_API constexpr bool operator()(const _U& lhs) const noexcept
+  _CCCL_TEMPLATE(class _Up)
+  _CCCL_REQUIRES(::cuda::std::__is_cpp17_equality_comparable_v<_Tp, _Up>)
+  [[nodiscard]] _CCCL_API constexpr bool operator()(const _Up& __lhs) const
+    noexcept(::cuda::std::__is_cpp17_nothrow_equality_comparable_v<_Tp, _Up>)
   {
-    return lhs == value_;
+    return __lhs == __value_;
   }
 };
 
-/// @brief Deduction guide for `__equal_to_value<_T>`.
-template <typename _T>
-__equal_to_value(_T) -> __equal_to_value<_T>;
+/// @brief Deduction guide for `__equal_to_value<_Tp>`.
+template <typename _Tp>
+__equal_to_value(_Tp) -> __equal_to_value<_Tp>;
 
 _CCCL_END_NAMESPACE_CUDA
 
