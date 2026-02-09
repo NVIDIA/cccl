2cab77aa353b40748f4105f74a418ea8c62071b5
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-02-09 13:24:42 +0100

    Fix operator identity and absorbing element for char

--- libcudacxx/test/libcudacxx/cuda/functional/identity_element.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/functional/identity_element.pass.cpp
@@ -122,6 +122,7 @@ __host__ __device__ constexpr void test_identity_integral()
 {
   test_identity<cuda::std::logical_and, bool>(true, true);
   test_identity<cuda::std::logical_or, bool>(true, false);
+  test_identity_integral<char>();
   test_identity_integral<signed char>();
   test_identity_integral<unsigned char>();
   test_identity_integral<short>();
