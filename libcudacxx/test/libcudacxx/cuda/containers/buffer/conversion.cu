ce47fc907c5c610ab25d872eec639eae90be2b39
anon <users.noreply.github.com>
2026-02-11 01:29:59 +0000

    Add explicit alignment argument to buffer

--- libcudacxx/test/libcudacxx/cuda/containers/buffer/conversion.cu
+++ libcudacxx/test/libcudacxx/cuda/containers/buffer/conversion.cu
@@ -18,7 +18,7 @@
 #include <cuda/std/type_traits>
 
 #include "helper.h"
-#include "test_resources.h"
+#include "../test_resources.h"
 #include "types.h"
 
 C2H_CCCLRT_TEST("cuda::buffer conversion", "[container][buffer]", test_types)
