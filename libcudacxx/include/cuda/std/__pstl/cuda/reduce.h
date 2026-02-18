bf79293cb0ca50b1456fad077b2d05e6931d13fd
Michael Schellenberger Costa <miscco@nvidia.com>
2026-02-18 11:37:01 +0100

    Unify allocation of temporary storage and result for pstl algorithms

--- libcudacxx/include/cuda/std/__pstl/cuda/reduce.h
+++ libcudacxx/include/cuda/std/__pstl/cuda/reduce.h
@@ -50,6 +50,7 @@ _CCCL_DIAG_POP
 #  include <cuda/std/__memory/construct_at.h>
 #  include <cuda/std/__new/bad_alloc.h>
 #  include <cuda/std/__numeric/reduce.h>
+#  include <cuda/std/__pstl/cuda/temporary_storage.h>
 #  include <cuda/std/__pstl/dispatch.h>
 #  include <cuda/std/__type_traits/always_false.h>
 #  include <cuda/std/__type_traits/is_nothrow_constructible.h>
@@ -67,62 +68,6 @@ _CCCL_BEGIN_NAMESPACE_ARCH_DEPENDENT
 template <>
 struct __pstl_dispatch<__pstl_algorithm::__reduce, __execution_backend::__cuda>
 {
-  //! Ensures we properly deallocate the memory allocated for the result
-  template <class _Tp, class _AccumT, class _Resource>
-  struct __allocation_guard
-  {
-    //! This helper struct ensures that we can properly assign types with a nontrivial assignment operator
-    struct __construct_result
-    {
-      _Tp* __ptr_;
-
-      _CCCL_HOST_API __construct_result(_Tp* __ptr = nullptr) noexcept
-          : __ptr_(__ptr)
-      {}
-
-      template <class _Index, class _Up>
-      _CCCL_DEVICE_API _CCCL_FORCEINLINE void
-      operator()(_Index, _Up&& __value) noexcept(is_nothrow_constructible_v<_Tp, _Up>)
-      {
-        ::cuda::std::__construct_at(__ptr_, ::cuda::std::forward<_Up>(__value));
-      }
-    };
-
-    ::cuda::stream_ref __stream_;
-    _Resource& __resource_;
-    _Tp* __ptr_;
-    size_t __num_bytes_;
-
-    _CCCL_HOST_API __allocation_guard(::cuda::stream_ref __stream, _Resource& __resource, size_t __num_bytes)
-        : __stream_(__stream)
-        , __resource_(__resource)
-        , __ptr_(static_cast<_Tp*>(__resource_.allocate(__stream_, sizeof(_Tp) + __num_bytes, alignof(_Tp))))
-        , __num_bytes_(sizeof(_Tp) + __num_bytes)
-    {}
-
-    _CCCL_HOST_API ~__allocation_guard()
-    {
-      __resource_.deallocate(__stream_, __ptr_, __num_bytes_, alignof(_Tp));
-    }
-
-    [[nodiscard]] _CCCL_HOST_API auto __get_result_iter() noexcept
-    {
-      if constexpr (::cuda::std::__detail::__can_optimize_construct_at<_Tp, _AccumT>)
-      {
-        return __ptr_;
-      }
-      else
-      {
-        return ::cuda::tabulate_output_iterator{__construct_result{__ptr_}};
-      }
-    }
-
-    [[nodiscard]] _CCCL_HOST_API void* __get_temp_storage() noexcept
-    {
-      return static_cast<void*>(__ptr_ + 1);
-    }
-  };
-
   template <class _Policy, class _Iter, class _Size, class _Tp, class _BinaryOp>
   [[nodiscard]] _CCCL_HOST_API static _Tp
   __par_impl([[maybe_unused]] const _Policy& __policy, _Iter __first, _Size __count, _Tp __init, _BinaryOp __func)
@@ -152,16 +97,16 @@ struct __pstl_dispatch<__pstl_algorithm::__reduce, __execution_backend::__cuda>
       ::cuda::mr::get_memory_resource, ::cuda::device_default_memory_pool(__stream.device()), __policy);
 
     {
-      __allocation_guard<_Tp, _AccumT, decltype(__resource)> __guard{__stream, __resource, __num_bytes};
+      __temporary_storage<_Tp, decltype(__resource)> __storage{__stream, __resource, __num_bytes};
 
       // Run the reduction
       _CCCL_TRY_CUDA_API(
         ::cub::DeviceReduce::Reduce,
         "__pstl_cuda_reduce: kernel launch of cub::DeviceReduce::Reduce failed",
-        __guard.__get_temp_storage(),
+        __storage.__get_temp_storage(),
         __num_bytes,
         ::cuda::std::move(__first),
-        __guard.__get_result_iter(),
+        __storage.template __get_result_iter<_AccumT>(),
         __count,
         ::cuda::std::move(__func),
         ::cuda::std::move(__init),
@@ -172,7 +117,7 @@ struct __pstl_dispatch<__pstl_algorithm::__reduce, __execution_backend::__cuda>
         ::cudaMemcpyAsync,
         "__pstl_cuda_reduce: copy of result from device to host failed",
         ::cuda::std::addressof(__ret),
-        __guard.__ptr_,
+        __storage.__res_,
         sizeof(_Tp),
         ::cudaMemcpyDefault,
         __stream.get());
