e856a0df0a3fca9c6aa76f012f21343b6a57354a
gonidelis <ggonidelis@nvidia.com>
2026-02-23 10:53:27 -0800

    Add env FindIf and SelectUnique*

--- cub/test/catch2_test_device_select_env.cu
+++ cub/test/catch2_test_device_select_env.cu
@@ -18,6 +18,9 @@ struct stream_registry_factory_t;
 
 DECLARE_LAUNCH_WRAPPER(cub::DeviceSelect::If, device_select_if);
 DECLARE_LAUNCH_WRAPPER(cub::DeviceSelect::Flagged, device_select_flagged);
+DECLARE_LAUNCH_WRAPPER(cub::DeviceSelect::FlaggedIf, device_select_flagged_if);
+DECLARE_LAUNCH_WRAPPER(cub::DeviceSelect::Unique, device_select_unique);
+DECLARE_LAUNCH_WRAPPER(cub::DeviceSelect::UniqueByKey, device_select_unique_by_key);
 
 // %PARAM% TEST_LAUNCH lid 0:1:2
 
@@ -75,6 +78,84 @@ TEST_CASE("Device select flagged works with default environment", "[select][devi
   REQUIRE(d_num_selected == expected_num_selected);
   d_out.resize(d_num_selected[0]);
   REQUIRE(d_out == expected_output);
+}
+
+TEST_CASE("Device select flagged_if works with default environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 8;
+  auto d_in             = c2h::device_vector<value_t>{1, 2, 3, 4, 5, 6, 7, 8};
+  auto d_flags          = c2h::device_vector<int>{2, 1, 1, 4, 1, 3, 6, 1};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  mod_n<int> select_op{2};
+
+  REQUIRE(cudaSuccess
+          == cub::DeviceSelect::FlaggedIf(
+            d_in.begin(), d_flags.begin(), d_out.begin(), d_num_selected.begin(), num_items, select_op));
+
+  c2h::device_vector<value_t> expected_output{1, 4, 6, 7};
+  c2h::device_vector<int> expected_num_selected{4};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+}
+
+TEST_CASE("Device select unique works with default environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 8;
+  auto d_in             = c2h::device_vector<value_t>{0, 2, 2, 9, 5, 5, 5, 8};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  REQUIRE(cudaSuccess == cub::DeviceSelect::Unique(d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items));
+
+  c2h::device_vector<value_t> expected_output{0, 2, 9, 5, 8};
+  c2h::device_vector<int> expected_num_selected{5};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+}
+
+TEST_CASE("Device select unique_by_key works with default environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 8;
+  auto d_keys_in        = c2h::device_vector<value_t>{0, 2, 2, 9, 5, 5, 5, 8};
+  auto d_values_in      = c2h::device_vector<value_t>{1, 2, 3, 4, 5, 6, 7, 8};
+  auto d_keys_out       = c2h::device_vector<value_t>(num_items);
+  auto d_values_out     = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  REQUIRE(
+    cudaSuccess
+    == cub::DeviceSelect::UniqueByKey(
+      d_keys_in.begin(),
+      d_values_in.begin(),
+      d_keys_out.begin(),
+      d_values_out.begin(),
+      d_num_selected.begin(),
+      num_items));
+
+  c2h::device_vector<value_t> expected_keys{0, 2, 9, 5, 8};
+  c2h::device_vector<value_t> expected_values{1, 2, 4, 5, 8};
+  c2h::device_vector<int> expected_num_selected{5};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_keys_out.resize(d_num_selected[0]);
+  d_values_out.resize(d_num_selected[0]);
+  REQUIRE(d_keys_out == expected_keys);
+  REQUIRE(d_values_out == expected_values);
 }
 
 #endif
@@ -143,6 +224,119 @@ C2H_TEST("Device select flagged uses environment", "[select][device]")
   REQUIRE(d_num_selected == expected_num_selected);
   d_out.resize(d_num_selected[0]);
   REQUIRE(d_out == expected_output);
+}
+
+C2H_TEST("Device select flagged_if uses environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 10;
+  auto d_in             = c2h::device_vector<value_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  auto d_flags          = c2h::device_vector<int>{2, 1, 2, 1, 2, 1, 2, 1, 2, 1};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  mod_n<int> select_op{2};
+
+  size_t expected_bytes_allocated{};
+  REQUIRE(
+    cudaSuccess
+    == cub::DeviceSelect::FlaggedIf(
+      nullptr,
+      expected_bytes_allocated,
+      d_in.begin(),
+      d_flags.begin(),
+      d_out.begin(),
+      d_num_selected.begin(),
+      num_items,
+      select_op));
+
+  auto env = stdexec::env{expected_allocation_size(expected_bytes_allocated)};
+
+  device_select_flagged_if(
+    d_in.begin(), d_flags.begin(), d_out.begin(), d_num_selected.begin(), num_items, select_op, env);
+
+  c2h::device_vector<value_t> expected_output{1, 3, 5, 7, 9};
+  c2h::device_vector<int> expected_num_selected{5};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+}
+
+C2H_TEST("Device select unique uses environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 10;
+  auto d_in             = c2h::device_vector<value_t>{1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+  auto d_out            = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  size_t expected_bytes_allocated{};
+  REQUIRE(cudaSuccess
+          == cub::DeviceSelect::Unique(
+            nullptr, expected_bytes_allocated, d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items));
+
+  auto env = stdexec::env{expected_allocation_size(expected_bytes_allocated)};
+
+  device_select_unique(d_in.begin(), d_out.begin(), d_num_selected.begin(), num_items, env);
+
+  c2h::device_vector<value_t> expected_output{1, 2, 3, 4, 5};
+  c2h::device_vector<int> expected_num_selected{5};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_out.resize(d_num_selected[0]);
+  REQUIRE(d_out == expected_output);
+}
+
+C2H_TEST("Device select unique_by_key uses environment", "[select][device]")
+{
+  using value_t     = int;
+  using num_items_t = int;
+
+  num_items_t num_items = 10;
+  auto d_keys_in        = c2h::device_vector<value_t>{1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+  auto d_values_in      = c2h::device_vector<value_t>{10, 11, 20, 21, 30, 31, 40, 41, 50, 51};
+  auto d_keys_out       = c2h::device_vector<value_t>(num_items);
+  auto d_values_out     = c2h::device_vector<value_t>(num_items);
+  auto d_num_selected   = c2h::device_vector<int>(1);
+
+  size_t expected_bytes_allocated{};
+  REQUIRE(
+    cudaSuccess
+    == cub::DeviceSelect::UniqueByKey(
+      nullptr,
+      expected_bytes_allocated,
+      d_keys_in.begin(),
+      d_values_in.begin(),
+      d_keys_out.begin(),
+      d_values_out.begin(),
+      d_num_selected.begin(),
+      num_items));
+
+  auto env = stdexec::env{expected_allocation_size(expected_bytes_allocated)};
+
+  device_select_unique_by_key(
+    d_keys_in.begin(),
+    d_values_in.begin(),
+    d_keys_out.begin(),
+    d_values_out.begin(),
+    d_num_selected.begin(),
+    num_items,
+    env);
+
+  c2h::device_vector<value_t> expected_keys{1, 2, 3, 4, 5};
+  c2h::device_vector<value_t> expected_values{10, 20, 30, 40, 50};
+  c2h::device_vector<int> expected_num_selected{5};
+
+  REQUIRE(d_num_selected == expected_num_selected);
+  d_keys_out.resize(d_num_selected[0]);
+  d_values_out.resize(d_num_selected[0]);
+  REQUIRE(d_keys_out == expected_keys);
+  REQUIRE(d_values_out == expected_values);
 }
 
 TEST_CASE("Device select uses custom stream", "[select][device]")
