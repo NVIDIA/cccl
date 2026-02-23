18ebca40d229f947fe8dadf6033ff9f81de2612f
Trent Nelson <trent@trent.me>
2026-02-22 22:50:20 -0800

    cuda.coop: extract block radix sort decls and rewrite nodes

--- python/cuda_cccl/cuda/coop/_decls/__init__.py
+++ python/cuda_cccl/cuda/coop/_decls/__init__.py
@@ -65,12 +65,6 @@ from ..block._block_discontinuity import (
 from ..block._block_radix_rank import (
     _make_radix_rank_rewrite as _make_block_radix_rank_rewrite,
 )
-from ..block._block_radix_sort import (
-    _make_radix_sort_keys_descending_rewrite as _make_block_radix_sort_keys_descending_rewrite,
-)
-from ..block._block_radix_sort import (
-    _make_radix_sort_keys_rewrite as _make_block_radix_sort_keys_rewrite,
-)
 from ..block._block_shuffle import (
     _make_shuffle_rewrite as _make_block_shuffle_rewrite,
 )
@@ -1729,203 +1723,8 @@ class CoopBlockDiscontinuityDecl(CoopAbstractTemplate, CoopDeclMixin):
 # Radix Sort
 # =============================================================================
 
-
-@register_global(coop.block.radix_sort_keys)
-class CoopBlockRadixSortDecl(CoopAbstractTemplate, CoopDeclMixin):
-    key = coop.block.radix_sort_keys
-    impl_key = _make_block_radix_sort_keys_rewrite
-    primitive_name = "coop.block.radix_sort_keys"
-    is_constructor = False
-    minimum_num_args = 2
-
-    @staticmethod
-    def signature(
-        keys: types.Array,
-        items_per_thread: int = None,
-        begin_bit: Optional[int] = None,
-        end_bit: Optional[int] = None,
-        values: types.Array = None,
-        decomposer: Optional[Callable] = None,
-        blocked_to_striped: Optional[bool] = None,
-        temp_storage: Union[types.Array, TempStorageType] = None,
-    ):
-        return inspect.signature(CoopBlockRadixSortDecl.signature).bind(
-            keys,
-            items_per_thread=items_per_thread,
-            begin_bit=begin_bit,
-            end_bit=end_bit,
-            values=values,
-            decomposer=decomposer,
-            blocked_to_striped=blocked_to_striped,
-            temp_storage=temp_storage,
-        )
-
-    def _validate_args_and_create_signature(self, bound, two_phase=False):
-        keys = bound.arguments["keys"]
-        if not isinstance(keys, types.Array):
-            raise errors.TypingError(
-                f"{self.primitive_name} requires 'keys' to be a device array"
-            )
-
-        arglist = [keys]
-        process_items_per_thread(self, bound, arglist, two_phase, target_array=keys)
-
-        begin_bit = bound.arguments.get("begin_bit")
-        end_bit = bound.arguments.get("end_bit")
-        if (begin_bit is None) != (end_bit is None):
-            raise errors.TypingError(
-                f"{self.primitive_name} requires both 'begin_bit' and 'end_bit'"
-            )
-        if begin_bit is not None:
-            if not isinstance(begin_bit, (types.Integer, types.IntegerLiteral)):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'begin_bit' to be an integer"
-                )
-            if not isinstance(end_bit, (types.Integer, types.IntegerLiteral)):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'end_bit' to be an integer"
-                )
-            arglist.extend([begin_bit, end_bit])
-
-        values = bound.arguments.get("values")
-        values_is_none_type = isinstance(values, types.NoneType)
-        if values_is_none_type:
-            arglist.append(values)
-            values = None
-        if values is not None:
-            if not isinstance(values, types.Array):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'values' to be a device array"
-                )
-            arglist.append(values)
-
-        decomposer = bound.arguments.get("decomposer")
-        decomposer_is_none_type = isinstance(decomposer, types.NoneType)
-        if decomposer_is_none_type:
-            arglist.append(decomposer)
-            decomposer = None
-        if decomposer is not None:
-            arglist.append(decomposer)
-
-        blocked_to_striped = bound.arguments.get("blocked_to_striped")
-        blocked_is_none_type = isinstance(blocked_to_striped, types.NoneType)
-        if blocked_is_none_type:
-            arglist.append(blocked_to_striped)
-            blocked_to_striped = None
-        if blocked_to_striped is not None:
-            if not isinstance(
-                blocked_to_striped, (types.Boolean, types.BooleanLiteral, bool)
-            ):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'blocked_to_striped' to be a boolean"
-                )
-            arglist.append(blocked_to_striped)
-
-        temp_storage = bound.arguments.get("temp_storage")
-        validate_temp_storage(self, temp_storage)
-        if temp_storage is not None:
-            arglist.append(temp_storage)
-
-        return signature(types.void, *arglist)
-
-
-@register_global(coop.block.radix_sort_keys_descending)
-class CoopBlockRadixSortDescendingDecl(CoopAbstractTemplate, CoopDeclMixin):
-    key = coop.block.radix_sort_keys_descending
-    impl_key = _make_block_radix_sort_keys_descending_rewrite
-    primitive_name = "coop.block.radix_sort_keys_descending"
-    is_constructor = False
-    minimum_num_args = 2
-
-    @staticmethod
-    def signature(
-        keys: types.Array,
-        items_per_thread: int = None,
-        begin_bit: Optional[int] = None,
-        end_bit: Optional[int] = None,
-        values: types.Array = None,
-        decomposer: Optional[Callable] = None,
-        blocked_to_striped: Optional[bool] = None,
-        temp_storage: Union[types.Array, TempStorageType] = None,
-    ):
-        return inspect.signature(CoopBlockRadixSortDescendingDecl.signature).bind(
-            keys,
-            items_per_thread=items_per_thread,
-            begin_bit=begin_bit,
-            end_bit=end_bit,
-            values=values,
-            decomposer=decomposer,
-            blocked_to_striped=blocked_to_striped,
-            temp_storage=temp_storage,
-        )
-
-    def _validate_args_and_create_signature(self, bound, two_phase=False):
-        keys = bound.arguments["keys"]
-        if not isinstance(keys, types.Array):
-            raise errors.TypingError(
-                f"{self.primitive_name} requires 'keys' to be a device array"
-            )
-
-        arglist = [keys]
-        process_items_per_thread(self, bound, arglist, two_phase, target_array=keys)
-
-        begin_bit = bound.arguments.get("begin_bit")
-        end_bit = bound.arguments.get("end_bit")
-        if (begin_bit is None) != (end_bit is None):
-            raise errors.TypingError(
-                f"{self.primitive_name} requires both 'begin_bit' and 'end_bit'"
-            )
-        if begin_bit is not None:
-            if not isinstance(begin_bit, (types.Integer, types.IntegerLiteral)):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'begin_bit' to be an integer"
-                )
-            if not isinstance(end_bit, (types.Integer, types.IntegerLiteral)):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'end_bit' to be an integer"
-                )
-            arglist.extend([begin_bit, end_bit])
-
-        values = bound.arguments.get("values")
-        values_is_none_type = isinstance(values, types.NoneType)
-        if values_is_none_type:
-            arglist.append(values)
-            values = None
-        if values is not None:
-            if not isinstance(values, types.Array):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'values' to be a device array"
-                )
-            arglist.append(values)
-
-        decomposer = bound.arguments.get("decomposer")
-        decomposer_is_none_type = isinstance(decomposer, types.NoneType)
-        if decomposer_is_none_type:
-            arglist.append(decomposer)
-            decomposer = None
-        if decomposer is not None:
-            arglist.append(decomposer)
-
-        blocked_to_striped = bound.arguments.get("blocked_to_striped")
-        blocked_is_none_type = isinstance(blocked_to_striped, types.NoneType)
-        if blocked_is_none_type:
-            arglist.append(blocked_to_striped)
-            blocked_to_striped = None
-        if blocked_to_striped is not None:
-            if not isinstance(
-                blocked_to_striped, (types.Boolean, types.BooleanLiteral, bool)
-            ):
-                raise errors.TypingError(
-                    f"{self.primitive_name} requires 'blocked_to_striped' to be a boolean"
-                )
-            arglist.append(blocked_to_striped)
-
-        temp_storage = bound.arguments.get("temp_storage")
-        validate_temp_storage(self, temp_storage)
-        if temp_storage is not None:
-            arglist.append(temp_storage)
-
-        return signature(types.void, *arglist)
+CoopBlockRadixSortDecl = None
+CoopBlockRadixSortDescendingDecl = None
 
 
 # =============================================================================
@@ -2323,64 +2122,29 @@ def lower_constant_block_discontinuity_instance_type(context, builder, typ, valu
     return context.get_dummy_value()
 
 
-class CoopBlockRadixSortInstanceType(CoopSimpleInstanceType):
-    decl_class = CoopBlockRadixSortDecl
-
-
-block_radix_sort_instance_type = CoopBlockRadixSortInstanceType()
-
-
-@typeof_impl.register(coop.block.radix_sort_keys)
-def typeof_block_radix_sort_instance(*args, **kwargs):
-    return block_radix_sort_instance_type
-
-
-@register
-class CoopBlockRadixSortInstanceDecl(CoopInstanceTemplate):
-    key = block_radix_sort_instance_type
-    instance_type = block_radix_sort_instance_type
-    primitive_name = "coop.block.radix_sort_keys"
-
-
-@register_model(CoopBlockRadixSortInstanceType)
-class CoopBlockRadixSortInstanceModel(models.OpaqueModel):
-    pass
-
-
-@lower_constant(CoopBlockRadixSortInstanceType)
-def lower_constant_block_radix_sort_instance_type(context, builder, typ, value):
-    return context.get_dummy_value()
-
-
-class CoopBlockRadixSortDescendingInstanceType(CoopSimpleInstanceType):
-    decl_class = CoopBlockRadixSortDescendingDecl
-
-
-block_radix_sort_descending_instance_type = CoopBlockRadixSortDescendingInstanceType()
-
-
-@typeof_impl.register(coop.block.radix_sort_keys_descending)
-def typeof_block_radix_sort_descending_instance(*args, **kwargs):
-    return block_radix_sort_descending_instance_type
-
-
-@register
-class CoopBlockRadixSortDescendingInstanceDecl(CoopInstanceTemplate):
-    key = block_radix_sort_descending_instance_type
-    instance_type = block_radix_sort_descending_instance_type
-    primitive_name = "coop.block.radix_sort_keys_descending"
-
-
-@register_model(CoopBlockRadixSortDescendingInstanceType)
-class CoopBlockRadixSortDescendingInstanceModel(models.OpaqueModel):
-    pass
-
-
-@lower_constant(CoopBlockRadixSortDescendingInstanceType)
-def lower_constant_block_radix_sort_descending_instance_type(
-    context, builder, typ, value
-):
-    return context.get_dummy_value()
+_decls_block_radix_sort = __import__(
+    "cuda.coop._decls.block._block_radix_sort",
+    fromlist=[
+        "CoopBlockRadixSortDecl",
+        "CoopBlockRadixSortDescendingDecl",
+        "CoopBlockRadixSortInstanceType",
+        "CoopBlockRadixSortDescendingInstanceType",
+        "block_radix_sort_instance_type",
+        "block_radix_sort_descending_instance_type",
+    ],
+)
+CoopBlockRadixSortDecl = _decls_block_radix_sort.CoopBlockRadixSortDecl
+CoopBlockRadixSortDescendingDecl = (
+    _decls_block_radix_sort.CoopBlockRadixSortDescendingDecl
+)
+CoopBlockRadixSortInstanceType = _decls_block_radix_sort.CoopBlockRadixSortInstanceType
+CoopBlockRadixSortDescendingInstanceType = (
+    _decls_block_radix_sort.CoopBlockRadixSortDescendingInstanceType
+)
+block_radix_sort_instance_type = _decls_block_radix_sort.block_radix_sort_instance_type
+block_radix_sort_descending_instance_type = (
+    _decls_block_radix_sort.block_radix_sort_descending_instance_type
+)
 
 
 class CoopBlockRadixRankInstanceType(CoopSimpleInstanceType):
