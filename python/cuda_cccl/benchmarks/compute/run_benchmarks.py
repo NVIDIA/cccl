ec7ffd0cf3dd008bda1adb80beb93f81003355f9
Daniel Rodriguez <daniel@danielfrg.com>
2026-02-27 13:48:38 -0600

    Update copyright year

--- python/cuda_cccl/benchmarks/compute/run_benchmarks.py
+++ python/cuda_cccl/benchmarks/compute/run_benchmarks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+# Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
