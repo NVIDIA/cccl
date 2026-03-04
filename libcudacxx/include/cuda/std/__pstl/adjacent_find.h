d69c88a8a70900ffe1601f68162d84b45822f08d
Michael Schellenberger Costa <miscco@nvidia.com>
2026-03-04 16:28:56 +0100

    Implement parallel `cuda::std::adjacent_find`
    This implements the adjacent_find algorithm for the cuda backend.
    
    * std::adjacent_find see https://en.cppreference.com/w/cpp/algorithm/adjacent_find.html
    
    It provides tests and benchmarks similar to Thrust and some boilerplate for libcu++
    
    The functionality is publicly available yet and implemented in a private internal header
    
    Fixes #7744

--- libcudacxx/include/cuda/std/__pstl/adjacent_find.h
+++ libcudacxx/include/cuda/std/__pstl/adjacent_find.h
@@ -8,8 +8,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _CUDA_STD___PSTL_ADJACENT_DIFFERENCE_H
-#define _CUDA_STD___PSTL_ADJACENT_DIFFERENCE_H
+#ifndef _CUDA_STD___PSTL_ADJACENT_FIND_H
+#define _CUDA_STD___PSTL_ADJACENT_FIND_H
 
 #include <cuda/std/detail/__config>
 
@@ -23,20 +23,22 @@
 
 #if !_CCCL_COMPILER(NVRTC)
 
+#  include <cuda/__iterator/zip_function.h>
+#  include <cuda/__iterator/zip_iterator.h>
 #  include <cuda/__nvtx/nvtx.h>
+#  include <cuda/std/__algorithm/adjacent_find.h>
 #  include <cuda/std/__concepts/concept_macros.h>
 #  include <cuda/std/__execution/policy.h>
 #  include <cuda/std/__functional/operations.h>
 #  include <cuda/std/__iterator/concepts.h>
-#  include <cuda/std/__iterator/distance.h>
-#  include <cuda/std/__numeric/adjacent_difference.h>
+#  include <cuda/std/__iterator/next.h>
 #  include <cuda/std/__pstl/dispatch.h>
 #  include <cuda/std/__type_traits/always_false.h>
 #  include <cuda/std/__type_traits/is_execution_policy.h>
 #  include <cuda/std/__utility/move.h>
 
 #  if _CCCL_HAS_BACKEND_CUDA()
-#    include <cuda/std/__pstl/cuda/adjacent_difference.h>
+#    include <cuda/std/__pstl/cuda/find_if.h>
 #  endif // _CCCL_HAS_BACKEND_CUDA()
 
 #  include <cuda/std/__cccl/prologue.h>
@@ -45,44 +47,36 @@ _CCCL_BEGIN_NAMESPACE_CUDA_STD
 
 _CCCL_BEGIN_NAMESPACE_ARCH_DEPENDENT
 
-_CCCL_TEMPLATE(class _Policy, class _InputIterator, class _OutputIterator, class _BinaryOp = ::cuda::std::minus<>)
-_CCCL_REQUIRES(__has_forward_traversal<_InputIterator> _CCCL_AND __has_forward_traversal<_OutputIterator> _CCCL_AND
-                 is_execution_policy_v<_Policy>)
-[[nodiscard]] _CCCL_HOST_API _OutputIterator adjacent_difference(
-  [[maybe_unused]] const _Policy& __policy,
-  _InputIterator __first,
-  _InputIterator __last,
-  _OutputIterator __result,
-  _BinaryOp __binary_op = {})
+_CCCL_TEMPLATE(class _Policy,
+               class _InputIterator,
+               class _BinaryPredicate = ::cuda::std::equal_to<iter_value_t<_InputIterator>>)
+_CCCL_REQUIRES(__has_forward_traversal<_InputIterator> _CCCL_AND is_execution_policy_v<_Policy>)
+_CCCL_HOST_API _InputIterator adjacent_find(
+  [[maybe_unused]] const _Policy& __policy, _InputIterator __first, _InputIterator __last, _BinaryPredicate __pred = {})
 {
   [[maybe_unused]] auto __dispatch =
-    ::cuda::std::execution::__pstl_select_dispatch<::cuda::std::execution::__pstl_algorithm::__adjacent_difference,
-                                                   _Policy>();
+    ::cuda::std::execution::__pstl_select_dispatch<::cuda::std::execution::__pstl_algorithm::__find_if, _Policy>();
   if constexpr (::cuda::std::execution::__pstl_can_dispatch<decltype(__dispatch)>)
   {
-    _CCCL_NVTX_RANGE_SCOPE("cuda::std::adjacent_difference");
+    _CCCL_NVTX_RANGE_SCOPE("cuda::std::adjacent_find");
 
     if (__first == __last)
     {
-      return __result;
+      return __first;
     }
 
-    return __dispatch(
+    auto __zipped_ret = __dispatch(
       __policy,
-      ::cuda::std::move(__first),
-      ::cuda::std::move(__last),
-      ::cuda::std::move(__result),
-      ::cuda::std::move(__binary_op));
+      ::cuda::zip_iterator{__first, ::cuda::std::next(__first)},
+      ::cuda::zip_iterator{__last, __last},
+      ::cuda::zip_function{::cuda::std::move(__pred)});
+    return ::cuda::std::get<0>(__zipped_ret.__iterators());
   }
   else
   {
     static_assert(__always_false_v<_Policy>,
-                  "Parallel cuda::std::adjacent_difference requires at least one selected backend");
-    return ::cuda::std::adjacent_difference(
-      ::cuda::std::move(__first),
-      ::cuda::std::move(__last),
-      ::cuda::std::move(__result),
-      ::cuda::std::move(__binary_op));
+                  "Parallel cuda::std::adjacent_find requires at least one selected backend");
+    return ::cuda::std::adjacent_find(::cuda::std::move(__first), ::cuda::std::move(__last), ::cuda::std::move(__pred));
   }
 }
 
@@ -94,4 +88,4 @@ _CCCL_END_NAMESPACE_CUDA_STD
 
 #endif // !_CCCL_COMPILER(NVRTC)
 
-#endif // _CUDA_STD___PSTL_ADJACENT_DIFFERENCE_H
+#endif // _CUDA_STD___PSTL_ADJACENT_FIND_H
