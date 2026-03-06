9bf3244c436cd9d8506538dfef704644313cbced
David Bayer <48736217+davebayer@users.noreply.github.com>
2026-03-06 10:55:30 +0100

    Apply suggestion from @davebayer

--- libcudacxx/cmake/LibcudacxxPublicHeaderTestingHost.cmake
+++ libcudacxx/cmake/LibcudacxxPublicHeaderTestingHost.cmake
@@ -80,7 +80,7 @@ function(
   )
   target_link_libraries(
     public_headers_host_only_with_ctk_${header_name}
-    PUBLIC CUDA::cudart
+    PUBLIC libcudacxx.compiler_interface CUDA::cudart
   )
   add_dependencies(
     libcudacxx.test.public_headers_host_only_with_ctk
