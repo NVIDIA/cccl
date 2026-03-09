3f54633ecf9bc5332dbcadfd5a0855f565828edf
Cedric AUGONNET <caugonnet@nvidia.com>
2026-03-09 18:43:16 +0100

    Move places/inner_shape.cuh in internal/inner_shape.cuh

--- cudax/include/cuda/experimental/__stf/internal/context.cuh
+++ cudax/include/cuda/experimental/__stf/internal/context.cuh
@@ -28,7 +28,7 @@
 #include <cuda/experimental/__stf/internal/task_dep.cuh>
 #include <cuda/experimental/__stf/internal/void_interface.cuh>
 #include <cuda/experimental/__stf/places/exec/cuda_stream.cuh>
-#include <cuda/experimental/__stf/places/inner_shape.cuh>
+#include <cuda/experimental/__stf/internal/inner_shape.cuh>
 #include <cuda/experimental/__stf/stream/stream_ctx.cuh>
 
 #include <map>
