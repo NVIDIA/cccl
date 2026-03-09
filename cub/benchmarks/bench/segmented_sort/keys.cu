09b73231d5d4704649ebc7c0bb0fd82d4f1c997a
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-03-09 18:04:45 +0100

    Implement the new tuning API for `DispatchSegmentedSort` (#7874)
    Fixes: #7643
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- cub/benchmarks/bench/segmented_sort/keys.cu
+++ cub/benchmarks/bench/segmented_sort/keys.cu
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2011-2023, NVIDIA CORPORATION. All rights reserved.
 // SPDX-License-Identifier: BSD-3
 
-#include <cub/device/device_segmented_sort.cuh>
+#include <cub/device/dispatch/dispatch_segmented_sort.cuh>
 
 #include <nvbench_helper.cuh>
 
@@ -22,95 +22,58 @@
 // %RANGE% TUNE_M_TRANSPOSE mtrp 0:1:1
 
 #if !TUNE_BASE
-
-#  define TUNE_SW_THREADS (1 << TUNE_SW_THREADS_POW2)
-#  define TUNE_MW_THREADS (1 << TUNE_MW_THREADS_POW2)
-
-#  define SMALL_SEGMENT_SIZE  TUNE_S_ITEMS* TUNE_SW_THREADS
-#  define MEDIUM_SEGMENT_SIZE TUNE_M_ITEMS* TUNE_MW_THREADS
-#  define LARGE_SEGMENT_SIZE  TUNE_L_ITEMS* TUNE_THREADS
-
-#  if (LARGE_SEGMENT_SIZE <= SMALL_SEGMENT_SIZE) || (LARGE_SEGMENT_SIZE <= MEDIUM_SEGMENT_SIZE)
-#    error Large segment size must be larger than small and medium segment sizes
-#  endif
-
-#  if (MEDIUM_SEGMENT_SIZE <= SMALL_SEGMENT_SIZE)
-#    error Medium segment size must be larger than small one
-#  endif
-
-#  if TUNE_LOAD == 0
-#    define TUNE_LOAD_MODIFIER cub::LOAD_DEFAULT
-#  elif TUNE_LOAD == 1
-#    define TUNE_LOAD_MODIFIER cub::LOAD_LDG
-#  else // TUNE_LOAD == 2
-#    define TUNE_LOAD_MODIFIER cub::LOAD_CA
-#  endif // TUNE_LOAD
-
-#  if TUNE_S_LOAD == 0
-#    define TUNE_S_LOAD_MODIFIER cub::LOAD_DEFAULT
-#  elif TUNE_S_LOAD == 1
-#    define TUNE_S_LOAD_MODIFIER cub::LOAD_LDG
-#  else // TUNE_S_LOAD == 2
-#    define TUNE_S_LOAD_MODIFIER cub::LOAD_CA
-#  endif // TUNE_S_LOAD
-
-#  if TUNE_M_LOAD == 0
-#    define TUNE_M_LOAD_MODIFIER cub::LOAD_DEFAULT
-#  elif TUNE_M_LOAD == 1
-#    define TUNE_M_LOAD_MODIFIER cub::LOAD_LDG
-#  else // TUNE_M_LOAD == 2
-#    define TUNE_M_LOAD_MODIFIER cub::LOAD_CA
-#  endif // TUNE_M_LOAD
-
-#  if TUNE_TRANSPOSE == 0
-#    define TUNE_LOAD_ALGORITHM cub::BLOCK_LOAD_DIRECT
-#  else // TUNE_TRANSPOSE == 1
-#    define TUNE_LOAD_ALGORITHM cub::BLOCK_LOAD_WARP_TRANSPOSE
-#  endif // TUNE_TRANSPOSE
-
-#  if TUNE_S_TRANSPOSE == 0
-#    define TUNE_S_LOAD_ALGORITHM cub::WarpLoadAlgorithm::WARP_LOAD_DIRECT
-#  else // TUNE_S_TRANSPOSE == 1
-#    define TUNE_S_LOAD_ALGORITHM cub::WarpLoadAlgorithm::WARP_LOAD_TRANSPOSE
-#  endif // TUNE_S_TRANSPOSE
-
-#  if TUNE_M_TRANSPOSE == 0
-#    define TUNE_M_LOAD_ALGORITHM cub::WarpLoadAlgorithm::WARP_LOAD_DIRECT
-#  else // TUNE_M_TRANSPOSE == 1
-#    define TUNE_M_LOAD_ALGORITHM cub::WarpLoadAlgorithm::WARP_LOAD_TRANSPOSE
-#  endif // TUNE_M_TRANSPOSE
-
 template <class KeyT>
-struct device_seg_sort_policy_hub
+struct device_seg_sort_policy_selector
 {
-  using DominantT = KeyT;
-
-  struct Policy500 : cub::ChainedPolicy<500, Policy500, Policy500>
+  _CCCL_API constexpr auto operator()(::cuda::arch_id /*arch*/) const
   {
-    static constexpr int BLOCK_THREADS          = TUNE_THREADS;
-    static constexpr int RADIX_BITS             = TUNE_RADIX_BITS;
-    static constexpr int PARTITIONING_THRESHOLD = TUNE_PARTITIONING_THRESHOLD;
+    constexpr int tune_sw_threads     = 1 << TUNE_SW_THREADS_POW2;
+    constexpr int tune_mw_threads     = 1 << TUNE_MW_THREADS_POW2;
+    constexpr int small_segment_size  = TUNE_S_ITEMS * tune_sw_threads;
+    constexpr int medium_segment_size = TUNE_M_ITEMS * tune_mw_threads;
+    constexpr int large_segment_size  = TUNE_L_ITEMS * TUNE_THREADS;
 
-    using LargeSegmentPolicy = cub::AgentRadixSortDownsweepPolicy<
-      BLOCK_THREADS,
-      TUNE_L_ITEMS,
-      DominantT,
-      TUNE_LOAD_ALGORITHM,
-      TUNE_LOAD_MODIFIER,
-      static_cast<cub::RadixRankAlgorithm>(TUNE_RANK_ALGORITHM),
-      cub::BLOCK_SCAN_WARP_SCANS,
-      RADIX_BITS>;
+    static_assert((large_segment_size > small_segment_size) && (large_segment_size > medium_segment_size),
+                  "Large segment size must be larger than small and medium segment sizes");
+    static_assert(medium_segment_size > small_segment_size, "Medium segment size must be larger than small one");
 
-    static constexpr int ITEMS_PER_SMALL_THREAD  = TUNE_S_ITEMS;
-    static constexpr int ITEMS_PER_MEDIUM_THREAD = TUNE_M_ITEMS;
+    using namespace cub::detail::segmented_sort;
 
-    using SmallSegmentPolicy = cub::
-      AgentSubWarpMergeSortPolicy<TUNE_SW_THREADS, ITEMS_PER_SMALL_THREAD, TUNE_S_LOAD_ALGORITHM, TUNE_S_LOAD_MODIFIER>;
-    using MediumSegmentPolicy = cub::
-      AgentSubWarpMergeSortPolicy<TUNE_MW_THREADS, ITEMS_PER_MEDIUM_THREAD, TUNE_M_LOAD_ALGORITHM, TUNE_M_LOAD_MODIFIER>;
-  };
-
-  using MaxPolicy = Policy500;
+    return segmented_sort_policy{
+      segmented_radix_sort_policy{
+        TUNE_THREADS,
+        TUNE_L_ITEMS,
+        (TUNE_TRANSPOSE == 0) ? cub::BLOCK_LOAD_DIRECT : cub::BLOCK_LOAD_WARP_TRANSPOSE,
+        (TUNE_LOAD == 0)   ? cub::LOAD_DEFAULT
+        : (TUNE_LOAD == 1) ? cub::LOAD_LDG
+                           : cub::LOAD_CA,
+        static_cast<cub::RadixRankAlgorithm>(TUNE_RANK_ALGORITHM),
+        cub::BLOCK_SCAN_WARP_SCANS,
+        TUNE_RADIX_BITS,
+      },
+      sub_warp_merge_sort_policy{
+        TUNE_THREADS,
+        tune_sw_threads,
+        TUNE_S_ITEMS,
+        (TUNE_S_TRANSPOSE == 0) ? cub::WarpLoadAlgorithm::WARP_LOAD_DIRECT : cub::WarpLoadAlgorithm::WARP_LOAD_TRANSPOSE,
+        cub::WARP_STORE_DIRECT,
+        (TUNE_S_LOAD == 0)   ? cub::LOAD_DEFAULT
+        : (TUNE_S_LOAD == 1) ? cub::LOAD_LDG
+                             : cub::LOAD_CA,
+      },
+      sub_warp_merge_sort_policy{
+        TUNE_THREADS,
+        tune_mw_threads,
+        TUNE_M_ITEMS,
+        (TUNE_M_TRANSPOSE == 0) ? cub::WarpLoadAlgorithm::WARP_LOAD_DIRECT : cub::WarpLoadAlgorithm::WARP_LOAD_TRANSPOSE,
+        cub::WARP_STORE_DIRECT,
+        (TUNE_M_LOAD == 0)   ? cub::LOAD_DEFAULT
+        : (TUNE_M_LOAD == 1) ? cub::LOAD_LDG
+                             : cub::LOAD_CA,
+      },
+      TUNE_PARTITIONING_THRESHOLD,
+    };
+  }
 };
 #endif // !TUNE_BASE
 
@@ -128,15 +91,6 @@ void seg_sort(nvbench::state& state,
   using end_offset_it_t   = const offset_t*;
   using key_t             = T;
   using value_t           = cub::NullType;
-
-#if !TUNE_BASE
-  using policy_t   = device_seg_sort_policy_hub<key_t>;
-  using dispatch_t = //
-    cub::DispatchSegmentedSort<sort_order, key_t, value_t, offset_t, begin_offset_it_t, end_offset_it_t, policy_t>;
-#else
-  using dispatch_t = //
-    cub::DispatchSegmentedSort<sort_order, key_t, value_t, offset_t, begin_offset_it_t, end_offset_it_t>;
-#endif
 
   const auto elements = static_cast<std::size_t>(state.get_int64("Elements{io}"));
   const auto segments = offsets.size() - 1;
@@ -160,7 +114,7 @@ void seg_sort(nvbench::state& state,
 
   std::size_t temp_storage_bytes{};
   std::uint8_t* d_temp_storage{};
-  dispatch_t::Dispatch(
+  cub::detail::segmented_sort::dispatch<sort_order, offset_t>(
     d_temp_storage,
     temp_storage_bytes,
     d_keys,
@@ -170,9 +124,14 @@ void seg_sort(nvbench::state& state,
     d_begin_offsets,
     d_end_offsets,
     is_overwrite_ok,
-    0);
+    0
+#if !TUNE_BASE
+    ,
+    device_seg_sort_policy_selector<key_t>{}
+#endif // !TUNE_BASE
+  );
 
-  thrust::device_vector<nvbench::uint8_t> temp_storage(temp_storage_bytes);
+  thrust::device_vector<nvbench::uint8_t> temp_storage(temp_storage_bytes, thrust::no_init);
   d_temp_storage = thrust::raw_pointer_cast(temp_storage.data());
 
   state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync,
@@ -180,7 +139,7 @@ void seg_sort(nvbench::state& state,
                cub::DoubleBuffer<key_t> keys     = d_keys;
                cub::DoubleBuffer<value_t> values = d_values;
 
-               dispatch_t::Dispatch(
+               cub::detail::segmented_sort::dispatch<sort_order, offset_t>(
                  d_temp_storage,
                  temp_storage_bytes,
                  keys,
@@ -190,7 +149,12 @@ void seg_sort(nvbench::state& state,
                  d_begin_offsets,
                  d_end_offsets,
                  is_overwrite_ok,
-                 launch.get_stream());
+                 launch.get_stream()
+#if !TUNE_BASE
+                   ,
+                 device_seg_sort_policy_selector<key_t>{}
+#endif // !TUNE_BASE
+               );
              });
 }
 
