8cbac1eb92ee6920d30f5e7669c244c09e023933
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-03-09 20:04:56 +0100

    fix

--- cub/benchmarks/bench/partition/three_way.cu
+++ cub/benchmarks/bench/partition/three_way.cu
@@ -92,13 +92,13 @@ void partition(nvbench::state& state, nvbench::type_list<T, OffsetT>)
     );
   };
 
-  dispatch(nullptr, temp_size, 0);
+  dispatch(nullptr, 0);
 
   thrust::device_vector<nvbench::uint8_t> temp(temp_size);
   auto* temp_storage = thrust::raw_pointer_cast(temp.data());
 
   state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch, [&](nvbench::launch& launch) {
-    dispatch(temp_storage, temp_size, launch.get_stream());
+    dispatch(temp_storage, launch.get_stream());
   });
 }
 
