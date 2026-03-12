2da3dd1e44b177d78c7bbd5a2805b051c67c7d5a
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-03-12 16:19:18 +0100

    Let the CI compile the benchmark

--- cub/benchmarks/bench/scan/exclusive/sum.warspeed.cu
+++ cub/benchmarks/bench/scan/exclusive/sum.warspeed.cu
@@ -8,18 +8,18 @@
 #include <cuda/__cccl_config>
 
 #if _CCCL_PP_COUNT(__CUDA_ARCH_LIST__) != 1
-#  error "This benchmark does not support being compiled for multiple architectures"
-#endif
+#  warning "This benchmark does not support being compiled for multiple architectures. Disabling it."
+#else // _CCCL_PP_COUNT(__CUDA_ARCH_LIST__) != 1
 
-#if __CUDA_ARCH_LIST__ < 1000
-#  error Warpspeed scan requires at least sm_100
-#endif // __CUDA_ARCH_LIST__ < 1000
+#  if __CUDA_ARCH_LIST__ < 1000
+#    warning "Warpspeed scan requires at least sm_100. Disabling it."
+#  else // __CUDA_ARCH_LIST__ < 1000
 
-#if __cccl_ptx_isa < 860
-#  error Warpspeed scan requires at least PTX ISA 8.6
-#endif // __cccl_ptx_isa < 860
+#    if __cccl_ptx_isa < 860
+#      warning "Warpspeed scan requires at least PTX ISA 8.6. Disabling it."
+#    else // if __cccl_ptx_isa < 860
 
-#include <nvbench_helper.cuh>
+#      include <nvbench_helper.cuh>
 
 // %RANGE% TUNE_NUM_REDUCE_SCAN_WARPS wrps 1:8:1
 // %RANGE% TUNE_NUM_LOOKBACK_ITEMS lbi 1:8:1
@@ -28,7 +28,11 @@
 // Should we specify nominal items per thread instead?
 // %RANGE% TUNE_ITEMS_PLUS_ONE ipt 8:256:8
 
-#define USES_WARPSPEED() 1
+#      define USES_WARPSPEED() 1
 using op_t              = ::cuda::std::plus<>;
 using scan_offset_types = nvbench::type_list<int64_t>;
-#include "base.cuh"
+#      include "base.cuh"
+
+#    endif // __cccl_ptx_isa < 860
+#  endif // __CUDA_ARCH_LIST__ < 1000
+#endif // _CCCL_PP_COUNT(__CUDA_ARCH_LIST__) != 1
