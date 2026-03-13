8e4722759e4b4ec74ba67fab9fb27b3f7319cddf
gonidelis <ggonidelis@nvidia.com>
2026-03-13 16:18:30 -0700

    Fix layout_right::mapping template parameter in dispatch_copy_mdspan
    decltype(mdspan_in.extents()) deduces to const E_In& since
    mdspan::extents() returns a const reference. layout_right::mapping
    requires an extents specialization, not a reference type.

--- cub/cub/device/dispatch/dispatch_copy_mdspan.cuh
+++ cub/cub/device/dispatch/dispatch_copy_mdspan.cuh
@@ -73,9 +73,8 @@ copy(::cuda::std::mdspan<T_In, E_In, L_In, A_In> mdspan_in,
   }
   // TODO (fbusato): add ForEachInLayout when mdspan_in and mdspan_out have compatible layouts
   // Compatible layouts could use more efficient iteration patterns
-  using extents_type = decltype(mdspan_in.extents());
   return cub::DeviceFor::__for_each_in_extents(
-    ::cuda::std::layout_right::mapping<extents_type>{mdspan_in.extents()}, copy_mdspan_t{mdspan_in, mdspan_out}, env);
+    ::cuda::std::layout_right::mapping<E_In>{mdspan_in.extents()}, copy_mdspan_t{mdspan_in, mdspan_out}, env);
 }
 } // namespace detail::copy_mdspan
 
