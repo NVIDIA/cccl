3ec8ec097a61bd50ccc7e41f93d9443ed270aa35
Nitish Naineni <naineninitish@gmail.com>
2026-03-24 13:27:14 -0500

    use cuda::std headers instead of std headers in isclose

--- c2h/include/c2h/isclose.h
+++ c2h/include/c2h/isclose.h
@@ -3,25 +3,25 @@
 
 #pragma once
 
-#include <algorithm>
-#include <cmath>
-#include <limits>
-#include <type_traits>
+#include <cuda/std/algorithm>
+#include <cuda/std/cmath>
+#include <cuda/std/limits>
+#include <cuda/std/type_traits>
 
 template <typename T>
 bool isclose(T a, T b, T r_tol, T a_tol)
 {
-  if constexpr (std::is_floating_point_v<T>)
+  if constexpr (cuda::std::is_floating_point_v<T>)
   {
     if (a == b)
     {
       return true;
     }
-    return std::abs(a - b) <= std::max(a_tol, r_tol * std::max(std::abs(a), std::abs(b)));
+    return cuda::std::abs(a - b) <= cuda::std::max(a_tol, r_tol * cuda::std::max(cuda::std::abs(a), cuda::std::abs(b)));
   }
   else
   {
-    static_assert(std::is_integral_v<T>, "isclose: unsupported type, expected floating point or integral");
+    static_assert(cuda::std::is_integral_v<T>, "isclose: unsupported type, expected floating point or integral");
     return a == b;
   }
 }
@@ -35,13 +35,13 @@ bool isclose(T a, T b, T r_tol)
 template <typename T>
 bool isclose(T a, T b)
 {
-  if constexpr (std::is_floating_point_v<T>)
+  if constexpr (cuda::std::is_floating_point_v<T>)
   {
-    return isclose(a, b, T(1 << 8) * std::numeric_limits<T>::epsilon(), T(0));
+    return isclose(a, b, T(1 << 8) * cuda::std::numeric_limits<T>::epsilon(), T(0));
   }
   else
   {
-    static_assert(std::is_integral_v<T>, "isclose: unsupported type, expected floating point or integral");
+    static_assert(cuda::std::is_integral_v<T>, "isclose: unsupported type, expected floating point or integral");
     return a == b;
   }
 }
