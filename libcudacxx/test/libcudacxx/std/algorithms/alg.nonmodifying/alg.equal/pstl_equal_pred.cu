1075a2c7a6927a4ba3785320cf80cbfcceecc98b
Michael Schellenberger Costa <miscco@nvidia.com>
2026-03-25 11:16:53 +0100

    Rename `with_{stream, memory_resource}` to `with(get_{stream, memory_resource}, `
    We want to use latter API because it is closer to the sender & receiver style

--- libcudacxx/test/libcudacxx/std/algorithms/alg.nonmodifying/alg.equal/pstl_equal_pred.cu
+++ libcudacxx/test/libcudacxx/std/algorithms/alg.nonmodifying/alg.equal/pstl_equal_pred.cu
@@ -100,14 +100,14 @@ C2H_TEST("cuda::std::equal(first1, last1, first2, pred)", "[parallel algorithm]"
   SECTION("with provided stream")
   {
     cuda::stream stream{cuda::device_ref{0}};
-    const auto policy = cuda::execution::__cub_par_unseq.with_stream(stream);
+    const auto policy = cuda::execution::__cub_par_unseq.with(cuda::get_stream, stream);
     test_equal(policy);
   }
 
   SECTION("with provided memory_resource")
   {
     cuda::device_memory_pool_ref device_resource = cuda::device_default_memory_pool(cuda::device_ref{0});
-    const auto policy = cuda::execution::__cub_par_unseq.with_memory_resource(device_resource);
+    const auto policy = cuda::execution::__cub_par_unseq.with(cuda::mr::get_memory_resource, device_resource);
     test_equal(policy);
   }
 
@@ -115,7 +115,8 @@ C2H_TEST("cuda::std::equal(first1, last1, first2, pred)", "[parallel algorithm]"
   {
     cuda::stream stream{cuda::device_ref{0}};
     cuda::device_memory_pool_ref device_resource = cuda::device_default_memory_pool(stream.device());
-    const auto policy = cuda::execution::__cub_par_unseq.with_stream(stream).with_memory_resource(device_resource);
+    const auto policy                            = cuda::execution::__cub_par_unseq.with(cuda::get_stream, stream)
+                          .with(cuda::mr::get_memory_resource, device_resource);
     test_equal(policy);
   }
 }
@@ -201,14 +202,14 @@ C2H_TEST("cuda::std::equal(first1, last1, first2, last2, pred)", "[parallel algo
   SECTION("with provided stream")
   {
     cuda::stream stream{cuda::device_ref{0}};
-    const auto policy = cuda::execution::__cub_par_unseq.with_stream(stream);
+    const auto policy = cuda::execution::__cub_par_unseq.with(cuda::get_stream, stream);
     test_equal2(policy);
   }
 
   SECTION("with provided memory_resource")
   {
     cuda::device_memory_pool_ref device_resource = cuda::device_default_memory_pool(cuda::device_ref{0});
-    const auto policy = cuda::execution::__cub_par_unseq.with_memory_resource(device_resource);
+    const auto policy = cuda::execution::__cub_par_unseq.with(cuda::mr::get_memory_resource, device_resource);
     test_equal2(policy);
   }
 
@@ -216,7 +217,8 @@ C2H_TEST("cuda::std::equal(first1, last1, first2, last2, pred)", "[parallel algo
   {
     cuda::stream stream{cuda::device_ref{0}};
     cuda::device_memory_pool_ref device_resource = cuda::device_default_memory_pool(stream.device());
-    const auto policy = cuda::execution::__cub_par_unseq.with_stream(stream).with_memory_resource(device_resource);
+    const auto policy                            = cuda::execution::__cub_par_unseq.with(cuda::get_stream, stream)
+                          .with(cuda::mr::get_memory_resource, device_resource);
     test_equal2(policy);
   }
 }
