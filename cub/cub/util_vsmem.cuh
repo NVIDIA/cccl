373baae569957f52926483d79f52ec66f229c124
Allison Piper <alliepiper16@gmail.com>
2026-03-25 16:32:30 -0400

    Revert unnecessary util_vsmem.cuh doc comment change

--- cub/cub/util_vsmem.cuh
+++ cub/cub/util_vsmem.cuh
@@ -119,20 +119,26 @@ public:
       static_cast<char*>(vsmem.gmem_ptr) + (vsmem_per_block * linear_block_id));
   }
 
-  // Hints to discard modified cache lines of the used virtual shared memory.
-  //
-  // Needs to be followed by `__syncthreads()` if the function returns true and the virtual shared memory is
-  // supposed to be reused after this function call.
+  /**
+   * @brief Hints to discard modified cache lines of the used virtual shared memory.
+   * modified cache lines.
+   *
+   * @note Needs to be followed by `__syncthreads()` if the function returns true and the virtual shared memory is
+   * supposed to be reused after this function call.
+   */
   template <bool needs_vsmem_ = needs_vsmem, ::cuda::std::enable_if_t<!needs_vsmem_, int> = 0>
   static _CCCL_DEVICE _CCCL_FORCEINLINE bool discard_temp_storage(typename AgentT::TempStorage& temp_storage)
   {
     return false;
   }
 
-  // Hints to discard modified cache lines of the used virtual shared memory.
-  //
-  // Needs to be followed by `__syncthreads()` if the function returns true and the virtual shared memory is
-  // supposed to be reused after this function call.
+  /**
+   * @brief Hints to discard modified cache lines of the used virtual shared memory.
+   * modified cache lines.
+   *
+   * @note Needs to be followed by `__syncthreads()` if the function returns true and the virtual shared memory is
+   * supposed to be reused after this function call.
+   */
   template <bool needs_vsmem_ = needs_vsmem, ::cuda::std::enable_if_t<needs_vsmem_, int> = 0>
   static _CCCL_DEVICE _CCCL_FORCEINLINE bool discard_temp_storage(typename AgentT::TempStorage& temp_storage)
   {
