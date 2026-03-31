a278094db302184851e5e7ac2e8f248573b995c4
Michael Schellenberger Costa <miscco@nvidia.com>
2026-03-31 09:48:55 +0200

    Start at 0

--- libcudacxx/include/cuda/std/__fwd/execution_policy.h
+++ libcudacxx/include/cuda/std/__fwd/execution_policy.h
@@ -48,13 +48,13 @@ enum __execution_backend : uint8_t
   // The backends we provide
   __none = 0,
 #if _CCCL_HAS_BACKEND_CUDA()
-  __cuda = 1 << 1,
+  __cuda = 1 << 0,
 #endif // _CCCL_HAS_BACKEND_CUDA()
 #if _CCCL_HAS_BACKEND_OMP()
-  __omp = 1 << 2,
+  __omp = 1 << 1,
 #endif // _CCCL_HAS_BACKEND_OMP()
 #if _CCCL_HAS_BACKEND_TBB()
-  __tbb = 1 << 3,
+  __tbb = 1 << 2,
 #endif // _CCCL_HAS_BACKEND_TBB()
 };
 
