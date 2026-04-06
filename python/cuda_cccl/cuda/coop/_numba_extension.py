86c2b4c7928d17935d19a4d17d0c887e5f903c64
Trent Nelson <trent@trent.me>
2026-04-06 13:59:37 -0700

    cuda.coop: add missing numba extension header

--- python/cuda_cccl/cuda/coop/_numba_extension.py
+++ python/cuda_cccl/cuda/coop/_numba_extension.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 """
 Initialise the cuda-cccl cooperative plug-in for Numba.
 
