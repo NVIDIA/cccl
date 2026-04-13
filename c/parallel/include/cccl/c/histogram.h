a91caecb0ca9b0f75d3ab22bf1f9f39175812a6e
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-04-13 12:45:29 -0400

    modernize-use-using

--- c/parallel/include/cccl/c/histogram.h
+++ c/parallel/include/cccl/c/histogram.h
@@ -9,6 +9,7 @@
 //===----------------------------------------------------------------------===//
 
 #pragma once
+// NOLINTBEGIN(modernize-use-using)
 
 #ifndef CCCL_C_EXPERIMENTAL
 #  error "C exposure is experimental and subject to change. Define CCCL_C_EXPERIMENTAL to acknowledge this notice."
@@ -94,3 +95,4 @@ CCCL_C_API CUresult cccl_device_histogram_even(
 CCCL_C_API CUresult cccl_device_histogram_cleanup(cccl_device_histogram_build_result_t* bld_ptr);
 
 CCCL_C_EXTERN_C_END
+// NOLINTEND(modernize-use-using)
