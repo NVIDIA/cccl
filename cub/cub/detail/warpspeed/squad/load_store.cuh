442f5979437ead02271e304a00a7223db21e4189
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-04-14 12:53:05 +0200

    miscco

--- cub/cub/detail/warpspeed/squad/load_store.cuh
+++ cub/cub/detail/warpspeed/squad/load_store.cuh
@@ -288,7 +288,7 @@ squadStoreBulkSync(Squad squad, CpAsyncOobInfo<OutputT> cpAsyncOobInfo, const ::
         // nvc++ seems to have an optimizer bug, crashing with an unaligned access error below. The addresses are fine
         // when printed, so let's shake the optimizer a bit.
         asm volatile("" : "+l"(cpAsyncOobInfo.ptrGmemEndAlignDown));
-#  endif
+#  endif // _CCCL_CUDA_COMPILER(NVHPC)
 
         // Copy a subset of the last 16 bytes
         if (::cuda::ptx::elect_sync(~0))
