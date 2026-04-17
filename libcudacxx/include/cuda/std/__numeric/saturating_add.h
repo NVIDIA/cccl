f6aa82eaf4f4005d3900779d1fb50265d8681af2
Michael Schellenberger Costa <miscco@nvidia.com>
2026-04-17 18:57:21 +0200

    [Tile] Avoid using `asm` optimizations for saturating arithmetics in tile mode

--- libcudacxx/include/cuda/std/__numeric/saturating_add.h
+++ libcudacxx/include/cuda/std/__numeric/saturating_add.h
@@ -204,12 +204,14 @@ _CCCL_TEMPLATE(class _Tp)
 _CCCL_REQUIRES(__cccl_is_integer_v<_Tp>)
 [[nodiscard]] _CCCL_API constexpr _Tp saturating_add(_Tp __x, _Tp __y) noexcept
 {
+#if !_CCCL_TILE_COMPILATION() // error: asm statement is unsupported in tile code
   _CCCL_IF_NOT_CONSTEVAL_DEFAULT
   {
     NV_IF_ELSE_TARGET(NV_IS_HOST,
                       (return ::cuda::std::__saturating_add_impl_host(__x, __y);),
                       (return ::cuda::std::__saturating_add_impl_device(__x, __y);))
   }
+#endif // !_CCCL_TILE_COMPILATION()
   return ::cuda::saturating_add_overflow(__x, __y).value;
 }
 
