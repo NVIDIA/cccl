46e1e2e4ffafae532fbcca337db4ff7eb807cea6
edenfunf <aa9736195201@gmail.com>
2026-04-21 22:14:30 +0800

    style: clang-format is_partitioned benchmark

--- thrust/benchmarks/bench/is_partitioned/basic.cu
+++ thrust/benchmarks/bench/is_partitioned/basic.cu
@@ -28,11 +28,11 @@ static void basic(nvbench::state& state, nvbench::type_list<T>)
 
   caching_allocator_t alloc{};
 
-  state.exec(
-    nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
-      do_not_optimize(thrust::is_partitioned(
-        policy(alloc, launch), dinput.begin(), dinput.end(), select_op_t{static_cast<T>(mismatch_point)}));
-    });
+  state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync,
+             [&](nvbench::launch& launch) {
+               do_not_optimize(thrust::is_partitioned(
+                 policy(alloc, launch), dinput.begin(), dinput.end(), select_op_t{static_cast<T>(mismatch_point)}));
+             });
 }
 
 NVBENCH_BENCH_TYPES(basic, NVBENCH_TYPE_AXES(fundamental_types))
