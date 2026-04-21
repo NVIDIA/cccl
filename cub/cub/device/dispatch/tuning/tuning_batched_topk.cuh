db820b61484ac2d5f01c1795519d6b14c9ba12d5
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-04-21 11:29:19 +0200

    Fix includes

--- cub/cub/device/dispatch/tuning/tuning_batched_topk.cuh
+++ cub/cub/device/dispatch/tuning/tuning_batched_topk.cuh
@@ -13,9 +13,6 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cub/agent/agent_batched_topk.cuh>
-#include <cub/util_device.cuh>
-
 #include <cuda/__device/arch_id.h>
 #include <cuda/std/__host_stdlib/ostream>
 #include <cuda/std/array>
