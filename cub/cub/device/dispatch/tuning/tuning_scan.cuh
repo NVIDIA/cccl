8580c8f6e92b4818f51889918580018587f09335
gonidelis <ggonidelis@nvidia.com>
2026-04-23 14:12:43 -0700

    remove i128 tunings from sm120

--- cub/cub/device/dispatch/tuning/tuning_scan.cuh
+++ cub/cub/device/dispatch/tuning/tuning_scan.cuh
@@ -927,18 +927,6 @@ struct policy_selector
   {
     if (arch >= ::cuda::arch_id::sm_120)
     {
-      // tunings from cub.bench.scan.exclusive.sum.warpspeed[T{ct}=I128, OffsetT{ct}=I64] on B200
-      if (operation_t == op_kind_t::plus && accum_is_primitive_or_trivially_copy_constructible)
-      {
-        switch (input_value_size)
-        {
-          case 16:
-            // wrps_5.lbi_8.ipt_16 (b200-077_i128/3.db)  1.159883  1.000000  1.143709  1.275821
-            return scan_warpspeed_policy{5, 8, 16 - 1};
-          default:
-            break;
-        }
-      }
       return get_sm120_fallback_warpspeed_policy();
     }
     if (arch >= ::cuda::arch_id::sm_100)
