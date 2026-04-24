52698efde345fa82b2d15c1ee962040912b3bd62
David Bayer <bayer.david99@gmail.com>
2026-04-24 10:13:09 +0200

    [libcu++] Fix `cuda/barrier/cp_async_bulk_tensor_1d.pass.cpp` for `a` and `f` archs

--- libcudacxx/test/libcudacxx/cuda/barrier/cp_async_bulk_tensor_1d.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/barrier/cp_async_bulk_tensor_1d.pass.cpp
@@ -35,11 +35,11 @@
 // cuda::std::array cannot be shared between host and device as some of its
 // member functions take a const reference, which is unsupported by nvcc.
 constexpr cuda::std::array<uint64_t, 1> GMEM_DIMS{256};
-TEST_FUNC constexpr cuda::std::array<uint64_t, 1> GMEM_DIMS_DEV{256};
+TEST_GLOBAL_VARIABLE constexpr cuda::std::array<uint64_t, 1> GMEM_DIMS_DEV{256};
 constexpr cuda::std::array<uint32_t, 1> SMEM_DIMS{32};
-TEST_FUNC constexpr cuda::std::array<uint32_t, 1> SMEM_DIMS_DEV{32};
+TEST_GLOBAL_VARIABLE constexpr cuda::std::array<uint32_t, 1> SMEM_DIMS_DEV{32};
 
-TEST_FUNC constexpr cuda::std::array<uint32_t, 1> TEST_SMEM_COORDS[] = {{0}, {4}, {8}};
+TEST_GLOBAL_VARIABLE constexpr cuda::std::array<uint32_t, 1> TEST_SMEM_COORDS[] = {{0}, {4}, {8}};
 
 constexpr size_t gmem_len = tensor_len(GMEM_DIMS);
 constexpr size_t smem_len = tensor_len(SMEM_DIMS);
