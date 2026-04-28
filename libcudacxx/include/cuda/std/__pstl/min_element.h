0a98b01438ae91ff77f1751891bf0d8b8e2e7a9b
Michael Schellenberger Costa <miscco@nvidia.com>
2026-04-28 11:10:08 +0200

    This implements the `{min,max}_element` algorithms for the cuda backend.
      * `std::max_element` see https://en.cppreference.com/w/cpp/algorithm/max_element.html
      * `std::max_element` see https://en.cppreference.com/w/cpp/algorithm/min_element.html
    
    It provides tests and benchmarks similar to Thrust and some boilerplate for libcu++
    
    The functionality is publicly available yet and implemented in a private internal header
    
    Fixes #7751

--- libcudacxx/include/cuda/std/__pstl/min_element.h
+++ libcudacxx/include/cuda/std/__pstl/min_element.h
@@ -8,8 +8,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _CUDA_STD___PSTL_FIND_H
-#define _CUDA_STD___PSTL_FIND_H
+#ifndef _CUDA_STD___PSTL_MIN_ELEMENT_H
+#define _CUDA_STD___PSTL_MIN_ELEMENT_H
 
 #include <cuda/std/detail/__config>
 
@@ -23,22 +23,19 @@
 
 #if _CCCL_HOSTED()
 
-#  include <cuda/__functional/equal_to_value.h>
 #  include <cuda/__nvtx/nvtx.h>
-#  include <cuda/std/__algorithm/find.h>
+#  include <cuda/std/__algorithm/min_element.h>
 #  include <cuda/std/__concepts/concept_macros.h>
 #  include <cuda/std/__execution/policy.h>
 #  include <cuda/std/__iterator/concepts.h>
 #  include <cuda/std/__iterator/iterator_traits.h>
 #  include <cuda/std/__pstl/dispatch.h>
 #  include <cuda/std/__type_traits/always_false.h>
-#  include <cuda/std/__type_traits/is_comparable.h>
 #  include <cuda/std/__type_traits/is_execution_policy.h>
-#  include <cuda/std/__type_traits/is_nothrow_copy_constructible.h>
 #  include <cuda/std/__utility/move.h>
 
 #  if _CCCL_HAS_BACKEND_CUDA()
-#    include <cuda/std/__pstl/cuda/find_if.h>
+#    include <cuda/std/__pstl/cuda/min_element.h>
 #  endif // _CCCL_HAS_BACKEND_CUDA()
 
 #  include <cuda/std/__cccl/prologue.h>
@@ -47,32 +44,28 @@ _CCCL_BEGIN_NAMESPACE_CUDA_STD
 
 _CCCL_BEGIN_NAMESPACE_ARCH_DEPENDENT
 
-_CCCL_TEMPLATE(class _Policy, class _Iter, class _Tp)
+_CCCL_TEMPLATE(class _Policy, class _Iter, class _BinaryPredicate = less<>)
 _CCCL_REQUIRES(__has_forward_traversal<_Iter> _CCCL_AND is_execution_policy_v<_Policy>)
 [[nodiscard]] _CCCL_HOST_API _Iter
-find([[maybe_unused]] const _Policy& __policy, _Iter __first, _Iter __last, const _Tp& __val)
+min_element([[maybe_unused]] const _Policy& __policy, _Iter __first, _Iter __last, _BinaryPredicate __pred = {})
 {
-  static_assert(__is_cpp17_equality_comparable_v<_Tp, iter_value_t<_Iter>>,
-                "Parallel cuda::std::find requires that T is equality comparable with iter_value_t<Iter>");
-
   [[maybe_unused]] auto __dispatch =
-    ::cuda::std::execution::__pstl_select_dispatch<::cuda::std::execution::__pstl_algorithm::__find_if, _Policy>();
+    ::cuda::std::execution::__pstl_select_dispatch<::cuda::std::execution::__pstl_algorithm::__min_element, _Policy>();
   if constexpr (::cuda::std::execution::__pstl_can_dispatch<decltype(__dispatch)>)
   {
-    _CCCL_NVTX_RANGE_SCOPE("cuda::std::find");
+    _CCCL_NVTX_RANGE_SCOPE("cuda::std::min_element");
 
     if (__first == __last)
     {
       return __first;
     }
 
-    return __dispatch(
-      __policy, ::cuda::std::move(__first), ::cuda::std::move(__last), ::cuda::equal_to_value<_Tp>{__val});
+    return __dispatch(__policy, ::cuda::std::move(__first), ::cuda::std::move(__last), ::cuda::std::move(__pred));
   }
   else
   {
-    static_assert(__always_false_v<_Policy>, "Parallel cuda::std::find requires at least one selected backend");
-    return ::cuda::std::find(::cuda::std::move(__first), ::cuda::std::move(__last), __val);
+    static_assert(__always_false_v<_Policy>, "Parallel cuda::std::min_element requires at least one selected backend");
+    return ::cuda::std::min_element(::cuda::std::move(__first), ::cuda::std::move(__last), ::cuda::std::move(__pred));
   }
 }
 
@@ -84,4 +77,4 @@ _CCCL_END_NAMESPACE_CUDA_STD
 
 #endif // _CCCL_HOSTED()
 
-#endif // _CUDA_STD___PSTL_FIND_H
+#endif // _CUDA_STD___PSTL_MIN_ELEMENT_H
