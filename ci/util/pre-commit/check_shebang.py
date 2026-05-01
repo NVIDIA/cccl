5118991eebf41468a38ad068f35ae5d9bc3a9286
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-05-01 19:17:27 -0400

    fixup! Replace /bin/bash with /usr/bin/env bash

---
+++ ci/util/pre-commit/check_shebang.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+
+def main() -> int:
+    # We pre-compile this regular expression as a micro-optimization for speed. The
+    # assumption is that the shebang is correct, so everything in the happy path should be
+    # as fast as possible.
+    FIRST_LINE_RE = re.compile(r"#!\s*/usr/bin/env.*")
+    ret = 0
+    for f in sys.argv[1:]:
+        with open(f) as fd:
+            first_line = fd.readline()
+
+        if not first_line.startswith("#!"):
+            # Not a shebang
+            continue
+
+        if FIRST_LINE_RE.match(first_line):
+            # Already correct
+            continue
+
+        ret = 1
+
+        if not (
+            m := re.match(r"#!\s*(?:/bin/(\w+)|/usr/bin/(\w+))\s*(.*)", first_line)
+        ):
+            # Not assert, pre-commit may compile with -O
+            raise AssertionError(f"Failed to match shebang for {first_line}")
+
+        with open(f) as fd:
+            # Read the remaining lines, we need them in order to overwrite
+            lines = fd.readlines()
+
+        lines[0] = f"#!/usr/bin/env {m[1] or m[2]} {m[3]}\n"
+
+        with open(f, "w") as fd:
+            fd.writelines(lines)
+
+    return ret
+
+
+if __name__ == "__main__":
+    sys.exit(main())
