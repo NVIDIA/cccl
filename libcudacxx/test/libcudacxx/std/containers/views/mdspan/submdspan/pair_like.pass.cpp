d45ce86a1ceecea678c84a6e3882239f0ba11842
Michael Schellenberger Costa <miscco@nvidia.com>
2026-05-05 10:22:22 +0200

    [Tile] Disable more tests that rely on return statement inside loop

--- libcudacxx/test/libcudacxx/std/containers/views/mdspan/submdspan/pair_like.pass.cpp
+++ libcudacxx/test/libcudacxx/std/containers/views/mdspan/submdspan/pair_like.pass.cpp
@@ -7,6 +7,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+// XFAIL: enable-tile
+// error: a return statement inside a loop is not currently supported in a tile function
+
 // <mdspan>
 
 #include <cuda/std/array>
