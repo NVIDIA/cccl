8dbc400afdaaae1249577bd483ece285fe779144
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-05-07 11:44:30 +0200

    More tests

--- cub/test/catch2_test_device_reduce_env.cu
+++ cub/test/catch2_test_device_reduce_env.cu
@@ -46,12 +46,24 @@ struct reduce_tuning
   }
 };
 
-struct unrelated_nondeterminisitc_reduce_tuning
+template <int ThreadsPerBlock>
+struct nondeterministic_reduce_tuning
 {
   _CCCL_API constexpr auto operator()(cuda::compute_capability) const
     -> cub::detail::reduce_nondeterministic::reduce_nondeterministic_policy
   {
-    return {}; // just zero everything
+    return {cub::detail::reduce::agent_reduce_policy{
+      ThreadsPerBlock, 1, 1, cub::BLOCK_REDUCE_WARP_REDUCTIONS_NONDETERMINISTIC, cub::LOAD_DEFAULT}};
+  }
+};
+
+template <int ThreadsPerBlock>
+struct deterministic_reduce_tuning
+{
+  _CCCL_API constexpr auto operator()(cuda::compute_capability) const -> cub::detail::rfa::rfa_policy
+  {
+    return {cub::detail::rfa::reduce_policy{ThreadsPerBlock, 1, cub::BLOCK_REDUCE_WARP_REDUCTIONS},
+            cub::detail::rfa::single_tile_policy{ThreadsPerBlock, 1, cub::BLOCK_REDUCE_WARP_REDUCTIONS}};
   }
 };
 
@@ -70,12 +82,12 @@ struct unrelated_tuning
 using block_sizes =
   c2h::type_list<cuda::std::integral_constant<unsigned int, 32>, cuda::std::integral_constant<unsigned int, 64>>;
 
+using block_size_check_plus_t = block_size_extracting_op<cuda::std::plus<>>;
+
 // Launcher helper always passes an environment.
 // We need a test of simple use to check if default environment works.
 // ifdef it out not to spend time compiling and running it twice.
 #if TEST_LAUNCH == 0
-using block_size_check_t = block_size_extracting_op<cuda::std::plus<>>;
-
 TEST_CASE("Device reduce works with default environment", "[reduce][device]")
 {
   using num_items_t = int;
@@ -89,12 +101,12 @@ TEST_CASE("Device reduce works with default environment", "[reduce][device]")
   REQUIRE(cudaSuccess == cub::detail::ptx_compute_cap(cc, current_device));
 
   unsigned int target_block_size =
-    cub::detail::reduce::policy_selector_from_types<value_t, offset_t, block_size_check_t>{}(cc)
+    cub::detail::reduce::policy_selector_from_types<value_t, offset_t, block_size_check_plus_t>{}(cc)
       .single_tile.threads_per_block;
 
   num_items_t num_items = 1;
   c2h::device_vector<unsigned int> d_block_size(1);
-  block_size_check_t block_size_check{thrust::raw_pointer_cast(d_block_size.data())};
+  block_size_check_plus_t block_size_check{thrust::raw_pointer_cast(d_block_size.data())};
   auto d_in  = cuda::constant_iterator(value_t{1});
   auto d_out = thrust::device_vector<value_t>(1);
 
@@ -109,7 +121,6 @@ TEST_CASE("Device Sum works with default environment", "[reduce][device]")
 {
   using num_items_t = int;
   using value_t     = int;
-  using offset_t    = cub::detail::choose_offset_t<num_items_t>;
 
   int current_device{};
   REQUIRE(cudaSuccess == cudaGetDevice(&current_device));
@@ -125,36 +136,94 @@ TEST_CASE("Device Sum works with default environment", "[reduce][device]")
   REQUIRE(cudaSuccess == cub::DeviceReduce::Sum(d_in, d_out.begin(), num_items));
   REQUIRE(d_out[0] == num_items);
 }
+#endif
 
+#if TEST_LAUNCH != 1
 C2H_TEST("Device reduce can be tuned", "[reduce][device]", block_sizes)
 {
   constexpr unsigned int target_block_size = c2h::get<0, TestType>::value;
   c2h::device_vector<unsigned int> d_block_size(1);
-  block_size_check_t block_size_check{thrust::raw_pointer_cast(d_block_size.data())};
 
-  auto num_items = 1;
-  auto d_in      = cuda::constant_iterator(1);
-  auto d_out     = thrust::device_vector<int>(1);
+  auto d_in  = block_size_extracting_constant_iterator(1, thrust::raw_pointer_cast(d_block_size.data()));
+  auto d_out = thrust::device_vector<int>(1);
 
-  // We are expecting that `unrelated_tuning` is ignored
   auto env = cuda::execution::tune(
-    reduce_tuning<target_block_size>{},
-    unrelated_tuning{}
-#  if _CCCL_CUDA_COMPILER(NVCC, >=, 12, 9)
-    // some rare combinations, like nvcc 12.0 + clang-14 in C++20, or nvcc 12.0 + GCC12 fail with:
-    // pod_tuple.h(130): error: Internal Compiler Error (codegen): "internal error during structure layout!"
-    ,
-    unrelated_nondeterminisitc_reduce_tuning{}
-#  endif // _CCCL_CUDA_COMPILER(NVCC, >=, 12, 9)
+    reduce_tuning<target_block_size>{}, // <-- should be taken
+    unrelated_tuning{}, // should be ignored
+    nondeterministic_reduce_tuning<target_block_size * 2>{}, // should be ignored
+    deterministic_reduce_tuning<target_block_size * 2>{} // should be ignored
   );
 
-  REQUIRE(cudaSuccess == cub::DeviceReduce::Reduce(d_in, d_out.begin(), num_items, block_size_check, 0, env));
-  REQUIRE(d_out[0] == num_items);
+  device_reduce(d_in, d_out.begin(), 1, ::cuda::std::plus<>{}, 0, env);
+  REQUIRE(d_out[0] == 1);
   REQUIRE(d_block_size[0] == target_block_size);
 }
-#endif
 
-#if TEST_LAUNCH != 1
+C2H_TEST("Device reduce not_guaranteed can be tuned", "[reduce][device]", block_sizes)
+{
+  constexpr unsigned int target_block_size = c2h::get<0, TestType>::value;
+  c2h::device_vector<unsigned int> d_block_size(1);
+
+  auto d_in  = block_size_extracting_constant_iterator(1, thrust::raw_pointer_cast(d_block_size.data()));
+  auto d_out = thrust::device_vector<int>(1);
+
+  auto env = cuda::std::execution::env{
+    cuda::execution::require(cuda::execution::determinism::not_guaranteed),
+    cuda::execution::tune(
+      reduce_tuning<target_block_size * 2>{}, // should be ignored
+      unrelated_tuning{}, // should be ignored
+      nondeterministic_reduce_tuning<target_block_size>{}, // <-- should be taken
+      deterministic_reduce_tuning<target_block_size * 2>{} // should be ignored
+      )};
+
+  device_reduce(d_in, d_out.begin(), 1, ::cuda::std::plus<>{}, 0, env);
+  REQUIRE(d_out[0] == 1);
+  REQUIRE(d_block_size[0] == target_block_size);
+}
+C2H_TEST("Device reduce run_to_run can be tuned", "[reduce][device]", block_sizes)
+{
+  constexpr unsigned int target_block_size = c2h::get<0, TestType>::value;
+  c2h::device_vector<unsigned int> d_block_size(1);
+
+  auto d_in  = block_size_extracting_constant_iterator(1, thrust::raw_pointer_cast(d_block_size.data()));
+  auto d_out = thrust::device_vector<int>(1);
+
+  auto env = cuda::std::execution::env{
+    cuda::execution::require(cuda::execution::determinism::run_to_run),
+    cuda::execution::tune(
+      reduce_tuning<target_block_size>{}, // <-- should be taken
+      unrelated_tuning{}, // should be ignored
+      nondeterministic_reduce_tuning<target_block_size * 2>{}, // should be ignored
+      deterministic_reduce_tuning<target_block_size * 2>{} // should be ignored
+      )};
+
+  device_reduce(d_in, d_out.begin(), 1, ::cuda::std::plus<>{}, 0, env);
+  REQUIRE(d_out[0] == 1);
+  REQUIRE(d_block_size[0] == target_block_size);
+}
+
+C2H_TEST("Device reduce gpu_to_gpu can be tuned", "[reduce][device]", block_sizes)
+{
+  constexpr unsigned int target_block_size = c2h::get<0, TestType>::value;
+  c2h::device_vector<unsigned int> d_block_size(1);
+
+  auto d_in  = block_size_extracting_constant_iterator(1, thrust::raw_pointer_cast(d_block_size.data()));
+  auto d_out = thrust::device_vector<int>(1);
+
+  auto env = cuda::std::execution::env{
+    cuda::execution::require(cuda::execution::determinism::gpu_to_gpu),
+    cuda::execution::tune(
+      reduce_tuning<target_block_size * 2>{}, // should be ignored
+      unrelated_tuning{}, // should be ignored
+      nondeterministic_reduce_tuning<target_block_size * 2>{}, // // should be ignored
+      deterministic_reduce_tuning<target_block_size>{} // <-- should be taken
+      )};
+
+  device_reduce(d_in, d_out.begin(), 1, ::cuda::std::plus<float>{}, 0, env); // make accum_t float to select RFA
+  REQUIRE(d_out[0] == 1);
+  REQUIRE(d_block_size[0] == target_block_size);
+}
+
 C2H_TEST("Device Sum can be tuned", "[reduce][device]", block_sizes)
 {
   constexpr unsigned int target_block_size = c2h::get<0, TestType>::value;
@@ -208,15 +277,14 @@ C2H_TEST("Device TransformReduce can be tuned", "[reduce][device]", block_sizes)
   auto d_in  = block_size_extracting_constant_iterator(1, thrust::raw_pointer_cast(d_block_size.data()));
   auto d_out = c2h::device_vector<int>(1);
 
-  // We are expecting that `unrelated_tuning` is ignored
   auto env = cuda::execution::tune(
     reduce_tuning<target_block_size>{},
-    unrelated_tuning{}
+    unrelated_tuning{} // should be ignored
 #  if _CCCL_CUDA_COMPILER(NVCC, >=, 12, 9)
     // some rare combinations, like nvcc 12.0 + clang-14 in C++20, or nvcc 12.0 + GCC12 fail with:
     // pod_tuple.h(130): error: Internal Compiler Error (codegen): "internal error during structure layout!"
     ,
-    unrelated_nondeterminisitc_reduce_tuning{}
+    nondeterministic_reduce_tuning<target_block_size * 2>{} // should be ignored
 #  endif // _CCCL_CUDA_COMPILER(NVCC, >=, 12, 9)
   );
 
