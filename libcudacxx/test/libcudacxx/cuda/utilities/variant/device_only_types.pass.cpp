bb0d90de667c601016148a0ee131dd4a1d99a740
Michael Schellenberger Costa <miscco@nvidia.com>
2026-05-13 14:20:45 +0200

    [Tile] Enable tests that relies on `__builtin_unreachable`
    The compiler team has implemented the builtin in tile mode, so we can use it now

--- libcudacxx/test/libcudacxx/cuda/utilities/variant/device_only_types.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/utilities/variant/device_only_types.pass.cpp
@@ -7,9 +7,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-// XFAIL: enable-tile
-// nvbug6067464: error: Internal Compiler Error (tile codegen): "call to unknown tile builtin function!
-
 #include <cuda/std/cassert>
 #include <cuda/std/variant>
 
