efd46684a85ec310937caa684dc05178c98cfb10
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-05-13 17:43:17 +0200

    revert

--- cub/cub/detail/temporary_storage.cuh
+++ cub/cub/detail/temporary_storage.cuh
@@ -347,7 +347,6 @@ private:
   }
 };
 
-_CCCL_EXEC_CHECK_DISABLE
 template <typename MRT>
 CUB_RUNTIME_FUNCTION cudaError_t
 allocate(::cuda::stream_ref stream, void*& d_temp_storage, size_t temp_storage_bytes, MRT& mr)
@@ -363,7 +362,6 @@ allocate(::cuda::stream_ref stream, void*& d_temp_storage, size_t temp_storage_b
   }
 }
 
-_CCCL_EXEC_CHECK_DISABLE
 template <typename MRT>
 CUB_RUNTIME_FUNCTION cudaError_t
 deallocate(::cuda::stream_ref stream, void* d_temp_storage, size_t temp_storage_bytes, MRT& mr)
