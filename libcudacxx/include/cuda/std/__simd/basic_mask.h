b15584063442dbdd60c1dccebbcff398708f701f
Michael Schellenberger Costa <miscco@nvidia.com>
2026-05-15 14:26:59 +0200

    [Tile] Mark `simd` as `_CCCL_HOST_DEVICE` (#9013)
    * [Tile] avoid use of CPO in variant
    
    We cannot access device variables in a tile program, so we should just create the empty CPO in place
    
    * [Tile] Mark `simd` as `_CCCL_HOST_DEVICE`
    
    it relies on asm and manuall loop unrolling a lot

--- libcudacxx/include/cuda/std/__simd/basic_mask.h
+++ libcudacxx/include/cuda/std/__simd/basic_mask.h
@@ -84,7 +84,7 @@ class basic_mask<_Bytes, _Abi, enable_if_t<__is_vectorizable_byte_size_v<_Bytes>
   {};
   static constexpr __storage_tag_t __storage_tag{};
 
-  _CCCL_API constexpr basic_mask(const _Storage __v, __storage_tag_t) noexcept
+  _CCCL_HOST_DEVICE_API constexpr basic_mask(const _Storage __v, __storage_tag_t) noexcept
       : __s_{__v}
   {}
 
@@ -95,27 +95,27 @@ public:
   using iterator       = __simd_iterator<basic_mask>;
   using const_iterator = __simd_iterator<const basic_mask>;
 
-  [[nodiscard]] _CCCL_API constexpr iterator begin() noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr iterator begin() noexcept
   {
     return {*this, 0};
   }
 
-  [[nodiscard]] _CCCL_API constexpr const_iterator begin() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr const_iterator begin() const noexcept
   {
     return {*this, 0};
   }
 
-  [[nodiscard]] _CCCL_API constexpr const_iterator cbegin() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr const_iterator cbegin() const noexcept
   {
     return {*this, 0};
   }
 
-  [[nodiscard]] _CCCL_API constexpr default_sentinel_t end() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr default_sentinel_t end() const noexcept
   {
     return {};
   }
 
-  [[nodiscard]] _CCCL_API constexpr default_sentinel_t cend() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr default_sentinel_t cend() const noexcept
   {
     return {};
   }
@@ -131,13 +131,13 @@ public:
 
   _CCCL_TEMPLATE(typename _Up)
   _CCCL_REQUIRES(same_as<_Up, value_type>)
-  _CCCL_API constexpr explicit basic_mask(const _Up __v) noexcept
+  _CCCL_HOST_DEVICE_API constexpr explicit basic_mask(const _Up __v) noexcept
       : __s_{_Impl::__broadcast(__v)}
   {}
 
   _CCCL_TEMPLATE(size_t _UBytes, typename _UAbi)
   _CCCL_REQUIRES((__simd_size_v<__integer_from<_UBytes>, _UAbi> == __size))
-  _CCCL_API constexpr explicit basic_mask(const basic_mask<_UBytes, _UAbi>& __x) noexcept
+  _CCCL_HOST_DEVICE_API constexpr explicit basic_mask(const basic_mask<_UBytes, _UAbi>& __x) noexcept
   {
     _CCCL_PRAGMA_UNROLL_FULL()
     for (__simd_size_type __i = 0; __i < __size; ++__i)
@@ -148,13 +148,13 @@ public:
 
   _CCCL_TEMPLATE(typename _Generator)
   _CCCL_REQUIRES(__can_generate_v<bool, _Generator, __size>)
-  _CCCL_API constexpr explicit basic_mask(_Generator&& __g)
+  _CCCL_HOST_DEVICE_API constexpr explicit basic_mask(_Generator&& __g)
       : __s_{_Impl::__generate(__g)}
   {}
 
   _CCCL_TEMPLATE(typename _Tp)
   _CCCL_REQUIRES(same_as<_Tp, bitset<__usize>>)
-  _CCCL_API constexpr basic_mask(const _Tp& __b) noexcept
+  _CCCL_HOST_DEVICE_API constexpr basic_mask(const _Tp& __b) noexcept
       : __s_{_Impl::__broadcast(false)}
   {
     _CCCL_PRAGMA_UNROLL_FULL()
@@ -166,7 +166,7 @@ public:
 
   _CCCL_TEMPLATE(typename _Tp)
   _CCCL_REQUIRES(is_integral_v<_Tp> _CCCL_AND is_unsigned_v<_Tp> _CCCL_AND(!is_same_v<_Tp, value_type>))
-  _CCCL_API constexpr explicit basic_mask(const _Tp __val) noexcept
+  _CCCL_HOST_DEVICE_API constexpr explicit basic_mask(const _Tp __val) noexcept
       : __s_{_Impl::__broadcast(false)}
   {
     constexpr auto __num_bits = __simd_size_type{__num_bits_v<_Tp>};
@@ -180,7 +180,7 @@ public:
 
   // [simd.mask.subscr], basic_mask subscript operators
 
-  [[nodiscard]] _CCCL_API constexpr value_type operator[](const __simd_size_type __i) const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr value_type operator[](const __simd_size_type __i) const noexcept
   {
     _CCCL_ASSERT(::cuda::in_range(__i, __simd_size_type{0}, __size), "Index is out of bounds");
     return static_cast<bool>(__s_.__get(__i));
@@ -192,24 +192,24 @@ public:
 
   // [simd.mask.unary], basic_mask unary operators
 
-  [[nodiscard]] _CCCL_API constexpr basic_mask operator!() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr basic_mask operator!() const noexcept
   {
     return {_Impl::__bitwise_not(__s_), __storage_tag};
   }
 
   using __unary_return_t = basic_vec<__integer_from<_Bytes>, _Abi>;
 
-  [[nodiscard]] _CCCL_API constexpr __unary_return_t operator+() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr __unary_return_t operator+() const noexcept
   {
     return static_cast<__unary_return_t>(*this);
   }
 
-  [[nodiscard]] _CCCL_API constexpr __unary_return_t operator-() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr __unary_return_t operator-() const noexcept
   {
     return -static_cast<__unary_return_t>(*this);
   }
 
-  [[nodiscard]] _CCCL_API constexpr __unary_return_t operator~() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr __unary_return_t operator~() const noexcept
   {
     return ~static_cast<__unary_return_t>(*this);
   }
@@ -218,7 +218,7 @@ public:
 
   _CCCL_TEMPLATE(typename _Up, typename _Ap)
   _CCCL_REQUIRES((sizeof(_Up) != _Bytes && __simd_size_v<_Up, _Ap> == __size))
-  _CCCL_API constexpr explicit operator basic_vec<_Up, _Ap>() const noexcept
+  _CCCL_HOST_DEVICE_API constexpr explicit operator basic_vec<_Up, _Ap>() const noexcept
   {
     basic_vec<_Up, _Ap> __result{};
     _CCCL_PRAGMA_UNROLL_FULL()
@@ -231,7 +231,7 @@ public:
 
   _CCCL_TEMPLATE(typename _Up, typename _Ap)
   _CCCL_REQUIRES((sizeof(_Up) == _Bytes && __simd_size_v<_Up, _Ap> == __size))
-  _CCCL_API constexpr operator basic_vec<_Up, _Ap>() const noexcept
+  _CCCL_HOST_DEVICE_API constexpr operator basic_vec<_Up, _Ap>() const noexcept
   {
     basic_vec<_Up, _Ap> __result{};
     _CCCL_PRAGMA_UNROLL_FULL()
@@ -242,7 +242,7 @@ public:
     return __result;
   }
 
-  [[nodiscard]] _CCCL_API constexpr bitset<__usize> to_bitset() const noexcept
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr bitset<__usize> to_bitset() const noexcept
   {
     bitset<__usize> __result{};
     _CCCL_PRAGMA_UNROLL_FULL()
@@ -253,7 +253,7 @@ public:
     return __result;
   }
 
-  [[nodiscard]] _CCCL_API constexpr unsigned long long to_ullong() const
+  [[nodiscard]] _CCCL_HOST_DEVICE_API constexpr unsigned long long to_ullong() const
   {
     constexpr __simd_size_type __nbits = __num_bits_v<unsigned long long>;
     if constexpr (__size > __nbits)
@@ -268,31 +268,31 @@ public:
 
   // [simd.mask.binary], basic_mask binary operators
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator&&(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return {_Impl::__logic_and(__lhs.__s_, __rhs.__s_), __storage_tag};
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator||(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return {_Impl::__logic_or(__lhs.__s_, __rhs.__s_), __storage_tag};
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator&(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return {_Impl::__bitwise_and(__lhs.__s_, __rhs.__s_), __storage_tag};
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator|(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return {_Impl::__bitwise_or(__lhs.__s_, __rhs.__s_), __storage_tag};
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator^(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return {_Impl::__bitwise_xor(__lhs.__s_, __rhs.__s_), __storage_tag};
@@ -300,54 +300,54 @@ public:
 
   // [simd.mask.cassign], basic_mask compound assignment
 
-  _CCCL_API friend constexpr basic_mask& operator&=(basic_mask& __lhs, const basic_mask& __rhs) noexcept
+  _CCCL_HOST_DEVICE_API friend constexpr basic_mask& operator&=(basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return __lhs = __lhs & __rhs;
   }
 
-  _CCCL_API friend constexpr basic_mask& operator|=(basic_mask& __lhs, const basic_mask& __rhs) noexcept
+  _CCCL_HOST_DEVICE_API friend constexpr basic_mask& operator|=(basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return __lhs = __lhs | __rhs;
   }
 
-  _CCCL_API friend constexpr basic_mask& operator^=(basic_mask& __lhs, const basic_mask& __rhs) noexcept
+  _CCCL_HOST_DEVICE_API friend constexpr basic_mask& operator^=(basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return __lhs = __lhs ^ __rhs;
   }
 
   // [simd.mask.comparison], basic_mask comparisons (element-wise)
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator==(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return !(__lhs ^ __rhs);
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator!=(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return __lhs ^ __rhs;
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator>=(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return __lhs || !__rhs;
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator<=(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return !__lhs || __rhs;
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator>(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return __lhs && !__rhs;
   }
 
-  [[nodiscard]] _CCCL_API friend constexpr basic_mask
+  [[nodiscard]] _CCCL_HOST_DEVICE_API friend constexpr basic_mask
   operator<(const basic_mask& __lhs, const basic_mask& __rhs) noexcept
   {
     return !__lhs && __rhs;
