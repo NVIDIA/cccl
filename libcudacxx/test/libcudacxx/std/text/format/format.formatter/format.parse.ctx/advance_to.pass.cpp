0ddfbadd9a61d616ff61b3d47bb86988ccbed5eb
Michael Schellenberger Costa <miscco@nvidia.com>
2026-05-15 22:31:46 +0200

    [Tile] Mark format tests as unsupported (#9033)
    tile does not allow bit fields

--- libcudacxx/test/libcudacxx/std/text/format/format.formatter/format.parse.ctx/advance_to.pass.cpp
+++ libcudacxx/test/libcudacxx/std/text/format/format.formatter/format.parse.ctx/advance_to.pass.cpp
@@ -7,6 +7,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+// UNSUPPORTED: enable-tile
+// error: bit field read/write is unsupported in tile code
+
 // <cuda/std/format>
 
 // constexpr void advance_to(const_iterator it);
