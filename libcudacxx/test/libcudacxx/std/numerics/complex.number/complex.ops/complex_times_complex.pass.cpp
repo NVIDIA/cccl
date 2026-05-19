2d278a8da26c5c25a10d6abf260bf9b418b72c3e
Michael Schellenberger Costa <miscco@nvidia.com>
2026-05-19 17:33:25 +0200

    [Tile] Partially support `__half` and `__nv_bfloat16` in tile mode

--- libcudacxx/test/libcudacxx/std/numerics/complex.number/complex.ops/complex_times_complex.pass.cpp
+++ libcudacxx/test/libcudacxx/std/numerics/complex.number/complex.ops/complex_times_complex.pass.cpp
@@ -168,20 +168,20 @@ int main(int, char**)
 #    endif // _CCCL_HAS_LONG_DOUBLE()
 #  endif // !TEST_COMPILER(GCC, <, 8)
 #endif // _LIBCUDACXX_HAS_CONSTEXPR_COMPLEX_OPERATIONS()
-#if _LIBCUDACXX_HAS_NVFP16()
+#if _LIBCUDACXX_HAS_NVFP16() && !_CCCL_TILE_COMPILATION()
   test<__half>();
-#endif // _LIBCUDACXX_HAS_NVFP16()
-#if _LIBCUDACXX_HAS_NVBF16()
+#endif // _LIBCUDACXX_HAS_NVFP16() && !_CCCL_TILE_COMPILATION()
+#if _LIBCUDACXX_HAS_NVBF16() && !_CCCL_TILE_COMPILATION()
   test<__nv_bfloat16>();
-#endif // _LIBCUDACXX_HAS_NVBF16()
+#endif // _LIBCUDACXX_HAS_NVBF16() && !_CCCL_TILE_COMPILATION()
 
   test_edges<double>();
-#if _LIBCUDACXX_HAS_NVFP16()
+#if _LIBCUDACXX_HAS_NVFP16() && !_CCCL_TILE_COMPILATION()
   test_edges<__half>();
-#endif // _LIBCUDACXX_HAS_NVFP16()
-#if _LIBCUDACXX_HAS_NVBF16()
+#endif // _LIBCUDACXX_HAS_NVFP16() && !_CCCL_TILE_COMPILATION()
+#if _LIBCUDACXX_HAS_NVBF16() && !_CCCL_TILE_COMPILATION()
   test_edges<__nv_bfloat16>();
-#endif // _LIBCUDACXX_HAS_NVBF16()
+#endif // _LIBCUDACXX_HAS_NVBF16() && !_CCCL_TILE_COMPILATION()
 
   return 0;
 }
