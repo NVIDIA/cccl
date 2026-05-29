395b10e37d5feffcc9b6f9d5d526020519fdd609
fbusato <fbusato@nvidia.com>
2026-05-29 15:00:54 -0700

    Fix/Improve `warp_match_all` function to support bitwise copyable types and use clearing padding builtin

--- libcudacxx/include/cuda/std/__cccl/builtin.h
+++ libcudacxx/include/cuda/std/__cccl/builtin.h
@@ -427,6 +427,10 @@
 #  define _CCCL_BUILTIN_IS_COMPLETE_TYPE(...) __is_complete_type(__VA_ARGS__)
 #endif // _CCCL_HAS_BUILTIN(__is_complete_type)
 
+#if _CCCL_HAS_BUILTIN(__builtin_clear_padding) && !_CCCL_DEVICE_COMPILATION()
+#  define _CCCL_BUILTIN_CLEAR_PADDING(...) __builtin_clear_padding(__VA_ARGS__)
+#endif // _CCCL_HAS_BUILTIN(__builtin_clear_padding) && !_CCCL_DEVICE_COMPILATION()
+
 // NVCC prior to 12.2 have trouble with pack expansion into __type_pack_element in an alias template
 #if _CCCL_CUDACC_BELOW(12, 2)
 #  undef _CCCL_BUILTIN_TYPE_PACK_ELEMENT
