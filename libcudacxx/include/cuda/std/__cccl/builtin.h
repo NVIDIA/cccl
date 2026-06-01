2b13f471725db368641dfb08bb3c6510c11677eb
fbusato <fbusato@nvidia.com>
2026-06-01 11:44:06 -0700

    fix builtin.h

--- libcudacxx/include/cuda/std/__cccl/builtin.h
+++ libcudacxx/include/cuda/std/__cccl/builtin.h
@@ -427,9 +427,11 @@
 #  define _CCCL_BUILTIN_IS_COMPLETE_TYPE(...) __is_complete_type(__VA_ARGS__)
 #endif // _CCCL_HAS_BUILTIN(__is_complete_type)
 
-#if _CCCL_HAS_BUILTIN(__builtin_clear_padding) && !_CCCL_DEVICE_COMPILATION()
+#if _CCCL_HAS_BUILTIN(__builtin_clear_padding) \
+  && (_CCCL_HOST_COMPILATION() || !(_CCCL_COMPILER(GCC) || _CCCL_COMPILER(NVHPC)))
 #  define _CCCL_BUILTIN_CLEAR_PADDING(...) __builtin_clear_padding(__VA_ARGS__)
-#endif // _CCCL_HAS_BUILTIN(__builtin_clear_padding) && !_CCCL_DEVICE_COMPILATION()
+#endif // _CCCL_HAS_BUILTIN(__builtin_clear_padding) && (_CCCL_HOST_COMPILATION() || !(_CCCL_COMPILER(GCC) ||
+       // _CCCL_COMPILER(NVHPC)))
 
 // NVCC prior to 12.2 have trouble with pack expansion into __type_pack_element in an alias template
 #if _CCCL_CUDACC_BELOW(12, 2)
