c35fb76fe3c3c38f58e392c98ae7a46d3ad3a2d2
Michael Schellenberger Costa <miscco@nvidia.com>
2026-06-05 11:09:51 +0200

    [libcu++] Improve `__constant_sequence`
    We should not allow empty sequences for `__constant_sequence`
    
    We also do not need all the wrappers

--- libcudacxx/include/cuda/__argument/argument.h
+++ libcudacxx/include/cuda/__argument/argument.h
@@ -27,6 +27,7 @@
 #include <cuda/std/__cccl/assert.h>
 #include <cuda/std/__iterator/readable_traits.h>
 #include <cuda/std/__ranges/concepts.h>
+#include <cuda/std/__ranges/empty.h>
 #include <cuda/std/__type_traits/is_arithmetic.h>
 #include <cuda/std/__type_traits/is_array.h>
 #include <cuda/std/__type_traits/is_integer.h>
@@ -113,6 +114,8 @@ struct __constant_sequence
   using __element_type = __element_type_of_t<value_type>;
 
   static_assert(__is_sequence_v<value_type>, "The value type of __constant_sequence must be a range or an array");
+  static_assert(!::cuda::std::ranges::__empty::__fn{}(_Value),
+                "The value type of __constant_sequence must be a non-empty sequence");
 
   [[nodiscard]] _CCCL_API static constexpr value_type value() noexcept
   {
@@ -483,46 +486,6 @@ template <class _Arg, class _StaticBounds>
   return ::cuda::std::move(__arg.__arg_);
 }
 
-template <auto _Value>
-_CCCL_API constexpr auto __constant_compute_lowest() noexcept
-{
-  return _Value;
-}
-
-template <auto _Value>
-_CCCL_API constexpr auto __constant_compute_highest() noexcept
-{
-  return _Value;
-}
-
-template <auto _Value>
-_CCCL_API constexpr auto __constant_sequence_compute_lowest() noexcept
-{
-  using _ElementType = __element_type_of_t<::cuda::std::remove_cvref_t<decltype(_Value)>>;
-  auto __first       = _Value.begin();
-  auto __last        = _Value.end();
-
-  if (__first == __last)
-  {
-    return ::cuda::std::numeric_limits<_ElementType>::lowest();
-  }
-  return static_cast<_ElementType>(*::cuda::std::min_element(__first, __last));
-}
-
-template <auto _Value>
-_CCCL_API constexpr auto __constant_sequence_compute_highest() noexcept
-{
-  using _ElementType = __element_type_of_t<::cuda::std::remove_cvref_t<decltype(_Value)>>;
-  auto __first       = _Value.begin();
-  auto __last        = _Value.end();
-
-  if (__first == __last)
-  {
-    return (::cuda::std::numeric_limits<_ElementType>::max)();
-  }
-  return static_cast<_ElementType>(*::cuda::std::max_element(__first, __last));
-}
-
 // =====================================================================
 // __traits
 // =====================================================================
@@ -551,8 +514,8 @@ struct __traits_impl<__constant<_Value>>
   static constexpr bool is_constant     = true;
   static constexpr bool is_deferred     = false;
   static constexpr bool is_single_value = true;
-  static constexpr element_type lowest  = __constant_compute_lowest<_Value>();
-  static constexpr element_type highest = __constant_compute_highest<_Value>();
+  static constexpr element_type lowest  = _Value;
+  static constexpr element_type highest = _Value;
 };
 
 template <class _Arg, class _StaticBounds>
@@ -579,8 +542,8 @@ struct __traits_impl<__constant_sequence<_Value>>
   static constexpr bool is_constant     = true;
   static constexpr bool is_deferred     = false;
   static constexpr bool is_single_value = false;
-  static constexpr element_type lowest  = __constant_sequence_compute_lowest<_Value>();
-  static constexpr element_type highest = __constant_sequence_compute_highest<_Value>();
+  static constexpr element_type lowest  = *::cuda::std::min_element(_Value.begin(), _Value.end());
+  static constexpr element_type highest = *::cuda::std::max_element(_Value.begin(), _Value.end());
 };
 
 template <class _Arg, class _StaticBounds>
@@ -648,13 +611,13 @@ _CCCL_REQUIRES((!__is_wrapper_v<::cuda::std::remove_cv_t<_Tp>>) )
 template <auto _Value>
 [[nodiscard]] _CCCL_API constexpr auto __lowest_(__constant<_Value>) noexcept
 {
-  return __constant_compute_lowest<_Value>();
+  return _Value;
 }
 
 template <auto _Value>
 [[nodiscard]] _CCCL_API constexpr auto __lowest_(__constant_sequence<_Value>) noexcept
 {
-  return __constant_sequence_compute_lowest<_Value>();
+  return *::cuda::std::min_element(_Value.begin(), _Value.end());
 }
 
 template <class _Arg, class _StaticBounds>
@@ -698,13 +661,13 @@ _CCCL_REQUIRES((!__is_wrapper_v<::cuda::std::remove_cv_t<_Tp>>) )
 template <auto _Value>
 [[nodiscard]] _CCCL_API constexpr auto __highest_(__constant<_Value>) noexcept
 {
-  return __constant_compute_highest<_Value>();
+  return _Value;
 }
 
 template <auto _Value>
 [[nodiscard]] _CCCL_API constexpr auto __highest_(__constant_sequence<_Value>) noexcept
 {
-  return __constant_sequence_compute_highest<_Value>();
+  return *::cuda::std::max_element(_Value.begin(), _Value.end());
 }
 
 template <class _Arg, class _StaticBounds>
