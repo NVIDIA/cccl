a31b43570fcb385e7282e71fcf604d4fda8a9c3e
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-06-10 19:16:08 -0400

    Add basic communicator concept

---
+++ cudax/test/multi_gpu/concepts/has_all_to_all_v.cu
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/__stream/stream_ref.h>
+#include <cuda/std/__cstddef/types.h>
+
+#include <cuda/experimental/__multi_gpu/concepts.h>
+
+#include "collective_concepts_common.cuh"
+
+namespace
+{
+namespace cudax = ::cuda::experimental;
+namespace types = cudax_multi_gpu_concepts;
+
+// nvcc ignores [[maybe_unused]] entirely
+_CCCL_BEGIN_NV_DIAG_SUPPRESS(177)
+
+struct all_to_all_v_returns_int : types::communicator_model
+{
+  template <class Tp>
+  int all_to_all_v(
+    group_token_type&,
+    Tp*,
+    const ::cuda::std::size_t*,
+    const ::cuda::std::size_t*,
+    Tp*,
+    const ::cuda::std::size_t*,
+    const ::cuda::std::size_t*,
+    ::cuda::stream_ref);
+};
+
+_CCCL_END_NV_DIAG_SUPPRESS()
+
+_CCCL_HOST_DEVICE_API constexpr bool test()
+{
+  static_assert(cudax::__has_all_to_all_v<types::collective_communicator_model>);
+  static_assert(cudax::__has_all_to_all_v<types::collective_communicator_model, long*>);
+  static_assert(!cudax::__has_all_to_all_v<types::communicator_model>);
+
+  static_assert(!cudax::__has_all_to_all_v<all_to_all_v_returns_int>);
+  return true;
+}
+} // namespace
+
+int main(int, char**)
+{
+  test();
+  static_assert(test());
+  return 0;
+}
---
+++ cudax/test/multi_gpu/concepts/has_reduce.cu
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cuda/__stream/stream_ref.h>
+#include <cuda/std/__cstddef/types.h>
+#include <cuda/std/cstdint>
+
+#include <cuda/experimental/__multi_gpu/concepts.h>
+
+#include "collective_concepts_common.cuh"
+
+namespace
+{
+namespace cudax = ::cuda::experimental;
+namespace types = cudax_multi_gpu_concepts;
+
+// nvcc ignores [[maybe_unused]] entirely
+_CCCL_BEGIN_NV_DIAG_SUPPRESS(177)
+
+struct reduce_returns_int : types::communicator_model
+{
+  template <class Tp, class Op>
+  int reduce(group_token_type&, Tp*, Tp*, ::cuda::std::size_t, Op, ::cuda::std::int32_t, ::cuda::stream_ref);
+};
+
+_CCCL_END_NV_DIAG_SUPPRESS()
+
+_CCCL_HOST_DEVICE_API constexpr bool test()
+{
+  static_assert(cudax::__has_reduce<types::collective_communicator_model>);
+  static_assert(cudax::__has_reduce<types::collective_communicator_model, long*>);
+  static_assert(!cudax::__has_reduce<types::communicator_model>);
+
+  static_assert(!cudax::__has_reduce<reduce_returns_int>);
+  return true;
+}
+} // namespace
+
+int main(int, char**)
+{
+  test();
+  static_assert(test());
+  return 0;
+}
