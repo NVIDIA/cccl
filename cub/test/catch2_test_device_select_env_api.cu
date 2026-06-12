e3a5e993c95d7efcc7abe0e2eeb3602049f5f5c8
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-06-13 00:55:03 +0200

    Allow public tuning of `cub::DeviceSelect` (without `UniqueByKey`) and `cub::DevicePartition` (without three-way) (#9316)
    * Productize the cub::DeviceSelect (without UniqueByKey) and cub::DevicePartition (without three-way) tuning API
    
    Fixes: #8583
    Fixes: #8575
    
    * Add PartitionPolicy alias
    
    * Fix clang
    
    * Use PartitionPolicy
    
    * Use PartitionPolicy
    
    * fix warning
    
    * Move PSTL to dispatch function
    
    * Dedicated policy
    
    * FIx
    
    * Fix policy extraction
    
    * Move policy since doxygen can't handle it

--- cub/test/catch2_test_device_select_env_api.cu
+++ cub/test/catch2_test_device_select_env_api.cu
@@ -7,6 +7,7 @@
 
 #include <thrust/device_vector.h>
 
+#include <cuda/__execution/tune.h>
 #include <cuda/devices>
 #include <cuda/stream>
 
@@ -401,3 +402,52 @@ C2H_TEST("cub::DeviceSelect::UniqueByKey accepts default env without equality_op
   REQUIRE(values_out == expected_values);
   REQUIRE(num_selected == expected_num_selected);
 }
+
+#if _CCCL_STD_VER >= 2020
+
+// example-begin select-if-policy-selector
+struct SelectPolicySelector
+{
+  __host__ __device__ constexpr auto operator()(cuda::compute_capability cc) const -> cub::SelectPolicy
+  {
+    return {.threads_per_block = 128,
+            .items_per_thread  = cc > cuda::compute_capability{9, 0} ? 16 : 10,
+            .load_algorithm    = cub::BLOCK_LOAD_DIRECT,
+            .load_modifier     = cub::LOAD_DEFAULT,
+            .scan_algorithm    = cub::BLOCK_SCAN_WARP_SCANS,
+            .lookback_delay    = {cub::LookbackDelayAlgorithm::fixed_delay, 350, 450}};
+  }
+};
+// example-end select-if-policy-selector
+
+C2H_TEST("cub::DeviceSelect::If env-based API with tuning", "[select][env]")
+{
+  // example-begin select-if-tuning
+  auto d_in           = thrust::device_vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
+  auto d_out          = thrust::device_vector<int>(4, thrust::no_init);
+  auto d_num_selected = thrust::device_vector<int>(1, thrust::no_init);
+
+  const auto error = cub::DeviceSelect::If(
+    d_in.begin(),
+    d_out.begin(),
+    d_num_selected.begin(),
+    d_in.size(),
+    [] __host__ __device__(int v) {
+      return v < 5;
+    },
+    cuda::execution::tune(SelectPolicySelector{}));
+  if (error != cudaSuccess)
+  {
+    std::cerr << "cub::DeviceSelect::If failed with status: " << error << '\n';
+  }
+
+  thrust::device_vector<int> expected_output{1, 2, 3, 4};
+  int expected_num_selected = 4;
+  // example-end select-if-tuning
+
+  REQUIRE(error == cudaSuccess);
+  REQUIRE(d_out == expected_output);
+  REQUIRE(d_num_selected[0] == expected_num_selected);
+}
+
+#endif // _CCCL_STD_VER >= 2020
