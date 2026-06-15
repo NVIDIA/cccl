b713d98099799eaf9af6c2b3864efd776eca0dd8
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-06-15 10:45:04 +0200

    Allow public tuning of three-way `cub::DevicePartition::If` (#9324)
    Fixes: #9314

--- cub/test/catch2_test_device_three_way_partition_custom_policy_hub.cu
+++ cub/test/catch2_test_device_three_way_partition_custom_policy_hub.cu
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+// TODO(bgruber): drop this test with CCCL 4.0 when we drop the three way partition dispatcher
+
+#define CCCL_IGNORE_DEPRECATED_API
+
 #include "insert_nested_NVTX_range_guard.h"
 
 #include <cub/device/dispatch/dispatch_three_way_partition.cuh>
