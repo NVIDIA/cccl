544d200ec01169e742a8ecbeb91e751b4a26db0d
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-06-22 10:19:29 -0400

    fixup! bugprone-crtp-constructor-accessibility

--- libcudacxx/include/cuda/__hierarchy/native_hierarchy_level_base.h
+++ libcudacxx/include/cuda/__hierarchy/native_hierarchy_level_base.h
@@ -52,11 +52,6 @@ _CCCL_DIAG_SUPPRESS_NVHPC(nodiscard_doesnt_apply)
 template <class _Level>
 struct _CCCL_DECLSPEC_EMPTY_BASES __native_hierarchy_level_base : hierarchy_level_base<_Level>
 {
-private:
-  __native_hierarchy_level_base() = default;
-  friend _Level;
-
-public:
   using __base_type = hierarchy_level_base<_Level>;
   using __base_type::count;
   using __base_type::count_as;
