d7f286b1fd73ce04b38729fb41362b3d58f4b60c
fbusato <fbusato@nvidia.com>
2026-06-24 10:53:40 -0700

    a few improvements

--- libcudacxx/include/cuda/__numeric/isclose.h
+++ libcudacxx/include/cuda/__numeric/isclose.h
@@ -31,6 +31,7 @@
 #include <cuda/std/__cmath/abs.h>
 #include <cuda/std/__cmath/hypot.h>
 #include <cuda/std/__cmath/isfinite.h>
+#include <cuda/std/__cmath/min_max.h>
 #include <cuda/std/__concepts/concept_macros.h>
 #include <cuda/std/__limits/numeric_limits.h>
 #include <cuda/std/__type_traits/conditional.h>
@@ -76,19 +77,22 @@ __isclose_fp_impl(const _Tp __lhs, const _Tp __rhs, const float __rel_tol, const
   {
     return false;
   }
-  const auto __diff      = ::cuda::std::abs(__lhs - __rhs);
-  const auto __lhs_abs   = ::cuda::std::abs(__lhs);
-  const auto __rhs_abs   = ::cuda::std::abs(__rhs);
-  const auto __rel_value = static_cast<_Tp>(__rel_tol * ::cuda::std::max(__lhs_abs, __rhs_abs));
-  return __diff <= ::cuda::std::max(__abs_tol, __rel_value);
+  const auto __diff      = ::cuda::std::fabs(__lhs - __rhs);
+  const auto __lhs_abs   = ::cuda::std::fabs(__lhs);
+  const auto __rhs_abs   = ::cuda::std::fabs(__rhs);
+  const auto __rel_value = static_cast<_Tp>(__rel_tol * ::cuda::std::fmax(__lhs_abs, __rhs_abs));
+  return __diff <= ::cuda::std::fmax(__abs_tol, __rel_value);
 }
 
 template <typename _ComplexType, typename _AbsTol>
 [[nodiscard]] _CCCL_HOST_DEVICE_API bool __isclose_complex_impl(
   const _ComplexType& __lhs, const _ComplexType& __rhs, const float __rel_tol, const _AbsTol __abs_tol) noexcept
 {
-  using __scalar_t  = typename _ComplexType::value_type;
-  using __compare_t = __isclose_compare_t<__scalar_t>;
+  using __scalar_t _CCCL_NODEBUG_ALIAS  = typename _ComplexType::value_type;
+  using __compare_t _CCCL_NODEBUG_ALIAS = __isclose_compare_t<__scalar_t>;
+#if _CCCL_HAS_FLOAT128()
+  static_assert(!::cuda::std::is_same_v<__scalar_t, __float128>, "cuda::isclose: __float128 is not supported");
+#endif // _CCCL_HAS_FLOAT128()
   _CCCL_ASSERT(::cuda::in_range(__rel_tol, 0.0f, 1.0f),
                "cuda::isclose: relative tolerance must be in the range [0.0, 1.0]");
   _CCCL_ASSERT(::cuda::std::isfinite(__abs_tol) && __abs_tol >= __scalar_t{0},
@@ -112,19 +116,19 @@ template <typename _ComplexType, typename _AbsTol>
   const auto __diff      = ::cuda::std::hypot(__lhs_real - __rhs_real, __lhs_imag - __rhs_imag);
   const auto __lhs_abs   = ::cuda::std::hypot(__lhs_real, __lhs_imag);
   const auto __rhs_abs   = ::cuda::std::hypot(__rhs_real, __rhs_imag);
-  const auto __rel_value = __rel_tol * ::cuda::std::max(__lhs_abs, __rhs_abs);
-  return __diff <= ::cuda::std::max(__abs, __rel_value);
+  const auto __rel_value = __rel_tol * ::cuda::std::fmax(__lhs_abs, __rhs_abs);
+  return __diff <= ::cuda::std::fmax(__abs, __rel_value);
 }
 
 template <typename _Tp>
 [[nodiscard]] _CCCL_API constexpr ::cuda::std::make_unsigned_t<_Tp>
-__safe_diff(const _Tp __lhs, const _Tp __rhs) noexcept
+__safe_abs_diff(const _Tp __lhs, const _Tp __rhs) noexcept
 {
-  using __unsigned_t           = ::cuda::std::make_unsigned_t<_Tp>;
-  const auto __lhs_abs         = ::cuda::uabs(__lhs);
-  const auto __rhs_abs         = ::cuda::uabs(__rhs);
-  const auto __is_lhs_negative = ::cuda::std::__cccl_is_signed_integer_v<_Tp> && __lhs < _Tp{0};
-  const auto __is_rhs_negative = ::cuda::std::__cccl_is_signed_integer_v<_Tp> && __rhs < _Tp{0};
+  using __unsigned_t _CCCL_NODEBUG_ALIAS = ::cuda::std::make_unsigned_t<_Tp>;
+  const auto __lhs_abs                   = ::cuda::uabs(__lhs);
+  const auto __rhs_abs                   = ::cuda::uabs(__rhs);
+  const auto __is_lhs_negative           = ::cuda::std::is_signed_v<_Tp> && __lhs < _Tp{0};
+  const auto __is_rhs_negative           = ::cuda::std::is_signed_v<_Tp> && __rhs < _Tp{0};
 
   if (__is_lhs_negative != __is_rhs_negative)
   {
@@ -145,11 +149,11 @@ __isclose_integer_impl(const _Tp __lhs, const _Tp __rhs, const float __rel_tol, 
   {
     _CCCL_ASSERT(__abs_tol >= _Tp{0}, "cuda::isclose: absolute tolerance must be non-negative");
   }
-  using __unsigned_t     = ::cuda::std::make_unsigned_t<_Tp>;
-  const auto __lhs_abs   = ::cuda::uabs(__lhs);
-  const auto __rhs_abs   = ::cuda::uabs(__rhs);
-  const auto __diff      = ::cuda::__safe_diff(__lhs, __rhs);
-  const auto __abs       = static_cast<__unsigned_t>(__abs_tol);
+  using __unsigned_t _CCCL_NODEBUG_ALIAS = ::cuda::std::make_unsigned_t<_Tp>;
+  const auto __lhs_abs                   = ::cuda::uabs(__lhs);
+  const auto __rhs_abs                   = ::cuda::uabs(__rhs);
+  const auto __diff                      = ::cuda::__safe_abs_diff(__lhs, __rhs);
+  const auto __abs                       = static_cast<__unsigned_t>(__abs_tol);
   const auto __rel_value = static_cast<__unsigned_t>(__rel_tol * ::cuda::std::max(__lhs_abs, __rhs_abs));
   return __diff <= ::cuda::std::max(__abs, __rel_value);
 }
@@ -160,6 +164,12 @@ __isclose_integer_impl(const _Tp __lhs, const _Tp __rhs, const float __rel_tol, 
 // Scalar overloads
 
 //! @brief Checks whether two arithmetic values are close to each other using a relative and absolute tolerance.
+//!
+//! @param __lhs The first value to compare.
+//! @param __rhs The second value to compare.
+//! @param __rel_tol The relative tolerance.
+//! @param __abs_tol The absolute tolerance.
+//! @return True if __lhs and __rhs are close to each other, false otherwise.
 _CCCL_TEMPLATE(typename _Tp)
 _CCCL_REQUIRES(::cuda::std::__cccl_is_integer_v<_Tp> || ::cuda::is_floating_point_v<_Tp>)
 [[nodiscard]] _CCCL_API constexpr bool
@@ -167,17 +177,22 @@ isclose(const _Tp __lhs, const _Tp __rhs, const float __rel_tol, const _Tp __abs
 {
   if constexpr (::cuda::std::__cccl_is_integer_v<_Tp>)
   {
-    return ::cuda::__isclose_integer_impl(__lhs, __rhs, __rel_tol, __abs_tol);
+    return ::cuda::__isclose_integer_impl(+__lhs, +__rhs, __rel_tol, +__abs_tol);
   }
   else
   {
-    using __value_t = __isclose_compare_t<_Tp>;
+    using __value_t _CCCL_NODEBUG_ALIAS = __isclose_compare_t<_Tp>;
     return ::cuda::__isclose_fp_impl(
       static_cast<__value_t>(__lhs), static_cast<__value_t>(__rhs), __rel_tol, static_cast<__value_t>(__abs_tol));
   }
 }
 
 //! @brief Checks whether two arithmetic values are close to each other using a relative tolerance.
+//!
+//! @param __lhs The first value to compare.
+//! @param __rhs The second value to compare.
+//! @param __rel_tol The relative tolerance.
+//! @return True if __lhs and __rhs are close to each other, false otherwise.
 _CCCL_TEMPLATE(typename _Tp)
 _CCCL_REQUIRES(::cuda::std::__cccl_is_integer_v<_Tp> || ::cuda::is_floating_point_v<_Tp>)
 [[nodiscard]] _CCCL_API constexpr bool isclose(const _Tp __lhs, const _Tp __rhs, const float __rel_tol) noexcept
@@ -186,6 +201,10 @@ _CCCL_REQUIRES(::cuda::std::__cccl_is_integer_v<_Tp> || ::cuda::is_floating_poin
 }
 
 //! @brief Checks whether two arithmetic values are close to each other using the default relative tolerance.
+//!
+//! @param __lhs The first value to compare.
+//! @param __rhs The second value to compare.
+//! @return True if __lhs and __rhs are close to each other, false otherwise.
 _CCCL_TEMPLATE(typename _Tp)
 _CCCL_REQUIRES(::cuda::std::__cccl_is_integer_v<_Tp> || ::cuda::is_floating_point_v<_Tp>)
 [[nodiscard]] _CCCL_API constexpr bool isclose(const _Tp __lhs, const _Tp __rhs) noexcept
@@ -211,6 +230,12 @@ inline constexpr bool __isclose_complex_comparison_v<_Tp, _AbsTol, true> =
   ::cuda::std::is_same_v<typename _Tp::value_type, _AbsTol>;
 
 //! @brief Checks whether two complex values are close to each other using a relative and absolute tolerance.
+//!
+//! @param __lhs The first value to compare.
+//! @param __rhs The second value to compare.
+//! @param __rel_tol The relative tolerance.
+//! @param __abs_tol The absolute tolerance.
+//! @return True if __lhs and __rhs are close to each other, false otherwise.
 _CCCL_TEMPLATE(typename _ComplexType, typename _AbsTol)
 _CCCL_REQUIRES(__isclose_complex_comparison_v<_ComplexType, _AbsTol>)
 [[nodiscard]] _CCCL_HOST_DEVICE_API bool
@@ -220,21 +245,30 @@ isclose(const _ComplexType& __lhs, const _ComplexType& __rhs, const float __rel_
 }
 
 //! @brief Checks whether two complex values are close to each other using a relative tolerance.
+//!
+//! @param __lhs The first value to compare.
+//! @param __rhs The second value to compare.
+//! @param __rel_tol The relative tolerance.
+//! @return True if __lhs and __rhs are close to each other, false otherwise.
 _CCCL_TEMPLATE(typename _ComplexType)
 _CCCL_REQUIRES(__is_any_complex_v<_ComplexType>)
 [[nodiscard]] _CCCL_HOST_DEVICE_API bool
 isclose(const _ComplexType& __lhs, const _ComplexType& __rhs, const float __rel_tol) noexcept
 {
-  using __scalar_t = typename _ComplexType::value_type;
+  using __scalar_t _CCCL_NODEBUG_ALIAS = typename _ComplexType::value_type;
   return ::cuda::isclose(__lhs, __rhs, __rel_tol, __scalar_t{0});
 }
 
 //! @brief Checks whether two complex values are close to each other using the default relative tolerance.
+//!
+//! @param __lhs The first value to compare.
+//! @param __rhs The second value to compare.
+//! @return True if __lhs and __rhs are close to each other, false otherwise.
 _CCCL_TEMPLATE(typename _ComplexType)
 _CCCL_REQUIRES(__is_any_complex_v<_ComplexType>)
 [[nodiscard]] _CCCL_HOST_DEVICE_API bool isclose(const _ComplexType& __lhs, const _ComplexType& __rhs) noexcept
 {
-  using __scalar_t = typename _ComplexType::value_type;
+  using __scalar_t _CCCL_NODEBUG_ALIAS = typename _ComplexType::value_type;
   return ::cuda::isclose(__lhs, __rhs, ::cuda::__isclose_default_relative_tolerance<__scalar_t>(), __scalar_t{0});
 }
 
