71c403402d733152744bf56b4fda9f84ad662be8
ptaylor <paul.e.taylor@me.com>
2026-06-24 10:01:40 -0700

    use 16-core runners with more memory

--- .github/actions/workflow-build/build-workflow.py
+++ .github/actions/workflow-build/build-workflow.py
@@ -476,8 +476,8 @@ def generate_dispatch_job_runner(matrix_job, job_type, job_info):
 
     if not job_info["gpu"]:
         if matrix_job.get("use_sccache_dist", False):
-            # Use smaller 8-core runners for build jobs if we can
-            return f"{runner_os}-{cpu}-cpu8m"
+            # Use runners with more memory for build cluster jobs
+            return f"{runner_os}-{cpu}-cpu16m"
         else:
             return f"{runner_os}-{cpu}-cpu16"
 
