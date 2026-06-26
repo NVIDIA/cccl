fa451b9c6d372300c26a940077f3169b1f93e5bf
Michael Schellenberger Costa <miscco@nvidia.com>
2026-06-26 09:21:30 +0200

    [CCCL] Enable C++23 mode for NVCC 13.3
    NVCC 13.3 has support for C++23. We want to run that in CI to test new language / compiler features

--- ci/windows/build_common.psm1
+++ ci/windows/build_common.psm1
@@ -2,7 +2,7 @@ Param(
     [Parameter(Mandatory = $false)]
     [Alias("std")]
     [ValidateNotNullOrEmpty()]
-    [ValidateSet(17, 20)]
+    [ValidateSet(17, 20, 23)]
     [int]$CXX_STANDARD = 17,
     [Parameter(Mandatory = $false)]
     [Alias("arch")]
