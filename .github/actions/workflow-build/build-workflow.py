d64b34bb534335492a959eda9ccc6c42d4ad2c6c
ptaylor <paul.e.taylor@me.com>
2026-07-01 13:47:22 -0700

    cpu16m -> cpu8m

--- .github/actions/workflow-build/build-workflow.py
+++ .github/actions/workflow-build/build-workflow.py
@@ -477,7 +477,7 @@ def generate_dispatch_job_runner(matrix_job, job_type, job_info):
     if not job_info["gpu"]:
         if matrix_job.get("use_sccache_dist", False):
             # Use runners with more memory for build cluster jobs
-            return f"{runner_os}-{cpu}-cpu16m"
+            return f"{runner_os}-{cpu}-cpu8m"
         else:
             return f"{runner_os}-{cpu}-cpu16"
 
