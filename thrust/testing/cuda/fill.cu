f57bb7f7c17160db7419c6d9b9889810f26fd4a8
Piotr Ciolkosz <pciolkosz@nvidia.com>
2026-07-09 17:30:31 -0700

    Use CCCL Runtime in Thrust fill tests

--- thrust/testing/cuda/fill.cu
+++ thrust/testing/cuda/fill.cu
@@ -1,74 +1,53 @@
 #include <thrust/execution_policy.h>
 #include <thrust/fill.h>
 
+#include <cuda/buffer>
+#include <cuda/cccl_runtime_test_helper.cuh>
+#include <cuda/launch>
+#include <cuda/std/cstddef>
+#include <cuda/stream>
+
 #include <algorithm>
 
 #include <unittest/unittest.h>
 
 #ifdef THRUST_TEST_DEVICE_SIDE
-template <typename ExecutionPolicy, typename Iterator, typename T>
-__global__ void fill_kernel(ExecutionPolicy exec, Iterator first, Iterator last, T value)
+struct fill_kernel
 {
-  thrust::fill(exec, first, last, value);
-}
+  template <typename ExecutionPolicy, typename Data, typename T>
+  __device__ void
+  operator()(ExecutionPolicy exec, Data data, cuda::std::size_t first, cuda::std::size_t last, T value) const
+  {
+    thrust::fill(exec, data.begin() + first, data.begin() + last, value);
+  }
+};
 
 template <typename T, typename ExecutionPolicy>
 void TestFillDevice(ExecutionPolicy exec, size_t n)
 {
-  thrust::host_vector<T> h_data   = unittest::random_integers<T>(n);
-  thrust::device_vector<T> d_data = h_data;
+  const auto device = test_runtime::current_test_device();
+  cuda::stream stream{device};
 
-  thrust::fill(h_data.begin() + std::min((size_t) 1, n), h_data.begin() + std::min((size_t) 3, n), (T) 0);
+  auto expected = test_runtime::random_integers_buffer<T>(stream, n);
+  auto data     = cuda::make_device_buffer<T>(stream, device, expected);
 
-  fill_kernel<<<1, 1>>>(exec, d_data.begin() + std::min((size_t) 1, n), d_data.begin() + std::min((size_t) 3, n), (T) 0);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
+  const auto run_fill = [&](size_t first, size_t last, T value) {
+    for (size_t i = first; i < last; ++i)
+    {
+      expected[i] = value;
+    }
 
-  ASSERT_EQUAL(h_data, d_data);
+    cuda::launch(stream, test_runtime::single_thread_config(), fill_kernel{}, exec, data, first, last, value);
+    stream.sync();
 
-  thrust::fill(h_data.begin() + std::min((size_t) 117, n), h_data.begin() + std::min((size_t) 367, n), (T) 1);
+    test_runtime::assert_equal(stream, data, expected);
+  };
 
-  fill_kernel<<<1, 1>>>(
-    exec, d_data.begin() + std::min((size_t) 117, n), d_data.begin() + std::min((size_t) 367, n), (T) 1);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
-
-  thrust::fill(h_data.begin() + std::min((size_t) 8, n), h_data.begin() + std::min((size_t) 259, n), (T) 2);
-
-  fill_kernel<<<1, 1>>>(
-    exec, d_data.begin() + std::min((size_t) 8, n), d_data.begin() + std::min((size_t) 259, n), (T) 2);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
-
-  thrust::fill(h_data.begin() + std::min((size_t) 3, n), h_data.end(), (T) 3);
-
-  fill_kernel<<<1, 1>>>(exec, d_data.begin() + std::min((size_t) 3, n), d_data.end(), (T) 3);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
-
-  thrust::fill(h_data.begin(), h_data.end(), (T) 4);
-
-  fill_kernel<<<1, 1>>>(exec, d_data.begin(), d_data.end(), (T) 4);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
+  run_fill(std::min<size_t>(1, n), std::min<size_t>(3, n), T{0});
+  run_fill(std::min<size_t>(117, n), std::min<size_t>(367, n), T{1});
+  run_fill(std::min<size_t>(8, n), std::min<size_t>(259, n), T{2});
+  run_fill(std::min<size_t>(3, n), n, T{3});
+  run_fill(0, n, T{4});
 }
 
 template <typename T>
@@ -85,75 +64,50 @@ void TestFillDeviceDevice(size_t n)
 }
 DECLARE_VARIABLE_UNITTEST(TestFillDeviceDevice);
 
-template <typename ExecutionPolicy, typename Iterator, typename Size, typename T>
-__global__ void fill_n_kernel(ExecutionPolicy exec, Iterator first, Size n, T value)
+struct fill_n_kernel
 {
-  thrust::fill_n(exec, first, n, value);
-}
+  template <typename ExecutionPolicy, typename Data, typename T>
+  __device__ void
+  operator()(ExecutionPolicy exec, Data data, cuda::std::size_t first, cuda::std::size_t count, T value) const
+  {
+    thrust::fill_n(exec, data.begin() + first, count, value);
+  }
+};
 
 template <typename T, typename ExecutionPolicy>
 void TestFillNDevice(ExecutionPolicy exec, size_t n)
 {
-  thrust::host_vector<T> h_data   = unittest::random_integers<T>(n);
-  thrust::device_vector<T> d_data = h_data;
+  const auto device = test_runtime::current_test_device();
+  cuda::stream stream{device};
 
-  size_t begin_offset = std::min<size_t>(1, n);
+  auto expected = test_runtime::random_integers_buffer<T>(stream, n);
+  auto data     = cuda::make_device_buffer<T>(stream, device, expected);
 
-  thrust::fill_n(h_data.begin() + begin_offset, std::min((size_t) 3, n) - begin_offset, (T) 0);
+  const auto run_fill_n = [&](size_t first, size_t count, T value) {
+    for (size_t i = first; i < first + count; ++i)
+    {
+      expected[i] = value;
+    }
 
-  fill_n_kernel<<<1, 1>>>(exec, d_data.begin() + begin_offset, std::min((size_t) 3, n) - begin_offset, (T) 0);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
+    cuda::launch(stream, test_runtime::single_thread_config(), fill_n_kernel{}, exec, data, first, count, value);
+    stream.sync();
 
-  ASSERT_EQUAL(h_data, d_data);
+    test_runtime::assert_equal(stream, data, expected);
+  };
 
-  begin_offset = std::min<size_t>(117, n);
+  size_t first = std::min<size_t>(1, n);
+  run_fill_n(first, std::min<size_t>(3, n) - first, T{0});
 
-  thrust::fill_n(h_data.begin() + begin_offset, std::min((size_t) 367, n) - begin_offset, (T) 1);
+  first = std::min<size_t>(117, n);
+  run_fill_n(first, std::min<size_t>(367, n) - first, T{1});
 
-  fill_n_kernel<<<1, 1>>>(exec, d_data.begin() + begin_offset, std::min((size_t) 367, n) - begin_offset, (T) 1);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
+  first = std::min<size_t>(8, n);
+  run_fill_n(first, std::min<size_t>(259, n) - first, T{2});
 
-  ASSERT_EQUAL(h_data, d_data);
+  first = std::min<size_t>(3, n);
+  run_fill_n(first, n - first, T{3});
 
-  begin_offset = std::min<size_t>(8, n);
-
-  thrust::fill_n(h_data.begin() + begin_offset, std::min((size_t) 259, n) - begin_offset, (T) 2);
-
-  fill_n_kernel<<<1, 1>>>(exec, d_data.begin() + begin_offset, std::min((size_t) 259, n) - begin_offset, (T) 2);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
-
-  begin_offset = std::min<size_t>(3, n);
-
-  thrust::fill_n(h_data.begin() + begin_offset, h_data.size() - begin_offset, (T) 3);
-
-  fill_n_kernel<<<1, 1>>>(exec, d_data.begin() + begin_offset, d_data.size() - begin_offset, (T) 3);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
-
-  thrust::fill_n(h_data.begin(), h_data.size(), (T) 4);
-
-  fill_n_kernel<<<1, 1>>>(exec, d_data.begin(), d_data.size(), (T) 4);
-  {
-    cudaError_t const err = cudaDeviceSynchronize();
-    ASSERT_EQUAL(cudaSuccess, err);
-  }
-
-  ASSERT_EQUAL(h_data, d_data);
+  run_fill_n(0, n, T{4});
 }
 
 template <typename T>
@@ -173,35 +127,29 @@ DECLARE_VARIABLE_UNITTEST(TestFillNDeviceDevice);
 
 void TestFillCudaStreams()
 {
-  thrust::device_vector<int> v{0, 1, 2, 3, 4};
+  const auto device = test_runtime::current_test_device();
+  cuda::stream stream{device};
 
-  cudaStream_t s;
-  cudaStreamCreate(&s);
+  auto v = cuda::make_device_buffer<int>(stream, device, {0, 1, 2, 3, 4});
 
-  thrust::fill(thrust::cuda::par.on(s), v.begin() + 1, v.begin() + 4, 7);
-  cudaStreamSynchronize(s);
+  thrust::fill(thrust::cuda::par.on(stream.get()), v.begin() + 1, v.begin() + 4, 7);
+  stream.sync();
 
-  thrust::device_vector<int> ref{0, 7, 7, 7, 4};
-  ASSERT_EQUAL(v, ref);
+  test_runtime::assert_equal(stream, v, {0, 7, 7, 7, 4});
 
-  thrust::fill(thrust::cuda::par.on(s), v.begin() + 0, v.begin() + 3, 8);
-  cudaStreamSynchronize(s);
+  thrust::fill(thrust::cuda::par.on(stream.get()), v.begin(), v.begin() + 3, 8);
+  stream.sync();
 
-  ref = {8, 8, 8, 7, 4};
-  ASSERT_EQUAL(v, ref);
+  test_runtime::assert_equal(stream, v, {8, 8, 8, 7, 4});
 
-  thrust::fill(thrust::cuda::par.on(s), v.begin() + 2, v.end(), 9);
-  cudaStreamSynchronize(s);
+  thrust::fill(thrust::cuda::par.on(stream.get()), v.begin() + 2, v.end(), 9);
+  stream.sync();
 
-  ref = {8, 8, 9, 9, 9};
-  ASSERT_EQUAL(v, ref);
+  test_runtime::assert_equal(stream, v, {8, 8, 9, 9, 9});
 
-  thrust::fill(thrust::cuda::par.on(s), v.begin(), v.end(), 1);
-  cudaStreamSynchronize(s);
+  thrust::fill(thrust::cuda::par.on(stream.get()), v.begin(), v.end(), 1);
+  stream.sync();
 
-  ref = {1, 1, 1, 1, 1};
-  ASSERT_EQUAL(v, ref);
-
-  cudaStreamDestroy(s);
+  test_runtime::assert_equal(stream, v, {1, 1, 1, 1, 1});
 }
 DECLARE_UNITTEST(TestFillCudaStreams);
