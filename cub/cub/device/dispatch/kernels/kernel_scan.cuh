f29ec53cffb3ba77595205041d44cf13a2e96671
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-07-14 13:53:22 +0200

    Remove _CCCL_GRID_CONSTANT from scan kernel
    Fixes: #9851

--- cub/cub/device/dispatch/kernels/kernel_scan.cuh
+++ cub/cub/device/dispatch/kernels/kernel_scan.cuh
@@ -69,7 +69,7 @@ template <typename PolicySelectorT,
           typename ScanTileState,
           typename AccumT>
 _CCCL_KERNEL_ATTRIBUTES __launch_bounds__(128) void DeviceScanInitKernel(
-  tile_state_kernel_arg_t<ScanTileState, AccumT> tile_state, _CCCL_GRID_CONSTANT const int num_tiles)
+  tile_state_kernel_arg_t<ScanTileState, AccumT> tile_state, const int num_tiles)
 {
   _CCCL_PDL_GRID_DEPENDENCY_SYNC();
   _CCCL_PDL_TRIGGER_NEXT_LAUNCH(); // beneficial for all problem sizes in cub.bench.scan.exclusive.sum.base
@@ -197,18 +197,14 @@ template <typename PolicySelector,
           bool StableReductionOrder = false,
           typename RealInitValueT   = typename InitValueT::value_type>
 __launch_bounds__(device_scan_launch_bounds<PolicySelector>, 1) _CCCL_KERNEL_ATTRIBUTES void DeviceScanKernel(
-  _CCCL_GRID_CONSTANT const InputIteratorT d_in,
-  _CCCL_GRID_CONSTANT const OutputIteratorT d_out,
+  const InputIteratorT d_in,
+  const OutputIteratorT d_out,
   tile_state_kernel_arg_t<ScanTileState, AccumT> tile_state,
-  _CCCL_GRID_CONSTANT const int start_tile,
-  _CCCL_GRID_CONSTANT const ScanOpT scan_op,
-// nvcc 12.0 gets stuck compiling some TUs like `cub.bench.scan.exclusive.sum.base`, so only enable for newer versions
-#if _CCCL_CUDACC_AT_LEAST(12, 8)
-  _CCCL_GRID_CONSTANT
-#endif // _CCCL_CUDACC_AT_LEAST(12, 8)
+  const int start_tile,
+  const ScanOpT scan_op,
   const InitValueT init_value,
-  _CCCL_GRID_CONSTANT const OffsetT num_items,
-  _CCCL_GRID_CONSTANT const int num_stages)
+  const OffsetT num_items,
+  const int num_stages)
 {
   static constexpr ScanPolicy active_policy = current_policy<PolicySelector>();
   if constexpr (active_policy.algorithm == ScanAlgorithm::lookahead)
