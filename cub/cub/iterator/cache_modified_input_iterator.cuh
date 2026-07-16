4471999608e641093ce89ea76edfe977e40fc873
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-07-16 10:34:07 +0200

    Use `thrust::is_contiguous_iterator_v` over `cuda::std::contiguous_iterator`
    The former trait includes more types that we can turn into pointers

--- cub/cub/iterator/cache_modified_input_iterator.cuh
+++ cub/cub/iterator/cache_modified_input_iterator.cuh
@@ -24,6 +24,7 @@
 
 #include <thrust/detail/raw_pointer_cast.h>
 #include <thrust/iterator/iterator_facade.h>
+#include <thrust/type_traits/is_contiguous_iterator.h>
 
 #include <cuda/std/__host_stdlib/ostream>
 #include <cuda/std/__iterator/iterator_traits.h>
@@ -226,9 +227,10 @@ inline constexpr bool is_CacheModifiedInputIterator<CacheModifiedInputIterator<M
 template <CacheLoadModifier LoadModifier, typename Iterator>
 _CCCL_HOST_DEVICE _CCCL_FORCEINLINE auto try_make_cache_modified_iterator(Iterator it)
 {
-  if constexpr (::cuda::std::contiguous_iterator<Iterator>)
+  if constexpr (THRUST_NS_QUALIFIER::is_contiguous_iterator_v<Iterator>)
   {
     return CacheModifiedInputIterator<LoadModifier, it_value_t<Iterator>, it_difference_t<Iterator>>{
+      // FIXME(bgruber): this should be THRUST_NS_QUALIFIER::unwrap_contiguous_iterator
       THRUST_NS_QUALIFIER::raw_pointer_cast(&*it)};
   }
   else
