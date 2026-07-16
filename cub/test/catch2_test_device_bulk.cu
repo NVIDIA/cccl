c52d64dbbcdb310c038c4ebdce9f21cf3bde191b
ahfoysal <ahfoysal30@gmail.com>
2026-07-16 14:57:45 +0600

    Unify incrementer_t test functors

--- cub/test/catch2_test_device_bulk.cu
+++ cub/test/catch2_test_device_bulk.cu
@@ -8,29 +8,15 @@
 #include <thrust/count.h>
 #include <thrust/detail/raw_pointer_cast.h>
 
-#include <cuda/std/type_traits>
-
 #include "catch2_test_launch_helper.h"
 #include <c2h/catch2_test_helper.h>
+#include <c2h/operator.cuh>
 
 // %PARAM% TEST_LAUNCH lid 0:1:2
 
 DECLARE_LAUNCH_WRAPPER(cub::DeviceFor::Bulk, device_bulk);
 
 using offset_type = c2h::type_list<std::int32_t, std::uint32_t, std::uint64_t, std::int64_t>;
-
-template <class T>
-struct incrementer_t
-{
-  int* d_counts;
-
-  template <class OffsetT>
-  __device__ void operator()(OffsetT i)
-  {
-    static_assert(cuda::std::is_same_v<T, OffsetT>, "T and OffsetT must be the same type");
-    atomicAdd(d_counts + i, 1); // Check if `i` was served more than once
-  }
-};
 
 C2H_TEST("Device bulk works", "[bulk][device]", offset_type)
 {
