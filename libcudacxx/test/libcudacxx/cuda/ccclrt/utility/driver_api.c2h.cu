155a9ed35c765dd1f8bef0fb97428789def7e6d6
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-07-17 11:14:28 -0400

    fixup! Push the streams context onto the driver stack when calling cumemcpy

--- libcudacxx/test/libcudacxx/cuda/ccclrt/utility/driver_api.c2h.cu
+++ libcudacxx/test/libcudacxx/cuda/ccclrt/utility/driver_api.c2h.cu
@@ -9,6 +9,8 @@
 //===----------------------------------------------------------------------===//
 
 #include <cuda/__driver/driver_api.h>
+#include <cuda/devices>
+#include <cuda/memory_pool>
 
 #include <testing.cuh>
 
@@ -73,4 +75,40 @@ C2H_TEST("Call each driver api", "[utility]")
   CCCLRT_REQUIRE(driver::__ctxGetCurrent() == ctx);
 
   CUDART(driver::__streamDestroyNoThrow(stream));
+}
+
+C2H_CCCLRT_TEST("memcpy async uses the stream context", "[utility][multi_gpu]")
+{
+  namespace driver = ::cuda::__driver;
+
+  if (cuda::devices.size() < 2)
+  {
+    SKIP("Need at least 2 devices");
+  }
+
+  cuda::device_ref current_device{0};
+  cuda::device_ref stream_device{1};
+  const auto initial_stack_depth = test::count_driver_stack();
+
+  {
+    const auto stream = cuda::stream{stream_device};
+    auto src          = cuda::make_device_buffer<int>(stream, stream_device, 1, cuda::no_init);
+    auto dst          = cuda::make_device_buffer<int>(stream, stream_device, 1, cuda::no_init);
+
+    {
+      const auto guard           = cuda::__ensure_current_context{current_device};
+      const auto current_context = driver::__ctxGetCurrent();
+      const auto stack_depth     = test::count_driver_stack();
+
+      REQUIRE(current_context != driver::__streamGetCtx(stream.get()));
+
+      driver::__memcpyAsync(dst.data(), src.data(), sizeof(*src.data()), stream.get());
+
+      REQUIRE(driver::__ctxGetCurrent() == current_context);
+      REQUIRE(test::count_driver_stack() == stack_depth);
+    }
+
+    stream.sync();
+  }
+  REQUIRE(test::count_driver_stack() == initial_stack_depth);
 }
