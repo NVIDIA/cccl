ad58174e92603a52ea91927f2063f77f45564580
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-07-23 11:25:52 -0400

    use config in the kernels

--- cudax/include/cuda/experimental/__multi_gpu/algorithm/sort/sample_probes.h
+++ cudax/include/cuda/experimental/__multi_gpu/algorithm/sort/sample_probes.h
@@ -30,6 +30,7 @@
 #include <cuda/std/__cstddef/types.h>
 #include <cuda/std/__optional/optional.h>
 #include <cuda/std/__random/philox_engine.h>
+#include <cuda/std/__type_traits/remove_cvref.h>
 #include <cuda/std/__utility/move.h>
 #include <cuda/std/cstdint>
 #include <cuda/std/span>
@@ -47,8 +48,9 @@ namespace cuda::experimental::__detail
 // Parallelize with multiple threads (but not too many!). __I_j is O(p-1), so in
 // practice at the absolute max a few thousand if you are running on the worlds
 // largest supercomputers.
-template <class _Tp, class _BinaryOp>
+template <class _Config, class _Tp, class _BinaryOp>
 _CCCL_KERNEL_ATTRIBUTES void __sample_probes_kernel(
+  _Config __config,
   ::cuda::std::philox4x64 __gen,
   const double __prob,
   const _Tp* __begin,
@@ -56,9 +58,9 @@ _CCCL_KERNEL_ATTRIBUTES void __sample_probes_kernel(
   const ::cuda::std::span<const ::cuda::std::pair<::cuda::std::optional<_Tp>, ::cuda::std::optional<_Tp>>> __I_j,
   _BinaryOp __cmp,
   const ::cuda::std::span<_Tp> __samples,
-  ::cuda::std::size_t* __samples_size)
+  ::cuda::std::size_t* const __samples_size)
 {
-  if (threadIdx.x != 0)
+  if (cuda::gpu_thread.rank(cuda::grid, __config) != 0)
   {
     // Just in case
     return;
@@ -66,7 +68,6 @@ _CCCL_KERNEL_ATTRIBUTES void __sample_probes_kernel(
 
   auto __samples_it = __samples.begin();
 
-  _CCCL_ASSERT(__prob > 0, "Cannot have 0 probably of picking elements");
   // By value so that load from global memory happens only once
   for (const auto [__lo, __hi] : __I_j)
   {
@@ -78,13 +79,13 @@ _CCCL_KERNEL_ATTRIBUTES void __sample_probes_kernel(
 
     _CCCL_ASSERT(__first <= __last, "Inputs are not sorted for binary search");
 
-    const auto __num_samples =
-      static_cast<::cuda::std::uint32_t>(::cuda::std::ceil(static_cast<double>(__last - __first) * __prob));
-    const auto __remaining_samples = static_cast<::cuda::std::uint32_t>(__samples.end() - __samples_it);
+    const auto __num_samples       = ::cuda::std::ceil(static_cast<double>(__last - __first) * __prob);
+    const auto __remaining_samples = __samples.end() - __samples_it;
+    const auto __n                 = ::cuda::std::min(
+      static_cast<::cuda::std::uint64_t>(__num_samples), static_cast<::cuda::std::uint64_t>(__remaining_samples));
 
-    __samples_it =
-      ::cuda::std::sample(__first, __last, __samples_it, ::cuda::std::min(__num_samples, __remaining_samples), __gen);
-    __begin = __last;
+    __samples_it = ::cuda::std::sample(__first, __last, __samples_it, __n, __gen);
+    __begin      = __last;
   }
 
   *__samples_size = static_cast<::cuda::std::size_t>(__samples_it - __samples.begin());
@@ -103,10 +104,12 @@ _CCCL_HOST_API void __sort_sample_probes(
   constexpr auto __config =
     ::cuda::make_config(::cuda::make_hierarchy(::cuda::block_dims<1>(), ::cuda::grid_dims<1>()));
 
+  _CCCL_VERIFY(__sampling_probability > 0, "Cannot have 0 probably of picking elements");
+
   ::cuda::launch(
     __stream,
     __config,
-    __sample_probes_kernel<_Tp, _BinaryOp>,
+    __sample_probes_kernel<::cuda::std::remove_cvref_t<decltype(__config)>, _Tp, _BinaryOp>,
     ::cuda::std::philox4x64{static_cast<::cuda::std::uint32_t>(__sampling_probability)},
     __sampling_probability,
     ::cuda::std::to_address(::cuda::std::ranges::begin(__input)),
