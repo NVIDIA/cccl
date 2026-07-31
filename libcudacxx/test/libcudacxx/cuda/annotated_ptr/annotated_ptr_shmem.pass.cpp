876abd9bcf24cca49cbe5e02a66fd38af48a8a01
Michael Schellenberger Costa <miscco@nvidia.com>
2026-07-31 08:04:05 +0200

    [Tile] Mark all atomics functions as `__host__ __device__` only

--- libcudacxx/test/libcudacxx/cuda/annotated_ptr/annotated_ptr_shmem.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/annotated_ptr/annotated_ptr_shmem.pass.cpp
@@ -8,9 +8,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-// UNSUPPORTED: enable-tile
-// error: asm statement is unsupported in tile code
-
 #include "test_macros.h"
 #include "utils.h"
 
