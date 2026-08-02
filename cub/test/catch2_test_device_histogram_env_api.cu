e790097e6c10be9204c4b53c26fa47836e0f06c6
Bryce Adelstein Lelbach <brycelelbach@gmail.com>
2026-08-02 10:56:48 +0000

    [cub] Use typed histogram privatization modes

--- cub/test/catch2_test_device_histogram_env_api.cu
+++ cub/test/catch2_test_device_histogram_env_api.cu
@@ -419,18 +419,17 @@ struct HistogramPolicySelector
   __host__ __device__ constexpr auto operator()(cuda::compute_capability cc) const -> cub::HistogramPolicy
   {
     const auto sweep = cub::HistogramSweepPolicy{
-      128,
-      cc > cuda::compute_capability{9, 0} ? 16 : 7,
-      4,
-      cub::BLOCK_LOAD_DIRECT,
-      cub::LOAD_LDG,
-      false,
-      cub::SMEM,
-      false};
-    return {.gmem                             = sweep,
-            .static_smem                      = {.sweep = sweep, .max_bins = 256, .min_blocks_per_sm = 0},
-            .dynamic_smem                     = {.sweep = sweep},
-            .init_kernel_pdl_trigger_max_bins = 2048};
+      128, cc > cuda::compute_capability{9, 0} ? 16 : 7, 4, cub::BLOCK_LOAD_DIRECT, cub::LOAD_LDG, false, false};
+    return {
+      .gmem         = sweep,
+      .static_smem  = {.sweep = sweep, .max_privatized_smem_bytes = 257 * sizeof(unsigned int), .min_blocks_per_sm = 0},
+      .dynamic_smem = {.sweep                     = sweep,
+                       .max_privatized_smem_bytes = 0,
+                       .range_max_bins            = 0,
+                       .even_2ch_max_bins         = 0,
+                       .even_3ch_max_bins         = 0,
+                       .even_4ch_max_bins         = 0},
+      .init_kernel_pdl_trigger_max_bins = 2048};
   }
 };
 // example-end histogram-even-policy-selector
