a787b4a8a158285ecda49f2ded67cef1453cf350
Michael Schellenberger Costa <miscco@nvidia.com>
2026-08-03 13:12:11 +0200

    [Tile] For now mark `<inplace_vector>` tests as host device only
    They are currently broken due to a compiler bug. We are still evaluating a fix

--- libcudacxx/test/libcudacxx/std/text/format/format.functions/format_to_n.pass.cpp
+++ libcudacxx/test/libcudacxx/std/text/format/format.functions/format_to_n.pass.cpp
@@ -10,6 +10,9 @@
 // UNSUPPORTED: force-tile
 // error: calling a __host__ __device__ function in tile is not allowed
 
+// UNSUPPORTED: enable-tile
+// nvbug6537343: error: Internal Compiler Error (tile codegen): "Static local variables not handled yet."
+
 // <cuda/std/format>
 
 // template<class Out, class... Args>
