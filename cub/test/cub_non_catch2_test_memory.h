6c940f9423c2e66c60f7a63d9e3d5d98f9b92644
Ratna Chandu <vgembali@nvidia.com>
2026-08-03 20:02:31 +0000

    cub: clarify non-Catch2 memory tag cleanup

--- cub/test/cub_non_catch2_test_memory.h
+++ cub/test/cub_non_catch2_test_memory.h
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: BSD-3
 
+// TODO: Remove this header once test_device_batch_copy.cu is migrated to Catch2.
+// Make all remaining non-Catch2 runtime tests run as CUB_SMALL.
+
 #pragma once
 
 // Non-Catch2 runtime tests must declare exactly one memory class. CMake checks
@@ -9,6 +12,4 @@
 #define CUB_TEST_MEMORY_CLASS(MEMORY_CLASS) CUB_TEST_MEMORY_CLASS_##MEMORY_CLASS
 
 #define CUB_TEST_MEMORY_CLASS_CUB_SMALL static_assert(true)
-// TODO: Remove CUB_LARGE once test_device_batch_copy.cu is migrated to Catch2.
-// All remaining non-Catch2 runtime tests are small.
 #define CUB_TEST_MEMORY_CLASS_CUB_LARGE static_assert(true)
