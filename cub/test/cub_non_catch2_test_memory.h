b32787c1e995678964162056c167f3f6b86ab44e
Ratna Chandu <vgembali@nvidia.com>
2026-08-03 19:55:33 +0000

    cub: document non-Catch2 large-memory exception

--- cub/test/cub_non_catch2_test_memory.h
+++ cub/test/cub_non_catch2_test_memory.h
@@ -9,4 +9,6 @@
 #define CUB_TEST_MEMORY_CLASS(MEMORY_CLASS) CUB_TEST_MEMORY_CLASS_##MEMORY_CLASS
 
 #define CUB_TEST_MEMORY_CLASS_CUB_SMALL static_assert(true)
+// TODO: Remove CUB_LARGE once test_device_batch_copy.cu is migrated to Catch2.
+// All remaining non-Catch2 runtime tests are small.
 #define CUB_TEST_MEMORY_CLASS_CUB_LARGE static_assert(true)
