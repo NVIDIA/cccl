ab5334f00cd4dc97b56d78b69bdd48cb325b8ce1
Yunsong Wang <yunsongw@nvidia.com>
2026-08-07 17:46:07 +0000

    Simplify hash tests
    Signed-off-by: Yunsong Wang <yunsongw@nvidia.com>

--- libcudacxx/test/libcudacxx/cuda/functional/hash.xxhash_32.pass.cpp
+++ libcudacxx/test/libcudacxx/cuda/functional/hash.xxhash_32.pass.cpp
@@ -14,33 +14,25 @@
 #include "hash_test_helper.h"
 #include "test_macros.h"
 
-struct test_xxhash32
+TEST_FUNC void test()
 {
   hash_test<cuda::hash_algorithm::xxhash_32> xxhash32_test;
 
-  TEST_FUNC void operator()()
-  {
-    xxhash32_test(static_cast<char>(0), 3479547966u, 0);
-    xxhash32_test(static_cast<char>(42), 3774771295u, 0);
-    xxhash32_test(static_cast<char>(0), 2099223482u, 42);
-    xxhash32_test(static_cast<cuda::std::int32_t>(0), 148298089u, 0);
-    xxhash32_test(static_cast<cuda::std::int32_t>(0), 2132181312u, 42);
-    xxhash32_test(static_cast<cuda::std::int32_t>(42), 1161967057u, 0);
-    xxhash32_test(static_cast<cuda::std::int32_t>(123456789), 2987034094u, 0);
-    xxhash32_test(static_cast<cuda::std::int64_t>(0), 3736311059u, 0);
-    xxhash32_test(static_cast<cuda::std::int64_t>(0), 1076387279u, 42);
-    xxhash32_test(static_cast<cuda::std::int64_t>(42), 2332451213u, 0);
-    xxhash32_test(static_cast<cuda::std::int64_t>(123456789), 1561711919u, 0);
+  xxhash32_test(static_cast<char>(0), 3479547966u, 0);
+  xxhash32_test(static_cast<char>(42), 3774771295u, 0);
+  xxhash32_test(static_cast<char>(0), 2099223482u, 42);
+  xxhash32_test(static_cast<cuda::std::int32_t>(0), 148298089u, 0);
+  xxhash32_test(static_cast<cuda::std::int32_t>(0), 2132181312u, 42);
+  xxhash32_test(static_cast<cuda::std::int32_t>(42), 1161967057u, 0);
+  xxhash32_test(static_cast<cuda::std::int32_t>(123456789), 2987034094u, 0);
+  xxhash32_test(static_cast<cuda::std::int64_t>(0), 3736311059u, 0);
+  xxhash32_test(static_cast<cuda::std::int64_t>(0), 1076387279u, 42);
+  xxhash32_test(static_cast<cuda::std::int64_t>(42), 2332451213u, 0);
+  xxhash32_test(static_cast<cuda::std::int64_t>(123456789), 1561711919u, 0);
 #if _CCCL_HAS_INT128()
-    xxhash32_test(static_cast<__int128_t>(123456789), 1846633701u, 0);
+  xxhash32_test(static_cast<__int128_t>(123456789), 1846633701u, 0);
 #endif
-    xxhash32_test(large_key<32>(123456789), 3715432378u, 0);
-  }
-};
-
-TEST_FUNC void test()
-{
-  test_xxhash32{}();
+  xxhash32_test(large_key<32>(123456789), 3715432378u, 0);
 }
 
 int main(int, char**)
