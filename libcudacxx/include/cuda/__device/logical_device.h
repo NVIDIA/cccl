112cda956882b4a1aab74254c4de682ad231726f
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-08-18 16:16:55 -0400

    Add __logical_device to model a green context subdevice

---
+++ libcudacxx/include/cuda/__device/logical_device.h
@@ -0,0 +1,90 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _CUDA___DEVICE_LOGICAL_DEVICE_H
+#define _CUDA___DEVICE_LOGICAL_DEVICE_H
+
+#include <cuda/__cccl_config>
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+#include <cuda/__device/logical_device_ref.h>
+#include <cuda/std/__cstddef/types.h>
+#include <cuda/std/__utility/exchange.h>
+#include <cuda/std/__utility/move.h>
+
+#include <cuda/std/__cccl/prologue.h>
+
+// Forward declare so we dont need CUDA 12.5 headers
+struct CUgreenCtx_st;
+using CUgreenCtx = ::CUgreenCtx_st*;
+
+_CCCL_BEGIN_NAMESPACE_CUDA
+
+class __logical_device : public __logical_device_ref
+{
+public:
+  _CCCL_HOST_API static constexpr __logical_device from_native_handle(device_ref __device, ::CUgreenCtx __gctx) noexcept
+  {
+    return {__device, __gctx};
+  }
+
+  static __logical_device from_native_handle(device_ref, int)                    = delete;
+  static __logical_device from_native_handle(device_ref, ::cuda::std::nullptr_t) = delete;
+
+  __logical_device(const __logical_device&)            = delete;
+  __logical_device& operator=(const __logical_device&) = delete;
+
+  _CCCL_HOST_API constexpr __logical_device(__logical_device&& __other) noexcept
+      : __logical_device_ref{::cuda::std::move(__other.__device_), ::cuda::std::exchange(__other.__gctx_, nullptr)}
+  {}
+
+  _CCCL_HOST_API __logical_device& operator=(__logical_device&& __other) noexcept
+  {
+    if (this != &__other)
+    {
+      __reset();
+      __device_ = ::cuda::std::move(__other.__device_);
+      __gctx_   = ::cuda::std::exchange(__other.__gctx_, nullptr);
+    }
+    return *this;
+  }
+
+  _CCCL_HOST_API ~__logical_device()
+  {
+    __reset();
+  }
+
+private:
+  _CCCL_HOST_API constexpr __logical_device(device_ref __device, ::CUgreenCtx __gctx) noexcept
+      : __logical_device_ref{__device, __gctx}
+  {}
+
+  _CCCL_HOST_API void __reset() noexcept
+  {
+    if (__gctx_)
+    {
+      static_cast<void>(::cuda::__driver::__greenCtxDestroyNoThrow(__gctx_));
+      __gctx_ = nullptr;
+    }
+  }
+};
+
+_CCCL_END_NAMESPACE_CUDA
+
+#include <cuda/std/__cccl/epilogue.h>
+
+#endif // _CUDA___DEVICE_LOGICAL_DEVICE_CUH
---
+++ libcudacxx/include/cuda/__device/logical_device_ref.h
@@ -0,0 +1,85 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _CUDA___DEVICE_LOGICAL_DEVICE_REF_H
+#define _CUDA___DEVICE_LOGICAL_DEVICE_REF_H
+
+#include <cuda/__cccl_config>
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+#include <cuda/__device/device_ref.h>
+
+#include <cuda/std/__cccl/prologue.h>
+
+// Forward declare so we dont need CUDA 12.5 headers
+struct CUgreenCtx_st;
+using CUgreenCtx = ::CUgreenCtx_st*;
+
+_CCCL_BEGIN_NAMESPACE_CUDA
+
+class __logical_device_ref
+{
+public:
+  _CCCL_HOST_API constexpr __logical_device_ref(device_ref __dev, ::CUgreenCtx __gctx) noexcept
+      : __device_{__dev}
+      , __gctx_{__gctx}
+  {}
+
+  _CCCL_HOST_API [[nodiscard]] constexpr device_ref underlying_device() const noexcept
+  {
+    return __device_;
+  }
+
+  _CCCL_HOST_API [[nodiscard]] constexpr ::CUgreenCtx green_context() const noexcept
+  {
+    return __gctx_;
+  }
+
+  //! @brief Compares two logical_devices for equality
+  //!
+  //! @param __lhs The first `logical_device` to compare
+  //! @param __rhs The second `logical_device` to compare
+  //! @return true if `lhs` and `rhs` refer to the same logical device
+  _CCCL_HOST_API [[nodiscard]] friend constexpr bool
+  operator==(const __logical_device_ref& __lhs, const __logical_device_ref& __rhs) noexcept
+  {
+    return __lhs.__device_ == __rhs.__device_ && __lhs.__gctx_ == __rhs.__gctx_;
+  }
+
+#if _CCCL_STD_VER <= 2017
+  //! @brief Compares two logical_devices for inequality
+  //!
+  //! @param __lhs The first `logical_device` to compare
+  //! @param __rhs The second `logical_device` to compare
+  //! @return true if `lhs` and `rhs` refer to the different logical device
+  _CCCL_HOST_API [[nodiscard]] friend constexpr bool
+  operator!=(const __logical_device_ref& __lhs, const __logical_device_ref& __rhs) noexcept
+  {
+    return !(__lhs == __rhs);
+  }
+#endif // _CCCL_STD_VER <= 2017
+
+protected:
+  device_ref __device_{0};
+  ::CUgreenCtx __gctx_{};
+};
+
+_CCCL_END_NAMESPACE_CUDA
+
+#include <cuda/std/__cccl/epilogue.h>
+
+#endif // _CUDA___DEVICE_LOGICAL_DEVICE_REF_CUH
