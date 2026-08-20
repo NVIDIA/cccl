3374a4bd8d91bf4df77d10c218cd26c33100db74
Michael Schellenberger Costa <miscco@nvidia.com>
2026-08-20 12:47:12 +0200

    Fix review comment

--- ci/windows/build_cccl_c_parallel_v2.ps1
+++ ci/windows/build_cccl_c_parallel_v2.ps1
@@ -19,7 +19,7 @@ If($CURRENT_PATH -ne "ci") {
 }
 
 Remove-Module -Name build_common -ErrorAction SilentlyContinue
-Import-Module $PSScriptRoot/build_common.psm1 -ArgumentList @(20, $CUDA_ARCH, $CMAKE_OPTIONS)
+Import-Module $PSScriptRoot/build_common.psm1 -ArgumentList @(20, $CUDA_ARCH, $CMAKE_OPTIONS, $ENABLE_TILE)
 
 $PRESET = "cccl-c-parallel-v2"
 $LOCAL_CMAKE_OPTIONS = ""
