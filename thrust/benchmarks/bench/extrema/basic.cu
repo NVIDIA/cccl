5644277f85117870068e15d434e5d518096920cd
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-08-23 23:20:18 +0200

    Fix

--- thrust/benchmarks/bench/extrema/basic.cu
+++ thrust/benchmarks/bench/extrema/basic.cu
@@ -8,7 +8,7 @@
 #include "nvbench_helper.cuh"
 
 template <typename T, typename Func>
-static void bench_extremum(nvbench::state& state, nvbench::type_list<T>, Func func)
+static void bench_extremum(nvbench::state& state, nvbench::type_list<T>, Func func, int output_count = 1)
 {
   const auto elements = static_cast<std::size_t>(state.get_int64("Elements"));
 
@@ -18,7 +18,7 @@ static void bench_extremum(nvbench::state& state, nvbench::type_list<T>, Func fu
 
   state.add_element_count(elements);
   state.add_global_memory_reads<T>(elements);
-  state.add_global_memory_writes<offset_t>(1);
+  state.add_global_memory_writes<offset_t>(output_count);
 
   caching_allocator_t alloc;
   state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync,
@@ -56,9 +56,13 @@ NVBENCH_BENCH_TYPES(max_element, NVBENCH_TYPE_AXES(fundamental_types))
 template <typename T>
 static void minmax_element(nvbench::state& state, nvbench::type_list<T> list)
 {
-  bench_extremum(state, list, [](auto&&... args) {
-    return thrust::minmax_element(args...);
-  });
+  bench_extremum(
+    state,
+    list,
+    [](auto&&... args) {
+      return thrust::minmax_element(args...);
+    },
+    /* output_count = */ 2);
 }
 
 NVBENCH_BENCH_TYPES(minmax_element, NVBENCH_TYPE_AXES(fundamental_types))
