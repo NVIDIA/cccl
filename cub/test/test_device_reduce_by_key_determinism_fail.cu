83828fa062034e8e93924ca4a05ff80e6b865ca2
Ahmed Taha <74939755+ahmedtaha100@users.noreply.github.com>
2026-08-30 23:23:33 -0400

    [CUB] Make determinism test locals const

--- cub/test/test_device_reduce_by_key_determinism_fail.cu
+++ cub/test/test_device_reduce_by_key_determinism_fail.cu
@@ -11,15 +11,15 @@
 
 int main()
 {
-  int* keys{};
-  double* values{};
-  int* num_runs{};
+  int* const keys{};
+  double* const values{};
+  int* const num_runs{};
 
 #if TEST_ERR == 0
   // clang-format off
   // expected-error-0 {{"run_to_run deterministic reduce-by-key requires integral types with known operators, primitive types with min/max, or floating-point types with plus operator"}}
   // clang-format on
-  auto error = cub::DeviceReduce::ReduceByKey(
+  const auto error = cub::DeviceReduce::ReduceByKey(
     keys,
     keys,
     values,
@@ -32,7 +32,7 @@ int main()
   // clang-format off
   // expected-error-1 {{"gpu_to_gpu deterministic reduce-by-key requires integral types with known operators or primitive types with min/max"}}
   // clang-format on
-  auto error = cub::DeviceReduce::ReduceByKey(
+  const auto error = cub::DeviceReduce::ReduceByKey(
     keys,
     keys,
     values,
