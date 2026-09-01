a386d5c196a687a5f4f399c90ebb2a048300a63c
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-09-01 10:27:29 -0400

    Get the current context from the stream not comm (#11067)
    * Get the current context from the stream not comm

--- cudax/include/cuda/experimental/__multi_gpu/algorithm/sort/hss/rebalance.h
+++ cudax/include/cuda/experimental/__multi_gpu/algorithm/sort/hss/rebalance.h
@@ -192,7 +192,7 @@ _CCCL_HOST_API void _HSSSorter<_Tp, _Env, _BinaryOp>::__rebalance_to_original_co
         __setup.__all_local_offsets[__idx].data()};
 
       __CUDAX_MULTI_GPU_DISPATCH(
-        __comm_it->logical_device(),
+        __counts.stream(),
         CUB_NS_QUALIFIER::DeviceTransform::Transform,
         ::cuda::counting_iterator<::cuda::std::uint64_t>{},
         ::cuda::std::move(__out),
@@ -211,9 +211,8 @@ _CCCL_HOST_API void _HSSSorter<_Tp, _Env, _BinaryOp>::__rebalance_to_original_co
           __counts.stream(),
           __counts,
           ::cuda::std::span<::cuda::std::size_t>{__h_counts_dest.data(), __num_columns * __h_counts_dest.size()},
-          ::cuda::copy_configuration{__comm_it->logical_device().underlying_device(),
-                                     ::cuda::host_memory_location,
-                                     ::cuda::source_access_order::stream});
+          ::cuda::copy_configuration{
+            __counts.stream().device(), ::cuda::host_memory_location, ::cuda::source_access_order::stream});
       }
 
       __local_rebalanced.emplace_back(
@@ -247,20 +246,19 @@ _CCCL_HOST_API void _HSSSorter<_Tp, _Env, _BinaryOp>::__rebalance_to_original_co
   }
 
   {
-    auto __comm_it      = ::cuda::std::ranges::begin(__comms);
     auto __env_it       = ::cuda::std::ranges::begin(__envs);
     auto __input_it     = ::cuda::std::ranges::begin(__input_iters);
     auto __num_items_it = ::cuda::std::ranges::begin(__num_items_range);
 
     for (::cuda::std::size_t __idx = 0; __idx < __num_local_inputs;
-         (void) ++__idx, (void) ++__comm_it, (void) ++__env_it, (void) ++__input_it, (void) ++__num_items_it)
+         (void) ++__idx, (void) ++__env_it, (void) ++__input_it, (void) ++__num_items_it)
     {
       const auto __n = *__num_items_it;
 
       _CCCL_VERIFY(__n == __local_rebalanced[__idx].size(), "Incorrect sizing for temp storage");
 
       __CUDAX_MULTI_GPU_DISPATCH(
-        __comm_it->logical_device(),
+        __local_rebalanced[__idx].stream(),
         CUB_NS_QUALIFIER::DeviceCopy::Copy,
         ::cuda::std::mdspan{__local_rebalanced[__idx].data(), __local_rebalanced[__idx].size()},
         ::cuda::std::mdspan{::cuda::std::to_address(*__input_it), __n},
