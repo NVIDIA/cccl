f326f583238d7e426ffcb283b8d1350e7cc5c3a9
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-09-03 17:15:50 +0200

    Format

--- thrust/testing/functional_placeholders_arithmetic.cu
+++ thrust/testing/functional_placeholders_arithmetic.cu
@@ -44,17 +44,17 @@ _CCCL_DIAG_SUPPRESS_MSVC(4244) // warning C4244: '=': conversion from 'int' to '
       ASSERT_ALMOST_EQUAL(reference, result);                                                                   \
     }                                                                                                           \
   };                                                                                                            \
-  DECLARE_VECTOR_UNITTEST_WITH_TYPES_AND_NAME(                                                                    \
-    TestFunctionalPlaceholders##name,                                                                             \
-    type_list,                                                                                                    \
-    thrust::device_vector,                                                                                        \
-    thrust::device_allocator,                                                                                     \
-    TestFunctionalPlaceholders##name##Device);                                                                    \
-  DECLARE_VECTOR_UNITTEST_WITH_TYPES_AND_NAME(                                                                    \
-    TestFunctionalPlaceholders##name,                                                                             \
-    type_list,                                                                                                    \
-    thrust::host_vector,                                                                                          \
-    std::allocator,                                                                                               \
+  DECLARE_VECTOR_UNITTEST_WITH_TYPES_AND_NAME(                                                                  \
+    TestFunctionalPlaceholders##name,                                                                           \
+    type_list,                                                                                                  \
+    thrust::device_vector,                                                                                      \
+    thrust::device_allocator,                                                                                   \
+    TestFunctionalPlaceholders##name##Device);                                                                  \
+  DECLARE_VECTOR_UNITTEST_WITH_TYPES_AND_NAME(                                                                  \
+    TestFunctionalPlaceholders##name,                                                                           \
+    type_list,                                                                                                  \
+    thrust::host_vector,                                                                                        \
+    std::allocator,                                                                                             \
     TestFunctionalPlaceholders##name##Host);
 
 BINARY_FUNCTIONAL_PLACEHOLDERS_TEST(Plus, +, ::cuda::std::plus, ThirtyTwoBitTypes);
