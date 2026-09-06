c4cb9b4d0e258c0281efc7002f69f0ed433be151
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-09-06 23:32:29 +0200

    Fix

--- thrust/testing/unittest/unittest.h
+++ thrust/testing/unittest/unittest.h
@@ -830,7 +830,7 @@ void for_each_type(L<Ts...>, Args&&... args)
   // don't forward args, since the arguments are passed multiple times
 #if _CCCL_COMPILER(MSVC, <=, 19, 50)
   // MSVC crashes with a C1001 internal compiler error on the fold-expression below, so expand into an initializer list
-  [[maybe_unused]] int dummy[] = {(TestFunc<Ts>{}(args...), 0)...};
+  [[maybe_unused]] int dummy[] = {0, (TestFunc<Ts>{}(args...), 0)...}; // leading 0 in case args is empty
 #else // _CCCL_COMPILER(MSVC, ==, 19, 50)
   (..., TestFunc<Ts>{}(args...));
 #endif // _CCCL_COMPILER(MSVC, ==, 19, 50)
