a23457a6f2fe1b9cb36d8d64292d2f0a82f5d99b
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-09-07 14:18:11 +0200

    Tile and int128

--- thrust/testing/cuda/sort.cu
+++ thrust/testing/cuda/sort.cu
@@ -176,10 +176,11 @@ struct TestRadixSortDispatch
 using test_types_radix_sort = cuda::std::__type_concat<
   IntegralTypes,
   FloatingPointTypes
-#if _CCCL_HAS_INT128()
+// nvcc's tile codegen does not support 128-bit integer types
+#if _CCCL_HAS_INT128() && !defined(__CUDACC_TILE__)
   ,
   unittest::type_list<__int128_t, __uint128_t>
-#endif // _CCCL_HAS_INT128()
+#endif // _CCCL_HAS_INT128() && !defined(__CUDACC_TILE__)
 #if _CCCL_HAS_NVFP16()
   ,
   unittest::type_list<__half>
@@ -314,10 +315,11 @@ struct TestSortAscendingKey
 
 using test_types_ascending_key = cuda::std::__type_concat<
   unittest::type_list<>
-#if _CCCL_HAS_INT128()
+// nvcc's tile codegen does not support 128-bit integer types
+#if _CCCL_HAS_INT128() && !defined(__CUDACC_TILE__)
   ,
   unittest::type_list<__int128_t, __uint128_t>
-#endif // _CCCL_HAS_INT128()
+#endif // _CCCL_HAS_INT128() && !defined(__CUDACC_TILE__)
 // CTK 12.2 offers __host__ __device__ operators for __half and __nv_bfloat16, so we can use std::sort
 #if _CCCL_CTK_AT_LEAST(12, 2)
 #  if _CCCL_HAS_NVFP16() || !defined(__CUDA_NO_HALF_OPERATORS__) && !defined(__CUDA_NO_HALF_CONVERSIONS__)
