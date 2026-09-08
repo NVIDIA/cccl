dec8b7cd416049ab8238b63c2102affd2c8f9a22
Wint3rNight <pratap2003singh@gmail.com>
2026-09-09 04:53:03 +0530

    Drop complex types from the WarpScan::Sum benchmark

--- cub/benchmarks/bench/scan/warp_scan_sum.cu
+++ cub/benchmarks/bench/scan/warp_scan_sum.cu
@@ -18,18 +18,12 @@ using value_types = nvbench::type_list<
   __nv_bfloat16,
 #endif
   float,
-  double,
+  double
 #if _CCCL_HAS_FLOAT128()
-  __float128,
+  ,
+  __float128
 #endif
-#if _CCCL_HAS_NVFP16() && _CCCL_CTK_AT_LEAST(12, 2)
-  cuda::std::complex<__half>,
-#endif
-#if _CCCL_HAS_NVBF16() && _CCCL_CTK_AT_LEAST(12, 2)
-  cuda::std::complex<__nv_bfloat16>,
-#endif
-  cuda::std::complex<float>,
-  cuda::std::complex<double>>;
+  >;
 
 using op_t = ::cuda::std::plus<>;
 #include "warp_scan_base.cuh"
