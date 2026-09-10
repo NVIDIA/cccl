aec077b95ed4f00be6b0a265b57c0bcd5d12199f
fbusato <fbusato@nvidia.com>
2026-09-10 13:20:41 -0700

    try to fix clang-tidy issue

--- libcudacxx/include/cuda/__mdspan/__copy/tensor_query.h
+++ libcudacxx/include/cuda/__mdspan/__copy/tensor_query.h
@@ -94,8 +94,9 @@ __stride_order(const __raw_tensor<_ExtentT, _StrideT, _Tp, _MaxRank>& __tensor) 
   {
     __perm[__i] = __i;
   }
-  ::cuda::std::stable_sort(
-    __perm.begin(), __perm.begin() + __tensor.__rank, __stride_compare<_StrideT, _MaxRank>{__tensor.__strides});
+  ::cuda::std::stable_sort(__perm.begin(),
+                           __perm.begin() + __tensor.__rank,
+                           __stride_compare<_StrideT, _MaxRank>{__tensor.__strides}); // NOLINT
   return __perm;
 }
 
