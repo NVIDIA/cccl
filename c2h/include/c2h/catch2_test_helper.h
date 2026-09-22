b449ce395e047b04ee9959d1499fb397b8a61b48
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-09-22 10:27:20 -0400

    cppcoreguidelines-virtual-class-destructor

--- c2h/include/c2h/catch2_test_helper.h
+++ c2h/include/c2h/catch2_test_helper.h
@@ -239,6 +239,8 @@ public:
       , m_matcher(matcher)
   {}
 
+  virtual ~QuietMatchExpr() = default;
+
   void streamReconstructedExpression(std::ostream& os) const override
   {
     os << m_matcher.toString();
